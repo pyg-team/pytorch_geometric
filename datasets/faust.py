590918813e039c79b37f7d0b4c6b11922883bc53
rusty1s <matthias.fey@tu-dortmund.de>
2017-10-08 12:41:14 +0200

    faust boiler

---
+++ datasets/faust.py
@@ -0,0 +1,12 @@
+from torch.utils.data import Dataset
+
+
+class FAUST(Dataset):
+    def __init__(self, root, train, transform, target_transform):
+        pass
+
+    def __getitem__(self):
+        pass
+
+    def __len__(self):
+        pass
