1c30923686977ee9ac7a979a610c8acfce75122c
rusty1s <matthias.fey@tu-dortmund.de>
2017-10-08 18:39:24 +0200

    no download because of access rights

--- datasets/faust.py
+++ datasets/faust.py
@@ -1,19 +1,15 @@
 import os
 
 from torch.utils.data import Dataset
-from torchvision.datasets.utils import download_url
 
 
 class FAUST(Dataset):
     """`MPI FAUST <http://faust.is.tue.mpg.de/>`_ Dataset.
     Args:
-        root (string): Root directory of dataset where ``processed/training.pt``
-            and  ``processed/test.pt`` exist.
+        root (string): Root directory of dataset where
+            ``processed/training.pt`` and  ``processed/test.pt`` exist.
         train (bool, optional): If True, creates dataset from ``training.pt``,
             otherwise from ``test.pt``.
-        download (bool, optional): If true, downloads the dataset from the
-            internet and puts it in root directory. If dataset is already
-            downloaded, it is not downloaded again.
         transform (callable, optional): A function/transform that  takes in an
             PIL image and returns a transformed version. E.g,
             ``transforms.RandomCrop``
@@ -21,27 +17,22 @@ class FAUST(Dataset):
             in the target and transforms it.
     """
 
-    url = 'http://faust.is.tue.mpg.de/main/download'
-    filename = 'MPI-FAUST.zip'
-    md5 = 'c58f30108f718f92721af3b95e74349a'
+    url = 'http://faust.is.tue.mpg.de/'
+    training_file = 'training.pt'
+    test_file = 'test.pt'
 
-    def __init__(self,
-                 root,
-                 train=True,
-                 download=False,
-                 transform=None,
+    def __init__(self, root, train=True, transform=None,
                  target_transform=None):
         self.root = os.path.expanduser(root)
         self.train = train
         self.transform = transform
         self.target_transform = target_transform
 
-        if download:
-            self.download()
+        self.process()
 
         if not self._check_exists():
-            raise RuntimeError('Dataset not found or corrupted. You can use' +
-                               ' download=True to download it')
+            raise RuntimeError('Dataset not found. Please download it from ' +
+                               '{}'.format(self.url))
         pass
 
     def __getitem__(self, index):
@@ -60,14 +51,13 @@ class FAUST(Dataset):
         pass
 
     def _check_exists(self):
+        return os.path.exists(self.root)
+
+    def _check_processed(self):
         return False
 
-    def download(self):
-        if self._check_exists():
-            return
-
-        download_url(self.url, self.root, self.filename, self.md5)
-        print('ihihi')
+    def process(self):
+        pass
 
 
-FAUST('.', download=True)
+FAUST('~/Downloads/MPI-FAUST')
