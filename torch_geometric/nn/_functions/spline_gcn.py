c180f580a03e2eccd36fd014e195ed03834eb3d8
rusty1s <matthias.fey@tu-dortmund.de>
2017-10-13 07:54:54 +0200

    lint fixes

--- torch_geometric/nn/_functions/spline_gcn.py
+++ torch_geometric/nn/_functions/spline_gcn.py
@@ -1,15 +1,22 @@
-import torch
+# import torch
 
-from ...sparse import mm, sum
+# from ...sparse import mm, sum
 
 
 def spline_gcn(adj, features, weight, bias, kernel_size, spline_degree):
     indices, dist, ang1, ang2 = adj
+    # Maybe we can obtain an ang matrix with [2, |E|]?
     # TODO: Implement spline_degree.values > 1
 
-    # Compute B and C:
-    # Than B and C will have size [dim, degree[i] + 1, |E|]
+    # COMPUTE B AND C with size [dim, spline_degree, |E|]
+    # 1. Scale ang1 and ang2 to be in interval [0, 1]
 
-    # We get
+    # 2. Calculate close b-spline for ang1 and ang2
 
-    # So wie geht es?
+    # 3. Calculate open b-spline for dist
+
+    # 4. Concatenate to one big tensor (maybe unneccassary, will see)
+
+    # DO THE CONVOLUTION:
+
+    # 1.
