93b0630b7e3beec8e1aa3f7b9e916b4306bae3d9
rusty1s <matthias.fey@tu-dortmund.de>
2017-10-17 16:52:49 +0200

    added tests

--- test/graph/test_geometry.py
+++ test/graph/test_geometry.py
@@ -2,7 +2,7 @@ from math import pi as PI
 
 from unittest import TestCase
 import torch
-from numpy.testing import assert_equal
+from numpy.testing import assert_equal, assert_almost_equal
 
 from torch_geometric.graph.geometry import (vec2ang, polar_coordinates,
                                             edges_from_faces)
@@ -17,8 +17,20 @@ class GeometryTest(TestCase):
 
         assert_equal((vec2ang(y, x) / PI).numpy(), expected)
 
-    def test_polar_coordinates(self):
-        pass
+    def test_2d_polar_coordinates(self):
+        vertices = torch.FloatTensor([[0, -1], [-1, 0], [0, 1], [1, 0]])
+        edges = torch.LongTensor([
+            [0, 0, 0, 1, 1, 2, 2, 2, 3, 3],
+            [1, 2, 3, 0, 2, 0, 1, 3, 0, 2],
+        ])
+
+        dists, angs = polar_coordinates(vertices, edges)
+
+        expected_dists = [1.4, 2, 1.4, 1.4, 1.4, 2, 1.4, 1.4, 1.4, 1.4]
+        assert_almost_equal(dists.numpy(), expected_dists, 1)
+
+        expected_angs = [1.75, 2, 0.25, 0.75, 0.25, 1, 1.25, 0.75, 1.25, 1.75]
+        assert_equal((angs / PI).numpy(), expected_angs)
 
     def test_edges_from_faces(self):
         faces = torch.LongTensor([[2, 3, 0], [1, 0, 2]])
