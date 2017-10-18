dbd79c1ed8511096c4e0f0593511526442a4fc61
rusty1s <matthias.fey@tu-dortmund.de>
2017-10-18 06:45:16 +0200

    fixed polar coordinates

--- test/graph/test_geometry.py
+++ test/graph/test_geometry.py
@@ -10,15 +10,15 @@ from torch_geometric.graph.geometry import (vec2ang, polar_coordinates,
 
 class GeometryTest(TestCase):
     def test_vec2ang(self):
-        y = torch.FloatTensor([0.5, 1, 0.5, 0, -0.5, -1, -0.5, 0])
-        x = torch.FloatTensor([0.5, 0, -0.5, -1, -0.5, 0, 0.5, 1])
+        x = torch.FloatTensor([0.5, 1, 0.5, 0, -0.5, -1, -0.5, 0])
+        y = torch.FloatTensor([0.5, 0, -0.5, -1, -0.5, 0, 0.5, 1])
 
         expected = [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2]
 
-        assert_equal((vec2ang(y, x) / PI).numpy(), expected)
+        assert_almost_equal((vec2ang(x, y) / PI).numpy(), expected, 2)
 
     def test_2d_polar_coordinates(self):
-        vertices = torch.FloatTensor([[0, -1], [-1, 0], [0, 1], [1, 0]])
+        vertices = torch.FloatTensor([[-1, -0], [0, -1], [1, 0], [0, 1]])
         edges = torch.LongTensor([
             [0, 0, 0, 1, 1, 2, 2, 2, 3, 3],
             [1, 2, 3, 0, 2, 0, 1, 3, 0, 2],
@@ -30,7 +30,23 @@ class GeometryTest(TestCase):
         assert_almost_equal(dists.numpy(), expected_dists, 1)
 
         expected_angs = [1.75, 2, 0.25, 0.75, 0.25, 1, 1.25, 0.75, 1.25, 1.75]
-        assert_equal((angs / PI).numpy(), expected_angs)
+        assert_almost_equal((angs / PI).numpy(), expected_angs, 2)
+
+    def test_3d_polar_coordinates(self):
+        vertices = torch.FloatTensor([[0, 0, 0], [1, 1, 1]])
+        edges = torch.LongTensor([[0, 1], [1, 0]])
+
+        dists, angs1, angs2 = polar_coordinates(vertices, edges)
+        print(angs2 / PI)
+
+        expected_dists = [1.73, 1.73]
+        assert_almost_equal(dists.numpy(), expected_dists, 2)
+
+        expected_angs1 = [0.25, 1.25]
+        assert_almost_equal((angs1 / PI).numpy(), expected_angs1, 2)
+
+        expected_angs2 = [0.25, 1.25]
+        assert_almost_equal((angs2 / PI).numpy(), expected_angs2, 2)
 
     def test_edges_from_faces(self):
         faces = torch.LongTensor([[2, 3, 0], [1, 0, 2]])
--- torch_geometric/graph/geometry.py
+++ torch_geometric/graph/geometry.py
@@ -3,8 +3,8 @@ from math import pi as PI
 import torch
 
 
-def vec2ang(y, x):
-    ang = torch.atan2(y, x)
+def vec2ang(x, y):
+    ang = torch.atan2(x, y)
     return ang + (ang <= 0).type(torch.FloatTensor) * 2 * PI
 
 
@@ -15,19 +15,21 @@ def polar_coordinates(vertices, edges):
     v = ends - starts
 
     # Calculate Euclidean distances.
-    dists = (v * v).sum(1).sqrt()
+    rho = (v * v).sum(1).sqrt()
 
     # Dimensionality case differentation.
     dim = vertices.size(1)
     if dim == 2:
-        return dists, vec2ang(v[:, 0], v[:, 1])
+        return rho, vec2ang(v[:, 1], v[:, 0])
     elif dim == 3:
-        return dists, vec2ang(v[:, 0], v[:, 1]), vec2ang(v[:, 0], v[:, 2])
+        return rho, vec2ang(v[:, 1], v[:, 0]), vec2ang(v[:, 2], v[:, 0])
 
     raise ValueError()
 
 
 def edges_from_faces(faces):
+    """Get undirected edges from triangular faces."""
+
     # Get directed edges.
     edges = torch.cat((faces[:, 0:2], faces[:, 1:3], faces[:, 0:3:2]))
     n = faces.max() + 1
