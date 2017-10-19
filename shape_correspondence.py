fb7654a02e308304bad284273d6a2f0c10dd2fc9
rusty1s <matthias.fey@tu-dortmund.de>
2017-10-19 09:55:50 +0200

    shape correspondence first test

--- shape_correspondence.py
+++ shape_correspondence.py
@@ -6,8 +6,7 @@ from torch.autograd import Variable
 from torch_geometric.datasets.faust import FAUST
 from torch_geometric.graph.geometry import MeshAdj
 from torch_geometric.utils.dataloader import DataLoader
-from torch_geometric.nn.modules.gcn import GCN
-from torch_geometric.nn.modules.gcn import Lin
+from torch_geometric.nn.modules import GCN, Lin
 
 path = '~/MPI-FAUST'
 train_dataset = FAUST(
@@ -22,8 +21,8 @@ if torch.cuda.is_available():
 else:
     kwargs = {}
 
-train_loader = DataLoader(train_dataset, batch_size=10, shuffle=True, **kwargs)
-test_loader = DataLoader(test_dataset, batch_size=10, shuffle=True, **kwargs)
+train_loader = DataLoader(train_dataset, batch_size=5, shuffle=True, **kwargs)
+test_loader = DataLoader(test_dataset, batch_size=5, shuffle=True, **kwargs)
 
 
 class Net(nn.Module):
@@ -41,22 +40,20 @@ class Net(nn.Module):
         x = F.relu(self.conv3(adj, x))
         x = F.relu(self.lin1(x))
         x = F.relu(self.lin2(x))
-        return x
+        return F.log_softmax(x)
 
 
 model = Net()
 if torch.cuda.is_available():
     model.cuda()
 
+optimizer = torch.optim.SGD(model.parameters(), lr=0.0001)
+
 
 def train(epoch):
     model.train()
-    print('Epoch:', epoch)
-    pass
-    # model.train()
 
     for batch_idx, ((_, (adj, _)), target) in enumerate(train_loader):
-        # Only needed for Kipf.
         n = adj.size(0)
         adj = torch.sparse.FloatTensor(adj._indices(),
                                        torch.ones(adj._indices().size(1)),
@@ -68,61 +65,43 @@ def train(epoch):
 
         x, adj, target = Variable(x), Variable(adj), Variable(target)
 
-        print(x.size())
-        print(adj.size())
+        optimizer.zero_grad()
 
         output = model(adj, x)
-        print(output.size())
 
-        # print(adjs.size())
+        loss = F.nll_loss(output, target.view(-1))
+        loss.backward()
+        optimizer.step()
 
-    #     pass
+    print('Train Epoch: {}\tLoss: {:6f}'.format(epoch, loss.data[0]))
 
 
-for epoch in range(1, 3):
+def test():
+    model.eval()
+    test_loss = 0
+    correct = 0
+
+    for (_, (adj, _)), target in test_loader:
+        n = adj.size(0)
+        adj = torch.sparse.FloatTensor(adj._indices(),
+                                       torch.ones(adj._indices().size(1)),
+                                       torch.Size([n, n]))
+
+        x = torch.ones(n).view(-1, 1)
+        if torch.cuda.is_available():
+            x, adj, target = x.cuda(), adj.cuda(), target.cuda()
+
+        x, adj, target = Variable(x), Variable(adj), Variable(target)
+        target = target.view(-1)
+
+        output = model(adj, x)
+        test_loss += F.nll_loss(output, target, size_average=False).data[0]
+        pred = output.data.max(1, keepdim=True)[1]
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+
+    print('Test set: Accuracy: {}/{}'.format(correct, 20 * 6890))
+
+
+for epoch in range(1, 100):
     train(epoch)
-
-    # print(vertices.size())
-    # print(adjs.size())
-    # print(slices.size())
-    # # print(adjs.size())
-    # print(target.size())
-    # Generate correspondence targets.
-    # identity(torch.Size([6890, 6890]))
-
-    # edges = edges.squeeze()
-    # adjs = [mesh_adj(vertices[i], edges[i]) for i in range(0, edges.size(0))]
-    # print(adjs[0].size())
-    # print(vertices.size())
-    # print(edges.size())
-    # print(target.size())
-
-# # TODO:  COmpute 544-dimensional SHOT descriptors (local histogram of normal
-# # vectors)
-# # Architecture:
-# # * Lin16
-# # * ReLU
-# # * CNN32
-# # * AMP
-# # * ReLU
-# # * CNN64
-# # * AMP
-# # * ReLU
-# # * CNN128
-# # * AMP
-# # * ReLU
-# # * Lin256
-# # * Lin6890
-# #
-# # output representing the soft correspondence as an 6890-dimensional vector.
-# #
-# # shape correspondence (labelling problem) where one tries to label each vertex
-# # to the index of a corresponding point.
-# # X = Indices
-# # y_j denote the vertex corresponding to x_i
-# # output representing the probability distribution on Y
-# # loss: multinominal regression loss
-# # ACN: adam optimizer 10^-3, beta_1 = 0.9, beta_2 = 0.999
-
-# # SHOT descriptor: Salti, Tombari, Stefano. SHOT, 2014
-# # Signature of Histograms of OrienTations (SHOT)
+    test()
--- torch_geometric/nn/functional/lin.py
+++ torch_geometric/nn/functional/lin.py
@@ -3,7 +3,7 @@ from torch.nn.functional import conv1d
 
 def lin(features, weight, bias=None):
     weight = weight.view(*weight.size(), 1)
-    weight = weight.transpose(0, 1)
+    weight = weight.transpose(0, 1).contiguous()
 
     features = features.view(1, features.size(0), weight.size(1))
     features = features.transpose(2, 1)
