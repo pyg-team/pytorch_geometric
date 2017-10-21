bccbd06c86d621f85325f6c19d2fd8014368789f
rusty1s <matthias.fey@tu-dortmund.de>
2017-10-21 09:32:31 +0200

    added todo

--- torch_geometric/utils/dataloader.py
+++ torch_geometric/utils/dataloader.py
@@ -8,6 +8,14 @@ from ..sparse.stack import stack
 
 def collate(batch):
     if torch.is_tensor(batch[0]) and batch[0].is_sparse:
+        # TODO: Shared memory
+        # out = None
+        # if _use_shared_memory:
+        #     # If we're in a background process, concatenate directly into a
+        #     # shared memory tensor to avoid an extra copy
+        #     numel = sum([x.numel() for x in batch])
+        #     storage = batch[0].storage()._new_shared(numel)
+        #     out = batch[0].new(storage)
         return stack(batch)
 
     elif isinstance(batch[0], collections.Mapping):
