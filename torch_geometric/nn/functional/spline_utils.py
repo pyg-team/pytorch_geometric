701d3cbc747be8abdb925af1f2d5bd6d8958f5b8
rusty1s <matthias.fey@tu-dortmund.de>
2017-10-21 12:28:39 +0200

    better impl

--- torch_geometric/nn/functional/spline_utils.py
+++ torch_geometric/nn/functional/spline_utils.py
@@ -1,47 +1,145 @@
 from math import pi as PI
+from functools import reduce
 import itertools
 
 import torch
 
 
-def open_spline(values, num_knots, degree=1):
-    dim = len(values.size())
-    values = num_knots * values / values.max()
-
+def open_spline_amount(values, degree=1):
+    # Passed values must be in the range [0, num_knots -1].
     amount_grow = values.frac()
     amount_fall = 1 - amount_grow
-    amount = torch.stack([amount_grow, amount_fall], dim)
+    return torch.stack([amount_grow, amount_fall], dim=len(values.size()))
 
+
+def open_spline_index(values, num_knots, degree=1):
+    # Passed values must be in the range [0, num_knots - 1].
     idx_fall = values.floor().long()
     idx_fall = idx_fall - (idx_fall >= num_knots).long()
     idx_grow = 1 + idx_fall
-    idx = torch.stack([idx_grow, idx_fall], dim)
-
-    return amount, idx
+    return torch.stack([idx_grow, idx_fall], dim=len(values.size()))
 
 
-def closed_spline(values, num_knots, degree=1):
-    dim = len(values.size())
-    values = num_knots * values / (2 * PI)
-
+def closed_spline_amount(values, degree=1):
+    # Passed values must be in the range [0, num_knots].
     amount_grow = values.frac()
     amount_fall = 1 - amount_grow
-    amount = torch.stack([amount_grow, amount_fall], dim)
+    return torch.stack([amount_grow, amount_fall], dim=len(values.size()))
 
+
+def closed_spline_index(values, num_knots, degree=1):
+    # Passed values must be in the range [0, num_knots].
     idx_grow = values.floor().long()
     idx_fall = idx_grow - 1
     idx_grow = idx_grow - (idx_grow >= num_knots).long() * num_knots
     idx_fall = idx_fall + (idx_fall < 0).long() * num_knots
-    idx = torch.stack([idx_grow, idx_fall], dim)
+    return torch.stack([idx_grow, idx_fall], dim=len(values.size()))
 
-    return amount, idx
+
+def rescale(values, kernel_size):
+    kernel = torch.FloatTensor(kernel_size)
+    kernel[0] -= 1  # Open spline has one less partition.
+
+    boundary = torch.FloatTensor([2 * PI for _ in range(len(kernel_size))])
+    # Radius is rescaled dependent on its max value.
+    boundary[0] = values[:, 0].max()
+
+    return (kernel / boundary) * values
+
+
+def spline_weights(values, kernel_size, degree=1):
+    # Rescale values to be in range for fast calculation splines.
+    values = rescale(values, kernel_size)
+
+    amount = weight_amount(values, kernel_size, degree)
+    index = weight_index(values, kernel_size, degree)
+    return amount, index
+
+
+def weight_amount(values, kernel_size, degree=1):
+    # values = num_knots * values / (2 * PI)
+    pass
+
+
+def weight_index(values, kernel_size, degree=1):
+    # Collect all indices for all dimensions.
+    index = []
+    for i, col in enumerate(values.t()):
+        if i == 0:
+            index.append(open_spline_index(col, kernel_size[i], degree))
+        else:
+            index.append(closed_spline_index(col, kernel_size[i], degree))
+
+    # Stack indices to a [|E| x (degree + 1) x dim] tensor.
+    index = torch.stack(index, dim=2)
+
+    # Multiply an offset to indices in each dimension, which allows flattening
+    # the indices in a later stage.
+    # The offset is defined by the `kernel_size`. In the 3D case with kernel
+    # [k_1, k_2, k_3]` the offset is then given by [1, k_1, k_1 * k_2].
+    offset = [1]
+    curr_kernel = 1
+    for i in range(len(kernel_size) - 1):
+        curr_kernel *= kernel_size[i]
+        offset.append(curr_kernel)
+    offset = torch.LongTensor(offset)
+
+    index = offset * index
+
+    # Finally, we can compute a flattened out version of the tensor
+    # [|E| x (degree + 1) x dim] with the shape [|E| x (degree + 1)^dim], which
+    # contains the summed up indices of all dimensions for all possible
+    # combinations of (degree + 1)^dim.
+    # The flattened out tensor then describes the weight indices influencing
+    # the end vertex of each passed edge.
+
+    print(index)
+
+    # print(indices.size())
+    # print(indices)
+    return index
+    # K = reduce(lambda x, y: x * y, kernel_size)
+    # print('dim', dim, 'K', K)
+    # print('values', values.size())
+    # print(values)
+
+    #     # Values contains polar coordinates with shape [|E| x dim].
+    #     # Iterate over each coordinate and calculate spline amounts and indices.
+    #     coff = 1
+    #     for i, col in enumerate(values.t()):
+    #         if i == 0:
+    #             # Rescale values to range [0, kernel_size[i] - 1].
+    #             col = (kernel_size[i] - 1) * col / col.max()
+
+    #             amount = closed_spline_amount(col, degree)
+    #             index = closed_spline_index(col, kernel_size[i], degree)
+    #             print(amount)
+    #         else:
+    #             coff *= kernel_size[i - 1]
+
+    #             # Rescale values to range [0, kernel_size[i]].
+    #             col = kernel_size[i] * col / (2 * PI)
+
+    #             amount = open_spline_amount(col, degree)
+    #             index = open_spline_index(col, kernel_size[i], degree)
+    #             print(amount)
+
+    #         index = coff * index
+
+    #         pass
+
+    # print(d)
+
+    # Calcu
+
+    pass
 
 
 def points(dim, degree):
     return itertools.product(* [range(degree) for _ in range(dim)])
 
 
-def weight_indices(spline_indices, kernel_size):
+def _weight_indices(spline_indices, kernel_size):
     d, m = spline_indices.size()
 
     # Get all possible combinations resulting in a [m^d x d] matrix.
--- torch_geometric/nn/functional/spline_utils_test.py
+++ torch_geometric/nn/functional/spline_utils_test.py
@@ -4,26 +4,64 @@ from unittest import TestCase
 import torch
 from numpy.testing import assert_equal, assert_almost_equal
 
-from .spline_utils import open_spline, closed_spline
+from .spline_utils import (open_spline_amount, open_spline_index,
+                           closed_spline_amount, closed_spline_index, rescale,
+                           spline_weights, weight_amount, weight_index)
+from ...graph.geometry import mesh_adj
 
 
 class SplineUtilsTest(TestCase):
     def test_open_spline(self):
-        values = torch.FloatTensor([0, 0.1, 0.5, 1, 1.5, 1.9, 2]) * PI
+        values = torch.FloatTensor([0, 0.2, 1, 2, 3, 3.8, 4])
 
-        B = [[0, 1], [0.2, 0.8], [0, 1], [0, 1], [0, 1], [0.8, 0.2], [0, 1]]
-        C = [[1, 0], [1, 0], [2, 1], [3, 2], [4, 3], [4, 3], [4, 3]]
+        a = [[0, 1], [0.2, 0.8], [0, 1], [0, 1], [0, 1], [0.8, 0.2], [0, 1]]
+        i = [[1, 0], [1, 0], [2, 1], [3, 2], [4, 3], [4, 3], [4, 3]]
 
-        out_B, out_C = open_spline(values, num_knots=4)
-        assert_almost_equal(out_B.numpy(), B, 1)
-        assert_equal(out_C.numpy(), C)
+        amount = open_spline_amount(values, degree=1)
+        index = open_spline_index(values, num_knots=4, degree=1)
+
+        assert_almost_equal(amount.numpy(), a, 1)
+        assert_equal(index.numpy(), i)
 
     def test_closed_spline(self):
-        values = torch.FloatTensor([0, 0.1, 0.5, 1, 1.5, 1.9, 2]) * PI
+        values = torch.FloatTensor([0, 0.2, 1, 2, 3, 3.8, 4])
 
-        B = [[0, 1], [0.2, 0.8], [0, 1], [0, 1], [0, 1], [0.8, 0.2], [0, 1]]
-        C = [[0, 3], [0, 3], [1, 0], [2, 1], [3, 2], [3, 2], [0, 3]]
+        a = [[0, 1], [0.2, 0.8], [0, 1], [0, 1], [0, 1], [0.8, 0.2], [0, 1]]
+        i = [[0, 3], [0, 3], [1, 0], [2, 1], [3, 2], [3, 2], [0, 3]]
 
-        out_B, out_C = closed_spline(values, num_knots=4)
-        assert_almost_equal(out_B.numpy(), B, 1)
-        assert_equal(out_C.numpy(), C)
+        amount = closed_spline_amount(values, degree=1)
+        index = closed_spline_index(values, num_knots=4, degree=1)
+
+        assert_almost_equal(amount.numpy(), a, 1)
+        assert_equal(index.numpy(), i)
+
+    def test_rescale(self):
+        values = [[1, 0.5 * PI], [2, PI], [3, 1.5 * PI], [4, 2 * PI]]
+
+        values = rescale(torch.FloatTensor(values), kernel_size=[3, 4])
+
+        expected_values = [[0.5, 1], [1, 2], [1.5, 3], [2, 4]]
+
+        assert_equal(values.numpy(), expected_values)
+
+    def test_spline_weights(self):
+        vertices = [[0, 0], [1, 1], [2, -2], [-3, -3], [-4, 4]]
+        edges = [[0, 0, 0, 0], [1, 2, 3, 4]]
+        adj = mesh_adj(torch.FloatTensor(vertices), torch.LongTensor(edges))
+
+        spline_weights(adj._values(), kernel_size=[3, 4], degree=1)
+
+
+#     def test_weight_amount(self):
+#         vertices = [[0, 0], [1, 1], [2, -2], [-3, -3], [-4, 4]]
+#         edges = [[0, 0, 0, 0], [1, 2, 3, 4]]
+#         adj = mesh_adj(torch.FloatTensor(vertices), torch.LongTensor(edges))
+
+#         # weight_amount(adj._values(), [3, 4], degree=1)
+
+#     def test_weight_index(self):
+#         vertices = [[0, 0], [4, 4], [1, -1], [-2, -2], [-3, -3]]
+#         edges = [[0, 0, 0, 0], [1, 2, 3, 4]]
+#         adj = mesh_adj(torch.FloatTensor(vertices), torch.LongTensor(edges))
+
+#         # weight_index(adj._values(), [3, 4], degree=1)
