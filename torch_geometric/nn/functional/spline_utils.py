b15f9187e07ab0a1896492148fed19e4772259f1
rusty1s <matthias.fey@tu-dortmund.de>
2017-10-21 18:22:25 +0200

    typos

--- torch_geometric/nn/functional/spline_utils.py
+++ torch_geometric/nn/functional/spline_utils.py
@@ -51,9 +51,9 @@ def closed_spline_index(values, kernel_size, degree=1):
 
 
 def rescale(values, kernel_size, max_radius):
-    # Scale values by defining a factor for each dimension.
+    """Scale values by defining a factor for each dimension."""
 
-    # Clamp radius to its maximum allowed radius.
+    # Clamp radius (dimension 0) to its maximum allowed value.
     clamped_radius = torch.clamp(values[:, :1], max=max_radius)
     values = torch.cat([clamped_radius, values[:, 1:]], dim=1)
 
@@ -61,7 +61,6 @@ def rescale(values, kernel_size, max_radius):
     # Dimension 0 with open splines has one less partition.
     kernel[0] -= 1
 
-    # TODO: this doesn't work batch-wise.
     boundary = torch.FloatTensor([2 * PI for _ in range(len(kernel_size))])
     # Dimension 0 must be clamped to the maxium allowed radius
     boundary[0] = max_radius
