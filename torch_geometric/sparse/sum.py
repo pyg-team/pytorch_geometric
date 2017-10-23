5b1b71ce0d59979947f95b12e4ffb593225ccdd2
rusty1s <matthias.fey@tu-dortmund.de>
2017-10-23 07:41:43 +0200

    fixed sum for sparse matrices with higher dimensional values

--- torch_geometric/sparse/sum.py
+++ torch_geometric/sparse/sum.py
@@ -1,26 +1,29 @@
 import torch
 from torch.autograd import Variable
 
-from torch_geometric.sparse import mm
 
-
-def sum(a, dim=None, keepdim=False):
+def sum(a, dim=None):
     if dim is None:
         return torch.sum(a._values())
 
-    if dim is 0:
-        a = a.t()
+    if dim > 1:
+        raise ValueError()
 
-    ones = torch.ones(a.size()[1], 1)
+    values = a._values()
+    index = a._indices()[1 - dim]
+
+    # TODO: zero as same type as `a`.
+    if len(a.size()) == 2:
+        zero = torch.zeros(a.size(dim))
+    else:
+        remaining_dim = list(a.size()[2:])
+        zero = torch.zeros(a.size(dim), *remaining_dim)
+        index = index.view(-1, 1).expand(index.size(0), *remaining_dim)
+
     if a.is_cuda is True:
-        ones = ones.cuda()
+        zero = zero.cuda()
 
     if not torch.is_tensor(a):
-        ones = Variable(ones)
+        zero = Variable(zero)
 
-    c = mm(a, ones)
-
-    if dim is 0:
-        c = c.view(1, -1)
-
-    return c if keepdim else c.view(-1)
+    return zero.scatter_add_(0, index, values)
