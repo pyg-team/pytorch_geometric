a7bd424597b71ed2e2f9a3bc8367789860f94901
rusty1s <matthias.fey@tu-dortmund.de>
2017-10-24 12:56:23 +0200

    fixed lin test

--- torch_geometric/nn/functional/lin_test.py
+++ torch_geometric/nn/functional/lin_test.py
@@ -16,8 +16,10 @@ class LinTest(TestCase):
         ]))
         weight = Variable(
             torch.FloatTensor([
-                [0.1, 0.2, 0.3, 0.4],
-                [0.5, 0.6, 0.7, 0.8],
+                [[0.1], [0.5]],
+                [[0.2], [0.6]],
+                [[0.3], [0.7]],
+                [[0.4], [0.8]],
             ]))
         bias = Variable(torch.FloatTensor([1, 2, 3, 4]))
 
