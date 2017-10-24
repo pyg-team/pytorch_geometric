7cacad45f13da584cc9c5eae0e59b7c22816bffb
rusty1s <matthias.fey@tu-dortmund.de>
2017-10-24 14:58:21 +0200

    cleaner

--- torch_geometric/nn/functional/lin.py
+++ torch_geometric/nn/functional/lin.py
@@ -2,7 +2,5 @@ from torch.nn.functional import conv1d
 
 
 def lin(features, weight, bias=None):
-    features = features.view(1, features.size(0), weight.size(1))
-    features = features.transpose(2, 1)
-
+    features = features.t().unsqueeze(0)
     return conv1d(features, weight, bias).squeeze().t()
