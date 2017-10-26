27b26394c32065bcb836e299c3fdc34a58318517
rusty1s <matthias.fey@tu-dortmund.de>
2017-10-26 10:27:34 +0200

    clean up shape correspondence test

--- shape_correspondence.py
+++ shape_correspondence.py
@@ -1,3 +1,5 @@
+from __future__ import division, print_function
+
 import torch
 from torch import nn
 import torch.nn.functional as F
@@ -15,7 +17,7 @@ test_dataset = FAUST(
     path, train=False, correspondence=True, transform=EuclideanAdj())
 
 train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=4, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=1, shuffle=True)
 
 
 class Net(nn.Module):
@@ -26,6 +28,8 @@ class Net(nn.Module):
         self.conv2 = SplineGCN(
             32, 64, dim=3, kernel_size=[5, 5, 2], is_open_spline=True)
         self.conv3 = SplineGCN(
+            64, 64, dim=3, kernel_size=[5, 5, 2], is_open_spline=True)
+        self.conv4 = SplineGCN(
             64, 128, dim=3, kernel_size=[5, 5, 2], is_open_spline=True)
         self.lin1 = Lin(128, 256)
         self.lin2 = Lin(256, 6890)
@@ -34,6 +38,7 @@ class Net(nn.Module):
         x = F.relu(self.conv1(adj, x))
         x = F.relu(self.conv2(adj, x))
         x = F.relu(self.conv3(adj, x))
+        x = F.relu(self.conv4(adj, x))
         x = F.relu(self.lin1(x))
         x = F.dropout(x, training=self.training)
         x = self.lin2(x)
@@ -51,57 +56,44 @@ def train(epoch):
     model.train()
 
     for batch_idx, ((_, (adj, _)), target) in enumerate(train_loader):
-        print('epoch: ', epoch, 'batch: ', batch_idx)
-        features = torch.ones(adj.size(0)).view(-1, 1)
+        input = torch.ones(adj.size(0)).view(-1, 1)
 
         if torch.cuda.is_available():
-            features, adj, target = features.cuda(), adj.cuda(), target.cuda()
+            input, adj, target = input.cuda(), adj.cuda(), target.cuda()
 
-        features, target = Variable(features), Variable(target)
-        output = model(adj, features)
+        input, target = Variable(input), Variable(target)
+        output = model(adj, input)
+
         optimizer.zero_grad()
         loss = F.nll_loss(output, target.view(-1), size_average=True)
         loss.backward()
         optimizer.step()
-        print('loss', float(loss.data.cpu().numpy()))
+        loss = loss.data.cpu()[0]
 
         pred = output.data.max(1, keepdim=True)[1]
         correct = pred.eq(target.data.view_as(pred)).cpu().sum()
-        print('correct', correct)
-
-        # print('epoch', epoch, 'batch', batch_idx, 'loss', l, 'correct',
-        #       correct)
-        # optimizer.step()
-
-    # print('Train Epoch: {}\tLoss: {:6f}'.format(epoch, loss.data[0]))
+        print('epoch: ', epoch, 'batch: ', batch_idx, 'loss: ', loss,
+              'correct:', correct)
 
 
 def test():
     model.eval()
-    test_loss = 0
     correct = 0
 
     for (_, (adj, _)), target in test_loader:
-        n = adj.size(0)
-        adj = torch.sparse.FloatTensor(adj._indices(),
-                                       torch.ones(adj._indices().size(1)),
-                                       torch.Size([n, n]))
+        input = torch.ones(adj.size(0)).view(-1, 1)
 
-        x = torch.ones(n).view(-1, 1)
         if torch.cuda.is_available():
-            x, adj, target = x.cuda(), adj.cuda(), target.cuda()
+            input, adj, target = input.cuda(), adj.cuda(), target.cuda()
 
-        x, adj, target = Variable(x), Variable(adj), Variable(target)
-        target = target.view(-1)
+        output = model(adj, Variable(input))
 
-        output = model(adj, x)
-        test_loss += F.nll_loss(output, target, size_average=False).data[0]
         pred = output.data.max(1, keepdim=True)[1]
-        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+        correct += pred.eq(target.view_as(pred)).cpu().sum()
 
-    print('Test set: Accuracy: {}/{}'.format(correct, 20 * 6890))
+    print('Accuracy:', correct / (20 * 6890))
 
 
 for epoch in range(1, 100):
     train(epoch)
-    # test()
+    test()
