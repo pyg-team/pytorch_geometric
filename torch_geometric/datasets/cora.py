2d51ea8851023933c531963ec13aada8a45771c1
rusty1s <matthias.fey@tu-dortmund.de>
2017-10-27 14:56:29 +0200

    added cora dataset

--- torch_geometric/datasets/cora.py
+++ torch_geometric/datasets/cora.py
@@ -1,34 +1,96 @@
-# 2708 vertices
-# 5429 edges
-# 1433 features
-# 7 classes
-
 import os
+
+import torch
+
+from .utils.dir import make_dirs
 
 
 class Cora(object):
     url = "https://linqs-data.soe.ucsc.edu/public/lbc/cora.tgz"
 
-    def __init__(self, root, train=True, transform=None,
-                 target_transform=None):
+    def __init__(self, root, transform=None, target_transform=None):
+
+        super(Cora, self).__init__()
+
         self.root = os.path.expanduser(root)
         self.processed_folder = os.path.join(self.root, 'processed')
-        print(self.root)
+        self.data_file = os.path.join(self.processed_folder, 'data.pt')
 
-        file = os.path.join(self.root, 'cora.content')
-        with open(file, 'r') as f:
+        self.transform = transform
+        self.target_transform = target_transform
+
+        self.process()
+
+        self.input, index, self.target = torch.load(self.data_file)
+        weight = torch.FloatTensor(index.size(1)).fill_(1)
+        n = self.input.size(0)
+        self.adj = torch.sparse.FloatTensor(index, weight, torch.Size([n, n]))
+
+    def __getitem__(self, index):
+        data = (self.input, self.adj)
+        target = self.target
+
+        if self.transform is not None:
+            data = self.transform(data)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return (data, target)
+
+    def __len__(self):
+        return 1
+
+    def _check_exists(self):
+        return os.path.exists(self.root)
+
+    def _check_processed(self):
+        return os.path.exists(self.data_file)
+
+    def _read(self):
+        key = []
+
+        with open(os.path.join(self.root, 'cora.content'), 'r') as f:
+            clx = []
+            target = []
+            input = []
             for line in f:
-                s = line.split('\t')
-                idx = s[0]
-                features = s[1:-1]
+                s = line[:-1].split('\t')
+                key.append(s[0])
+                input.append([int(i) for i in s[1:-1]])
                 label = s[-1]
-                print(idx)
-                print(label)
-                print(len(features))
-            print(f)
+                if label not in clx:
+                    clx.append(label)
+                target.append(clx.index(label))
 
-        pass
+            input = torch.ByteTensor(input)
+            target = torch.LongTensor(target)
 
+        with open(os.path.join(self.root, 'cora.cites'), 'r') as f:
+            index = []
+            for line in f:
+                s = line[:-1].split('\t')
+                x = key.index(s[0])
+                y = key.index(s[1])
+                index.append([x, y])
 
-# Training: 20 samples per class
-# Validation and tests sets consists of 500 and 1000 disjoint vertices??
+            index = torch.LongTensor(index).t()
+
+        return input, index, target
+
+    def process(self):
+        if not self._check_exists():
+            # TODO: Download dataset.
+            raise RuntimeError()
+
+        if self._check_processed():
+            return
+
+        print('Processing...')
+
+        make_dirs(os.path.join(self.processed_folder))
+
+        data = self._read()
+        torch.save(data, self.data_file)
+
+        print('Done!')
