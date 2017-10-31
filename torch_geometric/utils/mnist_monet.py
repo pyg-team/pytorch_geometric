added mnist monet position calculation, fixed tests

torch_geometric/utils/mnist_monet.py
@@ -1,4 +1,3 @@
-from math import pi as PI
 import os
 
 import h5py
@@ -63,73 +62,60 @@ def polar2euclidean(rho, theta):
     return torch.sparse.FloatTensor(nz, euclidean, torch.Size([75, 75, 2]))
 
 
+def sparse_to_dict(a):
+    index = a._indices()
+    x, y = a._values().t()
+
+    d = {}
+    for i in range(index.size(1)):
+        row = index[0, i]
+        if row not in d:
+            d[row] = []
+        d[row].append((index[1, i], x[i], y[i]))
+    return d
+
+
+def dict_to_pos(a):
+    pos = torch.FloatTensor(75, 2).fill_(0)
+
+    parent = [0 for _ in a[0]]
+    to_process = [c for c in a[0]]
+    processed = [0]
+
+    while len(to_process) > 0:
+        r = parent.pop(0)
+        data = to_process.pop(0)
+        c, x, y = data
+
+        r_pos = pos[r]
+        pos[c, 0] = r_pos[0] + x
+        pos[c, 1] = r_pos[1] + y
+
+        processed.append(c)
+
+        new_edges = list(filter(lambda x: x[0] not in processed, a[c]))
+        parent.extend([c for _ in new_edges])
+        to_process.extend([i for i in new_edges])
+
+    return pos
+
+
 path = os.path.expanduser('~/Downloads/MoNet_code/MNIST')
 # data = read_mnist_monet(os.path.join(path, 'datasets'))
 # torch.save(data, os.path.join(path, 'data.pt'))
 data = torch.load(os.path.join(path, 'data.pt'))
 input, target, rho, theta = data
+target = target.byte()
 
-for i in range(2):
-    a = polar2euclidean(rho[i], theta[i])
-    print(a.size())
+positions = []
+for i in range(70000):
+    adj = polar2euclidean(rho[i], theta[i])
+    adj = sparse_to_dict(adj)
+    pos = dict_to_pos(adj)
+    minimum, _ = pos.min(dim=0)
+    pos = pos - minimum
+    positions.append(pos)
 
-# from __future__ import division
+positions = torch.stack(positions, dim=0)
 
-# import os
-# import h5py
-# import numpy as np
-
-# path_train_vals = os.path.join('./mnist/train_vals.mat')
-# path_test_vals = os.path.join('./mnist/test_vals.mat')
-# path_train_coords = os.path.join('./mnist/train_patch_coords.mat')
-# path_test_coords = os.path.join('./mnist/test_patch_coords.mat')
-# path_train_centroids = os.path.join('./mnist/train_centroids.mat')
-# path_test_centroids = os.path.join('./mnist/test_centroids.mat')
-# path_train_labels = os.path.join('./mnist/train_labels.mat')
-# path_test_labels = os.path.join('./mnist/test_labels.mat')
-
-# def load_matlab_file(path_file, name_field, dtype=np.float32):
-#     db = h5py.File(path_file, 'r')
-#     ds = db[name_field]
-#     out = np.asarray(ds).astype(dtype).T.squeeze()
-#     db.close()
-#     return out
-
-# train_vals = load_matlab_file(path_train_vals, 'vals')
-# test_vals = load_matlab_file(path_test_vals, 'vals')
-# train_coords = load_matlab_file(path_train_coords, 'patch_coords')
-# test_coords = load_matlab_file(path_test_coords, 'patch_coords')
-# train_centroids = load_matlab_file(path_train_centroids, 'idx_centroids',
-#                                    np.int64)
-# test_centroids = load_matlab_file(path_test_centroids, 'idx_centroids',
-#                                   np.int64)
-# print(test_centroids)
-# train_labels = load_matlab_file(path_train_labels, 'labels', np.uint8)
-# test_labels = load_matlab_file(path_test_labels, 'labels', np.uint8)
-
-# # 75 * 75 * 2 values (warum 2?)
-# # 1-dim centroids? wtf?
-# # dist is 0 at root, rad is NaN
-
-# # I don't really need this if I have the centroids. I just need the row and col
-# # array
-# # sigma needs to be calculated, not passed
-# # TODO: find a way to extract these arrays
-# def stack_matrices(x):  # Stack 2D matrices into a 3D tensor
-#     n = int(np.sqrt(x.shape[1] // 2))
-#     num_examples = x.shape[0]
-
-#     rho = x[:, :n * n]
-#     theta = x[:, n * n:]
-#     rho = np.reshape(rho, (num_examples, n, n, 1))
-#     theta = np.reshape(theta, (num_examples, n, n, 1))
-#     return np.concatenate([rho, theta], axis=3)
-
-# # Load the coords
-# test_coords = stack_matrices(test_coords)
-# train_coords = stack_matrices(train_coords)
-
-# # Compute centroids
-# def compute_centroids(centroids, list_mapping_graph_0_to_graph_1,
-#                       list_mapping_graph_1_to_graph_2):
-#     pass
+torch.save(positions, os.path.join(path, 'positions.pt'))