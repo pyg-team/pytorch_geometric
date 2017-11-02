ea23468ebd22362aa815bda111dccb8a7637c94e
rusty1s <matthias.fey@tu-dortmund.de>
2017-11-02 15:34:30 +0100

    first graclus impl

--- torch_geometric/transform/graclus.py
+++ torch_geometric/transform/graclus.py
@@ -1,10 +1,140 @@
+from __future__ import division
+
+import torch
+
+
 class Graclus(object):
-    def __init__(self, level):
+    def __init__(self, level=1):
         self.level = level
 
-    def __call__(self, data):
+    def __call__(self, data, rid=None):
         input, adj, position = data
-        # Compute adjacencies
-        # Permute inputs and fill with zeros.
-        # permute positions and take mean.
-        return (input, (adj, adj), (position, position))
+
+        adjs = [adj]
+        positions = [position]
+        clusters = []
+        for _ in range(self.level):
+            # TODO: if position is not None, compute adj_dist
+            cluster, cluster_full = normalized_cut(adj, rid)
+            rid = None
+            print(cluster)
+            clusters.append(cluster_full)
+
+            # Compute new adjaceny.
+            adj = cluster_adj(adj, cluster)
+            adjs.append(adj)
+
+            # Compute new positions.
+            position = cluster_position(position, cluster)
+            positions.append(position)
+
+        # Permute inputs, adjacencies and positions.
+        perms = compute_perms(clusters)
+        adjs = [perm_adj(adjs[i], perms[i]) for i in range(len(perms))]
+        positions = [
+            perm_input(positions[i], perms[i]) for i in range(len(perms))
+        ]
+        input = perm_input(input, perms[0])
+
+        return input, adjs, positions
+
+
+def normalized_cut(adj, rid=None):
+    n = adj.size(0)
+    rid = torch.randperm(n) if rid is None else rid
+    cluster = torch.LongTensor(n).fill_(-1)
+
+    row, col = adj._indices()
+    weight = adj._values()
+    degree = 1 / weight.new(n).fill_(0).scatter_add_(0, row, weight)
+    weight = weight * (degree[row] + degree[col])
+
+    # Sort after weight.
+    weight, perm = weight.sort(dim=0, descending=True)
+    row = row[perm]
+    col = col[perm]
+
+    # Sort after rid.
+    _, perm = rid[row].sort()
+    row = row[perm]
+    col = col[perm]
+
+    # Find cluster values.
+    count = 0
+    while row.dim() > 0:
+        cluster[row[0]] = count
+        cluster[col[0]] = count
+
+        mask = (row != row[0]) & (row != col[0])  # && row != col[1]
+        mask &= (col != row[0]) & (col != col[0])
+
+        row = row[mask]
+        col = col[mask]
+        count += 1
+
+    # Set singleton values and append.
+    singleton = cluster == -1
+    index = torch.arange(count, singleton.sum() + count).long()
+    cluster[singleton] = index
+    return cluster, torch.cat([cluster, index], dim=0)
+
+
+def cluster_adj(adj, cluster):
+    n = cluster.max() + 1
+    row, col = adj._indices()
+    row, col = cluster[row], cluster[col]
+    weight = adj._values()
+    index = torch.stack([row, col], dim=0)
+    adj = torch.sparse.FloatTensor(index, weight, torch.Size([n, n]))
+    return adj.coalesce()
+
+
+def cluster_position(position, cluster):
+    n = cluster.max() + 1
+    x = position.new(n).fill_(0)
+    y = position.new(n).fill_(0)
+    x = x.scatter_add_(0, cluster, position[:, 0]) / 2
+    y = y.scatter_add_(0, cluster, position[:, 1]) / 2
+    return torch.stack([x, y], dim=0)
+
+
+def compute_perms(clusters):
+    max_cluster = n = clusters[-1].max() + 1
+
+    perm = torch.arange(0, n).long()
+    perms = [perm]
+
+    for i in range(len(clusters) - 1, -1, -1):
+        cluster = clusters[i]
+        max_cluster = cluster.size(0) // 2
+        if max_cluster < n:
+            index = torch.arange(max_cluster, n).long()
+            cluster = torch.cat([cluster, index], dim=0)
+
+        cluster = perm.sort()[1][cluster]
+        rid = cluster.sort()[1]
+        n *= 2
+        perm = torch.arange(0, n).long()[rid]
+        perms.append(perm)
+
+    return perms[::-1]
+
+
+def perm_adj(adj, perm):
+    n = perm.size(0)
+    row, col = adj._indices()
+
+    perm, _ = perm.sort()
+    row = perm[row]
+    col = perm[col]
+    index = torch.stack([row, col], dim=0)
+    return torch.sparse.FloatTensor(index, adj._values(), torch.Size([n, n]))
+
+
+def perm_input(input, perm):
+    n = input.size(0)
+    num_fake_nodes = perm.size(0) - n
+    size = list(input.size())
+    size[0] = num_fake_nodes
+    input = torch.cat([input, input.new(*size).fill_(0)], dim=0)
+    return input[perm]
---
+++ torch_geometric/transform/graclus_test.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+import torch
+# from numpy.testing import assert_equal, assert_almost_equal
+
+from .graclus import Graclus
+
+
+class GraclusTest(TestCase):
+    def test_graclus_adj(self):
+        input = torch.FloatTensor([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]])
+        position = torch.FloatTensor([[1, 1], [3, 2], [3, 0], [4, 1], [8, 3]])
+        index = torch.LongTensor([
+            [0, 0, 1, 1, 2, 2, 3, 3, 3, 4],
+            [1, 2, 0, 3, 0, 3, 1, 2, 4, 3],
+        ])
+        weight = torch.FloatTensor(10).fill_(1)
+        adj = torch.sparse.FloatTensor(index, weight, torch.Size([5, 5]))
+
+        transform = Graclus()
+        rid = torch.LongTensor([0, 1, 2, 3, 4])
+
+        input, adj, position = transform((input, adj, position), rid)
+
+        print(input)
+        print(position)
+        print(adj[0].to_dense())
+        print(adj[1].to_dense())
