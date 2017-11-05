5234bd72ae55a7860f682d2c6989850a5bd11a23
rusty1s <matthias.fey@tu-dortmund.de>
2017-11-05 12:12:55 +0100

    added mnist grid pic

--- torch_geometric/visualization/mnist_graph.py
+++ torch_geometric/visualization/mnist_graph.py
@@ -12,44 +12,62 @@ sys.path.insert(0, '..')
 sys.path.insert(0, '../..')
 
 from torch_geometric.datasets import MNISTSuperpixel75  # noqa
+from torch_geometric.graph.grid import grid, grid_position  # noqa
+
+
+def image_graph(image, input, graph, position, scale, offset):
+    image = image.copy()
+    image = np.repeat(image, scale, axis=0)
+    image = np.repeat(image, scale, axis=1)
+    image = skimage.img_as_float(image)
+    image = skimage.color.gray2rgb(image)
+    shape = image.shape
+
+    position *= scale
+    position += scale * offset
+
+    # Draw edges
+    index = adj._indices().t()
+    for i in range(index.size(0)):
+        start, end = index[i]
+        start_x, start_y = position[start]
+        end_x, end_y = position[end]
+        start_x, start_y = int(start_x), int(start_y)
+        end_x, end_y = int(end_x), int(end_y)
+
+        rr, cc = draw.line(start_y, start_x, end_y, end_x)
+        image[rr, cc] = [1, 0, 0]
+
+    # Draw nodes
+    for i in range(position.size(0)):
+        x, y = position[i]
+        rr, cc = draw.circle(y, x, 12, shape=shape)
+        image[rr, cc] = [1, 0, 0]
+        rr, cc = draw.circle(y, x, 8, shape=shape)
+        image[rr, cc] = [input[i], input[i], input[i]]
+
+    return image
+
 
 image_dataset = MNIST('/Users/rusty1s/MNIST', train=True, download=True)
 graph_dataset = MNISTSuperpixel75('~/MNISTSuperpixel75', train=True)
 
 scale = 32
-offset_x = 1
-offset_y = 1
+offset = torch.FloatTensor([1, 1])
 example = 2
 
 image, _ = image_dataset[example]
 image = np.array(image)
-image = np.repeat(image, scale, axis=0)
-image = np.repeat(image, scale, axis=1)
-image = skimage.img_as_float(image)
-image = skimage.color.gray2rgb(image)
-shape = image.shape
-
 (input, adj, position), _ = graph_dataset[example]
-position *= scale
-position += torch.FloatTensor([scale * offset_x, scale * offset_y])
 
-index = adj._indices().t()
-for i in range(index.size(0)):
-    start, end = index[i]
-    start_x, start_y = position[start]
-    end_x, end_y = position[end]
-    start_x, start_y = int(start_x), int(start_y)
-    end_x, end_y = int(end_x), int(end_y)
+grid_adj = grid(torch.Size([28, 28]))
+grid_position = grid_position(torch.Size([28, 28]))
+grid_input = image.flatten() / 255.0
+grid_offset = torch.FloatTensor([0.5, 0.5])
 
-    rr, cc = draw.line(start_y, start_x, end_y, end_x)
-    image[rr, cc] = [1, 0, 0]
-
-for i in range(75):
-    x, y = position[i]
-    rr, cc = draw.circle(y, x, 18, shape=shape)
-    image[rr, cc] = [1, 0, 0]
-    rr, cc = draw.circle(y, x, 12, shape=shape)
-    image[rr, cc] = [input[i], input[i], input[i]]
+image = image_graph(image, input, adj, position, scale, offset)
+# image = image_graph(image, grid_input, grid_adj, grid_position, scale,
+#                     grid_offset)
 
 io.imshow(image)
 io.show()
