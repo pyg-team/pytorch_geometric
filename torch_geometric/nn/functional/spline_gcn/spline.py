a4fb1aee3bd5afdd964794f46271ac0651a2e5dc
rusty1s <matthias.fey@tu-dortmund.de>
2017-11-07 12:46:14 +0100

    variable kernel size impl

--- torch_geometric/nn/functional/spline_gcn/spline.py
+++ torch_geometric/nn/functional/spline_gcn/spline.py
@@ -4,10 +4,16 @@ from .spline_cpu import spline_cpu
 
 if torch.cuda.is_available():
     from .spline_gpu import spline_gpu
+    from .spline_quadratic_gpu import spline_quadratic_gpu
 
 
 def spline(input, kernel_size, is_open_spline, K, degree):
     if input.is_cuda:
-        return spline_gpu(input, kernel_size, is_open_spline, K, degree)
+        if degree == 1:
+            return spline_gpu(input, kernel_size, is_open_spline, K)
+        if degree == 2:
+            return spline_quadratic_gpu(input, kernel_size, is_open_spline, K)
+        else:
+            raise NotImplementedError()
     else:
         return spline_cpu(input, kernel_size, is_open_spline, degree)
