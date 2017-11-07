f586a8c0573402bd5457872e6fceda06a01ac834
rusty1s <matthias.fey@tu-dortmund.de>
2017-11-07 10:53:04 +0100

    besser accuracies on mnist and cora

--- examples/mnist_superpixel_75.py
+++ examples/mnist_superpixel_75.py
@@ -12,16 +12,18 @@ sys.path.insert(0, '.')
 sys.path.insert(0, '..')
 
 from torch_geometric.datasets import MNISTSuperpixel75  # noqa
-from torch_geometric.transform import Graclus, PolarAdj  # noqa
+from torch_geometric.transform import Graclus, PolarAdj, EuclideanAdj  # noqa
 from torch_geometric.utils import DataLoader  # noqa
 from torch_geometric.nn.modules import SplineGCN, GraclusMaxPool  # noqa
 from torch_geometric.nn.functional import batch_average  # noqa
 
 path = '~/MNISTSuperpixel75'
 train_dataset = MNISTSuperpixel75(
-    path, train=True, transform=Compose([Graclus(4), PolarAdj()]))
+    path, train=True, transform=Compose([Graclus(4),
+                                         EuclideanAdj()]))
 test_dataset = MNISTSuperpixel75(
-    path, train=False, transform=Compose([Graclus(4), PolarAdj()]))
+    path, train=False, transform=Compose([Graclus(4),
+                                          EuclideanAdj()]))
 
 train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=64, shuffle=True)
@@ -32,22 +34,21 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.pool = GraclusMaxPool(2)
         self.conv1 = SplineGCN(
-            2, 32, dim=2, kernel_size=[2, 8], is_open_spline=[1, 0])
+            2, 32, dim=2, kernel_size=[5, 5], is_open_spline=[1, 1])
         self.conv2 = SplineGCN(
-            32, 64, dim=2, kernel_size=[2, 8], is_open_spline=[1, 0])
-        self.conv3 = SplineGCN(
-            64, 512, dim=2, kernel_size=[2, 8], is_open_spline=[1, 0])
-        self.fc1 = nn.Linear(512, 10)
+            32, 64, dim=2, kernel_size=[5, 5], is_open_spline=[1, 1])
+        self.fc1 = nn.Linear(64, 128)
+        self.fc2 = nn.Linear(128, 10)
 
     def forward(self, adjs, x, slice):
-        x = F.relu(self.conv1(adjs[0], x))
+        x = F.elu(self.conv1(adjs[0], x))
         x = self.pool(x)
-        x = F.relu(self.conv2(adjs[1], x))
+        x = F.elu(self.conv2(adjs[1], x))
         x = self.pool(x)
-        x = F.relu(self.conv3(adjs[2], x))
         x = batch_average(x, slice)
+        x = F.elu(self.fc1(x))
         x = F.dropout(x, training=self.training)
-        x = self.fc1(x)
+        x = self.fc2(x)
         return F.log_softmax(x)
 
 
@@ -61,18 +62,9 @@ optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 def train(epoch):
     model.train()
 
-    if epoch == 10:
+    if epoch == 11:
         for param_group in optimizer.param_groups:
             param_group['lr'] = 0.001
-
-    if epoch == 20:
-        for param_group in optimizer.param_groups:
-            param_group['lr'] = 0.0001
-
-    # Unnecessary...
-    if epoch == 40:
-        for param_group in optimizer.param_groups:
-            param_group['lr'] = 0.00001
 
     for batch, ((input, adjs, _), target) in enumerate(train_loader):
         adj_0, adj_1, adj_2 = adjs[0][0], adjs[2][0], adjs[4][0]
@@ -92,13 +84,12 @@ def train(epoch):
         loss.backward()
         optimizer.step()
 
+        # pred = output.data.max(2)[1]
+        # correct = pred.eq(target.data).cpu().sum()
+        # acc = correct / target.size(0)
 
-#         pred = output.data.max(1)[1]
-#         correct = pred.eq(target.data).cpu().sum()
-#         acc = correct / 64
-
-#         print('Epoch:', epoch, 'Batch:', batch, 'Loss:', loss.data[0],
-#               'Accuracy:', acc)
+        # print('Epoch:', epoch, 'Batch:', batch, 'Loss:', loss.data[0],
+        #       'Accuracy:', acc)
 
 
 def test(epoch):
@@ -126,6 +117,6 @@ def test(epoch):
     print('Epoch:', epoch, 'Accuracy:', correct / 10000)
 
 
-for epoch in range(1, 100):
+for epoch in range(1, 21):
     train(epoch)
     test(epoch)
--- torch_geometric/transform/__init__.py
+++ torch_geometric/transform/__init__.py
@@ -1,6 +1,9 @@
 from .euclidean import EuclideanAdj
 from .polar import PolarAdj
 from .degree import DegreeAdj
+from .target_degree import TargetDegreeAdj
 from .graclus import Graclus
 
-__all__ = ['EuclideanAdj', 'PolarAdj', 'DegreeAdj', 'Graclus']
+__all__ = [
+    'EuclideanAdj', 'PolarAdj', 'DegreeAdj', 'TargetDegreeAdj', 'Graclus'
+]
