3466fca0c47753374ca91b32cc0aa0eedf517af1
rusty1s <matthias.fey@tu-dortmund.de>
2017-11-08 09:35:59 +0100

    mnist grid to euclidean

--- examples/mnist_grid.py
+++ examples/mnist_grid.py
@@ -13,11 +13,11 @@ sys.path.insert(0, '..')
 
 from torch_geometric.graph.grid import grid, grid_position  # noqa
 from torch_geometric.transform.graclus import graclus, perm_input  # noqa
-from torch_geometric.transform import PolarAdj  # noqa
+from torch_geometric.transform import PolarAdj, EuclideanAdj  # noqa
 from torch_geometric.sparse.stack import stack  # noqa
 from torch_geometric.nn.modules import SplineGCN, GraclusMaxPool  # noqa
 
-batch_size = 64
+batch_size = 100
 transform = transforms.Compose([transforms.ToTensor()])
 train_loader = torch.utils.data.DataLoader(
     datasets.MNIST(
@@ -38,17 +38,8 @@ adjs = adjs[0:5:2]
 positions = positions[0:5:2]
 num_first_fc = adjs[2].size(0)
 
-transform = PolarAdj()
+transform = EuclideanAdj()
 _, adjs, positions = transform((None, adjs, positions))
-
-# new_adjs = []
-# for adj in adjs:
-#     index = adj._indices()
-#     weight = adj._values()[:, 1]
-#     n = adj.size(0)
-#     adj = torch.sparse.FloatTensor(index, weight, torch.Size([n, n]))
-#     new_adjs.append(adj)
-# adjs = new_adjs
 
 adjs = [stack([adj for _ in range(batch_size)])[0] for adj in adjs]
 
@@ -63,13 +54,13 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.pool = GraclusMaxPool(2)
         self.conv1 = SplineGCN(
-            1, 32, dim=2, kernel_size=[2, 8], is_open_spline=[1, 0])
+            1, 32, dim=2, kernel_size=[4, 4], is_open_spline=[1, 1])
         self.conv2 = SplineGCN(
-            32, 32, dim=2, kernel_size=[2, 8], is_open_spline=[1, 0])
+            32, 32, dim=2, kernel_size=[4, 4], is_open_spline=[1, 1])
         self.conv3 = SplineGCN(
-            32, 64, dim=2, kernel_size=[2, 8], is_open_spline=[1, 0])
+            32, 64, dim=2, kernel_size=[4, 4], is_open_spline=[1, 1])
         self.conv4 = SplineGCN(
-            64, 64, dim=2, kernel_size=[2, 8], is_open_spline=[1, 0])
+            64, 64, dim=2, kernel_size=[4, 4], is_open_spline=[1, 1])
         self.fc1 = nn.Linear(num_first_fc * 64, 1024)
         self.fc2 = nn.Linear(1024, 10)
 
@@ -91,11 +82,15 @@ model = Net()
 if torch.cuda.is_available():
     model.cuda()
 
-optimizer = torch.optim.Adam(model.parameters(), lr=0.00001)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
 
 def train(epoch):
     model.train()
+
+    if epoch == 11:
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = 0.0001
 
     for batch, (data, target) in enumerate(train_loader):
         data = data.view(data.size(0), -1, 1)
@@ -112,13 +107,29 @@ def train(epoch):
         loss.backward()
         optimizer.step()
 
+
+def test(epoch):
+    model.eval()
+
+    correct = 0
+
+    for batch, (data, target) in enumerate(test_loader):
+        data = data.view(data.size(0), -1, 1)
+        input = torch.cat([perm_input(img, perm) for img in data], dim=0)
+
+        if torch.cuda.is_available():
+            input, target = input.cuda(), target.cuda()
+
+        input, target = Variable(input), Variable(target)
+
+        output = model(adjs, input)
         pred = output.data.max(1)[1]
-        correct = pred.eq(target.data).cpu().sum()
-        acc = correct / target.size(0)
 
-        print('Epoch:', epoch, 'Batch:', batch, 'Loss:', loss.data[0],
-              'Accuracy:', acc)
+        correct += pred.eq(target.data).cpu().sum()
+
+    print('Epoch:', epoch, 'Accuracy:', correct / 10000)
 
 
-for epoch in range(1, 11):
+for epoch in range(1, 21):
     train(epoch)
+    test(epoch)
