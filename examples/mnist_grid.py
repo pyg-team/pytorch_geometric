a5658a5d588d261fc286780aaba9ca493c86cc09
rusty1s <matthias.fey@tu-dortmund.de>
2017-11-08 12:02:34 +0100

    bugfixes

--- examples/mnist_grid.py
+++ examples/mnist_grid.py
@@ -61,7 +61,7 @@ class Net(nn.Module):
             6, 16, dim=2, kernel_size=[4, 4], is_open_spline=[1, 1])
         self.conv4 = SplineGCN(
             16, 16, dim=2, kernel_size=[4, 4], is_open_spline=[1, 1])
-        self.fc1 = nn.Linear(num_first_fc * 64, 120)
+        self.fc1 = nn.Linear(num_first_fc * 16, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
@@ -72,7 +72,7 @@ class Net(nn.Module):
         x = F.elu(self.conv3(adjs[1], x))
         x = F.elu(self.conv4(adjs[1], x))
         x = self.pool(x)
-        x = x.contiguous().view(-1, num_first_fc * 64)
+        x = x.contiguous().view(-1, num_first_fc * 16)
         x = F.elu(self.fc1(x))
         x = F.elu(self.fc2(x))
         x = F.dropout(x, training=self.training)
