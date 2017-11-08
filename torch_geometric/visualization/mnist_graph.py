988181dc9d50f3394684aa4d13cb6c8f5416cbc5
rusty1s <matthias.fey@tu-dortmund.de>
2017-11-08 17:27:38 +0100

    added alpha channel

--- torch_geometric/visualization/mnist_graph.py
+++ torch_geometric/visualization/mnist_graph.py
@@ -76,25 +76,26 @@ image = np.array(image)
 image = np.ones((28, 28), np.uint8)
 image = np.repeat(image, scale * rescale, axis=0)
 image = np.repeat(image, scale * rescale, axis=1)
-image = gray2rgb(image)
+image = gray2rgb(image, alpha=True)
 # image *= np.array([228, 246, 232], np.uint8)
-image *= np.array([255, 255, 255], np.uint8)
+# image *= np.array([255, 255, 255, 1], np.uint8)
+image *= np.array([0, 0, 0, 0], np.uint8)
 image = Image.fromarray(image)
 draw = ImageDraw.Draw(image)
 
 offset = torch.FloatTensor([(28 - position[:, 0].max()) / 2,
                             (28 - position[:, 1].max()) / 2])
 
-row, col = adj._indices()
-direction = position[row] - position[col]
-direction = (direction * direction).sum(dim=1)
-mask = direction < 30
-row = row[mask]
-col = col[mask]
-index = torch.stack([row, col], dim=0)
-weight = torch.FloatTensor(row.size(0)).fill_(1)
-n = adj.size(0)
-adj = torch.sparse.FloatTensor(index, weight, torch.Size([n, n]))
+# row, col = adj._indices()
+# direction = position[row] - position[col]
+# direction = (direction * direction).sum(dim=1)
+# mask = direction < 30
+# row = row[mask]
+# col = col[mask]
+# index = torch.stack([row, col], dim=0)
+# weight = torch.FloatTensor(row.size(0)).fill_(1)
+# n = adj.size(0)
+# adj = torch.sparse.FloatTensor(index, weight, torch.Size([n, n]))
 
 position *= scale * rescale
 position += scale * offset * rescale
@@ -113,17 +114,28 @@ for i in range(index.size(0)):
     end_x, end_y = int(end_x), int(end_y)
 
     draw.line(
-        (start_x, start_y, end_x, end_y), fill=(0, 0, 0), width=rescale * 2)
+        (start_x, start_y, end_x, end_y),
+        fill=(0, 0, 0, 255),
+        width=rescale * 2)
 
 for i in range(position.size(0)):
     x, y = position[i]
     r = 16 * rescale
-    draw.ellipse((x - r, y - r, x + r, y + r), fill=(0, 0, 0))
+    draw.ellipse((x - r, y - r, x + r, y + r), fill=(0, 0, 0, 255))
     r = 14 * rescale
-    if input[i] > 0.3:
-        draw.ellipse((x - r, y - r, x + r, y + r), fill=(49, 130, 219))
-    else:
-        draw.ellipse((x - r, y - r, x + r, y + r), fill=(228, 246, 232))
+    draw.ellipse(
+        (x - r, y - r, x + r, y + r), fill=(49, 130, 219, int(255 * input[i])))
+    # if v > 0:
+    #     print(v)
+    # c1 = [49 * v, 130 * v, 219 * v]
+    # c2 = [228 * (1 - v), 246 * (1 - v), 232 * (1 - v)]
+    # c = (int(c1[0] + c2[0]), 130, 219)
+
+    # draw.ellipse((x - r, y - r, x + r, y + r), fill=c)
+    # if input[i] > 0.3:
+    #     draw.ellipse((x - r, y - r, x + r, y + r), fill=(49, 130, 219))
+    # else:
+    #     draw.ellipse((x - r, y - r, x + r, y + r), fill=(228, 246, 232))
 
 image = image.resize((28 * scale, 28 * scale), Image.ANTIALIAS)
 
