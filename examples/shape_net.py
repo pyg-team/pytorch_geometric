a2f3a19a455f7b8ddca232652fb0f0bea866df56
rusty1s <matthias.fey@tu-dortmund.de>
2017-11-10 13:49:14 +0100

    reset faust, added shape net

--- examples/shape_net.py
+++ examples/shape_net.py
@@ -3,11 +3,122 @@ from __future__ import division, print_function
 import sys
 
 import torch
+from torch import nn
+import torch.nn.functional as F
+from torch.autograd import Variable
 
 sys.path.insert(0, '.')
 sys.path.insert(0, '..')
 
 from torch_geometric.datasets import ShapeNet  # noqa
+from torch_geometric.transform import EuclideanAdj  # noqa
+from torch_geometric.utils import DataLoader  # noqa
+from torch_geometric.nn.modules import SplineGCN, Lin  # noqa
 
 path = '~/ShapeNet'
-train_dataset = ShapeNet(path, train=True)
+train_dataset = ShapeNet(
+    path, train=True, category='cap', transform=EuclideanAdj())
+test_dataset = ShapeNet(
+    path, train=False, category='cap', transform=EuclideanAdj())
+
+train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=1, shuffle=True)
+num_classes = 2
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1_1 = SplineGCN(1, 32, dim=3, kernel_size=5)
+        self.conv1_2 = SplineGCN(32, 64, dim=3, kernel_size=5)
+        self.lin1_3 = Lin(64, 16)
+        self.conv2_1 = SplineGCN(16, 32, dim=3, kernel_size=5)
+        self.conv2_2 = SplineGCN(32, 64, dim=3, kernel_size=5)
+        self.lin2_3 = Lin(64, 16)
+        self.conv3_1 = SplineGCN(16, 32, dim=3, kernel_size=5)
+        self.conv3_2 = SplineGCN(32, 64, dim=3, kernel_size=5)
+        self.lin3_3 = Lin(64, 16)
+        self.conv4_1 = SplineGCN(16, 32, dim=3, kernel_size=5)
+        self.conv4_2 = SplineGCN(32, 64, dim=3, kernel_size=5)
+        self.lin4_3 = Lin(64, 512)
+        self.lin4_4 = Lin(512, num_classes)
+
+    def forward(self, adj, x):
+        x = F.elu(self.conv1_1(adj, x))
+        x = F.elu(self.conv1_2(adj, x))
+        x = F.elu(self.lin1_3(x))
+        x = F.elu(self.conv2_1(adj, x))
+        x = F.elu(self.conv2_2(adj, x))
+        x = F.elu(self.lin2_3(x))
+        x = F.elu(self.conv3_1(adj, x))
+        x = F.elu(self.conv3_2(adj, x))
+        x = F.elu(self.lin3_3(x))
+        x = F.elu(self.conv4_1(adj, x))
+        x = F.elu(self.conv4_2(adj, x))
+        x = F.elu(self.lin4_3(x))
+        x = F.dropout(x, training=self.training)
+        x = self.lin4_4(x)
+        return F.log_softmax(x)
+
+
+model = Net()
+if torch.cuda.is_available():
+    model.cuda()
+
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+
+def train(epoch):
+    model.train()
+
+    if epoch == 15:
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = 0.001
+
+    if epoch == 150:
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = 0.0001
+
+    for batch, ((input, (adj, _), _), target) in enumerate(train_loader):
+        if torch.cuda.is_available():
+            input, adj, target = input.cuda(), adj.cuda(), target.cuda()
+
+        input, target = Variable(input), Variable(target)
+
+        optimizer.zero_grad()
+        output = model(adj, input)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+
+
+def test(epoch):
+    model.eval()
+
+    acc = 0
+
+    for (input, (adj, _), _), target in test_loader:
+        if torch.cuda.is_available():
+            input, adj, target = input.cuda(), adj.cuda(), target.cuda()
+
+        output = model(adj, Variable(input))
+        pred = output.data.max(1)[1]
+
+        a = 0
+        for i in range(num_classes):
+            p = pred == i
+            t = target == i
+
+            tp = ((p + t) > 1).sum()
+            fp = p.sum() - tp
+            fn = t.sum() - tp
+            a += tp / (tp + fp + fn)
+        a /= num_classes
+        acc += a
+
+    print('Epoch:', epoch, 'Accuracy:', acc / len(test_loader))
+
+
+for epoch in range(1, 400):
+    train(epoch)
+    test(epoch)
--- torch_geometric/datasets/shape_net.py
+++ torch_geometric/datasets/shape_net.py
@@ -7,11 +7,14 @@ from torch.utils.data import Dataset
 from .utils.dir import make_dirs
 from .utils.download import download_url
 from .utils.extract import extract_zip
+from ..graph.point_cloud import nn_graph
 
 
 class ShapeNet(Dataset):
     url = 'https://shapenet.cs.stanford.edu/ericyi/shapenetcore_partanno_segmentation_benchmark_v0.zip'
     raw_folder = 'shapenetcore_partanno_segmentation_benchmark_v0'
+    training_filename = 'training.pt'
+    test_filename = 'test.pt'
 
     categories = {
         'airplaine': '02691156',
@@ -49,12 +52,38 @@ class ShapeNet(Dataset):
         self.raw_folder = os.path.join(self.root, self.raw_folder)
 
         self.category = category
-        self.train = train
         self.transform = transform
         self.target_transform = target_transform
 
         self.download()
         self.process()
+
+        filename = self.training_filename if train else self.test_filename
+        data_file = os.path.join(self.processed_folder, category, filename)
+        self.index, self.point, target, self.slice = torch.load(data_file)
+        self.target = target.long() - 1
+
+    def __getitem__(self, i):
+        start, end = self.slice[i:i + 2]
+        index = self.index[:, 6 * start:6 * end]
+        weight = torch.FloatTensor(index.size(1)).fill_(1)
+        point = self.point[start:end]
+        target = self.target[start:end]
+        n = point.size(0)
+        adj = torch.sparse.FloatTensor(index, weight, torch.Size([n, n]))
+        input = point.new(n, 1).fill_(1)
+        data = (input, adj, point)
+
+        if self.transform is not None:
+            data = self.transform(data)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return data, target
+
+    def __len__(self):
+        return self.slice.size(0) - 1
 
     def _check_exists(self):
         return os.path.exists(self.root)
@@ -73,12 +102,12 @@ class ShapeNet(Dataset):
         os.unlink(file_path)
 
     def process(self):
-        # if self._check_processed():
-        #     return
+        if self._check_processed():
+            return
 
         print('Processing...')
 
-        make_dirs(os.path.join(self.processed_folder))
+        make_dirs(self.processed_folder)
 
         for category in self.categories:
             self._process(category)
@@ -100,12 +129,16 @@ class ShapeNet(Dataset):
         point_dir = os.path.join(self.raw_folder, idx, 'points')
         target_dir = os.path.join(self.raw_folder, idx, 'points_label')
 
+        train_slices = [0]
         train_points = []
         train_targets = []
+        train_edges = []
+        test_slices = [0]
         test_points = []
         test_targets = []
+        test_edges = []
 
-        for filename in os.listdir(point_dir):
+        for i, filename in enumerate(os.listdir(point_dir)):
             idx = filename.split('.')[0]
 
             point_file = os.path.join(point_dir, '{}.pts'.format(idx))
@@ -113,20 +146,37 @@ class ShapeNet(Dataset):
                 points = [float(x) for x in f.read().split()]
                 point = torch.FloatTensor(points).view(-1, 3)
 
+            index = nn_graph(point, k=6)
+
             target_file = os.path.join(target_dir, '{}.seg'.format(idx))
             with open(target_file, 'r') as f:
                 targets = [int(x) for x in f.read().split()]
                 target = torch.ByteTensor(targets)
 
-            print(target.size(), point.size())
             if idx not in test_indices:
+                train_slices.append(train_slices[-1] + len(point))
                 train_points.append(point)
+                train_edges.append(index)
                 train_targets.append(target)
             else:
+                test_slices.append(test_slices[-1] + len(point))
                 test_points.append(point)
+                test_edges.append(index)
                 test_targets.append(target)
 
-            # TODO: Generate edges
-            # TODO: Generate weights
+        processed_folder = os.path.join(self.processed_folder, category)
+        make_dirs(processed_folder)
 
-            # TODO: Save as *.pt
+        train_file = os.path.join(processed_folder, self.training_filename)
+        slice = torch.LongTensor(train_slices)
+        point = torch.cat(train_points, dim=0)
+        index = torch.cat(train_edges, dim=1)
+        target = torch.cat(train_targets, dim=0)
+        torch.save((index, point, target, slice), train_file)
+
+        test_file = os.path.join(processed_folder, self.test_filename)
+        slice = torch.LongTensor(test_slices)
+        point = torch.cat(test_points, dim=0)
+        index = torch.cat(test_edges, dim=1)
+        target = torch.cat(test_targets, dim=0)
+        torch.save((index, point, target, slice), test_file)
