5298b9edfdd8c35a5bc6ff786e967e5bb0baf4f9
rusty1s <matthias.fey@tu-dortmund.de>
2017-11-11 15:25:48 +0100

    added doc script

--- docs.sh
+++ docs.sh
@@ -3,3 +3,9 @@
 git checkout gh-pages
 git checkout master docs torch_geometric
 cd docs && make html
+mv -fv docs/build/html/* ./
+rm -rf docs torch_geometric
+git add -A
+git commit -m "Generated gh-pages for $(git log master -1 --pretty=short --abbrev-commit)"
+git push origin gh-pages
+git checkout master
