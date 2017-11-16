f8c9e071bf96e4d69324ce5d07252f3b98322be7
rusty1s <matthias.fey@tu-dortmund.de>
2017-11-16 19:34:49 +0100

    added methods to init

--- torch_geometric/sparse/__init__.py
+++ torch_geometric/sparse/__init__.py
@@ -1,11 +1,13 @@
 import torch.sparse as sparse
 
 from .mm import mm
+from .mm_diagonal import mm_diagonal
 from .sum import sum
+from .eye import eye
 
 
 def SparseTensor(indices, values, size):
     return getattr(sparse, values.__class__.__name__)(indices, values, size)
 
 
-__all__ = ['SparseTensor', 'mm', 'sum']
+__all__ = ['SparseTensor', 'mm', 'mm_diagonal', 'sum', 'eye']
