44aa742260f1add3c6ef7885cd2984c8054b44ea
rusty1s <matthias.fey@tu-dortmund.de>
2017-11-16 18:46:16 +0100

    sum impl

--- torch_geometric/sparse/sum.py
+++ torch_geometric/sparse/sum.py
@@ -1,47 +1,12 @@
 import torch
-from torch.autograd import Function
-
-
-class _Sum(Function):
-    def __init__(self, dim):
-        super(_Sum, self).__init__()
-        self.dim = dim
-
-    def forward(self, a):
-        self.save_for_backward(a)
-        return _sum(a, self.dim)
-
-    def backward(self, grad_output):
-        print(grad_output)
-        pass
-
-
-def _sum(a, dim):
-    if dim is None:
-        return torch.sum(a._values())
-
-    if dim > 1:
-        raise ValueError()
-
-    values = a._values()
-    index = a._indices()[1 - dim]
-
-    # TODO: zero as same type as `a`.
-    if len(a.size()) == 2:
-        zero = torch.zeros(a.size(dim))
-    else:
-        remaining_dim = list(a.size()[2:])
-        zero = torch.zeros(a.size(dim), *remaining_dim)
-        index = index.view(-1, 1).expand(index.size(0), *remaining_dim)
-
-    if a.is_cuda is True:
-        zero = zero.cuda()
-
-    return zero.scatter_add_(0, index, values)
 
 
 def sum(a, dim=None):
-    if torch.is_tensor(a):
-        return _sum(a, dim)
-    else:
-        return _Sum(dim)(a)
+    if dim is None:
+        return torch.sum(a._values())
+
+    value = a._values()
+    index = a._indices()[1 - dim]
+    n = a.size(1 - dim)
+
+    return value.new(n).fill_(0).scatter_add_(0, index, value)
--- torch_geometric/sparse/sum_test.py
+++ torch_geometric/sparse/sum_test.py
@@ -1,61 +1,25 @@
 from unittest import TestCase
 
 import torch
-from torch.autograd import Variable
 from numpy.testing import assert_equal
 
 from .sum import sum
 
 
 class SumTest(TestCase):
-    def test_tensor_2d(self):
-        i = torch.LongTensor([[0, 0, 1], [1, 2, 0]])
-        v = torch.FloatTensor([4, 3, 2])
-        a_1 = torch.sparse.FloatTensor(i, v, torch.Size([3, 3]))
-        a_2 = torch.FloatTensor([[0, 4, 3], [2, 0, 0], [0, 0, 0]])
+    def test_sum(self):
+        index = torch.LongTensor([[0, 0, 1, 3], [1, 2, 0, 2]])
+        value = torch.FloatTensor([4, 3, 2, 3])
+        a = torch.sparse.FloatTensor(index, value, torch.Size([4, 3]))
 
-        assert_equal(a_1.to_dense().numpy(), a_2.numpy())
-        assert_equal(sum(a_1), torch.sum(a_2))
-        assert_equal(sum(a_1, dim=0).numpy(), torch.sum(a_2, dim=0).numpy())
-        assert_equal(sum(a_1, dim=1).numpy(), torch.sum(a_2, dim=1).numpy())
+        expected_a = [
+            [0, 4, 3],
+            [2, 0, 0],
+            [0, 0, 0],
+            [0, 0, 3],
+        ]
 
-    def test_tensor_3d(self):
-        i = torch.LongTensor([[0, 0, 1], [1, 2, 0]])
-        v = torch.FloatTensor([[4, 5], [3, 7], [2, 1]])
-        a_1 = torch.sparse.FloatTensor(i, v, torch.Size([3, 3, 2]))
-        a_2 = torch.FloatTensor([
-            [[0, 0], [4, 5], [3, 7]],
-            [[2, 1], [0, 0], [0, 0]],
-            [[0, 0], [0, 0], [0, 0]],
-        ])
-
-        assert_equal(a_1.to_dense().numpy(), a_2.numpy())
-        assert_equal(sum(a_1), torch.sum(a_2))
-        assert_equal(sum(a_1, dim=0).numpy(), torch.sum(a_2, dim=0).numpy())
-        assert_equal(sum(a_1, dim=1).numpy(), torch.sum(a_2, dim=1).numpy())
-
-    def test_tensor_4d(self):
-        print("TODO")
-
-    def test_autograd(self):
-        i = torch.LongTensor([[0, 0, 1], [1, 2, 0]])
-        v = torch.FloatTensor([4, 3, 2])
-        a_1 = torch.sparse.FloatTensor(i, v, torch.Size([3, 3]))
-        a_1 = Variable(a_1, requires_grad=True)
-        a_2 = torch.FloatTensor([[0, 4, 3], [2, 0, 0], [0, 0, 0]])
-        a_2 = Variable(a_2, requires_grad=True)
-
-        c_1 = sum(a_1, dim=1)
-        c_2 = torch.sum(a_2, dim=1)
-
-        gradients = torch.FloatTensor([1, 1, 1])
-        c_2.backward(gradients)
-        # c_1.max().backward()
-        # c_2.max().backward()
-
-        # print(c_2)
-        # print(a_2.grad)
-        # print()
-        assert_equal(a_1.data.to_dense().numpy(), a_2.data.numpy())
-        assert_equal(c_1.data.numpy(), c_2.data.numpy())
-        # assert_equal(a_1.grad.data.numpy(), a_2.grad.data.numpy())
+        assert_equal(a.to_dense().numpy(), expected_a)
+        assert_equal(sum(a), 12)
+        assert_equal(sum(a, dim=0).numpy(), [2, 4, 6])
+        assert_equal(sum(a, dim=1).numpy(), [7, 2, 0, 3])
