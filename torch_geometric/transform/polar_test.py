831e2ebde9122f2ffb2d128c4f8b8e85151efad0
rusty1s <matthias.fey@tu-dortmund.de>
2017-11-18 18:35:04 +0100

    transform to new format

--- torch_geometric/transform/polar_test.py
+++ torch_geometric/transform/polar_test.py
@@ -4,34 +4,27 @@ from unittest import TestCase
 import torch
 from numpy.testing import assert_equal, assert_almost_equal
 
+from ..datasets.data import Data
 from .polar import PolarAdj
 
 
 class PolarTest(TestCase):
     def test_polar_adj(self):
-        position = torch.LongTensor([[1, 0], [0, 0], [-2, 2]])
         index = torch.LongTensor([[0, 1, 1, 2], [1, 0, 2, 1]])
         weight = torch.FloatTensor([1, 1, 1, 1])
         adj = torch.sparse.FloatTensor(index, weight, torch.Size([3, 3]))
+        position = torch.FloatTensor([[1, 0], [0, 0], [-2, 2]])
+        data = Data(None, adj, position, None)
 
-        transform = PolarAdj()
+        data = PolarAdj()(data)
+        adj, position = data.adj, data.position
 
-        _, adj, position = transform((None, adj, position))
-        adj = adj.to_dense()
-
+        expected_adj = [
+            [[0, 0], [1 / sqrt(8), 0.5], [0, 0]],
+            [[1 / sqrt(8), 0], [0, 0], [1, 0.375]],
+            [[0, 0], [1, 0.875], [0, 0]],
+        ]
         expected_position = [[1, 0], [0, 0], [-2, 2]]
-        expected_adj_rho = [
-            [0, 1 / sqrt(8), 0],
-            [1 / sqrt(8), 0, 1],
-            [0, 1, 0],
-        ]
-        expected_adj_theta = [
-            [0, 0.5, 0],
-            [0, 0, 0.375],
-            [0, 0.875, 0],
-        ]
 
-        assert_equal(adj.size(), [3, 3, 2])
+        assert_almost_equal(adj.to_dense().numpy(), expected_adj)
         assert_equal(position.numpy(), expected_position)
-        assert_almost_equal(adj[:, :, 0].numpy(), expected_adj_rho)
-        assert_almost_equal(adj[:, :, 1].numpy(), expected_adj_theta)
--- torch_geometric/transform/spherical_test.py
+++ torch_geometric/transform/spherical_test.py
@@ -4,40 +4,27 @@ from unittest import TestCase
 import torch
 from numpy.testing import assert_equal, assert_almost_equal
 
+from ..datasets.data import Data
 from .spherical import SphericalAdj
 
 
 class SphericalTest(TestCase):
     def test_spherical_adj(self):
-        position = torch.LongTensor([[1, 0, 0], [1, 0, 1], [-1, 2, 1]])
         index = torch.LongTensor([[0, 1, 1, 2], [1, 0, 2, 1]])
         weight = torch.FloatTensor([1, 1, 1, 1])
         adj = torch.sparse.FloatTensor(index, weight, torch.Size([3, 3]))
+        position = torch.FloatTensor([[1, 0, 0], [1, 0, 1], [-1, 2, 1]])
+        data = Data(None, adj, position, None)
 
-        transform = SphericalAdj()
+        data = SphericalAdj()(data)
+        adj, position = data.adj, data.position
 
-        _, adj, position = transform((None, adj, position))
-        adj = adj.to_dense()
-
+        expected_adj = [
+            [[0, 0, 0], [1 / sqrt(8), 0, 0], [0, 0, 0]],
+            [[1 / sqrt(8), 0, 1], [0, 0, 0], [1, 0.375, 0.5]],
+            [[0, 0, 0], [1, 0.875, 0.5], [0, 0, 0]],
+        ]
         expected_position = [[1, 0, 0], [1, 0, 1], [-1, 2, 1]]
-        expected_adj_rho = [
-            [0, 1 / sqrt(8), 0],
-            [1 / sqrt(8), 0, 1],
-            [0, 1, 0],
-        ]
-        expected_adj_theta = [
-            [0, 0, 0],
-            [0, 0, 0.375],
-            [0, 0.875, 0],
-        ]
-        expected_adj_phi = [
-            [0, 0, 0],
-            [1, 0, 0.5],
-            [0, 0.5, 0],
-        ]
 
-        assert_equal(adj.size(), [3, 3, 3])
+        assert_almost_equal(adj.to_dense().numpy(), expected_adj)
         assert_equal(position.numpy(), expected_position)
-        assert_almost_equal(adj[:, :, 0].numpy(), expected_adj_rho)
-        assert_almost_equal(adj[:, :, 1].numpy(), expected_adj_theta)
-        assert_almost_equal(adj[:, :, 2].numpy(), expected_adj_phi)
