47d9df0718659f0f4e642f8f70f404eeec149bfb
rusty1s <matthias.fey@tu-dortmund.de>
2017-11-19 15:09:21 +0100

    graclus to new data format

--- torch_geometric/transforms/graclus_test.py
+++ torch_geometric/transforms/graclus_test.py
@@ -2,34 +2,29 @@ from unittest import TestCase
 import torch
 from numpy.testing import assert_equal
 
+from ..datasets.data import Data
 from .graclus import Graclus
 
 
 class GraclusTest(TestCase):
     def test_graclus_adj(self):
         input = torch.FloatTensor([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]])
-        position = torch.FloatTensor([[1, 1], [3, 2], [3, 0], [4, 1], [8, 3]])
         index = torch.LongTensor([
             [0, 0, 1, 1, 2, 2, 3, 3, 3, 4],
             [1, 2, 0, 3, 0, 3, 1, 2, 4, 3],
         ])
         weight = torch.FloatTensor(10).fill_(1)
         adj = torch.sparse.FloatTensor(index, weight, torch.Size([5, 5]))
+        position = torch.FloatTensor([[1, 1], [3, 2], [3, 0], [4, 1], [8, 3]])
+        data = Data(input, adj, position, None)
 
-        transform = Graclus()
-        rid = torch.LongTensor([4, 3, 2, 0, 1])
+        data = Graclus()(data, rid=torch.LongTensor([4, 3, 2, 0, 1]))
 
-        input, adj, position = transform((input, adj, position), rid)
+        self.assertEqual(len(data), 2)
 
         # Cluster = [1, 0, 1, 0, 2, 2].
         expected_input = [[3, 4], [7, 8], [1, 2], [5, 6], [9, 10], [0, 0]]
-        assert_equal(input.numpy(), expected_input)
-
-        self.assertEqual(len(position), 2)
-        expected_position_0 = [[3, 2], [4, 1], [1, 1], [3, 0], [8, 3], [0, 0]]
-        assert_equal(position[0].numpy(), expected_position_0)
-        expected_position_1 = [[3.5, 1.5], [2, 0.5], [8, 3]]
-        assert_equal(position[1].numpy(), expected_position_1)
+        assert_equal(data[0].input.numpy(), expected_input)
 
         expected_adj_0 = [
             [0, 1, 1, 0, 0, 0],
@@ -39,10 +34,15 @@ class GraclusTest(TestCase):
             [0, 1, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0],
         ]
-        assert_equal(adj[0].to_dense().numpy(), expected_adj_0)
+        assert_equal(data[0].adj.to_dense().numpy(), expected_adj_0)
         expected_adj_1 = [
             [0, 2, 1],
             [2, 0, 0],
             [1, 0, 0],
         ]
-        assert_equal(adj[1].to_dense().numpy(), expected_adj_1)
+        assert_equal(data[1].adj.to_dense().numpy(), expected_adj_1)
+
+        expected_position_0 = [[3, 2], [4, 1], [1, 1], [3, 0], [8, 3], [0, 0]]
+        assert_equal(data[0].position.numpy(), expected_position_0)
+        expected_position_1 = [[3.5, 1.5], [2, 0.5], [8, 3]]
+        assert_equal(data[1].position.numpy(), expected_position_1)
