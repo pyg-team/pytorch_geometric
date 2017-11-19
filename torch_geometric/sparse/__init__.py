bcc5b1413cc84b3b16786560d443948a718a1abb
rusty1s <matthias.fey@tu-dortmund.de>
2017-11-19 15:20:13 +0100

    cleanup

--- torch_geometric/sparse/__init__.py
+++ torch_geometric/sparse/__init__.py
@@ -4,10 +4,11 @@ from .mm import mm
 from .mm_diagonal import mm_diagonal
 from .sum import sum
 from .eye import eye
+from .stack import stack
 
 
 def SparseTensor(indices, values, size):
     return getattr(sparse, values.__class__.__name__)(indices, values, size)
 
 
-__all__ = ['SparseTensor', 'mm', 'mm_diagonal', 'sum', 'eye']
+__all__ = ['SparseTensor', 'mm', 'mm_diagonal', 'sum', 'eye', 'stack']
