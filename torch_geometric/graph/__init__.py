29c5bdf03608255286380db23944b14922da03c7
rusty1s <matthias.fey@tu-dortmund.de>
2017-11-19 15:18:58 +0100

    cleanup

---
+++ torch_geometric/graph/__init__.py
@@ -0,0 +1,7 @@
+from .grid import grid_3x3, grid_5x5, grid_position
+from .geometry import edges_from_faces
+from .point_cloud import nn_graph
+
+__all__ = [
+    'grid_3x3', 'grid_5x5', 'grid_position', 'edges_from_faces', 'nn_graph'
+]
