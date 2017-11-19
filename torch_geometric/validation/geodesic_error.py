9cd142f53c08c7a9af9e60596af35abf550a143c
rusty1s <matthias.fey@tu-dortmund.de>
2017-11-19 15:17:25 +0100

    cleanup

--- torch_geometric/validation/geodesic_error.py
+++
@@ -1,8 +0,0 @@
-def geodesic_error(position, pred, target):
-    direction = position[pred] - position[target]
-    return (direction * direction).sum(1).float().sqrt()
-
-
-def max_geodesic_error_accuracy(position, pred, target, error=None):
-    err = geodesic_error(position, pred, target)
-    return (err <= error).sum()
