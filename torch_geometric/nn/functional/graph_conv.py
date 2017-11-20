cee211ad16eb0a0147d195bcfac740118806216c
rusty1s <matthias.fey@tu-dortmund.de>
2017-11-20 15:01:28 +0100

    fixed graph conv on gpu

--- torch_geometric/nn/functional/graph_conv.py
+++ torch_geometric/nn/functional/graph_conv.py
@@ -7,7 +7,9 @@ def graph_conv(adj, input, weight, bias=None):
     n, m = adj.size()
 
     i = eye(n, m)
+    i = i.cuda() if adj.is_cuda else i
     adj = adj + i
+
     deg = sum(adj, dim=1)
     deg = deg.pow(-0.5)
 
--- torch_geometric/sparse/__init__.py
+++ torch_geometric/sparse/__init__.py
@@ -1,14 +1,8 @@
-import torch.sparse as sparse
-
+from .sparse import SparseTensor
 from .mm import mm
 from .mm_diagonal import mm_diagonal
 from .sum import sum
 from .eye import eye
 from .stack import stack
-
-
-def SparseTensor(indices, values, size):
-    return getattr(sparse, values.__class__.__name__)(indices, values, size)
-
 
 __all__ = ['SparseTensor', 'mm', 'mm_diagonal', 'sum', 'eye', 'stack']
