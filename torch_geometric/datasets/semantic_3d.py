2d27b90c4d87826beae6f630e300647c94f6445f
rusty1s <matthias.fey@tu-dortmund.de>
2017-11-25 20:51:05 +0100

    cleaner

--- torch_geometric/datasets/semantic_3d.py
+++ torch_geometric/datasets/semantic_3d.py
@@ -11,22 +11,39 @@ class Semantic3D(Dataset):
 
     url = 'http://www.semantic3d.net/data/point-clouds/training1/' \
           '{}_intensity_rgb.7z'
-    files = [
-        'bildstein_station1_xyz_intensity_rgb',
-        'bildstein_station3_xyz_intensity_rgb',
-        'bildstein_station5_xyz_intensity_rgb',
-        'domfountain_station1_xyz_intensity_rgb',
-        'domfountain_station2_xyz_intensity_rgb',
-        'domfountain_station3_xyz_intensity_rgb',
-        'neugasse_station1_xyz_intensity_rgb',
-        'sg27_station1_intensity_rgb',
-        'sg27_station2_intensity_rgb',
-        'sg27_station4_intensity_rgb',
-        'sg27_station5_intensity_rgb',
-        'sg27_station9_intensity_rgb',
-        'sg28_station4_intensity_rgb',
-        'untermaederbrunnen_station1_xyz_intensity_rgb',
-        'untermaederbrunnen_station3_xyz_intensity_rgb',
+    c_files = [
+        'bildstein_station1_xyz_intensity_rgb.7z',
+        'bildstein_station3_xyz_intensity_rgb.7z',
+        'bildstein_station5_xyz_intensity_rgb.7z',
+        'domfountain_station1_xyz_intensity_rgb.7z',
+        'domfountain_station2_xyz_intensity_rgb.7z',
+        'domfountain_station3_xyz_intensity_rgb.7z',
+        'neugasse_station1_xyz_intensity_rgb.7z',
+        'sg27_station1_intensity_rgb.7z',
+        'sg27_station2_intensity_rgb.7z',
+        'sg27_station4_intensity_rgb.7z',
+        'sg27_station5_intensity_rgb.7z',
+        'sg27_station9_intensity_rgb.7z',
+        'sg28_station4_intensity_rgb.7z',
+        'untermaederbrunnen_station1_xyz_intensity_rgb.7z',
+        'untermaederbrunnen_station3_xyz_intensity_rgb.7z',
+    ]
+    e_files = [
+        'bildstein_station1_xyz_intensity_rgb.txt',
+        'bildstein_station3_xyz_intensity_rgb.txt',
+        'bildstein_station5_xyz_intensity_rgb.txt',
+        'domfountain_station1_xyz_intensity_rgb.txt',
+        'domfountain_station2_xyz_intensity_rgb.txt',
+        'domfountain_station3_xyz_intensity_rgb.txt',
+        'station1_xyz_intensity_rgb.txt',
+        'sg27_station1_intensity_rgb.txt',
+        'sg27_station2_intensity_rgb.txt',
+        'sg27_station4_intensity_rgb.txt',
+        'sg27_station5_intensity_rgb.txt',
+        'sg27_station9_intensity_rgb.txt',
+        'sg28_station4_intensity_rgb.txt',
+        'untermaederbrunnen_station1_xyz_intensity_rgb.txt',
+        'untermaederbrunnen_station3_xyz_intensity_rgb.txt',
     ]
 
     def __init__(self, root, train=True, transform=None):
@@ -35,19 +52,27 @@ class Semantic3D(Dataset):
         # Set dataset properites.
         self.root = os.path.expanduser(root)
         self.raw_folder = os.path.join(self.root, 'raw')
+        self.processed_folder = os.path.join(self.root, 'processed')
         self.train = train
         self.transform = transform
 
         self.download()
 
-    def download(self):
-        for f in self.files:
-            compressed_file = os.path.join(self.raw_folder, '{}.7z'.format(f))
-            extracted_file = os.path.join(self.raw_folder, '{}.txt'.format(f))
+    @property
+    def _processed_exists(self):
+        return os.path.exists(self.processed_folder)
 
-            if not os.path.exists(extracted_file):
-                if not os.path.exists(compressed_file):
-                    url = '{}{}.7z'.format(self.url, f)
+    def download(self):
+        if self._processed_exists:
+            return
+
+        for i in range(len(self.compressed_files)):
+            c_file = os.path.join(self.raw_folder, self.c_files[i])
+            e_file = os.path.join(self.raw_folder, self.e_files[i])
+
+            if not os.path.exists(e_file):
+                if not os.path.exists(c_file):
+                    url = '{}{}'.format(self.url, c_file)
                     download_url(url, self.raw_folder)
-                extract_7z(compressed_file, self.raw_folder)
-                os.unlink(compressed_file)
+                extract_7z(c_file, self.raw_folder)
+                os.unlink(c_file)
