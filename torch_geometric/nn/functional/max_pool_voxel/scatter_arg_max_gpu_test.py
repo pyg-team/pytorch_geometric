7b5eaaee4cfd9449299d489c1830c6e9e586bae7
rusty1s <matthias.fey@tu-dortmund.de>
2017-11-25 15:37:08 +0100

    added arg max

--- torch_geometric/nn/functional/max_pool_voxel/scatter_arg_max_gpu_test.py
+++ torch_geometric/nn/functional/max_pool_voxel/scatter_arg_max_gpu_test.py
@@ -11,8 +11,10 @@ class ScatterArgMaxGPUTest(unittest.TestCase):
     def test_scatter_max(self):
         input = [[-1, 3], [-3, 5], [4, -5], [2, 1], [-1, -2]]
         input = torch.cuda.FloatTensor(input)
-        cluster = torch.cuda.IntTensor([0, 1, 1, 0, 2])
+        cluster = torch.cuda.LongTensor([0, 1, 1, 0, 2])
         max = torch.cuda.FloatTensor([[2, 3], [4, 5], [-1, -2]])
 
-        argmax = scatter_arg_max_gpu(input, cluster, max)
-        print(argmax)
+        arg_max = scatter_arg_max_gpu(input, cluster, max)
+        expected_arg_max = [[3, 0], [2, 1], [4, 4]]
+
+        assert_equal(arg_max.cpu().numpy(), expected_arg_max)
