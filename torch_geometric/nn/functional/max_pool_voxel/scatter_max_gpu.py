d5573e6202c44932ef91982218c52c8f92a10c7f
rusty1s <matthias.fey@tu-dortmund.de>
2017-11-25 15:16:46 +0100

    rename

--- torch_geometric/nn/functional/max_pool_voxel/scatter_max_gpu.py
+++ torch_geometric/nn/functional/max_pool_voxel/scatter_max_gpu.py
@@ -6,8 +6,8 @@ from .int_as_float_gpu import int_as_float_gpu
 
 kernel = kernel_loop + """
 extern "C"
-__global__ void scatter_arg_max(
-const float* input, const int* cluster, int* max, long* max_index) {
+__global__ void scatter_max(
+const float* input, const int* cluster, int* max) {
 
   CUDA_KERNEL_LOOP(idx, ${num_threads}) {
 
@@ -26,22 +26,17 @@ const float* input, const int* cluster, int* max, long* max_index) {
 """
 
 
-def scatter_arg_max_gpu(input, cluster, K):
+def scatter_max_gpu(input, cluster, K):
     with torch.cuda.device_of(input):
         M = input.size(1)
-        max = input.new(K, M).int().fill_(-2147483648)
-        max_index = cluster.new(K, M).fill_(-1)
+        max = torch.cuda.IntTensor(K, M).fill_(-2147483648)
         num_threads = input.numel()
-        f = load_kernel(
-            'scatter_arg_max', kernel, num_threads=num_threads, M=M)
-        f(args=[
-            input.data_ptr(),
-            cluster.data_ptr(),
-            max.data_ptr(),
-            max_index.data_ptr()
-        ],
+        f = load_kernel('scatter_max', kernel, num_threads=num_threads, M=M)
+        f(args=[input.data_ptr(),
+                cluster.data_ptr(),
+                max.data_ptr()],
           block=(cuda_num_threads, 1, 1),
           grid=(get_blocks(num_threads), 1, 1),
           stream=Stream(ptr=torch.cuda.current_stream().cuda_stream))
 
-    return int_as_float_gpu(max), max_index
+    return int_as_float_gpu(max)
