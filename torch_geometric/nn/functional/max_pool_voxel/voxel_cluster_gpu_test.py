9cf254eeacc5dacfdcca2b1b6d90300fc8ae964d
rusty1s <matthias.fey@tu-dortmund.de>
2017-11-25 14:31:37 +0100

    added test

--- torch_geometric/nn/functional/max_pool_voxel/voxel_cluster_gpu_test.py
+++ torch_geometric/nn/functional/max_pool_voxel/voxel_cluster_gpu_test.py
@@ -1,5 +1,6 @@
 import unittest
 import torch
+from numpy.testing import assert_equal
 
 if torch.cuda.is_available():
     from .voxel_cluster_gpu import voxel_cluster_gpu
@@ -8,9 +9,12 @@ if torch.cuda.is_available():
 class VoxelClusterGPUTest(unittest.TestCase):
     @unittest.skipIf(not torch.cuda.is_available(), 'no GPU')
     def test_voxel_cluster(self):
-        position = [[-2, -2], [2, 2], [-1, -1], [1, 1]]
+        position = [[-2, -2], [2, 2], [-1, -1], [1, 1], [-2, 2], [2, -2]]
         position = torch.cuda.FloatTensor(position)
         cluster_size = torch.cuda.LongTensor([2, 2])
         K = cluster_size.prod()
         cluster = voxel_cluster_gpu(position, cluster_size, K)
-        print(cluster)
+
+        expected_cluster = [0, 3, 0, 3, 1, 2]
+
+        assert_equal(cluster.cpu().numpy(), expected_cluster)
