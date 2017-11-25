7902b49900cd314ad9de68fb7d6e34bddacf2edc
rusty1s <matthias.fey@tu-dortmund.de>
2017-11-25 15:30:30 +0100

    impl arg max

--- torch_geometric/nn/functional/max_pool_voxel/scatter_arg_max_gpu.py
+++ torch_geometric/nn/functional/max_pool_voxel/scatter_arg_max_gpu.py
@@ -1,43 +1,49 @@
 import torch
 
-from ....utils.cuda import (cuda_num_threads, Stream, load_kernel, kernel_loop,
-                            get_blocks)
-from .int_as_float_gpu import int_as_float_gpu
+from ....utils.cuda import (cuda_num_threads, Stream, Dtype, load_kernel,
+                            kernel_loop, get_blocks)
 
 kernel = kernel_loop + """
 extern "C"
 __global__ void scatter_arg_max(
-const float* input, const int* cluster, int* max) {
+const ${Dtype}* input, const long* cluster, const ${Dtype}* max,
+long* argmax) {
 
   CUDA_KERNEL_LOOP(idx, ${num_threads}) {
 
     const int n_idx = idx / ${M};
     const int m_idx = idx % ${M};
+
     int c = cluster[n_idx] * ${M} + m_idx;
 
-    // Convert value `f` to ordered integer value to perform `atomicMax`.
-    int f = __float_as_int(input[idx]);
-    f = f >= 0 ? f : f ^ 0x7FFFFFFF;
+    ${Dtype} f = input[idx];
+    ${Dtype} m = max[c];
 
-    // Compute `scatter_max`.
-    atomicMax((int*) &max[c], f);
+    if (f == m) argmax[c] = n_idx;
   }
 }
 """
 
 
-def scatter_arg_max_gpu(input, cluster, K):
+def scatter_arg_max_gpu(input, cluster, max):
     with torch.cuda.device_of(input):
-        M = input.size(1)
-        max = torch.cuda.IntTensor(K, M).fill_(-2147483648)
+        K, M = max.size()
+        argmax = torch.cuda.LongTensor(K, M)
         num_threads = input.numel()
         f = load_kernel(
-            'scatter_arg_max', kernel, num_threads=num_threads, M=M)
-        f(args=[input.data_ptr(),
-                cluster.data_ptr(),
-                max.data_ptr()],
+            'scatter_arg_max',
+            kernel,
+            Dtype=Dtype(input),
+            num_threads=num_threads,
+            M=M)
+        f(args=[
+            input.data_ptr(),
+            cluster.data_ptr(),
+            max.data_ptr(),
+            argmax.data_ptr()
+        ],
           block=(cuda_num_threads, 1, 1),
           grid=(get_blocks(num_threads), 1, 1),
           stream=Stream(ptr=torch.cuda.current_stream().cuda_stream))
 
-    return int_as_float_gpu(max)
+    return argmax
