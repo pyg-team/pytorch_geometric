29b2ae9006eb7476a7daf2e0c86770215ed54506
rusty1s <matthias.fey@tu-dortmund.de>
2017-11-25 21:08:36 +0100

    bugfix

--- torch_geometric/datasets/semantic_3d.py
+++ torch_geometric/datasets/semantic_3d.py
@@ -9,8 +9,9 @@ from .utils.extract import extract_7z
 class Semantic3D(Dataset):
     """`Semantic3D <http://www.semantic3d.net/>`_ Dataset."""
 
-    url = 'http://www.semantic3d.net/data/point-clouds/training1/' \
-          '{}_intensity_rgb.7z'
+    url = 'http://www.semantic3d.net/data/point-clouds/training1/'
+    target_url = 'http://www.semantic3d.net/data/'
+
     c_files = [
         'bildstein_station1_xyz_intensity_rgb.7z',
         'bildstein_station3_xyz_intensity_rgb.7z',
@@ -45,6 +46,24 @@ class Semantic3D(Dataset):
         'untermaederbrunnen_station1_xyz_intensity_rgb.txt',
         'untermaederbrunnen_station3_xyz_intensity_rgb.txt',
     ]
+    c_target_file = 'sem8_labels_training.7z'
+    e_target_files = [
+        'bildstein_station1_xyz_intensity_rgb.labels',
+        'bildstein_station3_xyz_intensity_rgb.labels',
+        'bildstein_station5_xyz_intensity_rgb.labels',
+        'domfountain_station1_xyz_intensity_rgb.labels',
+        'domfountain_station2_xyz_intensity_rgb.labels',
+        'domfountain_station3_xyz_intensity_rgb.labels',
+        'neugasse_station1_xyz_intensity_rgb.labels',
+        'sg27_station1_intensity_rgb.labels',
+        'sg27_station2_intensity_rgb.labels',
+        'sg27_station4_intensity_rgb.labels',
+        'sg27_station5_intensity_rgb.labels',
+        'sg27_station9_intensity_rgb.labels',
+        'sg28_station4_intensity_rgb.labels',
+        'untermaederbrunnen_station1_xyz_intensity_rgb.labels',
+        'untermaederbrunnen_station3_xyz_intensity_rgb.labels',
+    ]
 
     def __init__(self, root, train=True, transform=None):
         super(Semantic3D, self).__init__()
@@ -66,13 +85,24 @@ class Semantic3D(Dataset):
         if self._processed_exists:
             return
 
-        for i in range(len(self.compressed_files)):
-            c_file = os.path.join(self.raw_folder, self.c_files[i])
-            e_file = os.path.join(self.raw_folder, self.e_files[i])
+        for i in range(len(self.c_files)):
+            c = os.path.join(self.raw_folder, self.c_files[i])
+            e = os.path.join(self.raw_folder, self.e_files[i])
 
-            if not os.path.exists(e_file):
-                if not os.path.exists(c_file):
-                    url = '{}{}'.format(self.url, c_file)
+            if not os.path.exists(e):
+                if not os.path.exists(c):
+                    url = '{}{}'.format(self.url, self.c_files[i])
                     download_url(url, self.raw_folder)
-                extract_7z(c_file, self.raw_folder)
-                os.unlink(c_file)
+                extract_7z(c, self.raw_folder)
+                os.unlink(c)
+
+        c = os.path.join(self.raw_folder, self.c_target_file)
+        es = [os.path.join(self.raw_folder, f) for f in self.e_target_files]
+        exists = all(os.path.exists(e) for e in es)
+
+        if not exists:
+            if not os.path.exists(c):
+                url = '{}{}'.format(self.target_url, self.c_target_file)
+                download_url(url, self.raw_folder)
+            extract_7z(c, self.raw_folder)
+            os.unlink(c)
