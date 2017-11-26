91c38bcce756db58b0418bcd0aa4505ad9f7dd4b
rusty1s <matthias.fey@tu-dortmund.de>
2017-11-26 20:41:45 +0100

    bugfix

--- torch_geometric/datasets/utils/extract.py
+++ torch_geometric/datasets/utils/extract.py
@@ -2,26 +2,32 @@ import tarfile
 import zipfile
 import subprocess
 
-from .progress import Progress
+from .spinner import Spinner
 
 
 def extract_tar(file_path, dir, mode='r:gz'):
+    spinner = Spinner('Extracting', file_path).start()
+
     with tarfile.open(file_path, mode) as tar:
         tar.extractall(dir)
+        spinner.success()
 
 
 def extract_zip(file_path, dir):
+    spinner = Spinner('Extracting', file_path).start()
+
     with zipfile.ZipFile(file_path, 'r') as zip:
         zip.extractall(dir)
+        spinner.success()
 
 
 def extract_7z(file_path, dir):
-    progress = Progress('Extracting', file_path)
+    spinner = Spinner('Extracting', file_path).start()
 
     try:
         cmd = ['7z', '-y', 'e', file_path, '-o{}'.format(dir)]
         process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
         process.wait()
-        progress.success()
+        spinner.success()
     except:
-        progress.fail()
+        spinner.fail()
