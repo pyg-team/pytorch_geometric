d4ba0b92af253944595a52779161118292a18528
rusty1s <matthias.fey@tu-dortmund.de>
2017-11-27 12:12:19 +0100

    added adj computation

--- torch_geometric/nn/functional/max_pool_voxel/max_pool_voxel.py
+++ torch_geometric/nn/functional/max_pool_voxel/max_pool_voxel.py
@@ -5,6 +5,8 @@ from .voxel_cluster_gpu import voxel_cluster_gpu
 from .scatter_max_gpu import scatter_max_gpu
 from .scatter_arg_max_gpu import scatter_arg_max_gpu
 
+from ....sparse import SparseTensor
+
 
 class MaxPoolVoxel(Function):
     def __init__(self, adj, position, cluster_size, K):
@@ -12,17 +14,38 @@ class MaxPoolVoxel(Function):
         self.adj = adj
         self.position = position
         self.cluster_size = cluster_size
+        self.n, self.dim = position.size()
         self.K = K
 
     def forward(self, input):
         cluster = voxel_cluster_gpu(self.position, self.cluster_size, self.K)
         max = scatter_max_gpu(input, cluster.int(), self.K)
         argmax = scatter_arg_max_gpu(input, cluster, max)
-        self.n, self.argmax = input.size(0), argmax
+        self.argmax = argmax
 
-        return max
+        node_count = self.position.new(self.K).fill_(0)
+        node_count.scatter_add_(0, cluster, self.position.new(self.n).fill_(1))
 
-    def backward(self, grad_output):
+        position = self.position.new(self.K * self.dim).fill_(0)
+        # cluster = cluster.view(-1, 1).repeat(1, 2).view(-1)
+        # print(cluster)
+        # position.scatter_add_(0, cluster, self.position.view(-1))
+        # print(node_count)
+        # print(position.view(-1, self.dim))
+        # position, adj
+
+        row, col = self.adj._indices()
+        row, col = cluster[row], cluster[col]
+        weight = self.adj._values()
+        mask = row != col
+        row, col, weight = row[mask], col[mask], weight[mask]
+        index = torch.stack([row, col], dim=0)
+        size = torch.Size([self.K, self.K])
+        adj = SparseTensor(index, weight, size)
+
+        return max, adj, position
+
+    def backward(self, grad_output, adj=None, position=None):
         k, m = grad_output.size()
         grad_input = grad_output.new(self.n * grad_output.size(1)).fill_(0)
 
