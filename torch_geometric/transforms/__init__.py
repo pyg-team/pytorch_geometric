65f55d2cae3c1b183123f91fb4b17ff7a06834e0
rusty1s <matthias.fey@tu-dortmund.de>
2017-12-07 11:36:32 +0100

    added flatten position transform

--- torch_geometric/transforms/__init__.py
+++ torch_geometric/transforms/__init__.py
@@ -2,6 +2,7 @@ from .cartesian import CartesianAdj
 from .polar import PolarAdj
 from .spherical import SphericalAdj
 from .target_indegree import TargetIndegreeAdj
+from .flat_position import FlatPosition
 from .graclus import Graclus
 
 __all__ = [
