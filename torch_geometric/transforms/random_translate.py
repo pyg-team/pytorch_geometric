3a79a7c687e1f76c351c6a5a0c916a8a9c89dd6a
rusty1s <matthias.fey@tu-dortmund.de>
2017-12-07 19:16:19 +0100

    no random uniform

--- torch_geometric/transforms/random_translate.py
+++ torch_geometric/transforms/random_translate.py
@@ -1,9 +1,12 @@
+import torch
+
+
 class RandomTranslate(object):
-    def __init__(self, std):
-        self.std = abs(std)
+    def __init__(self, max):
+        self.max = abs(max)
 
     def __call__(self, data):
         position = data.position
-        translate = position.new(position.size()).uniform_(-self.std, self.std)
-        data.position = position + translate
+        translate = torch.rand(position.size()) * 2 * self.max - self.max
+        position += translate.type_as(position)
         return data
