9de254c43561f1820351692d13020d4dc1d30bc1
rusty1s <matthias.fey@tu-dortmund.de>
2017-12-07 19:16:19 +0100

    no random uniform

--- torch_geometric/transforms/random_scale.py
+++ torch_geometric/transforms/random_scale.py
@@ -2,24 +2,22 @@ import torch
 
 
 class RandomScale(object):
-    def __init__(self, scale):
-        self.scale = abs(scale)
+    def __init__(self, max):
+        self.max = abs(max)
 
     def __call__(self, data):
         position = data.position
         mean = position.mean(dim=0)
         position -= mean
-        print(position)
 
-        scale = position.new(position.size(1)).uniform_(
-            -self.scale, self.scale)
+        scale = torch.rand(position.size(1)) * 2 * self.max
         # scale = (scale + 1).diag()
-        print(scale)
+        # print(scale)
 
         position *= scale
-        print(position)
+        # print(position)
         position += mean
-        print(position)
+        # print(position)
 
         # translate = position.new(position.size()).uniform_(-self.std, self.std)
         # data.position = position + translate
