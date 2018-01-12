4084402314b0cfa74488527f728ef3b5becb0978
rusty1s <matthias.fey@tu-dortmund.de>
2018-01-12 13:25:20 +0100

    pooling done with wrong scatters

--- torch_geometric/nn/functional/pool/coalesce_test.py
+++ torch_geometric/nn/functional/pool/coalesce_test.py
@@ -3,10 +3,21 @@ import torch
 
 from numpy.testing import assert_equal
 
-from .coalesce import coalesce
+from .coalesce import remove_self_loops, coalesce
 
 
 class CoalesceTest(TestCase):
+    def test_remove_self_loops(self):
+        index = torch.LongTensor([
+            [2, 2, 0, 2, 1, 0, 2, 1, 1],
+            [0, 1, 0, 2, 0, 2, 0, 0, 1],
+        ])
+        expected_index = [[2, 2, 1, 0, 2, 1], [0, 1, 0, 2, 0, 0]]
+
+        index = remove_self_loops(index)
+        assert_equal(index.tolist(), expected_index)
+
+
     def test_coalesce(self):
         index = torch.LongTensor([[2, 2, 1, 0, 2, 1], [0, 1, 0, 2, 0, 0]])
         expected_index = [[0, 1, 2, 2], [2, 0, 0, 1]]
