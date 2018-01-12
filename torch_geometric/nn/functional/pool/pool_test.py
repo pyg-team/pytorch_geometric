a786566ced33d1ed76f945e828377c661fa607f3
rusty1s <matthias.fey@tu-dortmund.de>
2018-01-12 13:56:29 +0100

    cleaner format

--- torch_geometric/nn/functional/pool/pool_test.py
+++ torch_geometric/nn/functional/pool/pool_test.py
@@ -1,9 +1,10 @@
 from unittest import TestCase
 import torch
+from torch.autograd import Variable, gradcheck
 
 from numpy.testing import assert_equal
 
-from .pool import max_pool, average_pool
+from .pool import max_pool, avg_pool, _max_pool, _avg_pool
 
 
 class PoolTest(TestCase):
@@ -19,5 +20,14 @@ class PoolTest(TestCase):
         expected_position = [[-2, 0], [2, 0]]
 
         input, index, position = max_pool(input, index, position, cluster)
+
+        input, index, position = max_pool(input, index, position, cluster)
+
+    def test_backward(self):
+        input = torch.DoubleTensor([[5, 2], [4, 7], [8, 3], [1, 3]])
+        input = Variable(input, requires_grad=True)
+        index = torch.LongTensor([[0, 0, 1, 1, 2, 2], [1, 2, 0, 2, 0, 1]])
+        position = torch.FloatTensor([[-2, 2], [2, 2], [2, -2], [-2, -2]])
+        cluster = torch.LongTensor([0, 1, 1, 0])
 
         input, index, position = max_pool(input, index, position, cluster)
