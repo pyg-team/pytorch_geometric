53314074bf6a968d1e9631205a54a9c3266efee7
rusty1s <matthias.fey@tu-dortmund.de>
2018-01-18 08:23:18 +0100

    added roc example

--- examples/cuneiform_roc.py
+++ examples/cuneiform_roc.py
@@ -2,7 +2,6 @@ from __future__ import division, print_function
 
 import os
 import sys
-import time
 
 import torch
 from torch import nn
@@ -19,6 +18,7 @@ from torch_geometric.transforms import (RandomRotate, RandomScale,
 from torch_geometric.utils import DataLoader  # noqa
 from torch_geometric.nn.modules import SplineConv  # noqa
 from torch_geometric.nn.functional import batch_average  # noqa
+from torch_geometric.datasets.utils.tu_format import read_file
 
 mode=1
 path = os.path.dirname(os.path.realpath(__file__))
@@ -41,12 +41,13 @@ input = torch.cat([input, input.new(input.size(0), 1).fill_(1)], dim=1)
 train_dataset.input = input
 test_dataset.input = input
 
-# Modify splits.
-n = 267
-step = (n + 10) // 10
-perm = torch.randperm(n)
-perm = torch.load('/Users/rusty1s/Desktop/perm.pt')
-split = torch.arange(0, n + step, step, out=torch.LongTensor())
+set = read_file(train_dataset.raw_folder, train_dataset.prefix, 'graph_sets')
+set = set.byte()
+train_dataset.split = train_dataset.split[set == 0]
+test_dataset.split = test_dataset.split[set]
+
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
 
 class Net(nn.Module):
@@ -54,8 +55,8 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.conv1 = SplineConv(9, 32, dim=2, kernel_size=5)
         self.conv2 = SplineConv(32, 64, dim=2, kernel_size=5)
-        self.conv3 = SplineConv(64, 64, dim=2, kernel_size=5)
-        self.fc1 = nn.Linear(64, 30)
+        self.conv3 = SplineConv(64, 128, dim=2, kernel_size=5)
+        self.fc1 = nn.Linear(128, 30)
 
     def forward(self, x, adj, slice):
         x = F.elu(self.conv1(adj, x))
@@ -64,7 +65,7 @@ class Net(nn.Module):
         x = batch_average(x, slice)
         x = F.dropout(x, training=self.training)
         x = self.fc1(x)
-        return F.log_softmax(x, dim=1)
+        return F.softmax(x, dim=1)
 
 
 model = Net()
@@ -73,17 +74,11 @@ if torch.cuda.is_available():
 
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
-times = []
-
 
 def train(epoch):
     model.train()
 
-    if epoch == 1:
-        for param_group in optimizer.param_groups:
-            param_group['lr'] = 0.01
-
-    if epoch == 201:
+    if epoch == 501:
         for param_group in optimizer.param_groups:
             param_group['lr'] = 0.001
 
@@ -97,74 +92,53 @@ def train(epoch):
 
         input, target = Variable(input), Variable(target)
 
-        t = time.process_time()
         optimizer.zero_grad()
         output = model(input, adj, slice)
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
-        times.append(time.process_time() - t)
 
 
-def test(epoch, loader, string):
+def test(clx):
     model.eval()
 
-    correct = 0
-    num_examples = 0
+    conf = torch.FloatTensor([])
+    label = torch.LongTensor([])
 
-    for data in loader:
+    for data in test_loader:
         adj, slice = data['adj']['content'], data['adj']['slice'][:, 0]
         input, target = data['input'], data['target']
-        num_examples += target.size(0)
 
         if torch.cuda.is_available():
             adj, slice = adj.cuda(), slice.cuda()
-            input, target = input.cuda(), target.cuda()
+            input = input.cuda()
 
         output = model(Variable(input), adj, slice)
-        pred = output.data.max(1)[1]
-        correct += pred.eq(target).cpu().sum()
+        output = output.data.cpu()
+        output = output[:, clx]
+        conf = torch.cat([conf, output], dim=0)
+        label = torch.cat([label, target], dim=0)
+    return conf, label
 
-    acc = correct / num_examples
-    print('Epoch', epoch, string, acc)
-    return acc
+
+model.load_state_dict(torch.load('/Users/rusty1s/Desktop/roc.pt'))
 
 
-# 10-fold cross-validation.
-accs = []
-for i in range(split.size(0) - 1):
-    print('Split', i)
+str = ''
+for clx in range(0, 30):
+    str += 'Class {}:\n'.format(clx)
+    conf, label = test(clx)
+    _, arg = conf.sort(descending=True)
+    sorted_label = label[arg]
+    true_count = (label == clx).sum()
+    false_count = label.size(0) - true_count
 
-    test_split = perm[split[i]:split[i + 1]]
+    for i in range(1, label.size(0)-10):
+        c1 = (sorted_label[:i] == clx).sum()
+        c2 = (sorted_label[:i] != clx).sum()
+        str += '{:0.2f}, {:0.6f}\n'.format(c1 / true_count, c2 / false_count)
+        if c1 / true_count == 1:
+            break
 
-    if i == 0:
-        train_split = perm[split[i + 1]:]
-    elif i == split.size(0) - 2:
-        train_split = perm[:split[i]]
-    else:
-        train_split = torch.cat([perm[:split[i]], perm[split[i + 1]:]])
-
-    train_dataset.split = train_split
-    test_dataset.split = test_split
-
-    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=True)
-
-    accs_single = []
-    for _ in range(10):
-        model.conv1.reset_parameters()
-        model.conv2.reset_parameters()
-        model.fc1.reset_parameters()
-        times = []
-        for epoch in range(1, 301):
-            train(epoch)
-        times = torch.FloatTensor(times)
-        acc = test(epoch, test_loader, ' Test Accuracy')
-        accs_single.append(acc)
-    accs.append(accs_single)
-
-acc = torch.FloatTensor(accs)
-print('Mean over splits:', acc.mean(dim=1).tolist())
-print('Std over splits:', acc.std(dim=1).tolist())
-print('Mean:', acc.mean())
-print('Std:', acc.std())
+with open('/Users/rusty1s/Desktop/roc.txt', 'w') as f:
+    f.write(str)
