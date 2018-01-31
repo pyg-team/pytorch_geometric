81fd88dcbd7c7965103bf2cb7c4dc6ebe1a21f0c
rusty1s <matthias.fey@tu-dortmund.de>
2018-01-31 10:10:30 +0100

    added qm9 boilerplate

--- torch_geometric/datasets/__init__.py
+++ torch_geometric/datasets/__init__.py
@@ -7,8 +7,9 @@ from .faust import FAUST
 from .mnist_superpixels import MNISTSuperpixels
 from .semantic_3d import Semantic3D
 from .cuneiform import Cuneiform
+from .qm9 import QM9
 
 __all__ = [
     'Data', 'Cora', 'PubMed', 'CiteSeer', 'FAUST', 'MNISTSuperpixels',
-    'Semantic3D', 'Cuneiform'
+    'Semantic3D', 'Cuneiform', 'QM9'
 ]
