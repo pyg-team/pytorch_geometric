fef0551dd7a229867f0bf1649573d0fd66c83af9
rusty1s <matthias.fey@tu-dortmund.de>
2018-02-02 21:21:40 +0100

    added processing

--- torch_geometric/datasets/qm9.py
+++ torch_geometric/datasets/qm9.py
@@ -1,12 +1,13 @@
 import os
 import os.path as osp
-import re
 
 import torch
 from torch.utils.data import Dataset
 
 from .utils.download import download_url
 from .utils.extract import extract_tar
+from .utils.progress import Progress
+from .utils.sdf import read_sdf
 
 
 class QM9(Dataset):
@@ -76,7 +77,30 @@ class QM9(Dataset):
             mask = torch.ByteTensor(133885).fill_(1)
             mask[tmp] = 0
 
-        # with open(osp.join(self.raw_folder, 'input.sdf'), 'r') as f:
-        #     for idx, sdf in enumerate(f.read().split('$$$$\n')[:-1]):
-        #         if mask[idx] == 0:
-        #             continue
+        input = torch.FloatTensor(0, 5)
+        index = torch.LongTensor(0, 2)
+        weight = torch.FloatTensor(0)
+        position = torch.FloatTensor(0, 3)
+        target = torch.FloatTensor(0, 12)
+        slice = index_slice = [0]
+
+        progress = Progress('Processing', end=mask.size(0), type='')
+
+        with open(osp.join(self.raw_folder, 'input.sdf'), 'r') as f:
+            for idx, sdf in enumerate(f.read().split('$$$$\n')[:-1]):
+                if mask[idx] == 0:
+                    continue
+                f, i, w, p, t = read_sdf(sdf)
+                input = torch.cat([input, f], dim=0)
+                index = torch.cat([index, i], dim=0)
+                weight = torch.cat([weight, w], dim=0)
+                position = torch.cat([position, p], dim=0)
+                target = torch.cat([target, t], dim=0)
+                slice.append(slice[-1] + input.size(0))
+                index_slice.append(index_slice[-1] + weight.size(0))
+                progress.update(idx + 1)
+        index = index.t()
+        slice = torch.LongTensor(slice)
+        index_slice = torch.LongTensor(index_slice)
+
+        progress.success()
---
+++ torch_geometric/datasets/utils/sdf.py
@@ -0,0 +1,23 @@
+import torch
+
+
+def read_sdf(content):
+    lines = content.split('\n')
+    target = torch.FloatTensor([float(x) for x in lines[0].split()[5:]])
+    N, E = [int(x) for x in lines[3].split()[:2]]
+
+    atoms = lines[4:4 + N]
+    position = torch.Tensor([[float(y) for y in x.split()[:3]] for x in atoms])
+    types = {'H': 0, 'C': 1, 'N': 2, 'O': 3, 'F': 4}
+    types = torch.LongTensor([[types[x.split()[3]]] for x in atoms])
+    value = torch.FloatTensor(N, 1).fill_(1)
+    input = torch.FloatTensor(N, 5).fill_(0).scatter_(1, types, value)
+
+    bonds = [[int(y) for y in x.split()[:3]] for x in lines[4 + N:4 + N + E]]
+    bonds = torch.LongTensor(bonds)
+    index = bonds[:, :2] - 1
+    index_rev = torch.cat([index[:, 1:], index[:, :1]], dim=1)
+    index = torch.cat([index, index_rev])
+    weight = bonds[:, 2].repeat(2).float()
+
+    return input, index, weight, position, target
