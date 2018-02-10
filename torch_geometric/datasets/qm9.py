f3c6507f311505eeabffc680c1696d6ff574c12b
rusty1s <matthias.fey@tu-dortmund.de>
2018-02-10 11:37:09 +0100

    docfix

--- torch_geometric/datasets/qm9.py
+++ torch_geometric/datasets/qm9.py
@@ -15,7 +15,7 @@ class QM9(Dataset):
     """`QM9 <http://quantum-machine.org/datasets>`_ Molecule Dataset from the
     `"Quantum Chemistry Structures and Properties of 134 Kilo Molecules"
     <https://www.nature.com/articles/sdata201422>`_ paper. QM9 consists of
-    130k molecules with 13 properties for each molecule.
+    approximately 130k molecules with 12 properties for each molecule.
 
     Args:
         root (string): Root directory of dataset. Downloads data automatically
