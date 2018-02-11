608d7021e8a39a52a9691eb4b023eeb7fdd210a6
rusty1s <matthias.fey@tu-dortmund.de>
2018-02-11 12:38:07 +0100

    better tu format reader

--- torch_geometric/datasets/utils/tu_format.py
+++ torch_geometric/datasets/utils/tu_format.py
@@ -1,33 +1,114 @@
 import os
+import os.path as osp
+
+from ..dataset import Set
 
 import torch
 import numpy as np
 
 
+def to_one_hot(tensor):
+    max = tensor.max(dim=0)[0] + 1
+    D = max.sum()
+    out = torch.FloatTensor(tensor.size(0), D).fill_(0)
+    offset = tensor.new(max.size()).fill_(0)
+    for i in range(1, offset.size(0)):
+        offset[i] = offset[i - 1] + max[i - 1]
+
+    tensor += offset
+    offset = torch.arange(0, tensor.size(0) * D, D, out=torch.LongTensor())
+    tensor += offset.view(-1, 1)
+
+    out.view(-1)[tensor.view(-1)] = 1
+    return out.view(-1, D)
+
+
 def tu_files(prefix,
-             A=False,
              graph_indicator=False,
-             graph_labels=False,
              graph_attributes=False,
-             node_labels=False,
+             graph_labels=False,
              node_attributes=False,
-             edge_labels=False,
+             node_labels=False,
              edge_attributes=False,
+             edge_labels=False,
              graph_sets=False):
 
     func = lambda x: ['{}_{}.txt'.format(prefix, x)]  # noqa
 
     files = []
-    files += func('A') if A else []
+    files += func('A')
     files += func('graph_indicator') if graph_indicator else []
-    files += func('graph_labels') if graph_labels else []
     files += func('graph_attributes') if graph_attributes else []
-    files += func('node_labels') if node_labels else []
+    files += func('graph_labels') if graph_labels else []
     files += func('node_attributes') if node_attributes else []
-    files += func('edge_labels') if edge_labels else []
+    files += func('node_labels') if node_labels else []
     files += func('edge_attributes') if edge_attributes else []
+    files += func('edge_labels') if edge_labels else []
     files += func('graph_sets') if graph_sets else []
     return files
+
+
+def read_tu_sets(dir,
+                 prefix,
+                 graph_indicator=False,
+                 graph_attributes=False,
+                 graph_labels=False,
+                 node_attributes=False,
+                 node_labels=False,
+                 edge_attributes=False,
+                 edge_labels=False):
+
+    index = read_tu_index(dir, prefix)
+    input = read_tu_x(dir, prefix, 'node', node_attributes, node_labels)
+    weight = read_tu_x(dir, prefix, 'edge', edge_attributes, edge_labels)
+
+    if graph_labels is True and graph_attributes is False:
+        target = read_tu_file(
+            dir, prefix, 'graph_labels', out=torch.LongTensor())
+    else:
+        target = read_tu_x(dir, prefix, 'graph', graph_attributes,
+                           graph_labels)
+
+    # print(target)
+
+    index, index_slice = None, None
+    return Set(input, None, index, weight, target, index, index_slice)
+
+
+def read_tu_file(dir, prefix, name, out=None):
+    with open(osp.join(dir, '{}_{}.txt'.format(prefix, name)), 'r') as f:
+        lines = f.read().split('\n')[:-1]
+        tensor = [[float(x) for x in line.split(', ')] for line in lines]
+    tensor = torch.FloatTensor(tensor).squeeze()
+    return tensor if out is None else tensor.type_as(out)
+
+
+def read_tu_index(dir, prefix):
+    index = read_tu_file(dir, prefix, 'A', out=torch.LongTensor()).t()
+    index -= index.min()
+    n = index.max()
+    weight = torch.ByteTensor(index.size(1))
+    adj = torch.sparse.ByteTensor(index, weight, torch.Size([n, n])).coalesce()
+    return adj._indices()
+
+
+def read_tu_x(dir, prefix, name, attributes=False, labels=False):
+    if attributes is False and labels is False:
+        return None
+
+    x = []
+    if attributes is True:
+        f = '{}_attributes'.format(name)
+        x += [read_tu_file(dir, prefix, f)]
+
+    if labels is True:
+        f = '{}_labels'.format(name)
+        x += [to_one_hot(read_tu_file(dir, prefix, f, out=torch.LongTensor()))]
+
+    return x[0] if len(x) == 1 else torch.cat(x, dim=1)
+
+
+# OLD VERSION -----------------------------------------------------------------
 
 
 def read_file(dir, prefix, name):
