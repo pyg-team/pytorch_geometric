365072b5874ce3b5d692a8468afa023d4d4e4c63
rusty1s <matthias.fey@tu-dortmund.de>
2018-02-11 15:16:19 +0100

    added mnist superpixel dataset with new format

--- torch_geometric/datasets/__init__.py
+++ torch_geometric/datasets/__init__.py
@@ -5,6 +5,7 @@ from .pubmed import PubMed
 from .citeseer import CiteSeer
 from .faust import FAUST
 from .mnist_superpixels import MNISTSuperpixels
+from .mnist_superpixels2 import MNISTSuperpixels2
 from .shapenet import ShapeNet
 from .cuneiform import Cuneiform
 from .cuneiform2 import Cuneiform2
@@ -12,5 +13,5 @@ from .qm9 import QM9
 
 __all__ = [
     'Data', 'Cora', 'PubMed', 'CiteSeer', 'FAUST', 'MNISTSuperpixels',
-    'ShapeNet', 'Cuneiform', 'Cuneiform2', 'QM9'
+    'MNISTSuperpixels2', 'ShapeNet', 'Cuneiform', 'Cuneiform2', 'QM9'
 ]
