880b85fbcf9b6a7c95b0d0e06e8e7c67f55d3b20
Jan Eric Lenssen <janeric.lenssen@tu-dortmund.de>
2018-02-13 20:42:56 +0100

    splineconv forward now approx. 10 times faster

--- torch_geometric/nn/functional/spline_conv/spline.py
+++ torch_geometric/nn/functional/spline_conv/spline.py
@@ -1,20 +1,13 @@
 import torch
 
 if torch.cuda.is_available():
-    from .spline_linear_gpu import spline_linear_gpu
+    from .compute_spline_basis import compute_spline_basis
     from .spline_quadratic_gpu import spline_quadratic_gpu
     from .spline_cubic_gpu import spline_cubic_gpu
 
 
-def spline(input, kernel_size, is_open_spline, K, degree):
+def spline(input, kernel_size, is_open_spline, K, degree, basis_kernel):
     if input.is_cuda:
-        if degree == 1:
-            return spline_linear_gpu(input, kernel_size, is_open_spline, K)
-        if degree == 2:
-            return spline_quadratic_gpu(input, kernel_size, is_open_spline, K)
-        if degree == 3:
-            return spline_cubic_gpu(input, kernel_size, is_open_spline, K)
-        else:
-            raise NotImplementedError()
+        return compute_spline_basis(input, kernel_size, is_open_spline, K, basis_kernel)
     else:
         raise NotImplementedError()
