be298a87dac31511c6eace2d48c5740940acc67e
Jan Eric Lenssen <janeric.lenssen@tu-dortmund.de>
2018-02-13 20:46:16 +0100

    minor fix

--- torch_geometric/nn/functional/spline_conv/spline.py
+++ torch_geometric/nn/functional/spline_conv/spline.py
@@ -2,8 +2,6 @@ import torch
 
 if torch.cuda.is_available():
     from .compute_spline_basis import compute_spline_basis
-    from .spline_quadratic_gpu import spline_quadratic_gpu
-    from .spline_cubic_gpu import spline_cubic_gpu
 
 
 def spline(input, kernel_size, is_open_spline, K, degree, basis_kernel):
