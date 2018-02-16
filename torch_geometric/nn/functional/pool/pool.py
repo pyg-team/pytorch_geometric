f15f885e5d2e30e538311a19c042c1d1b2055a28
Jan Eric Lenssen <janeric.lenssen@tu-dortmund.de>
2018-02-16 17:00:28 +0100

    dirty-fixed maxpooling

--- torch_geometric/nn/functional/pool/pool.py
+++ torch_geometric/nn/functional/pool/pool.py
@@ -20,9 +20,9 @@ def _pool(index, position, cluster):
 def _max_pool(input, cluster):
     cluster = cluster.unsqueeze(1).expand(-1, input.size(1))
     if torch.is_tensor(input):
-        return scatter_max(cluster, input, dim=0)[0]
+        return scatter_max(cluster, input, dim=0, fill_value=-100000)[0]
     else:
-        return scatter_max(Variable(cluster), input, dim=0)[0]
+        return scatter_max(Variable(cluster), input, dim=0, fill_value=-100000)[0]
 
 
 def max_pool(input, index, position, cluster):
