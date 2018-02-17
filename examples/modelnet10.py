e043d2429c6baf58c1ca8e0e82b376a2c5c75a8e
rusty1s <matthias.fey@tu-dortmund.de>
2018-02-17 07:54:29 +0100

    added progress bar

--- examples/modelnet10.py
+++ examples/modelnet10.py
@@ -14,9 +14,13 @@ path = os.path.join(path, '..', 'data', 'ModelNet10')
 transform = CartesianAdj()
 train_dataset = ModelNet10(path, True, transform=transform)
 test_dataset = ModelNet10(path, False, transform=transform)
-train_loader = DataLoader2(train_dataset, batch_size=16, shuffle=True)
-test_loader = DataLoader2(test_dataset, batch_size=16)
+train_loader = DataLoader2(train_dataset, batch_size=8, shuffle=True)
+test_loader = DataLoader2(test_dataset, batch_size=8)
 
 for data in train_loader:
+    data = data.cuda().to_variable()
+    print(data.num_nodes, data.num_edges, data.adj.size())
+
+for data in test_loader:
     data = data.cuda().to_variable()
     print(data.num_nodes, data.num_edges, data.adj.size())
