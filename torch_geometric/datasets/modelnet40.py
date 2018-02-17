5f155ea511b32c4fbad0d2c2baaf6719dcb1c0b5
rusty1s <matthias.fey@tu-dortmund.de>
2018-02-17 08:23:38 +0100

    off bugfix, modelnet40 impl

--- torch_geometric/datasets/modelnet40.py
+++ torch_geometric/datasets/modelnet40.py
@@ -1,20 +1,37 @@
 import os
+import os.path as osp
 import glob
+
+import torch
 
 from .dataset import Dataset
 from .utils.download import download_url
 from .utils.extract import extract_zip
+from .utils.progress import Progress
+from .utils.off import read_off
 
 
 class ModelNet40(Dataset):
     url = 'http://modelnet.cs.princeton.edu/ModelNet40.zip'
+    categories = [
+        'airplane', 'bathtub', 'bed', 'bench', 'bookshelf', 'bottle', 'bowl',
+        'car', 'chair', 'cone', 'cup', 'curtain', 'desk', 'door', 'dresser',
+        'flower_pot', 'glass_box', 'guitar', 'keyboard', 'lamp', 'laptop',
+        'mantel', 'monitor', 'night_stand', 'person', 'piano', 'plant',
+        'radio', 'range_hood', 'sink', 'sofa', 'stairs', 'stool', 'table',
+        'tent', 'toilet', 'tv_stand', 'vase', 'wardrobe', 'xbox'
+    ]
+    num_examples = 30991 + 9008  # train + test
 
     def __init__(self, root, train=True, transform=None):
         super(ModelNet40, self).__init__(root, transform)
 
+        name = self._processed_files[0] if train else self._processed_files[1]
+        self.set = torch.load(name)
+
     @property
     def raw_files(self):
-        return ['awd']
+        return ['ModelNet40/{}'.format(c) for c in self.categories]
 
     @property
     def processed_files(self):
@@ -25,16 +42,20 @@ class ModelNet40(Dataset):
         extract_zip(file_path, self.raw_folder)
         os.unlink(file_path)
 
+    def process(self):
+        train, test = [], []
+        p = Progress('Processing', end=self.num_examples, type='')
+        for target, category in enumerate(self.categories):
+            dir = osp.join(self.raw_folder, 'ModelNet40', category)
+            train_files = glob.glob('{}/train/*.off'.format(dir))
+            test_files = glob.glob('{}/test/*.off'.format(dir))
+            train += [self.process_example(f, target, p) for f in train_files]
+            test += [self.process_example(f, target, p) for f in test_files]
+        p.success()
+        return train, test
 
-#     def process(self):
-#         train, test = [], []
-#         for c, target in enumerate(self.categories):
-#             dir = osp.join(self.raw_folder, 'ModelNet10')
-#             train_files = glob
-#             test_files
-#             pass
-
-#         raise NotImplementedError
-
-#     def process_example(self, filename, target):
-#         pass
+    def process_example(self, filename, target, progress):
+        data = read_off(filename)
+        data.target = target
+        progress.inc()
+        return data
--- torch_geometric/datasets/utils/off.py
+++ torch_geometric/datasets/utils/off.py
@@ -8,12 +8,18 @@ def read_off(filename):
     with open(filename, 'r') as f:
         data = f.read().split()
 
-    num_nodes = int(data[1])
+    if data[0] == 'OFF':
+        data.pop(0)
+    else:
+        # Some files contain a bug and don't have a carriage return after OFF.
+        data[0] = data[0][3:]
 
-    pos = [float(x) for x in data[4:num_nodes * 3 + 4]]
+    num_nodes = int(data[0])
+
+    pos = [float(x) for x in data[3:num_nodes * 3 + 3]]
     pos = torch.FloatTensor(pos).view(-1, 3)
 
-    face = [int(x) for x in data[num_nodes * 3 + 4:]]
+    face = [int(x) for x in data[num_nodes * 3 + 3:]]
     face = torch.LongTensor(face).view(-1, 4)[:, 1:].contiguous()
 
     index = edges_from_faces(face)
