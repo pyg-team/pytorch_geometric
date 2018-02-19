0fe6124488136dab0e3e31f6de067d4d2658b896
rusty1s <matthias.fey@tu-dortmund.de>
2018-02-19 08:32:54 +0100

    basic fixes

--- examples/mnist_superpixels2.py
+++ examples/mnist_superpixels2.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import time
 
 import torch
 from torch.autograd import Variable
@@ -15,7 +14,7 @@ from torch_geometric.datasets import MNISTSuperpixels2  # noqa
 from torch_geometric.utils import DataLoader2  # noqa
 from torch_geometric.transform import CartesianAdj  # noqa
 from torch_geometric.nn.modules import SplineConv  # noqa
-from torch_geometric.nn.functional import voxel_avg_pool  # noqa
+from torch_geometric.nn.functional import voxel_max_pool  # noqa
 
 path = os.path.dirname(os.path.realpath(__file__))
 path = os.path.join(path, '..', 'data', 'MNISTSuperpixels2')
@@ -23,23 +22,27 @@ path = os.path.join(path, '..', 'data', 'MNISTSuperpixels2')
 transform = CartesianAdj()
 train_dataset = MNISTSuperpixels2(path, True, transform=transform)
 test_dataset = MNISTSuperpixels2(path, False, transform=transform)
-train_loader = DataLoader2(train_dataset, batch_size=64, shuffle=True)
-test_loader = DataLoader2(test_dataset, batch_size=64)
+ones = torch.ones(train_dataset.set.num_nodes)
+train_dataset.set.input = torch.stack([train_dataset.set.input, ones], dim=1)
+ones = torch.ones(test_dataset.set.num_nodes)
+test_dataset.set.input = torch.stack([test_dataset.set.input, ones], dim=1)
+train_loader = DataLoader2(train_dataset, batch_size=128, shuffle=True)
+test_loader = DataLoader2(test_dataset, batch_size=128)
 
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = SplineConv(1, 32, dim=2, kernel_size=5)
+        self.conv1 = SplineConv(2, 32, dim=2, kernel_size=5)
         self.conv2 = SplineConv(32, 64, dim=2, kernel_size=5)
-        self.conv3 = SplineConv(64, 64, dim=2, kernel_size=5)
-        self.fc1 = nn.Linear(64, 10)
+        self.conv3 = SplineConv(64, 128, dim=2, kernel_size=5)
+        self.fc1 = nn.Linear(128, 10)
 
     def forward(self, data):
         data.input = F.elu(self.conv1(data.adj, data.input))
-        data, _ = voxel_avg_pool(data, 5, transform)
+        data, _ = voxel_max_pool(data, 5, transform)
         data.input = F.elu(self.conv2(data.adj, data.input))
-        data, _ = voxel_avg_pool(data, 7, transform)
+        data, _ = voxel_max_pool(data, 7, transform)
         data.input = F.elu(self.conv3(data.adj, data.input))
 
         data.batch = Variable(data.batch.view(-1, 1).expand(data.input.size()))
@@ -60,15 +63,14 @@ optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 def train(epoch):
     model.train()
 
-    if epoch == 16:
+    if epoch == 21:
         for param_group in optimizer.param_groups:
             param_group['lr'] = 0.001
 
-    if epoch == 26:
+    if epoch == 31:
         for param_group in optimizer.param_groups:
             param_group['lr'] = 0.0001
 
-    t_train = time.process_time()
     for data in train_loader:
         data = data.cuda().to_variable()
         optimizer.zero_grad()
@@ -76,7 +78,6 @@ def train(epoch):
         loss = F.nll_loss(output, data.target)
         loss.backward()
         optimizer.step()
-    # print('Training runtime:', time.process_time() - t_train)
 
 
 def test(epoch):
@@ -92,6 +93,6 @@ def test(epoch):
     print('Epoch:', epoch, 'Test Accuracy:', correct / len(test_dataset))
 
 
-for epoch in range(1, 31):
+for epoch in range(1, 41):
     train(epoch)
     test(epoch)
