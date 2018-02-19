512fe0f9ea4f0808e87f26bd320c5182ecce7a7c
rusty1s <matthias.fey@tu-dortmund.de>
2018-02-19 13:35:59 +0100

    added normalize scale

---
+++ torch_geometric/transform/normalize_scale.py
@@ -0,0 +1,10 @@
+from __future__ import division
+
+
+class NormalizeScale(object):
+    def __call__(self, data):
+        pos = data.pos
+        pos -= pos.min(dim=0)
+        pos /= pos.max()
+        data.pos = pos
+        return data
