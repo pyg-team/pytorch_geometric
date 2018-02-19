f97ec7fc07ca375b63b12017b86eae79f735112a
rusty1s <matthias.fey@tu-dortmund.de>
2018-02-19 13:35:59 +0100

    added normalize scale

--- torch_geometric/transform/__init__.py
+++ torch_geometric/transform/__init__.py
@@ -3,8 +3,9 @@ from .random_translate import RandomTranslate
 from .random_scale import RandomScale
 from .random_rotate import RandomRotate
 from .random_shear import RandomShear
+from .normalize_scale import NormalizeScale
 
 __all__ = [
     'CartesianAdj', 'RandomTranslate', 'RandomScale', 'RandomRotate',
-    'RandomShear'
+    'RandomShear', 'NormalizeScale'
 ]
