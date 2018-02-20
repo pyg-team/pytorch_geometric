4f0639dbe9657326736199bc3ddbb50595307fe4
rusty1s <matthias.fey@tu-dortmund.de>
2018-02-20 10:24:53 +0100

    rename

--- torch_geometric/nn/functional/pool/voxel_pool.py
+++ torch_geometric/nn/functional/pool/voxel_pool.py
@@ -5,11 +5,11 @@ from ....datasets.dataset import Data
 from ...modules.spline_conv import repeat_to
 
 
-def voxel_max_pool(data, size, offset=None, fake_nodes=False, transform=None):
+def voxel_max_pool(data, size, origin=None, fake_nodes=False, transform=None):
     size = repeat_to(size, data.pos.size(1))
     size = data.pos.new(size)
 
-    c, b = grid_cluster(data.pos, size, data.batch, offset, fake_nodes)
+    c, b = grid_cluster(data.pos, size, data.batch, origin, fake_nodes)
     b = None if fake_nodes else b
     input, index, pos = max_pool(data.input, data.index, data.pos, c)
     data = Data(input, pos, index, None, data.target, b)
