ae73849123f5a0b6e51e5900f0c0a0c52315b040
Jan Eric Lenssen <janeric.lenssen@tu-dortmund.de>
2018-02-21 15:17:00 +0100

    move to mid transform

--- torch_geometric/transform/normalize_scale.py
+++ torch_geometric/transform/normalize_scale.py
@@ -4,7 +4,8 @@ from __future__ import division
 class NormalizeScale(object):
     def __call__(self, data):
         pos = data.pos
-        pos = pos - pos.min(dim=0)[0]
+        pos = pos - pos.min(dim=0,keepdim=True)[0]
         pos /= pos.max()
+        pos += 0.5*(1-pos.max(0,keepdim=True)[0])
         data.pos = pos*0.9999
         return data
