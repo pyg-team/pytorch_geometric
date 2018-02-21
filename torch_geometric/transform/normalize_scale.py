c254da22f450192e8cc67ecfa81c65cea4a449dd
Jan Eric Lenssen <janeric.lenssen@tu-dortmund.de>
2018-02-21 13:09:16 +0100

    updated transforms

--- torch_geometric/transform/normalize_scale.py
+++ torch_geometric/transform/normalize_scale.py
@@ -4,7 +4,7 @@ from __future__ import division
 class NormalizeScale(object):
     def __call__(self, data):
         pos = data.pos
-        pos = pos - pos.min(dim=0)
+        pos = pos - pos.min(dim=0)[0]
         pos /= pos.max()
-        data.pos = pos
+        data.pos = pos*0.9999
         return data
