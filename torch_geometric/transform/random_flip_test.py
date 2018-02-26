f1acff7f1d42c71f6aa03e3078ddf4a3709c70b1
rusty1s <matthias.fey@tu-dortmund.de>
2018-02-26 20:28:16 +0100

    bugfix

--- torch_geometric/transform/random_flip_test.py
+++ torch_geometric/transform/random_flip_test.py
@@ -2,7 +2,7 @@ import unittest
 import torch
 
 from ..datasets.dataset import Data
-from .random_flip import flip
+from .random_flip import RandomFlip
 
 
 class CartesianLocalTest(unittest.TestCase):
@@ -10,12 +10,12 @@ class CartesianLocalTest(unittest.TestCase):
         pos = torch.FloatTensor([[-1, 3], [0, 0], [2, -2]])
         data = Data(None, pos, None, None, None)
 
-        data = flip(data, axis=0)
+        data = RandomFlip(axis=0, p=1)(data)
         self.assertEqual(data.pos.tolist(), [[2, 3], [1, 0], [-1, -2]])
 
     def test_flip_y(self):
         pos = torch.FloatTensor([[-1, 3], [0, 0], [2, -2]])
         data = Data(None, pos, None, None, None)
 
-        data = flip(data, axis=1)
+        data = RandomFlip(axis=1, p=1)(data)
         self.assertEqual(data.pos.tolist(), [[-1, -2], [0, 1], [2, 3]])
