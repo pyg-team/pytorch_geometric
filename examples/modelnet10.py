77db14e81a90424ccf266dc12228368554c77787
rusty1s <matthias.fey@tu-dortmund.de>
2018-02-27 15:26:45 +0100

    new architecture

--- examples/modelnet10.py
+++ examples/modelnet10.py
@@ -37,42 +37,48 @@ train_loader = DataLoader2(train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader2(test_dataset, batch_size=batch_size)
 
 
-# To beat: 95.40 %
 class Net(nn.Module):
-    def pool_args(self, min, max):
+    def pool_args(self, mean, x):
         if not self.training:
-            return 1 / (0.5 * min + max), 0
-        return 1 / random.uniform(min, max), random.uniform(-1 / min, 0)
+            return 1 / mean, 0
+        size = 1 / random.uniform(mean - x, mean + x)
+        start = random.uniform(-1 / (mean - x), 0)
+        return size, start
 
     def __init__(self):
         super(Net, self).__init__()
         self.conv1 = SplineConv(1, 32, dim=3, kernel_size=5)
         self.conv2 = SplineConv(32, 64, dim=3, kernel_size=5)
-        self.conv3 = SplineConv(64, 128, dim=3, kernel_size=5)
-        self.conv4 = SplineConv(128, 256, dim=3, kernel_size=5)
-        self.fc1 = nn.Linear(8 * 256, 512)
+        self.conv3 = SplineConv(64, 64, dim=3, kernel_size=5)
+        self.conv4 = SplineConv(64, 64, dim=3, kernel_size=5)
+        self.conv5 = SplineConv(64, 128, dim=3, kernel_size=5)
+        self.fc1 = nn.Linear(8 * 128, 512)
         self.fc2 = nn.Linear(512, 10)
 
     def forward(self, data):
-        size, start = self.pool_args(32, 64)
+        size, start = self.pool_args(64, 16)
         data = max_pool(data, size, start, transform)
 
         data.input = F.elu(self.conv1(data.adj, data.input))
-        size, start = self.pool_args(16, 32)
+        size, start = self.pool_args(32, 8)
         data = max_pool(data, size, start, transform)
 
         data.input = F.elu(self.conv2(data.adj, data.input))
-        size, start = self.pool_args(8, 16)
+        size, start = self.pool_args(16, 4)
         data = max_pool(data, size, start, transform)
 
         data.input = F.elu(self.conv3(data.adj, data.input))
-        size, start = self.pool_args(4, 8)
+        size, start = self.pool_args(8, 2)
         data = max_pool(data, size, start, transform)
 
         data.input = F.elu(self.conv4(data.adj, data.input))
+        size, start = self.pool_args(4, 1)
+        data = max_pool(data, size, start, transform)
+
+        data.input = F.elu(self.conv5(data.adj, data.input))
         data = dense_voxel_max_pool(data, 0.5, 0, 1, transform)
 
-        x = data.input.view(-1, 8 * 256)
+        x = data.input.view(-1, 8 * 128)
         x = F.elu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
@@ -97,12 +103,14 @@ def train(epoch):
         for param_group in optimizer.param_groups:
             param_group['lr'] = 0.00001
 
+    if epoch == 31:
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = 0.000001
+
     for data in train_loader:
         data = data.cuda().to_variable()
         optimizer.zero_grad()
-        output = model(data)
-        loss = F.nll_loss(output, data.target)
-        loss.backward()
+        F.nll_loss(model(data), data.target).backward()
         optimizer.step()
 
 
@@ -112,14 +120,13 @@ def test(epoch, loader, dataset, str):
 
     for data in loader:
         data = data.cuda().to_variable(['input'])
-        output = model(data)
-        pred = output.data.max(1)[1]
+        pred = model(data).data.max(1)[1]
         correct += pred.eq(data.target).cpu().sum()
 
     print('Epoch:', epoch, str, 'Accuracy:', correct / len(dataset))
 
 
-for epoch in range(1, 51):
+for epoch in range(1, 41):
     train(epoch)
     test(epoch, train_loader, train_dataset, 'Train')
     test(epoch, test_loader, test_dataset, 'Test')
