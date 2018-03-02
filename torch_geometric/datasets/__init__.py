beb6f95a0df3568ecb9f99109aef6dab6a57632a
Jan Eric Lenssen <janeric.lenssen@tu-dortmund.de>
2018-03-02 17:12:51 +0100

    random pointcloud processors/loaders

--- torch_geometric/datasets/__init__.py
+++ torch_geometric/datasets/__init__.py
@@ -15,9 +15,12 @@ from .kitti import KITTI
 from .modelnet10 import ModelNet10
 from .modelnet40 import ModelNet40
 from .modelnet10_pc import ModelNet10PC
+from .modelnet10_rand_pc import ModelNet10RandPC
+from .modelnet40_rand_pc import ModelNet40RandPC
 
 __all__ = [
     'Data', 'Cora', 'PubMed', 'CiteSeer', 'FAUST', 'MNISTSuperpixels',
     'MNISTSuperpixels2', 'ShapeNet', 'ShapeNet2', 'Cuneiform', 'Cuneiform2',
-    'QM9', 'KITTI', 'ModelNet10', 'ModelNet40', 'ModelNet10PC'
+    'QM9', 'KITTI', 'ModelNet10', 'ModelNet40', 'ModelNet10PC',
+    'ModelNet10RandPC', 'ModelNet40RandPC'
 ]
