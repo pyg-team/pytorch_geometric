several bugs in pos gradient fixed

examples/mnist_superpixels2.py
@@ -29,8 +29,10 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.conv1 = SplineConv(1, 32, dim=2, kernel_size=5)
-        self.conv2 = SplineConv(32, 64, dim=2, kernel_size=5)
-        self.conv3 = SplineConv(64, 128, dim=2, kernel_size=5)
+        self.conv2 = SplineConv(32, 64, dim=2, kernel_size=5,
+                                backprop_to_adj=True)
+        self.conv3 = SplineConv(64, 128, dim=2, kernel_size=5,
+                                backprop_to_adj=True)
         self.fc1 = nn.Linear(4 * 128, 128)
         self.fc2 = nn.Linear(128, 10)
 
@@ -41,7 +43,7 @@ class Net(nn.Module):
         data, _ = sparse_voxel_max_pool(data, 7, 0, transform)
         data.input = F.elu(self.conv3(data.adj, data.input))
 
-        data, _ = dense_voxel_max_pool(data, 14, 0, 28, transform)
+        data, _ = dense_voxel_max_pool(data, 25, -10, 40)
 
         x = data.input.view(-1, self.fc1.weight.size(1))
         x = F.elu(self.fc1(x))
@@ -69,10 +71,12 @@ def train(epoch):
             param_group['lr'] = 0.0001
 
     for data in train_loader:
-        data = data.cuda().to_variable()
+        data = data.cuda().to_variable(['input', 'target', 'weight', 'pos'])
         optimizer.zero_grad()
-        F.nll_loss(model(data), data.target).backward()
+        loss = F.nll_loss(model(data), data.target)
+        loss.backward()
         optimizer.step()
+        print(loss.data[0])
 
 
 def test(epoch):
@@ -80,7 +84,7 @@ def test(epoch):
     correct = 0
 
     for data in test_loader:
-        data = data.cuda().to_variable('input')
+        data = data.cuda().to_variable(['input', 'weight', 'pos'])
         pred = model(data).data.max(1)[1]
         correct += pred.eq(data.target).sum()
 

torch_geometric/nn/functional/pool/voxel_pool.py
@@ -1,17 +1,18 @@
 from torch_cluster import sparse_grid_cluster, dense_grid_cluster
-
+import torch
 from .pool import max_pool
 from ....datasets.dataset import Data
 from ...modules.spline_conv import repeat_to
 
 
 def sparse_voxel_max_pool(data, size, start=None, transform=None):
-    size = data.pos.new(repeat_to(size, data.pos.size(1)))
+    pos_tensor = data.pos if torch.is_tensor(data.pos) else data.pos.data
+    size = pos_tensor.new(repeat_to(size, data.pos.size(1)))
 
     if start is not None:
-        start = data.pos.new(repeat_to(start, data.pos.size(1)))
+        start = pos_tensor.new(repeat_to(start, data.pos.size(1)))
 
-    output = sparse_grid_cluster(data.pos, size, data.batch, start)
+    output = sparse_grid_cluster(pos_tensor, size, data.batch, start)
     cluster = output[0] if isinstance(output, tuple) else output
     batch = output[1] if isinstance(output, tuple) else None
 
@@ -26,15 +27,17 @@ def sparse_voxel_max_pool(data, size, start=None, transform=None):
 
 
 def dense_voxel_max_pool(data, size, start=None, end=None, transform=None):
-    size = data.pos.new(repeat_to(size, data.pos.size(1)))
+    pos_tensor = data.pos if torch.is_tensor(data.pos) else data.pos.data
+    size = pos_tensor.new(repeat_to(size, data.pos.size(1)))
 
     if start is not None:
-        start = data.pos.new(repeat_to(start, data.pos.size(1)))
+        start = pos_tensor.new(repeat_to(start, data.pos.size(1)))
 
     if end is not None:
-        end = data.pos.new(repeat_to(end, data.pos.size(1)))
+        end = pos_tensor.new(repeat_to(end, data.pos.size(1)))
 
-    cluster, C = dense_grid_cluster(data.pos, size, data.batch, start, end)
+    cluster, C = dense_grid_cluster(pos_tensor, size, data.batch, start, end)
+    #print(cluster.min(), cluster.max(), C)
     input, index, pos = max_pool(data.input, data.index, data.pos, cluster, C)
 
     data = Data(input, pos, index, None, data.target)

torch_geometric/nn/functional/spline_conv/spline_conv_gpu.py
@@ -1,5 +1,5 @@
 import torch
-from torch.autograd import Function
+from torch.autograd import Function, Variable
 
 from ....utils.cuda import (cuda_num_threads, Stream, load_kernel, kernel_loop,
                             get_blocks)
@@ -120,6 +120,7 @@ const ${Dtype}* amount, const long* index, int num_threads) {
       k = e_idx * ${k_max} + k_idx;
       b = amount[k];
       c = index[k];
+      ${Dtype} adj_g = 0.0;
 
       for (int m_in_idx = 0; m_in_idx < ${M_in}; m_in_idx++) {
         w_idx = c * ${M_out} * ${M_in} +
@@ -212,8 +213,7 @@ const long* kernel_size, const long* is_open_spline, int num_threads) {
       k_idx >>= 1;
 
       value = input[e_idx * ${dim} + d_idx];
-      if(value==1.0)
-        value -= 0.000001;
+
       value *= kernel_size[d_idx] - is_open_spline[d_idx];
         
       frac = value - floor(value);
@@ -340,16 +340,12 @@ int num_threads) {
     const int e_idx = idx / ${dim};
     int d_idx = idx % ${dim};
 
-    int K = ${K};
     int k_idx_mod;
-    int bot;
-    int top;
     ${Dtype} value;
     ${Dtype} frac;
     ${Dtype} grad_out = 0.0;
-    long i = 0;
     
-    int quotient = (int)pow(2.0,(float)d_idx);
+    int quotient = (int)pow(2.0,(double)d_idx);
     
     for (int k_idx = 0; k_idx < ${k_max}; k_idx++) {
       
@@ -359,8 +355,8 @@ int num_threads) {
       value *= kernel_size[d_idx] - is_open_spline[d_idx];
 
       frac = value - floor(value);
-      residual = (1 - k_idx_mod) * (frac - 1) + k_idx_mod * frac;
-      int a_idx = e_idx*${k_max} + k_idx
+      ${Dtype} residual = (1 - k_idx_mod) * (frac - 1) + k_idx_mod * frac;
+      int a_idx = e_idx*${k_max} + k_idx;
       grad_out += grad_amount[a_idx]*amount[a_idx]/residual;
 
     }
@@ -471,9 +467,12 @@ class SplineConvGPU(Function):
                 stream=Stream(ptr=torch.cuda.current_stream().cuda_stream))
 
         if self.bp_to_adj:
-            self.save_for_backward(input, weight, adj_values, amount, index)
+            self.save_for_backward(input, weight, adj_values)
         else:
-            self.save_for_backward(input, weight, amount, index)
+            self.save_for_backward(input, weight)
+
+        self.amount = amount
+        self.index = index
 
         return output
 
@@ -483,7 +482,9 @@ class SplineConvGPU(Function):
         num_threads = grad_output.numel()
 
         if self.bp_to_adj:
-            input, weight, adj_values, amount, index = self.saved_tensors
+            input, weight, adj_values = self.saved_tensors
+            amount = self.amount
+            index = self.index
             grad_amount = grad_output.new(amount.size(0),
                                           amount.size(1)).fill_(0)
             with torch.cuda.device_of(grad_output):
@@ -502,9 +503,9 @@ class SplineConvGPU(Function):
                     ],
                     stream=Stream(ptr=torch.cuda.current_stream().cuda_stream))
 
-            num_threads = grad_amount.numel()
             grad_adj = grad_amount.new(grad_amount.size(0),
                                        self.kernel_size.size(0)).fill_(0)
+            num_threads = grad_adj.numel()
 
             with torch.cuda.device_of(grad_amount):
                 self.f_basis_bw(
@@ -516,14 +517,17 @@ class SplineConvGPU(Function):
                         amount.data_ptr(),
                         grad_adj.data_ptr(),
                         self.kernel_size.data_ptr(),
-                        self.is_open_spline.data_ptr(), num_threads
+                        self.is_open_spline.data_ptr(),
+                        num_threads
                     ],
                     stream=Stream(ptr=torch.cuda.current_stream().cuda_stream))
 
-            return grad_input, grad_weight, grad_adj
+            return grad_input, grad_weight, None
 
         else:
-            input, weight, amount, index = self.saved_tensors
+            input, weight = self.saved_tensors
+            amount = self.amount
+            index = self.index
             with torch.cuda.device_of(grad_output):
                 self.f_weighting_bw(
                     block=(cuda_num_threads, 1, 1),
@@ -539,4 +543,4 @@ class SplineConvGPU(Function):
                     ],
                     stream=Stream(ptr=torch.cuda.current_stream().cuda_stream))
 
-            return grad_input, grad_weight
+            return grad_input, grad_weight, None