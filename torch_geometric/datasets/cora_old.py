430beec651fe14e4f0f032cbc27e18a9ca415d90
rusty1s <matthias.fey@tu-dortmund.de>
2018-03-06 11:30:32 +0100

    new cora data format

---
+++ torch_geometric/datasets/cora_old.py
@@ -0,0 +1,16 @@
+from .planetoid import Planetoid
+
+
+class Cora(Planetoid):
+    """`Cora <https://linqs.soe.ucsc.edu/node/236>`_ Dataset.
+
+    Args:
+        root (string): Root directory of dataset. Downloads and processes data
+            automatically if dataset doesn't exist.
+        transform (callable, optional): A function/transform that takes in a
+            :class:`Data` object and returns a transformed version.
+            (default: :obj:`None`)
+    """
+
+    def __init__(self, root, transform=None):
+        super(Cora, self).__init__(root, 'cora', transform)
--- torch_geometric/datasets/utils/planetoid.py
+++ torch_geometric/datasets/utils/planetoid.py
@@ -1,52 +1,44 @@
 from itertools import repeat
-import os
 import sys
 
 import torch
-
 try:
     import cPickle as pickle
 except ImportError:
     import pickle
 
+from ..dataset import Data
 
-def read_planetoid(dir, name):
-    # Read input files.
-    objects = []
-    for e in ['tx', 'ty', 'allx', 'ally', 'graph']:
-        file_name = os.path.join(dir, 'ind.{}.{}'.format(name, e))
-        with open(file_name, 'rb') as f:
-            if sys.version_info > (3, 0):
-                objects.append(pickle.load(f, encoding='latin1'))
-            else:
-                objects.append(pickle.load(f))
 
-    tx, ty, allx, ally, graph = tuple(objects)
+def pickle_load(filename):
+    with open(filename, 'rb') as f:
+        if sys.version_info > (3, 0):
+            return pickle.load(f, encoding='latin1')
+        else:
+            return pickle.load(f)
 
-    with open(os.path.join(dir, 'ind.{}.test.index'.format(name)), 'r') as f:
+
+def read_planetoid(filenames):
+    # filename order = ['tx', 'ty', 'allx', 'ally', 'graph', 'test.index']
+    tx, ty, allx, ally, graph = tuple([pickle_load(f) for f in filenames[:-1]])
+    with open(filenames[-1], 'r') as f:
         test_index = torch.LongTensor([int(line.strip()) for line in f])
-    test_index_sorted = test_index.sort()[0]
+        test_index_sorted, _ = test_index.sort()
 
-    # Input.
-    tx = torch.FloatTensor(tx.todense())
-    allx = torch.FloatTensor(allx.todense())
+    allx, tx = torch.Tensor(allx.todense()), torch.Tensor(tx.todense())
     input = torch.cat([allx, tx], dim=0)
     input[test_index, :] = input[test_index_sorted, :]
 
-    # Graph.
-    row = []
-    col = []
+    ty, ally = torch.LongTensor(ty), torch.LongTensor(ally)
+    target = torch.cat([ally, ty], dim=0)
+    target[test_index, :] = target[test_index_sorted, :]
+    target = target.max(dim=1)[1]
+
+    row, col = [], []
     for key, value in graph.items():
         row += repeat(key, len(value))
         col += value
-
     row, col = torch.LongTensor(row), torch.LongTensor(col)
     index = torch.stack([row, col], dim=0)
 
-    # Target.
-    ty, ally = torch.IntTensor(ty), torch.IntTensor(ally)
-    target = torch.cat([ally, ty], dim=0)
-    target = target.max(dim=1)[1].byte()
-    target[test_index] = target[test_index_sorted]
-
-    return input, index, target
+    return Data(input, None, index, None, target)
