several bugs in pos gradient fixed

torch_geometric/nn/functional/spline_conv/spline_conv.py
@@ -22,11 +22,12 @@ def spline_conv(
     if input.dim() == 1:
         input = input.unsqueeze(1)
 
-    values = adj._values()
-    row, col = adj._indices()
+    values = adj['values']
+    row, col = adj['indices']
 
     # Get features for every end vertex with shape [|E| x M_in].
     output = input[col]
+
     # Convert to [|E| x M_in] feature matrix and calculate [|E| x M_out].
     if output.is_cuda:
         output = SplineConvGPU(kernel_size, is_open_spline, K, degree,
@@ -39,7 +40,7 @@ def spline_conv(
 
     # Convolution via `scatter_add`. Converts [|E| x M_out] feature matrix to
     # [n x M_out] feature matrix.
-    zero = output.data.new(adj.size(1), output.size(1)).fill_(0.0)
+    zero = output.data.new(adj['size'][1], output.size(1)).fill_(0.0)
     zero = Variable(zero) if not torch.is_tensor(output) else zero
     r = row.view(-1, 1).expand(row.size(0), output.size(1))
     output = zero.scatter_add_(0, Variable(r), output)
@@ -48,8 +49,8 @@ def spline_conv(
     output += torch.mm(input, weight[-1])
 
     # Normalize output by degree.
-    ones = values.new(values.size(0)).fill_(1)
-    zero = values.new(output.size(0)).fill_(0)
+    ones = values.data.new(values.size(0)).fill_(1)
+    zero = values.data.new(output.size(0)).fill_(0)
     degree = zero.scatter_add_(0, row, ones)
     degree = torch.clamp(degree, min=1)
     output = output / Variable(degree.view(-1, 1))