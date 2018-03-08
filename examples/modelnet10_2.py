44f031979c0b51b4e6d08cb359a9fc698d3ef450
Jan Eric Lenssen <janeric.lenssen@tu-dortmund.de>
2018-03-08 10:23:47 +0100

    fixes voxel_avg_pool

--- examples/modelnet10_2.py
+++ examples/modelnet10_2.py
@@ -6,6 +6,8 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 from torchvision.transforms import Compose
+from torch.autograd import Variable
+from torch_scatter import scatter_mean
 
 sys.path.insert(0, '.')
 sys.path.insert(0, '..')
@@ -34,11 +36,16 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.conv1 = SplineConv(1, 32, dim=3, kernel_size=5)
-        self.conv2 = SplineConv(32, 64, dim=3, kernel_size=5)
-        self.conv3 = SplineConv(64, 64, dim=3, kernel_size=5)
-        self.conv4 = SplineConv(64, 64, dim=3, kernel_size=5)
-        self.conv5 = SplineConv(64, 64, dim=3, kernel_size=5)
-        self.fc1 = nn.Linear(8 * 64, 10)
+        self.conv12 = SplineConv(32, 64, dim=3, kernel_size=5)
+        self.conv2 = SplineConv(1, 64, dim=3, kernel_size=5)
+        self.conv22 = SplineConv(64, 64, dim=3, kernel_size=5)
+        self.conv3 = SplineConv(1, 64, dim=3, kernel_size=5)
+        self.conv32 = SplineConv(64, 64, dim=3, kernel_size=5)
+        self.conv4 = SplineConv(1, 64, dim=3, kernel_size=5)
+        self.conv42 = SplineConv(64, 64, dim=3, kernel_size=5)
+        self.conv5 = SplineConv(1, 64, dim=3, kernel_size=5)
+        self.conv52 = SplineConv(64, 64, dim=3, kernel_size=5)
+        self.fc1 = nn.Linear(5 * 64, 10)
 
         self.att1 = Lin(32, 2)
         self.att2 = Lin(64, 2)
@@ -55,34 +62,57 @@ class Net(nn.Module):
         return size, start
 
     def forward(self, data):
-        data.input = F.relu(self.conv1(data.adj, data.input))
-        #att1 = self.att1(data.input)
+        f1 = F.relu(self.conv1(data.adj, data.input))
+        f1 = F.relu(self.conv12(data.adj, f1))
+        batch = Variable(data.batch.view(-1, 1).expand(data.batch.size(0), 64))
+        f1_res = scatter_mean(batch, f1)
+
         size, start = self.pool_args(32, 8)
-        data, _ = sparse_voxel_max_pool(data, size, start, transform,
-                                        weight=data.input[:,0])
+        data2, _ = sparse_voxel_max_pool(data, size, start, transform,
+                                        weight=f1[:,0])
 
-        data.input = F.relu(self.conv2(data.adj, data.input))
-        #att2 = self.att2(data.input)
+        f2 = F.relu(self.conv2(data2.adj, data2.input))
+        f2 = F.relu(self.conv22(data2.adj, f2))
+        batch = Variable(data2.batch.view(-1, 1).expand(data2.batch.size(0), 64))
+        f2_res = scatter_mean(batch, f2)
+
         size, start = self.pool_args(16, 4)
-        data, _ = sparse_voxel_max_pool(data, size, start, transform,
-                                        weight=data.input[:,0])
+        data3, _ = sparse_voxel_max_pool(data2, size, start, transform,
+                                        weight=f2[:,0])
 
-        data.input = F.relu(self.conv3(data.adj, data.input))
-        #att3 = self.att3(data.input)
+        f3 = F.relu(self.conv3(data3.adj, data3.input))
+        f3 = F.relu(self.conv32(data3.adj, f3))
+        batch = Variable(
+            data3.batch.view(-1, 1).expand(data3.batch.size(0), 64))
+        f3_res = scatter_mean(batch, f3)
+
         size, start = self.pool_args(8, 2)
-        data, _ = sparse_voxel_max_pool(data, size, start, transform,
-                                        weight=data.input[:,0])
+        data4, _ = sparse_voxel_max_pool(data3, size, start, transform,
+                                        weight=f3[:,0])
 
-        data.input = F.relu(self.conv4(data.adj, data.input))
-        #att4 = self.att4(data.input)
+        f4 = F.relu(self.conv4(data4.adj, data4.input))
+        f4 = F.relu(self.conv42(data4.adj, f4))
+        batch = Variable(
+            data4.batch.view(-1, 1).expand(data4.batch.size(0), 64))
+        f4_res = scatter_mean(batch, f4)
+
+
         size, start = self.pool_args(4, 1)
-        data, _ = sparse_voxel_max_pool(data, size, start, transform,
-                                        weight=data.input[:,0])
+        data5, _ = sparse_voxel_max_pool(data4, size, start, transform,
+                                        weight=f4[:,0])
 
-        data.input = F.relu(self.conv5(data.adj, data.input))
-        data, _ = dense_voxel_max_pool(data, 1, -0.5, 1.5)
+        f5 = F.relu(self.conv5(data5.adj, data5.input))
+        f5 = F.relu(self.conv52(data5.adj, f5))
+        batch = Variable(
+            data5.batch.view(-1, 1).expand(data5.batch.size(0), 64))
+        f5_res = scatter_mean(batch, f5)
 
-        x = data.input.view(-1, self.fc1.weight.size(1))
+        x = torch.cat([f1_res,f2_res,f3_res,f4_res,f5_res],dim=1)
+
+        #data.input = F.relu(self.conv5(data.adj, data.input))
+        #data, _ = dense_voxel_max_pool(data, 1, -0.5, 1.5)
+
+        x = x.view(-1, self.fc1.weight.size(1))
         x = F.dropout(x, training=self.training)
         x = self.fc1(x)
 
