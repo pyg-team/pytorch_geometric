1780e3eb00e001d2b0daf4a2322a4fed4d927663
Jan Eric Lenssen <janeric.lenssen@tu-dortmund.de>
2018-03-13 11:56:42 +0100

    fixed faust processor

--- torch_geometric/datasets/utils/ply.py
+++ torch_geometric/datasets/utils/ply.py
@@ -1,5 +1,6 @@
 import torch
 from plyfile import PlyData, make2d
+import numpy as np
 
 from ...graph.geometry import edges_from_faces
 from ..dataset import Data
@@ -8,14 +9,13 @@ from ..dataset import Data
 def read_ply(filename):
     with open(filename, 'rb') as f:
         plydata = PlyData.read(f)
-
     x = torch.FloatTensor(plydata['vertex']['x'])
     y = torch.FloatTensor(plydata['vertex']['y'])
     z = torch.FloatTensor(plydata['vertex']['z'])
     pos = torch.stack([x, y, z], dim=1)
     input = torch.ones(pos.size(0))
-
-    face = torch.LongTensor(make2d(plydata['face']['vertex_indices']))
+    arrays = [np.expand_dims(x, 0) for x in plydata['face']['vertex_indices']]
+    face = torch.LongTensor(np.concatenate(arrays, axis=0))
     index = edges_from_faces(face)
 
     return Data(input, pos, index, None, None)
