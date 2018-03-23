b61959c9df5e93a4d342c805ba3f733de3521def
rusty1s <matthias.fey@tu-dortmund.de>
2018-03-23 11:00:42 +0100

    removed redundant code

--- examples/faust.py
+++ examples/faust.py
@@ -2,6 +2,7 @@ from __future__ import division, print_function
 
 import os.path as osp
 import sys
+import time
 
 import torch
 from torch import nn
@@ -24,27 +25,16 @@ test_dataset = FAUST(path, train=False, transform=CartesianAdj())
 train_loader = DataLoader2(train_dataset, batch_size=1, shuffle=True)
 test_loader = DataLoader2(test_dataset, batch_size=1)
 
-#url = 'http://ls7-www.cs.uni-dortmund.de/cvpr_geometric_dl/' \
-#      'faust_geodesic_distance.tar.gz'
-#download_url(url, path)
-#extract_tar(osp.join(path, 'faust_geodesic_distance.tar.gz'), path)
-
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = SplineConv(1, 64, dim=3, kernel_size=5)
-        self.conv2 = SplineConv(64, 64, dim=3, kernel_size=5)
+        self.conv1 = SplineConv(1, 32, dim=3, kernel_size=5)
+        self.conv2 = SplineConv(32, 64, dim=3, kernel_size=5)
         self.conv3 = SplineConv(64, 64, dim=3, kernel_size=5)
         self.conv4 = SplineConv(64, 64, dim=3, kernel_size=5)
         self.conv5 = SplineConv(64, 64, dim=3, kernel_size=5)
         self.conv6 = SplineConv(64, 64, dim=3, kernel_size=5)
-        self.conv7 = SplineConv(64, 64, dim=3, kernel_size=5)
-        self.conv8 = SplineConv(64, 64, dim=3, kernel_size=5)
-        self.conv9 = SplineConv(64, 64, dim=3, kernel_size=5)
-        self.conv10 = SplineConv(64, 64, dim=3, kernel_size=5)
-        self.conv11 = SplineConv(64, 64, dim=3, kernel_size=5)
-        self.conv12 = SplineConv(64, 64, dim=3, kernel_size=5)
         self.fc1 = nn.Linear(64, 256)
         self.fc2 = nn.Linear(256, 6890)
 
@@ -56,12 +46,6 @@ class Net(nn.Module):
         x = F.elu(self.conv4(adj, x))
         x = F.elu(self.conv5(adj, x))
         x = F.elu(self.conv6(adj, x))
-        x = F.elu(self.conv7(adj, x))
-        x = F.elu(self.conv8(adj, x))
-        x = F.elu(self.conv9(adj, x))
-        x = F.elu(self.conv10(adj, x))
-        x = F.elu(self.conv11(adj, x))
-        x = F.elu(self.conv12(adj, x))
         x = F.elu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
@@ -84,40 +68,37 @@ def train(epoch):
         for param_group in optimizer.param_groups:
             param_group['lr'] = 0.001
 
+    t_f = 0
+    t_b = 0
     for data in train_loader:
         data = data.cuda().to_variable('input')
         optimizer.zero_grad()
-        F.nll_loss(model(data), target).backward()
+        torch.cuda.synchronize()
+        t = time.perf_counter()
+        output = model(data)
+        torch.cuda.synchronize()
+        t_f += time.perf_counter() - t
+        loss = F.nll_loss(output, target)
+
+        torch.cuda.synchronize()
+        t = time.perf_counter()
+        loss.backward()
+        torch.cuda.synchronize()
+        t_b += time.perf_counter() - t
         optimizer.step()
+    print(t_f / 80, t_b / 80)
 
 
 def test(epoch):
     model.eval()
-    acc_0 = acc_1 = acc_2 = acc_4 = acc_6 = acc_8 = acc_10 = 0
+    correct = 0
 
     for i, data in enumerate(test_loader):
         data = data.cuda().to_variable('input')
         pred = model(data).data.max(1)[1]
-        filename = '{}.pt'.format(80 + i)
-        distance = torch.load(osp.join(path, 'geodesic_distance', filename))
-        distance = distance.cuda()
-        geodesic_error = distance[pred, target.data]
-        acc_0 += (geodesic_error <= 0.0000002).sum()
-        acc_1 += (geodesic_error <= 0.01).sum()
-        acc_2 += (geodesic_error <= 0.02).sum()
-        acc_4 += (geodesic_error <= 0.04).sum()
-        acc_6 += (geodesic_error <= 0.06).sum()
-        acc_8 += (geodesic_error <= 0.08).sum()
-        acc_10 += (geodesic_error <= 0.1).sum()
+        correct += pred.eq(data.target).sum()
 
-    print('Epoch', epoch)
-    print('Accuracy 0:', acc_0 / (20 * 6890))
-    print('Accuracy 1:', acc_1 / (20 * 6890))
-    print('Accuracy 2:', acc_2 / (20 * 6890))
-    print('Accuracy 4:', acc_4 / (20 * 6890))
-    print('Accuracy 6:', acc_6 / (20 * 6890))
-    print('Accuracy 8:', acc_8 / (20 * 6890))
-    print('Accuracy 10:', acc_10 / (20 * 6890))
+    print('Epoch', epoch, 'Test Accuracy:', correct / (20 * 6890))
 
 
 for epoch in range(1, 101):
