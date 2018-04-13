a685a7a91d2d9c4bb1639fbbf9009c88e206e0b0
rusty1s <matthias.fey@tu-dortmund.de>
2018-04-13 09:32:18 +0200

    added setuptool

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+__version__ = '0.1.0'
+url = 'https://github.com/rusty1s/pytorch_geometric'
+
+install_requires = [
+    'torch-scatter', 'torch-unique', 'torch-cluster', 'torch-spline-conv'
+]
+setup_requires = ['pytest-runner']
+tests_require = ['pytest', 'pytest-cov']
+
+setup(
+    name='torch_geometric',
+    version=__version__,
+    description='Geometric Deep Learning extension library for PyTorch',
+    author='Matthias Fey',
+    author_email='matthias.fey@tu-dortmund.de',
+    url=url,
+    download_url='{}/archive/{}.tar.gz'.format(url, __version__),
+    keywords=[
+        'pytorch', 'geometric-deep-learning', 'graph', 'mesh',
+        'neural-networks', 'spline-cnn'
+    ],
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    tests_require=tests_require,
+    packages=find_packages())
