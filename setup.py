1b9a38685f99ff04f07faafa8c33ffa5fbd6b5db
rusty1s <matthias.fey@tu-dortmund.de>
2018-04-14 07:10:21 +0200

    fixes

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ from setuptools import setup, find_packages
 __version__ = '1.0.0'
 url = 'https://github.com/rusty1s/pytorch_geometric'
 
-print(find_packages())
-
 install_requires = [
     'cffi',
     'torch-scatter',
@@ -36,4 +34,4 @@ setup(
     install_requires=install_requires,
     setup_requires=setup_requires,
     tests_require=tests_require,
-    packages=['torch_geometric'])
+    packages=find_packages())
