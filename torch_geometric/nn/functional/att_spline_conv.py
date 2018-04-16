62e9a015a83ebd3561371f79412bf0276ecc16ae
rusty1s <matthias.fey@tu-dortmund.de>
2018-04-16 13:53:42 +0200

    added attentional spline conv

---
+++ torch_geometric/nn/functional/att_spline_conv.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torch_spline_conv.basis import spline_basis
+from torch_spline_conv.weighting import spline_weighting
+
+
+def att_spline_conv(x,
+                    edge_index,
+                    pseudo,
+                    weight,
+                    root_weight,
+                    att_weight,
+                    kernel_size,
+                    is_open_spline,
+                    degree=1,
+                    negative_slope=0.2,
+                    dropout=0.5,
+                    bias=None):
+
+    x = x.unsqueeze(-1) if x.dim() == 1 else x
+    row, col = edge_index
+    pseudo = pseudo.unsqueeze(-1) if pseudo.dim() == 1 else pseudo
+
+    # Weight root node.
+    output = torch.mm(x, root_weight)
+
+    # Weight each node.
+    basis, weight_index = spline_basis(degree, pseudo, kernel_size,
+                                       is_open_spline)
+    x_col = spline_weighting(x[col], weight, basis, weight_index)
+
+    # Compute attention across edges.
+    x_row = output[row]
+    alpha = torch.cat([x_row, x_col], dim=-1)
+    alpha = torch.matmul(alpha, att_weight.unsqueeze(-1)).squeeze()
+    alpha = F.leaky_relu(alpha, negative_slope)
+
+    # Scatter softmax.
+    alpha = alpha.exp_()
+    alpha_sum = Variable(alpha.data.new(x.size(0))).fill_(0)
+    alpha /= alpha_sum.scatter_add_(0, Variable(row), alpha)[row]
+
+    # Apply attention.
+    alpha_dropout = F.dropout(alpha, p=dropout, training=True)
+    x_col = alpha_dropout.unsqueeze(-1) * x_col
+
+    # Sum up neighborhoods.
+    output.scatter_add_(0, Variable(row).unsqueeze(-1).expand_as(x_col), x_col)
+
+    # Add bias (if wished).
+    if bias is not None:
+        output += bias
+
+    return output, alpha
--- torch_geometric/nn/modules/__init__.py
+++ torch_geometric/nn/modules/__init__.py
@@ -1,8 +1,12 @@
 from .spline_conv import SplineConv
+from .att_spline_conv import AttSplineConv
 from .graph_conv import GraphConv
 from .cheb_conv import ChebConv
 from .gat import GAT
 from .agnn import AGNN
 from .mlp_conv import MLPConv
 
-__all__ = ['SplineConv', 'GraphConv', 'ChebConv', 'GAT', 'AGNN', 'MLPConv']
+__all__ = [
+    'SplineConv', 'AttSplineConv', 'GraphConv', 'ChebConv', 'GAT', 'AGNN',
+    'MLPConv'
+]
