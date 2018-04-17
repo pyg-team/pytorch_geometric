4ccf5b5b7878fb41148f0ea6062c60c135ba92d7
rusty1s <matthias.fey@tu-dortmund.de>
2018-04-17 11:36:38 +0200

    one hot implementation

--- torch_geometric/utils/__init__.py
+++ torch_geometric/utils/__init__.py
@@ -5,8 +5,9 @@ from .degree import degree
 from .softmax import softmax
 from .coalesce import coalesce
 from .loop import add_self_loops, remove_self_loops
+from .one_hot import one_hot
 
 __all__ = [
     'Data', 'DataLoader', 'matmul', 'degree', 'softmax', 'coalesce',
-    'remove_self_loops', 'add_self_loops'
+    'remove_self_loops', 'add_self_loops', 'one_hot'
 ]
---
+++ torch_geometric/utils/one_hot.py
@@ -0,0 +1,15 @@
+import torch
+
+from .new import new
+
+
+def one_hot(src, max_value=None, out=None):
+    out = new(src).float() if out is None else out
+    src = src if torch.is_tensor(src) else src.data
+    max_value = src.max() + 1 if max_value is None else max_value
+    sizes = src.size(0), max_value
+
+    out.resize_(*sizes).fill_(0)
+    out[torch.arange(sizes[0], out=src.new()), src] = 1
+
+    return out
