4c1355c2bdff5f82e16985f31ed7bc08cbc391ba
rusty1s <matthias.fey@tu-dortmund.de>
2018-04-17 13:48:35 +0200

    added edge_attr training

--- alpha/cora_random_walk.py
+++ alpha/cora_random_walk.py
@@ -3,21 +3,22 @@ from __future__ import division, print_function
 import os.path as osp
 
 import torch
-from torch.autograd import Variable as Var
+from torch.autograd import Variable
 from torch import nn
 import torch.nn.functional as F
 from torch_geometric.datasets import Cora
 from torch_geometric.nn.modules import RandomWalk
-from torch_geometric.utils import one_hot, degree
+from torch_geometric.utils import one_hot, softmax, degree  # noqa
 
 path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data')
 dataset = Cora(osp.join(path, 'Cora'))
 data = dataset[0].cuda() if torch.cuda.is_available() else dataset[0]
-x, edge_index, target = data.input, data.index, data.target
+edge_index, target = data.index, data.target
 row, col = edge_index
-num_nodes, num_features, num_classes = x.size(0), x.size(1), target.max() + 1
+num_nodes, num_features = data.input.size()
+num_classes = target.max() + 1
 one_hot = one_hot(target, num_classes)
-edge_attr = (1 / degree(row, num_nodes))[col]
+edge_attr = Variable((1 / degree(row, num_nodes))[col])
 
 train_mask = torch.arange(0, 20 * num_classes).long()
 val_mask = torch.arange(train_mask.size(0), train_mask.size(0) + 500).long()
@@ -25,16 +26,26 @@ test_mask = torch.arange(num_nodes - 1000, num_nodes).long()
 
 one_hot[train_mask[-1]:, :] = 0
 
-edge_attr, one_hot, target = Var(edge_attr), Var(one_hot), Var(target)
+one_hot, target = Variable(one_hot), Variable(target)
 
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
+        self.fc1 = nn.Linear(num_features, 16)
+        self.fc2 = nn.Linear(32, 1)
         self.walk = RandomWalk(num_classes, 30)
 
     def forward(self):
+        # x = Variable(data.input)
+        # x = F.relu(self.fc1(x))
+        # x = torch.cat([x[row], x[col]], dim=-1)
+        # x = F.dropout(x, training=self.training)
+        # x = self.fc2(x).squeeze()
+        # edge_attr = softmax(row, x, num_nodes)
+        # x = F.dropout(x, training=self.training)
         x = self.walk(edge_index, edge_attr, one_hot)
+        # x = F.dropout(x, training=self.training)
         return F.log_softmax(x, dim=1)
 
 
