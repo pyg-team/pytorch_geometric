2043bb8b304b674dfd277d108e6a46880ddf47c3
rusty1s <matthias.fey@tu-dortmund.de>
2018-04-18 18:29:40 +0200

    added collate

---
+++ torch_geometric/utils/collate.py
@@ -0,0 +1,38 @@
+import torch
+from torch_geometric.utils import Data
+
+
+def collate_to_batch(data_list):
+    keys = data_list[0].keys
+    batch = {key: [] for key in keys}
+
+    for data, key in zip(data_list, keys):
+        batch[key].append(data[key])
+
+    batch['batch'] = []
+    for idx, data in enumerate(data_list):
+        batch['batch'].append(torch.LongTensor(data.num_nodes).fill_(idx))
+
+    for key in keys:
+        batch[key] = torch.cat(batch[key], dim=0)
+
+    if 'edge_index' in keys:
+        batch['edge_index'] = batch['edge_index'].t().contiguous()
+
+    return Data.from_dict(batch)
+
+
+def collate_to_set(data_list):
+    keys = data_list[0].keys
+    dataset = {key: [] for key in keys}
+    slices = {key: [0] for key in keys}
+
+    for data, key in zip(data_list, keys):
+        dataset[key].append(data[key])
+        slices[key].append(slices[key][-1] + data[key].size(0))
+
+    for key in keys:
+        dataset[key] = torch.cat(dataset[key], dim=0)
+        slices[key] = torch.LongTensor(slices[key])
+
+    return Data.from_dict(dataset), slices
