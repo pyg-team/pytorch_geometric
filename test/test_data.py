83025c3d4ad087c1581c8a358333c77e913ca030
rusty1s <matthias.fey@tu-dortmund.de>
2018-04-18 09:27:48 +0200

    data iterator

--- test/test_data.py
+++ test/test_data.py
@@ -4,5 +4,4 @@ from torch_geometric.utils import Data
 
 def test_data():
     data = Data(torch.Tensor([1, 2, 3]), torch.LongTensor([1, 1, 1, 1, 1]))
-    data.to_variable('x')
-    data.to_tensor()
+    data.num_nodes
