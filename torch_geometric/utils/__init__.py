201a3d441398db583f046829b56f4a961fbbd88c
rusty1s <matthias.fey@tu-dortmund.de>
2018-04-19 13:46:52 +0200

    new base dataset

--- torch_geometric/utils/__init__.py
+++ torch_geometric/utils/__init__.py
@@ -1,6 +1,7 @@
 from .new import new
 from .data import Data
 from .collate import collate_to_set, collate_to_batch
+from .dataset import Dataset
 from .dataloader import DataLoader
 from .matmul import matmul
 from .degree import degree
@@ -11,7 +12,7 @@ from .one_hot import one_hot
 from .normalized_cut import normalized_cut
 
 __all__ = [
-    'new', 'Data', 'collate_to_set', 'collate_to_batch', 'DataLoader',
-    'matmul', 'degree', 'softmax', 'coalesce', 'remove_self_loops',
-    'add_self_loops', 'one_hot', 'normalized_cut'
+    'new', 'Data', 'collate_to_set', 'collate_to_batch', 'Dataset',
+    'DataLoader', 'matmul', 'degree', 'softmax', 'coalesce',
+    'remove_self_loops', 'add_self_loops', 'one_hot', 'normalized_cut'
 ]
