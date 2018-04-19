8a5ac077f8ab40c8495ef00e592699890d0e8b29
rusty1s <matthias.fey@tu-dortmund.de>
2018-04-19 15:36:52 +0200

    add split set

--- test/data/test_collate.py
+++ test/data/test_collate.py
@@ -1,17 +1,18 @@
-from torch import Tensor
+from torch import Tensor, LongTensor
 from torch_geometric.data import Data, collate_to_set, collate_to_batch
 
 
 def test_collate_to_set():
-    data1 = Data(x=Tensor([1, 2, 3]), edge_index=Tensor([[0, 1], [1, 2]]))
-    data2 = Data(x=Tensor([4, 5]), edge_index=Tensor([[0, 1], [1, 0]]))
-    output, slices = collate_to_set([data1, data2])
+    data1 = Data(x=Tensor([1, 2, 3]), edge_index=LongTensor([[0, 1], [1, 2]]))
+    data2 = Data(x=Tensor([4, 5]), edge_index=LongTensor([[0, 1], [1, 0]]))
+    output, output_slices = collate_to_set([data1, data2])
 
     assert len(output) == 2
     assert output.x.tolist() == [1, 2, 3, 4, 5]
     assert output.edge_index.tolist() == [[0, 1], [1, 2], [0, 1], [1, 0]]
-    assert slices['x'].tolist() == [0, 3, 5]
-    assert slices['edge_index'].tolist() == [0, 2, 4]
+    assert len(output_slices.keys()) == 2
+    assert output_slices['x'].tolist() == [0, 3, 5]
+    assert output_slices['edge_index'].tolist() == [0, 2, 4]
 
 
 def test_collate_to_batch():
--- torch_geometric/data/__init__.py
+++ torch_geometric/data/__init__.py
@@ -1,11 +1,12 @@
 from .makedirs import makedirs
 from .data import Data
 from .collate import collate_to_set, collate_to_batch
+from .split import data_from_set, split_set
 from .dataset import Dataset
 from .in_memory_dataset import InMemoryDataset
 from .dataloader import DataLoader
 
 __all__ = [
-    'makedirs', 'Data', 'collate_to_set', 'collate_to_batch', 'Dataset',
-    'DataLoader', 'InMemoryDataset', 'DataLoader'
+    'makedirs', 'Data', 'collate_to_set', 'collate_to_batch', 'data_from_set',
+    'split_set', 'Dataset', 'DataLoader', 'InMemoryDataset', 'DataLoader'
 ]
