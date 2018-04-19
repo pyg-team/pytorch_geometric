2eb9a32a0807e1b59ace1d5b8ebaf43485f2f105
rusty1s <matthias.fey@tu-dortmund.de>
2018-04-19 13:46:52 +0200

    new base dataset

---
+++ torch_geometric/utils/dataset.py
@@ -0,0 +1,75 @@
+import collections
+import os.path as osp
+
+import torch
+
+
+def to_list(x):
+    return x if isinstance(x, collections.Iterable) else [x]
+
+
+def files_exist(files):
+    return all([osp.exists(f) for f in files])
+
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, root, transform=None):
+        super(Dataset, self).__init__()
+
+        self.root = osp.expanduser(osp.normpath(root))
+        self.raw_dir = osp.join(self.root, 'raw')
+        self.processed_dir = osp.join(self.root, 'processed')
+        self.transform = transform
+
+        self._download()
+        self._process()
+
+    @property
+    def raw_files(self):
+        raise NotImplementedError
+
+    @property
+    def processed_files(self):
+        raise NotImplementedError
+
+    @property
+    def _raw_files(self):
+        files = to_list(self.raw_files)
+        return [osp.join(self.raw_dir, f) for f in files]
+
+    @property
+    def _processed_files(self):
+        files = to_list(self.processed_files)
+        return [osp.join(self.processed_dir, f) for f in files]
+
+    def download(self):
+        raise NotImplementedError
+
+    def process(self):
+        raise NotImplementedError
+
+    def _download(self):
+        if files_exist(self._raw_files):
+            return
+
+        self.download()
+
+    def _process(self):
+        if files_exist(self._processed_files):
+            return
+
+        self.process()
+
+    def __len__(self):
+        raise NotImplementedError
+
+    def get_data(self, i):
+        raise NotImplementedError
+
+    def __getitem__(self, i):
+        data = self.get_data(i)
+
+        if self.transform is not None:
+            data = self.transform(data)
+
+        return data
