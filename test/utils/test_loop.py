056c01d6264397c0f30dd475b80a3a2618629b53
rusty1s <matthias.fey@tu-dortmund.de>
2018-04-24 16:04:55 +0200

    has self loop method

--- test/utils/test_loop.py
+++ test/utils/test_loop.py
@@ -1,5 +1,18 @@
 import torch
-from torch_geometric.utils import add_self_loops, remove_self_loops
+from torch_geometric.utils import (has_self_loops, add_self_loops,
+                                   remove_self_loops)
+
+
+def test_has_self_loops():
+    row = torch.LongTensor([0, 1, 0])
+    col = torch.LongTensor([1, 0, 0])
+
+    assert has_self_loops(torch.stack([row, col], dim=0)) is True
+
+    row = torch.LongTensor([0, 1, 1])
+    col = torch.LongTensor([1, 0, 2])
+
+    assert has_self_loops(torch.stack([row, col], dim=0)) is False
 
 
 def test_add_self_loops():
