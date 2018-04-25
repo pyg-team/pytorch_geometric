1dfa5ac8b8aaf59773fe12e4fbfe6a20a3990602
rusty1s <matthias.fey@tu-dortmund.de>
2018-04-25 17:26:29 +0200

    cleaner

--- torch_geometric/data/collate.py
+++ torch_geometric/data/collate.py
@@ -39,12 +39,14 @@ def collate_to_batch(data_list):
         batch['batch'].append(torch.LongTensor(data.num_nodes).fill_(idx))
 
     if 'edge_index' in keys:
+        data = batch['edge_index']
         for i in range(len(cum_num_nodes) - 1):
-            batch['edge_index'][i] = batch['edge_index'][i] + cum_num_nodes[i]
+            data[i] = data[i] + cum_num_nodes[i]
 
     if 'face' in keys:
+        data = batch['face']
         for i in range(len(cum_num_nodes) - 1):
-            batch['face'][i] = batch['face'][i] + cum_num_nodes[i]
+            data[i] = data[i] + cum_num_nodes[i]
 
     for key in batch.keys():
         batch[key] = torch.cat(batch[key], dim=dims[key])
