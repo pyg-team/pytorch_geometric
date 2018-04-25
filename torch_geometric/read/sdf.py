912650ef18b2918a4e06e9da508fa526fb1c1ac1
rusty1s <matthias.fey@tu-dortmund.de>
2018-04-25 09:07:30 +0200

    py2.7 fix

--- torch_geometric/read/sdf.py
+++ torch_geometric/read/sdf.py
@@ -10,13 +10,13 @@ def parse_sdf(src):
     counts_line = src[0].split()
     num_atoms, num_bonds = int(counts_line[0]), int(counts_line[1])
 
-    atom_block = [x for x in src[1:num_atoms + 1]]
+    atom_block = src[1:num_atoms + 1]
     atom_block = [x.split()[:4] for x in atom_block]
     pos = torch.Tensor([[float(y) for y in x[:3]] for x in atom_block])
-    x = one_hot(Long([elems[x[3]] for x in atom_block]), len(elems))
+    x = one_hot(Long([elems[a[3]] for a in atom_block]), len(elems))
 
-    bond_block = [x for x in src[1 + num_atoms:1 + num_atoms + num_bonds]]
-    bond_block = Long([[int(y) for y in x.split()[:3]] for x in bond_block])
+    bond_block = src[1 + num_atoms:1 + num_atoms + num_bonds]
+    bond_block = Long([[int(y) for y in b.split()[:3]] for b in bond_block])
     edge_index = bond_block[:, :2] - 1
     edge_index, perm = coalesce(edge_index.t())
     edge_index = edge_index.t().contiguous()
