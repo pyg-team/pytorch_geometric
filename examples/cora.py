fix softmax and agnn

examples/cora.py
@@ -61,6 +61,8 @@ for run in range(1, 101):
         train()
 
     acc.append(test(test_mask))
-    print('Run:', run, 'Test Accuracy:', acc[-1])
+    print('Run: {:03d}, Test: {:.4f}'.format(run, acc[-1]))
 
-print('Mean:', torch.Tensor(acc).mean(), 'Stddev:', torch.Tensor(acc).std())
+mean = torch.tensor(acc).mean().item()
+std = torch.tensor(acc).std().item()
+print('Mean: {:.4f}, Stddev: {:.4f}'.format(mean, std))

torch_geometric/nn/functional/agnn.py
@@ -1,31 +1,23 @@
-from __future__ import division
-
 import torch
-from torch.autograd import Variable
+from torch_geometric.utils import add_self_loops, softmax
+from torch_scatter import scatter_add
 
 
 def agnn(x, edge_index, beta):
-    n, e, m = x.size(0), edge_index.size(1), x.size(1)
-    row, col = edge_index
+    num_nodes = x.size(0)
+    row, col = add_self_loops(edge_index, num_nodes)
 
     # Create sparse propagation matrix.
-    norm = torch.norm(x, 2, 1)
+    norm = torch.norm(x, p=2, dim=1)
     norm = norm[row] * norm[col]
     cos = torch.matmul(x[row].unsqueeze(-2), x[col].unsqueeze(-1)).squeeze()
-    cos /= norm + 1e-7  # Prevent underflow.
-    cos *= beta
-
-    # Scatter softmax.
-    cos = cos.exp_()
-    cos_sum = Variable(cos.data.new(n)).fill_(0)
-    cos /= cos_sum.scatter_add_(0, Variable(row), cos)[row]
+    cos /= norm
+    cos = softmax(cos * beta, row, num_nodes)
 
     # Apply attention.
     x_col = cos.unsqueeze(-1) * x[col]
 
     # Sum up neighborhoods.
-    var_row = Variable(row.view(e, 1).expand(e, m))
-    output = Variable(x.data.new(n, m)).fill_(0)
-    output.scatter_add_(0, var_row, x_col)
+    out = scatter_add(x_col, row, dim=0, dim_size=num_nodes)
 
-    return output
+    return out

torch_geometric/utils/softmax.py
@@ -1,19 +1,8 @@
-from itertools import repeat
-
-import torch
-from torch.autograd import Variable
-from torch_geometric.utils import new
+from torch_scatter import scatter_add
 
 
-def softmax(index, src, num_nodes=None):
+def softmax(src, index, num_nodes=None):
     num_nodes = index.max().item() + 1 if num_nodes is None else num_nodes
-
-    index = index if torch.is_tensor(src) else Variable(index)
-    sizes = list(src.size())[1:]
-
-    output = src.exp()
-    output_sum = new(output, num_nodes, *sizes).fill_(0)
-    index_expand = index.view(-1, *repeat(1, len(sizes))).expand_as(src)
-    output /= output_sum.scatter_add_(0, index_expand, output)[index]
-
-    return output
+    out = src.exp()
+    out = out / scatter_add(out, index, dim=0, dim_size=num_nodes)[index]
+    return out