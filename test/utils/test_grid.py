9cfcb55c29a63b8c92fc6d1405af3be4fb032bbf
rusty1s <matthias.fey@tu-dortmund.de>
2018-05-07 21:48:05 +0200

    added grid implementation

---
+++ test/utils/test_grid.py
@@ -0,0 +1,16 @@
+from torch_geometric.utils import grid
+
+
+def test_grid():
+    (row, col), pos = grid(height=3, width=2)
+
+    expected_row = [0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2]
+    expected_col = [0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 4, 5]
+    expected_row += [3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5]
+    expected_col += [0, 1, 2, 3, 4, 5, 2, 3, 4, 5, 2, 3, 4, 5]
+
+    expected_pos = [[0, 2], [1, 2], [0, 1], [1, 1], [0, 0], [1, 0]]
+
+    assert row.tolist() == expected_row
+    assert col.tolist() == expected_col
+    assert pos.tolist() == expected_pos
--- torch_geometric/utils/__init__.py
+++ torch_geometric/utils/__init__.py
@@ -8,9 +8,10 @@ from .loop import has_self_loops, add_self_loops, remove_self_loops
 from .face import face_to_edge_index
 from .normalized_cut import normalized_cut
 from .one_hot import one_hot
+from .grid import grid
 
 __all__ = [
     'DataLoader', 'matmul', 'degree', 'softmax', 'coalesce', 'to_undirected',
     'has_self_loops', 'remove_self_loops', 'add_self_loops',
-    'face_to_edge_index', 'normalized_cut', 'one_hot'
+    'face_to_edge_index', 'normalized_cut', 'one_hot', 'grid'
 ]
