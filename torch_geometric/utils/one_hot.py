4917195f648577ae3c7c8af27fc771980787cc7a
rusty1s <matthias.fey@tu-dortmund.de>
2018-05-07 20:28:11 +0200

    typo

--- torch_geometric/utils/one_hot.py
+++ torch_geometric/utils/one_hot.py
@@ -1,9 +1,8 @@
 import torch
 
 
-def one_hot(src, size=None, dtype=None, device=None):
-    size = src.max().item() + 1 if size is None else size
-
-    out = torch.zeros(src.size(0), size, dtype=dtype, device=device)
+def one_hot(src, num_classes=None, dtype=None, device=None):
+    num_classes = src.max().item() + 1 if num_classes is None else num_classes
+    out = torch.zeros(src.size(0), num_classes, dtype=dtype, device=device)
     out.scatter_(1, src.unsqueeze(-1).expand_as(out), 1)
     return out
