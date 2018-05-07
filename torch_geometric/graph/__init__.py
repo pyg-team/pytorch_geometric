a5de6590f0c644406f4051aa40339a06e2b910fa
rusty1s <matthias.fey@tu-dortmund.de>
2018-05-07 21:53:03 +0200

    removed unused package

--- torch_geometric/graph/__init__.py
+++
@@ -1,7 +0,0 @@
-from .grid import grid_3x3, grid_5x5, grid_position
-from .geometry import edges_from_faces
-from .point_cloud import nn_graph
-
-__all__ = [
-    'grid_3x3', 'grid_5x5', 'grid_position', 'edges_from_faces', 'nn_graph'
-]
