fixed backprop

torch_geometric/transform/polar.py
@@ -46,7 +46,7 @@ class Polar(object):
 
         cart = pos[col] - pos[row]
         rho = torch.norm(cart, p=2, dim=-1)
-        rho /= rho.max()
+        rho = rho / rho.max()
         theta = torch.atan2(cart[..., 1], cart[..., 0]) / (2 * PI)
         theta += (theta < 0).type_as(theta)
         polar = torch.stack([rho, theta], dim=1)