7b26f881f385aa5e04cd39fc0b75422170e94ced
rusty1s <matthias.fey@tu-dortmund.de>
2018-05-19 20:57:25 +0200

    one hot can now take multiple targets

--- test/utils/test_one_hot.py
+++ test/utils/test_one_hot.py
@@ -6,5 +6,10 @@ def test_one_hot():
     src = torch.LongTensor([1, 0, 3])
     expected_output = [[0, 1, 0, 0], [1, 0, 0, 0], [0, 0, 0, 1]]
 
-    output = one_hot(src)
-    assert output.tolist() == expected_output
+    assert one_hot(src).tolist() == expected_output
+    assert one_hot(src, 4).tolist() == expected_output
+
+    src = torch.LongTensor([[1, 0], [0, 1], [2, 0]])
+    expected_output = [[0, 1, 0, 1, 0], [1, 0, 0, 0, 1], [0, 0, 1, 1, 0]]
+    assert one_hot(src).tolist() == expected_output
+    assert one_hot(src, torch.tensor([3, 2])).tolist() == expected_output
