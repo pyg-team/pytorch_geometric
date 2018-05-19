f9231a2864a1adea06d50c99dcd342ffb36edfb4
rusty1s <matthias.fey@tu-dortmund.de>
2018-05-19 13:20:33 +0200

    test batch

--- test/data/test_batch.py
+++ test/data/test_batch.py
@@ -1,7 +1,18 @@
-# def test_collate_to_batch():
-#     data = collate_to_batch([data1, data2])
+import torch
+from torch_geometric.data import Data, Batch
 
-#     assert len(data) == 3
-#     assert data.x.tolist() == [1, 2, 3, 1, 2]
-#     assert data.edge_index.tolist() == [[0, 1, 1, 2, 3, 4], [1, 0, 2, 1, 4, 3]]
-#     assert data.batch.tolist() == [0, 0, 0, 1, 1]
+
+def test_batch():
+    x1 = torch.tensor([1, 2, 3], dtype=torch.float)
+    e1 = torch.tensor([[0, 1, 1, 2], [1, 0, 2, 1]])
+    x2 = torch.tensor([1, 2], dtype=torch.float)
+    e2 = torch.tensor([[0, 1], [1, 0]])
+
+    data = Batch([Data(x1, e1), Data(x2, e2)])
+
+    assert data.__repr__() == 'Batch(batch=[5], edge_index=[2, 6], x=[5])'
+    assert len(data) == 3
+    assert data.x.tolist() == [1, 2, 3, 1, 2]
+    assert data.edge_index.tolist() == [[0, 1, 1, 2, 3, 4], [1, 0, 2, 1, 4, 3]]
+    assert data.batch.tolist() == [0, 0, 0, 1, 1]
+    assert data.num_graphs == 2
--- torch_geometric/data/batch.py
+++ torch_geometric/data/batch.py
@@ -5,26 +5,30 @@ from .data import Data
 
 class Batch(Data):
     def __init__(self, data_list):
-        assert data_list[0].batch is None
         keys = data_list[0].keys
+        assert 'batch' not in keys
 
         for key in keys:
             self[key] = []
         self.batch = []
 
         cumsum = 0
-        for i, d in enumerate(data_list):
-            self.batch.append(torch.full((d.num_nodes, ), i, dtype=torch.long))
+        for i, data in enumerate(data_list):
+            num_nodes = data.num_nodes
+            self.batch.append(torch.full((num_nodes, ), i, dtype=torch.long))
             for key in keys:
-                self[key].append(d[key] + cumsum if d.cumsum(key) else d[key])
-            cumsum += d.num_nodes
+                item = data[key]
+                item = item + cumsum if self.cumsum(key, item) else item
+                self[key].append(item)
+            cumsum += num_nodes
 
-        for key, item in self:
-            self[key] = torch.cat(item, dim=data_list[0].cat_dim(key))
+        for key in keys:
+            self[key] = torch.cat(self[key], dim=data_list[0].cat_dim(key))
+        self.batch = torch.cat(self.batch, dim=-1)
         self.contiguous()
 
-    def cumsum(self, key):
-        return self[key].dim() > 1 and self[key].dtype == torch.long
+    def cumsum(self, key, item):
+        return item.dim() > 1 and item.dtype == torch.long
 
     @property
     def num_graphs(self):
