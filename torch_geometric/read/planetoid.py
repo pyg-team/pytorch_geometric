b95f08a4e3921cd95e0d9e39a50d7aa80c3f8187
rusty1s <matthias.fey@tu-dortmund.de>
2018-05-20 00:13:03 +0200

    citeseer isolated node fix

--- torch_geometric/read/planetoid.py
+++ torch_geometric/read/planetoid.py
@@ -3,9 +3,9 @@ import os.path as osp
 from itertools import repeat
 
 import torch
-from torch_geometric.utils import coalesce
 from torch_geometric.data import Data
 from torch_geometric.read import read_txt_array
+from torch_geometric.utils import coalesce, remove_self_loops
 
 try:
     import cPickle as pickle
@@ -22,14 +22,24 @@ def read_planetoid_data(folder, prefix):
     x, tx, allx, y, ty, ally, graph, test_index = items
     train_index = torch.arange(y.size(0), dtype=torch.long)
     val_index = torch.arange(y.size(0), y.size(0) + 500, dtype=torch.long)
+    sorted_test_index = test_index.sort()[0]
 
     if prefix.lower() == 'citeseer':
-        pass
+        # There are some isolated nodes in the Citeseer graph, resulting in
+        # none consecutive test indices. We need to identify them and add them
+        # as zero vectors to `tx` and `ty`.
+        len_test_indices = (test_index.max() - test_index.min()).item() + 1
+
+        tx_ext = torch.zeros(len_test_indices, tx.size(1))
+        tx_ext[sorted_test_index - test_index.min(), :] = tx
+        ty_ext = torch.zeros(len_test_indices, ty.size(1))
+        ty_ext[sorted_test_index - test_index.min(), :] = ty
+
+        tx, ty = tx_ext, ty_ext
 
     x = torch.cat([allx, tx], dim=0)
     y = torch.cat([ally, ty], dim=0).max(dim=1)[1]
 
-    sorted_test_index = test_index.sort()[0]
     x[test_index] = x[sorted_test_index]
     y[test_index] = y[sorted_test_index]
 
@@ -51,9 +61,7 @@ def read_file(folder, prefix, name):
     path = osp.join(folder, 'ind.{}.{}'.format(prefix.lower(), name))
 
     if name == 'test.index':
-        obj = read_txt_array(path, dtype=torch.long)
-        print(obj.size())
-        return obj
+        return read_txt_array(path, dtype=torch.long)
 
     with open(path, 'rb') as f:
         if sys.version_info > (3, 0):
@@ -75,9 +83,10 @@ def edge_index_from_dict(graph_dict, num_nodes=None):
         row += repeat(key, len(value))
         col += value
     edge_index = torch.stack([torch.tensor(row), torch.tensor(col)], dim=0)
-    # NOTE: There are duplicated edges in the planetoid datasets. Other
-    # implementations do not remove them.
+    # NOTE: There are duplicated edges and self loops in the planetoid
+    # datasets. Other implementations do not remove them.
     edge_index, _ = coalesce(edge_index, num_nodes)
+    edge_index = remove_self_loops(edge_index)
     return edge_index
 
 
