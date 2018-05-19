e77334dc40ae35aafc2c7805fc595a8944329caa
rusty1s <matthias.fey@tu-dortmund.de>
2018-05-19 21:26:43 +0200

    enzyme test

---
+++ test/data/test_tu_dataset.py
@@ -0,0 +1,38 @@
+import sys
+import random
+import os.path as osp
+import shutil
+
+import pytest
+from torch_geometric.dataset import TUDataset
+from torch_geometric.data import DataLoader
+
+
+def test_enzymes():
+    root = osp.join('/', 'tmp', str(random.randrange(sys.maxsize)), 'test')
+    dataset = TUDataset(root, 'ENZYMES')
+    loader = DataLoader(dataset, batch_size=len(dataset))
+
+    assert len(dataset) == 600
+    assert dataset.__repr__() == 'ENZYMES(600)'
+
+    for data in loader:
+        assert data.num_graphs == 600
+
+        avg_num_nodes = data.num_nodes / data.num_graphs
+        assert pytest.approx(avg_num_nodes, abs=1e-2) == 32.63
+
+        avg_num_edges = data.num_edges / (2 * data.num_graphs)
+        assert pytest.approx(avg_num_edges, abs=1e-2) == 62.14
+
+        assert len(data) == 4
+        assert list(data.x.size()) == [data.num_nodes, 21]
+        assert list(data.y.size()) == [data.num_graphs]
+        assert data.y.max().item() + 1 == 6
+        assert list(data.batch.size()) == [data.num_nodes]
+
+        assert data.contains_isolated_nodes()
+        assert not data.contains_self_loops()
+        assert data.is_undirected()
+
+    shutil.rmtree(root)
