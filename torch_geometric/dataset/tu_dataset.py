c3ad3e316cdf8b7dacc015feacfd88219b7c0a8a
rusty1s <matthias.fey@tu-dortmund.de>
2018-05-19 19:50:45 +0200

    rename

--- torch_geometric/dataset/tu_dataset.py
+++ torch_geometric/dataset/tu_dataset.py
@@ -3,7 +3,7 @@ import os.path as osp
 
 import torch
 from torch_geometric.data import InMemoryDataset, download_url, extract_zip
-from torch_geometric.read import read_tu_dataset
+from torch_geometric.read import read_tu_data
 
 
 class TUDataset(InMemoryDataset):
@@ -13,9 +13,7 @@ class TUDataset(InMemoryDataset):
     def __init__(self, root, name, transform=None):
         self.name = name
         super(TUDataset, self).__init__(root, transform)
-        self.dataset, self.slices = torch.load(self.processed_paths[0])
-        print(self.dataset)
-        print(self.slices)
+        self.data, self.slices = torch.load(self.processed_paths[0])
 
     @property
     def raw_file_names(self):
@@ -34,5 +32,5 @@ class TUDataset(InMemoryDataset):
         os.rename(osp.join(self.root, self.name), self.raw_dir)
 
     def process(self):
-        dataset, slices = read_tu_dataset(self.raw_dir, self.name)
-        torch.save((dataset, slices), self.processed_paths[0])
+        data, slices = read_tu_data(self.raw_dir, self.name)
+        torch.save((data, slices), self.processed_paths[0])
