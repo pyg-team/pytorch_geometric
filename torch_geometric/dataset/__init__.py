a798bddb2419360ed6bd0473f269b5c4b4af3d4f
rusty1s <matthias.fey@tu-dortmund.de>
2018-05-19 23:40:03 +0200

    planetoid dataset

--- torch_geometric/dataset/__init__.py
+++ torch_geometric/dataset/__init__.py
@@ -1,3 +1,4 @@
 from .tu_dataset import TUDataset
+from .planetoid import Planetoid
 
-__all__ = ['TUDataset']
+__all__ = ['TUDataset', 'Planetoid']
---
+++ torch_geometric/dataset/planetoid.py
@@ -0,0 +1,33 @@
+import torch
+from torch_geometric.data import InMemoryDataset, download_url
+from torch_geometric.read import read_planetoid_data
+
+
+class Planetoid(InMemoryDataset):
+    url = 'https://github.com/kimiyoung/planetoid/raw/master/data'
+
+    def __init__(self, root, name, transform=None):
+        self.name = name
+        super(Planetoid, self).__init__(root, transform)
+        self.data, self.slices = torch.load(self.processed_paths[0])
+
+    @property
+    def raw_file_names(self):
+        names = ['x', 'tx', 'allx', 'y', 'ty', 'ally', 'graph', 'test.index']
+        return ['ind.{}.{}'.format(self.name.lower(), name) for name in names]
+
+    @property
+    def processed_file_names(self):
+        return 'data.pt'
+
+    def download(self):
+        for name in self.raw_file_names:
+            download_url('{}/{}'.format(self.url, name), self.raw_dir)
+
+    def process(self):
+        data = read_planetoid_data(self.raw_dir, self.name)
+        data, slices = self.collate([data])
+        torch.save((data, slices), self.processed_paths[0])
+
+    def __repr__(self):
+        return '{}()'.format(self.name)
