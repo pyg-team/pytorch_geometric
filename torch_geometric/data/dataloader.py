47fc6d38a06c6493b0c53e35bf6897a28fbd2f35
rusty1s <matthias.fey@tu-dortmund.de>
2018-05-19 14:06:44 +0200

    cleaner dataloader

--- torch_geometric/data/dataloader.py
+++ torch_geometric/data/dataloader.py
@@ -1,13 +1,8 @@
 import torch.utils.data
-from torch_geometric.data import Data, Batch
-
-
-def collate(batch):
-    if type(batch[0]) is Data:
-        return Batch(batch)
-    return torch.utils.data.dataloader.default_collate(batch)
+from torch_geometric.data import Batch
 
 
 class DataLoader(torch.utils.data.DataLoader):
     def __init__(self, dataset, **kwargs):
-        super(DataLoader, self).__init__(dataset, collate_fn=collate, **kwargs)
+        super(DataLoader, self).__init__(
+            dataset, collate_fn=lambda batch: Batch(batch), **kwargs)
