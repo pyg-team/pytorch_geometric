c8d7dcbb5a2ae3de1c52a520451fe7fc22f809a7
rusty1s <matthias.fey@tu-dortmund.de>
2018-05-20 09:58:02 +0200

    mnist superpixel dataset

--- torch_geometric/data/extract.py
+++ torch_geometric/data/extract.py
@@ -1,8 +1,14 @@
+import tarfile
 import zipfile
+
+
+def extract_tar(path, folder, mode='r:gz'):
+    print('Extracting', path)
+    with tarfile.open(path, mode) as f:
+        f.extractall(folder)
 
 
 def extract_zip(path, folder):
     print('Extracting', path)
-
     with zipfile.ZipFile(path, 'r') as f:
         f.extractall(folder)
---
+++ torch_geometric/dataset/mnist_superpixels.py
@@ -0,0 +1,45 @@
+import os
+
+import torch
+from torch_geometric.data import (InMemoryDataset, Data, download_url,
+                                  extract_tar)
+
+
+class MNISTSuperpixels(InMemoryDataset):
+    url = 'http://ls7-www.cs.uni-dortmund.de/cvpr_geometric_dl/' \
+          'mnist_superpixels.tar.gz'
+
+    def __init__(self, root, train=True, transform=None):
+        super(MNISTSuperpixels, self).__init__(root, transform)
+        path = self.processed_paths[0] if train else self.processed_paths[1]
+        self.data, self.slices = torch.load(path)
+
+    @property
+    def raw_file_names(self):
+        return ['training.pt', 'test.pt']
+
+    @property
+    def processed_file_names(self):
+        return ['training.pt', 'test.pt']
+
+    def download(self):
+        path = download_url(self.url, self.raw_dir)
+        extract_tar(path, self.raw_dir, mode='r')
+        os.unlink(path)
+
+    def process(self):
+        for raw_path, path in zip(self.raw_paths, self.processed_paths):
+            x, edge_index, edge_slice, pos, y = torch.load(raw_path)
+            edge_index, y = edge_index.to(torch.long), y.to(torch.long)
+            m, n = y.size(0), 75
+            x, pos = x.view(m * n, 1), pos.view(m * n, 2)
+            node_slice = torch.arange(0, (m + 1) * n, step=n, dtype=torch.long)
+            graph_slice = torch.arange(m + 1, dtype=torch.long)
+            data = Data(x=x, edge_index=edge_index, y=y, pos=pos)
+            slices = {
+                'x': node_slice,
+                'edge_index': edge_slice,
+                'y': graph_slice,
+                'pos': node_slice
+            }
+            torch.save((data, slices), path)
