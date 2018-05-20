1f67335a0244c3593180e5e2300636afb4195ed9
rusty1s <matthias.fey@tu-dortmund.de>
2018-05-21 00:39:58 +0200

    rename

--- torch_geometric/transform/__init__.py
+++ torch_geometric/transform/__init__.py
@@ -5,7 +5,7 @@ from .polar import Polar
 from .spherical import Spherical
 from .target_indegree import TargetIndegree
 from .linear_transformation import LinearTransformation
-from .normalize import Normalize
+from .normalize_features import NormalizeFeatures
 from .nn_graph import NNGraph
 
 from .log_cartesian import LogCartesian
@@ -15,12 +15,10 @@ from .random_rotate import RandomRotate
 from .random_shear import RandomShear
 from .normalize_scale import NormalizeScale
 from .random_flip import RandomFlip
-from .sample_pointcloud import SamplePointcloud, PCToGraphNN, PCToGraphRad
 
 __all__ = [
     'Compose', 'Cartesian', 'LocalCartesian', 'Polar', 'Spherical',
-    'TargetIndegree', 'LinearTransformation', 'Normalize', 'NNGraph',
-    'LogCartesian', 'RandomFlip', 'PCToGraphRad', 'PCToGraphNN',
-    'SamplePointcloud', 'RandomTranslate', 'RandomScale', 'RandomRotate',
-    'RandomShear', 'NormalizeScale'
+    'TargetIndegree', 'LinearTransformation', 'NormalizeFeatures', 'NNGraph',
+    'LogCartesian', 'RandomFlip', 'RandomTranslate', 'RandomScale',
+    'RandomRotate', 'RandomShear', 'NormalizeScale'
 ]
