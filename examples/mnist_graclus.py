94b73517f59400d5c1a7fbaae140231249bd8f5d
rusty1s <matthias.fey@tu-dortmund.de>
2018-05-22 15:55:24 +0200

    fixed graclus pooling for new data object

--- examples/mnist_graclus.py
+++ examples/mnist_graclus.py
@@ -1,56 +1,53 @@
-from __future__ import division, print_function
-
 import os.path as osp
 
 import torch
-from torch import nn
 import torch.nn.functional as F
 from torch_scatter import scatter_mean
-from torch_geometric.datasets import MNISTSuperpixels
-from torch_geometric.utils import DataLoader, normalized_cut
-from torch_geometric.transform import Cartesian
+from torch_geometric.dataset import MNISTSuperpixels
+import torch_geometric.transform as T
+from torch_geometric.data import DataLoader
+from torch_geometric.utils import normalized_cut
 from torch_geometric.nn.modules import SplineConv
 from torch_geometric.nn.functional.pool import graclus_pool
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
 path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'MNIST')
-train_dataset = MNISTSuperpixels(path, True, transform=Cartesian())
-test_dataset = MNISTSuperpixels(path, False, transform=Cartesian())
-
+train_dataset = MNISTSuperpixels(path, True, transform=T.Cartesian())
+test_dataset = MNISTSuperpixels(path, False, transform=T.Cartesian())
 train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=64)
+d = train_dataset.data
 
 
 def normalized_cut_2d(edge_index, pos):
     row, col = edge_index
     edge_attr = torch.norm(pos[row] - pos[col], p=2, dim=1)
-    return normalized_cut(edge_index, edge_attr, pos.size(0))
+    return normalized_cut(edge_index, edge_attr, num_nodes=pos.size(0))
 
 
-class Net(nn.Module):
+class Net(torch.nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = SplineConv(1, 32, dim=2, kernel_size=5)
+        self.conv1 = SplineConv(d.num_features, 32, dim=2, kernel_size=5)
         self.conv2 = SplineConv(32, 64, dim=2, kernel_size=5)
-        self.fc1 = nn.Linear(64, 128)
-        self.fc2 = nn.Linear(128, 10)
+        self.fc1 = torch.nn.Linear(64, 128)
+        self.fc2 = torch.nn.Linear(128, d.num_classes)
 
     def forward(self, data):
-        data.input = F.elu(self.conv1(data.input, data.index, data.weight))
-        weight = normalized_cut_2d(data.index, data.pos)
-        data = graclus_pool(data, weight, transform=Cartesian())
+        data.x = F.elu(self.conv1(data.x, data.edge_index, data.edge_attr))
+        edge_attr = normalized_cut_2d(data.edge_index, data.pos)
+        data = graclus_pool(data, edge_attr, transform=T.Cartesian())
 
-        data.input = F.elu(self.conv2(data.input, data.index, data.weight))
-        weight = normalized_cut_2d(data.index, data.pos)
-        data = graclus_pool(data, weight, transform=Cartesian())
+        data.x = F.elu(self.conv2(data.x, data.edge_index, data.edge_attr))
+        edge_attr = normalized_cut_2d(data.edge_index, data.pos)
+        data = graclus_pool(data, edge_attr, transform=T.Cartesian())
 
-        x = scatter_mean(data.input, data.batch, dim=0)
+        x = scatter_mean(data.x, data.batch, dim=0)
         x = F.elu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         return F.log_softmax(self.fc2(x), dim=1)
 
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = Net().to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
@@ -67,9 +64,9 @@ def train(epoch):
             param_group['lr'] = 0.0001
 
     for data in train_loader:
-        data = data.cuda()
+        data = data.to(device)
         optimizer.zero_grad()
-        F.nll_loss(model(data), data.target).backward()
+        F.nll_loss(model(data), data.y).backward()
         optimizer.step()
 
 
@@ -78,9 +75,9 @@ def test():
     correct = 0
 
     for data in test_loader:
-        data = data.cuda()
+        data = data.to(device)
         pred = model(data).max(1)[1]
-        correct += pred.eq(data.target).sum().item()
+        correct += pred.eq(data.y).sum().item()
     return correct / len(test_dataset)
 
 
--- torch_geometric/nn/functional/pool/graclus.py
+++ torch_geometric/nn/functional/pool/graclus.py
@@ -1,21 +1,20 @@
 from torch_cluster import graclus_cluster
+from torch_geometric.data import Batch
 
 from .consecutive import consecutive_cluster
 from .pool import pool, max_pool
-from ....datasets.dataset import Data
 
 
-def graclus_pool(data, weight=None, transform=None):
-    row, col = data.index
+def graclus_pool(data, edge_attr=None, transform=None):
+    row, col = data.edge_index
 
-    cluster = graclus_cluster(row, col, weight, data.num_nodes)
+    cluster = graclus_cluster(row, col, edge_attr, data.num_nodes)
     cluster, batch = consecutive_cluster(cluster, data.batch)
 
-    x = max_pool(data.input, cluster)
-    edge_index, edge_attr, pos = pool(data.index, cluster, data.weight,
-                                      data.pos)
+    x = max_pool(data.x, cluster)
+    edge_index, _, pos = pool(data.edge_index, cluster, None, data.pos)
 
-    data = Data(x, pos, edge_index, edge_attr, data.target, batch)
+    data = Batch(x=x, edge_index=edge_index, pos=pos, batch=batch)
 
     if transform is not None:
         data = transform(data)
