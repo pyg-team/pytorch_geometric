1224af5890b7584dbeeca974ba8b4b542486d048
rusty1s <matthias.fey@tu-dortmund.de>
2018-05-23 09:01:33 +0200

    fixed tests

--- test/transform/test_random_flip.py
+++ test/transform/test_random_flip.py
@@ -1,13 +1,13 @@
 import torch
 from torch_geometric.transform import RandomFlip
-from torch_geometric.datasets.dataset import Data
+from torch_geometric.data import Data
 
 
 def test_cartesian():
     assert RandomFlip(axis=0).__repr__() == 'RandomFlip(axis=0, p=0.5)'
 
     pos = torch.tensor([[-1, 1], [-3, 0], [2, -1]], dtype=torch.float)
-    data = Data(None, pos, None, None, None)
+    data = Data(pos=pos)
 
     out = RandomFlip(axis=0, p=1)(data).pos.tolist()
     assert out == [[1, 1], [3, 0], [-2, -1]]
--- test/transform/test_random_rotate.py
+++ test/transform/test_random_rotate.py
@@ -1,14 +1,14 @@
 from pytest import approx
 import torch
 from torch_geometric.transform import RandomRotate
-from torch_geometric.datasets.dataset import Data
+from torch_geometric.data import Data
 
 
 def test_spherical():
     assert RandomRotate(-180).__repr__() == 'RandomRotate((-180, 180))'
 
     pos = torch.tensor([[1, 0], [0, 1]], dtype=torch.float)
-    data = Data(None, pos, None, None, None)
+    data = Data(pos=pos)
 
     out = RandomRotate((90, 90))(data).pos.view(-1).tolist()
     assert approx(out) == [0, 1, -1, 0]
