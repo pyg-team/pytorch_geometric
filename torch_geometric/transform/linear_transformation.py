952024222b494c8869528f1661267bbf02f46837
rusty1s <matthias.fey@tu-dortmund.de>
2018-05-23 09:00:01 +0200

    removed alphas

--- torch_geometric/transform/linear_transformation.py
+++ torch_geometric/transform/linear_transformation.py
@@ -12,14 +12,14 @@ class LinearTransformation(object):
     .. testsetup::
 
         import torch
-        from torch_geometric.datasets.dataset import Data
+        from torch_geometric.data import Data
 
     .. testcode::
 
         from torch_geometric.transform import LinearTransformation
 
         pos = torch.tensor([[-1, 1], [-3, 0], [2, -1]], dtype=torch.float)
-        data = Data(None, pos, None, None, None)
+        data = Data(pos=pos)
 
         matrix = torch.tensor([[2, 0], [0, 2]], dtype=torch.float)
         data = LinearTransformation(matrix)(data)
