5abe2ecf6d05baed81f54b7b0335e4eeb7732085
rusty1s <matthias.fey@tu-dortmund.de>
2018-05-23 08:42:55 +0200

    fixed examples for new dataset

---
+++ examples/agnn.py
@@ -0,0 +1,57 @@
+import os.path as osp
+
+import torch
+import torch.nn.functional as F
+from torch_geometric.dataset import Planetoid
+import torch_geometric.transform as T
+from torch_geometric.nn.modules import AGNN
+
+dataset = 'Cora'
+path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', dataset)
+data = Planetoid(path, dataset, T.NormalizeFeatures())[0]
+
+
+class Net(torch.nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = torch.nn.Linear(data.num_features, 16)
+        self.conv1 = AGNN(requires_grad=False)
+        self.conv2 = AGNN(requires_grad=True)
+        self.fc2 = torch.nn.Linear(16, data.num_classes)
+
+    def forward(self):
+        x = F.dropout(data.x, training=self.training)
+        x = F.relu(self.fc1(x))
+        x = self.conv1(x, data.edge_index)
+        x = self.conv2(x, data.edge_index)
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model, data = Net().to(device), data.to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+
+
+def train():
+    model.train()
+    optimizer.zero_grad()
+    F.nll_loss(model()[data.train_mask], data.y[data.train_mask]).backward()
+    optimizer.step()
+
+
+def test():
+    model.eval()
+    logits, accs = model(), []
+    for _, mask in data('train_mask', 'val_mask', 'test_mask'):
+        pred = logits[mask].max(1)[1]
+        acc = pred.eq(data.y[mask]).sum().item() / mask.sum().item()
+        accs.append(acc)
+    return accs
+
+
+for epoch in range(1, 151):
+    train()
+    log = 'Epoch: {:03d}, Train: {:.4f}, Val: {:.4f}, Test: {:.4f}'
+    print(log.format(epoch, *test()))
---
+++ examples/gat.py
@@ -0,0 +1,53 @@
+import os.path as osp
+
+import torch
+import torch.nn.functional as F
+from torch_geometric.dataset import Planetoid
+import torch_geometric.transform as T
+from torch_geometric.nn.modules import GAT
+
+dataset = 'Cora'
+path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', dataset)
+data = Planetoid(path, dataset, T.NormalizeFeatures())[0]
+
+
+class Net(torch.nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.att1 = GAT(data.num_features, 8, heads=8, dropout=0.6)
+        self.att2 = GAT(8 * 8, data.num_classes, dropout=0.6)
+
+    def forward(self):
+        x = F.dropout(data.x, p=0.6, training=self.training)
+        x = F.elu(self.att1(x, data.edge_index))
+        x = F.dropout(x, p=0.6, training=self.training)
+        x = self.att2(x, data.edge_index)
+        return F.log_softmax(x, dim=1)
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model, data = Net().to(device), data.to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.005, weight_decay=5e-4)
+
+
+def train():
+    model.train()
+    optimizer.zero_grad()
+    F.nll_loss(model()[data.train_mask], data.y[data.train_mask]).backward()
+    optimizer.step()
+
+
+def test():
+    model.eval()
+    logits, accs = model(), []
+    for _, mask in data('train_mask', 'test_mask'):
+        pred = logits[mask].max(1)[1]
+        acc = pred.eq(data.y[mask]).sum().item() / mask.sum().item()
+        accs.append(acc)
+    return accs
+
+
+for epoch in range(1, 101):
+    train()
+    log = 'Epoch: {:03d}, Train: {:.4f}, Test: {:.4f}'
+    print(log.format(epoch, *test()))
