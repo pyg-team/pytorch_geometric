63b6b2c7005bfe895689a066e14252b2527b0653
rusty1s <matthias.fey@tu-dortmund.de>
2018-05-24 18:32:45 +0200

    remove first bevore adding self loops

--- torch_geometric/utils/__init__.py
+++ torch_geometric/utils/__init__.py
@@ -4,7 +4,7 @@ from .softmax import softmax
 from .coalesce import is_coalesced, coalesce
 from .undirected import is_undirected, to_undirected
 from .isolated import contains_isolated_nodes
-from .loop import contains_self_loops, add_self_loops, remove_self_loops
+from .loop import contains_self_loops, remove_self_loops, add_self_loops
 from .one_hot import one_hot
 from .grid import grid
 from .normalized_cut import normalized_cut
