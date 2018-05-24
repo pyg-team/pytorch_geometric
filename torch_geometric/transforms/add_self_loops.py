86c86b4683c599ebb9f35e1694ed353c9715685d
rusty1s <matthias.fey@tu-dortmund.de>
2018-05-24 18:44:44 +0200

    coalesce call

--- torch_geometric/transforms/add_self_loops.py
+++ torch_geometric/transforms/add_self_loops.py
@@ -1,9 +1,12 @@
-from torch_geometric.utils import add_self_loops
+from torch_geometric.utils import add_self_loops, coalesce
 
 
 class AddSelfLoops(object):
     def __call__(self, data):
-        data.edge_index = add_self_loops(data.edge_index, data.num_nodes)
+        edge_index = data.edge_index
+        edge_index = add_self_loops(edge_index, num_nodes=data.num_nodes)
+        edge_index, _ = coalesce(edge_index, num_nods=data.num_nodes)
+        data.edge_index = edge_index
         return data
 
     def __repr__(self):
