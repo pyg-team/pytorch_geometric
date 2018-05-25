91f29bb3202551a183b533499d1b8a91ee81d933
rusty1s <matthias.fey@tu-dortmund.de>
2018-05-25 09:43:50 +0200

    bugfix for zero vecs

--- torch_geometric/transforms/normalize_features.py
+++ torch_geometric/transforms/normalize_features.py
@@ -1,6 +1,6 @@
 class NormalizeFeatures(object):
     def __call__(self, data):
-        data.x = data.x / data.x.sum(1, keepdim=True)
+        data.x = data.x / data.x.sum(1, keepdim=True).clamp(min=1)
         return data
 
     def __repr__(self):
