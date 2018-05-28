bf8363a164237ff09f74a6ff041e1ee771ba87ec
rusty1s <matthias.fey@tu-dortmund.de>
2018-05-28 22:01:55 +0200

    dense_to_sparse implementation

--- torch_geometric/utils/__init__.py
+++ torch_geometric/utils/__init__.py
@@ -8,6 +8,7 @@ from .loop import contains_self_loops, remove_self_loops, add_self_loops
 from .one_hot import one_hot
 from .grid import grid
 from .normalized_cut import normalized_cut
+from .sparse import dense_to_sparse
 
 __all__ = [
     'matmul',
@@ -24,4 +25,5 @@ __all__ = [
     'one_hot',
     'grid',
     'normalized_cut',
+    'dense_to_sparse',
 ]
