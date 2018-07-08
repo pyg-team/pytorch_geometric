8b3cc555610b129feab233bb98ebb8283ff09eb5
rusty1s <matthias.fey@tu-dortmund.de>
2018-07-08 11:53:31 +0200

    typo

--- torch_geometric/transforms/add_self_loops.py
+++ torch_geometric/transforms/add_self_loops.py
@@ -5,7 +5,7 @@ class AddSelfLoops(object):
     def __call__(self, data):
         edge_index = data.edge_index
         edge_index = add_self_loops(edge_index, num_nodes=data.num_nodes)
-        edge_index, _ = coalesce(edge_index, num_nods=data.num_nodes)
+        edge_index, _ = coalesce(edge_index, num_nodes=data.num_nodes)
         data.edge_index = edge_index
         return data
 
