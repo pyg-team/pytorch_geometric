6620d840d41af3104ab791e785b5e5a221d80bf4
rusty1s <matthias.fey@tu-dortmund.de>
2018-07-12 16:26:24 +0200

    graclus num nodes fix

--- torch_geometric/nn/pool/graclus.py
+++ torch_geometric/nn/pool/graclus.py
@@ -1,6 +1,6 @@
 from torch_cluster import graclus_cluster
 
 
-def graclus(edge_index, weight=None):
+def graclus(edge_index, weight=None, num_nodes=None):
     row, col = edge_index
-    return graclus_cluster(row, col, weight)
+    return graclus_cluster(row, col, weight, num_nodes)
