acf4ffad290fbb332d7a67f61dfe18ca1061bfc0
rusty1s <matthias.fey@tu-dortmund.de>
2018-08-27 14:49:43 +0200

    allow batch_size and shuffle passed as non-default args

--- torch_geometric/data/dataloader.py
+++ torch_geometric/data/dataloader.py
@@ -3,8 +3,10 @@ from torch_geometric.data import Batch
 
 
 class DataLoader(torch.utils.data.DataLoader):
-    def __init__(self, dataset, **kwargs):
+    def __init__(self, dataset, batch_size=1, shuffle=True, **kwargs):
         super(DataLoader, self).__init__(
             dataset,
+            batch_size,
+            shuffle,
             collate_fn=lambda batch: Batch.from_data_list(batch),
             **kwargs)
