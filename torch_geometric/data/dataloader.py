eabfd725d60df4acd3a8cbc2d7837852f1c1dcfc
rusty1s <matthias.fey@tu-dortmund.de>
2018-09-11 14:33:04 +0200

    to dense transform and loader

--- torch_geometric/data/dataloader.py
+++ torch_geometric/data/dataloader.py
@@ -1,4 +1,6 @@
 import torch.utils.data
+from torch.utils.data.dataloader import default_collate
+
 from torch_geometric.data import Batch
 
 
@@ -10,3 +12,16 @@ class DataLoader(torch.utils.data.DataLoader):
             shuffle,
             collate_fn=lambda batch: Batch.from_data_list(batch),
             **kwargs)
+
+
+class DenseDataLoader(torch.utils.data.DataLoader):
+    def __init__(self, dataset, batch_size=1, shuffle=True, **kwargs):
+        super(DenseDataLoader, self).__init__(
+            dataset, batch_size, shuffle, collate_fn=dense_collate, **kwargs)
+
+
+def dense_collate(data_list):
+    batch = Batch()
+    for key in data_list[0].keys:
+        batch[key] = default_collate([d[key] for d in data_list])
+    return batch
