7c500681b414e055723d22a180f7e0474b5c6cc5
rusty1s <matthias.fey@tu-dortmund.de>
2018-09-13 15:01:06 +0200

    only graph sage mean

--- torch_geometric/nn/conv/sage_conv.py
+++ torch_geometric/nn/conv/sage_conv.py
@@ -1,16 +1,20 @@
 import torch
+import torch.nn.functional as F
 from torch.nn import Parameter
+from torch_scatter import scatter_mean
+
+from torch_geometric.utils import remove_self_loops, add_self_loops
 
 from ..inits import uniform
 
 
-class SAGEConv(torch.nn.Module):
-    def __init__(self, aggregator, out_channels, bias=True):
-        super(SAGEConv, self).__init__()
+class GraphSAGEMean(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, normalize=True, bias=True):
+        super(GraphSAGEMean, self).__init__()
 
-        self.aggregator = aggregator
-        in_channels = sum(list(self.aggregator.weight.size()))
-        self.weight = Parameter(torch.Tensor(in_channels, out_channels))
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.weight = Parameter(torch.Tensor(self.in_channels, out_channels))
 
         if bias:
             self.bias = Parameter(torch.Tensor(out_channels))
@@ -23,18 +27,23 @@ class SAGEConv(torch.nn.Module):
         size = self.weight.size(0)
         uniform(size, self.weight)
         uniform(size, self.bias)
-        self.aggregator.reset_parameters()
 
     def forward(self, x, edge_index):
-        out = torch.cat([x, self.aggregator(x, edge_index)], dim=-1)
+        edge_index, _ = remove_self_loops(edge_index, None)
+        edge_index = add_self_loops(edge_index, num_nodes=x.size(0))
+
+        row, col = edge_index
+        out = scatter_mean(x[row], row, dim=0, dim_size=x.size(0))
         out = torch.matmul(out, self.weight)
-        out = out / torch.norm(out, p=2, dim=-1).clamp(min=1).view(-1, 1)
 
         if self.bias is not None:
             out = out + self.bias
 
+        if self.normalize:
+            out = F.normalize(out, p=2, dim=-1)
+
         return out
 
     def __repr__(self):
-        return '{}({}, {})'.format(self.__class__.__name__, self.aggregator,
-                                   self.weight.size(1))
+        return '{}({}, {})'.format(self.__class__.__name__, self.in_channels,
+                                   self.out_channels)
