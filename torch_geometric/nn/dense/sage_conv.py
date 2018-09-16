928a7acce380f51bb1357a8c32d7323b2dc3e8c1
rusty1s <matthias.fey@tu-dortmund.de>
2018-09-16 20:58:56 +0200

    init mean conv

--- torch_geometric/nn/dense/sage_conv.py
+++ torch_geometric/nn/dense/sage_conv.py
@@ -1,13 +1,32 @@
 import torch
+from torch.nn import Parameter
+
+from ..inits import uniform
 
 
-class DenseSAGEConv(torch.nn.Module):
-    def __init__(self, aggregator, out_channels, bias=True):
-        super(DenseSAGEConv, self).__init__()
+class DenseSAGEMeanConv(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, normalize=True, bias=True):
+        super(DenseSAGEMeanConv, self).__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.weight = Parameter(torch.Tensor(self.in_channels, out_channels))
+
+        if bias:
+            self.bias = Parameter(torch.Tensor(out_channels))
+        else:
+            self.register_parameter('bias', None)
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        size = self.weight.size(0)
+        uniform(size, self.weight)
+        uniform(size, self.bias)
 
     def forward(self, x, adj):
         pass
 
     def __repr__(self):
-        return '{}({}, {})'.format(self.__class__.__name__, self.aggregator,
-                                   self.weight.size(1))
+        return '{}({}, {})'.format(self.__class__.__name__, self.in_channels,
+                                   self.out_channels)
