f2504ede6176923c9fdd8fcaa37cd1b74a8ff583
rusty1s <matthias.fey@tu-dortmund.de>
2018-09-26 10:08:54 +0200

    set2set

--- torch_geometric/nn/pool/__init__.py
+++ torch_geometric/nn/pool/__init__.py
@@ -1,7 +1,8 @@
 from .global_pool import global_add_pool, global_mean_pool, global_max_pool
+from .set2set import Set2Set
+
 from .max_pool import max_pool, max_pool_x
 from .avg_pool import avg_pool, avg_pool_x
-
 from .graclus import graclus
 from .voxel_grid import voxel_grid
 
@@ -9,6 +10,7 @@ __all__ = [
     'global_add_pool',
     'global_mean_pool',
     'global_max_pool',
+    'Set2Set',
     'max_pool',
     'max_pool_x',
     'avg_pool',
