3e0d1b64faecc0a42f522a351e8131dc57feaae1
Riccardo Bucco <riccardobucco@gmail.com>
2018-11-05 16:33:00 +0100

    Add zeros init function

--- torch_geometric/nn/inits.py
+++ torch_geometric/nn/inits.py
@@ -9,3 +9,7 @@ def glorot(tensor):
     stdv = math.sqrt(6.0/(tensor.shape[0]+tensor.shape[1]))
     if tensor is not None:
         tensor.data.uniform_(-stdv, stdv)
+
+def zeros(tensor):
+    if tensor is not None:
+        tensor.data.zero_()
