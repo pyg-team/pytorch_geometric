0d26091d82fc9cbaf071d256a0f5f15408288517
rusty1s <matthias.fey@tu-dortmund.de>
2018-11-12 16:27:15 +0100

    fixed test

--- test/nn/pool/test_topk_pool.py
+++ test/nn/pool/test_topk_pool.py
@@ -1,10 +1,8 @@
 import torch
-from torch_geometric.nn.pool import TopKPooling
+from torch_geometric.nn.pool.topk_pool import topk, filter_adj
 
 
 def test_topk():
-    topk = TopKPooling(in_channels=1).topk
-
     x = torch.tensor([2, 4, 5, 6, 2, 9], dtype=torch.float)
     batch = torch.tensor([0, 0, 1, 1, 1, 1])
 
@@ -15,8 +13,6 @@ def test_topk():
 
 
 def test_filter_adj():
-    filter_adj = TopKPooling(in_channels=1).filter_adj
-
     edge_index = torch.tensor([[0, 0, 1, 1, 2, 2, 3, 3],
                                [1, 3, 0, 2, 1, 3, 0, 2]])
     edge_attr = torch.tensor([1, 2, 3, 4, 5, 6, 7, 8])
