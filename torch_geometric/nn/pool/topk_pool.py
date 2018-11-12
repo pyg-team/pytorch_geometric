bef2fdfb386718ee26805673c747b4be1fe75d7a
rusty1s <matthias.fey@tu-dortmund.de>
2018-11-12 15:20:51 +0100

    outsourced functions

--- torch_geometric/nn/pool/topk_pool.py
+++ torch_geometric/nn/pool/topk_pool.py
@@ -6,6 +6,56 @@ from ..inits import uniform
 from ...utils.num_nodes import maybe_num_nodes
 
 
+def topk(x, ratio, batch):
+    num_nodes = scatter_add(batch.new_ones(x.size(0)), batch, dim=0)
+    batch_size, max_num_nodes = num_nodes.size(0), num_nodes.max().item()
+    k = (ratio * num_nodes.to(torch.float)).round().to(torch.long)
+
+    cum_num_nodes = torch.cat(
+        [num_nodes.new_zeros(1),
+         num_nodes.cumsum(dim=0)[:-1]], dim=0)
+
+    index = torch.arange(batch.size(0), dtype=torch.long, device=x.device)
+    index = (index - cum_num_nodes[batch]) + (batch * max_num_nodes)
+
+    x_min_value = x.min().item()
+    dense_x = x.new_full((batch_size * max_num_nodes, ), x_min_value - 1)
+    dense_x[index] = x
+    dense_x = dense_x.view(batch_size, max_num_nodes)
+    _, perm = dense_x.sort(dim=-1, descending=True)
+
+    perm = perm + cum_num_nodes.view(-1, 1)
+    perm = perm.view(-1)
+
+    mask = [
+        torch.arange(k[i], dtype=torch.long, device=x.device) +
+        i * max_num_nodes for i in range(len(num_nodes))
+    ]
+    mask = torch.cat(mask, dim=0)
+
+    perm = perm[mask]
+
+    return perm
+
+
+def filter_adj(edge_index, edge_attr, perm, num_nodes=None):
+    num_nodes = maybe_num_nodes(edge_index, num_nodes)
+
+    mask = perm.new_full((num_nodes, ), -1)
+    i = torch.arange(perm.size(0), dtype=torch.long, device=perm.device)
+    mask[perm] = i
+
+    row, col = edge_index
+    row, col = mask[row], mask[col]
+    mask = (row >= 0) & (col >= 0)
+    row, col = row[mask], col[mask]
+
+    if edge_attr is not None:
+        edge_attr = edge_attr[mask]
+
+    return torch.stack([row, col], dim=0), edge_attr
+
+
 class TopKPooling(torch.nn.Module):
     """Top-k Pooling Layer from the `"Graph U-Net"
     <https://openreview.net/forum?id=HJePRoAct7>`_ paper.
@@ -29,63 +79,15 @@ class TopKPooling(torch.nn.Module):
         size = self.in_channels
         uniform(size, self.weight)
 
-    def topk(self, x, k, batch):
-        num_nodes = scatter_add(batch.new_ones(x.size(0)), batch, dim=0)
-        batch_size, max_num_nodes = num_nodes.size(0), num_nodes.max().item()
-        k = (self.ratio * num_nodes.to(torch.float)).round().to(torch.long)
-
-        cum_num_nodes = torch.cat(
-            [num_nodes.new_zeros(1),
-             num_nodes.cumsum(dim=0)[:-1]], dim=0)
-
-        index = torch.arange(batch.size(0), dtype=torch.long, device=x.device)
-        index = (index - cum_num_nodes[batch]) + (batch * max_num_nodes)
-
-        x_min_value = x.min().item()
-        dense_x = x.new_full((batch_size * max_num_nodes, ), x_min_value - 1)
-        dense_x[index] = x
-        dense_x = dense_x.view(batch_size, max_num_nodes)
-        _, perm = dense_x.sort(dim=-1, descending=True)
-
-        perm = perm + cum_num_nodes.view(-1, 1)
-        perm = perm.view(-1)
-
-        mask = [
-            torch.arange(k[i], dtype=torch.long, device=x.device) +
-            i * max_num_nodes for i in range(len(num_nodes))
-        ]
-        mask = torch.cat(mask, dim=0)
-
-        perm = perm[mask]
-
-        return perm
-
-    def filter_adj(self, edge_index, edge_attr, perm, num_nodes=None):
-        num_nodes = maybe_num_nodes(edge_index, num_nodes)
-
-        mask = perm.new_full((num_nodes, ), -1)
-        i = torch.arange(perm.size(0), dtype=torch.long, device=perm.device)
-        mask[perm] = i
-
-        row, col = edge_index
-        row, col = mask[row], mask[col]
-        mask = (row >= 0) & (col >= 0)
-        row, col = row[mask], col[mask]
-
-        if edge_attr is not None:
-            edge_attr = edge_attr[mask]
-
-        return torch.stack([row, col], dim=0), edge_attr
-
     def forward(self, x, edge_index, edge_attr, batch):
         x = x.unsqueeze(-1) if x.dim() == 1 else x
 
         score = (x * self.weight).sum(dim=-1)
         score = score / self.weight.norm(p=2, dim=-1)
-        perm = self.topk(score, self.ratio, batch)
+        perm = topk(score, self.ratio, batch)
         x = x[perm] * torch.tanh(score[perm]).view(-1, 1)
         batch = batch[perm]
-        edge_index, edge_attr = self.filter_adj(
+        edge_index, edge_attr = filter_adj(
             edge_index, edge_attr, perm, num_nodes=score.size(0))
 
         return x, edge_index, edge_attr, batch, perm
