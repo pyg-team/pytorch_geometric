e91cde3d40b7b8bb7f4b4d541e2cf72e8d9bf308
rusty1s <matthias.fey@tu-dortmund.de>
2018-11-23 09:02:43 +0100

    gin conv with eps parameter

--- torch_geometric/nn/conv/gin_conv.py
+++ torch_geometric/nn/conv/gin_conv.py
@@ -1,26 +1,30 @@
-import torch.nn as nn
+import torch
 from torch_scatter import scatter_add
 
 from ..inits import reset
 
 
-class GINConv(nn.Module):
-    def __init__(self, nn, add_root=True):
+class GINConv(torch.nn.Module):
+    def __init__(self, nn, eps=0, train=False):
         super(GINConv, self).__init__()
         self.nn = nn
-        self.add_root = add_root
+        self.initial_eps = eps
+        if train:
+            self.eps = torch.nn.Parameter(torch.Tensor([eps]))
+        else:
+            self.register_buffer('eps', torch.Tensor([eps]))
         self.reset_parameters()
 
     def reset_parameters(self):
         reset(self.nn)
+        self.eps.data.fill_(self.initial_eps)
 
     def forward(self, x, edge_index, size=None):
         size = x.size(0) if size is None else size
 
         row, col = edge_index
         out = scatter_add(x[col], row, dim=0, dim_size=size)
-        if self.add_root:
-            out = out + x
+        out = (1 + self.eps) * out + x
         out = self.nn(out)
         return out
 
