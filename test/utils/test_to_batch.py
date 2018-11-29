674eeaec2fb49e8e1e526e687dfcca4f18bc0208
rusty1s <matthias.fey@tu-dortmund.de>
2018-11-29 11:29:01 +0100

    rename

--- test/utils/test_to_batch.py
+++ test/utils/test_to_batch.py
@@ -6,10 +6,11 @@ def test_to_batch():
     x = torch.Tensor([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12]])
     batch = torch.tensor([0, 0, 1, 2, 2, 2])
 
-    x = to_batch(x, batch)
+    x, num_nodes = to_batch(x, batch)
     expected = [
         [[1, 2], [3, 4], [0, 0]],
         [[5, 6], [0, 0], [0, 0]],
         [[7, 8], [9, 10], [11, 12]],
     ]
     assert x.tolist() == expected
+    assert num_nodes.tolist() == [2, 1, 3]
