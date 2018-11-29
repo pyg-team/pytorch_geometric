c95b6e4b4adb720459db6cb4cf65da552d20007c
rusty1s <matthias.fey@tu-dortmund.de>
2018-11-29 11:04:30 +0100

    to_batch

---
+++ test/utils/test_batch.py
@@ -0,0 +1,12 @@
+import torch
+from torch_geometric.utils import to_batch
+
+
+def test_to_batch():
+    x = torch.Tensor([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12]])
+    batch = torch.tensor([0, 0, 1, 2, 2, 2])
+
+    x = to_batch(x, batch)
+    expected = [[[1, 2], [3, 4], [0, 0]], [[5, 6], [0, 0], [0, 0]],
+                [[7, 8], [9, 10], [11, 12]]]
+    assert x.tolist() == expected
