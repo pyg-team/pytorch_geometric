f5adc701e27930cababaaa02c62f1c099c728762
rusty1s <matthias.fey@tu-dortmund.de>
2018-12-12 20:20:02 +0100

    restructure + doc

--- torch_geometric/nn/pool/__init__.py
+++ torch_geometric/nn/pool/__init__.py
@@ -1,18 +1,10 @@
-from .global_pool import global_add_pool, global_mean_pool, global_max_pool
-from .set2set import Set2Set
-
 from .max_pool import max_pool, max_pool_x
 from .avg_pool import avg_pool, avg_pool_x
 from .graclus import graclus
 from .voxel_grid import voxel_grid
 from .topk_pool import TopKPooling
-from .sort_pool import sort_pool
 
 __all__ = [
-    'global_add_pool',
-    'global_mean_pool',
-    'global_max_pool',
-    'Set2Set',
     'max_pool',
     'max_pool_x',
     'avg_pool',
@@ -20,5 +12,4 @@ __all__ = [
     'graclus',
     'voxel_grid',
     'TopKPooling',
-    'sort_pool',
 ]
