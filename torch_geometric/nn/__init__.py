983877513baf2d795a57af970f68fcb3a6e88aca
rusty1s <matthias.fey@tu-dortmund.de>
2018-12-14 10:02:52 +0100

    x-conv

--- torch_geometric/nn/__init__.py
+++ torch_geometric/nn/__init__.py
@@ -1,10 +1,12 @@
+from .meta import MetaLayer
+from .reshape import Reshape
 from .conv import *  # noqa
 from .prop import *  # noqa
 from .glob import *  # noqa
 from .pool import *  # noqa
 from .dense import *  # noqa
-from .meta import MetaLayer
 
 __all__ = [
     'MetaLayer',
+    'Reshape',
 ]
---
+++ torch_geometric/nn/reshape.py
@@ -0,0 +1,16 @@
+import torch
+
+
+class Reshape(torch.nn.Module):
+    def __init__(self, *shape):
+        super(Reshape, self).__init__()
+        self.shape = shape
+
+    def forward(self, x):
+        """"""
+        x = x.view(*self.shape)
+        return x
+
+    def __repr__(self):
+        return '{}({})'.format(self.__class__.__name__,
+                               ', '.join([str(d) for d in self.shape]))
