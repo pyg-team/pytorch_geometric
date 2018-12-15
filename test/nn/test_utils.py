4ec6f7b97ca3d220d8d5ff908a85175cbfb98d65
rusty1s <matthias.fey@tu-dortmund.de>
2018-12-15 16:50:45 +0100

    test print function

--- test/nn/test_utils.py
+++ test/nn/test_utils.py
@@ -46,6 +46,7 @@ def test_reset():
 def test_reshape():
     x = torch.randn(10, 4)
     op = Reshape(5, 2, 4)
+    assert op.__repr__() == 'Reshape(5, 2, 4)'
 
     assert op(x).size() == (5, 2, 4)
     assert op(x).view(10, 4).tolist() == x.tolist()
