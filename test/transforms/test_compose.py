51499dfab98493d8561dabba75b06d3de60e4d8d
rusty1s <matthias.fey@tu-dortmund.de>
2018-12-16 19:03:28 +0100

    cleaner test compose

--- test/transforms/test_compose.py
+++ test/transforms/test_compose.py
@@ -4,16 +4,17 @@ from torch_geometric.data import Data
 
 
 def test_compose():
-    transform = T.Compose([T.Cartesian(), T.TargetIndegree()])
-    out = ('Compose([\n'
-           '    Cartesian(norm=True, max_value=None),\n'
-           '    TargetIndegree(cat=True),\n'
-           '])')
-    assert transform.__repr__() == out
+    transform = T.Compose([T.Center(), T.AddSelfLoops()])
+    assert transform.__repr__() == ('Compose([\n'
+                                    '    Center(),\n'
+                                    '    AddSelfLoops(),\n'
+                                    '])')
 
-    pos = torch.tensor([[-1, 0], [0, 0], [2, 0]], dtype=torch.float)
-    edge_index = torch.tensor([[0, 1], [1, 2]])
+    pos = torch.tensor([[0, 0], [2, 0], [4, 0]], dtype=torch.float)
+    edge_index = torch.tensor([[0, 1, 1, 2], [1, 0, 2, 1]])
     data = Data(edge_index=edge_index, pos=pos)
 
-    out = transform(data).edge_attr.tolist()
-    assert out == [[0.75, 0.5, 1], [1, 0.5, 1]]
+    out = transform(data)
+    assert out.pos.tolist() == [[-2, 0], [0, 0], [2, 0]]
+    assert out.edge_index.tolist() == [[0, 0, 1, 1, 1, 2, 2],
+                                       [0, 1, 0, 1, 2, 1, 2]]
