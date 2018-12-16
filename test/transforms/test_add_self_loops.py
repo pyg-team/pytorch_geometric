e0554201a89909aceea2f0722d6d2714236cefe3
rusty1s <matthias.fey@tu-dortmund.de>
2018-12-16 19:00:23 +0100

    test center

--- test/transforms/test_add_self_loops.py
+++ test/transforms/test_add_self_loops.py
@@ -3,7 +3,7 @@ from torch_geometric.transforms import AddSelfLoops
 from torch_geometric.data import Data
 
 
-def test_constant():
+def test_add_self_loops():
     assert AddSelfLoops().__repr__() == 'AddSelfLoops()'
 
     edge_index = torch.tensor([[0, 1, 1, 2], [1, 0, 2, 1]])
