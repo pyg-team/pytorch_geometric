f4235ece32e6184f935c5e2937da584da92bcc8c
rusty1s <matthias.fey@tu-dortmund.de>
2018-12-27 18:57:41 +0100

    docs

--- torch_geometric/datasets/tu_dataset.py
+++ torch_geometric/datasets/tu_dataset.py
@@ -7,6 +7,31 @@ from torch_geometric.read import read_tu_data
 
 
 class TUDataset(InMemoryDataset):
+    r"""A variety of graph kernel benchmark datasets, *.e.g.* IMDB, REDDIT and
+    PROTEINS, collected from the `TU Dortmund University
+    <http://graphkernels.cs.tu-dortmund.de>`_.
+
+    Args:
+        root (string): Root directory where the dataset should be saved.
+        name (string): The `name <http://graphkernels.cs.tu-dortmund.de>`_ of
+            the dataset.
+        transform (callable, optional): A function/transform that takes in an
+            :obj:`torch_geometric.data.Data` object and returns a transformed
+            version. The data object is transformed before every access.
+            (default: :obj:`None`)
+        pre_transform (callable, optional): A function/transform that takes in
+            an :obj:`torch_geometric.data.Data` object and returns a
+            transformed version. The data object is transformed before being
+            saved to disk. (default: :obj:`None`)
+        filter (callabla, optional): A function that takes in an
+            :obj:`torch_geometric.data.Data` object and returns a boolean
+            value, indicating whether the data object should be included in the
+            final dataset. (default: :obj:`None`)
+        use_node_attr (bool, optional): If :obj:`True`, the dataset will
+            contain additional continuous node features (if present).
+            (default: :obj:`False`)
+    """
+
     url = 'https://ls11-www.cs.uni-dortmund.de/people/morris/' \
           'graphkerneldatasets'
 
