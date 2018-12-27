658174c9bff09309600d8d111352088cff9862c2
rusty1s <matthias.fey@tu-dortmund.de>
2018-12-27 17:37:08 +0100

    karateclub doc

--- torch_geometric/datasets/karate.py
+++ torch_geometric/datasets/karate.py
@@ -4,16 +4,31 @@ from torch_geometric.data import InMemoryDataset, Data
 
 
 class KarateClub(InMemoryDataset):
+    r""""Zachary's Karate Club Network from the `"An Information Flow Model for
+    Conflict and Fission in Small Groups"
+    <http://www1.ind.ku.dk/complexLearning/zachary1977.pdf>`_ paper, containing
+    34 nodes, connected by 154 (undirected and unweighted) edges.
+    Every node is labeled by one of two classes.
+
+    Args:
+        transform (callable, optional): A function/transform that takes in an
+            :obj:`torch_geometric.data.Data` object and returns a transformed
+            version. (default: :obj:`None`)
+    """
+
     def __init__(self, transform=None):
         super(KarateClub, self).__init__('.', transform, None, None)
 
         G = nx.karate_club_graph()
+
         adj = nx.to_scipy_sparse_matrix(G).tocoo()
         row = torch.from_numpy(adj.row).to(torch.long)
         col = torch.from_numpy(adj.col).to(torch.long)
         edge_index = torch.stack([row, col], dim=0)
         data = Data(edge_index=edge_index)
         data.x = torch.eye(data.num_nodes, dtype=torch.float)
+        y = [0 if G.node[i]['club'] == 'Mr. Hi' else 1 for i in G.nodes]
+        data.y = torch.tensor(y)
         self.data, self.slices = self.collate([data])
 
     def _download(self):
