3f0abc1b337084139a6d6463a59a7cea84286f03
rusty1s <matthias.fey@tu-dortmund.de>
2018-12-28 09:27:07 +0100

    test cheb conv

---
+++ test/nn/conv/test_cheb_conv.py
@@ -0,0 +1,15 @@
+import torch
+from torch_geometric.nn import ChebConv
+
+
+def test_cheb_conv():
+    in_channels, out_channels = (16, 32)
+    edge_index = torch.tensor([[0, 0, 0, 1, 2, 3], [1, 2, 3, 0, 0, 0]])
+    num_nodes = edge_index.max().item() + 1
+    edge_attr = torch.rand(edge_index.size(1))
+    x = torch.randn((num_nodes, in_channels))
+
+    conv = ChebConv(in_channels, out_channels, K=3)
+    assert conv.__repr__() == 'ChebConv(16, 32, K=3)'
+    assert conv(x, edge_index).size() == (num_nodes, out_channels)
+    assert conv(x, edge_index, edge_attr).size() == (num_nodes, out_channels)
