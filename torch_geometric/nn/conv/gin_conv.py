ba00341f5fb1e277c8a6de8fed79fa8145f309da
rusty1s <matthias.fey@tu-dortmund.de>
2018-12-28 12:29:43 +0100

    test gin conv

--- torch_geometric/nn/conv/gin_conv.py
+++ torch_geometric/nn/conv/gin_conv.py
@@ -49,4 +49,4 @@ class GINConv(torch.nn.Module):
         return out
 
     def __repr__(self):
-        return '{}({})'.format(self.__class__.__name__, self.nn)
+        return '{}(nn={})'.format(self.__class__.__name__, self.nn)
