da2b0af1b04af6bdffba2b4961ad9f56a354a310
rusty1s <matthias.fey@tu-dortmund.de>
2018-12-28 09:46:13 +0100

    test graph conv

---
+++ test/nn/conv/test_graph_conv.py
@@ -0,0 +1,13 @@
+import torch
+from torch_geometric.nn import GraphConv
+
+
+def test_gcn_conv():
+    in_channels, out_channels = (16, 32)
+    edge_index = torch.tensor([[0, 0, 0, 1, 2, 3], [1, 2, 3, 0, 0, 0]])
+    num_nodes = edge_index.max().item() + 1
+    x = torch.randn((num_nodes, in_channels))
+
+    conv = GraphConv(in_channels, out_channels)
+    assert conv.__repr__() == 'GraphConv(16, 32)'
+    assert conv(x, edge_index).size() == (num_nodes, out_channels)
