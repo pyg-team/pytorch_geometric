a0dcbcba00fee925c46174af2dfb84cfdfdc388a
rusty1s <matthias.fey@tu-dortmund.de>
2018-12-28 09:15:44 +0100

    test convs

--- test/nn/conv/test_gcn_conv.py
+++ test/nn/conv/test_gcn_conv.py
@@ -5,11 +5,11 @@ from torch_geometric.nn import GCNConv
 def test_gcn_conv():
     in_channels, out_channels = (16, 32)
     edge_index = torch.tensor([[0, 0, 0, 1, 2, 3], [1, 2, 3, 0, 0, 0]])
+    num_nodes = edge_index.max().item() + 1
     edge_attr = torch.rand(edge_index.size(1))
-    x = torch.randn((edge_index.max().item() + 1, in_channels))
+    x = torch.randn((num_nodes, in_channels))
 
     conv = GCNConv(in_channels, out_channels)
     assert conv.__repr__() == 'GCNConv(16, 32)'
-
-    conv(x, edge_index).size() == (4, 32)
-    conv(x, edge_index, edge_attr).size() == (4, 32)
+    assert conv(x, edge_index).size() == (num_nodes, out_channels)
+    assert conv(x, edge_index, edge_attr).size() == (num_nodes, out_channels)
