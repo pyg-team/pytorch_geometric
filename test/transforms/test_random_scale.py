ed3ba5ee3726394afb9756dbae91c5e07a03a8f2
rusty1s <matthias.fey@tu-dortmund.de>
2018-12-29 19:52:24 +0100

    test random scale

---
+++ test/transforms/test_random_scale.py
@@ -0,0 +1,19 @@
+import torch
+from torch_geometric.transforms import RandomScale
+from torch_geometric.data import Data
+
+
+def test_random_scale():
+    assert RandomScale([1, 2]).__repr__() == 'RandomScale([1, 2])'
+
+    pos = torch.Tensor([[-1, -1], [-1, 1], [1, -1], [1, 1]])
+
+    data = Data(pos=pos)
+    data = RandomScale([1, 1])(data)
+    assert len(data) == 1
+    assert data.pos.tolist() == pos.tolist()
+
+    data = Data(pos=pos)
+    data = RandomScale([2, 2])(data)
+    assert len(data) == 1
+    assert data.pos.tolist() == [[-2, -2], [-2, 2], [2, -2], [2, 2]]
