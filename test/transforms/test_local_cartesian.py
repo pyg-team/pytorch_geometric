c8984bf57de474a1e67a9bfe25dd589207eccdda
rusty1s <matthias.fey@tu-dortmund.de>
2018-12-29 19:32:35 +0100

    cartesian tests

--- test/transforms/test_local_cartesian.py
+++ test/transforms/test_local_cartesian.py
@@ -6,14 +6,22 @@ from torch_geometric.data import Data
 def test_local_cartesian():
     assert LocalCartesian().__repr__() == 'LocalCartesian()'
 
-    pos = torch.tensor([[-1, 0], [0, 0], [2, 0]], dtype=torch.float)
+    pos = torch.Tensor([[-1, 0], [0, 0], [2, 0]])
     edge_index = torch.tensor([[0, 1, 1, 2], [1, 0, 2, 1]])
+    edge_attr = torch.Tensor([1, 1, 1, 1])
+
     data = Data(edge_index=edge_index, pos=pos)
+    data = LocalCartesian()(data)
+    assert len(data) == 3
+    assert data.pos.tolist() == pos.tolist()
+    assert data.edge_index.tolist() == edge_index.tolist()
+    assert data.edge_attr.tolist() == [[1, 0.5], [0.25, 0.5], [1, 0.5],
+                                       [0, 0.5]]
 
-    out = LocalCartesian()(data).edge_attr.tolist()
-    assert out == [[1, 0.5], [0.25, 0.5], [1, 0.5], [0, 0.5]]
-
-    data.edge_attr = torch.tensor([1, 1, 1, 1], dtype=torch.float)
-
-    out = LocalCartesian(cat=True)(data).edge_attr.tolist()
-    assert out == [[1, 1, 0.5], [1, 0.25, 0.5], [1, 1, 0.5], [1, 0, 0.5]]
+    data = Data(edge_index=edge_index, pos=pos, edge_attr=edge_attr)
+    data = LocalCartesian()(data)
+    assert len(data) == 3
+    assert data.pos.tolist() == pos.tolist()
+    assert data.edge_index.tolist() == edge_index.tolist()
+    assert data.edge_attr.tolist() == [[1, 1, 0.5], [1, 0.25, 0.5],
+                                       [1, 1, 0.5], [1, 0, 0.5]]
