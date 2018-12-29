7d8d1bec8a532cef188f26e9ce610e203f95f804
rusty1s <matthias.fey@tu-dortmund.de>
2018-12-29 20:11:29 +0100

    test one hot degree

---
+++ test/transforms/test_one_hot_degree.py
@@ -0,0 +1,24 @@
+import torch
+from torch_geometric.transforms import OneHotDegree
+from torch_geometric.data import Data
+
+
+def test_one_hot_degree():
+    assert OneHotDegree(max_degree=3).__repr__() == 'OneHotDegree(3)'
+
+    edge_index = torch.tensor([[0, 0, 0, 1, 2, 3], [1, 2, 3, 0, 0, 0]])
+    x = torch.Tensor([1, 1, 1, 1])
+
+    data = Data(edge_index=edge_index)
+    data = OneHotDegree(max_degree=3)(data)
+    assert len(data) == 2
+    assert data.edge_index.tolist() == edge_index.tolist()
+    assert data.x.tolist() == [[0, 0, 0, 1], [0, 1, 0, 0], [0, 1, 0, 0],
+                               [0, 1, 0, 0]]
+
+    data = Data(edge_index=edge_index, x=x)
+    data = OneHotDegree(max_degree=3)(data)
+    assert len(data) == 2
+    assert data.edge_index.tolist() == edge_index.tolist()
+    assert data.x.tolist() == [[1, 0, 0, 0, 1], [1, 0, 1, 0, 0],
+                               [1, 0, 1, 0, 0], [1, 0, 1, 0, 0]]
