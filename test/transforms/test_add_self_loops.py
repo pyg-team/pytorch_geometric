e9f0066badfc9fd0394fceabe9cce68cf407b864
rusty1s <matthias.fey@tu-dortmund.de>
2018-12-29 20:27:55 +0100

    test transforms done

--- test/transforms/test_add_self_loops.py
+++ test/transforms/test_add_self_loops.py
@@ -7,7 +7,9 @@ def test_add_self_loops():
     assert AddSelfLoops().__repr__() == 'AddSelfLoops()'
 
     edge_index = torch.tensor([[0, 1, 1, 2], [1, 0, 2, 1]])
-    data = Data(edge_index=edge_index)
 
-    out = AddSelfLoops()(data).edge_index
-    assert out.tolist() == [[0, 0, 1, 1, 1, 2, 2], [0, 1, 0, 1, 2, 1, 2]]
+    data = Data(edge_index=edge_index)
+    data = AddSelfLoops()(data)
+    assert len(data) == 1
+    assert data.edge_index.tolist() == [[0, 0, 1, 1, 1, 2, 2],
+                                        [0, 1, 0, 1, 2, 1, 2]]
--- test/transforms/test_center.py
+++ test/transforms/test_center.py
@@ -7,7 +7,8 @@ def test_center():
     assert Center().__repr__() == 'Center()'
 
     pos = torch.Tensor([[0, 0], [2, 0], [4, 0]])
-    data = Data(pos=pos)
 
-    out = Center()(data).pos
-    assert out.tolist() == [[-2, 0], [0, 0], [2, 0]]
+    data = Data(pos=pos)
+    data = Center()(data)
+    assert len(data) == 1
+    assert data.pos.tolist() == [[-2, 0], [0, 0], [2, 0]]
