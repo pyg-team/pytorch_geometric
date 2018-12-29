55bb7fa4c7da8b904c4be67385e9841b9b14b51d
rusty1s <matthias.fey@tu-dortmund.de>
2018-12-29 20:06:25 +0100

    test radius graph

--- test/transforms/test_radius_graph.py
+++ test/transforms/test_radius_graph.py
@@ -4,11 +4,12 @@ from torch_geometric.data import Data
 
 
 def test_radius_graph():
-    assert RadiusGraph(1).__repr__() == 'RadiusGraph(r=1)'
+    assert RadiusGraph(r=1).__repr__() == 'RadiusGraph(r=1)'
 
-    pos = [[0, 0], [1, 0], [2, 0], [0, 1], [-2, 0], [0, -2]]
-    pos = torch.tensor(pos, dtype=torch.float)
+    pos = torch.Tensor([[0, 0], [1, 0], [2, 0], [0, 1], [-2, 0], [0, -2]])
+
     data = Data(pos=pos)
-
-    edge_index = RadiusGraph(1)(data).edge_index
-    assert edge_index.tolist() == [[0, 0, 1, 1, 2, 3], [1, 3, 0, 2, 1, 0]]
+    data = RadiusGraph(r=1)(data)
+    assert len(data) == 2
+    assert data.pos.tolist() == pos.tolist()
+    assert data.edge_index.tolist() == [[0, 0, 1, 1, 2, 3], [1, 3, 0, 2, 1, 0]]
