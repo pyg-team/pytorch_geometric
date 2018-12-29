81f75264daced158041c5e8379632dee5adcde21
rusty1s <matthias.fey@tu-dortmund.de>
2018-12-29 19:50:13 +0100

    test random shear

--- test/transforms/test_random_translate.py
+++ test/transforms/test_random_translate.py
@@ -6,7 +6,12 @@ from torch_geometric.data import Data
 def test_random_translate():
     assert RandomTranslate(0.1).__repr__() == 'RandomTranslate(0.1)'
 
-    pos = torch.Tensor([[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]])
+    pos = torch.Tensor([[0, 0], [0, 0], [0, 0], [0, 0]])
+
+    data = Data(pos=pos)
+    data = RandomTranslate(0)(data)
+    assert len(data) == 1
+    assert data.pos.tolist() == pos.tolist()
 
     data = Data(pos=pos)
     data = RandomTranslate(0.1)(data)
@@ -15,11 +20,9 @@ def test_random_translate():
     assert data.pos.max().item() <= 0.1
 
     data = Data(pos=pos)
-    data = RandomTranslate([0.1, 1, 0.1])(data)
+    data = RandomTranslate([0.1, 1])(data)
     assert len(data) == 1
     assert data.pos[:, 0].min().item() >= -0.1
     assert data.pos[:, 0].max().item() <= 0.1
     assert data.pos[:, 1].min().item() >= -1
     assert data.pos[:, 1].max().item() <= 1
-    assert data.pos[:, 2].min().item() >= -0.1
-    assert data.pos[:, 2].max().item() <= 0.1
