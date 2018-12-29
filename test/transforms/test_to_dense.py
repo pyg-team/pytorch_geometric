9325c7bb0e59f75692a6f17d7b8880e0d3ca75a4
rusty1s <matthias.fey@tu-dortmund.de>
2018-12-29 18:50:54 +0100

    transform tests

---
+++ test/transforms/test_to_dense.py
@@ -0,0 +1,51 @@
+import torch
+from torch_geometric.transforms import ToDense
+from torch_geometric.data import Data
+
+
+def test_to_dense():
+    edge_index = torch.tensor([[0, 0, 0, 1, 2, 3], [1, 2, 3, 0, 0, 0]])
+    edge_attr = torch.Tensor([1, 2, 3, 4, 5, 6])
+    num_nodes = edge_index.max().item() + 1
+    x = torch.randn((num_nodes, 4))
+    pos = torch.randn((num_nodes, 3))
+    y = torch.randint(0, 4, (num_nodes, ), dtype=torch.long)
+
+    assert ToDense().__repr__() == 'ToDense()'
+    data = Data(x=x, pos=pos, edge_index=edge_index, edge_attr=edge_attr, y=y)
+    data = ToDense()(data)
+    assert len(data) == 5
+    assert data.x.tolist() == x.tolist()
+    assert data.pos.tolist() == pos.tolist()
+    assert data.y.tolist() == y.tolist()
+    assert data.adj.size() == (num_nodes, num_nodes)
+    assert data.adj.tolist() == [
+        [0, 1, 2, 3],
+        [4, 0, 0, 0],
+        [5, 0, 0, 0],
+        [6, 0, 0, 0],
+    ]
+    assert data.mask.tolist() == [1, 1, 1, 1]
+
+    assert ToDense(num_nodes=5).__repr__() == 'ToDense(num_nodes=5)'
+    data = Data(x=x, pos=pos, edge_index=edge_index, edge_attr=edge_attr, y=y)
+    data = ToDense(num_nodes=5)(data)
+    assert len(data) == 5
+    assert data.x.size() == (5, 4)
+    assert data.x[:4].tolist() == x.tolist()
+    assert data.x[4].tolist() == [0, 0, 0, 0]
+    assert data.pos.size() == (5, 3)
+    assert data.pos[:4].tolist() == pos.tolist()
+    assert data.pos[4].tolist() == [0, 0, 0]
+    assert data.y.size() == (5, )
+    assert data.y[:4].tolist() == y.tolist()
+    assert data.y[4].tolist() == 0
+    assert data.adj.size() == (5, 5)
+    assert data.adj.tolist() == [
+        [0, 1, 2, 3, 0],
+        [4, 0, 0, 0, 0],
+        [5, 0, 0, 0, 0],
+        [6, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ]
+    assert data.mask.tolist() == [1, 1, 1, 1, 0]
