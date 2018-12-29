4499097c89622ff3671f9ca50ce0163ea323eddf
rusty1s <matthias.fey@tu-dortmund.de>
2018-12-29 20:14:21 +0100

    test normalize scale

---
+++ test/transforms/test_normalize_scale.py
@@ -0,0 +1,15 @@
+import torch
+from torch_geometric.transforms import NormalizeScale
+from torch_geometric.data import Data
+
+
+def test_normalize_scale():
+    assert NormalizeScale().__repr__() == 'NormalizeScale()'
+
+    pos = torch.randn((10, 3))
+
+    data = Data(pos=pos)
+    data = NormalizeScale()(data)
+    assert len(data) == 1
+    assert data.pos.min().item() > -1
+    assert data.pos.max().item() < 1
