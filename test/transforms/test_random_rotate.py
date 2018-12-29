adfef5fd63c38039e015a45da2b941d2e198a18a
rusty1s <matthias.fey@tu-dortmund.de>
2018-12-29 19:59:22 +0100

    test random rotate

--- test/transforms/test_random_rotate.py
+++ test/transforms/test_random_rotate.py
@@ -1,14 +1,34 @@
-from pytest import approx
 import torch
 from torch_geometric.transforms import RandomRotate
 from torch_geometric.data import Data
 
 
-def test_spherical():
-    assert RandomRotate(-180).__repr__() == 'RandomRotate((-180, 180))'
+def test_random_rotate():
+    assert RandomRotate([-180, 180]).__repr__() == 'RandomRotate([-180, 180])'
 
-    pos = torch.tensor([[1, 0], [0, 1]], dtype=torch.float)
+    pos = torch.Tensor([[-1, -1], [-1, 1], [1, -1], [1, 1]])
+
     data = Data(pos=pos)
+    data = RandomRotate([180, 180])(data)
+    assert len(data) == 1
+    assert data.pos.tolist() == [[1, 1], [1, -1], [-1, 1], [-1, -1]]
 
-    out = RandomRotate((90, 90))(data).pos.view(-1).tolist()
-    assert approx(out) == [0, 1, -1, 0]
+    pos = torch.Tensor([[-1, -1, 1], [-1, 1, 1], [1, -1, -1], [1, 1, -1]])
+
+    data = Data(pos=pos)
+    data = RandomRotate([180, 180], axis=0)(data)
+    assert len(data) == 1
+    assert data.pos.tolist() == [[-1, 1, -1], [-1, -1, -1], [1, 1, 1],
+                                 [1, -1, 1]]
+
+    data = Data(pos=pos)
+    data = RandomRotate([180, 180], axis=1)(data)
+    assert len(data) == 1
+    assert data.pos.tolist() == [[1, -1, -1], [1, 1, -1], [-1, -1, 1],
+                                 [-1, 1, 1]]
+
+    data = Data(pos=pos)
+    data = RandomRotate([180, 180], axis=2)(data)
+    assert len(data) == 1
+    assert data.pos.tolist() == [[1, 1, 1], [1, -1, 1], [-1, 1, -1],
+                                 [-1, -1, -1]]
