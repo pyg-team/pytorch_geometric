e12fdd4466f96fd3aec0e2cb46b1eef4ca9e3ff8
rusty1s <matthias.fey@tu-dortmund.de>
2018-12-29 20:22:48 +0100

    test knn graph

--- test/transforms/test_knn_graph.py
+++ test/transforms/test_knn_graph.py
@@ -6,13 +6,14 @@ from torch_geometric.data import Data
 def test_knn_graph():
     assert KNNGraph().__repr__() == 'KNNGraph(k=6)'
 
-    pos = [[0, 0], [1, 0], [2, 0], [0, 1], [-2, 0], [0, -2]]
-    pos = torch.tensor(pos, dtype=torch.float)
-    data = Data(pos=pos)
+    pos = torch.Tensor([[0, 0], [1, 0], [2, 0], [0, 1], [-2, 0], [0, -2]])
 
-    row, col = KNNGraph(2)(data).edge_index
     expected_row = [0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 3, 3, 3, 4, 4, 5, 5]
     expected_col = [1, 2, 3, 4, 5, 0, 2, 3, 5, 0, 1, 0, 1, 4, 0, 3, 0, 1]
 
-    assert row.tolist() == expected_row
-    assert col.tolist() == expected_col
+    data = Data(pos=pos)
+    data = KNNGraph(k=2)(data)
+    assert len(data) == 2
+    assert data.pos.tolist() == pos.tolist()
+    assert data.edge_index[0].tolist() == expected_row
+    assert data.edge_index[1].tolist() == expected_col
