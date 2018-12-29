c98896e9b26d47d517140bdd760a4ad4671697cf
rusty1s <matthias.fey@tu-dortmund.de>
2018-12-29 15:56:40 +0100

    test cluster methods

--- test/nn/pool/test_voxel_grid.py
+++ test/nn/pool/test_voxel_grid.py
@@ -1,11 +1,12 @@
-# import torch
-# from torch_geometric.nn.pool.voxel_grid import voxel_grid
+import torch
+from torch_geometric.nn import voxel_grid
 
-# def test_voxel_grid():
-#     pos = torch.Tensor([[0.5, 0.5], [1.2, 1.5], [0.2, 1.7], [0.3, 0.4],
-#                         [0.5, 0.5], [1.2, 1.5], [0.2, 1.7], [0.3, 0.4]])
-#     batch = torch.LongTensor([0, 0, 0, 0, 1, 1, 1, 1])
 
-#     cluster, batch = voxel_grid(pos, size=1, start=0, end=2, batch=batch)
-#     assert cluster.tolist() == [0, 2, 1, 0, 3, 5, 4, 3]
-#     assert batch.tolist() == [0, 0, 0, 1, 1, 1]
+def test_voxel_grid():
+    pos = torch.Tensor([[0, 0], [11, 9], [2, 8], [2, 2], [8, 3]])
+    batch = torch.tensor([0, 0, 0, 1, 1])
+
+    assert voxel_grid(pos, batch, size=5).tolist() == [0, 5, 3, 6, 7]
+
+    cluster = voxel_grid(pos, batch, size=5, start=-1, end=[18, 14])
+    assert cluster.tolist() == [0, 10, 4, 16, 17]
