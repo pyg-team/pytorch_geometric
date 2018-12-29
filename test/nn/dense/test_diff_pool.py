cd598ed39fc842a782776d44f748741e93be4384
rusty1s <matthias.fey@tu-dortmund.de>
2018-12-29 16:52:14 +0100

    rename tests, tests for diff pool

---
+++ test/nn/dense/test_diff_pool.py
@@ -0,0 +1,15 @@
+import torch
+from torch_geometric.nn import dense_diff_pool
+
+
+def test_dense_diff_pool():
+    batch_size, num_nodes, channels, num_clusters = (2, 20, 16, 10)
+    x = torch.randn((batch_size, num_nodes, channels))
+    adj = torch.rand((batch_size, num_nodes, num_nodes))
+    s = torch.randn((batch_size, num_nodes, num_clusters))
+    mask = torch.randint(0, 1, (batch_size, num_nodes), dtype=torch.uint8)
+
+    x, adj, reg = dense_diff_pool(x, adj, s, mask)
+    assert x.size() == (2, 10, 16)
+    assert adj.size() == (2, 10, 10)
+    assert reg.item() >= 0
