76c6f828c25dc52a5ec7c3b2f2b63d10b9b682b2
rusty1s <matthias.fey@tu-dortmund.de>
2018-12-29 20:23:53 +0100

    test face to edge

--- test/transforms/test_face_to_edge.py
+++ test/transforms/test_face_to_edge.py
@@ -7,9 +7,10 @@ def test_face_to_edge():
     assert FaceToEdge().__repr__() == 'FaceToEdge()'
 
     face = torch.tensor([[0, 0], [1, 1], [2, 3]])
+
     data = Data()
     data.face = face
-
-    row, col = FaceToEdge()(data).edge_index
-    assert row.tolist() == [0, 0, 0, 1, 1, 1, 2, 2, 3, 3]
-    assert col.tolist() == [1, 2, 3, 0, 2, 3, 0, 1, 0, 1]
+    data = FaceToEdge()(data)
+    assert len(data) == 1
+    assert data.edge_index.tolist() == [[0, 0, 0, 1, 1, 1, 2, 2, 3, 3],
+                                        [1, 2, 3, 0, 2, 3, 0, 1, 0, 1]]
