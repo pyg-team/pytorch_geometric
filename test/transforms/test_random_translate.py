8057e5c3d540c477e697059dfdd932ce02eff765
rusty1s <matthias.fey@tu-dortmund.de>
2018-12-29 19:45:17 +0100

    test random translate

---
+++ test/transforms/test_random_translate.py
@@ -0,0 +1,25 @@
+import torch
+from torch_geometric.transforms import RandomTranslate
+from torch_geometric.data import Data
+
+
+def test_random_translate():
+    assert RandomTranslate(0.1).__repr__() == 'RandomTranslate(0.1)'
+
+    pos = torch.Tensor([[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]])
+
+    data = Data(pos=pos)
+    data = RandomTranslate(0.1)(data)
+    assert len(data) == 1
+    assert data.pos.min().item() >= -0.1
+    assert data.pos.max().item() <= 0.1
+
+    data = Data(pos=pos)
+    data = RandomTranslate([0.1, 1, 0.1])(data)
+    assert len(data) == 1
+    assert data.pos[:, 0].min().item() >= -0.1
+    assert data.pos[:, 0].max().item() <= 0.1
+    assert data.pos[:, 1].min().item() >= -1
+    assert data.pos[:, 1].max().item() <= 1
+    assert data.pos[:, 2].min().item() >= -0.1
+    assert data.pos[:, 2].max().item() <= 0.1
