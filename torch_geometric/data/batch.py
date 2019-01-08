d8b655ab37d4f22b1efd1aa1291d5bebaef209b9
rusty1s <matthias.fey@tu-dortmund.de>
2019-01-08 16:45:36 +0100

    typo

--- torch_geometric/data/batch.py
+++ torch_geometric/data/batch.py
@@ -4,8 +4,8 @@ from torch_geometric.data import Data
 
 class Batch(Data):
     r"""A plain old python object modeling a batch of graphs as one big
-    (unconnected) graph. With :class:`torch_geometric.data.Data` being the base
-    class, all its methods can also be used here.
+    (dicconnected) graph. With :class:`torch_geometric.data.Data` being the
+    base class, all its methods can also be used here.
     In addition, single graphs can be reconstructed via the assignment vector
     :obj:`batch`, which maps each node to its respective graph identifier.
     """
