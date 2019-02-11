95b7c9d9a0ec50702bd1a97f7f07cc48fc1cf177
rusty1s <matthias.fey@tu-dortmund.de>
2019-02-11 11:28:44 +0100

    documentation

--- torch_geometric/datasets/coauthor.py
+++ torch_geometric/datasets/coauthor.py
@@ -4,6 +4,28 @@ from torch_geometric.read import read_npz
 
 
 class Coauthor(InMemoryDataset):
+    r"""The Coauthor CS and Coauthor Physics networks from the
+    `"Pitfalls of Graph Neural Network Evaluation"
+    <https://arxiv.org/abs/1811.05868>`_ paper.
+    Nodes represent authors that are connected by an edge if they co-authored a
+    paper.
+    Given paper keywords for each author's papers, the task is to map authors
+    to their respective field of study.
+
+    Args:
+        root (string): Root directory where the dataset should be saved.
+        name (string): The name of the dataset (:obj:`"CS"`,
+            :obj:`"Physics"`).
+        transform (callable, optional): A function/transform that takes in an
+            :obj:`torch_geometric.data.Data` object and returns a transformed
+            version. The data object will be transformed before every access.
+            (default: :obj:`None`)
+        pre_transform (callable, optional): A function/transform that takes in
+            an :obj:`torch_geometric.data.Data` object and returns a
+            transformed version. The data object will be transformed before
+            being saved to disk. (default: :obj:`None`)
+    """
+
     url = 'https://github.com/shchur/gnn-benchmark/raw/master/data/npz/'
 
     def __init__(self, root, name, transform=None, pre_transform=None):
