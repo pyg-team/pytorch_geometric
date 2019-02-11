78e11e19327d6046feabe91ea6c1a6c1b2ca1ec9
rusty1s <matthias.fey@tu-dortmund.de>
2019-02-11 08:56:06 +0100

    linting

--- torch_geometric/datasets/geometry.py
+++ torch_geometric/datasets/geometry.py
@@ -1,25 +1,25 @@
 import os
 import os.path as osp
 import glob
-from os.path import basename
 import torch
 from torch_geometric.data import InMemoryDataset, download_url, extract_zip
 from torch_geometric.read import read_off
 
-class GeometricShapes(InMemoryDataset):
 
+class GeometricShapes(InMemoryDataset):
     url = "https://drive.google.com/open?id=1VE-EMIPKWsxJTvFRacrY_YZHB1-fH9Mr"
 
     def __init__(self,
-             root,
-             train=True,
-             transform=None,
-             pre_transform=None,
-             pre_filter=None):
-        super(GeometricShapes, self).__init__(root,
-                                   transform=transform,
-                                   pre_transform=pre_transform,
-                                   pre_filter=pre_filter)
+                 root,
+                 train=True,
+                 transform=None,
+                 pre_transform=None,
+                 pre_filter=None):
+        super(GeometricShapes, self).__init__(
+            root,
+            transform=transform,
+            pre_transform=pre_transform,
+            pre_filter=pre_filter)
         path = self.processed_paths[0] if train else self.processed_paths[1]
         path = self.processed_paths[0]
         self.data, self.slices = torch.load(path)
@@ -33,7 +33,7 @@ class GeometricShapes(InMemoryDataset):
         return ['training.pt', 'test.pt']
 
     def download(self):
-        path = download_url(url, self.root)
+        path = download_url(self.url, self.root)
         extract_zip(path, self.root)
         os.unlink(path)
 
@@ -41,7 +41,6 @@ class GeometricShapes(InMemoryDataset):
         torch.save(self.process_set('train'), self.processed_paths[0])
         torch.save(self.process_set('test'), self.processed_paths[1])
         return
-
 
     def process_set(self, dataset):
         categories = glob.glob(osp.join(self.raw_dir, '*', ''))
@@ -63,7 +62,3 @@ class GeometricShapes(InMemoryDataset):
             data_list = [self.pre_transform(d) for d in data_list]
 
         return self.collate(data_list)
-
-    def __repr__(self):
-        return '{}()'.format(self.__class__.__name__)
-
--- torch_geometric/read/off.py
+++ torch_geometric/read/off.py
@@ -18,36 +18,29 @@ def parse_off(src):
 
     face = face_to_tri(face)
 
-
-
     data = Data(pos=pos)
     data.face = face
 
     return data
 
-def face_to_tri(face):
-    ### triangles to triangles list, as before
-    tri = [[float(x) for x in line.split(None)[1:]] for line in face if line[0] == '3']
 
-    ### squares to triangle list
-    first = [1,2,3]
-    second = [1,3,4]
-    rects = [
-           [float(x) for x in line.split(None)]
-        for line in face if line[0] == '4']
-    rects = [
-        [
-        [a[i] for i in first],
-        [a[i] for i in second]
-        ]
-        for a in rects]
+def face_to_tri(face):
+    # Triangles to triangles list.
+    tri = [[float(x) for x in line.split(None)[1:]] for line in face
+           if line[0] == '3']
+
+    # Squares to triangle list.
+    first = [1, 2, 3]
+    second = [1, 3, 4]
+    rects = [[float(x) for x in line.split(None)] for line in face
+             if line[0] == '4']
+    rects = [[[a[i] for i in first], [a[i] for i in second]] for a in rects]
     rects = [item for sublist in rects for item in sublist]
 
-
-    ### put all lists together, convert and return
+    # Put all lists together, convert and return.
     tri.extend(rects)
-    tensor = torch.tensor(tri, dtype=torch.long) 
-    
+    tensor = torch.tensor(tri, dtype=torch.long)
+
     return tensor.t().contiguous()
 
 
