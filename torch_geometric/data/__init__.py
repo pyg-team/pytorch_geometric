2e0c0f5df7c472fc6ec4d61712730065056aab7a
rusty1s <matthias.fey@tu-dortmund.de>
2019-03-01 10:15:43 +0100

    bitcoin otc dataset

--- torch_geometric/data/__init__.py
+++ torch_geometric/data/__init__.py
@@ -4,7 +4,7 @@ from .dataset import Dataset
 from .in_memory_dataset import InMemoryDataset
 from .dataloader import DataLoader, DataListLoader, DenseDataLoader
 from .download import download_url
-from .extract import extract_tar, extract_zip, extract_bz2
+from .extract import extract_tar, extract_zip, extract_bz2, extract_gz
 
 __all__ = [
     'Data',
@@ -18,4 +18,5 @@ __all__ = [
     'extract_tar',
     'extract_zip',
     'extract_bz2',
+    'extract_gz',
 ]
