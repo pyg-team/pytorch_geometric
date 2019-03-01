1210f169b0f97716afdcf939ec6a73b922f62b66
rusty1s <matthias.fey@tu-dortmund.de>
2019-03-01 10:16:09 +0100

    typo

--- torch_geometric/datasets/bitcoin_otc.py
+++ torch_geometric/datasets/bitcoin_otc.py
@@ -8,17 +8,10 @@ from torch_geometric.data import (InMemoryDataset, Data, download_url,
 
 
 class BitcoinOTC(InMemoryDataset):
-    r"""The QM9 dataset from the `"MoleculeNet: A Benchmark for Molecular
-    Machine Learning" <https://arxiv.org/abs/1703.00564>`_ paper, consisting of
-    about 130,000 molecules with 13 regression targets.
-    Each molecule includes complete spatial information for the single low
-    energy conformation of the atoms in the molecule.
-    In addition, we provide the atom features from the `"Neural Message
-    Passing for Quantum Chemistry" <https://arxiv.org/abs/1704.01212>`_ paper.
-
-    The Bitcoin-OTC dataset from the `"EvolveGCN: Evolving Graph Convolutional
-    Networks for Dynamic Graphs" <https://arxiv.org/abs/1902.10191>`_ paper,
-    consisting of 138 who-trusts-whom networks of sequential time steps.
+    r"""The Bitcoin-OTC dataset from the `"EvolveGCN: Evolving Graph
+    Convolutional Networks for Dynamic Graphs"
+    <https://arxiv.org/abs/1902.10191>`_ paper, consisting of 138
+    who-trusts-whom networks of sequential time steps.
 
     Args:
         root (string): Root directory where the dataset should be saved.
