b6b64661b83a99a677926f957cb71a09e461ff32
rusty1s <matthias.fey@tu-dortmund.de>
2019-03-18 14:10:17 +0100

    runtime benchmark scripts

---
+++ benchmark/runtime/gat.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import GATConv
+
+
+class GAT(torch.nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(GAT, self).__init__()
+        self.conv1 = GATConv(in_channels, 8, heads=8, dropout=0.6)
+        self.conv2 = GATConv(8 * 8, out_channels, dropout=0.6)
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+        x = F.dropout(x, p=0.6, training=self.training)
+        x = F.elu(self.conv1(x, edge_index))
+        x = F.dropout(x, p=0.6, training=self.training)
+        x = self.conv2(x, edge_index)
+        return F.log_softmax(x, dim=1)
---
+++ benchmark/runtime/main.py
@@ -0,0 +1,27 @@
+import os.path as osp
+from itertools import product
+
+import torch
+from runtime import train_runtime
+from runtime.gcn import GCN
+from runtime.gat import GAT
+from runtime.rgcn import RGCN
+
+from torch_geometric.datasets import Planetoid, Entities
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+root = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data')
+Cora = Planetoid(osp.join(root, 'Cora'), 'Cora')
+CiteSeer = Planetoid(osp.join(root, 'CiteSeer'), 'CiteSeer')
+PubMed = Planetoid(osp.join(root, 'PubMed'), 'PubMed')
+MUTAG = Entities(osp.join(root, 'MUTAG'), 'MUTAG')
+
+for d, Net in product([Cora, CiteSeer, PubMed], [GCN, GAT]):
+    model = Net(d.num_features, d.num_classes)
+    t = train_runtime(model, d[0], epochs=200, device=device)
+    print('{} - {}: {:.2f}s'.format(d.__repr__()[:-2], Net.__name__, t))
+
+for d, Net in product([MUTAG], [RGCN]):
+    model = Net(d[0].num_nodes, d.num_classes, d.num_relations)
+    t = train_runtime(model, d[0], epochs=50, device=device)
+    print('{} - {}: {:.2f}s'.format(d.__repr__()[:-2], Net.__name__, t))
