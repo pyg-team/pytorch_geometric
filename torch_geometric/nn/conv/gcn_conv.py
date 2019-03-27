b62aee4a8ed3420be90a1aea6f90345992a3b858
rusty1s <matthias.fey@tu-dortmund.de>
2019-03-27 15:05:26 +0100

    fixed self-loop bug in gcn

--- torch_geometric/nn/conv/gcn_conv.py
+++ torch_geometric/nn/conv/gcn_conv.py
@@ -65,18 +65,18 @@ class GCNConv(MessagePassing):
     @staticmethod
     def norm(edge_index, num_nodes, edge_weight, improved=False, dtype=None):
         if edge_weight is None:
-            edge_weight = torch.ones(
-                (edge_index.size(1), ), dtype=dtype, device=edge_index.device)
+            edge_weight = torch.ones((edge_index.size(1), ),
+                                     dtype=dtype,
+                                     device=edge_index.device)
         edge_weight = edge_weight.view(-1)
         assert edge_weight.size(0) == edge_index.size(1)
 
-        edge_index, _ = remove_self_loops(edge_index)
+        edge_index, edge_weight = remove_self_loops(edge_index, edge_weight)
         edge_index = add_self_loops(edge_index, num_nodes)
-        loop_weight = torch.full(
-            (num_nodes, ),
-            1 if not improved else 2,
-            dtype=edge_weight.dtype,
-            device=edge_weight.device)
+        loop_weight = torch.full((num_nodes, ),
+                                 1 if not improved else 2,
+                                 dtype=edge_weight.dtype,
+                                 device=edge_weight.device)
         edge_weight = torch.cat([edge_weight, loop_weight], dim=0)
 
         row, col = edge_index
@@ -91,8 +91,7 @@ class GCNConv(MessagePassing):
         x = torch.matmul(x, self.weight)
 
         if not self.cached or self.cached_result is None:
-            edge_index, norm = GCNConv.norm(edge_index,
-                                            x.size(0), edge_weight,
+            edge_index, norm = GCNConv.norm(edge_index, x.size(0), edge_weight,
                                             self.improved, x.dtype)
             self.cached_result = edge_index, norm
 
