42b7428c5e1953ef64ec49731d1c39065dbd0e7d
rusty1s <matthias.fey@tu-dortmund.de>
2019-03-28 14:13:21 +0100

    clean up + doc

--- test/transforms/test_line_graph.py
+++ test/transforms/test_line_graph.py
@@ -4,33 +4,20 @@ from torch_geometric.data import Data
 
 
 def test_line_graph():
-    assert LineGraph().__repr__() == 'LineGraph'
+    assert LineGraph().__repr__() == 'LineGraph()'
 
-
-def test_line_graph_directed():
-    edge_index = torch.tensor([[0, 1, 2, 2, 3],
-                               [1, 2, 0, 3, 0]], dtype=torch.long)
-
-    expected_row = [0, 1, 1, 2, 3, 4]
-    expected_col = [1, 2, 3, 0, 4, 0]
-
+    # Directed.
+    edge_index = torch.tensor([[0, 1, 2, 2, 3], [1, 2, 0, 3, 0]])
     data = Data(edge_index=edge_index)
     data = LineGraph()(data)
-    print(data.edge_index[0].tolist())
-    print(data.edge_index[1].tolist())
-    assert data.edge_index[0].tolist() == expected_row
-    assert data.edge_index[1].tolist() == expected_col
+    assert data.edge_index.tolist() == [[0, 1, 1, 2, 3, 4], [1, 2, 3, 0, 4, 0]]
 
-
-def test_line_graph_undirected():
+    # Undirected.
     edge_index = torch.tensor([[0, 1, 1, 2, 2, 0, 2, 3, 3, 0],
-                               [1, 0, 2, 1, 0, 2, 3, 2, 0, 3]],
-                              dtype=torch.long)
-
-    expected_row = [0, 1, 0, 2, 0, 4, 1, 2, 1, 3, 2, 3, 2, 4, 3, 4]
-    expected_col = [1, 0, 2, 0, 4, 0, 2, 1, 3, 1, 3, 2, 4, 2, 4, 3]
-
+                               [1, 0, 2, 1, 0, 2, 3, 2, 0, 3]])
     data = Data(edge_index=edge_index)
     data = LineGraph()(data)
-    assert data.edge_index[0].tolist() == expected_row
-    assert data.edge_index[1].tolist() == expected_col
+    assert data.edge_index.tolist() == [
+        [0, 1, 0, 2, 0, 4, 1, 2, 1, 3, 2, 3, 2, 4, 3, 4],
+        [1, 0, 2, 0, 4, 0, 2, 1, 3, 1, 3, 2, 4, 2, 4, 3],
+    ]
