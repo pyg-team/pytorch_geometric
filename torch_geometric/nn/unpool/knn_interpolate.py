fixed formula

torch_geometric/nn/unpool/knn_interpolate.py
@@ -7,11 +7,13 @@ def knn_interpolate(x, pos_x, pos_y, batch_x=None, batch_y=None, k=3):
     r"""The k-NN interpolation from the `"PointNet++: Deep Hierarchical
     Feature Learning on Point Sets in a Metric Space"
     <https://arxiv.org/abs/1706.02413>`_ paper.
-    For each point :obj:`y`, its interpolated features are given by
+    For each point :math:`y` with position :math:`\mathbf{p}(y)`, its
+    interpolated features :math:`\mathbf{f}(y)` are given by
 
     .. math::
-        f(\mathbf{x}) = \frac{\sum_{i=1}^k w_i(y) f_i}{\sum_{i=1}^k w_i(y)}
-        \textrm{, where } w_i(y) = \frac{1}{d(y, x_i)^2}.
+        \mathbf{f}(y) = \frac{\sum_{i=1}^k w(x_i) \mathbf{f}(x_i)}{\sum_{i=1}^k
+        w(x_i)} \textrm{, where } w(x_i) = \frac{1}{d(\mathbf{p}(y),
+        \mathbf{p}(x_i))^2}.
 
     Args:
         x (Tensor): Node feature matrix
@@ -29,19 +31,14 @@ def knn_interpolate(x, pos_x, pos_y, batch_x=None, batch_y=None, k=3):
             each node from :math:`\mathbf{Y}` to a specific example.
             (default: :obj:`None`)
         k (int, optional): Number of neighbors. (default: :obj:`3`)
-
-    :rtype: :class:`Tensor`
     """
 
     with torch.no_grad():
         y_idx, x_idx = knn(pos_x, pos_y, k, batch_x=batch_x, batch_y=batch_y)
         diff = pos_x[x_idx] - pos_y[y_idx]
         squared_distance = (diff * diff).sum(dim=-1, keepdim=True)
-        print(squared_distance)
         weights = 1.0 / torch.clamp(squared_distance, min=1e-16)
 
-    # print(weights)
-    print(x)
     y = scatter_add(x[x_idx] * weights, y_idx, dim=0, dim_size=pos_y.size(0))
     y = y / scatter_add(weights, y_idx, dim=0, dim_size=pos_y.size(0))
 