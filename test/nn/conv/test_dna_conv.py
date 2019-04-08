03905dc5734ea1056aca64bafdbf75ab8f903a38
rusty1s <matthias.fey@tu-dortmund.de>
2019-04-08 11:54:58 +0200

    linting fix

--- test/nn/conv/test_dna_conv.py
+++ test/nn/conv/test_dna_conv.py
@@ -12,12 +12,3 @@ def test_dna_conv():
     conv = DNAConv(channels, heads=4, groups=8, dropout=0.5)
     assert conv.__repr__() == 'DNAConv(32, heads=4, groups=8)'
     assert conv(x, edge_index).size() == (num_nodes, channels)
-
-
-#     edge_weight = torch.rand(edge_index.size(1))
-#     x = torch.randn((num_nodes, in_channels))
-
-#     conv = GCNConv(in_channels, out_channels)
-#     assert conv.__repr__() == 'GCNConv(16, 32)'
-#     assert conv(x, edge_index).size() == (num_nodes, out_channels)
-#     assert conv(x, edge_index, edge_weight).size() == (num_nodes, out_channels)
