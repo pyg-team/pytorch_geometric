7a04f05c70da1b70c55cd9b7587d3ccb666d32c1
rusty1s <matthias.fey@tu-dortmund.de>
2019-04-08 12:08:16 +0200

    fixed dropout random seed

--- test/utils/test_dropout.py
+++ test/utils/test_dropout.py
@@ -13,9 +13,9 @@ def test_dropout_adj():
     assert edge_attr.tolist() == out[1].tolist()
 
     out = dropout_adj(edge_index, edge_attr)
-    assert out[0].tolist() == [[1, 3], [0, 2]]
-    assert out[1].tolist() == [2, 6]
+    assert out[0].tolist() == [[0, 1, 2, 3], [1, 2, 1, 2]]
+    assert out[1].tolist() == [1, 3, 4, 6]
 
     out = dropout_adj(edge_index, edge_attr, force_undirected=True)
-    assert out[0].tolist() == [[0, 1, 2, 3], [1, 0, 3, 2]]
-    assert out[1].tolist() == [1, 1, 5, 5]
+    assert out[0].tolist() == [[2, 3], [3, 2]]
+    assert out[1].tolist() == [5, 5]
