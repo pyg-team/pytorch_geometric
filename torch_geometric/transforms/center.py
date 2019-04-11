82025e5219c27aa801c579ae2cb8dd662a61be85
rusty1s <matthias.fey@tu-dortmund.de>
2019-04-11 12:39:23 +0200

    fix for different pos shapes

--- torch_geometric/transforms/center.py
+++ torch_geometric/transforms/center.py
@@ -2,7 +2,7 @@ class Center(object):
     r"""Centers node positions around the origin."""
 
     def __call__(self, data):
-        data.pos = data.pos - data.pos.mean(dim=0, keepdim=True)
+        data.pos = data.pos - data.pos.mean(dim=-2, keepdim=True)
         return data
 
     def __repr__(self):
