4afff45e511e4df67dd6c5cf0ee2d064b79b1d8c
rusty1s <matthias.fey@tu-dortmund.de>
2019-04-12 14:13:07 +0200

    temporal datasets

---
+++ torch_geometric/datasets/icews.py
@@ -0,0 +1,98 @@
+import torch
+from torch_geometric.data import InMemoryDataset, download_url, Data
+from torch_geometric.read import read_txt_array
+
+
+class TemporalDataset(InMemoryDataset):
+    url = 'https://github.com/INK-USC/RENet/raw/master/data'
+    names = ['ICEWS18', 'GDELT']
+    splits = ['train', 'val', 'test']
+
+    def __init__(self,
+                 root,
+                 name,
+                 split='train',
+                 transform=None,
+                 pre_transform=None,
+                 pre_filter=None):
+        assert name in self.names
+        assert split in self.splits
+        self.name = name
+        super(TemporalDataset, self).__init__(root, transform, pre_transform,
+                                              pre_filter)
+        path = self.processed_paths[self.splits.index(split)]
+        self.data, self.slices = torch.load(path)
+
+    @property
+    def raw_file_names(self):
+        return ['{}.txt'.format(name) for name in ['train', 'valid', 'test']]
+
+    @property
+    def processed_file_names(self):
+        return ['train.pt', 'val.pt', 'test.pt']
+
+    def download(self):
+        for filename in self.raw_file_names:
+            url = '{}/{}/{}'.format(self.url, self.name, filename)
+            download_url(url, self.raw_dir)
+
+    def process(self):
+        for r_path, p_path in zip(self.raw_paths, self.processed_paths):
+            srot = read_txt_array(r_path, sep='\t', end=4, dtype=torch.long)
+            row, rel, col, time = srot.t().contiguous()
+
+            count = time.bincount()
+            split_sections = count[count > 0].tolist()
+
+            rows = row.split(split_sections)
+            cols = col.split(split_sections)
+            rels = rel.split(split_sections)
+            times = time.split(split_sections)
+
+            data_list = []
+            for row, col, rel, time in zip(rows, cols, rels, times):
+                edge_index = torch.stack([row, col], dim=0)
+                data = Data(edge_index=edge_index, edge_attr=rel)
+                data.time = time
+                if self.pre_filter is not None and not self.pre_filter(data):
+                    continue
+                if self.pre_transform is not None:
+                    data = self.pre_transform(data)
+                data_list.append(data)
+
+            torch.save(self.collate(data_list), p_path)
+
+
+class ICEWS(TemporalDataset):
+    r"""The Integrated Crisis Early Warning System (ICEWS) dataset used in
+    the, *e.g.*, `"Recurrent Event Network for Reasoning over Temporal
+    Knowledge Graphs" <https://arxiv.org/abs/1904.05530>`_ paper, consisting of
+    events collected from 1/1/2014 to 12/31/2014 (24 hours time granularity).
+
+    Args:
+        root (string): Root directory where the dataset should be saved.
+        split (string): If :obj:`"train"`, loads the training dataset.
+            If :obj:`"val"`, loads the validation dataset.
+            If :obj:`"test"`, loads the test dataset. (default: :obj:`"train"`)
+        transform (callable, optional): A function/transform that takes in an
+            :obj:`torch_geometric.data.Data` object and returns a transformed
+            version. The data object will be transformed before every access.
+            (default: :obj:`None`)
+        pre_transform (callable, optional): A function/transform that takes in
+            an :obj:`torch_geometric.data.Data` object and returns a
+            transformed version. The data object will be transformed before
+            being saved to disk. (default: :obj:`None`)
+        pre_filter (callable, optional): A function that takes in an
+            :obj:`torch_geometric.data.Data` object and returns a boolean
+            value, indicating whether the data object should be included in the
+            final dataset. (default: :obj:`None`)
+    """
+
+    def __init__(self,
+                 root,
+                 split='train',
+                 transform=None,
+                 pre_transform=None,
+                 pre_filter=None):
+        super(ICEWS, self).__init__(root, 'ICEWS18', split, transform,
+                                    pre_transform, pre_filter)
