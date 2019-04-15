ee003b4f46138de25c12b28968544b020c60b993
rusty1s <matthias.fey@tu-dortmund.de>
2019-04-15 10:11:40 +0200

    follow batch argument

--- torch_geometric/data/batch.py
+++ torch_geometric/data/batch.py
@@ -15,10 +15,12 @@ class Batch(Data):
         self.batch = batch
 
     @staticmethod
-    def from_data_list(data_list):
+    def from_data_list(data_list, follow_batch=[]):
         r"""Constructs a batch object from a python list holding
         :class:`torch_geometric.data.Data` objects.
-        The assignment vector :obj:`batch` is created on the fly."""
+        The assignment vector :obj:`batch` is created on the fly.
+        Additionally, creates assignment batch vectors for each key in
+        :obj:`follow_batch`."""
         keys = [set(data.keys) for data in data_list]
         keys = list(set.union(*keys))
         assert 'batch' not in keys
@@ -27,6 +29,10 @@ class Batch(Data):
 
         for key in keys:
             batch[key] = []
+
+        for key in follow_batch:
+            batch['batch_{}'.format(key)] = []
+
         batch.batch = []
 
         cumsum = 0
@@ -36,13 +42,21 @@ class Batch(Data):
                 item = item + cumsum if data.__cumsum__(key, item) else item
                 batch[key].append(item)
 
+            for key in follow_batch:
+                size = data[key].size(data.__cat_dim__(key, data[key]))
+                item = torch.full((size, ), i, dtype=torch.long)
+                batch['batch_{}'.format(key)].append(item)
+
             num_nodes = data.num_nodes
             if num_nodes is not None:
-                batch.batch.append(
-                    torch.full((num_nodes, ), i, dtype=torch.long))
+                item = torch.full((num_nodes, ), i, dtype=torch.long)
+                batch.batch.append(item)
                 cumsum += num_nodes
 
-        for key in keys:
+        if num_nodes is None:
+            batch.batch = None
+
+        for key in batch.keys:
             item = batch[key][0]
             if torch.is_tensor(item):
                 batch[key] = torch.cat(
@@ -51,11 +65,6 @@ class Batch(Data):
                 batch[key] = torch.tensor(batch[key])
             else:
                 raise ValueError('Unsupported attribute type.')
-
-        if num_nodes is not None:
-            batch.batch = torch.cat(batch.batch, dim=-1)
-        else:
-            batch.batch = None
 
         return batch.contiguous()
 
