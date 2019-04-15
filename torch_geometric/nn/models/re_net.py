e3757cfe3b4823f05680386b6c3accd6bed98121
rusty1s <matthias.fey@tu-dortmund.de>
2019-04-15 10:54:00 +0200

    first renet impl

--- torch_geometric/nn/models/re_net.py
+++ torch_geometric/nn/models/re_net.py
@@ -1,7 +1,9 @@
 import math
 
 import torch
-from torch.nn import Parameter
+from torch.nn import Parameter, GRU, Linear
+import torch.nn.functional as F
+from torch_scatter import scatter_mean
 
 
 class RENet(torch.nn.Module):
@@ -9,29 +11,145 @@ class RENet(torch.nn.Module):
                  num_nodes,
                  num_rels,
                  hidden_channels,
-                 aggr='mean',
                  seq_len=10,
-                 dropout=0):
+                 num_layers=1,
+                 dropout=0,
+                 bias=True):
         super(RENet, self).__init__()
 
         self.num_nodes = num_nodes
         self.hidden_channels = hidden_channels
         self.num_rels = num_rels
-        self.aggr = aggr
         self.seq_len = seq_len
         self.dropout = dropout
 
-        self.obj = Parameter(torch.Tensor(num_nodes, hidden_channels))
+        self.ent = Parameter(torch.Tensor(num_nodes, hidden_channels))
         self.rel = Parameter(torch.Tensor(num_rels, hidden_channels))
+
+        self.sub_gru = GRU(
+            3 * hidden_channels,
+            hidden_channels,
+            num_layers,
+            batch_first=True,
+            bias=bias)
+        self.obj_gru = GRU(
+            3 * hidden_channels,
+            hidden_channels,
+            num_layers,
+            batch_first=True,
+            bias=bias)
+
+        self.sub_lin = Linear(3 * hidden_channels, num_nodes, bias=bias)
+        self.obj_lin = Linear(3 * hidden_channels, num_nodes, bias=bias)
 
         self.reset_parameters()
 
     def reset_parameters(self):
-        torch.nn.init.xavier_uniform_(self.obj, gain=math.sqrt(2.0))
+        torch.nn.init.xavier_uniform_(self.ent, gain=math.sqrt(2.0))
         torch.nn.init.xavier_uniform_(self.rel, gain=math.sqrt(2.0))
 
-    def precompute_history(self, dataset):
-        pass
+        self.sub_gru.reset_parameters()
+        self.obj_gru.reset_parameters()
+        self.sub_lin.reset_parameters()
+        self.obj_lin.reset_parameters()
 
-    def forward(self, edge_index, rel, history):
-        pass
+    @staticmethod
+    def pre_transform(seq_len):
+        class PreTransform(object):
+            def __init__(self, seq_len):
+                self.seq_len = seq_len
+                self.inc = 5000
+                self.t_last = 0
+                self.sub_hist = self.increase_hist_node_size([])
+                self.obj_hist = self.increase_hist_node_size([])
+
+            def increase_hist_node_size(self, hist):
+                hist_inc = torch.zeros((self.inc, self.seq_len, 0))
+                return hist + hist_inc.tolist()
+
+            def get_history(self, hist, node, rel):
+                hists, ts = [], []
+                for s in range(seq_len):
+                    h = hist[node][s]
+                    hists += h
+                    ts.append(torch.full((len(h), ), s, dtype=torch.long))
+                node, rels = torch.tensor(
+                    hists, dtype=torch.long).view(-1, 2).t().contiguous()
+                node = node[rels == rel]
+                t = torch.cat(ts, dim=0)[rels == rel]
+                return node, t
+
+            def step(self, hist):
+                for i in range(len(hist)):
+                    hist[i] = hist[i][1:]
+                    hist[i].append([])
+                return hist
+
+            def __call__(self, data):
+                sub, rel, obj, t = data.sub, data.rel, data.obj, data.t
+
+                if max(sub, obj) + 1 > len(self.sub_hist):
+                    self.sub_hist = self.increase_hist_node_size(self.sub_hist)
+                    self.obj_hist = self.increase_hist_node_size(self.obj_hist)
+
+                # Save history in data object.
+                data.h_sub, data.h_sub_t = self.get_history(
+                    self.sub_hist, sub, rel)
+                data.h_obj, data.h_obj_t = self.get_history(
+                    self.obj_hist, obj, rel)
+
+                # Delete last timestamp in history.
+                if t > self.t_last:
+                    self.sub_hist = self.step(self.sub_hist)
+                    self.obj_hist = self.step(self.obj_hist)
+                    self.t_last = t
+
+                # Add new event to history.
+                self.sub_hist[sub][-1].append([obj, rel])
+                self.obj_hist[obj][-1].append([sub, rel])
+
+                return data
+
+            def __repr__(self):
+                return '{}(seq_len={})'.format(self.__class__.__name__,
+                                               self.seq_len)
+
+        return PreTransform(seq_len)
+
+    def forward(self, data):
+        batch_size, seq_len = data.sub.size(0), self.seq_len
+
+        h_sub_t = data.h_sub_t + data.h_sub_batch * seq_len
+        h_obj_t = data.h_obj_t + data.h_obj_batch * seq_len
+
+        h_sub = scatter_mean(
+            self.ent[data.h_sub],
+            h_sub_t,
+            dim=0,
+            dim_size=batch_size * seq_len).view(batch_size, seq_len, -1)
+        h_obj = scatter_mean(
+            self.ent[data.h_obj],
+            h_obj_t,
+            dim=0,
+            dim_size=batch_size * seq_len).view(batch_size, seq_len, -1)
+
+        sub = self.ent[data.sub].unsqueeze(1).repeat(1, seq_len, 1)
+        rel = self.rel[data.rel].unsqueeze(1).repeat(1, seq_len, 1)
+        obj = self.ent[data.obj].unsqueeze(1).repeat(1, seq_len, 1)
+
+        _, h_sub = self.sub_gru(torch.cat([sub, h_sub, rel], dim=-1))
+        _, h_obj = self.obj_gru(torch.cat([obj, h_obj, rel], dim=-1))
+        h_sub, h_obj = h_sub.squeeze(0), h_obj.squeeze(0)
+
+        h_sub = torch.cat([self.ent[data.sub], h_sub, self.rel[data.rel]],
+                          dim=-1)
+        h_obj = torch.cat([self.ent[data.obj], h_obj, self.rel[data.rel]],
+                          dim=-1)
+
+        h_sub = F.dropout(h_sub, p=self.dropout, training=self.training)
+        h_obj = F.dropout(h_obj, p=self.dropout, training=self.training)
+
+        pred_sub = F.log_softmax(self.sub_lin(h_sub), dim=1)
+        pred_obj = F.log_softmax(self.obj_lin(h_obj), dim=1)
+
+        return pred_sub, pred_obj
