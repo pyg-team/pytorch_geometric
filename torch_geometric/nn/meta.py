3bef2a3d5f3d2afd04ad20f15cfa4f3225f4265f
rusty1s <matthias.fey@tu-dortmund.de>
2019-04-17 18:34:00 +0200

    update meta

--- torch_geometric/nn/meta.py
+++ torch_geometric/nn/meta.py
@@ -16,6 +16,10 @@ class MetaLayer(torch.nn.Module):
     calling the functions :obj:`edge_model`, :obj:`node_model` and
     :obj:`global_model`, respectively.
 
+    To allow for batch-wise graph processing, all callable functions take an
+    additional argument :obj:`batch`, which determines the assignment of
+    edges or nodes to their specific graphs.
+
     Args:
         edge_model (func, optional): A callable which updates a graph's edge
             features based on its source and target node features, its current
@@ -26,41 +30,42 @@ class MetaLayer(torch.nn.Module):
             (default: :obj:`None`)
         global_model (func, optional): A callable which updates a graph's
             global features based on its node features, its graph connectivity,
-            its edge features and its current global features. To allow for
-            batch-wise graph processing, the callable takes a fifth argument
-            :obj:`batch`, which determines the assignment of nodes to their
-            specific graphs. (default: :obj:`None`)
-
+            its edge features and its current global features.
     .. code-block:: python
 
         from torch.nn import Sequential as Seq, Linear as Lin, ReLU
         from torch_scatter import scatter_mean
         from torch_geometric.nn import MetaLayer
 
-        class Net(torch.nn.Module):
+        class MyLayer(torch.nn.Module):
             def __init__(self):
-                super(Net, self).__init__()
+                super(MyLayer, self).__init__()
 
                 self.edge_mlp = Seq(Lin(..., ...), ReLU(), Lin(..., ...))
-                self.node_mlp = Seq(Lin(..., ...), ReLU(), Lin(..., ...))
+                self.node_mlp_1 = Seq(Lin(..., ...), ReLU(), Lin(..., ...))
+                self.node_mlp_2 = Seq(Lin(..., ...), ReLU(), Lin(..., ...))
                 self.global_mlp = Seq(Lin(..., ...), ReLU(), Lin(..., ...))
 
-                def edge_model(source, target, edge_attr, u):
+                def edge_model(src, dest, edge_attr, u, batch):
                     # source, target: [E, F_x], where E is the number of edges.
                     # edge_attr: [E, F_e]
                     # u: [B, F_u], where B is the number of graphs.
-                    out = torch.cat([source, target, edge_attr], dim=1)
+                    # batch: [E] with max entry B - 1.
+                    out = torch.cat([src, dest, edge_attr, u[batch]], 1)
                     return self.edge_mlp(out)
 
-                def node_model(x, edge_index, edge_attr, u):
+                def node_model(x, edge_index, edge_attr, u, batch):
                     # x: [N, F_x], where N is the number of nodes.
                     # edge_index: [2, E] with max entry N - 1.
                     # edge_attr: [E, F_e]
                     # u: [B, F_u]
+                    # batch: [N] with max entry B - 1.
                     row, col = edge_index
                     out = torch.cat([x[col], edge_attr], dim=1)
                     out = self.node_mlp(out)
-                    return scatter_mean(out, row, dim=0, dim_size=x.size(0))
+                    out = scatter_mean(out, row, dim=0, dim_size=x.size(0))
+                    out = torch.cat([out, u[batch]], dim=1)
+                    return self.node_mlp_2(out)
 
                 def global_model(x, edge_index, edge_attr, u, batch):
                     # x: [N, F_x], where N is the number of nodes.
@@ -95,10 +100,11 @@ class MetaLayer(torch.nn.Module):
         row, col = edge_index
 
         if self.edge_model is not None:
-            edge_attr = self.edge_model(x[row], x[col], edge_attr, u)
+            edge_attr = self.edge_model(x[row], x[col], edge_attr, u,
+                                        batch[row])
 
         if self.node_model is not None:
-            x = self.node_model(x, edge_index, edge_attr, u)
+            x = self.node_model(x, edge_index, edge_attr, u, batch)
 
         if self.global_model is not None:
             u = self.global_model(x, edge_index, edge_attr, u, batch)
