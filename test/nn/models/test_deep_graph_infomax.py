fb8a67e3619b7da106f5212c524eb0b70e7abd32
rusty1s <matthias.fey@tu-dortmund.de>
2019-04-19 13:04:53 +0200

    test cases

---
+++ test/nn/models/test_deep_graph_infomax.py
@@ -0,0 +1,29 @@
+import torch
+from torch_geometric.nn import DeepGraphInfomax
+
+
+def test_deep_graph_infomax():
+    def corruption(z):
+        return z + 1
+
+    model = DeepGraphInfomax(
+        hidden_channels=16,
+        encoder=lambda x: x,
+        summary=lambda z, *args: z.mean(dim=0),
+        corruption=lambda x: x + 1)
+
+    assert model.__repr__() == 'DeepGraphInfomax(16)'
+
+    x = torch.ones(20, 16)
+
+    pos_z, neg_z, summary = model(x)
+    assert pos_z.size() == (20, 16) and neg_z.size() == (20, 16)
+    assert summary.size() == (16, )
+
+    loss = model.loss(pos_z, neg_z, summary)
+    assert 0 <= loss.item()
+
+    acc = model.test(
+        torch.ones(20, 16), torch.randint(10, (20, )), torch.ones(20, 16),
+        torch.randint(10, (20, )))
+    assert 0 <= acc and acc <= 1
