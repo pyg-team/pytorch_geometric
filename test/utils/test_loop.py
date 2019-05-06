bb2f83a04bfb83acbfb071e17240673843658582
rusty1s <matthias.fey@tu-dortmund.de>
2019-05-06 05:17:51 -0500

    added remaining self loops

--- test/utils/test_loop.py
+++ test/utils/test_loop.py
@@ -1,41 +1,42 @@
 import torch
 from torch_geometric.utils import (contains_self_loops, remove_self_loops,
-                                   add_self_loops)
+                                   add_self_loops, add_remaining_self_loops)
 
 
 def test_contains_self_loops():
-    row = torch.tensor([0, 1, 0])
-    col = torch.tensor([1, 0, 0])
+    edge_index = torch.tensor([[0, 1, 0], [1, 0, 0]])
+    assert contains_self_loops(edge_index)
 
-    assert contains_self_loops(torch.stack([row, col], dim=0))
-
-    row = torch.tensor([0, 1, 1])
-    col = torch.tensor([1, 0, 2])
-
-    assert not contains_self_loops(torch.stack([row, col], dim=0))
+    edge_index = torch.tensor([[0, 1, 1], [1, 0, 2]])
+    assert not contains_self_loops(edge_index)
 
 
 def test_remove_self_loops():
-    row = torch.tensor([1, 0, 1, 0, 2, 1])
-    col = torch.tensor([0, 1, 1, 1, 2, 0])
-    edge_index = torch.stack([row, col], dim=0)
-    edge_attr = [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12]]
-    edge_attr = torch.tensor(edge_attr)
+    edge_index = torch.tensor([[0, 1, 0], [1, 0, 0]])
+    edge_attr = [[1, 2], [3, 4], [5, 6]]
+    edge_attr = torch.Tensor(edge_attr)
 
-    out = remove_self_loops(edge_index, edge_attr)
-    assert out[0].tolist() == [[1, 0, 0, 1], [0, 1, 1, 0]]
-    assert out[1].tolist() == [[1, 2], [3, 4], [7, 8], [11, 12]]
+    edge_index, edge_attr = remove_self_loops(edge_index, edge_attr)
+    assert edge_index.tolist() == [[0, 1], [1, 0]]
+    assert edge_attr.tolist() == [[1, 2], [3, 4]]
 
 
 def test_add_self_loops():
-    row = torch.tensor([0, 1, 0])
-    col = torch.tensor([1, 0, 0])
-    edge_index = torch.stack([row, col], dim=0)
+    edge_index = torch.tensor([[0, 1, 0], [1, 0, 0]])
 
     expected = [[0, 1, 0, 0, 1], [1, 0, 0, 0, 1]]
     assert add_self_loops(edge_index)[0].tolist() == expected
 
-    edge_weight = torch.Tensor([0.5, 0.5, 0.5])
+    edge_weight = torch.tensor([0.5, 0.5, 0.5])
     edge_index, edge_weight = add_self_loops(edge_index, edge_weight)
     assert edge_index.tolist() == expected
     assert edge_weight.tolist() == [0.5, 0.5, 0.5, 1, 1]
+
+
+def test_add_remaining_self_loops():
+    edge_index = torch.tensor([[0, 1, 0], [1, 0, 0]])
+    edge_weight = torch.tensor([0.5, 0.5, 0.5])
+
+    edge_index, edge_weight = add_remaining_self_loops(edge_index, edge_weight)
+    assert edge_index.tolist() == [[0, 1, 0, 1], [1, 0, 0, 1]]
+    assert edge_weight.tolist() == [0.5, 0.5, 0.5, 1]
--- torch_geometric/utils/loop.py
+++ torch_geometric/utils/loop.py
@@ -31,8 +31,7 @@ def remove_self_loops(edge_index, edge_attr=None):
     row, col = edge_index
     mask = row != col
     edge_attr = edge_attr if edge_attr is None else edge_attr[mask]
-    mask = mask.unsqueeze(0).expand_as(edge_index)
-    edge_index = edge_index[mask].view(2, -1)
+    edge_index = edge_index[:, mask]
 
     return edge_index, edge_attr
 
@@ -40,6 +39,8 @@ def remove_self_loops(edge_index, edge_attr=None):
 def add_self_loops(edge_index, edge_weight=None, fill_value=1, num_nodes=None):
     r"""Adds a self-loop :math:`(i,i) \in \mathcal{E}` to every node
     :math:`i \in \mathcal{V}` in the graph given by :attr:`edge_index`.
+    In case the graph is weighted, all existent self-loops will be removed and
+    replaced by weights denoted by :obj:`fill_value`.
 
     Args:
         edge_index (LongTensor): The edge indices.
@@ -55,15 +56,65 @@ def add_self_loops(edge_index, edge_weight=None, fill_value=1, num_nodes=None):
     """
     num_nodes = maybe_num_nodes(edge_index, num_nodes)
 
-    dtype, device = edge_index.dtype, edge_index.device
-    loop = torch.arange(0, num_nodes, dtype=dtype, device=device)
-    loop = loop.unsqueeze(0).repeat(2, 1)
+    loop_index = torch.arange(0,
+                              num_nodes,
+                              dtype=torch.long,
+                              device=edge_index.device)
+    loop_index = loop_index.unsqueeze(0).repeat(2, 1)
 
     if edge_weight is not None:
         assert edge_weight.numel() == edge_index.size(1)
         loop_weight = edge_weight.new_full((num_nodes, ), fill_value)
         edge_weight = torch.cat([edge_weight, loop_weight], dim=0)
 
-    edge_index = torch.cat([edge_index, loop], dim=1)
+    edge_index = torch.cat([edge_index, loop_index], dim=1)
+
+    return edge_index, edge_weight
+
+
+def add_remaining_self_loops(edge_index,
+                             edge_weight=None,
+                             fill_value=1,
+                             num_nodes=None):
+    r"""Adds remaining self-loop :math:`(i,i) \in \mathcal{E}` to every node
+    :math:`i \in \mathcal{V}` in the graph given by :attr:`edge_index`.
+    In case the graph is weighted and already contains a few self-loops, only
+    non-existent self-loops will be added with weights denoted by
+    :obj:`fill_value`.
+
+    Args:
+        edge_index (LongTensor): The edge indices.
+        edge_weight (Tensor, optional): One-dimensional edge weights.
+            (default: :obj:`None`)
+        fill_value (int, optional): If :obj:`edge_weight` is not :obj:`None`,
+            will add self-loops with edge weights of :obj:`fill_value` to the
+            graph. (default: :obj:`1`)
+        num_nodes (int, optional): The number of nodes, *i.e.*
+            :obj:`max_val + 1` of :attr:`edge_index`. (default: :obj:`None`)
+
+    :rtype: (:class:`LongTensor`, :class:`Tensor`)
+    """
+    num_nodes = maybe_num_nodes(edge_index, num_nodes)
+    row, col = edge_index
+
+    mask = row != col
+    inv_mask = 1 - mask
+    loop_weight = torch.full(
+        (num_nodes, ),
+        fill_value,
+        dtype=None if edge_weight is None else edge_weight.dtype,
+        device=edge_index.device)
+
+    if edge_weight is not None:
+        assert edge_weight.numel() == edge_index.size(1)
+        loop_weight[row[inv_mask]] = edge_weight[inv_mask].view(-1)
+        edge_weight = torch.cat([edge_weight[mask], loop_weight], dim=0)
+
+    loop_index = torch.arange(0,
+                              num_nodes,
+                              dtype=torch.long,
+                              device=row.device)
+    loop_index = loop_index.unsqueeze(0).repeat(2, 1)
+    edge_index = torch.cat([edge_index[:, mask], loop_index], dim=1)
 
     return edge_index, edge_weight
