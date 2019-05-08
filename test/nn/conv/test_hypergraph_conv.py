1c0ab72b12d58cb32812007b1c8870fc04c6dc96
rusty1s <matthias.fey@tu-dortmund.de>
2019-05-07 20:32:46 -0500

    hypergraph update

--- test/nn/conv/test_hypergraph_conv.py
+++ test/nn/conv/test_hypergraph_conv.py
@@ -4,12 +4,32 @@ from torch_geometric.nn import HypergraphConv
 
 def test_hypergraph_conv():
     in_channels, out_channels = (16, 32)
-    hyper_index = torch.tensor([[0, 0, 0, 1, 2, 3], [1, 2, 3, 0, 0, 0]])
-    hyper_weight = torch.tensor([1, 0.5, 0.3, 0.7])
-    num_nodes = hyper_index.max().item() + 1
+    hyperedge_index = torch.tensor([[0, 0, 1, 1, 2, 3], [0, 1, 0, 1, 0, 1]])
+    hyperedge_weight = torch.tensor([1, 0.5])
+    num_nodes = hyperedge_index[0].max().item() + 1
     x = torch.randn((num_nodes, in_channels))
 
     conv = HypergraphConv(in_channels, out_channels)
     assert conv.__repr__() == 'HypergraphConv(16, 32)'
-    out = conv(x, hyper_index, hyper_weight)
+    out = conv(x, hyperedge_index)
     assert out.size() == (num_nodes, out_channels)
+    out = conv(x, hyperedge_index, hyperedge_weight)
+    assert out.size() == (num_nodes, out_channels)
+
+    conv = HypergraphConv(in_channels,
+                          out_channels,
+                          use_attention=True,
+                          heads=2)
+    out = conv(x, hyperedge_index)
+    assert out.size() == (num_nodes, 2 * out_channels)
+    out = conv(x, hyperedge_index, hyperedge_weight)
+    assert out.size() == (num_nodes, 2 * out_channels)
+
+    conv = HypergraphConv(in_channels,
+                          out_channels,
+                          use_attention=True,
+                          heads=2,
+                          concat=True,
+                          dropout=0.5)
+    out = conv(x, hyperedge_index, hyperedge_weight)
+    assert out.size() == (num_nodes, 2 * out_channels)
