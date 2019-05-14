50ece31e69e845401a4f2e781cb4c36a0a154263
rusty1s <matthias.fey@tu-dortmund.de>
2019-05-14 18:06:20 +0200

    doc

--- torch_geometric/datasets/dynamic_faust.py
+++ torch_geometric/datasets/dynamic_faust.py
@@ -6,6 +6,52 @@ from torch_geometric.data import InMemoryDataset, Data
 
 
 class DynamicFAUST(InMemoryDataset):
+    r"""The dynamic FAUST humans dataset from the `"Dynamic FAUST: Registering
+    Human Bodies in Motion"
+    <http://files.is.tue.mpg.de/black/papers/dfaust2017.pdf>`_ paper.
+
+    .. note::
+
+        Data objects hold mesh faces instead of edge indices.
+        To convert the mesh to a graph, use the
+        :obj:`torch_geometric.transforms.FaceToEdge` as :obj:`pre_transform`.
+        To convert the mesh to a point cloud, use the
+        :obj:`torch_geometric.transforms.SamplePoints` as :obj:`transform` to
+        sample a fixed number of points on the mesh faces according to their
+        face area.
+
+    Args:
+        root (string): Root directory where the dataset should be saved.
+        subjects (list, optional): List of subjects to include in the
+            dataset. Can include the subjects :obj:`"50002"`, :obj:`"50004"`,
+            :obj:`"50007"`, :obj:`"50009"`, :obj:`"50020"`, :obj:`"50021"`,
+            :obj:`"50022"`, :obj:`"50025"`, :obj:`"50026"`, :obj:`"50027"`.
+            If set to :obj:`None`, the dataset will contain all subjects.
+            (default: :obj:`None`)
+        categories (list, optional): List of categories to include in the
+            dataset. Can include the categories :obj:`"chicken_wings"`,
+            :obj:`"hips"`, :obj:`"jiggle_on_toes"`, :obj:`"jumping_jacks"`,
+            :obj:`"knees"`, :obj:`"light_hopping_loose"`,
+            :obj:`"light_hopping_stiff"`, :obj:`"one_leg_jump"`,
+            :obj:`"one_leg_loose"`, :obj:`"personal_move"`, :obj:`"punching"`,
+            :obj:`"running_on_spot"`, :obj:`"running_on_spot_bugfix"`,
+            :obj:`"shake_arms"`, :obj:`"shake_hips"`, :obj:`"shoulders"`.
+            If set to :obj:`None`, the dataset will contain all categories.
+            (default: :obj:`None`)
+        transform (callable, optional): A function/transform that takes in an
+            :obj:`torch_geometric.data.Data` object and returns a transformed
+            version. The data object will be transformed before every access.
+            (default: :obj:`None`)
+        pre_transform (callable, optional): A function/transform that takes in
+            an :obj:`torch_geometric.data.Data` object and returns a
+            transformed version. The data object will be transformed before
+            being saved to disk. (default: :obj:`None`)
+        pre_filter (callable, optional): A function that takes in an
+            :obj:`torch_geometric.data.Data` object and returns a boolean
+            value, indicating whether the data object should be included in the
+            final dataset. (default: :obj:`None`)
+    """
+
     url = 'http://dfaust.is.tue.mpg.de/'
 
     subjects = [
