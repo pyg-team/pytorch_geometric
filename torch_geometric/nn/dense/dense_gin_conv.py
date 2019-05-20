9770bf969eba8b467c4722684894823ffc827d2f
rusty1s <matthias.fey@tu-dortmund.de>
2019-05-20 13:51:28 +0200

    dense gin conv

---
+++ torch_geometric/nn/dense/dense_gin_conv.py
@@ -0,0 +1,60 @@
+import torch
+
+from ..inits import reset
+
+
+class DenseGINConv(torch.nn.Module):
+    r"""See :class:`torch_geometric.nn.conv.GINConv`.
+
+    :rtype: :class:`Tensor`
+    """
+
+    def __init__(self, nn, eps=0, train_eps=False):
+        super(DenseGINConv, self).__init__()
+
+        self.nn = nn
+        self.initial_eps = eps
+        if train_eps:
+            self.eps = torch.nn.Parameter(torch.Tensor([eps]))
+        else:
+            self.register_buffer('eps', torch.Tensor([eps]))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        reset(self.nn)
+        self.eps.data.fill_(self.initial_eps)
+
+    def forward(self, x, adj, mask=None, add_loop=True):
+        r"""
+        Args:
+            x (Tensor): Node feature tensor :math:`\mathbf{X} \in \mathbb{R}^{B
+                \times N \times F}`, with batch-size :math:`B`, (maximum)
+                number of nodes :math:`N` for each graph, and feature
+                dimension :math:`F`.
+            adj (Tensor): Adjacency tensor :math:`\mathbf{A} \in \mathbb{R}^{B
+                \times N \times N}`.
+            mask (ByteTensor, optional): Mask matrix
+                :math:`\mathbf{M} \in {\{ 0, 1 \}}^{B \times N}` indicating
+                the valid nodes for each graph. (default: :obj:`None`)
+            add_loop (bool, optional): If set to :obj:`False`, the layer will
+                not automatically add self-loops to the adjacency matrices.
+                (default: :obj:`True`)
+        """
+        x = x.unsqueeze(0) if x.dim() == 2 else x
+        adj = adj.unsqueeze(0) if adj.dim() == 2 else adj
+        B, N, _ = x.size()
+
+        out = torch.matmul(adj, x)
+        if add_loop:
+            out = (1 + self.eps) * x + out
+
+        out = self.nn(out)
+
+        if mask is not None:
+            mask = mask.view(B, N, 1).to(x.dtype)
+            out = out * mask
+
+        return out
+
+    def __repr__(self):
+        return '{}(nn={})'.format(self.__class__.__name__, self.nn)
