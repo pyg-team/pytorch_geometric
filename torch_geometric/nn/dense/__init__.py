ca56c2f2b3293130ffc59d2d4b331ef6764e0ac2
rusty1s <matthias.fey@tu-dortmund.de>
2019-05-20 13:51:28 +0200

    dense gin conv

--- torch_geometric/nn/dense/__init__.py
+++ torch_geometric/nn/dense/__init__.py
@@ -1,7 +1,9 @@
 from .dense_sage_conv import DenseSAGEConv
+from .dense_gin_conv import DenseGINConv
 from .diff_pool import dense_diff_pool
 
 __all__ = [
     'DenseSAGEConv',
+    'DenseGINConv',
     'dense_diff_pool',
 ]
