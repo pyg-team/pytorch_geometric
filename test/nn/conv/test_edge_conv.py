b9b09a3246c0eb480e2efd38172a81294935c046
rusty1s <matthias.fey@tu-dortmund.de>
2019-05-31 17:16:36 +0200

    test dynamic edge conv

--- test/nn/conv/test_edge_conv.py
+++ test/nn/conv/test_edge_conv.py
@@ -1,6 +1,6 @@
 import torch
 from torch.nn import Sequential as Seq, Linear as Lin, ReLU
-from torch_geometric.nn import EdgeConv
+from torch_geometric.nn import EdgeConv, DynamicEdgeConv
 
 
 def test_edge_conv_conv():
@@ -18,3 +18,19 @@ def test_edge_conv_conv():
         '  (2): Linear(in_features=32, out_features=32, bias=True)\n'
         '))')
     assert conv(x, edge_index).size() == (num_nodes, out_channels)
+
+
+def test_dynamic_edge_conv_conv():
+    in_channels, out_channels = (16, 32)
+    num_nodes = 20
+    x = torch.randn((num_nodes, in_channels))
+
+    nn = Seq(Lin(2 * in_channels, 32), ReLU(), Lin(32, out_channels))
+    conv = DynamicEdgeConv(nn, k=6)
+    assert conv.__repr__() == (
+        'DynamicEdgeConv(nn=Sequential(\n'
+        '  (0): Linear(in_features=32, out_features=32, bias=True)\n'
+        '  (1): ReLU()\n'
+        '  (2): Linear(in_features=32, out_features=32, bias=True)\n'
+        '), k=6)')
+    assert conv(x).size() == (num_nodes, out_channels)
