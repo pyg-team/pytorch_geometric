19cb3fa552a53072b22b4b36726b4f3a10b4483a
rusty1s <matthias.fey@tu-dortmund.de>
2019-06-01 14:45:11 +0200

    linting

--- benchmark/kernel/gcn.py
+++ benchmark/kernel/gcn.py
@@ -37,21 +37,21 @@ class GCN(torch.nn.Module):
 
 
 class GCNWithJK(torch.nn.Module):
-    def __init__(self, dataset, num_layers, hidden, mode='cat', **kwargs):
+    def __init__(self, dataset, num_layers, hidden):
         super(GCNWithJK, self).__init__()
         self.conv1 = GCNConv(dataset.num_features, hidden)
         self.convs = torch.nn.ModuleList()
         for i in range(num_layers - 1):
             self.convs.append(GCNConv(hidden, hidden))
+        self.jump = JumpingKnowledge(mode='cat')
         self.lin1 = Linear(num_layers * hidden, hidden)
         self.lin2 = Linear(hidden, dataset.num_classes)
-        self.mode = mode
-        self.kwargs = kwargs
 
     def reset_parameters(self):
         self.conv1.reset_parameters()
         for conv in self.convs:
             conv.reset_parameters()
+        self.jump.reset_parameters()
         self.lin1.reset_parameters()
         self.lin2.reset_parameters()
 
@@ -62,7 +62,8 @@ class GCNWithJK(torch.nn.Module):
         for conv in self.convs:
             x = F.relu(conv(x, edge_index))
             xs += [x]
-        x = global_mean_pool(JumpingKnowledge(mode=self.mode, **self.kwargs)(xs), batch)
+        x = self.jump(xs)
+        x = global_mean_pool(x, batch)
         x = F.relu(self.lin1(x))
         x = F.dropout(x, p=0.5, training=self.training)
         x = self.lin2(x)
