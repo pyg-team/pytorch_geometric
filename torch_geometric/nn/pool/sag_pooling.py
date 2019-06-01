f86f5e084b6b2ad6ceed485ad6673cc5514892e2
ekka <ekagra.ranjan@gmail.com>
2019-06-02 00:10:29 +0530

    Reuse helper functions from topk_pool.py

--- torch_geometric/nn/pool/sag_pooling.py
+++ torch_geometric/nn/pool/sag_pooling.py
@@ -6,55 +6,7 @@ import torch.nn.functional as F
 
 from torch_geometric.nn.inits import uniform
 from torch_geometric.utils.num_nodes import maybe_num_nodes
-
-
-def topk(x, ratio, batch):
-    num_nodes = scatter_add(batch.new_ones(x.size(0)), batch, dim=0)
-    batch_size, max_num_nodes = num_nodes.size(0), num_nodes.max().item()
-    k = (ratio * num_nodes.to(torch.float)).ceil().to(torch.long)
-
-    cum_num_nodes = torch.cat(
-        [num_nodes.new_zeros(1), num_nodes.cumsum(dim=0)[:-1]], dim=0)
-
-    index = torch.arange(batch.size(0), dtype=torch.long, device=x.device)
-    index = (index - cum_num_nodes[batch]) + (batch * max_num_nodes)
-
-    x_min_value = x.min().item()
-    dense_x = x.new_full((batch_size * max_num_nodes, ), x_min_value - 1)
-    dense_x[index] = x
-    dense_x = dense_x.view(batch_size, max_num_nodes)
-    _, perm = dense_x.sort(dim=-1, descending=True)
-
-    perm = perm + cum_num_nodes.view(-1, 1)
-    perm = perm.view(-1)
-
-    mask = [
-        torch.arange(k[i], dtype=torch.long, device=x.device) + i *
-        max_num_nodes for i in range(len(num_nodes))
-    ]
-    mask = torch.cat(mask, dim=0)
-
-    perm = perm[mask]
-
-    return perm
-
-
-def filter_adj(edge_index, edge_attr, perm, num_nodes=None):
-    num_nodes = maybe_num_nodes(edge_index, num_nodes)
-
-    mask = perm.new_full((num_nodes, ), -1)
-    i = torch.arange(perm.size(0), dtype=torch.long, device=perm.device)
-    mask[perm] = i
-
-    row, col = edge_index
-    row, col = mask[row], mask[col]
-    mask = (row >= 0) & (col >= 0)
-    row, col = row[mask], col[mask]
-
-    if edge_attr is not None:
-        edge_attr = edge_attr[mask]
-
-    return torch.stack([row, col], dim=0), edge_attr
+from torch_geometric.nn.pool.topk_pool import topk, filter_adj
 
 
 class SAGPooling(torch.nn.Module):
