8772c410c8b6cbc26ea3de2d0cc9d7fd9e1c0087
rusty1s <matthias.fey@tu-dortmund.de>
2019-06-01 10:42:52 +0200

    test sampler

--- torch_geometric/utils/erdos_renyi.py
+++ torch_geometric/utils/erdos_renyi.py
@@ -3,20 +3,20 @@ import networkx as nx
 from torch_geometric.utils import from_networkx, to_undirected
 
 
-def erdos_renyi_graph(n, p, seed=None, directed=False):
+def erdos_renyi_graph(num_nodes, prob, seed=None, directed=False):
     r"""Returns the :obj:`edge_index` of a random Erdős-Rényi graph.
 
     Args:
-        n (int): The number of nodes.
-        p (float): Probability of an edge.
+        num_nodes (int): The number of nodes.
+        prob (float): Probability of an edge.
         seed (int, optional): Seed for random number generator.
             (default: :obj:`None`)
         directed (bool, optional): If set to :obj:`True`, will return a
             directed graph. (default: :obj:`False`)
     """
 
-    G = nx.erdos_renyi_graph(n, p, seed, directed)
+    G = nx.erdos_renyi_graph(num_nodes, prob, seed, directed)
     edge_index = from_networkx(G).edge_index
     if not directed:
-        edge_index = to_undirected(edge_index, n)
+        edge_index = to_undirected(edge_index, num_nodes)
     return edge_index
