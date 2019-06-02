d33124bfdd661653a0a84ec4e28845da476204eb
rusty1s <matthias.fey@tu-dortmund.de>
2019-06-02 14:14:51 +0200

    syntax

--- torch_geometric/utils/convert.py
+++ torch_geometric/utils/convert.py
@@ -96,7 +96,8 @@ def from_networkx(G):
 
     for key, item in data.items():
         data[key] = torch.tensor(item)
-    data['edge_index'] = edge_index
-    data['num_nodes'] = len(G.nodes)
+
+    data.edge_index = edge_index
+    data.num_nodes = G.number_of_nodes()
 
     return torch_geometric.data.Data.from_dict(data)
