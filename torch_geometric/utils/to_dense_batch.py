08f95e867a2ffae7b81c96d5ffcfd70d24e4820f
rusty1s <matthias.fey@tu-dortmund.de>
2019-06-12 12:09:07 +0200

    optional batch

--- torch_geometric/utils/to_dense_batch.py
+++ torch_geometric/utils/to_dense_batch.py
@@ -2,7 +2,7 @@ import torch
 from torch_geometric.utils import scatter_
 
 
-def to_dense_batch(x, batch, fill_value=0):
+def to_dense_batch(x, batch=None, fill_value=0):
     r"""Given a sparse batch of node features
     :math:`\mathbf{X} \in \mathbb{R}^{(N_1 + \ldots + N_B) \times F}` (with
     :math:`N_i` indicating the number of nodes in graph :math:`i`), creates a
@@ -15,13 +15,16 @@ def to_dense_batch(x, batch, fill_value=0):
     Args:
         x (Tensor): Node feature matrix
             :math:`\mathbf{X} \in \mathbb{R}^{(N_1 + \ldots + N_B) \times F}`.
-        batch (LongTensor): Batch vector :math:`\mathbf{b} \in {\{ 0, \ldots,
-            B-1\}}^N`, which assigns each node to a specific example.
+        batch (LongTensor, optional): Batch vector
+            :math:`\mathbf{b} \in {\{ 0, \ldots, B-1\}}^N`, which assigns each
+            node to a specific example. (default: :obj:`None`)
         fill_value (float, optional): The value for invalid entries in the
             resulting dense output tensor. (default: :obj:`0`)
 
     :rtype: (:class:`Tensor`, :class:`LongTensor`)
     """
+    if batch is None:
+        batch = x.new_zeros(x.size(0), dtype=torch.long)
     batch_size = batch[-1].item() + 1
     num_nodes = scatter_('add', batch.new_ones(x.size(0)), batch, batch_size)
     cum_nodes = torch.cat([batch.new_zeros(1), num_nodes.cumsum(dim=0)])
