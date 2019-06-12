98b513cabff79172059c33cb2db75448841d290b
rusty1s <matthias.fey@tu-dortmund.de>
2019-06-12 07:25:56 +0200

    allow subpackage imports

--- torch_geometric/__init__.py
+++ torch_geometric/__init__.py
@@ -1,3 +1,12 @@
+import torch_geometric.nn
+import torch_geometric.data
+import torch_geometric.datasets
+import torch_geometric.transforms
+import torch_geometric.utils
+
 __version__ = '1.3.0'
 
-__all__ = ['__version__']
+__all__ = [
+    'torch_geometric',
+    '__version__',
+]
