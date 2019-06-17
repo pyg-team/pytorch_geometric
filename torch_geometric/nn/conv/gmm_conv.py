401e194d78492271e2eb66ad8e16930302dcbe53
rusty1s <matthias.fey@tu-dortmund.de>
2019-06-17 15:16:15 +0200

    fix gmm conv

--- torch_geometric/nn/conv/gmm_conv.py
+++ torch_geometric/nn/conv/gmm_conv.py
@@ -1,8 +1,8 @@
 import torch
-from torch.nn import Parameter
+from torch.nn import Parameter, Linear
 from torch_geometric.nn.conv import MessagePassing
 
-from ..inits import uniform, reset
+from ..inits import reset, uniform, zeros
 
 EPS = 1e-15
 
@@ -61,44 +61,54 @@ class GMMConv(MessagePassing):
         self.dim = dim
         self.kernel_size = kernel_size
 
-        self.mu = Parameter(torch.Tensor(in_channels * kernel_size, dim))
-        self.sigma = Parameter(torch.Tensor(in_channels * kernel_size, dim))
+        self.lin = Linear(in_channels, out_channels * kernel_size, bias=False)
+        self.mu = Parameter(torch.Tensor(kernel_size, dim))
+        self.sigma = Parameter(torch.Tensor(kernel_size, dim))
+
         if root_weight:
             self.root = Parameter(torch.Tensor(in_channels, out_channels))
         else:
             self.register_parameter('root', None)
-        self.lin = torch.nn.Linear(in_channels, out_channels, bias=bias)
+
+        if bias:
+            self.bias = Parameter(torch.Tensor(out_channels))
+        else:
+            self.register_parameter('bias', None)
 
         self.reset_parameters()
 
     def reset_parameters(self):
-        size = self.kernel_size * self.in_channels
-        uniform(size, self.mu)
-        uniform(size, self.sigma)
-        uniform(size, self.root)
         reset(self.lin)
+        uniform(self.kernel_size, self.mu)
+        uniform(self.kernel_size, self.sigma)
+        uniform(self.in_channels, self.root)
+        zeros(self.bias)
 
     def forward(self, x, edge_index, pseudo):
         """"""
         x = x.unsqueeze(-1) if x.dim() == 1 else x
         pseudo = pseudo.unsqueeze(-1) if pseudo.dim() == 1 else pseudo
 
-        out = self.propagate(edge_index, x=x, pseudo=pseudo)
+        out = self.lin(x).view(-1, self.kernel_size, self.out_channels)
+        out = self.propagate(edge_index, x=out, pseudo=pseudo)
 
-        out = self.lin(out)
         if self.root is not None:
             out = out + torch.matmul(x, self.root)
+
+        if self.bias is not None:
+            out = out + self.bias
+
         return out
 
     def message(self, x_j, pseudo):
-        F, (E, D), K = x_j.size(1), pseudo.size(), self.mu.size(0)
+        (E, D), K = pseudo.size(), self.mu.size(0)
 
         # See: https://github.com/shchur/gnn-benchmark
         gaussian = -0.5 * (pseudo.view(E, 1, D) - self.mu.view(1, K, D))**2
         gaussian = gaussian / (EPS + self.sigma.view(1, K, D)**2)
-        gaussian = torch.exp(gaussian.sum(dim=-1))  # [E, K]
+        gaussian = torch.exp(gaussian.sum(dim=-1, keepdim=True))  # [E, K, 1]
 
-        return x_j * gaussian.view(E, F, -1).sum(dim=-1)
+        return (x_j * gaussian).sum(dim=1)
 
     def __repr__(self):
         return '{}({}, {})'.format(self.__class__.__name__, self.in_channels,
