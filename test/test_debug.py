8045df322a34578dde34cde5615db2d62cbd9b52
rusty1s <matthias.fey@tu-dortmund.de>
2019-06-24 18:05:21 +0200

    debug test

---
+++ test/test_debug.py
@@ -0,0 +1,28 @@
+from torch_geometric import is_debug_enabled, debug, set_debug
+
+
+def test_debug():
+    assert is_debug_enabled() is False
+    set_debug(True)
+    assert is_debug_enabled() is True
+    set_debug(False)
+    assert is_debug_enabled() is False
+
+    assert is_debug_enabled() is False
+    with set_debug(True):
+        assert is_debug_enabled() is True
+    assert is_debug_enabled() is False
+
+    assert is_debug_enabled() is False
+    set_debug(True)
+    assert is_debug_enabled() is True
+    with set_debug(False):
+        assert is_debug_enabled() is False
+    assert is_debug_enabled() is True
+    set_debug(False)
+    assert is_debug_enabled() is False
+
+    assert is_debug_enabled() is False
+    with debug():
+        assert is_debug_enabled() is True
+    assert is_debug_enabled() is False
