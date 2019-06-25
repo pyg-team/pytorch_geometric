1b5e36bef16241a15780a8dcf0d2da7687bcbe27
Yutai Zhou <yutai.20m5@gmail.com>
2019-06-25 08:56:31 -0400

    Update num_nodes.py
    Original implementation assumes the edge index belongs to a graph that is not an induced subgraph. New version returns the number of unique node entries in edge_index, rather than the max node label + 1.

--- torch_geometric/utils/num_nodes.py
+++ torch_geometric/utils/num_nodes.py
@@ -1,2 +1,2 @@
 def maybe_num_nodes(index, num_nodes=None):
-    return index.max().item() + 1 if num_nodes is None else num_nodes
+    return len(torch.unique(torch.cat((index[0,:], index[1,:])))) if num_nodes is None else num_nodes
