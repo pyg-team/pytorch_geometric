818291ed68f50c392d60b6cb58e628348764f1a5
Frederik Diehl <3789648+fdiehl@users.noreply.github.com>
2019-07-05 09:36:42 +0200

    Reusing permuted and masked values; changed to shorter naming

--- test/nn/glob/test_glob.py
+++ test/nn/glob/test_glob.py
@@ -28,25 +28,24 @@ def test_permuted_global_pool():
     N_1, N_2 = 4, 6
     x = torch.randn(N_1 + N_2, 4)
     batch = torch.cat([torch.zeros(N_1), torch.ones(N_2)]).to(torch.long)
-
     perm = torch.randperm(N_1 + N_2)
-    perm_x = x[perm]
-    perm_batch = batch[perm]
 
-    mask_N_1 = perm_batch == 0
-    mask_N_2 = perm_batch == 1
+    px = x[perm]
+    pbatch = batch[perm]
+    px1 = px[pbatch == 0]
+    px2 = px[pbatch == 1]
 
-    out = global_add_pool(perm_x, perm_batch)
+    out = global_add_pool(px, pbatch)
     assert out.size() == (2, 4)
-    assert torch.allclose(out[0], perm_x[mask_N_1].sum(dim=0))
-    assert torch.allclose(out[1], perm_x[mask_N_2].sum(dim=0))
+    assert torch.allclose(out[0], px1.sum(dim=0))
+    assert torch.allclose(out[1], px2.sum(dim=0))
 
-    out = global_mean_pool(perm_x, perm_batch)
+    out = global_mean_pool(px, pbatch)
     assert out.size() == (2, 4)
-    assert torch.allclose(out[0], perm_x[mask_N_1].mean(dim=0))
-    assert torch.allclose(out[1], perm_x[mask_N_2].mean(dim=0))
+    assert torch.allclose(out[0], px1.mean(dim=0))
+    assert torch.allclose(out[1], px2.mean(dim=0))
 
-    out = global_max_pool(perm_x, perm_batch)
+    out = global_max_pool(px, pbatch)
     assert out.size() == (2, 4)
-    assert torch.allclose(out[0], perm_x[mask_N_1].max(dim=0)[0])
-    assert torch.allclose(out[1], perm_x[mask_N_2].max(dim=0)[0])
+    assert torch.allclose(out[0], px1.max(dim=0)[0])
+    assert torch.allclose(out[1], px2.max(dim=0)[0])
