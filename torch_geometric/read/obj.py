3c6ef9bfcd662beb549b92c32c841d6a868c357d
Sophia-Lena Saada <slsaada@eidosmontreal.com>
2019-07-31 16:45:56 -0400

    Added_obj_file read functionality

--- torch_geometric/read/obj.py
+++ torch_geometric/read/obj.py
@@ -1,25 +1,18 @@
-import os
-import sys
 import torch
 from torch_geometric.data import Data
 
+
 def yield_file(in_file):
-    import torch
     f = open(in_file)
     buf = f.read()
     f.close()
-    vertices = []
-    faces = []
     for b in buf.split('\n'):
         if b.startswith('v '):
-            x = b.split(" ")
             yield ['v', [float(x) for x in b.split(" ")[1:]]]
         elif b.startswith('f '):
             triangles = b.split(' ')[1:]
-            #We apply -1 because .obj is base 1 but the Data class expects base 0 indices
+            # -1 as .obj is base 1 but the Data class expects base 0 indices
             yield ['f', [int(t.split("/")[0])-1 for t in triangles]]
-        elif b.startswith('g '):
-            yield ['g', b.split(" ")[-1]]
         else:
             yield ['', ""]
 
@@ -29,9 +22,7 @@ def read_obj(in_file):
     vertices = []
     faces = []
 
-    # if mesh.index[x]: f.index[mesh.index[x]]
-    # else: mesh.append(x); f[x] = mesh.index[x]
-    for k,v in yield_file(in_file):
+    for k, v in yield_file(in_file):
         if k == 'v':
             vertices.append(torch.tensor(v))
         elif k == 'f':
@@ -45,4 +36,3 @@ def read_obj(in_file):
     data = Data(pos=pos)
     data.face = face
     return data
-
