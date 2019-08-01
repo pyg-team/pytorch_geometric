accfb1ac56d7b78000e5877e726215d6f4da2f33
rusty1s <matthias.fey@tu-dortmund.de>
2019-08-01 09:42:41 +0200

    added to read_obj to __init__

--- torch_geometric/read/__init__.py
+++ torch_geometric/read/__init__.py
@@ -2,6 +2,7 @@ from .txt_array import parse_txt_array, read_txt_array
 from .tu import read_tu_data
 from .planetoid import read_planetoid_data
 from .ply import read_ply
+from .obj import read_obj
 from .sdf import read_sdf, parse_sdf
 from .off import read_off, parse_off
 from .npz import read_npz, parse_npz
@@ -12,6 +13,7 @@ __all__ = [
     'read_tu_data',
     'read_planetoid_data',
     'read_ply',
+    'read_obj',
     'read_sdf',
     'parse_sdf',
     'read_off',
