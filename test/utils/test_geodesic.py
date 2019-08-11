dd97be0ed3c6f249df1df338e11567ac29a27339
rusty1s <matthias.fey@tu-dortmund.de>
2019-08-11 14:08:54 +0200

    test geodesic

--- test/utils/test_geodesic.py
+++ test/utils/test_geodesic.py
@@ -8,16 +8,34 @@ def test_geodesic_distance():
     pos = torch.Tensor([[0, 0, 0], [2, 0, 0], [0, 2, 0], [2, 2, 0]])
     face = torch.tensor([[0, 1, 3], [0, 2, 3]]).t()
 
-    mat = geodesic_distance(pos, face)
+    out = geodesic_distance(pos, face)
     expected = [
         [0, 1, 1, sqrt(2)],
         [1, 0, sqrt(2), 1],
         [1, sqrt(2), 0, 1],
         [sqrt(2), 1, 1, 0],
     ]
-    assert torch.allclose(mat, torch.tensor(expected))
+    assert torch.allclose(out, torch.tensor(expected))
+
+    out = geodesic_distance(pos, face, norm=False)
+    expected = [
+        [0, 2, 2, 2 * sqrt(2)],
+        [2, 0, 2 * sqrt(2), 2],
+        [2, 2 * sqrt(2), 0, 2],
+        [2 * sqrt(2), 2, 2, 0],
+    ]
+    assert torch.allclose(out, torch.tensor(expected))
 
     src = torch.tensor([0, 0, 0, 0])
     dest = torch.tensor([0, 1, 2, 3])
-    vec = geodesic_distance(pos, face, src, dest)
-    assert torch.allclose(mat[0], vec)
+    out = geodesic_distance(pos, face, src=src, dest=dest)
+    expected = [0, 1, 1, sqrt(2)]
+    assert torch.allclose(out, torch.tensor(expected))
+
+    out = geodesic_distance(pos, face, src=src[0:1])
+    expected = [0, 1, 1, sqrt(2)]
+    assert torch.allclose(out, torch.tensor(expected))
+
+    out = geodesic_distance(pos, face, dest=dest)
+    expected = [0, 0, 0, 0]
+    assert torch.allclose(out, torch.Tensor(expected))
--- torch_geometric/utils/geodesic.py
+++ torch_geometric/utils/geodesic.py
@@ -43,7 +43,7 @@ def geodesic_distance(pos, face, src=None, dest=None, norm=True,
     if norm:
         area = (pos[face[1]] - pos[face[0]]).cross(pos[face[2]] - pos[face[0]])
         norm = (area.norm(p=2, dim=1) / 2).sum().sqrt().item()
-    else:  # pragma: no cover
+    else:
         norm = 1.0
 
     dtype = pos.dtype
@@ -52,30 +52,29 @@ def geodesic_distance(pos, face, src=None, dest=None, norm=True,
     face = face.detach().t().cpu().to(torch.int).numpy()
 
     if src is None and dest is None:
-        out = torch.from_numpy(
-            gdist.local_gdist_matrix(pos, face, max_distance * norm).toarray()
-            / norm).to(dtype)
+        out = gdist.local_gdist_matrix(pos, face,
+                                       max_distance * norm).toarray() / norm
+        return torch.from_numpy(out).to(dtype)
+
+    if src is None:
+        src = np.arange(pos.shape[0], dtype=np.int32)
     else:
-        if src is None:
-            src = np.arange(pos.size(0), dtype=np.int32)
-        else:
-            src = src.detach().cpu().to(torch.int).numpy()
+        src = src.detach().cpu().to(torch.int).numpy()
 
-        dest = None if dest is None else dest.detach().cpu().to(
-            torch.int).numpy()
-        outs = []
-        for i in range(len(src)):
-            s = src[i:i + 1]
-            d = None if dest is None else dest[i:i + 1]
+    dest = None if dest is None else dest.detach().cpu().to(torch.int).numpy()
 
-            out = gdist.compute_gdist(pos, face, s, d,
-                                      max_distance * norm) / norm
-            out = torch.from_numpy(out).to(dtype)
-            outs.append(out)
+    outs = []
+    for i in range(len(src)):
+        s = src[i:i + 1]
+        d = None if dest is None else dest[i:i + 1]
 
-        out = torch.cat(outs, dim=0)
+        out = gdist.compute_gdist(pos, face, s, d, max_distance * norm) / norm
+        out = torch.from_numpy(out).to(dtype)
+        outs.append(out)
 
-        if dest is None:
-            out = out.view(-1, pos.shape[0])
+    out = torch.cat(outs, dim=0)
+
+    if dest is None:
+        out = out.view(-1, pos.shape[0])
 
     return out
