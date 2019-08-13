21b693c75e11f3f02f1cc3d269cc102aad638aa2
rusty1s <matthias.fey@tu-dortmund.de>
2019-08-13 22:02:13 +0200

    codecov

--- test/nn/conv/test_dna_conv.py
+++ test/nn/conv/test_dna_conv.py
@@ -16,3 +16,7 @@ def test_dna_conv():
     conv = DNAConv(channels, heads=1, groups=1, dropout=0.5)
     assert conv.__repr__() == 'DNAConv(32, heads=1, groups=1)'
     assert conv(x, edge_index).size() == (num_nodes, channels)
+
+    conv = DNAConv(channels, heads=1, groups=1, dropout=0.5, cached=True)
+    conv(x, edge_index).size() == (num_nodes, channels)
+    conv(x, edge_index).size() == (num_nodes, channels)
