06ed6b1aadfc55e28ce6344969fd42ae312e4954
rusty1s <matthias.fey@tu-dortmund.de>
2019-10-21 07:59:45 +0200

    allow arguments of knn to be passed in xconv

--- torch_geometric/nn/conv/x_conv.py
+++ torch_geometric/nn/conv/x_conv.py
@@ -47,9 +47,11 @@ class XConv(torch.nn.Module):
             classical convolutional operators. (default: :obj:`1`)
         bias (bool, optional): If set to :obj:`False`, the layer will not learn
             an additive bias. (default: :obj:`True`)
+        **kwargs (optional): Additional arguments of
+            :class:`torch_cluster.knn_graph`.
     """
     def __init__(self, in_channels, out_channels, dim, kernel_size,
-                 hidden_channels=None, dilation=1, bias=True):
+                 hidden_channels=None, dilation=1, bias=True, **kwargs):
         super(XConv, self).__init__()
 
         self.in_channels = in_channels
@@ -61,7 +63,7 @@ class XConv(torch.nn.Module):
         self.dim = dim
         self.kernel_size = kernel_size
         self.dilation = dilation
-        self.cosine = False  # Set to `True` for using cosine distance in `knn`
+        self.kwargs = kwargs
 
         C_in, C_delta, C_out = in_channels, hidden_channels, out_channels
         D, K = dim, kernel_size
@@ -111,7 +113,7 @@ class XConv(torch.nn.Module):
         (N, D), K = pos.size(), self.kernel_size
 
         row, col = knn_graph(pos, K * self.dilation, batch, loop=True,
-                             flow='target_to_source', cosine=self.cosine)
+                             flow='target_to_source', **self.kwargs)
 
         if self.dilation > 1:
             dil = self.dilation
