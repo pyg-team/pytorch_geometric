2b178a7318e8e582c16caa5977fea337b903aa1a
Johannes Klicpera <klicpera@in.tum.de>
2019-11-21 17:14:00 +0100

    Check undirected for edge attributes as well

--- test/utils/test_undirected.py
+++ test/utils/test_undirected.py
@@ -5,8 +5,12 @@ from torch_geometric.utils import is_undirected, to_undirected
 def test_is_undirected():
     row = torch.tensor([0, 1, 0])
     col = torch.tensor([1, 0, 0])
+    sym_weight = torch.tensor([0, 0, 1])
+    asym_weight = torch.tensor([0, 1, 1])
 
     assert is_undirected(torch.stack([row, col], dim=0))
+    assert is_undirected(torch.stack([row, col], dim=0), sym_weight)
+    assert not is_undirected(torch.stack([row, col], dim=0), asym_weight)
 
     row = torch.tensor([0, 1, 1])
     col = torch.tensor([1, 0, 2])
