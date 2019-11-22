94de0d8574d44db0ea0f7214994545f815100ecd
Johannes Klicpera <klicpera@in.tum.de>
2019-11-22 13:20:01 +0100

    Add option to use GDC in GCN example

--- examples/gcn.py
+++ examples/gcn.py
@@ -1,4 +1,5 @@
 import os.path as osp
+import argparse
 
 import torch
 import torch.nn.functional as F
@@ -6,17 +7,29 @@ from torch_geometric.datasets import Planetoid
 import torch_geometric.transforms as T
 from torch_geometric.nn import GCNConv, ChebConv  # noqa
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--use_gdc', action='store_true', help='Use GDC preprocessing.')
+args = parser.parse_args()
+
 dataset = 'Cora'
 path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', dataset)
 dataset = Planetoid(path, dataset, T.NormalizeFeatures())
 data = dataset[0]
 
+if args.use_gdc:
+    gdc = T.GDC(self_loop_weight=1,
+                normalization_in='sym', normalization_out='col',
+                diffusion_kwargs=dict(method='ppr', alpha=0.05),
+                sparsification_kwargs=dict(method='topk', k=128),
+                exact=True)
+    data = gdc(data)
+
 
 class Net(torch.nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = GCNConv(dataset.num_features, 16, cached=True)
-        self.conv2 = GCNConv(16, dataset.num_classes, cached=True)
+        self.conv1 = GCNConv(dataset.num_features, 16, cached=True, normalize=not args.use_gdc)
+        self.conv2 = GCNConv(16, dataset.num_classes, cached=True, normalize=not args.use_gdc)
         # self.conv1 = ChebConv(data.num_features, 16, K=2)
         # self.conv2 = ChebConv(16, data.num_features, K=2)
 
@@ -24,10 +37,10 @@ class Net(torch.nn.Module):
         self.non_reg_params = self.conv2.parameters()
 
     def forward(self):
-        x, edge_index = data.x, data.edge_index
-        x = F.relu(self.conv1(x, edge_index))
+        x, edge_index, edge_weight = data.x, data.edge_index, data.edge_attr
+        x = F.relu(self.conv1(x, edge_index, edge_weight))
         x = F.dropout(x, training=self.training)
-        x = self.conv2(x, edge_index)
+        x = self.conv2(x, edge_index, edge_weight)
         return F.log_softmax(x, dim=1)
 
 
