0d29c2cc9f8fcee12e540a17ad6f47a8a52e4290
Matthias Barth <b4rthm@github.com>
2020-03-06 14:48:42 +0100

    snapdataset, evaluated with flake8

--- torch_geometric/datasets/snapdataset.py
+++ torch_geometric/datasets/snapdataset.py
@@ -4,7 +4,8 @@ import shutil
 import numpy as np
 
 import torch
-from torch_geometric.data import Data, InMemoryDataset, download_url, extract_gz, extract_tar, extract_zip
+from torch_geometric.data import Data, InMemoryDataset, download_url, \
+                                 extract_gz, extract_tar
 from torch_geometric.data.makedirs import makedirs
 
 
@@ -24,13 +25,14 @@ def from_assoc(assoc, idx):
         return -1
 
 
-def read_stanford_data(dir, name):
+def read_snap_dataset_as_list(dir, name):
     list_dir = os.listdir(dir)
-    if len(list_dir) == 1 and osp.isdir(osp.join(dir,list_dir[0])):
+    if len(list_dir) == 1 and osp.isdir(osp.join(dir, list_dir[0])):
         dir = osp.join(dir, list_dir[0])
 
     if 'ego-' in name:
-        files = [file for file in os.listdir(dir) if osp.isfile(osp.join(dir, file))]
+        files = [file for file in os.listdir(dir)
+                 if osp.isfile(osp.join(dir, file))]
         files.sort()
 
         data_list = []
@@ -39,40 +41,44 @@ def read_stanford_data(dir, name):
             edges_file = files[i+1]
             egofeat_file = files[i+2]
             feat_file = files[i+3]
-            featnames_file = files[i+4]
+            # featnames_file = files[i+4]
 
             x = torch.from_numpy(np.loadtxt(osp.join(dir, feat_file))).long()
-            indices = x[:,0]
+            indices = x[:, 0]
             indices_assoc = to_assoc(indices)
-            x = x[:,1:]
+            x = x[:, 1:]
 
             circles = []
-            f = open(osp.join(dir ,circles_file), "r")
+            f = open(osp.join(dir, circles_file), "r")
             c_line = f.readline()
             while not c_line == '':
-                circles.append([from_assoc(indices_assoc, int(i)) for i in c_line.split()[1:]])
+                circles.append([from_assoc(indices_assoc, int(i))
+                                for i in c_line.split()[1:]])
                 c_line = f.readline()
             f.close()
 
-            edge_index = torch.from_numpy(np.loadtxt(osp.join(dir, edges_file)))\
-                              .transpose(0,1).long()
+            edge_index = np.loadtxt(osp.join(dir, edges_file))
+            edge_index = torch.from_numpy(edge_index).transpose(0, 1).long()
 
             # TODO find more efficient way to do this
             for i in range(edge_index.shape[0]):
                 for j in range(edge_index.shape[1]):
-                    edge_index[i][j] = from_assoc(indices_assoc, edge_index[i][j].item())
+                    edge_index[i][j] = from_assoc(indices_assoc,
+                                                  edge_index[i][j].item())
 
-
-            x_ego = torch.from_numpy(np.loadtxt(osp.join(dir, egofeat_file))).long()
+            x_ego = np.loadtxt(osp.join(dir, egofeat_file))
+            x_ego = torch.from_numpy(x_ego).long()
             x = torch.cat((x, x_ego.unsqueeze(0)))
 
             # ego Node is connected so every other node
-            edge_index_ego = torch.fill_(torch.zeros((2,x.shape[0]-1)), x.shape[0]-1)
+            edge_index_ego = torch.fill_(torch.zeros((2, x.shape[0]-1)),
+                                         x.shape[0]-1)
             edge_index_ego[0] = torch.arange(x.shape[0]-1)
 
             # nodes undirected in ego-Facebook
             if name == 'ego-Facebook':
-                edge_index_ego2 = torch.fill_(torch.zeros((2,x.shape[0]-1)), x.shape[0]-1)
+                edge_index_ego2 = torch.fill_(torch.zeros((2, x.shape[0]-1)),
+                                              x.shape[0]-1)
                 edge_index_ego2[1] = torch.arange(x.shape[0]-1)
                 edge_index = torch.cat((edge_index,
                                         edge_index_ego.long(),
@@ -86,58 +92,58 @@ def read_stanford_data(dir, name):
 
     elif 'soc-' in name:
         if name == 'soc-Pokec':
-            #TODO? read out features from 'soc-pokec-profiles.txt'
-            edge_index = torch.from_numpy(np.loadtxt(osp.join(dir, 'soc-pokec-relationships.txt')))\
-                              .transpose(0,1).long()
+            # TODO? read out features from 'soc-pokec-profiles.txt'
+            edge_index = np.loadtxt(osp.join(dir,
+                                             'soc-pokec-relationships.txt'))
+            edge_index = torch.from_numpy(edge_index).transpose(0, 1).long()
             data = Data(edge_index=edge_index)
             return [data]
         else:
             list_dir = os.listdir(dir)
             if len(list_dir) == 1 and osp.isfile(osp.join(dir, list_dir[0])):
-                edge_index = torch.from_numpy(np.loadtxt(osp.join(dir, list_dir[0])))\
-                                  .transpose(0,1).long()
-                # print(np.sum(np.unique(edge_index.numpy()) == np.arange(1, torch.max(edge_index).item()+2)))
+                edge_index = np.loadtxt(osp.join(dir, list_dir[0]))
+                edge_index = torch.from_numpy(edge_index).transpose(0, 1)\
+                                                         .long()
+                '''
+                print(np.sum(np.unique(edge_index.numpy()) \
+                             == np.arange(1, torch.max(edge_index).item()+2)))
+                '''
                 data = Data(edge_index=edge_index)
                 return [data]
 
 
-class StanfordDataset(InMemoryDataset):
+class SNAPDataset(InMemoryDataset):
 
     def __init__(self, root,  name, transform=None, pre_transform=None,
                  pre_filter=None, use_node_attr=False, use_edge_attr=False):
 
         self.url = 'https://snap.stanford.edu/data'
-        self.available_datasets_file = {'ego-Facebook': ['facebook.tar.gz'],
-                                        'ego-Gplus': ['gplus.tar.gz'],
-                                        'ego-Twitter': ['twitter.tar.gz'],
-                                        'soc-Epinions1': ['soc-Epinions1.txt.gz'],
-                                        'soc-LiveJournal1': ['soc-LiveJournal1.txt.gz'],
-                                        'soc-Pokec': ['soc-pokec-relationships.txt.gz',
-                                                      'soc-pokec-profiles.txt.gz'],
-                                        'soc-Slashdot0811': ['Slashdot0811.txt.gz'],
-                                        'soc-Slashdot0922': ['Slashdot0902.txt.gz'],
-                                        }
+        self.available_sets = {'ego-Facebook': ['facebook.tar.gz'],
+                               'ego-Gplus': ['gplus.tar.gz'],
+                               'ego-Twitter': ['twitter.tar.gz'],
+                               'soc-Epinions1': ['soc-Epinions1.txt.gz'],
+                               'soc-LiveJournal1':
+                               ['soc-LiveJourna l1.txt.gz'],
+                               'soc-Pokec': ['soc-pokec-relationships.txt.gz',
+                                             'soc-pokec-profiles.txt.gz'],
+                               'soc-Slashdot0811': ['Slashdot0811.txt.gz'],
+                               'soc-Slashdot0922': ['Slashdot0902.txt.gz'],
+                               }
         available_datasets = list(self.available_datasets_file.keys())
 
         if name not in available_datasets:
-            raise NameError('Name \'{}\' is not an available dataset. Try one of these {}.'.format(name, available_datasets))
+            raise NameError('Name \'{}\' is not an available dataset. Try one'
+                            'of these {}.'.format(name, available_datasets))
 
         self.name = name
-        super(StanfordDataset, self).__init__(root, transform, pre_transform, pre_filter)
+        super(SNAPDataset, self).__init__(root, transform, pre_transform,
+                                          pre_filter)
 
         self.data_list = torch.load(self.processed_paths[0])
 
-        '''
-        self.data, self.slices = torch.load(self.processed_paths[0])
-
-        if self.data.x is not None and not use_node_attr:
-            self.data.x = self.data.x[:, self.num_node_attributes:]
-        if self.data.edge_attr is not None and not use_edge_attr:
-            self.data.edge_attr = self.data.edge_attr[:, self.num_edge_attributes:]
-        '''
+        # self.data, self.slices = torch.load(self.processed_paths[0])
 
     def _download(self):
-        print(self.raw_dir)
         if not osp.isdir(self.raw_dir):
             makedirs(self.raw_dir)
             self.download()
@@ -145,7 +151,6 @@ class StanfordDataset(InMemoryDataset):
             self.download()
         else:
             return
-
 
     def download(self):
         folder = osp.join(self.root, self.name)
@@ -158,42 +163,46 @@ class StanfordDataset(InMemoryDataset):
             os.unlink(path)
         shutil.rmtree(self.raw_dir)
         os.rename(folder, self.raw_dir)
-
+        print('Raw\n{}'.format(self.raw_dir))
 
     @property
     def processed_file_names(self):
         return 'data.pt'
 
-
     def process(self):
         print(self.processed_paths[0])
-        self.data_list = read_stanford_data(self.raw_dir, self.name)
+        self.data_list = read_snap_dataset_as_list(self.raw_dir, self.name)
 
         if self.pre_filter is not None:
-            self.data_list = [data for data in self.data_list if self.pre_filter(data)]
+            self.data_list = [data for data in self.data_list
+                              if self.pre_filter(data)]
 
         if self.pre_transform is not None:
-            self.data_list = [self.pre_transform(data) for data in self.data_list]
+            self.data_list = [self.pre_transform(data)
+                              for data in self.data_list]
 
+        print(self.data_list)
         # torch.save(self.collate(self.data_list), self.processed_paths[0])
         torch.save(self.data_list, self.processed_paths[0])
-
 
     def get(self, idx):
         try:
             return self.data_list[idx]
         except IndexError:
-            raise IndexError("Index out of bounds. Try an index <= {}".format(len(self.data_list)-1))
+            raise IndexError("Index out of bounds. Try an index <= {}"
+                             .format(len(self.data_list)-1))
 
+    def len(self):
+        return len(self.data_list)
 
     def __repr__(self):
         return '{}({})'.format(self.name, len(self))
 
 
 if __name__ == '__main__':
-    # dataset_name = 'ego-Facebook'
-    dataset_name = 'soc-Pokec'
-    path = path = osp.join(osp.dirname(osp.realpath(__file__)), '..', '..', 'data', dataset_name)
-    dataset = StanfordDataset(path, dataset_name)
-    data = dataset[0]
+    dataset_name = 'ego-Facebook'
+    path = path = osp.join(osp.dirname(osp.realpath(__file__)),
+                           '..', '..', 'data', dataset_name)
+    dataset = SNAPDataset(path, dataset_name)
+    data = dataset[8]
     print(data)
