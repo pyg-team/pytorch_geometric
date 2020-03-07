fix formula

torch_geometric/nn/norm/graph_size_norm.py
@@ -10,7 +10,7 @@ class GraphSizeNorm(nn.Module):
     paper
 
     .. math::
-        \mathbf{x}^{\prime}_i = \frac{\mathbf{x}_i}{\sqrt{|\mathcal{V}|}
+        \mathbf{x}^{\prime}_i = \frac{\mathbf{x}_i}{\sqrt{|\mathcal{V}|}}
     """
     def __init__(self):
         super(GraphSizeNorm, self).__init__()