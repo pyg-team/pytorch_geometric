92e674c185fcb1e9c28e11aa27cc86876c2b65b0
RuihongQiu_gs522-6979 <qiuruihong@foxmail.com>
2020-03-07 13:26:28 +1000

    Typo

--- test/nn/norm/test_graph_size_norm.py
+++ test/nn/norm/test_graph_size_norm.py
@@ -2,7 +2,7 @@ import torch
 from torch_geometric.nn import GraphSizeNorm
 
 
-def test_batch_norm():
+def test_graph_size_norm():
     batch = torch.repeat_interleave(torch.full((10, ), 10, dtype=torch.long))
     norm = GraphSizeNorm()
     out = norm(torch.randn(100, 16), batch)
--- torch_geometric/nn/norm/__init__.py
+++ torch_geometric/nn/norm/__init__.py
@@ -1,6 +1,6 @@
 from .batch_norm import BatchNorm
 from .instance_norm import InstanceNorm
-from.graph_size_norm import GraphSizeNorm
+from .graph_size_norm import GraphSizeNorm
 
 __all__ = [
     'BatchNorm',
