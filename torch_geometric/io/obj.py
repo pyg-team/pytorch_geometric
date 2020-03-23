71a07134a0a86ef824a52ae8b3f1091280ed37c1
rusty1s <matthias.fey@tu-dortmund.de>
2020-03-23 16:41:40 +0100

    correct format

--- torch_geometric/io/obj.py
+++ torch_geometric/io/obj.py
@@ -10,11 +10,12 @@ def yield_file(in_file):
         if b.startswith('v '):
             yield ['v', [float(x) for x in b.split(" ")[1:]]]
         elif b.startswith('f '):
-            triangles = b.split(' ')[1:]
+            triangle = b.split(' ')[1:]
             # -1 as .obj is base 1 but the Data class expects base 0 indices
-            yield ['f', [[int(i)-1 for i in t.split("/")] for t in triangles]]
+            yield ['f', [[int(i) - 1 for i in t.split("/")] for t in triangle]]
         else:
             yield ['', ""]
+
 
 def read_obj(in_file):
     vertices = []
@@ -29,7 +30,7 @@ def read_obj(in_file):
 
     if not len(faces) or not len(vertices):
         return None
-    
+
     pos = torch.tensor(vertices, dtype=torch.float)
     face = torch.tensor(faces, dtype=torch.long).t().contiguous()
 
