f0f4b54881ba31bfdd8ab6178539e861d5b1ccde
rusty1s <matthias.fey@tu-dortmund.de>
2020-04-06 15:00:50 +0200

    unify gravnet interface

--- test/nn/conv/test_gravnet_conv.py
+++ test/nn/conv/test_gravnet_conv.py
@@ -2,17 +2,11 @@ import torch
 from torch_geometric.nn import GravNetConv
 
 
-def test_gravnet_conv_conv():
-    in_channels, out_channels = (16, 32)
-    space_dimensions_s=4
-    propagate_dimensions_flr=8
-    k_neighbors=12
-                 
-    num_nodes = 20
+def test_gravnet_conv():
+    num_nodes, in_channels, out_channels = 20, 16, 32
     x = torch.randn((num_nodes, in_channels))
 
-    conv = GravNetConv(in_channels,space_dimensions_s,
-                       propagate_dimensions_flr,k_neighbors,
-                       out_channels)
+    conv = GravNetConv(in_channels, out_channels, space_dimensions=4,
+                       propagate_dimensions=8, k=12)
+    assert conv.__repr__() == 'GravNetConv(16, 32, k=12)'
     assert conv(x).size() == (num_nodes, out_channels)
-
