fix tag conv normalization

torch_geometric/nn/conv/tag_conv.py
@@ -1,7 +1,7 @@
 import torch
 from torch.nn import Linear
 from torch_scatter import scatter_add
-from torch_geometric.nn.conv import MessagePassing, GCNConv
+from torch_geometric.nn.conv import MessagePassing
 
 
 class TAGConv(MessagePassing):
@@ -25,6 +25,7 @@ class TAGConv(MessagePassing):
         **kwargs (optional): Additional arguments of
             :class:`torch_geometric.nn.conv.MessagePassing`.
     """
+
     def __init__(self, in_channels, out_channels, K=3, bias=True, **kwargs):
         super(TAGConv, self).__init__(aggr='add', **kwargs)
 
@@ -54,8 +55,8 @@ class TAGConv(MessagePassing):
 
     def forward(self, x, edge_index, edge_weight=None):
         """"""
-        edge_index, norm = GCNConv.norm(edge_index, x.size(self.node_dim),
-                                        edge_weight, dtype=x.dtype)
+        edge_index, norm = self.norm(edge_index, x.size(self.node_dim),
+                                     edge_weight, dtype=x.dtype)
 
         xs = [x]
         for k in range(self.K):