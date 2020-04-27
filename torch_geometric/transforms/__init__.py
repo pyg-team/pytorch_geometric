39bbf134f2453b5851f8704a01f8bef459bb0363
rusty1s <matthias.fey@tu-dortmund.de>
2020-04-27 17:42:12 +0200

    added sign transform

--- torch_geometric/transforms/__init__.py
+++ torch_geometric/transforms/__init__.py
@@ -34,6 +34,7 @@ from .generate_mesh_normals import GenerateMeshNormals
 from .delaunay import Delaunay
 from .to_superpixels import ToSLIC
 from .gdc import GDC
+from .sign import SIGN
 from .grid_sampling import GridSampling
 
 __all__ = [
@@ -73,5 +74,6 @@ __all__ = [
     'Delaunay',
     'ToSLIC',
     'GDC',
-    'GridSampling'
+    'SIGN',
+    'GridSampling',
 ]
