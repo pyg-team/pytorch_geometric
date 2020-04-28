18e2478fae1a6ed92a691bde983c8f55ee8b002c
rusty1s <matthias.fey@tu-dortmund.de>
2020-04-28 09:10:25 +0200

    typo

--- test/nn/conv/test_gat_conv.py
+++ test/nn/conv/test_gat_conv.py
@@ -15,6 +15,7 @@ def test_gat_conv():
     out = conv(x, edge_index, return_attention_weights=True)
     assert out[0].size() == (num_nodes, 2 * out_channels)
     assert out[1].size() == (edge_index.size(1) + num_nodes, 2)
+    assert conv.alpha is None
 
     conv = GATConv(in_channels, out_channels, heads=2, concat=False)
     assert conv(x, edge_index).size() == (num_nodes, out_channels)
@@ -22,3 +23,4 @@ def test_gat_conv():
     out = conv(x, edge_index, return_attention_weights=True)
     assert out[0].size() == (num_nodes, out_channels)
     assert out[1].size() == (edge_index.size(1) + num_nodes, 2)
+    assert conv.alpha is None
