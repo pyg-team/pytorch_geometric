a307e0f1041fc2d0d6f7ffe70c44ab2a454f2b79
Lindsey Gray <lindsey.gray@gmail.com>
2020-05-06 14:52:02 -0500

    mnist_nn_conv with jit-scripted NNConv trains!

--- examples/mnist_nn_conv_jit.py
+++ examples/mnist_nn_conv_jit.py
@@ -18,17 +18,51 @@ train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
 d = train_dataset
 
+from torch_geometric.nn.pool.consecutive import consecutive_cluster
+from torch_geometric.nn.pool.pool import pool_edge, pool_batch, pool_pos
+from torch_geometric.nn.pool.max_pool import _max_pool_x
+
+def cartesian_jit(x, edge_index, edge_attr, pos, batch, cat=False, norm=True, max=None):
+    (row, col), pos, pseudo = edge_index, pos, edge_attr
+
+    cart = pos[col] - pos[row]
+    cart = cart.view(-1, 1) if cart.dim() == 1 else cart
+
+    if norm and cart.numel() > 0:
+        max_value = cart.abs().max() if max is None else max
+        cart = cart / (2 * max_value) + 0.5
+
+    if pseudo is not None and cat:
+        pseudo = pseudo.view(-1, 1) if pseudo.dim() == 1 else pseudo
+        edge_attr = torch.cat([pseudo, cart.type_as(pseudo)], dim=-1)
+    else:
+        edge_attr = cart
+
+    return x, edge_index, edge_attr, pos, batch
+
+def max_pool_jit(cluster, x, edge_index, edge_attr, pos, batch, transform):
+    cluster, perm = consecutive_cluster(cluster)
+
+    x = None if x is None else _max_pool_x(cluster, x)
+    edge_index, attr = pool_edge(cluster, edge_index, edge_attr)
+    batch = None if batch is None else pool_batch(perm, batch)
+    pos = None if pos is None else pool_pos(cluster, pos)
+
+    if transform is not None:
+        x, edge_index, edge_attr, pos, batch = transform(x, edge_index, edge_attr, pos, batch)
+
+    return x, edge_index, edge_attr, pos, batch
 
 def normalized_cut_2d(edge_index, pos):
-    row, col = edge_index
+    row, col = edge_index[0], edge_index[1]
     edge_attr = torch.norm(pos[row] - pos[col], p=2, dim=1)
     return normalized_cut(edge_index, edge_attr, num_nodes=pos.size(0))
 
 init_data = None
-for i, data in enumerate(test_loader):
+for i, tdata in enumerate(test_loader):
     if i > 0:
         break
-    init_data = data
+    init_data = tdata
 
 class Net(nn.Module):
     def __init__(self):
@@ -42,24 +76,24 @@ class Net(nn.Module):
         self.fc1 = torch.nn.Linear(64, 128)
         self.fc2 = torch.nn.Linear(128, d.num_classes)
         
-        self.conv1 = conv1.jittable(x=init_data.x, edge_index=init_data.edge_index, edge_attr=init_data.edge_attr)
+        self.conv1 = torch.jit.script(conv1.jittable(x=init_data.x, edge_index=init_data.edge_index, edge_attr=init_data.edge_attr))
         tempx = self.conv1(x=init_data.x, edge_index=init_data.edge_index, edge_attr=init_data.edge_attr)
-        self.conv2 = conv2.jittable(x=tempx, edge_index=init_data.edge_index, edge_attr=init_data.edge_attr)
+        self.conv2 = torch.jit.script(conv2.jittable(x=tempx, edge_index=init_data.edge_index, edge_attr=init_data.edge_attr))
         
         print('NET CONV1 --->', self.conv1)
         print('NET CONV2 --->', self.conv2)
 
     def forward(self, x, edge_index, edge_attr, pos, batch):
-        data.x = F.elu(self.conv1(data.x, data.edge_index, data.edge_attr))
-        weight = normalized_cut_2d(data.edge_index, data.pos)
-        cluster = graclus(data.edge_index, weight, data.x.size(0))
-        data.edge_attr = None
-        data = max_pool(cluster, data, transform=transform)
+        x = F.elu(self.conv1(x, edge_index, edge_attr))
+        weight = normalized_cut_2d(edge_index, pos)
+        cluster = graclus(edge_index, weight, x.size(0))
+        
+        x, edge_index, edge_attr, pos, batch = max_pool_jit(cluster, x, edge_index, None, pos, batch, transform=cartesian_jit)
 
-        data.x = F.elu(self.conv2(data.x, data.edge_index, data.edge_attr))
-        weight = normalized_cut_2d(data.edge_index, data.pos)
-        cluster = graclus(data.edge_index, weight, data.x.size(0))
-        x, batch = max_pool_x(cluster, data.x, data.batch)
+        x = F.elu(self.conv2(x, edge_index, edge_attr))
+        weight = normalized_cut_2d(edge_index, pos)
+        cluster = graclus(edge_index, weight, x.size(0))
+        x, batch = max_pool_x(cluster, x, batch)
 
         x = global_mean_pool(x, batch)
         x = F.elu(self.fc1(x))
@@ -68,12 +102,10 @@ class Net(nn.Module):
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = torch.jit.script(Net()).to(device)
+model = Net().to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
 print(model)
-
-exit()
 
 def train(epoch):
     model.train()
@@ -89,7 +121,7 @@ def train(epoch):
     for data in train_loader:
         data = data.to(device)
         optimizer.zero_grad()
-        F.nll_loss(model(data), data.y).backward()
+        F.nll_loss(model(data.x, data.edge_index, data.edge_attr, data.pos, data.batch), data.y).backward()
         optimizer.step()
 
 
@@ -99,7 +131,7 @@ def test():
 
     for data in test_loader:
         data = data.to(device)
-        pred = model(data).max(1)[1]
+        pred = model(data.x, data.edge_index, data.edge_attr, data.pos, data.batch).max(1)[1]
         correct += pred.eq(data.y).sum().item()
     return correct / len(test_dataset)
 
