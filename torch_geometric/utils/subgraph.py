c537593c53fd85995c7b780a881cd4aec14f7bd2
rusty1s <matthias.fey@tu-dortmund.de>
2020-05-08 14:34:02 +0200

    k-hop subgraph for multiple nodes

--- torch_geometric/utils/subgraph.py
+++ torch_geometric/utils/subgraph.py
@@ -58,11 +58,13 @@ def k_hop_subgraph(node_idx, num_hops, edge_index, relabel_nodes=False,
     r"""Computes the :math:`k`-hop subgraph of :obj:`edge_index` around node
     :attr:`node_idx`.
     It returns (1) the nodes involved in the subgraph, (2) the filtered
-    :obj:`edge_index` connectivity, and (3) the edge mask indicating which
-    edges were preserved.
+    :obj:`edge_index` connectivity, (3) the mapping from node indices in
+    :obj:`node_idx` to their new location, and (4) the edge mask indicating
+    which edges were preserved.
 
     Args:
-        node_idx (int): The central node.
+        node_idx (int, list, tuple or :obj:`torch.Tensor`): The central
+            node(s).
         num_hops: (int): The number of hops :math:`k`.
         edge_index (LongTensor): The edge indices.
         relabel_nodes (bool, optional): If set to :obj:`True`, the resulting
@@ -74,7 +76,8 @@ def k_hop_subgraph(node_idx, num_hops, edge_index, relabel_nodes=False,
             aggregation (:obj:`"source_to_target"` or
             :obj:`"target_to_source"`). (default: :obj:`"source_to_target"`)
 
-    :rtype: (:class:`LongTensor`, :class:`LongTensor`, :class:`BoolTensor`)
+    :rtype: (:class:`LongTensor`, :class:`LongTensor`, :class:`LongTensor`,
+             :class:`BoolTensor`)
     """
 
     num_nodes = maybe_num_nodes(edge_index, num_nodes)
@@ -88,16 +91,21 @@ def k_hop_subgraph(node_idx, num_hops, edge_index, relabel_nodes=False,
     node_mask = row.new_empty(num_nodes, dtype=torch.bool)
     edge_mask = row.new_empty(row.size(0), dtype=torch.bool)
 
-    subsets = [torch.tensor([node_idx], device=row.device).flatten()]
+    if isinstance(node_idx, (int, list, tuple)):
+        node_idx = torch.tensor([node_idx], device=row.device).flatten()
+    else:
+        node_idx = node_idx.to(row.device)
+
+    subsets = [node_idx]
+
     for _ in range(num_hops):
         node_mask.fill_(False)
         node_mask[subsets[-1]] = True
         torch.index_select(node_mask, 0, row, out=edge_mask)
         subsets.append(col[edge_mask])
-    subset = torch.cat(subsets).unique()
-    # Add `node_idx` to the beginning of `subset`.
-    subset = subset[subset != node_idx]
-    subset = torch.cat([torch.tensor([node_idx], device=row.device), subset])
+
+    subset, inv = torch.cat(subsets).unique(return_inverse=True)
+    inv = inv[:node_idx.numel()]
 
     node_mask.fill_(False)
     node_mask[subset] = True
@@ -110,4 +118,4 @@ def k_hop_subgraph(node_idx, num_hops, edge_index, relabel_nodes=False,
         node_idx[subset] = torch.arange(subset.size(0), device=row.device)
         edge_index = node_idx[edge_index]
 
-    return subset, edge_index, edge_mask
+    return subset, edge_index, inv, edge_mask
