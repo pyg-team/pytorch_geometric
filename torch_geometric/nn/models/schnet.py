22cc9ce4412f3f2ebf0438f4d0f9655483eedb89
rusty1s <matthias.fey@tu-dortmund.de>
2020-05-14 11:33:29 +0200

    docstring

--- torch_geometric/nn/models/schnet.py
+++ torch_geometric/nn/models/schnet.py
@@ -36,6 +36,51 @@ qm9_target_dict = {
 
 
 class SchNet(torch.nn.Module):
+    r"""The continuous-filter convolutional neural network SchNet from the
+    `"SchNet: A Continuous-filter Convolutional Neural Network for Modeling
+    Quantum Interactions" <https://arxiv.org/abs/1706.08566>`_ paper that uses
+    the interactions blocks of the form
+
+    .. math::
+        \mathbf{x}^{\prime}_i = \sum_{j \in \mathcal{N}(i)} \mathbf{x}_j \odot
+        h_{\mathbf{\Theta}} ( \exp(-\gamma(\mathbf{e}_{j,i} - \mathbf{\mu}))),
+
+    here :math:`h_{\mathbf{\Theta}}` denotes an MLP and
+    :math:`\mathbf{e}_{j,i}` denotes the interatomic distances between atoms.
+
+    .. note::
+
+        For an example of using a pretrained SchNet variant, see
+        `examples/qm9_pretrained_schnet.py
+        <https://github.com/rusty1s/pytorch_geometric/blob/master/examples/
+        qm9_pretrained_schnet.py>`_.
+
+    Args:
+        hidden_channels (int, optional): Hidden embedding size.
+            (default: :obj:`128`)
+        num_filters (int, optional): The number of filters to use.
+            (default: :obj:`128`)
+        num_interactions (int, optional): The number of interaction blocks.
+            (default: :obj:`6`)
+        num_gaussians (int, optional): The number of gaussians :math:`\mu`.
+            (default: :obj:`50`)
+        cutoff (float, optional): Cutoff distance for interatomic interactions.
+            (default: :obj:`10.0`)
+        readout (string, optional): Whether to apply :obj:`"add"` or
+            :obj:`"mean"` global aggregation. (default: :obj:`"add"`)
+        dipole (bool, optional): If set to :obj:`True`, will use the magnitude
+            of the dipole moment to make the final prediction, *e.g.*, for
+            target 0 of :class:`torch_geometric.datasets.QM9`.
+            (default: :obj:`False`)
+        mean (float, optional): The mean of the property to predict.
+            (default: :obj:`None`)
+        std (float, optional): The standard deviation of the property to
+            predict. (default: :obj:`None`)
+        atomref (torch.Tensor, optional): The reference of single-atom
+            properties.
+            Expects a vector of shape :obj:`(max_atomic_number, )`.
+    """
+
     url = 'http://www.quantum-machine.org/datasets/trained_schnet_models.zip'
 
     def __init__(self, hidden_channels=128, num_filters=128,
