effc617c6ad6daad506038bb79e4407082e74740
rusty1s <matthias.fey@tu-dortmund.de>
2020-05-17 18:49:30 +0200

    outsourced computation of attention weights

--- test/nn/conv/test_gat_conv.py
+++ test/nn/conv/test_gat_conv.py
@@ -12,15 +12,15 @@ def test_gat_conv():
     assert conv.__repr__() == 'GATConv(16, 32, heads=2)'
     assert conv(x, edge_index).size() == (num_nodes, 2 * out_channels)
     assert conv((x, None), edge_index).size() == (num_nodes, 2 * out_channels)
-    out = conv(x, edge_index, return_attention_weights=True)
-    assert out[0].size() == (num_nodes, 2 * out_channels)
-    assert out[1].size() == (edge_index.size(1) + num_nodes, 2)
-    assert conv.alpha is None
+
+    out = conv.get_attention_weight(x, edge_index)
+    assert out[0].size() == (2, edge_index.size(1) + num_nodes)
+    assert out[1].size() == (edge_index.size(1) + num_nodes, conv.heads)
 
     conv = GATConv(in_channels, out_channels, heads=2, concat=False)
     assert conv(x, edge_index).size() == (num_nodes, out_channels)
     assert conv((x, None), edge_index).size() == (num_nodes, out_channels)
-    out = conv(x, edge_index, return_attention_weights=True)
-    assert out[0].size() == (num_nodes, out_channels)
-    assert out[1].size() == (edge_index.size(1) + num_nodes, 2)
-    assert conv.alpha is None
+
+    out = conv.get_attention_weight(x, edge_index)
+    assert out[0].size() == (2, edge_index.size(1) + num_nodes)
+    assert out[1].size() == (edge_index.size(1) + num_nodes, conv.heads)
