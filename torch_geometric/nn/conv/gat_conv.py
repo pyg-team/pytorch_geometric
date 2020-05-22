6f9d05566dbabecfaf75721f8c632209c6a95c46
Lindsey Gray <lindsey.gray@gmail.com>
2020-05-22 13:52:11 -0500

    fix dummy values in gatconv

--- torch_geometric/nn/conv/gat_conv.py
+++ torch_geometric/nn/conv/gat_conv.py
@@ -104,9 +104,9 @@ class GATConv(MessagePassing):
         prop = \
             self.propagate(edge_index, x=x_prop,
                            return_attention_weights=return_attention_weights)
+        dim0 = self.heads if self.concat else 1
         out = \
-            prop if prop is not None else torch.ones((2 if self.concat else 1,
-                                                      self.out_channels),
+            prop if prop is not None else torch.ones((dim0, self.out_channels),
                                                      device=x_prop[1].device,
                                                      dtype=x_prop[1].dtype)
 
