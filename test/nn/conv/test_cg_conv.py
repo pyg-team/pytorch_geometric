235188d0cd7a6414e3097f852fcf6202f87c3710
Lindsey Gray <lindsey.gray@gmail.com>
2020-05-23 09:28:59 -0500

    CGConv is jittable

--- test/nn/conv/test_cg_conv.py
+++ test/nn/conv/test_cg_conv.py
@@ -11,3 +11,14 @@ def test_cg_conv():
     conv = CGConv(16, 3)
     assert conv.__repr__() == 'CGConv(16, 16, dim=3)'
     assert conv(x, edge_index, pseudo).size() == (num_nodes, 16)
+    jitcls = conv.jittable(x=x, edge_index=edge_index, edge_attr=pseudo)
+    jitconv = jitcls(16, 3)
+    jitconv.load_state_dict(conv.state_dict())
+    jittedconv = torch.jit.script(jitconv)
+    conv.eval()
+    jitconv.eval()
+    jittedconv.eval()
+    assert (torch.abs(conv(x, edge_index, pseudo) -
+            jitconv(x, edge_index, pseudo)) < 1e-6).all().item()
+    assert (torch.abs(conv(x, edge_index, pseudo) -
+            jittedconv(x, edge_index, pseudo)) < 1e-6).all().item()
