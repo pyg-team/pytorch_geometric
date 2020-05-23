52701bea03101ce7ca84473ddb0a244255a9f6d5
Lindsey Gray <lindsey.gray@gmail.com>
2020-05-23 09:28:14 -0500

    APPNP is jittable

--- test/nn/conv/test_appnp.py
+++ test/nn/conv/test_appnp.py
@@ -12,3 +12,14 @@ def test_appnp():
     conv = APPNP(K=10, alpha=0.1)
     assert conv.__repr__() == 'APPNP(K=10, alpha=0.1)'
     assert conv(lin(x), edge_index).size() == (num_nodes, out_channels)
+    jitcls = conv.jittable(x=lin(x), edge_index=edge_index, full_eval=True)
+    jitconv = jitcls(K=10, alpha=0.1)
+    jitconv.load_state_dict(conv.state_dict())
+    jittedconv = torch.jit.script(jitconv)
+    conv.eval()
+    jitconv.eval()
+    jittedconv.eval()
+    assert (torch.abs(conv(x, edge_index) -
+            jitconv(x, edge_index)) < 1e-6).all().item()
+    assert (torch.abs(conv(x, edge_index) -
+            jittedconv(x, edge_index)) < 1e-6).all().item()
