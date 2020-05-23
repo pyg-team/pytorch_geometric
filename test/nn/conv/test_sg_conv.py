60bf8ab1ab003d26b619ff05cfbe2be6be2dd4c3
Lindsey Gray <lindsey.gray@gmail.com>
2020-05-23 09:36:21 -0500

    SGConv is jittable

--- test/nn/conv/test_sg_conv.py
+++ test/nn/conv/test_sg_conv.py
@@ -12,8 +12,34 @@ def test_sg_conv():
     assert conv.__repr__() == 'SGConv(16, 32, K=10)'
     assert conv(x, edge_index).size() == (num_nodes, out_channels)
     assert conv(x, edge_index).size() == (num_nodes, out_channels)
+    conv.reset_parameters()
+    jitcls = conv.jittable(x=x, edge_index=edge_index, full_eval=True)
+    conv.reset_parameters()
+    jitconv = jitcls(in_channels, out_channels, K=10, cached=True)
+    jitconv.load_state_dict(conv.state_dict())
+    jittedconv = torch.jit.script(jitconv)
+    conv.eval()
+    jitconv.eval()
+    jittedconv.eval()
+    assert (torch.abs(conv(x, edge_index) -
+            jitconv(x, edge_index)) < 1e-6).all().item()
+    assert (torch.abs(conv(x, edge_index) -
+            jittedconv(x, edge_index)) < 1e-6).all().item()
 
     conv = SGConv(in_channels, out_channels, K=10, cached=False)
     assert conv.__repr__() == 'SGConv(16, 32, K=10)'
     assert conv(x, edge_index).size() == (num_nodes, out_channels)
     assert conv(x, edge_index).size() == (num_nodes, out_channels)
+    conv.reset_parameters()
+    jitcls = conv.jittable(x=x, edge_index=edge_index, full_eval=True)
+    conv.reset_parameters()
+    jitconv = jitcls(in_channels, out_channels, K=10, cached=False)
+    jitconv.load_state_dict(conv.state_dict())
+    jittedconv = torch.jit.script(jitconv)
+    conv.eval()
+    jitconv.eval()
+    jittedconv.eval()
+    assert (torch.abs(conv(x, edge_index) -
+            jitconv(x, edge_index)) < 1e-6).all().item()
+    assert (torch.abs(conv(x, edge_index) -
+            jittedconv(x, edge_index)) < 1e-6).all().item()
--- torch_geometric/nn/conv/sg_conv.py
+++ torch_geometric/nn/conv/sg_conv.py
@@ -1,5 +1,10 @@
+import torch
 from torch.nn import Linear
-from torch_geometric.nn.conv import MessagePassing, GCNConv
+from torch_scatter import scatter_add
+from torch_geometric.nn.conv import MessagePassing
+from torch_geometric.utils import add_remaining_self_loops
+
+from typing import Optional
 
 
 class SGConv(MessagePassing):
@@ -29,6 +34,10 @@ class SGConv(MessagePassing):
         **kwargs (optional): Additional arguments of
             :class:`torch_geometric.nn.conv.MessagePassing`.
     """
+    cached_num_edges: Optional[int]
+    cached_result: Optional[torch.Tensor]
+    has_cached_result: bool
+
     def __init__(self, in_channels, out_channels, K=1, cached=False, bias=True,
                  **kwargs):
         super(SGConv, self).__init__(aggr='add', **kwargs)
@@ -38,6 +47,10 @@ class SGConv(MessagePassing):
         self.K = K
         self.cached = cached
 
+        self.cached_num_edges = None
+        self.cached_result = None
+        self.has_cached_result = False
+
         self.lin = Linear(in_channels, out_channels, bias=bias)
 
         self.reset_parameters()
@@ -46,11 +59,39 @@ class SGConv(MessagePassing):
         self.lin.reset_parameters()
         self.cached_result = None
         self.cached_num_edges = None
+        self.has_cached_result = False
 
-    def forward(self, x, edge_index, edge_weight=None):
+    def norm(self, edge_index, num_nodes: int,
+             edge_weight: Optional[torch.Tensor] = None,
+             improved: bool = False,
+             dtype: Optional[int] = None):
+        if edge_weight is None:
+            edge_weight = torch.ones((edge_index.size(1), ), dtype=dtype,
+                                     device=edge_index.device)
+
+        fill_value = 1 if not improved else 2
+        edge_index, edge_weight = add_remaining_self_loops(
+            edge_index, edge_weight, fill_value, num_nodes)
+
+        row, col = edge_index[0], edge_index[1]
+        assert edge_weight is not None
+        deg = scatter_add(edge_weight, row, dim=0, dim_size=num_nodes)
+        deg_inv_sqrt = deg.pow(-0.5)
+        mask = (deg_inv_sqrt == float('inf'))
+        zeros = torch.zeros(deg_inv_sqrt.shape,
+                            dtype=deg_inv_sqrt.dtype,
+                            device=deg_inv_sqrt.device)
+        deg_inv_sqrt = torch.where(mask, zeros, deg_inv_sqrt)
+
+        return edge_index, deg_inv_sqrt[row] * edge_weight * deg_inv_sqrt[col]
+
+    def forward(self, x, edge_index,
+                edge_weight: Optional[torch.Tensor] = None):
         """"""
-        if self.cached and self.cached_result is not None:
-            if edge_index.size(1) != self.cached_num_edges:
+        if self.cached and self.has_cached_result:
+            cached_num_edges = self.cached_num_edges
+            assert cached_num_edges is not None
+            if edge_index.size(1) != cached_num_edges:
                 raise RuntimeError(
                     'Cached {} number of edges, but found {}. Please '
                     'disable the caching behavior of this layer by removing '
@@ -60,17 +101,20 @@ class SGConv(MessagePassing):
         if not self.cached:
             x = self.lin(x)
 
-        if not self.cached or self.cached_result is None:
+        if not self.cached or not self.has_cached_result:
             self.cached_num_edges = edge_index.size(1)
-            edge_index, norm = GCNConv.norm(edge_index, x.size(self.node_dim),
-                                            edge_weight, dtype=x.dtype)
+            edge_index, norm = self.norm(edge_index, x.size(self.node_dim),
+                                         edge_weight, dtype=x.dtype)
 
             for k in range(self.K):
                 x = self.propagate(edge_index, x=x, norm=norm)
             self.cached_result = x
+            self.has_cached_result = True
 
         if self.cached:
-            x = self.lin(self.cached_result)
+            tempx = self.cached_result
+            assert tempx is not None
+            x = self.lin(tempx)
 
         return x
 
