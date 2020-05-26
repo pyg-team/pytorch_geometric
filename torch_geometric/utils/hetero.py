a886627f410051152fee753f17d1556f44248279
rusty1s <matthias.fey@tu-dortmund.de>
2020-05-26 17:14:54 +0200

    hetero grouping

---
+++ torch_geometric/utils/hetero.py
@@ -0,0 +1,39 @@
+import torch
+
+from .num_nodes import maybe_num_nodes_dict
+
+
+def group_hetero_graph(edge_index_dict, num_nodes_dict=None):
+    num_nodes_dict = maybe_num_nodes_dict(edge_index_dict, num_nodes_dict)
+
+    tmp = list(edge_index_dict.values())[0]
+
+    key2int = {}
+
+    cumsum, offset = 0, {}  # Helper data.
+    node_types, local_node_indices = [], []
+    local2global = {}
+    for i, (key, N) in enumerate(num_nodes_dict.items()):
+        key2int[key] = i
+        node_types.append(tmp.new_full((N, ), i))
+        local_node_indices.append(torch.arange(N, device=tmp.device))
+        offset[key] = cumsum
+        local2global[key] = local_node_indices[-1] + cumsum
+        local2global[i] = local2global[key]
+        cumsum += N
+
+    node_type = torch.cat(node_types, dim=0)
+    local_node_idx = torch.cat(local_node_indices, dim=0)
+
+    edge_indices, edge_types = [], []
+    for i, (keys, edge_index) in enumerate(edge_index_dict.items()):
+        key2int[keys] = i
+        inc = torch.tensor([offset[keys[0]], offset[keys[-1]]]).view(2, 1)
+        edge_indices.append(edge_index + inc.to(tmp.device))
+        edge_types.append(tmp.new_full((edge_index.size(1), ), i))
+
+    edge_index = torch.cat(edge_indices, dim=-1)
+    edge_type = torch.cat(edge_types, dim=0)
+
+    return (edge_index, edge_type, node_type, local_node_idx, local2global,
+            key2int)
--- torch_geometric/utils/num_nodes.py
+++ torch_geometric/utils/num_nodes.py
@@ -1,2 +1,25 @@
-def maybe_num_nodes(index, num_nodes=None):
-    return index.max().item() + 1 if num_nodes is None else num_nodes
+from copy import copy
+
+
+def maybe_num_nodes(edge_index, num_nodes=None):
+    return edge_index.max().item() + 1 if num_nodes is None else num_nodes
+
+
+def maybe_num_nodes_dict(edge_index_dict, num_nodes_dict=None):
+    num_nodes_dict = {} if num_nodes_dict is None else copy(num_nodes_dict)
+
+    found_types = list(num_nodes_dict.keys())
+
+    for keys, edge_index in edge_index_dict.items():
+
+        key = keys[0]
+        if key not in found_types:
+            N = int(edge_index[0].max() + 1)
+            num_nodes_dict[key] = max(N, num_nodes_dict.get(key, N))
+
+        key = keys[-1]
+        if key not in found_types:
+            N = int(edge_index[1].max() + 1)
+            num_nodes_dict[key] = max(N, num_nodes_dict.get(key, N))
+
+    return num_nodes_dict
