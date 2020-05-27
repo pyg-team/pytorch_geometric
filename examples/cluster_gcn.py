ab1d74868183e211b8ae7aa155cdcbb5f43843d8
rusty1s <matthias.fey@tu-dortmund.de>
2020-05-27 12:40:43 +0200

    perform inference on the full-graph

--- examples/cluster_gcn.py
+++ examples/cluster_gcn.py
@@ -1,51 +1,83 @@
 import torch
 import torch.nn.functional as F
+from torch.nn import ModuleList
+from tqdm import tqdm
 from torch_geometric.datasets import Reddit
-from torch_geometric.data import ClusterData, ClusterLoader
+from torch_geometric.data import ClusterData, ClusterLoader, NeighborSampler
 from torch_geometric.nn import SAGEConv
 
 dataset = Reddit('../data/Reddit')
 data = dataset[0]
 
-print('Partioning the graph... (this may take a while)')
 cluster_data = ClusterData(data, num_parts=1500, recursive=False,
                            save_dir=dataset.processed_dir)
-loader = ClusterLoader(cluster_data, batch_size=20, shuffle=True,
-                       num_workers=6)
-print('Done!')
+train_loader = ClusterLoader(cluster_data, batch_size=20, shuffle=True,
+                             num_workers=12)
+
+subgraph_loader = NeighborSampler(data.edge_index, sizes=[-1], batch_size=1024,
+                                  shuffle=False, num_workers=12)
 
 
 class Net(torch.nn.Module):
     def __init__(self, in_channels, out_channels):
         super(Net, self).__init__()
-        self.conv1 = SAGEConv(in_channels, 128)
-        self.conv2 = SAGEConv(128, out_channels)
+        self.convs = ModuleList(
+            [SAGEConv(in_channels, 128),
+             SAGEConv(128, out_channels)])
 
     def forward(self, x, edge_index):
-        x = F.dropout(x, p=0.2, training=self.training)
-        x = F.relu(self.conv1(x, edge_index))
-        x = F.dropout(x, p=0.2, training=self.training)
-        x = self.conv2(x, edge_index)
-        return F.log_softmax(x, dim=1)
+        for i, conv in enumerate(self.convs):
+            x = conv(x, edge_index)
+            if i != len(self.convs) - 1:
+                x = F.relu(x)
+                x = F.dropout(x, p=0.5, training=self.training)
+        return F.log_softmax(x, dim=-1)
+
+    def inference(self, x_all):
+        pbar = tqdm(total=x_all.size(0) * len(self.convs))
+        pbar.set_description('Evaluating')
+
+        # Compute representations of nodes layer by layer, using *all*
+        # available edges. This leads to faster computation in contrast to
+        # immediately computing the final representations of each batch.
+        for i, conv in enumerate(self.convs):
+            xs = []
+            for batch_size, n_id, adj in subgraph_loader:
+                edge_index, _, size = adj.to(device)
+                x = x_all[n_id].to(device)
+                x_target = x[:size[1]]
+                x = conv((x, x_target), edge_index)
+                if i != len(self.convs) - 1:
+                    x = F.relu(x)
+                xs.append(x.cpu())
+
+                pbar.update(batch_size)
+
+            x_all = torch.cat(xs, dim=0)
+
+        pbar.close()
+
+        return x_all
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = Net(dataset.num_features, dataset.num_classes).to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
 
 
 def train():
     model.train()
+
     total_loss = total_nodes = 0
-    for data in loader:
-        data = data.to(device)
+    for batch in train_loader:
+        batch = batch.to(device)
         optimizer.zero_grad()
-        logits = model(data.x, data.edge_index)
-        loss = F.nll_loss(logits[data.train_mask], data.y[data.train_mask])
+        out = model(batch.x, batch.edge_index)
+        loss = F.nll_loss(out[batch.train_mask], batch.y[batch.train_mask])
         loss.backward()
         optimizer.step()
 
-        nodes = data.train_mask.sum().item()
+        nodes = batch.train_mask.sum().item()
         total_loss += loss.item() * nodes
         total_nodes += nodes
 
@@ -53,24 +85,24 @@ def train():
 
 
 @torch.no_grad()
-def test():
+def test():  # Inference should be performed on the full graph.
     model.eval()
-    total_correct, total_nodes = [0, 0, 0], [0, 0, 0]
-    for data in loader:
-        data = data.to(device)
-        logits = model(data.x, data.edge_index)
-        pred = logits.argmax(dim=1)
 
-        masks = [data.train_mask, data.val_mask, data.test_mask]
-        for i, mask in enumerate(masks):
-            total_correct[i] += (pred[mask] == data.y[mask]).sum().item()
-            total_nodes[i] += mask.sum().item()
+    out = model.inference(data.x)
+    y_pred = out.argmax(dim=-1)
 
-    return (torch.Tensor(total_correct) / torch.Tensor(total_nodes)).tolist()
+    accs = []
+    for mask in [data.train_mask, data.val_mask, data.test_mask]:
+        correct = y_pred[mask].eq(data.y[mask]).sum().item()
+        accs.append(correct / mask.sum().item())
+    return accs
 
 
 for epoch in range(1, 31):
     loss = train()
-    accs = test()
-    print('Epoch: {:02d}, Loss: {:.4f}, Train: {:.4f}, Val: {:.4f}, '
-          'Test: {:.4f}'.format(epoch, loss, *accs))
+    if epoch % 5 == 0:
+        train_acc, val_acc, test_acc = test()
+        print(f'Epoch: {epoch:02d}, Loss: {loss:.4f}, Train: {train_acc:.4f}, '
+              f'Val: {val_acc:.4f}, test: {test_acc:.4f}')
+    else:
+        print(f'Epoch: {epoch:02d}, Loss: {loss:.4f}')
