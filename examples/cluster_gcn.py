c2be4cfd504d8b30bd6406daa40f71d18d0d23df
CFF-Dream <44110141+CFF-Dream@users.noreply.github.com>
2020-06-07 21:20:01 +0800

    Update cluster_gcn.py
    Using cluster gcn to process PPI dataset

--- examples/cluster_gcn.py
+++ examples/cluster_gcn.py
@@ -1,108 +1,137 @@
+import os
+import time
+import math    
 import torch
+import numpy as np
+import torch.nn as nn
+import os.path as osp
 import torch.nn.functional as F
-from torch.nn import ModuleList
-from tqdm import tqdm
-from torch_geometric.datasets import Reddit
-from torch_geometric.data import ClusterData, ClusterLoader, NeighborSampler
-from torch_geometric.nn import SAGEConv
+from sklearn.metrics import f1_score 
+import torch_geometric.transforms as T
+from torch_geometric.data import Batch
+from torch_geometric.nn import ChebConv
+from torch_geometric.datasets import PPI
+from torch_geometric.datasets import Planetoid
+from cluster import ClusterData, ClusterLoader
+from torch_geometric.data import DataLoader, Data
 
-dataset = Reddit('../data/Reddit')
-data = dataset[0]
 
-cluster_data = ClusterData(data, num_parts=1500, recursive=False,
-                           save_dir=dataset.processed_dir)
-train_loader = ClusterLoader(cluster_data, batch_size=20, shuffle=True,
-                             num_workers=12)
+path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'PPI')
+train_dataset = PPI(path, split='train')    #20graphs
+val_dataset = PPI(path, split='val')        #2graphs
+test_dataset = PPI(path, split='test')      #2graphs
+dataset = PPI(path)
 
-subgraph_loader = NeighborSampler(data.edge_index, sizes=[-1], batch_size=1024,
-                                  shuffle=False, num_workers=12)
+
+train_data_list = [data for data in train_dataset]
+for data in train_data_list:
+    data.train_mask = torch.ones(data.num_nodes, dtype=torch.bool)
+    data.val_mask = torch.zeros(data.num_nodes, dtype=torch.bool)
+    data.test_mask = torch.zeros(data.num_nodes, dtype=torch.bool)
+    
+val_data_list = [data for data in val_dataset]
+for data in val_data_list:
+    data.train_mask = torch.zeros(data.num_nodes, dtype=torch.bool)
+    data.val_mask = torch.ones(data.num_nodes, dtype=torch.bool)
+    data.test_mask = torch.zeros(data.num_nodes, dtype=torch.bool)
+
+test_data_list = [data for data in test_dataset]
+for data in test_data_list:
+    data.train_mask = torch.zeros(data.num_nodes, dtype=torch.bool)
+    data.val_mask = torch.zeros(data.num_nodes, dtype=torch.bool)
+    data.test_mask = torch.ones(data.num_nodes, dtype=torch.bool)
+
+
+data = Batch.from_data_list(train_data_list + val_data_list + test_data_list)
+cluster_data = ClusterData(data, num_parts=50, recursive=False,
+                            save_dir=dataset.processed_dir)
+loader = ClusterLoader(cluster_data, batch_size=1, shuffle=True,
+                        num_workers=0)    
+
 
 
 class Net(torch.nn.Module):
     def __init__(self, in_channels, out_channels):
         super(Net, self).__init__()
-        self.convs = ModuleList(
-            [SAGEConv(in_channels, 128),
-             SAGEConv(128, out_channels)])
+        dim = 2048
+        self.gcn1 = ChebConv(in_channels, dim, K=1)
+        self.lin1 = nn.Linear(in_channels, dim)
+        
+        self.gcn2 = ChebConv(dim, dim, K=1)
+        self.lin2 = nn.Linear(dim, dim)
+        
+        self.gcn3 = ChebConv(dim, dim, K=1)
+        self.lin3 = nn.Linear(dim, dim)
+        
+        self.gcn4 = ChebConv(dim, dim, K=1)
+        self.lin4 = nn.Linear(dim, dim)
+        
+        self.gcn5 = ChebConv(dim, out_channels, K=1)
+        self.lin5 = nn.Linear(dim, out_channels)
+        
 
     def forward(self, x, edge_index):
-        for i, conv in enumerate(self.convs):
-            x = conv(x, edge_index)
-            if i != len(self.convs) - 1:
-                x = F.relu(x)
-                x = F.dropout(x, p=0.5, training=self.training)
-        return F.log_softmax(x, dim=-1)
+        x = F.elu(self.gcn1(x, edge_index) + self.lin1(x))
+        x = F.elu(self.gcn2(x, edge_index) + self.lin2(x))
+        x = F.elu(self.gcn3(x, edge_index) + self.lin3(x))
+        x = F.elu(self.gcn4(x, edge_index) + self.lin4(x))
+        x = self.gcn5(x, edge_index) + self.lin5(x)
 
-    def inference(self, x_all):
-        pbar = tqdm(total=x_all.size(0) * len(self.convs))
-        pbar.set_description('Evaluating')
-
-        # Compute representations of nodes layer by layer, using *all*
-        # available edges. This leads to faster computation in contrast to
-        # immediately computing the final representations of each batch.
-        for i, conv in enumerate(self.convs):
-            xs = []
-            for batch_size, n_id, adj in subgraph_loader:
-                edge_index, _, size = adj.to(device)
-                x = x_all[n_id].to(device)
-                x_target = x[:size[1]]
-                x = conv((x, x_target), edge_index)
-                if i != len(self.convs) - 1:
-                    x = F.relu(x)
-                xs.append(x.cpu())
-
-                pbar.update(batch_size)
-
-            x_all = torch.cat(xs, dim=0)
-
-        pbar.close()
-
-        return x_all
+        return x
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = Net(dataset.num_features, dataset.num_classes).to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
+loss_op = torch.nn.BCEWithLogitsLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.003)
 
 
 def train():
     model.train()
-
     total_loss = total_nodes = 0
-    for batch in train_loader:
-        batch = batch.to(device)
+    for data in loader:
+        #because some data.train_mask in loader are all False, it would cause the loss=nan
+        if data.train_mask.sum().item() == 0:
+            continue
+        data = data.to(device)
         optimizer.zero_grad()
-        out = model(batch.x, batch.edge_index)
-        loss = F.nll_loss(out[batch.train_mask], batch.y[batch.train_mask])
+        out = model(data.x, data.edge_index)
+        loss = loss_op(out[data.train_mask], data.y[data.train_mask])
         loss.backward()
-        optimizer.step()
-
-        nodes = batch.train_mask.sum().item()
+        optimizer.step()        
+        nodes = data.train_mask.sum().item()
         total_loss += loss.item() * nodes
         total_nodes += nodes
 
-    return total_loss / total_nodes
-
-
-@torch.no_grad()
-def test():  # Inference should be performed on the full graph.
+    return total_loss / total_nodes     
+        
+        
+def test():
     model.eval()
-
-    out = model.inference(data.x)
-    y_pred = out.argmax(dim=-1)
-
+    ys, preds = [[], [], []], [[], [], []]
     accs = []
-    for mask in [data.train_mask, data.val_mask, data.test_mask]:
-        correct = y_pred[mask].eq(data.y[mask]).sum().item()
-        accs.append(correct / mask.sum().item())
+    for data in loader:
+        data = data.to(device)
+        with torch.no_grad():
+            out = model(data.x, data.edge_index)
+        masks = [data.train_mask, data.val_mask, data.test_mask]
+        for i, mask in enumerate(masks):
+            ys[i].append(data.y[mask])
+            preds[i].append((out[mask]>0).float().cpu())
+            
+    for i in range(3):
+        y, pred = torch.cat(ys[i], dim=0).cpu(), torch.cat(preds[i], dim=0).cpu()
+        acc = f1_score(y, pred, average='micro') if pred.sum() > 0 else 0
+        accs.append(acc)
     return accs
-
-
-for epoch in range(1, 31):
-    loss = train()
-    if epoch % 5 == 0:
-        train_acc, val_acc, test_acc = test()
-        print(f'Epoch: {epoch:02d}, Loss: {loss:.4f}, Train: {train_acc:.4f}, '
-              f'Val: {val_acc:.4f}, test: {test_acc:.4f}')
-    else:
-        print(f'Epoch: {epoch:02d}, Loss: {loss:.4f}')
+        
+       
+for epoch in range(1, 500):
+    loss = train()     
+    accs= test()
+    print('Epoch: {:02d}, Loss: {:.4f}, Train: {:.4f}, Val: {:.4f}, Test: {:.4f}, '
+          .format(epoch, loss, *accs))
+        
+        
+        
+    
