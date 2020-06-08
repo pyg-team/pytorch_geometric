4511a4ef8da6432f137f2ebdbb2ab78a617215f3
rusty1s <matthias.fey@tu-dortmund.de>
2020-06-08 16:17:25 +0200

    nn conv

--- test/nn/conv/test_nn_conv.py
+++ test/nn/conv/test_nn_conv.py
@@ -13,16 +13,9 @@ def test_nn_conv():
     nn = Seq(Lin(3, 32), ReLU(), Lin(32, in_channels * out_channels))
     conv = NNConv(in_channels, out_channels, nn)
     assert conv.__repr__() == 'NNConv(16, 32)'
-    assert conv(x, edge_index, pseudo).size() == (num_nodes, out_channels)
+    out = conv(x, edge_index, pseudo)
+    assert out.size() == (num_nodes, out_channels)
 
-    jitcls = conv.jittable(x=x, edge_index=edge_index, edge_attr=pseudo)
-    jitconv = jitcls(in_channels, out_channels, nn)
-    jitconv.load_state_dict(conv.state_dict())
-    jittedconv = torch.jit.script(jitconv)
-    conv.eval()
-    jitconv.eval()
-    jittedconv.eval()
-    assert (torch.abs(conv(x, edge_index, pseudo) -
-            jitconv(x, edge_index, pseudo)) < 1e-6).all().item()
-    assert (torch.abs(conv(x, edge_index, pseudo) -
-            jittedconv(x, edge_index, pseudo)) < 1e-6).all().item()
+    jit_conv = conv.jittable(x=x, edge_index=edge_index, edge_attr=pseudo)
+    jit_conv = torch.jit.script(jit_conv)
+    assert jit_conv(x, edge_index, pseudo).tolist() == out.tolist()
