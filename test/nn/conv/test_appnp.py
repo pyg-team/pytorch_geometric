f8449d8979d825d7b902bed171df80a80872e7a5
rusty1s <matthias.fey@tu-dortmund.de>
2020-06-08 17:17:11 +0200

    appnp

--- test/nn/conv/test_appnp.py
+++ test/nn/conv/test_appnp.py
@@ -5,21 +5,19 @@ from torch_geometric.nn import APPNP
 def test_appnp():
     in_channels, out_channels = (16, 32)
     edge_index = torch.tensor([[0, 0, 0, 1, 2, 3], [1, 2, 3, 0, 0, 0]])
+    edge_weight = torch.rand(edge_index.size(1))
     num_nodes = edge_index.max().item() + 1
     x = torch.randn((num_nodes, in_channels))
 
     lin = torch.nn.Linear(in_channels, out_channels)
     conv = APPNP(K=10, alpha=0.1)
     assert conv.__repr__() == 'APPNP(K=10, alpha=0.1)'
-    assert conv(lin(x), edge_index).size() == (num_nodes, out_channels)
-    jitcls = conv.jittable(x=lin(x), edge_index=edge_index, full_eval=True)
-    jitconv = jitcls(K=10, alpha=0.1)
-    jitconv.load_state_dict(conv.state_dict())
-    jittedconv = torch.jit.script(jitconv)
-    conv.eval()
-    jitconv.eval()
-    jittedconv.eval()
-    assert (torch.abs(conv(x, edge_index) -
-            jitconv(x, edge_index)) < 1e-6).all().item()
-    assert (torch.abs(conv(x, edge_index) -
-            jittedconv(x, edge_index)) < 1e-6).all().item()
+    out1 = conv(lin(x), edge_index)
+    assert out1.size() == (num_nodes, out_channels)
+    out2 = conv(lin(x), edge_index, edge_weight)
+    assert out2.size() == (num_nodes, out_channels)
+
+    jit_conv = conv.jittable(x=x, edge_index=edge_index)
+    jit_conv = torch.jit.script(jit_conv)
+    assert jit_conv(lin(x), edge_index).tolist() == out1.tolist()
+    assert jit_conv(lin(x), edge_index, edge_weight).tolist() == out2.tolist()
--- torch_geometric/nn/conv/appnp.py
+++ torch_geometric/nn/conv/appnp.py
@@ -1,9 +1,8 @@
-import torch
-from torch_scatter import scatter_add
-from torch_geometric.nn.conv import MessagePassing
-from torch_geometric.utils import add_remaining_self_loops
-
 from typing import Optional
+
+import torch
+from torch_geometric.nn.conv import MessagePassing
+from torch_geometric.nn.conv.gcn_conv import gcn_norm
 
 
 class APPNP(MessagePassing):
@@ -32,43 +31,16 @@ class APPNP(MessagePassing):
         **kwargs (optional): Additional arguments of
             :class:`torch_geometric.nn.conv.MessagePassing`.
     """
-    K: int
-    alpha: float
-
     def __init__(self, K, alpha, bias=True, **kwargs):
         super(APPNP, self).__init__(aggr='add', **kwargs)
-        self.K = K
-        self.alpha = alpha
-
-    def norm(self, edge_index, num_nodes: int,
-             edge_weight: Optional[torch.Tensor] = None,
-             improved: bool = False,
-             dtype: Optional[int] = None):
-        if edge_weight is None:
-            edge_weight = torch.ones((edge_index.size(1), ), dtype=dtype,
-                                     device=edge_index.device)
-
-        fill_value = 1 if not improved else 2
-        edge_index, edge_weight = add_remaining_self_loops(
-            edge_index, edge_weight, fill_value, num_nodes)
-
-        row, col = edge_index[0], edge_index[1]
-        assert edge_weight is not None
-        deg = scatter_add(edge_weight, row, dim=0, dim_size=num_nodes)
-        deg_inv_sqrt = deg.pow(-0.5)
-        mask = (deg_inv_sqrt == float('inf'))
-        zeros = torch.zeros(deg_inv_sqrt.shape,
-                            dtype=deg_inv_sqrt.dtype,
-                            device=deg_inv_sqrt.device)
-        deg_inv_sqrt = torch.where(mask, zeros, deg_inv_sqrt)
-
-        return edge_index, deg_inv_sqrt[row] * edge_weight * deg_inv_sqrt[col]
+        self.K: int = K
+        self.alpha: float = alpha
 
     def forward(self, x, edge_index,
                 edge_weight: Optional[torch.Tensor] = None):
         """"""
-        edge_index, norm = self.norm(edge_index, x.size(self.node_dim),
-                                     edge_weight, dtype=x.dtype)
+        edge_index, norm = gcn_norm(edge_index, x.size(self.node_dim),
+                                    edge_weight, dtype=x.dtype)
 
         hidden = x
         for k in range(self.K):
