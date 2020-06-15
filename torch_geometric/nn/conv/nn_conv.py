950ae747e055d41f3d2cad24e7ed0ddad34dead6
rusty1s <matthias.fey@tu-dortmund.de>
2020-06-15 14:18:40 +0200

    nnconv

--- torch_geometric/nn/conv/nn_conv.py
+++ torch_geometric/nn/conv/nn_conv.py
@@ -1,6 +1,8 @@
-from torch_geometric.typing import OptTensor
+from typing import Union, Tuple, Callable
+from torch_geometric.typing import OptTensor, OptPairTensor, Adj, Size
 
 import torch
+from torch import Tensor
 from torch.nn import Parameter
 from torch_geometric.nn.conv import MessagePassing
 
@@ -25,7 +27,8 @@ class NNConv(MessagePassing):
     a MLP.
 
     Args:
-        in_channels (int): Size of each input sample.
+        in_channels (int or tuple): Size of each input sample. A tuple
+            corresponds to the sizes of source and target dimensionalities.
         out_channels (int): Size of each output sample.
         nn (torch.nn.Module): A neural network :math:`h_{\mathbf{\Theta}}` that
             maps edge features :obj:`edge_attr` of shape :obj:`[-1,
@@ -43,8 +46,9 @@ class NNConv(MessagePassing):
         **kwargs (optional): Additional arguments of
             :class:`torch_geometric.nn.conv.MessagePassing`.
     """
-    def __init__(self, in_channels, out_channels, nn, aggr='add',
-                 root_weight=True, bias=True, **kwargs):
+    def __init__(self, in_channels: Union[int, Tuple[int, int]],
+                 out_channels: int, nn: Callable, aggr: str = 'add',
+                 root_weight: bool = True, bias: bool = True, **kwargs):
         super(NNConv, self).__init__(aggr=aggr, **kwargs)
 
         self.in_channels = in_channels
@@ -52,8 +56,13 @@ class NNConv(MessagePassing):
         self.nn = nn
         self.aggr = aggr
 
+        if isinstance(in_channels, int):
+            in_channels = (in_channels, in_channels)
+
+        self.in_channels_l = in_channels[0]
+
         if root_weight:
-            self.root = Parameter(torch.Tensor(in_channels, out_channels))
+            self.root = Parameter(torch.Tensor(in_channels[1], out_channels))
         else:
             self.register_parameter('root', None)
 
@@ -66,23 +75,32 @@ class NNConv(MessagePassing):
 
     def reset_parameters(self):
         reset(self.nn)
-        uniform(self.in_channels, self.root)
+        if self.root is not None:
+            uniform(self.root.size(0), self.root)
         zeros(self.bias)
 
-    def forward(self, x, edge_index, edge_attr: OptTensor = None):
+    def forward(self, x: Union[Tensor, OptPairTensor], edge_index: Adj,
+                edge_attr: OptTensor = None, size: Size = None) -> Tensor:
         """"""
-        # propagate_type: (x: Tensor, edge_attr: OptTensor)
-        out = self.propagate(edge_index, x=x, edge_attr=edge_attr, size=None)
-        if self.root is not None:
-            out += torch.matmul(x, self.root)
+        if isinstance(x, Tensor):
+            x: OptPairTensor = (x, x)
+
+        # propagate_type: (x: OptPairTensor, edge_attr: OptTensor)
+        out = self.propagate(edge_index, x=x, edge_attr=edge_attr, size=size)
+
+        x_r = x[1]
+        if x_r is not None and self.root is not None:
+            out += torch.matmul(x_r, self.root)
+
         if self.bias is not None:
             out += self.bias
+
         return out
 
-    def message(self, x_j, edge_attr: OptTensor):
+    def message(self, x_j: Tensor, edge_attr: OptTensor) -> Tensor:
         assert edge_attr is not None
         weight = self.nn(edge_attr)
-        weight = weight.view(-1, self.in_channels, self.out_channels)
+        weight = weight.view(-1, self.in_channels_l, self.out_channels)
         return torch.matmul(x_j.unsqueeze(1), weight).squeeze(1)
 
     def __repr__(self):
