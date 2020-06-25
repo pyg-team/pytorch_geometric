327fc5fb36c2dccea558740c16adf5e7eef1b41f
rusty1s <matthias.fey@tu-dortmund.de>
2020-06-25 11:51:26 +0200

    graph conv

--- torch_geometric/nn/conv/graph_conv.py
+++ torch_geometric/nn/conv/graph_conv.py
@@ -1,7 +1,9 @@
-from torch_geometric.typing import OptTensor
+from typing import Union, Tuple
+from torch_geometric.typing import OptTensor, OptPairTensor, Adj, Size
 
 from torch import Tensor
 from torch.nn import Linear
+from torch_sparse import SparseTensor, matmul
 from torch_geometric.nn.conv import MessagePassing
 
 
@@ -15,7 +17,8 @@ class GraphConv(MessagePassing):
         \sum_{j \in \mathcal{N}(i)} \mathbf{\Theta}_2 \mathbf{x}_j.
 
     Args:
-        in_channels (int): Size of each input sample.
+        in_channels (int or tuple): Size of each input sample. A tuple
+            corresponds to the sizes of source and target dimensionalities.
         out_channels (int): Size of each output sample.
         aggr (string, optional): The aggregation scheme to use
             (:obj:`"add"`, :obj:`"mean"`, :obj:`"max"`).
@@ -25,32 +28,49 @@ class GraphConv(MessagePassing):
         **kwargs (optional): Additional arguments of
             :class:`torch_geometric.nn.conv.MessagePassing`.
     """
-    def __init__(self, in_channels, out_channels, aggr='add', bias=True,
+    def __init__(self, in_channels: Union[int, Tuple[int, int]],
+                 out_channels: int, aggr: str = 'add', bias: bool = True,
                  **kwargs):
         super(GraphConv, self).__init__(aggr=aggr, **kwargs)
 
         self.in_channels = in_channels
         self.out_channels = out_channels
 
-        self.lin_rel = Linear(in_channels, out_channels, bias=False)
-        self.lin_root = Linear(in_channels, out_channels, bias=bias)
+        if isinstance(in_channels, int):
+            in_channels = (in_channels, in_channels)
+
+        self.lin_l = Linear(in_channels[0], out_channels, bias=bias)
+        self.lin_r = Linear(in_channels[1], out_channels, bias=False)
 
         self.reset_parameters()
 
     def reset_parameters(self):
-        self.lin_rel.reset_parameters()
-        self.lin_root.reset_parameters()
+        self.lin_l.reset_parameters()
+        self.lin_r.reset_parameters()
 
-    def forward(self, x, edge_index, edge_weight: OptTensor = None):
+    def forward(self, x: Union[Tensor, OptPairTensor], edge_index: Adj,
+                edge_weight: OptTensor = None, size: Size = None) -> Tensor:
         """"""
-        h = self.lin_rel(x)
-        # propagate_type: (x: Tensor, norm: OptTensor)
-        out = self.propagate(edge_index, x=h, norm=edge_weight, size=None)
-        out += self.lin_root(x)
+        if isinstance(x, Tensor):
+            x: OptPairTensor = (x, x)
+
+        # propagate_type: (x: OptPairTensor, edge_weight: OptTensor)
+        out = self.propagate(edge_index, x=x, edge_weight=edge_weight,
+                             size=size)
+        out = self.lin_l(out)
+
+        x_r = x[1]
+        if x_r is not None:
+            out += self.lin_r(x_r)
+
         return out
 
-    def message(self, x_j: Tensor, norm: OptTensor) -> Tensor:
-        return norm.view(-1, 1) * x_j if norm is not None else x_j
+    def message(self, x_j: Tensor, edge_weight: OptTensor) -> Tensor:
+        return x_j if edge_weight is None else edge_weight.view(-1, 1) * x_j
+
+    def message_and_aggregate(self, adj_t: SparseTensor,
+                              x: OptPairTensor) -> Tensor:
+        return matmul(adj_t, x[0], reduce=self.aggr)
 
     def __repr__(self):
         return '{}({}, {})'.format(self.__class__.__name__, self.in_channels,
