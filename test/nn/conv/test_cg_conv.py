60d8849b1969b30acd93fdecfab8b4f5aff7e44f
rusty1s <matthias.fey@tu-dortmund.de>
2020-06-26 14:22:03 +0200

    cg conv -> bipartite + sparsetensor

--- test/nn/conv/test_cg_conv.py
+++ test/nn/conv/test_cg_conv.py
@@ -1,17 +1,45 @@
 import torch
+from torch_sparse import SparseTensor
 from torch_geometric.nn import CGConv
 
 
 def test_cg_conv():
-    edge_index = torch.tensor([[0, 0, 0, 1, 2, 3], [1, 2, 3, 0, 0, 0]])
-    num_nodes = edge_index.max().item() + 1
-    x = torch.randn((num_nodes, 16))
-    pseudo = torch.rand((edge_index.size(1), 3))
+    x1 = torch.randn(4, 8)
+    x2 = torch.randn(2, 16)
+    edge_index = torch.tensor([[0, 1, 2, 3], [0, 0, 1, 1]])
+    row, col = edge_index
+    value = torch.rand(row.size(0), 3)
+    adj = SparseTensor(row=row, col=col, value=value, sparse_sizes=(4, 4))
 
-    conv = CGConv(16, 3)
-    assert conv.__repr__() == 'CGConv(16, 16, dim=3)'
-    out = conv(x, edge_index, pseudo)
-    assert out.size() == (num_nodes, 16)
+    conv = CGConv(8, dim=3)
+    assert conv.__repr__() == 'CGConv(8, dim=3)'
+    out = conv(x1, edge_index, value)
+    assert out.size() == (4, 8)
+    assert conv(x1, edge_index, value, size=(4, 4)).tolist() == out.tolist()
+    assert conv(x1, adj.t()).tolist() == out.tolist()
 
-    jit = torch.jit.script(conv.jittable())
-    assert jit(x, edge_index, pseudo).tolist() == out.tolist()
+    t = '(Tensor, Tensor, OptTensor, Size) -> Tensor'
+    jit = torch.jit.script(conv.jittable(t))
+    assert jit(x1, edge_index, value).tolist() == out.tolist()
+    assert jit(x1, edge_index, value, size=(4, 4)).tolist() == out.tolist()
+
+    t = '(Tensor, SparseTensor, OptTensor, Size) -> Tensor'
+    jit = torch.jit.script(conv.jittable(t))
+    assert jit(x1, adj.t()).tolist() == out.tolist()
+
+    adj = adj.sparse_resize((4, 2))
+    conv = CGConv((8, 16), dim=3)
+    assert conv.__repr__() == 'CGConv((8, 16), dim=3)'
+    out = conv((x1, x2), edge_index, value)
+    assert out.size() == (2, 16)
+    assert conv((x1, x2), edge_index, value, (4, 2)).tolist() == out.tolist()
+    assert conv((x1, x2), adj.t()).tolist() == out.tolist()
+
+    t = '(PairTensor, Tensor, OptTensor, Size) -> Tensor'
+    jit = torch.jit.script(conv.jittable(t))
+    assert jit((x1, x2), edge_index, value).tolist() == out.tolist()
+    assert jit((x1, x2), edge_index, value, (4, 2)).tolist() == out.tolist()
+
+    t = '(PairTensor, SparseTensor, OptTensor, Size) -> Tensor'
+    jit = torch.jit.script(conv.jittable(t))
+    assert jit((x1, x2), adj.t()).tolist() == out.tolist()
--- torch_geometric/nn/conv/cg_conv.py
+++ torch_geometric/nn/conv/cg_conv.py
@@ -1,6 +1,8 @@
-from torch_geometric.typing import OptTensor
+from typing import Union, Tuple
+from torch_geometric.typing import PairTensor, Adj, OptTensor, Size
 
 import torch
+from torch import Tensor
 from torch.nn import Linear
 import torch.nn.functional as F
 from torch_geometric.nn.conv import MessagePassing
@@ -25,8 +27,9 @@ class CGConv(MessagePassing):
     functions, respectively.
 
     Args:
-        channels (int): Size of each input sample.
-        dim (int): Edge feature dimensionality.
+        channels (int or tuple): Size of each input sample. A tuple
+            corresponds to the sizes of source and target dimensionalities.
+        dim (int, optional): Edge feature dimensionality. (default: :obj:`0`)
         aggr (string, optional): The aggregation operator to use
             (:obj:`"add"`, :obj:`"mean"`, :obj:`"max"`).
             (default: :obj:`"add"`)
@@ -35,14 +38,17 @@ class CGConv(MessagePassing):
         **kwargs (optional): Additional arguments of
             :class:`torch_geometric.nn.conv.MessagePassing`.
     """
-    def __init__(self, channels, dim, aggr='add', bias=True, **kwargs):
+    def __init__(self, channels: Union[int, Tuple[int, int]], dim: int = 0,
+                 aggr: str = 'add', bias: bool = True, **kwargs):
         super(CGConv, self).__init__(aggr=aggr, **kwargs)
-        self.in_channels = channels
-        self.out_channels = channels
+        self.channels = channels
         self.dim = dim
 
-        self.lin_f = Linear(2 * channels + dim, channels, bias=bias)
-        self.lin_s = Linear(2 * channels + dim, channels, bias=bias)
+        if isinstance(channels, int):
+            channels = (channels, channels)
+
+        self.lin_f = Linear(sum(channels) + dim, channels[1], bias=bias)
+        self.lin_s = Linear(sum(channels) + dim, channels[1], bias=bias)
 
         self.reset_parameters()
 
@@ -50,19 +56,24 @@ class CGConv(MessagePassing):
         self.lin_f.reset_parameters()
         self.lin_s.reset_parameters()
 
-    def forward(self, x, edge_index, edge_attr: OptTensor = None):
+    def forward(self, x: Union[Tensor, PairTensor], edge_index: Adj,
+                edge_attr: OptTensor = None, size: Size = None):
         """"""
-        # propagate_type: (x: Tensor, edge_attr: OptTensor)
-        out = self.propagate(edge_index, x=x, edge_attr=edge_attr, size=None)
-        out += x
+        if isinstance(x, Tensor):
+            x: PairTensor = (x, x)
+
+        # propagate_type: (x: PairTensor, edge_attr: OptTensor)
+        out = self.propagate(edge_index, x=x, edge_attr=edge_attr, size=size)
+        out += x[1]
         return out
 
     def message(self, x_i, x_j, edge_attr: OptTensor):
-        assert edge_attr is not None
-        z = torch.cat([x_i, x_j, edge_attr], dim=-1)
+        if edge_attr is None:
+            z = torch.cat([x_i, x_j], dim=-1)
+        else:
+            z = torch.cat([x_i, x_j, edge_attr], dim=-1)
         return self.lin_f(z).sigmoid() * F.softplus(self.lin_s(z))
 
     def __repr__(self):
-        return '{}({}, {}, dim={})'.format(self.__class__.__name__,
-                                           self.in_channels, self.out_channels,
-                                           self.dim)
+        return '{}({}, dim={})'.format(self.__class__.__name__, self.channels,
+                                       self.dim)
