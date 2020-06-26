466aefc43b4293d0cfb0f4d897d0ec7191948de1
rusty1s <matthias.fey@tu-dortmund.de>
2020-06-26 11:26:22 +0200

    create subfolder for jit modules

--- torch_geometric/nn/conv/utils/jit.py
+++ torch_geometric/nn/conv/utils/jit.py
@@ -1,10 +1,14 @@
 import sys
-from tempfile import NamedTemporaryFile
+import os.path as osp
+from tempfile import NamedTemporaryFile as TempFile
 from importlib.util import spec_from_file_location, module_from_spec
+from torch_geometric.data.makedirs import makedirs
 
 
 def class_from_module_repr(cls_name, module_repr):
-    with NamedTemporaryFile(mode='w+', suffix='.py', delete=False) as f:
+    path = osp.join('/tmp', 'pyg_jit')
+    makedirs(path)
+    with TempFile(mode='w+', suffix='.py', delete=False, dir=path) as f:
         f.write(module_repr)
     spec = spec_from_file_location(cls_name, f.name)
     mod = module_from_spec(spec)
