d93e9a8800ce74c2a46f62f0f1e33e7e75f6fd27
rusty1s <matthias.fey@tu-dortmund.de>
2020-06-26 14:23:54 +0200

    test with no edge features

--- test/nn/conv/test_cg_conv.py
+++ test/nn/conv/test_cg_conv.py
@@ -8,6 +8,47 @@ def test_cg_conv():
     x2 = torch.randn(2, 16)
     edge_index = torch.tensor([[0, 1, 2, 3], [0, 0, 1, 1]])
     row, col = edge_index
+    adj = SparseTensor(row=row, col=col, sparse_sizes=(4, 4))
+
+    conv = CGConv(8)
+    assert conv.__repr__() == 'CGConv(8, dim=0)'
+    out = conv(x1, edge_index)
+    assert out.size() == (4, 8)
+    assert conv(x1, edge_index, size=(4, 4)).tolist() == out.tolist()
+    assert conv(x1, adj.t()).tolist() == out.tolist()
+
+    t = '(Tensor, Tensor, OptTensor, Size) -> Tensor'
+    jit = torch.jit.script(conv.jittable(t))
+    assert jit(x1, edge_index).tolist() == out.tolist()
+    assert jit(x1, edge_index, size=(4, 4)).tolist() == out.tolist()
+
+    t = '(Tensor, SparseTensor, OptTensor, Size) -> Tensor'
+    jit = torch.jit.script(conv.jittable(t))
+    assert jit(x1, adj.t()).tolist() == out.tolist()
+
+    adj = adj.sparse_resize((4, 2))
+    conv = CGConv((8, 16))
+    assert conv.__repr__() == 'CGConv((8, 16), dim=0)'
+    out = conv((x1, x2), edge_index)
+    assert out.size() == (2, 16)
+    assert conv((x1, x2), edge_index, size=(4, 2)).tolist() == out.tolist()
+    assert conv((x1, x2), adj.t()).tolist() == out.tolist()
+
+    t = '(PairTensor, Tensor, OptTensor, Size) -> Tensor'
+    jit = torch.jit.script(conv.jittable(t))
+    assert jit((x1, x2), edge_index).tolist() == out.tolist()
+    assert jit((x1, x2), edge_index, size=(4, 2)).tolist() == out.tolist()
+
+    t = '(PairTensor, SparseTensor, OptTensor, Size) -> Tensor'
+    jit = torch.jit.script(conv.jittable(t))
+    assert jit((x1, x2), adj.t()).tolist() == out.tolist()
+
+
+def test_cg_conv_with_edge_features():
+    x1 = torch.randn(4, 8)
+    x2 = torch.randn(2, 16)
+    edge_index = torch.tensor([[0, 1, 2, 3], [0, 0, 1, 1]])
+    row, col = edge_index
     value = torch.rand(row.size(0), 3)
     adj = SparseTensor(row=row, col=col, value=value, sparse_sizes=(4, 4))
 
