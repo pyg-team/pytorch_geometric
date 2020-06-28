06c51440effe6615eb13d7cecad199bca09b3fea
rusty1s <matthias.fey@tu-dortmund.de>
2020-06-28 08:15:04 +0200

    added batch norm argument to .

--- torch_geometric/nn/conv/cg_conv.py
+++ torch_geometric/nn/conv/cg_conv.py
@@ -3,8 +3,8 @@ from torch_geometric.typing import PairTensor, Adj, OptTensor, Size
 
 import torch
 from torch import Tensor
-from torch.nn import Linear
 import torch.nn.functional as F
+from torch.nn import Linear, BatchNorm1d
 from torch_geometric.nn.conv import MessagePassing
 
 
@@ -33,41 +33,48 @@ class CGConv(MessagePassing):
         aggr (string, optional): The aggregation operator to use
             (:obj:`"add"`, :obj:`"mean"`, :obj:`"max"`).
             (default: :obj:`"add"`)
+        batch_norm (bool, optional): If set to :obj:`True`, will make use of
+            batch normalization. (default: :obj:`False`)
         bias (bool, optional): If set to :obj:`False`, the layer will not learn
             an additive bias. (default: :obj:`True`)
         **kwargs (optional): Additional arguments of
             :class:`torch_geometric.nn.conv.MessagePassing`.
     """
     def __init__(self, channels: Union[int, Tuple[int, int]], dim: int = 0,
-                 aggr: str = 'add', bias: bool = True, **kwargs):
+                 aggr: str = 'add', batch_norm: bool = False,
+                 bias: bool = True, **kwargs):
         super(CGConv, self).__init__(aggr=aggr, **kwargs)
         self.channels = channels
         self.dim = dim
+        self.batch_norm = batch_norm
 
         if isinstance(channels, int):
             channels = (channels, channels)
 
         self.lin_f = Linear(sum(channels) + dim, channels[1], bias=bias)
         self.lin_s = Linear(sum(channels) + dim, channels[1], bias=bias)
+        self.bn = BatchNorm1d(channels[1])
 
         self.reset_parameters()
 
     def reset_parameters(self):
         self.lin_f.reset_parameters()
         self.lin_s.reset_parameters()
+        self.bn.reset_parameters()
 
     def forward(self, x: Union[Tensor, PairTensor], edge_index: Adj,
-                edge_attr: OptTensor = None, size: Size = None):
+                edge_attr: OptTensor = None, size: Size = None) -> Tensor:
         """"""
         if isinstance(x, Tensor):
             x: PairTensor = (x, x)
 
         # propagate_type: (x: PairTensor, edge_attr: OptTensor)
         out = self.propagate(edge_index, x=x, edge_attr=edge_attr, size=size)
+        out = self.bn(out) if self.batch_norm else out
         out += x[1]
         return out
 
-    def message(self, x_i, x_j, edge_attr: OptTensor):
+    def message(self, x_i, x_j, edge_attr: OptTensor) -> Tensor:
         if edge_attr is None:
             z = torch.cat([x_i, x_j], dim=-1)
         else:
