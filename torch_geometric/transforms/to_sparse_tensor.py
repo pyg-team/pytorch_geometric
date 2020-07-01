7abf8eb931f060f20e7d88daf4fff7bc3a04d100
rusty1s <matthias.fey@tu-dortmund.de>
2020-07-01 08:45:41 +0200

    docstring

--- torch_geometric/transforms/to_sparse_tensor.py
+++ torch_geometric/transforms/to_sparse_tensor.py
@@ -2,22 +2,33 @@ from torch_sparse import SparseTensor
 
 
 class ToSparseTensor(object):
-    r"""Converts the :obj:`edge_index` format into the sparse tensor format
-    of :class:`torch_sparse.SparseTensor`.
+    r"""Converts the :obj:`edge_index` attribute of a data object into a
+    (transposed) :class:`torch_sparse.SparseTensor` type with key
+    :obj:`adj_.t`.
+
+    Args:
+        remove_faces (bool, optional): If set to :obj:`False`, the
+            :obj:`edge_index` tensor will not be removed.
     """
+    def __init__(self, remove_edge_index: bool = True):
+        self.remove_edge_index = remove_edge_index
+
     def __call__(self, data):
         assert data.edge_index is not None
 
         (row, col), N, E = data.edge_index, data.num_nodes, data.num_edges
-        data.edge_index = None
         perm = (col * N + row).argsort()
         row, col = row[perm], col[perm]
+
+        if self.remove_edge_index:
+            data.edge_index = None
 
         value = None
         for key in ['edge_weight', 'edge_attr', 'edge_type']:
             if data[key] is not None:
                 value = data[key][perm]
-                data[key] = None
+                if self.remove_edge_index:
+                    data[key] = None
                 break
 
         for key, item in data:
