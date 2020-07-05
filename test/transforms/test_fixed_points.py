2575e473448ac17aa1f33d8a7d6cdb335d5a1d0b
rusty1s <matthias.fey@tu-dortmund.de>
2020-07-05 18:11:33 +0200

    add duplicates option

--- test/transforms/test_fixed_points.py
+++ test/transforms/test_fixed_points.py
@@ -1,3 +1,5 @@
+from copy import copy
+
 import torch
 from torch_geometric.transforms import FixedPoints
 from torch_geometric.data import Data
@@ -7,29 +9,50 @@ def test_fixed_points():
     assert FixedPoints(1024).__repr__() == 'FixedPoints(1024, replace=True)'
 
     data = Data(
-        pos=torch.randn(100, 3), x=torch.randn(100, 16), y=torch.randn(1),
-        edge_attr=torch.randn(100, 3))
-    out = FixedPoints(50)(data)
+        pos=torch.randn(100, 3),
+        x=torch.randn(100, 16),
+        y=torch.randn(1),
+        edge_attr=torch.randn(100, 3),
+    )
+
+    out = FixedPoints(50, replace=True)(copy(data))
     assert len(out) == 4
     assert out.pos.size() == (50, 3)
     assert out.x.size() == (50, 16)
     assert out.y.size() == (1, )
     assert out.edge_attr.size() == (100, 3)
 
-    out = FixedPoints(50, replace=False)(data)
-    assert len(out) == 4
-    assert out.pos.size() == (50, 3)
-
-    data = Data(
-        pos=torch.randn(100, 3), x=torch.randn(100, 16), y=torch.randn(1),
-        edge_attr=torch.randn(100, 3))
-    out = FixedPoints(200)(data)
+    out = FixedPoints(200, replace=True)(copy(data))
     assert len(out) == 4
     assert out.pos.size() == (200, 3)
     assert out.x.size() == (200, 16)
     assert out.y.size() == (1, )
     assert out.edge_attr.size() == (100, 3)
 
-    out = FixedPoints(200, replace=False)(data)
+    out = FixedPoints(50, replace=False, allow_duplicates=False)(copy(data))
+    assert len(out) == 4
+    assert out.pos.size() == (50, 3)
+    assert out.x.size() == (50, 16)
+    assert out.y.size() == (1, )
+    assert out.edge_attr.size() == (100, 3)
+
+    out = FixedPoints(200, replace=False, allow_duplicates=False)(copy(data))
+    assert len(out) == 4
+    assert out.pos.size() == (100, 3)
+    assert out.x.size() == (100, 16)
+    assert out.y.size() == (1, )
+    assert out.edge_attr.size() == (100, 3)
+
+    out = FixedPoints(50, replace=False, allow_duplicates=True)(copy(data))
+    assert len(out) == 4
+    assert out.pos.size() == (50, 3)
+    assert out.x.size() == (50, 16)
+    assert out.y.size() == (1, )
+    assert out.edge_attr.size() == (100, 3)
+
+    out = FixedPoints(200, replace=False, allow_duplicates=True)(copy(data))
     assert len(out) == 4
     assert out.pos.size() == (200, 3)
+    assert out.x.size() == (200, 16)
+    assert out.y.size() == (1, )
+    assert out.edge_attr.size() == (100, 3)
--- torch_geometric/transforms/fixed_points.py
+++ torch_geometric/transforms/fixed_points.py
@@ -1,5 +1,3 @@
-from __future__ import division
-
 import re
 import math
 
@@ -13,25 +11,35 @@ class FixedPoints(object):
 
     Args:
         num (int): The number of points to sample.
-        replace (bool, optional): If set to :obj:`False`, samples fixed
-            points without replacement. In case :obj:`num` is greater than
-            the number of points, duplicated points are kept to a
-            minimum. (default: :obj:`True`)
+        replace (bool, optional): If set to :obj:`False`, samples points
+            without replacement. (default: :obj:`True`)
+        allow_duplicates (bool, optional): In case :obj:`replace` is
+            :obj`False` and :obj:`num` is greater than the number of points,
+            this option determines whether to add duplicated nodes to the
+            output points or not.
+            In case :obj:`allow_duplicates` is :obj:`False`, the number of
+            output points might be smaller than :obj:`num`.
+            In case :obj:`allow_duplicates` is :obj:`True`, the number of
+            duplicated points are kept to a minimum. (default: :obj:`False`)
     """
-    def __init__(self, num, replace=True):
+    def __init__(self, num, replace=True, allow_duplicates=False):
         self.num = num
         self.replace = replace
+        self.allow_duplicates = allow_duplicates
 
     def __call__(self, data):
         num_nodes = data.num_nodes
 
         if self.replace:
             choice = np.random.choice(num_nodes, self.num, replace=True)
+            choice = torch.from_numpy(choice).to(torch.long)
+        elif not self.allow_duplicates:
+            choice = torch.randperm(num_nodes)[:self.num]
         else:
             choice = torch.cat([
                 torch.randperm(num_nodes)
                 for _ in range(math.ceil(self.num / num_nodes))
-            ], dim=0)[:min(self.num, num_nodes)]
+            ], dim=0)[:self.num]
 
         for key, item in data:
             if bool(re.search('edge', key)):
