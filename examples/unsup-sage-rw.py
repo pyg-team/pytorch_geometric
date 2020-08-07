4f1956b90f3e9ed61927b23e27ab48fe064b1356
yuanx749 <yuanx749@gmail.com>
2020-08-07 20:10:38 +0200

    Add GraphSAGE example

---
+++ examples/unsup-sage-rw.py
@@ -0,0 +1,152 @@
+import os.path as osp
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from tqdm import tqdm
+from torch_geometric.datasets import Planetoid
+from torch_geometric.data import NeighborSampler
+from torch_geometric.nn import SAGEConv
+from torch_cluster import random_walk
+from sklearn.linear_model import LogisticRegression
+
+dataset = 'Cora'
+path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', dataset)
+dataset = Planetoid(path, dataset)
+data = dataset[0]
+
+num_samples = [10, 5]
+batch_size = 64
+hidden_channels = 32
+walk_length = 1
+epochs = 10
+
+subgraph_loader = NeighborSampler(
+    data.edge_index, node_idx=None,
+    sizes=[-1], batch_size=batch_size, shuffle=False,
+    num_workers=0)
+
+
+class SAGE(nn.Module):
+    def __init__(self, in_channels, hidden_channels):
+        super(SAGE, self).__init__()
+        self.num_layers = 2
+        self.convs = nn.ModuleList()
+        self.convs.append(
+            SAGEConv(in_channels, hidden_channels, normalize=True))
+        self.convs.append(
+            SAGEConv(hidden_channels, hidden_channels, normalize=True))
+
+    def forward(self, x, adjs):
+        # `train_loader` computes the k-hop neighborhood of a batch of nodes,
+        # and returns, for each layer, a bipartite graph object, holding the
+        # bipartite edges `edge_index`, the index `e_id` of the original edges,
+        # and the size/shape `size` of the bipartite graph.
+        # Target nodes are also included in the source nodes so that one can
+        # easily apply skip-connections or add self-loops.
+        for i, (edge_index, _, size) in enumerate(adjs):
+            x_target = x[:size[1]]  # Target nodes are always placed first.
+            x = self.convs[i]((x, x_target), edge_index)
+            if i != self.num_layers - 1:
+                x = F.relu(x)
+        return x
+
+    def inference(self, x_all):
+        pbar = tqdm(total=x_all.size(0) * self.num_layers)
+        pbar.set_description('Evaluating')
+        # Compute representations of nodes layer by layer, using *all*
+        # available edges. This leads to faster computation in contrast to
+        # immediately computing the final representations of each batch.
+        for i in range(self.num_layers):
+            xs = []
+            for batch_size, n_id, adj in subgraph_loader:
+                edge_index, _, size = adj.to(device)
+                x = x_all[n_id].to(device)
+                x_target = x[:size[1]]
+                x = self.convs[i]((x, x_target), edge_index)
+                if i != self.num_layers - 1:
+                    x = F.relu(x)
+                xs.append(x.cpu())
+                pbar.update(batch_size)
+            x_all = torch.cat(xs, dim=0)
+        pbar.close()
+        return x_all
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = SAGE(data.num_node_features, hidden_channels).to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+x = data.x.to(device)
+
+
+def train(epoch):
+    model.train()
+
+    pbar = tqdm(total=data.num_nodes)
+    pbar.set_description(f'Epoch {epoch:02d}')
+
+    total_loss = 0
+
+    node_idx = torch.randperm(data.num_nodes)  # shuffle all nodes
+    train_loader = NeighborSampler(
+        data.edge_index, node_idx=node_idx,
+        sizes=num_samples, batch_size=batch_size, shuffle=False,
+        num_workers=0)
+
+    rw = random_walk(
+        data.edge_index[0], data.edge_index[1],
+        node_idx, walk_length=walk_length)
+    rw_idx = rw[:, 1:].flatten()
+    pos_loader = NeighborSampler(
+        data.edge_index, node_idx=rw_idx,
+        sizes=num_samples, batch_size=batch_size * walk_length, shuffle=False,
+        num_workers=0)
+
+    neg_loader = NeighborSampler(
+        data.edge_index, node_idx=node_idx,
+        sizes=num_samples, batch_size=batch_size, shuffle=True,
+        num_workers=0)
+
+    for (batch_size_, u_id, adjs_u), (_, v_id, adjs_v), (_, vn_id, adjs_vn) in\
+            zip(train_loader, pos_loader, neg_loader):
+        # `adjs` holds a list of `(edge_index, e_id, size)` tuples.
+        adjs_u = [adj.to(device) for adj in adjs_u]
+        z_u = model(x[u_id], adjs_u)
+
+        adjs_v = [adj.to(device) for adj in adjs_v]
+        z_v = model(x[v_id], adjs_v)
+
+        adjs_vn = [adj.to(device) for adj in adjs_vn]
+        z_vn = model(x[vn_id], adjs_vn)
+
+        optimizer.zero_grad()
+        pos_loss = -F.logsigmoid(
+            (z_u.repeat_interleave(walk_length, dim=0)*z_v).sum(dim=1)).mean()
+        neg_loss = -F.logsigmoid(
+            -(z_u*z_vn).sum(dim=1)).mean()
+        loss = pos_loss + neg_loss
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item()
+        pbar.update(batch_size_)
+
+    pbar.close()
+
+    loss = total_loss / len(train_loader)
+
+    return loss
+
+
+for epoch in range(1, epochs):
+    loss = train(epoch)
+    print(f'Epoch {epoch:02d}, Loss: {loss:.4f}')
+
+
+z = model.inference(x).detach().cpu().numpy()
+y = data.y.detach().cpu().numpy()
+z_train, z_test = z[data.train_mask], z[data.test_mask]
+y_train, y_test = y[data.train_mask], y[data.test_mask]
+clf = LogisticRegression()
+clf.fit(z_train, y_train)
+print(f'Accuracy: {clf.score(z_test, y_test):.4f}')
