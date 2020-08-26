af8f0154f490b166351ffe5717ced2d67baa0dcc
rusty1s <matthias.fey@tu-dortmund.de>
2020-08-26 10:34:04 +0200

    fix karate test

--- test/datasets/test_karate.py
+++ test/datasets/test_karate.py
@@ -6,11 +6,12 @@ def test_karate():
 
     assert len(dataset) == 1
     assert dataset.num_features == 34
-    assert dataset.num_classes == 2
+    assert dataset.num_classes == 4
     assert dataset.__repr__() == 'KarateClub()'
 
-    assert len(dataset[0]) == 3
+    assert len(dataset[0]) == 4
     assert dataset[0].edge_index.size() == (2, 156)
     assert dataset[0].x.size() == (34, 34)
     assert dataset[0].y.size() == (34, )
-    assert dataset[0].y.sum().item() == 17
+    assert dataset[0].train_mask.size() == (34, )
+    assert dataset[0].train_mask.sum().item() == 4
