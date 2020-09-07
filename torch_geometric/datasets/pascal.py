3234dbf0b4fdce06b3fb1a54f75792c4b09f603c
rusty1s <matthias.fey@tu-dortmund.de>
2020-09-07 12:49:04 +0200

    fix index

--- torch_geometric/datasets/pascal.py
+++ torch_geometric/datasets/pascal.py
@@ -223,7 +223,7 @@ class PascalVOCKeypoints(InMemoryDataset):
 
             out1 = F.interpolate(vgg16_outputs[0], (256, 256), mode='bilinear',
                                  align_corners=False)
-            out2 = F.interpolate(vgg16_outputs[0], (256, 256), mode='bilinear',
+            out2 = F.interpolate(vgg16_outputs[1], (256, 256), mode='bilinear',
                                  align_corners=False)
 
             for j in range(out1.size(0)):
