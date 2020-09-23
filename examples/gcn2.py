4e3894852c5f2a3f31a996e7d484c076c270385f
rusty1s <matthias.fey@tu-dortmund.de>
2020-09-23 09:08:19 +0200

    use shared weights

--- examples/gcn2.py
+++ examples/gcn2.py
@@ -50,7 +50,7 @@ class Net(torch.nn.Module):
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = Net(hidden_channels=64, num_layers=64, alpha=0.1, theta=0.5,
-            shared_weights=False, dropout=0.6).to(device)
+            shared_weights=True, dropout=0.6).to(device)
 data = data.to(device)
 optimizer = torch.optim.Adam([
     dict(params=model.convs.parameters(), weight_decay=0.01),
