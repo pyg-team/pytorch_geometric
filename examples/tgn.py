50822819b664cd9d751d410a5eabd4cc0688311d
rusty1s <matthias.fey@tu-dortmund.de>
2020-11-03 11:22:36 +0100

    first try gnn + sampling

--- examples/tgn.py
+++ examples/tgn.py
@@ -2,9 +2,11 @@ import os.path as osp
 
 import torch
 from torch.nn import Linear
+from torch_sparse import SparseTensor
 from sklearn.metrics import average_precision_score, roc_auc_score
+
 from torch_geometric.datasets import JODIEDataset
-from torch_geometric.nn import TGN
+from torch_geometric.nn import TGN, SAGEConv
 from torch_geometric.nn.models.tgn import IdentityMessage, LastAggregator
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -12,14 +14,43 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'JODIE')
 dataset = JODIEDataset(path, name='wikipedia')
 data = dataset[0].to(device)
+
+
+class NeighborSampler(object):
+    def __init__(self, data, size):
+        self.size = size
+        self.device = data.src.device
+        self.adj = SparseTensor(row=data.src.cpu(), col=data.dst.cpu(),
+                                value=torch.arange(data.num_events),
+                                sparse_sizes=(data.num_nodes, data.num_nodes))
+
+    def __call__(self, n_id, threshold):
+        _, _, value = self.adj.coo()
+        mask = value < threshold
+        adj = self.adj.masked_select_nnz(mask, layout='coo').set_value_(None)
+        if adj.numel() == 0:
+            adj = adj.sparse_resize([n_id.numel(), n_id.numel()])
+        else:
+            adj, n_id = adj.sample_adj(n_id.cpu(), num_neighbors=self.size)
+        return adj.to(self.device), n_id.to(self.device)
+
+
+sampler = NeighborSampler(data, size=10)
 train_data, val_data, test_data = data.train_val_test_split(
     val_ratio=0.15, test_ratio=0.15)
 
-model = TGN(
-    data.num_nodes, data.msg.size(-1), memory_dim=100, time_dim=100,
-    message_module=IdentityMessage(raw_msg_dim=data.msg.size(-1),
-                                   memory_dim=100, time_dim=100),
-    aggregator_module=LastAggregator()).to(device)
+
+class GraphEmbedding(torch.nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(GraphEmbedding, self).__init__()
+        self.conv = SAGEConv(in_channels, out_channels)
+
+    def forward(self, x, adj_t):
+        if adj_t.nnz() > 0:
+            x = self.conv((x, x[:adj_t.size(0)]), adj_t)
+        else:
+            x = self.conv.lin_r(x)
+        return x.relu()
 
 
 class LinkPredictor(torch.nn.Module):
@@ -35,10 +66,17 @@ class LinkPredictor(torch.nn.Module):
         return self.lin_end(h)
 
 
+model = TGN(
+    data.num_nodes, data.msg.size(-1), memory_dim=100, time_dim=100,
+    message_module=IdentityMessage(raw_msg_dim=data.msg.size(-1),
+                                   memory_dim=100, time_dim=100),
+    aggregator_module=LastAggregator()).to(device)
+gnn = GraphEmbedding(in_channels=100, out_channels=100).to(device)
 link_pred = LinkPredictor(in_channels=100).to(device)
 
 optimizer = torch.optim.Adam(
-    list(model.parameters()) + list(link_pred.parameters()), lr=0.0001)
+    list(model.parameters()) + list(gnn.parameters()) +
+    list(link_pred.parameters()), lr=0.0001)
 criterion = torch.nn.BCEWithLogitsLoss()
 
 assoc = torch.empty(data.num_nodes, dtype=torch.long, device=device)
@@ -49,7 +87,7 @@ def train():
     model.reset_state()
     link_pred.train()
 
-    total_loss = 0
+    total_loss = current_event_id = 0
     for batch in train_data.seq_batches(batch_size=200):
         optimizer.zero_grad()
 
@@ -58,9 +96,13 @@ def train():
                                 dtype=torch.long, device=device)
 
         n_id = torch.cat([src, pos_dst, neg_dst]).unique()
+        # adj_t, n_id = sampler(n_id, threshold=current_event_id)
         assoc[n_id] = torch.arange(n_id.size(0), device=device)
 
-        z, last_update = model(n_id)
+        # Sample 1-hop neighborhood.
+
+        z, last_update = model(n_id)  # Get memory.
+        # z = gnn(z, adj_t)  # Embed memory via graph convolution.
 
         pos_out = link_pred(z[assoc[src]], z[assoc[pos_dst]])
         neg_out = link_pred(z[assoc[src]], z[assoc[neg_dst]])
@@ -74,6 +116,7 @@ def train():
         optimizer.step()
         model.detach_memory()
         total_loss += float(loss) * batch.num_events
+        current_event_id += batch.num_events
 
     return total_loss / train_data.num_events
 
@@ -85,15 +128,18 @@ def test(inference_data):
     torch.manual_seed(12345)  # Ensure deterministic sampling across epochs.
 
     aps, aucs = [], []
+    current_event_id = train_data.num_events
     for batch in inference_data.seq_batches(batch_size=200):
         src, pos_dst, t, msg = batch.src, batch.dst, batch.t, batch.msg
         neg_dst = torch.randint(0, train_data.num_nodes, (src.size(0), ),
                                 dtype=torch.long, device=device)
 
         n_id = torch.cat([src, pos_dst, neg_dst]).unique()
+        # adj_t, n_id = sampler(n_id, threshold=current_event_id)
         assoc[n_id] = torch.arange(n_id.size(0), device=device)
 
         z, last_update = model(n_id)
+        # z = gnn(z, adj_t)  # Embed memory via graph convolution.
 
         pos_out = link_pred(z[assoc[src]], z[assoc[pos_dst]])
         neg_out = link_pred(z[assoc[src]], z[assoc[neg_dst]])
@@ -107,6 +153,7 @@ def test(inference_data):
         aucs.append(roc_auc_score(y_true, y_pred))
 
         model.update_state(src, pos_dst, t, msg)
+        current_event_id += batch.num_events
 
     return float(torch.tensor(aps).mean()), float(torch.tensor(aucs).mean())
 
