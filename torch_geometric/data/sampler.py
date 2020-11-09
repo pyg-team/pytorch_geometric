3e09d3dbedfefd88e8ce3bc35224debe675d076a
rusty1s <matthias.fey@tu-dortmund.de>
2020-11-09 12:53:59 +0100

    add sparse tensor support for neighborsampler

--- torch_geometric/data/sampler.py
+++ torch_geometric/data/sampler.py
@@ -1,18 +1,33 @@
 import copy
-from typing import List, Optional, Tuple, NamedTuple
+from typing import List, Optional, Tuple, NamedTuple, Union
 
 import torch
+from torch import Tensor
 from torch_sparse import SparseTensor
 
+from torch_geometric.utils.num_nodes import maybe_num_nodes
 
-class Adj(NamedTuple):
-    edge_index: torch.Tensor
-    e_id: torch.Tensor
+
+class EdgeIndex(NamedTuple):
+    edge_index: Tensor
+    e_id: Optional[Tensor]
     size: Tuple[int, int]
 
     def to(self, *args, **kwargs):
-        return Adj(self.edge_index.to(*args, **kwargs),
-                   self.e_id.to(*args, **kwargs), self.size)
+        edge_index = self.edge_index.to(*args, **kwargs)
+        e_id = self.e_id.to(*args, **kwargs) if self.e_id is not None else None
+        return EdgeIndex(edge_index, e_id, self.size)
+
+
+class Adj(NamedTuple):
+    adj_t: SparseTensor
+    e_id: Optional[Tensor]
+    size: Tuple[int, int]
+
+    def to(self, *args, **kwargs):
+        adj_t = self.adj_t.to(*args, **kwargs)
+        e_id = self.e_id.to(*args, **kwargs) if self.e_id is not None else None
+        return Adj(adj_t, e_id, self.size)
 
 
 class NeighborSampler(torch.utils.data.DataLoader):
@@ -58,63 +73,93 @@ class NeighborSampler(torch.utils.data.DataLoader):
         ogbn_products_sage.py>`_.
 
     Args:
-        edge_index (LongTensor): The edge indices of the full-graph.
-        size ([int]): The number of neighbors to
-            sample for each node in each layer. If set to :obj:`sizes[i] = -1`,
-            all neighbors are included in layer :obj:`l`.
+        edge_index (Tensor or SparseTensor): A :obj:`torch.LongTensor` or a
+            :obj:`torch_sparse.SparseTensor` that defines the underlying graph
+            connectivity/message passing flow.
+            :obj:`edge_index` holds the indices of a (sparse) symmetric
+            adjacency matrix.
+            If :obj:`edge_index` is of type :obj:`torch.LongTensor`, its shape
+            must be defined as :obj:`[2, num_edges]`, where messages from nodes
+            :obj:`edge_index[0]` are sent to nodes in :obj:`edge_index[1]`
+            (in case :obj:`flow="source_to_target"`).
+            If :obj:`edge_index` is of type :obj:`torch_sparse.SparseTensor`,
+            its sparse indices :obj:`(row, col)` should relate to
+            :obj:`row = edge_index[1]` and :obj:`col = edge_index[0]`.
+            The major difference between both formats is that we need to input
+            the *transposed* sparse adjacency matrix.
+        size ([int]): The number of neighbors to sample for each node in each
+            layer. If set to :obj:`sizes[i] = -1`, all neighbors are included
+            in layer :obj:`l`.
         node_idx (LongTensor, optional): The nodes that should be considered
             for creating mini-batches. If set to :obj:`None`, all nodes will be
             considered.
-        flow (string, optional): The flow direction of message passing
-            (:obj:`"source_to_target"` or :obj:`"target_to_source"`).
-            (default: :obj:`"source_to_target"`)
+        num_nodes (int, optional): The number of nodes in the graph.
+            (default: :obj:`None`)
+        return_e_id (bool, optional): If set to :obj:`False`, will not return
+            original edge indices of sampled edges. This is only useful in case
+            when operating on graphs without edge features to save memory.
+            (default: :obj:`True`)
         **kwargs (optional): Additional arguments of
             :class:`torch.utils.data.DataLoader`, such as :obj:`batch_size`,
             :obj:`shuffle`, :obj:`drop_last` or :obj:`num_workers`.
     """
-    def __init__(self, edge_index: torch.Tensor, sizes: List[int],
-                 node_idx: Optional[torch.Tensor] = None,
-                 num_nodes: Optional[int] = None,
-                 flow: str = "source_to_target", **kwargs):
+    def __init__(self, edge_index: Union[Tensor, SparseTensor],
+                 sizes: List[int], node_idx: Optional[Tensor] = None,
+                 num_nodes: Optional[int] = None, return_e_id: bool = True,
+                 **kwargs):
 
-        edge_index = edge_index.cpu()
-        N = int(edge_index.max() + 1) if num_nodes is None else num_nodes
-        edge_attr = torch.arange(edge_index.size(1))
-        adj = SparseTensor(row=edge_index[0], col=edge_index[1],
-                           value=edge_attr, sparse_sizes=(N, N),
-                           is_sorted=False)
-        adj = adj.t() if flow == 'source_to_target' else adj
-        self.adj = adj.to('cpu')
+        self.sizes = sizes
+        self.return_e_id = return_e_id
+        self.is_sparse_tensor = isinstance(edge_index, SparseTensor)
+        self.__val__ = None
+
+        # Obtain a *transposed* `SparseTensor` instance.
+        edge_index = edge_index.to('cpu')
+        if not self.is_sparse_tensor:
+            num_nodes = maybe_num_nodes(edge_index, num_nodes)
+            value = torch.arange(edge_index.size(1)) if return_e_id else None
+            self.adj_t = SparseTensor(row=edge_index[0], col=edge_index[1],
+                                      value=value,
+                                      sparse_sizes=(num_nodes, num_nodes)).t()
+        else:
+            adj_t = edge_index
+            if return_e_id:
+                self.__val__ = adj_t.storage.value()
+                value = torch.arange(adj_t.nnz()) if return_e_id else self.val
+                adj_t = adj_t.set_value(value, layout='coo')
+            self.adj_t = adj_t
+
+        self.adj_t.storage.rowptr()
 
         if node_idx is None:
-            node_idx = torch.arange(N)
+            node_idx = torch.arange(self.adj_t.sparse_size(0))
         elif node_idx.dtype == torch.bool:
             node_idx = node_idx.nonzero(as_tuple=False).view(-1)
 
-        self.sizes = sizes
-        self.flow = flow
-        assert self.flow in ['source_to_target', 'target_to_source']
-
-        super(NeighborSampler, self).__init__(node_idx.tolist(),
-                                              collate_fn=self.sample, **kwargs)
+        super(NeighborSampler, self).__init__(
+            node_idx.view(-1).tolist(), collate_fn=self.sample, **kwargs)
 
     def sample(self, batch):
-        if not isinstance(batch, torch.Tensor):
+        if not isinstance(batch, Tensor):
             batch = torch.tensor(batch)
 
         batch_size: int = len(batch)
-        adjs: List[Adj] = []
 
+        adjs = []
         n_id = batch
         for size in self.sizes:
-            adj, n_id = self.adj.sample_adj(n_id, size, replace=False)
-            if self.flow == 'source_to_target':
-                adj = adj.t()
-            row, col, e_id = adj.coo()
-            size = adj.sparse_sizes()
-            edge_index = torch.stack([row, col], dim=0)
+            adj_t, n_id = self.adj_t.sample_adj(n_id, size, replace=False)
+            e_id = adj_t.storage.value()
+            size = adj_t.sparse_sizes()[::-1]
+            if self.__val__ is not None:
+                adj_t.set_value_(self.__val__[e_id], layout='coo')
 
-            adjs.append(Adj(edge_index, e_id, size))
+            if self.is_sparse_tensor:
+                adjs.append(Adj(adj_t, e_id, size))
+            else:
+                row, col, _ = adj_t.coo()
+                edge_index = torch.cat([col, row], dim=0)
+                adjs.append(EdgeIndex(edge_index, e_id, size))
 
         if len(adjs) > 1:
             return batch_size, n_id, adjs[::-1]
@@ -198,9 +243,9 @@ class RandomNodeSampler(torch.utils.data.DataLoader):
         data.edge_index = torch.stack([row, col], dim=0)
 
         for key, item in self.data:
-            if isinstance(item, torch.Tensor) and item.size(0) == self.N:
+            if isinstance(item, Tensor) and item.size(0) == self.N:
                 data[key] = item[node_idx]
-            elif isinstance(item, torch.Tensor) and item.size(0) == self.E:
+            elif isinstance(item, Tensor) and item.size(0) == self.E:
                 data[key] = item[edge_idx]
             else:
                 data[key] = item
