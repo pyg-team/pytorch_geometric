e772c5edcd3f1154c43f830ced280ae292c58de2
Torsten Wörtwein <twoertwein@gmail.com>
2020-11-09 01:28:40 -0500

    jitable: avoid permission issue for temporary file

--- torch_geometric/nn/conv/utils/jit.py
+++ torch_geometric/nn/conv/utils/jit.py
@@ -1,14 +1,16 @@
-import sys
 import os.path as osp
-from tempfile import NamedTemporaryFile as TempFile
-from importlib.util import spec_from_file_location, module_from_spec
+import sys
+from getpass import getuser
+from importlib.util import module_from_spec, spec_from_file_location
+from tempfile import NamedTemporaryFile, gettempdir
+
 from torch_geometric.data.makedirs import makedirs
 
 
 def class_from_module_repr(cls_name, module_repr):
-    path = osp.join('/tmp', 'pyg_jit')
+    path = osp.join(gettempdir(), f"{getuser()}_pyg_jit")
     makedirs(path)
-    with TempFile(mode='w+', suffix='.py', delete=False, dir=path) as f:
+    with NamedTemporaryFile(mode="w+", suffix=".py", delete=False, dir=path) as f:
         f.write(module_repr)
     spec = spec_from_file_location(cls_name, f.name)
     mod = module_from_spec(spec)
