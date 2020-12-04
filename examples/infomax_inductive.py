668661f4bd6bb5cb6e7e9f757e43ae36049118e4
rusty1s <matthias.fey@tu-dortmund.de>
2020-12-04 07:23:18 +0100

    clean up

--- examples/infomax_inductive.py
+++ examples/infomax_inductive.py
@@ -8,14 +8,6 @@ from torch_geometric.datasets import Reddit
 from torch_geometric.data import NeighborSampler
 from torch_geometric.nn import SAGEConv
 from torch_geometric.nn import DeepGraphInfomax
-'''
-In '"Deep Graph Infomax" <https://arxiv.org/abs/1809.10341>' two methods
-were provided, a transductive method for small graphs (see examples/infomax.py)
-and a inductive method for large graphs, implemented here. The inductive
-implementation utilises the mean pooling propagation rule as detailed in
-"Inductive Representation Learning on Large Graphs"
-<https://arxiv.org/abs/1706.02216>.
-'''
 
 path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'Reddit')
 dataset = Reddit(path)
@@ -33,8 +25,7 @@ test_loader = NeighborSampler(data.edge_index, node_idx=None,
 class Encoder(nn.Module):
     def __init__(self, in_channels, hidden_channels):
         super(Encoder, self).__init__()
-        self.convs = torch.nn.ModuleList()
-        self.convs.extend([
+        self.convs = torch.nn.ModuleList([
             SAGEConv(in_channels, hidden_channels),
             SAGEConv(hidden_channels, hidden_channels),
             SAGEConv(hidden_channels, hidden_channels)
@@ -48,7 +39,6 @@ class Encoder(nn.Module):
         ])
 
     def forward(self, x, adjs):
-
         for i, (edge_index, _, size) in enumerate(adjs):
             x_target = x[:size[1]]  # Target nodes are always placed first.
             x = self.convs[i]((x, x_target), edge_index)
@@ -69,55 +59,45 @@ model = DeepGraphInfomax(
 model = model.to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
 
-x = data.x.to(device)
-y = data.y.squeeze().to(device)
+x, y = data.x.to(device), data.y.to(device)
 
 
 def train(epoch):
     model.train()
-    total_loss = 0
 
+    total_loss = total_examples = 0
     for batch_size, n_id, adjs in tqdm(train_loader,
                                        desc=f'Epoch {epoch:02d}'):
         # `adjs` holds a list of `(edge_index, e_id, size)` tuples.
         adjs = [adj.to(device) for adj in adjs]
 
         optimizer.zero_grad()
-
         pos_z, neg_z, summary = model(x[n_id], adjs)
         loss = model.loss(pos_z, neg_z, summary)
         loss.backward()
         optimizer.step()
-        total_loss += loss.item()
+        total_loss += float(loss) * pos_z.size(0)
+        total_examples += pos_z.size(0)
 
-    loss = total_loss / len(train_loader)
-
-    return loss
+    return total_loss / total_examples
 
 
 @torch.no_grad()
 def test():
-
     model.eval()
 
+    zs = []
     for i, (batch_size, n_id, adjs) in enumerate(test_loader):
-
         adjs = [adj.to(device) for adj in adjs]
-        z, _, _ = model(x[n_id], adjs)
-
-        if i == 0:
-            z_test = z
-        else:
-            z_test = torch.cat([z_test, z], dim=0)
-    train_val_mask = data.train_mask + data.val_mask
-    acc = model.test(z_test[train_val_mask], y[train_val_mask],
-                     z_test[data.test_mask], y[data.test_mask], max_iter=10000)
-
+        zs.append(model(x[n_id], adjs)[0])
+    z = torch.cat(zs, dim=0)
+    train_val_mask = data.train_mask | data.val_mask
+    acc = model.test(z[train_val_mask], y[train_val_mask], z[data.test_mask],
+                     y[data.test_mask], max_iter=10000)
     return acc
 
 
-for epoch in range(1, 150):
-
+for epoch in range(1, 31):
     loss = train(epoch)
     print(f'Epoch {epoch:02d}, Loss: {loss:.4f}')
 
