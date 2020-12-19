fd15b9ac3343814828ca5e4be47c9d954af80aab
Ubuntu <ubuntu@ip-172-31-62-109.ec2.internal>
2020-12-19 21:10:46 +0000

    typo

--- examples/multi_gpu/distributed_batching.py
+++ examples/multi_gpu/distributed_batching.py
@@ -73,10 +73,7 @@ def run(rank, world_size):
     model = GIN(128, dataset.num_tasks, num_layers=3, dropout=0.5).to(rank)
     model = DistributedDataParallel(model, device_ids=[rank])
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    criterion = torch.nn.
-    
-    
-    ()
+    criterion = torch.nn.BCEWithLogitsLoss()
 
     if rank == 0:
         val_loader = DataLoader(dataset[split_idx['valid']], batch_size=256)
