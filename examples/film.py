f4622e59a087d04bc0628b52c33f0cc9f296223e
rusty1s <matthias.fey@tu-dortmund.de>
2020-12-24 14:40:29 +0100

    update

--- examples/film.py
+++ examples/film.py
@@ -2,57 +2,51 @@ import os.path as osp
 
 import torch
 import torch.nn.functional as F
-from torch.nn import Linear
+from torch.nn import BatchNorm1d
+from sklearn.metrics import f1_score
+from torch_geometric.nn import FiLMConv
 from torch_geometric.datasets import PPI
 from torch_geometric.data import DataLoader
-from torch_geometric.nn import GATConv, FiLMConv
-import torch_geometric.transforms as T
-from sklearn.metrics import f1_score
 
-path = osp.join("/home/laurent/graph_data", 'PPI')
+path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'PPI')
 train_dataset = PPI(path, split='train')
-val_dataset   = PPI(path, split='val')
-test_dataset  = PPI(path, split='test')
-# you might want to add self-loops as follows
-'''
-train_dataset = PPI(path, transform=T.AddSelfLoops(), split='train')
-val_dataset   = PPI(path, transform=T.AddSelfLoops(), split='val')
-test_dataset  = PPI(path, transform=T.AddSelfLoops(), split='test')
-'''
-train_loader  = DataLoader(train_dataset, batch_size=1, shuffle=True)
-val_loader    = DataLoader(val_dataset,   batch_size=2, shuffle=False)
-test_loader   = DataLoader(test_dataset,  batch_size=2, shuffle=False)
+val_dataset = PPI(path, split='val')
+test_dataset = PPI(path, split='test')
+train_loader = DataLoader(train_dataset, batch_size=2, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=2, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=2, shuffle=False)
 
-
-num_hidden = 256
 
 class Net(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, in_channels, hidden_channels, out_channels, num_layers,
+                 dropout=0.0):
         super(Net, self).__init__()
-        self.conv1 = FiLMConv(train_dataset.num_features, num_hidden,
-                              nn=Linear(train_dataset.num_features, 2*num_hidden),
-                              bias=False)
-        self.lin1 = Linear(train_dataset.num_features, num_hidden)
-        self.conv2 = FiLMConv(num_hidden, num_hidden,
-                              nn=Linear(num_hidden, 2*num_hidden),
-                              bias=False)
-        self.lin2 = Linear(num_hidden, num_hidden)
-        self.conv3 = FiLMConv(num_hidden, train_dataset.num_classes,
-                              nn=Linear(num_hidden, 2*train_dataset.num_classes),
-                              bias=False)
-        self.lin3 = Linear(num_hidden, train_dataset.num_classes)
+        self.dropout = dropout
+
+        self.convs = torch.nn.ModuleList()
+        self.convs.append(FiLMConv(in_channels, hidden_channels))
+        for _ in range(num_layers - 2):
+            self.convs.append(FiLMConv(hidden_channels, hidden_channels))
+        self.convs.append(FiLMConv(hidden_channels, out_channels, act=None))
+
+        self.norms = torch.nn.ModuleList()
+        for _ in range(num_layers - 1):
+            self.norms.append(BatchNorm1d(hidden_channels))
 
     def forward(self, x, edge_index):
-        x = F.elu(self.conv1(x, edge_index) + self.lin1(x))
-        x = F.elu(self.conv2(x, edge_index) + self.lin2(x))
-        x = self.conv3(x, edge_index) + self.lin3(x)
+        for conv, norm in zip(self.convs[:-1], self.norms):
+            x = norm(conv(x, edge_index))
+            x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.convs[-1](x, edge_index)
         return x
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = Net().to(device)
-loss_op = torch.nn.BCEWithLogitsLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+model = Net(in_channels=train_dataset.num_features, hidden_channels=320,
+            out_channels=train_dataset.num_classes, num_layers=4,
+            dropout=0.1).to(device)
+criterion = torch.nn.BCEWithLogitsLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
 
 def train():
@@ -62,7 +56,7 @@ def train():
     for data in train_loader:
         data = data.to(device)
         optimizer.zero_grad()
-        loss = loss_op(model(data.x, data.edge_index), data.y)
+        loss = criterion(model(data.x, data.edge_index), data.y)
         total_loss += loss.item() * data.num_graphs
         loss.backward()
         optimizer.step()
@@ -83,7 +77,7 @@ def test(loader):
     return f1_score(y, pred, average='micro') if pred.sum() > 0 else 0
 
 
-for epoch in range(1, 101):
+for epoch in range(1, 501):
     loss = train()
     val_f1 = test(val_loader)
     test_f1 = test(test_loader)
