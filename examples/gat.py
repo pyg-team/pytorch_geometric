3878461c578ffee7875ded834e78e53292406671
Dongkwan Kim <todoaskit@gmail.com>
2021-01-18 00:39:39 +0900

    Add SuperGAT example

--- examples/gat.py
+++ examples/gat.py
@@ -1,10 +1,16 @@
 import os.path as osp
+import argparse
 
 import torch
 import torch.nn.functional as F
 from torch_geometric.datasets import Planetoid
 import torch_geometric.transforms as T
-from torch_geometric.nn import GATConv
+from torch_geometric.nn import GATConv, SuperGATConv
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--use_supergat', action='store_true',
+                    help='Use self-supervised graph attention')
+args = parser.parse_args()
 
 dataset = 'Cora'
 path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', dataset)
@@ -15,10 +21,22 @@ data = dataset[0]
 class Net(torch.nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = GATConv(dataset.num_features, 8, heads=8, dropout=0.6)
-        # On the Pubmed dataset, use heads=8 in conv2.
-        self.conv2 = GATConv(8 * 8, dataset.num_classes, heads=1, concat=False,
-                             dropout=0.6)
+
+        if not args.use_supergat:
+            self.conv1 = GATConv(dataset.num_features, 8, heads=8, dropout=0.6)
+            # On the Pubmed dataset, use heads=8 in conv2.
+            self.conv2 = GATConv(8 * 8, dataset.num_classes, heads=1,
+                                 concat=False, dropout=0.6)
+        else:
+            self.conv1 = SuperGATConv(dataset.num_features, 8, heads=8,
+                                      dropout=0.6, attention_type='MX',
+                                      edge_sample_ratio=0.8,
+                                      is_undirected=True)
+            self.conv2 = SuperGATConv(8 * 8, dataset.num_classes, heads=8,
+                                      concat=False, dropout=0.6,
+                                      attention_type='MX',
+                                      edge_sample_ratio=0.8,
+                                      is_undirected=True)
 
     def forward(self):
         x = F.dropout(data.x, p=0.6, training=self.training)
@@ -36,7 +54,10 @@ optimizer = torch.optim.Adam(model.parameters(), lr=0.005, weight_decay=5e-4)
 def train():
     model.train()
     optimizer.zero_grad()
-    F.nll_loss(model()[data.train_mask], data.y[data.train_mask]).backward()
+    loss = F.nll_loss(model()[data.train_mask], data.y[data.train_mask])
+    if args.use_supergat:
+        loss += 2.0 * SuperGATConv.get_attention_loss(model)
+    loss.backward()
     optimizer.step()
 
 
@@ -50,7 +71,8 @@ def test():
     return accs
 
 
-for epoch in range(1, 201):
+max_epochs = 201 if not args.use_supergat else 501
+for epoch in range(1, max_epochs):
     train()
     log = 'Epoch: {:03d}, Train: {:.4f}, Val: {:.4f}, Test: {:.4f}'
     print(log.format(epoch, *test()))
