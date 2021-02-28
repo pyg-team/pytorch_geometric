83c4a1ecf04b5b2d51b7cbf9ba9324b776fec99b
rusty1s <matthias.fey@tu-dortmund.de>
2021-02-28 10:50:00 +0100

    fix gcn conv formulation

--- torch_geometric/nn/conv/gcn_conv.py
+++ torch_geometric/nn/conv/gcn_conv.py
@@ -83,12 +83,12 @@ class GCNConv(MessagePassing):
     Its node-wise formulation is given by:
 
     .. math::
-        \mathbf{x}^{\prime}_i = \mathbf{\Theta} \sum_{j}
-        \frac{1}{\sqrt{\hat{d}_j \hat{d}_i}} \mathbf{x}_j
+        \mathbf{x}^{\prime}_i = \mathbf{\Theta} \sum_{j \in \mathcal{N}(v) \cup
+        \{ i \}} \frac{1}{\sqrt{\hat{d}_j \hat{d}_i}} \mathbf{x}_j
 
     with :math:`\hat{d}_i = 1 + \sum_{j \in \mathcal{N}(i)} e_{j,i}`, where
     :math:`e_{j,i}` denotes the edge weight from source node :obj:`i` to target
-    node :obj:`j` (default: :obj:`1`)
+    node :obj:`j` (default: :obj:`1.0`)
 
     Args:
         in_channels (int): Size of each input sample.
