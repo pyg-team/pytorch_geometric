a5a6a5077bfbfcc04ef52130c01ef56e627b516b
rusty1s <matthias.fey@tu-dortmund.de>
2021-03-02 08:09:09 +0100

    import packages only when they are needed

--- torch_geometric/utils/convert.py
+++ torch_geometric/utils/convert.py
@@ -4,11 +4,6 @@ import networkx as nx
 
 import torch_geometric.data
 
-try:
-    import trimesh
-except ImportError:
-    trimesh = None
-
 from .num_nodes import maybe_num_nodes
 
 
@@ -147,9 +142,7 @@ def to_trimesh(data):
     Args:
         data (torch_geometric.data.Data): The data object.
     """
-
-    if trimesh is None:
-        raise ImportError('Package `trimesh` could not be found.')
+    import trimesh
 
     return trimesh.Trimesh(vertices=data.pos.detach().cpu().numpy(),
                            faces=data.face.detach().t().cpu().numpy(),
@@ -163,9 +156,7 @@ def from_trimesh(mesh):
     Args:
         mesh (trimesh.Trimesh): A :obj:`trimesh` mesh.
     """
-
-    if trimesh is None:
-        raise ImportError('Package `trimesh` could not be found.')
+    import trimesh
 
     pos = torch.from_numpy(mesh.vertices).to(torch.float)
     face = torch.from_numpy(mesh.faces).t().contiguous()
