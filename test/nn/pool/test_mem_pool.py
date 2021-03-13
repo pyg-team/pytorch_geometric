a48e16478d53261842b3871a45f70da27dd65b7f
Jinu Sunil <jinu.sunil@gmail.com>
2021-03-13 23:26:36 +0530

    fixed repr.

--- test/nn/pool/test_mem_pool.py
+++ test/nn/pool/test_mem_pool.py
@@ -5,7 +5,7 @@ from torch_geometric.nn import MemPool
 def test_mem_pool():
 
     mpool = MemPool(heads=3, num_keys=2, in_channels=2, out_channels=3)
-    assert mpool.__repr__() == 'MemPool(2, 3, heads=3, keys=2)'
+    assert mpool.__repr__() == 'MemPool(2, 3, heads=3, num_keys=2)'
     x = torch.rand((5, 4, 2))
     mask = torch.Tensor([[1, 1, 1, 0], [1, 1, 1, 1], [1, 1, 1, 1],
                          [1, 1, 1, 1], [1, 1, 1, 0]]).bool()
@@ -17,7 +17,7 @@ def test_mem_pool():
     assert (P[~mask] == 0).all()
 
     mpool = MemPool(heads=2, num_keys=2, in_channels=2, out_channels=4)
-    assert mpool.__repr__() == 'MemPool(2, 4, heads=2, keys=2)'
+    assert mpool.__repr__() == 'MemPool(2, 4, heads=2, num_keys=2)'
     x = torch.rand((4, 2))
     x, S = mpool(x)
     loss, P = MemPool.kl_loss(S)
