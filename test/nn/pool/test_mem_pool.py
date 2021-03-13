bcb41d7acbc5978abed513e6979fc4d5f4247b66
Jinu Sunil <jinu.sunil@gmail.com>
2021-03-13 19:26:12 +0530

    testing repr string.

--- test/nn/pool/test_mem_pool.py
+++ test/nn/pool/test_mem_pool.py
@@ -3,15 +3,23 @@ from torch_geometric.nn import MemPool
 
 
 def test_mem_pool():
-    mpool = MemPool(in_feat=1, out_feat=2)
-    q = torch.Tensor([[[1], [2], [3], [4]], [[-2], [2], [-2], [2]],
-                      [[-3], [3], [-3], [3]], [[4], [4], [4], [4]],
-                      [[5], [5], [5], [5]]])
-    mask = torch.Tensor([[1, 1, 1, 0], [1, 1, 1, 0], [1, 1, 1, 1],
-                         [1, 1, 1, 1], [1, 1, 1, 0]])
 
-    v, C = mpool(q, mask)
-    assert v.shape == torch.Size([5, 2, 2])
+    mpool = MemPool(heads=3, num_keys=2, in_channels=2, out_channels=3)
+    assert mpool.__repr__() == 'MemPool(2, 3, heads=3, keys=2)'
+    x = torch.rand((5, 4, 2))
+    mask = torch.Tensor([[1, 1, 1, 0], [1, 1, 1, 1], [1, 1, 1, 1],
+                         [1, 1, 1, 1], [1, 1, 1, 0]]).bool()
+    x, S = mpool(x, mask)
+    loss, P = MemPool.computeKl(S, mask)
+    assert x.shape == torch.Size([5, 2, 3])
+    assert (S[~mask] == 0).all()
+    assert P.shape == S.shape
+    assert (P[~mask] == 0).all()
 
-    loss, P = mpool.computeKl(C, mask)
-    assert P.shape == C.shape
+    mpool = MemPool(heads=2, num_keys=2, in_channels=2, out_channels=4)
+    assert mpool.__repr__() == 'MemPool(2, 4, heads=2, keys=2)'
+    x = torch.rand((4, 2))
+    x, S = mpool(x)
+    loss, P = MemPool.computeKl(S)
+    assert x.shape == torch.Size([1, 2, 4])
+    assert P.shape == S.shape
