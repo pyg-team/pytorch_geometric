7ef82e2daa1939d10320a6f326826a68add2182d
rusty1s <matthias.fey@tu-dortmund.de>
2021-03-15 09:05:21 +0100

    cleanup

--- torch_geometric/utils/homophily.py
+++ torch_geometric/utils/homophily.py
@@ -18,50 +18,52 @@ def homophily(edge_index, y, method):
     pass
 
 
-def homophily(edge_index: Adj, y: Tensor, method: str = "edge"):
-    r"""The homophily of a graph characterizes how likely nodes with the same label
-    are near each other in the graph. There are many measures of homophily
-    that fits this definition. In particular:
-    
-    - In the `"Beyond Homophily in Graph Neural Networks: Current Limitations and Effective Designs"
-      <https://arxiv.org/abs/2006.11468>`_ paper, the
+def homophily(edge_index: Adj, y: Tensor, method: str = 'edge'):
+    r"""The homophily of a graph characterizes how likely nodes with the same
+    label are near each other in a graph.
+    There are many measures of homophily that fits this definition.
+    In particular:
+
+    - In the `"Beyond Homophily in Graph Neural Networks: Current Limitations
+      and Effective Designs" <https://arxiv.org/abs/2006.11468>`_ paper, the
       homophily is the fraction of edges in a graph which connects nodes
       that have the same class label:
-    
-      .. math::
-        \text{homophily} = \frac{\vert (u,v) : (u,v) \in V \ \wedge \ y_u=y_v \vert }
-                                {\vert V \vert} 
-    
-      That measure is called the *edge homophily ratio* in the paper.
-    
-    - In the `"Geom-GCN: Geometric Graph Convolutional Networks"
-      <https://arxiv.org/abs/2002.05287>`_ paper, we find
 
       .. math::
-        \text{homophily} = \frac{1}{\vert V \vert} \sum_{v \in V}{
-        \frac{ \text{Number of }v\text{'s neighbors who have the same label as }v}
-             { \text{Number of }v\text{'s neighbors} }
-        }
-    
-    In both formulas, :math:`V` designates the set of nodes.
+        \text{homophily} = \frac{| \{ (v,w) : (v,w) \in \mathcal{E} \wedge
+        y_v = y_w \} | } {|\mathcal{V}|}
+
+      That measure is called the *edge homophily ratio*.
+
+    - In the `"Geom-GCN: Geometric Graph Convolutional Networks"
+      <https://arxiv.org/abs/2002.05287>`_ paper, edge homophily is normalized
+      across neighborhoods:
+
+      .. math::
+        \text{homophily} = \frac{1}{|\mathcal{V}|} \sum_{v \in \mathcal{V}}
+        \frac{ | \{ (w,v) : w \in \mathcal{N}(v) \wedge y_v = y_w \} |  }
+        { |\mathcal{N}(v)| }
+
+      That measure is called the *node homophily ratio*.
 
     Args:
         edge_index (Tensor or SparseTensor): The graph connectivity.
         y (Tensor): The labels.
         method (str, optional): The method used to calculate the homophily,
-            either :obj:`"edge"` (first formula above) or :obj:`"node"`
-            (second formula above). (default: :obj:`"edge"`)
+            either :obj:`"edge"` (first formula) or :obj:`"node"`
+            (second formula). (default: :obj:`"edge"`)
     """
-    
+    assert method in ['edge', 'node']
+
     if isinstance(edge_index, SparseTensor):
-        row, col, _ = edge_index.coo()
+        col, row, _ = edge_index.coo()
     else:
         row, col = edge_index
-    
-    if method == "edge":
+
+    if method == 'edge':
         return int((y[row] == y[col]).sum()) / row.size(0)
     else:
-        sim = torch.zeros_like(row, dtype=float)
-        sim[ (y[row] == y[col]) ] = 1
-        mean_value = scatter_mean(sim, col, 0, dim_size=y.size(0))
-        return mean_value.sum().item() / y.size(0)
+        out = torch.zeros_like(row, dtype=float)
+        out[y[row] == y[col]] = 1.
+        out = scatter_mean(out, col, 0, dim_size=y.size(0))
+        return float(out.mean())
