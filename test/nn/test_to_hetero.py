db0ddbfade2144eefb9813829a4274620d5dc5c5
rusty1s <matthias.fey@tu-dortmund.de>
2021-05-02 16:30:45 +0200

    fuse

--- test/nn/test_to_hetero.py
+++ test/nn/test_to_hetero.py
@@ -45,3 +45,32 @@ for node in gm.graph.nodes:
     print(node)
     if node.op == 'call_module':
         print(getattr(gm, node.target))
+
+# HOW TO ADD MODULES? LAZY MODULES
+
+
+def fuse(model: torch.nn.Module) -> torch.nn.Module:
+    model = copy.deepcopy(model)
+    fx_model: fx.GraphModule = fx.symbolic_trace(model)
+    modules = dict(fx_model.named_modules())
+
+    for node in fx_model.graph.nodes:
+        if node.op != 'call_module':
+            continue
+        # For call sites, `Node.target` represents the module/function/method
+        # that's being called. Here, we check `Node.target` to see if it's a
+        # batch norm module, and then check `Node.args[0].target` to see if the
+        # input `Node` is a convolution.
+        if type(modules[node.target]) is nn.BatchNorm2d and type(
+                modules[node.args[0].target]) is nn.Conv2d:
+            if len(node.args[0].users) > 1:
+                continue
+            conv = modules[node.args[0].target]
+            bn = modules[node.target]
+            fused_conv = fuse_conv_bn_eval(conv, bn)
+            replace_node_module(node.args[0], modules, fused_conv)
+            node.replace_all_uses_with(node.args[0])
+            fx_model.graph.erase_node(node)
+    fx_model.graph.lint()
+    fx_model.recompile()
+    return fx_model
