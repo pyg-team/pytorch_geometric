688313d8e3c6b83ae35f8871f77524e3c73e30da
rusty1s <matthias.fey@tu-dortmund.de>
2021-05-04 14:50:45 +0200

    first version

--- test/nn/test_to_hetero.py
+++ test/nn/test_to_hetero.py
@@ -1,43 +1,37 @@
-from typing import Dict, Tuple, List
+from typing import Dict
 
 import torch
 from torch import Tensor
-from torch.nn import Linear, ReLU, LazyLinear, Module, ModuleDict
+from torch.nn import Linear, ModuleDict
 from torch_geometric.datasets import Planetoid
-from torch_geometric.nn import GCNConv, Sequential
-from torch_geometric.nn import MessagePassing
-
-model = Sequential('x, edge_index', [
-    (GCNConv(16, 16), 'x , edge_index -> x'),
-    ReLU(),
-    (GCNConv(16, 16), 'x , edge_index -> x'),
-    ReLU(),
-])
+from torch_geometric.nn.to_hetero import duplicate_submodules, transform
+from torch.fx import symbolic_trace
 
 
 class Net1(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.lin1 = LazyLinear(16)
-        self.lin2 = Linear(16, 16)
-        self.conv1 = GCNConv(16, 16)
+        self.lin1 = Linear(16, 16)
 
     def forward(self, x: Tensor) -> Tensor:
-        x = self.lin1(x).relu_()
-        x = self.lin2(x).relu_()
+        x = self.lin1(x)
         return x
 
 
 class Net2(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.lin1 = Linear(16, 16)
-        self.lin2 = Linear(16, 16)
+        self.lin1 = ModuleDict()
+        self.lin1['paper'] = Linear(16, 16)
+        self.lin1['author'] = Linear(16, 16)
+        self.lin2 = ModuleDict()
+        self.lin2['paper'] = Linear(16, 16)
+        self.lin2['author'] = Linear(16, 16)
 
     def forward(self, x: Dict[str, Tensor]) -> Dict[str, Tensor]:
         for key, item in x.items():
-            x[key] = self.lin1(x[key]).relu_()
-            x[key] = self.lin2(x[key]).relu_()
+            x[key] = self.lin1[key](x[key]).relu_()
+            x[key] = self.lin2[key](x[key]).relu_()
         return x
 
 
@@ -50,58 +44,15 @@ data.x = data.x[:, :16]
 metadata = (['paper', 'author'], [('paper', 'paper'), ('author', 'paper'),
                                   ('paper', 'author')])
 
-from torch.fx import symbolic_trace
-
 gm = symbolic_trace(model2,
                     concrete_args={'x': {
                         'paper': data.x,
                         'author': data.x
                     }})
-gm = symbolic_trace(model1)
-# print()
-# print(gm)
-# print()
-# gm.graph.print_tabular()
-# print()
 
-Metagraph = Tuple[List[str], List[Tuple[str, str, str]]]
-
-
-def duplicate_modules_(module: Module, metagraph: Metagraph):
-    modules = dict(module.named_children())
-    for key, item in modules.items():
-        if isinstance(item, MessagePassing):
-            pass
-        else:
-            pass
-
-
-print(isinstance(model1.conv1, MessagePassing))
-duplicate_modules_(model1, (['paper', 'author'], []))
-
-# gm.graph.call_module(model1.lin)
-
-# for node in gm.graph.nodes:
-#     print(node)
-#     if node.op == 'call_module':
-#         print(getattr(gm, node.target))
-
-# HOW TO ADD MODULES? LAZY MODULES
-
-# def fuse(model: torch.nn.Module) -> torch.nn.Module:
-#     model = copy.deepcopy(model)
-#     fx_model: fx.GraphModule = fx.symbolic_trace(model)
-#     modules = dict(fx_model.named_modules())
-
-for node in list(gm.graph.nodes):
-    if node.op != 'call_module':
-        continue
-
-    with gm.graph.inserting_after(node):
-        new_node = gm.graph.call_module(node.target, args=(node, ))
-        # node.replace_all_uses_with(new_node)
-
-# print()
-# gm.graph.print_tabular()
-# gm.graph.lint()
-# gm.recompile()
+print(gm.graph.python_code('self'))
+gm.graph.print_tabular()
+print('-------------')
+duplicate_submodules(model1, (['paper', 'author'], [('paper', 'author')]))
+out_model = transform(model1, metadata, input_map={'x': 'node'})
+out_model({'author': torch.randn(100, 16), 'paper': torch.randn(100, 16)})
---
+++ torch_geometric/nn/to_hetero.py
@@ -0,0 +1,129 @@
+from typing import Tuple, List, Dict, Union, Optional
+
+import copy
+import warnings
+
+import torch
+from torch.nn import Module
+from torch import fx
+from torch_geometric.nn import MessagePassing
+
+Metagraph = Tuple[List[str], List[Tuple[str, str, str]]]
+
+
+def metatype2str(metatype: Union[str, Tuple[str, str, str]]) -> str:
+    return '__'.join(metatype) if isinstance(metatype, tuple) else metatype
+
+
+def duplicate_submodules(module: Module, metagraph: Metagraph) -> Module:
+    # TODO: Handle `torch.nn.ModuleList` and `torch.nn.ModuleDict`
+    metagraph = metagraph[:1] + ([metatype2str(t) for t in metagraph[1]], )
+
+    for name, submodule in dict(module.named_children()).items():
+        module_dict = torch.nn.ModuleDict()
+
+        is_message_passing = isinstance(submodule, MessagePassing)
+        for metatype in metagraph[1] if is_message_passing else metagraph[0]:
+            module_dict[metatype] = copy.deepcopy(submodule)
+            if hasattr(submodule, 'reset_parameters'):
+                module_dict[metatype].reset_parameters()
+            else:
+                warnings.warn((f"'{name}' will be duplicated, but its "
+                               "parameters cannot be reset"))
+
+        assert not hasattr(module, f'{name}_dict')
+        module.add_module(f'{name}_dict', module_dict)
+
+    return module
+
+
+def transform(module: Module, metagraph: Metagraph, input_map: Dict[str, str],
+              debug: bool = True) -> Module:
+    metagraph = metagraph[:1] + ([metatype2str(t) for t in metagraph[1]], )
+
+    print(module.lin1_dict)
+
+    gm = fx.symbolic_trace(module)
+
+    if debug:
+        gm.graph.print_tabular()
+        print()
+        print(gm.graph.python_code('self'))
+
+    for node in list(gm.graph.nodes):
+        if node.op == 'placeholder' and input_map[node.name] == 'node':
+            unwrap_placeholder(gm.graph, node, metagraph[0])
+        elif node.op == 'placeholder' and input_map[node.name] == 'edge':
+            unwrap_placeholder(gm.graph, node, metagraph[1])
+        elif node.op == 'call_module':
+            unwrap_node_call_module(gm.graph, node, metagraph[0])
+        elif node.op == 'output':
+            unwrap_output(gm.graph, node, metagraph[0])
+
+    erase_unused_nodes(gm.graph)
+
+    if debug:
+        gm.graph.print_tabular()
+        print()
+        print(gm.graph.python_code('self'))
+
+    gm.graph.lint()
+    gm.recompile()
+
+    return gm
+
+
+def find_node(graph: fx.Graph, name: str) -> Optional[fx.Node]:
+    out: Optional[fx.Node] = None
+    for node in graph.nodes:
+        if node.name == name:
+            out = node
+    return out
+
+
+def unwrap_placeholder(graph: fx.Graph, node: fx.Node,
+                       metatypes: List[str]) -> List[fx.Node]:
+    outs: List[fx.Node] = []
+    node.type = Dict[str, node.type]  # Replace input type.
+    graph.inserting_after(node)
+    for metatype in metatypes:
+        out = graph.create_node('call_method', 'get', args=(node, metatype),
+                                name=f'{node.target}__{metatype}')
+        graph.inserting_after(out)
+        outs.append(out)
+    return outs
+
+
+def unwrap_node_call_module(graph: fx.Graph, node: fx.Node,
+                            metatypes: List[str]) -> List[fx.Node]:
+    outs: List[fx.Node] = []
+    graph.inserting_after(node)
+    for metatype in metatypes:
+        input_node = find_node(graph, name=f'{node.args[0].name}__{metatype}')
+        out = graph.create_node('call_module',
+                                f'{node.target}_dict.{metatype}',
+                                args=(input_node, ),
+                                name=f'{node.target}__{metatype}')
+        graph.inserting_after(out)
+        outs.append(out)
+    return outs
+
+
+def unwrap_output(graph: fx.Graph, node: fx.Node,
+                  metatypes: List[str]) -> fx.Node:
+    node.args = ({
+        metatype: find_node(graph, f'{node.args[0].name}__{metatype}')
+        for metatype in metatypes
+    }, )
+    node.type = Dict[str, node.type]  # Replace output type.
+    return node
+
+
+def erase_unused_nodes(graph: fx.Graph) -> fx.Graph:
+    for node in list(graph.nodes):
+        try:
+            if node.op not in ['output']:
+                graph.erase_node(node)
+        except RuntimeError:
+            pass
+    return graph
