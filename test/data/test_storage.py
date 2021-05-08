0097ad711ec4982a6161fd9d42873087f4050046
rusty1s <matthias.fey@tu-dortmund.de>
2021-05-08 15:07:54 +0200

    new test storage2

--- test/data/test_storage.py
+++ test/data/test_storage.py
@@ -2,42 +2,46 @@ import copy
 
 import torch
 
-from torch_geometric.data.storage import Storage
+from torch_geometric.data.storage import Storage, Storage2
 
 
 def test_storage():
-    storage = Storage()
+    storage = Storage2()
     storage.x = torch.zeros(1)
     storage.y = torch.ones(1)
     assert len(storage) == 2
     assert storage.x is not None
     assert storage.y is not None
 
-    keys = storage.keys('x', 'y', 'z')
-    assert len(keys) == 2
+    # keys = storage.keys('x', 'y', 'z')
+    # assert len(keys) == 2
 
-    values = storage.values('x', 'y', 'z')
-    assert len(values) == 2
+    # values = storage.values('x', 'y', 'z')
+    # assert len(values) == 2
 
-    items = storage.items('x', 'y', 'z')
-    assert len(items) == 2
+    # items = storage.items('x', 'y', 'z')
+    # assert len(items) == 2
 
     del storage.y
     assert len(storage) == 1
     assert storage.x is not None
     assert storage.y is None
 
-    storage = Storage({'x': torch.zeros(1)})
+    storage = Storage2({'x': torch.zeros(1)})
     assert len(storage) == 1
     assert storage.x is not None
 
-    storage = Storage(x=torch.zeros(1))
+    storage = Storage2(x=torch.zeros(1))
     assert len(storage) == 1
     assert storage.x is not None
+
+    storage = Storage2(key='key', parent={}, x=torch.zeros(1))
 
     copied_storage = copy.copy(storage)
     assert storage == copied_storage
     assert id(storage) != id(copied_storage)
+    assert id(storage._key) == id(storage._key)
+    assert id(storage._parent) == id(storage._parent)
     assert storage.x.data_ptr() == copied_storage.x.data_ptr()
     assert int(storage.x) == 0
     assert int(copied_storage.x) == 0
@@ -45,10 +49,15 @@ def test_storage():
     deepcopied_storage = copy.deepcopy(storage)
     assert storage == deepcopied_storage
     assert id(storage) != id(deepcopied_storage)
+    assert id(storage._key) == id(storage._key)
+    assert id(storage._parent) == id(storage._parent)
     assert storage.x.data_ptr() != deepcopied_storage.x.data_ptr()
     assert int(storage.x) == 0
     assert int(deepcopied_storage.x) == 0
 
 
 def test_storage_functionality():
-    pass
+    storage = Storage2(x=1, y=2)
+    bla = storage.items()
+    # print(storage)
+    # print(bla)
--- torch_geometric/data/storage.py
+++ torch_geometric/data/storage.py
@@ -1,7 +1,83 @@
-from typing import Any, Optional, Iterable
+from typing import Any, Optional, Iterable, Dict
+from collections import UserDict
+from collections.abc import MutableMapping
+import collections
 
 import copy
 import warnings
+
+from typing import Union, Tuple, Callable, Optional
+
+NodeType = str
+EdgeType = Tuple[str, str, str]
+QueryType = Union[str, NodeType, EdgeType, Tuple[str, str]]
+AttrType = Union[str, Tuple[str, Callable]]
+
+
+class Storage2(collections.abc.MutableMapping):
+    # This class extends the Python dictionary class by the following:
+    # 1. It allows attribute assignment:
+    #    `storage.x = ...` rather than `storage['x'] = ...`
+    # 2. We can iterate over a subset of keys:
+    #    `storage.items('x', 'y')` or `storage.values('x', 'y')
+    # 3. It hides private attributes to the user, e.g.:
+    #    `storage._key` and `storage._parent`
+    def __init__(
+        self,
+        dict: Optional[Dict[str, Any]] = None,
+        key: Optional[Union[NodeType, EdgeType]] = None,
+        parent: Optional[Any] = None,
+        **kwargs,
+    ):
+        self.__dict__['_data'] = {}
+        self.__dict__['_key'] = key
+        self.__dict__['_parent'] = parent
+
+        if dict is not None:
+            self.update(dict)
+        if kwargs:
+            self.update(kwargs)
+
+    def __len__(self):
+        return len(self._data)
+
+    def __getitem__(self, key: str) -> Optional[Any]:
+        return self._data.get(key, None)
+
+    def __getattr__(self, key: str) -> Optional[Any]:
+        return self[key]
+
+    def __setitem__(self, key: str, item: Optional[Any]):
+        if item is None:
+            del self[key]
+        self._data[key] = item
+
+    def __setattr__(self, key: str, item: Optional[Any]):
+        if key in self.__dict__:
+            self.__dict__[key] = item
+        self[key] = item
+
+    def __delitem__(self, key: str):
+        if key in self._data:
+            del self._data[key]
+
+    def __delattr__(self, key: str):
+        if key in self.__dict__:
+            del self.__dict__[key]
+        del self[key]
+
+    def __iter__(self) -> Iterable:
+        return iter(self._data)
+
+    def __repr__(self) -> str:
+        return repr(self._data)
+
+    def __copy__(self):
+        return self.__class__(copy.copy(self._data), self._key, self._parent)
+
+    def __deepcopy__(self, memo):
+        return self.__class__(copy.deepcopy(self._data, memo), self._key,
+                              self._parent)
 
 
 class Storage(dict):
