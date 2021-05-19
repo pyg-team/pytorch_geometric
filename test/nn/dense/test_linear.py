065b257d833fd1d750ff4da8e98eb6a01477c339
rusty1s <matthias.fey@tu-dortmund.de>
2021-05-19 08:10:34 +0200

    cleanup docstring, add test for identical initialization

--- test/nn/dense/test_linear.py
+++ test/nn/dense/test_linear.py
@@ -1,28 +1,49 @@
 import pytest
+from itertools import product
+
 import torch
 from torch_geometric.nn import Linear
+from torch.nn import Linear as PTLinear
+
 try:
-    torch.nn.parameter.UninitializedParameter
-    with_uninitialized_parameter = True
-except AttributeError:
-    with_uninitialized_parameter = False
+    from torch.nn.parameter import UninitializedParameter  # noqa
+    with_lazy_support = True
+except ImportError:
+    with_lazy_support = False  # Skip error for PyTorch <= 1.7.
+
+weight_inits = ['glorot', 'kaiming_uniform', None]
+bias_inits = ['zeros', None]
 
 
-@pytest.mark.skipif(not with_uninitialized_parameter,
-                    reason='No UninitializedParameter')
-def test_linear():
+@pytest.mark.parametrize('weight,bias', product(weight_inits, bias_inits))
+def test_linear(weight, bias):
     x = torch.randn(3, 4, 16)
-    linear = Linear(16, 32)
-    out = linear(x)
-    assert out.size() == (3, 4, 32)
+    lin = Linear(16, 32, weight_initializer=weight, bias_initializer=bias)
+    assert str(lin) == 'Linear(16, 32, bias=True)'
+    assert lin(x).size() == (3, 4, 32)
 
 
-@pytest.mark.skipif(not with_uninitialized_parameter,
-                    reason='No UninitializedParameter')
-def test_lazy_linear():
-    if torch.__version__ < "1.8":
-        return
+@pytest.mark.skipif(not with_lazy_support, reason='No lazy support')
+@pytest.mark.parametrize('weight,bias', product(weight_inits, bias_inits))
+def test_lazy_linear(weight, bias):
     x = torch.randn(3, 4, 16)
-    linear = Linear(-1, 32)
-    out = linear(x)
-    assert out.size() == (3, 4, 32)
+    lin = Linear(-1, 32, weight_initializer=weight, bias_initializer=bias)
+    assert str(lin) == 'Linear(-1, 32, bias=True)'
+    assert lin(x).size() == (3, 4, 32)
+    assert str(lin) == 'Linear(16, 32, bias=True)'
+
+
+@pytest.mark.parametrize('lazy', [True, False])
+def test_identical_linear_default_initialization(lazy):
+    x = torch.randn(3, 4, 16)
+
+    torch.manual_seed(12345)
+    lin1 = Linear(-1 if lazy else 16, 32)
+    lin1(x)
+
+    torch.manual_seed(12345)
+    lin2 = PTLinear(16, 32)
+
+    assert lin1.weight.tolist() == lin2.weight.tolist()
+    assert lin1.bias.tolist() == lin2.bias.tolist()
+    assert lin1(x).tolist() == lin2(x).tolist()
--- torch_geometric/nn/dense/linear.py
+++ torch_geometric/nn/dense/linear.py
@@ -1,84 +1,90 @@
 from typing import Optional
+
 import math
 
 import torch
 from torch import Tensor
 import torch.nn.functional as F
 from torch.nn.parameter import Parameter
-try:
-    from torch.nn.parameter import UninitializedParameter
-except ImportError:
-    # skip error for PyTorch 1.7 or before.
-    pass
 
 from torch_geometric.nn import inits
 
+try:
+    from torch.nn.parameter import UninitializedParameter
+except ImportError:
+    pass  # Skip error for PyTorch <= 1.7.
+
 
 class Linear(torch.nn.Module):
-    r"""
-    A customized linear transformation similar to torch.nn.Linear.
+    r"""Applies a linear tranformation to the incoming data
 
-    It support lazy initialization and customized initialization function.
+    .. math::
+        \mathbf{x}^{\prime} = \mathbf{x} \mathbf{W}^{\top} + \mathbf{b}
+
+    similar to :class:`torch.nn.Linear`.
+    It supports lazy initialization and customizable weight and bias
+    initialization.
 
     Args:
-        in_features: size of each input sample, or -1 for lazy initialization.
-        out_features: size of each output sample.
-        bias: whether the layer learn the bias or not, default to False.
-        weight_initializer: the initializer for the weight, currently support
-         `glorot` and `kaiming_uniform`, default or not set will use
-         `kaiming_uniform`.
-        bias_initializer: the initializer for the bias, current support 'zeros'
-         , or `constant (1 / math.sqrt(in_features)`, default or not set will
-         use `constant`.
+        in_channels (int): Size of each input sample.
+            Will be initialized lazily in case :obj:`-1`.
+        out_channels (int): Size of each output sample.
+        bias (bool, optional): If set to :obj:`False`, the layer will not learn
+            an additive bias. (default: :obj:`True`)
+        weight_initializer (str, optional): The initializer for the weight
+            matrix (:obj:`"glorot"`, :obj:`"kaiming_uniform"` or :obj:`None`).
+            If set to :obj:`None`, will match default weight initialization of
+            :class:`torch.nn.Linear`. (default: :obj:`None`)
+        bias_initializer (str, optional): The initializer for the bias
+            vector (:obj:`"zeros"` or :obj:`None`).
+            If set to :obj:`None`, will match default bias initialization of
+            :class:`torch.nn.Linear`. (default: :obj:`None`)
     """
-    def __init__(self,
-                 in_features: int,
-                 out_features: int,
-                 bias: bool = True,
+    def __init__(self, in_channels: int, out_channels: int, bias: bool = True,
                  weight_initializer: Optional[str] = None,
                  bias_initializer: Optional[str] = None):
         super().__init__()
-        self.in_features = in_features
-        self.out_features = out_features
+        self.in_channels = in_channels
+        self.out_channels = out_channels
         self.weight_initializer = weight_initializer
         self.bias_initializer = bias_initializer
 
-        if in_features >= 0:
-            self.weight = Parameter(torch.Tensor(out_features, in_features))
+        if in_channels > 0:
+            self.weight = Parameter(torch.Tensor(out_channels, in_channels))
         else:
             self.weight = UninitializedParameter()
             self._hook = self.register_forward_pre_hook(
                 self.initialize_parameters)
+
         if bias:
-            self.bias = Parameter(torch.Tensor(out_features))
+            self.bias = Parameter(torch.Tensor(out_channels))
         else:
             self.register_parameter('bias', None)
+
         self.reset_parameters()
 
-    def reset_parameters(self) -> None:
-        if self.in_features < 0:
-            return
+    def reset_parameters(self):
+        if self.in_channels > 0:
+            if self.weight_initializer == 'glorot':
+                inits.glorot(self.weight)
+            elif (self.weight_initializer == 'kaiming_uniform'
+                  or self.weight_initializer is None):
+                inits.kaiming_uniform(self.weight, fan=self.in_channels,
+                                      a=math.sqrt(5))
+            else:
+                raise RuntimeError(
+                    f"Linear layer weight initializer "
+                    f"'{self.weight_initializer}' is not supported")
 
-        if self.weight_initializer == 'glorot':
-            inits.glorot(self.weight)
-        elif self.weight_initializer == 'kaiming_uniform' \
-                or self.weight_initializer is None:
-            # By default, we use kaiming uniform initializer.
-            inits.kaiming_uniform(
-                self.weight, fan=self.in_features, a=math.sqrt(5))
-        else:
-            raise RuntimeError(f"Linear layer weight initializer "
-                               f"{self.weight_initializer} is not supported")
-
-        if self.bias is not None:
+        if self.in_channels > 0 and self.bias is not None:
             if self.bias_initializer == 'zeros':
                 inits.zeros(self.bias)
-            elif self.bias_initializer == 'constant' \
-                    or self.bias_initializer is None:
-                inits.normal(self.bias, 0, 1 / math.sqrt(self.in_features))
+            elif self.bias_initializer is None:
+                inits.uniform(self.in_channels, self.bias)
             else:
-                raise RuntimeError(f"Linear layer bias initializer "
-                                   f"{self.bias_initializer} is not supported")
+                raise RuntimeError(
+                    f"Linear layer bias initializer "
+                    f"'{self.bias_initializer}' is not supported")
 
     def forward(self, x: Tensor) -> Tensor:
         return F.linear(x, self.weight, self.bias)
@@ -86,13 +92,12 @@ class Linear(torch.nn.Module):
     @torch.no_grad()
     def initialize_parameters(self, module, input):
         if isinstance(self.weight, UninitializedParameter):
-            self.in_features = input[0].size(-1)
-            self.weight.materialize((self.out_features, self.in_features))
+            self.in_channels = input[0].size(-1)
+            self.weight.materialize((self.out_channels, self.in_channels))
             self.reset_parameters()
-
         module._hook.remove()
         delattr(module, '_hook')
 
     def __repr__(self) -> str:
-        return (f'{self.__class__.__name__}({self.in_features}, '
-                f'{self.out_features}, bias={self.bias is not None})')
+        return (f'{self.__class__.__name__}({self.in_channels}, '
+                f'{self.out_channels}, bias={self.bias is not None})')
