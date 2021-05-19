e6a1c9bd33aa4dd77b7bc1367e1446a77bfc1f7d
rusty1s <matthias.fey@tu-dortmund.de>
2021-05-19 19:21:36 +0200

    update with state

--- test/nn/test_to_hetero.py
+++ test/nn/test_to_hetero.py
@@ -1,8 +1,9 @@
-from typing import Dict
+from typing import Dict, Tuple
 
 import torch
 from torch import Tensor
 from torch.nn import Linear, ModuleDict
+from torch_geometric.nn import SAGEConv
 from torch_geometric.datasets import Planetoid
 from torch_geometric.nn.to_hetero import transform
 from torch.fx import symbolic_trace
@@ -12,48 +13,59 @@ class Net1(torch.nn.Module):
     def __init__(self):
         super().__init__()
         self.lin1 = Linear(16, 16)
+        # self.conv1 = SAGEConv(16, 16)
         self.lin2 = Linear(16, 16)
 
-    def forward(self, x: Tensor) -> Tensor:
+    def forward(self, x: Tensor, edge_index: Tensor, y: Tensor) -> Tensor:
         x = self.lin1(x).relu_()
-        x = self.lin2(x).relu_()
-        return x
+        y = self.lin1(y).relu_()
+        x = x + x
+        return x, y
 
 
-class Net2(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.lin1 = ModuleDict()
-        self.lin1['paper'] = Linear(16, 16)
-        self.lin1['author'] = Linear(16, 16)
-        self.lin2 = ModuleDict()
-        self.lin2['paper'] = Linear(16, 16)
-        self.lin2['author'] = Linear(16, 16)
+model = Net1()
 
-    def forward(self, x: Dict[str, Tensor]) -> Dict[str, Tensor]:
-        for key, item in x.items():
-            x[key] = self.lin1[key](x[key]).relu_()
-            x[key] = self.lin2[key](x[key]).relu_()
-        return x
+metadata = (['paper', 'author'], [('paper', '_', 'paper'),
+                                  ('author', '_', 'paper'),
+                                  ('paper', '_', 'author'),
+                                  ('author', '_', 'author')])
 
+x_dict = {'author': torch.randn(100, 16), 'paper': torch.randn(100, 16)}
+edge_index_dict = {
+    ('paper', '_', 'paper'): torch.randint(100, (2, 200), dtype=torch.long),
+    ('paper', '_', 'author'): torch.randint(100, (2, 200), dtype=torch.long),
+    ('author', '_', 'paper'): torch.randint(100, (2, 200), dtype=torch.long),
+    ('author', '_', 'author'): torch.randint(100, (2, 200), dtype=torch.long),
+}
+y_dict = {'author': torch.randn(100, 16), 'paper': torch.randn(100, 16)}
 
-model1 = Net1()
-model2 = Net2()
+out_model = transform(model, metadata)
+out_model(x_dict, edge_index_dict, y_dict)
 
-data = Planetoid('/tmp/Planetoid', 'Cora')[0]
-data.x = data.x[:, :16]
+# print(out_model.modules())
 
-metadata = (['paper', 'author'], [('paper', 'paper'), ('author', 'paper'),
-                                  ('paper', 'author')])
+# class Net2(torch.nn.Module):
+#     def __init__(self):
+#         super().__init__()
+#         self.lin1 = ModuleDict()
+#         self.lin1['paper'] = Linear(16, 16)
+#         self.lin1['author'] = Linear(16, 16)
+#         self.lin2 = ModuleDict()
+#         self.lin2['paper'] = Linear(16, 16)
+#         self.lin2['author'] = Linear(16, 16)
 
-gm = symbolic_trace(model2,
-                    concrete_args={'x': {
-                        'paper': data.x,
-                        'author': data.x
-                    }})
+#     def forward(self, x: Dict[str, Tensor]) -> Dict[str, Tensor]:
+#         for key, item in x.items():
+#             x[key] = self.lin1[key](x[key]).relu_()
+#             x[key] = self.lin2[key](x[key]).relu_()
+#         return x
 
-print(gm.graph.python_code('self'))
-gm.graph.print_tabular()
-print('-------------')
-out_model = transform(model1, metadata, input_map={'x': 'node'})
-out_model({'author': torch.randn(100, 16), 'paper': torch.randn(100, 16)})
+# model2 = Net2()
+# gm = symbolic_trace(model2,
+#                     concrete_args={'x': {
+#                         'paper': data.x,
+#                         'author': data.x
+#                     }})
+
+# print(gm.graph.python_code('self'))
+# gm.graph.print_tabular()
--- torch_geometric/nn/to_hetero.py
+++ torch_geometric/nn/to_hetero.py
@@ -1,6 +1,8 @@
-from typing import Tuple, List, Dict, Union, Optional
+from typing import Tuple, List, Dict, Union, Optional, Any
 
+import re
 import copy
+import inspect
 import warnings
 
 import torch
@@ -8,46 +10,26 @@ from torch.nn import Module
 from torch import fx
 from torch_geometric.nn import MessagePassing
 
-Metagraph = Tuple[List[str], List[Tuple[str, str, str]]]
+NodeType = str
+EdgeType = Tuple[str, str, str]
+Metadata = Tuple[List[NodeType], List[EdgeType]]
 
 # TODO:
+# * Modules with multiple return statements
 # * LazyInitialization
 # * What are leaf modules?
 # * Flexible args/kwargs replacement
 # * What happens in ModuleLists and ModuleDicts?
 
 
-def metatype2str(metatype: Union[str, Tuple[str, str, str]]) -> str:
-    return '__'.join(metatype) if isinstance(metatype, tuple) else metatype
-
-
-def duplicate_submodules(module: Module, metagraph: Metagraph) -> Module:
-    # TODO: Handle `torch.nn.ModuleList` and `torch.nn.ModuleDict`
-    metagraph = metagraph[:1] + ([metatype2str(t) for t in metagraph[1]], )
-
-    for name, submodule in dict(module.named_children()).items():
-        module_dict = torch.nn.ModuleDict()
-
-        is_message_passing = isinstance(submodule, MessagePassing)
-        for metatype in metagraph[1] if is_message_passing else metagraph[0]:
-            module_dict[metatype] = copy.deepcopy(submodule)
-            if hasattr(submodule, 'reset_parameters'):
-                module_dict[metatype].reset_parameters()
-            else:
-                warnings.warn((f"'{name}' will be duplicated, but its "
-                               "parameters cannot be reset"))
-
-        module._modules[name] = module_dict
-
-    return module
-
-
-def transform(module: Module, metagraph: Metagraph, input_map: Dict[str, str],
+def transform(module: Module, metadata: Metadata,
+              input_map: Optional[Dict[str, str]] = None,
               debug: bool = True) -> Module:
-    metagraph = metagraph[:1] + ([metatype2str(t) for t in metagraph[1]], )
+
+    state = init_state(module, input_map)
 
     gm = fx.symbolic_trace(module)
-    gm = duplicate_submodules(gm, metagraph)
+    gm = duplicate_submodules(gm, metadata)
 
     if debug:
         gm.graph.print_tabular()
@@ -55,20 +37,19 @@ def transform(module: Module, metagraph: Metagraph, input_map: Dict[str, str],
         print(gm.graph.python_code('self'))
 
     for node in list(gm.graph.nodes):
-        if node.op == 'placeholder' and input_map[node.name] == 'node':
-            unwrap_placeholder(gm.graph, node, metagraph[0])
-        elif node.op == 'placeholder' and input_map[node.name] == 'edge':
-            unwrap_placeholder(gm.graph, node, metagraph[1])
+        print(node.op)
+        if node.op == 'placeholder':
+            unwrap_placeholder(gm.graph, node, state, metadata)
+        elif node.op == 'output':
+            unwrap_output(gm.graph, node, state, metadata)
         elif node.op == 'get_attr':
             raise NotImplementedError
         elif node.op == 'call_module':
-            unwrap_call_node_module(gm.graph, node, metagraph[0])
+            unwrap_call_module(gm.graph, node, state, metadata)
         elif node.op == 'call_method':
-            unwrap_call_method(gm.graph, node, metagraph[0])
+            unwrap_call_method(gm.graph, node, state, metadata)
         elif node.op == 'call_function':
-            raise NotImplementedError
-        elif node.op == 'output':
-            unwrap_output(gm.graph, node, metagraph[0])
+            unwrap_call_function(gm.graph, node, state, metadata)
         else:
             raise NotImplementedError
 
@@ -85,6 +66,47 @@ def transform(module: Module, metagraph: Metagraph, input_map: Dict[str, str],
     return gm
 
 
+def init_state(module: Module,
+               input_map: Optional[Dict[str, str]] = None) -> Dict[str, str]:
+    out = {}
+    for key in inspect.signature(module.forward).parameters.keys():
+        if input_map is not None and key in input_map:
+            out[key] = input_map[key]
+            assert input_map[key] in ['node', 'edge']
+        else:
+            out[key] = 'edge' if bool(re.search('(edge|adj)', key)) else 'node'
+    return out
+
+
+def key2str(metatype: Union[str, Tuple[str, str, str]]) -> str:
+    return '__'.join(metatype) if isinstance(metatype, tuple) else metatype
+
+
+def duplicate_submodules(module: Module, metadata: Metadata) -> Module:
+    # TODO: Handle `torch.nn.ModuleList` and `torch.nn.ModuleDict`
+    metadata = metadata[:1] + ([key2str(t) for t in metadata[1]], )
+
+    for name, submodule in dict(module.named_children()).items():
+        module_dict = torch.nn.ModuleDict()
+
+        is_message_passing = isinstance(submodule, MessagePassing)
+        for metatype in metadata[1] if is_message_passing else metadata[0]:
+            module_dict[metatype] = copy.deepcopy(submodule)
+            if hasattr(submodule, 'reset_parameters'):
+                module_dict[metatype].reset_parameters()
+            else:
+                warnings.warn((f"'{name}' will be duplicated, but its "
+                               "parameters cannot be reset"))
+
+        module._modules[name] = module_dict
+
+    return module
+
+
+def get_type(name: str, state: Dict[str, str]) -> Union[NodeType, EdgeType]:
+    return NodeType if state[name] == 'node' else EdgeType
+
+
 def find_node(graph: fx.Graph, name: str) -> Optional[fx.Node]:
     out: Optional[fx.Node] = None
     for node in graph.nodes:
@@ -93,62 +115,132 @@ def find_node(graph: fx.Graph, name: str) -> Optional[fx.Node]:
     return out
 
 
-def unwrap_placeholder(graph: fx.Graph, node: fx.Node,
-                       metatypes: List[str]) -> List[fx.Node]:
-    outs: List[fx.Node] = []
-    node.type = Dict[str, node.type]  # Replace input type.
-    graph.inserting_after(node)
-    for metatype in metatypes:
-        out = graph.create_node('call_method', 'get', args=(node, metatype),
-                                name=f'{node.name}__{metatype}')
-        graph.inserting_after(out)
-        outs.append(out)
-    return outs
-
-
-def unwrap_call_node_module(graph: fx.Graph, node: fx.Node,
-                            metatypes: List[str]) -> List[fx.Node]:
-    outs: List[fx.Node] = []
-    graph.inserting_after(node)
-    for metatype in metatypes:
-        input_node = find_node(graph, name=f'{node.args[0].name}__{metatype}')
-        out = graph.create_node('call_module', f'{node.target}.{metatype}',
-                                args=(input_node, ),
-                                name=f'{node.name}__{metatype}')
-        graph.inserting_after(out)
-        outs.append(out)
-    return outs
-
-
-def unwrap_call_method(graph: fx.Graph, node: fx.Node,
-                       metatypes: List[str]) -> List[fx.Node]:
-    outs: List[fx.Node] = []
-    graph.inserting_after(node)
-    for metatype in metatypes:
-        input_node = find_node(graph, name=f'{node.args[0].name}__{metatype}')
-        out = graph.create_node('call_method', node.target,
-                                args=(input_node, ),
-                                name=f'{node.name}__{metatype}')
-        graph.inserting_after(out)
-        outs.append(out)
-    return outs
-
-
-def unwrap_output(graph: fx.Graph, node: fx.Node,
-                  metatypes: List[str]) -> fx.Node:
-    node.args = ({
-        metatype: find_node(graph, f'{node.args[0].name}__{metatype}')
-        for metatype in metatypes
-    }, )
-    node.type = Dict[str, node.type]  # Replace output type.
-    return node
-
-
 def erase_unused_nodes(graph: fx.Graph) -> fx.Graph:
     for node in list(graph.nodes)[::-1]:  # Iterate in reverse-mode.
         try:
-            if node.op not in ['output']:
+            if node.op not in ['placeholder', 'output']:
                 graph.erase_node(node)
         except RuntimeError:
             pass
     return graph
+
+
+def unwrap_placeholder(graph: fx.Graph, node: fx.Node, state: Dict[str, str],
+                       metadata: Metadata) -> List[fx.Node]:
+
+    outs: List[fx.Node] = []
+    if node.type is not None:
+        node.type = Dict[get_type(node.name, state), node.type]
+    graph.inserting_after(node)
+    for key in metadata[0] if state[node.name] == 'node' else metadata[1]:
+        out = graph.create_node('call_method', 'get', args=(node, key),
+                                name=f'{node.name}__{key2str(key)}')
+        graph.inserting_after(out)
+        outs.append(out)
+    return outs
+
+
+def unwrap_output(graph: fx.Graph, node: fx.Node, state: Dict[str, str],
+                  metadata: Metadata):
+    def _get_mapping(name):
+        return {
+            key: find_node(graph, f'{name}__{key2str(key)}')
+            for key in (metadata[0] if state[name] == 'node' else metadata[1])
+        }
+
+    if isinstance(node.args[0], fx.Node):  # Single output value.
+        if node.type is not None:
+            node.type = Dict[get_type(node.args[0].name, state), node.type]
+        node.args = (_get_mapping(node.args[0].name), )
+
+    elif isinstance(node.args[0], tuple):  # Multiple output values.
+        if node.type is not None:
+            if (node.type.__dict__.get('__origin__', None) is not None
+                    and issubclass(node.type.__dict__['__origin__'], tuple)):
+                node.type.__dict__['__args__'] = tuple(
+                    Dict[get_type(arg.name, state), t] for arg, t in zip(
+                        node.args[0], node.type.__dict__['__args__']))
+            else:
+                node.type = None
+        node.args = (tuple(_get_mapping(arg.name) for arg in node.args[0]), )
+
+    else:
+        raise NotImplementedError
+
+
+def unwrap_call_module(graph: fx.Graph, node: fx.Node, state: Dict[str, str],
+                       metadata: Metadata) -> List[fx.Node]:
+
+    outs: List[fx.Node] = []
+    graph.inserting_after(node)
+
+    if all([state[arg.name] == 'node' for arg in node.args]):
+        for key in metadata[0]:
+            args = tuple(
+                find_node(graph, f'{arg.name}__{key}') for arg in node.args)
+            kwargs = {}  # TODO
+            out = graph.create_node('call_module', f'{node.target}.{key}',
+                                    args, kwargs, name=f'{node.name}__{key}')
+            state[node.name] = 'node'
+            graph.inserting_after(out)
+            outs.append(out)
+
+    elif all([state[arg.name] == 'edge' for arg in node.args]):
+        raise NotImplementedError
+
+    else:
+        raise NotImplementedError
+
+    return outs
+
+
+def unwrap_call_method(graph: fx.Graph, node: fx.Node, state: Dict[str, str],
+                       metadata: Metadata) -> List[fx.Node]:
+
+    outs: List[fx.Node] = []
+    graph.inserting_after(node)
+
+    if all([state[arg.name] == 'node' for arg in node.args]):
+        for key in metadata[0]:
+            args = tuple(
+                find_node(graph, f'{arg.name}__{key}') for arg in node.args)
+            kwargs = {}  # TODO
+            out = graph.create_node('call_method', node.target, args, kwargs,
+                                    name=f'{node.name}__{key}')
+            state[node.name] = 'node'
+            graph.inserting_after(out)
+            outs.append(out)
+
+    elif all([state[arg.name] == 'edge' for arg in node.args]):
+        raise NotImplementedError
+
+    else:
+        raise NotImplementedError
+
+    return outs
+
+
+def unwrap_call_function(graph: fx.Graph, node: fx.Node, state: Dict[str, str],
+                         metadata: Metadata) -> List[fx.Node]:
+
+    outs: List[fx.Node] = []
+    graph.inserting_after(node)
+
+    if all([state[arg.name] == 'node' for arg in node.args]):
+        for key in metadata[0]:
+            args = tuple(
+                find_node(graph, f'{arg.name}__{key}') for arg in node.args)
+            kwargs = {}  # TODO
+            out = graph.create_node('call_function', node.target, args, kwargs,
+                                    name=f'{node.name}__{key}')
+            state[node.name] = 'node'
+            graph.inserting_after(out)
+            outs.append(out)
+
+    elif all([state[arg.name] == 'edge' for arg in node.args]):
+        raise NotImplementedError
+
+    else:
+        raise NotImplementedError
+
+    return outs
