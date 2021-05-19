ad07e83a2af0fc2b734c206881653406f68b091d
rusty1s <matthias.fey@tu-dortmund.de>
2021-05-19 12:50:00 +0200

    Clean up dataset + example

--- examples/upfd.py
+++ examples/upfd.py
@@ -1,242 +1,109 @@
 import argparse
-import time
-from tqdm import tqdm
-from sklearn.metrics import f1_score, accuracy_score, recall_score
-from sklearn.metrics import precision_score, roc_auc_score
-from sklearn.metrics import average_precision_score
+import os.path as osp
 
 import torch
+from torch.nn import Linear
 import torch.nn.functional as F
-from torch.utils.data import random_split, Subset
-from torch_geometric.nn import global_max_pool as gmp
-from torch_geometric.nn import GCNConv, SAGEConv, GATConv, DataParallel
 from torch_geometric.datasets import UPFD
-from torch_geometric.data import DataLoader, DataListLoader
+from torch_geometric.data import DataLoader
 from torch_geometric.transforms import ToUndirected
+from torch_geometric.nn import GCNConv, SAGEConv, GATConv, global_max_pool
 
-"""
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', type=str, default='politifact',
+                    choices=['politifact', 'gossipcop'])
+parser.add_argument('--feature', type=str, default='spacy',
+                    choices=['profile', 'spacy', 'bert', 'content'])
+parser.add_argument('--model', type=str, default='GCN',
+                    choices=['GCN', 'GAT', 'SAGE'])
+args = parser.parse_args()
 
-The example implementation of UPFD-GCN, UPFD-GAT, and UPFD-SAGE
-based on the UPFD graph classification dataset
+path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'UPFD')
+train_dataset = UPFD(path, args.dataset, args.feature, 'train', ToUndirected())
+val_dataset = UPFD(path, args.dataset, args.feature, 'val', ToUndirected())
+test_dataset = UPFD(path, args.dataset, args.feature, 'test', ToUndirected())
 
-Paper: User Preference-aware Fake News Detection
-Link: https://arxiv.org/abs/2104.12259
-Source Code: https://github.com/safe-graph/GNN-FakeNews
-
-"""
+train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=128, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=128, shuffle=False)
 
 
-class Model(torch.nn.Module):
-    def __init__(self, args, concat=False):
-        super(Model, self).__init__()
-        self.args = args
-        self.num_features = args.num_features
-        self.nhid = args.nhid
-        self.num_classes = args.num_classes
-        self.model = args.model
+class Net(torch.nn.Module):
+    def __init__(self, model, in_channels, hidden_channels, out_channels,
+                 concat=False):
+        super(Net, self).__init__()
         self.concat = concat
 
-        if self.model == 'gcn':
-            self.conv1 = GCNConv(self.num_features, self.nhid)
-        elif self.model == 'sage':
-            self.conv1 = SAGEConv(self.num_features, self.nhid)
-        elif self.model == 'gat':
-            self.conv1 = GATConv(self.num_features, self.nhid)
+        if model == 'GCN':
+            self.conv1 = GCNConv(in_channels, hidden_channels)
+        elif model == 'SAGE':
+            self.conv1 = SAGEConv(in_channels, hidden_channels)
+        elif model == 'GAT':
+            self.conv1 = GATConv(in_channels, hidden_channels)
 
         if self.concat:
-            self.lin0 = torch.nn.Linear(self.num_features, self.nhid)
-            self.lin1 = torch.nn.Linear(self.nhid * 2, self.nhid)
+            self.lin0 = Linear(in_channels, hidden_channels)
+            self.lin1 = Linear(2 * hidden_channels, hidden_channels)
 
-        self.lin2 = torch.nn.Linear(self.nhid, self.num_classes)
+        self.lin2 = Linear(hidden_channels, out_channels)
 
-    def forward(self, data):
-
-        x, edge_index, batch = data.x, data.edge_index, data.batch
-
-        edge_attr = None
-
-        x = F.relu(self.conv1(x, edge_index, edge_attr))
-        x = gmp(x, batch)
+    def forward(self, x, edge_index, batch):
+        h = self.conv1(x, edge_index).relu()
+        h = global_max_pool(h, batch)
 
         if self.concat:
-            news = [data.x[(data.batch == idx).nonzero().squeeze()[0]]
-                    for idx in range(data.num_graphs)]
-            news = torch.stack(news)
-            news = F.relu(self.lin0(news))
-            x = torch.cat([x, news], dim=1)
-            x = F.relu(self.lin1(x))
+            # Get the root node (tweet) features of each graph:
+            root = (batch[1:] - batch[:-1]).nonzero(as_tuple=False).view(-1)
+            root = torch.cat([root.new_zeros(1), root + 1], dim=0)
+            news = x[root]
 
-        x = F.log_softmax(self.lin2(x), dim=-1)
+            news = self.lin0(news).relu()
+            h = self.lin1(torch.cat([news, h], dim=-1)).relu()
 
-        return x
+        h = self.lin2(h)
+        return h.log_softmax(dim=-1)
 
 
-def eval_deep(log, loader):
-    """
-    Evaluating the classification performance given mini-batch data
-    """
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = Net(args.model, train_dataset.num_features, 128,
+            train_dataset.num_classes, concat=True).to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=0.01)
 
-    # get the empirical batch_size for each mini-batch
-    data_size = len(loader.dataset.indices)
-    batch_size = loader.batch_size
-    if data_size % batch_size == 0:
-        size_list = [batch_size] * (data_size // batch_size)
-    else:
-        size_list = [batch_size] * (data_size // batch_size) + \
-                    [data_size % batch_size]
 
-    assert len(log) == len(size_list)
+def train():
+    model.train()
 
-    accuracy, f1_macro, f1_micro, precision, recall = 0, 0, 0, 0, 0
+    total_loss = 0
+    for data in train_loader:
+        data = data.to(device)
+        optimizer.zero_grad()
+        out = model(data.x, data.edge_index, data.batch)
+        loss = F.nll_loss(out, data.y)
+        loss.backward()
+        optimizer.step()
+        total_loss += float(loss) * data.num_graphs
 
-    prob_log, label_log = [], []
-
-    for batch, size in zip(log, size_list):
-        pred_y = batch[0].data.cpu().numpy().argmax(axis=1)
-        y = batch[1].data.cpu().numpy().tolist()
-        prob_log.extend(batch[0].data.cpu().numpy()[:, 1].tolist())
-        label_log.extend(y)
-
-        accuracy += accuracy_score(y, pred_y) * size
-        f1_macro += f1_score(y, pred_y, average='macro') * size
-        f1_micro += f1_score(y, pred_y, average='micro') * size
-        precision += precision_score(y, pred_y, zero_division=0) * size
-        recall += recall_score(y, pred_y, zero_division=0) * size
-
-    auc = roc_auc_score(label_log, prob_log)
-    ap = average_precision_score(label_log, prob_log)
-
-    accuracy /= data_size
-    f1_macro /= data_size
-    f1_micro /= data_size
-    precision /= data_size
-    recall /= data_size
-
-    return accuracy, f1_macro, f1_micro, precision, recall, auc, ap
+    return total_loss / len(train_loader.dataset)
 
 
 @torch.no_grad()
-def compute_test(loader, verbose=False):
+def test(loader):
     model.eval()
-    loss_test = 0.0
-    out_log = []
+
+    total_correct = total_examples = 0
     for data in loader:
-        if not args.multi_gpu:
-            data = data.to(args.device)
-        out = model(data)
-        if args.multi_gpu:
-            y = torch.cat([d.y.unsqueeze(0) for d in data]) \
-                .squeeze().to(out.device)
-        else:
-            y = data.y
-        if verbose:
-            print(F.softmax(out, dim=1).cpu().numpy())
-        out_log.append([F.softmax(out, dim=1), y])
-        loss_test += F.nll_loss(out, y).item()
-    return eval_deep(out_log, loader), loss_test
+        data = data.to(device)
+        pred = model(data.x, data.edge_index, data.batch).argmax(dim=-1)
+        total_correct += int((pred == data.y).sum())
+        total_examples += data.num_graphs
+
+    return total_correct / total_examples
 
 
-parser = argparse.ArgumentParser()
-
-parser.add_argument('--seed', type=int, default=777, help='random seed')
-parser.add_argument('--device', type=str, default='cuda:0',
-                    help='specify cuda devices')
-
-# hyper-parameters
-parser.add_argument('--dataset', type=str, default='politifact',
-                    help='[politifact, gossipcop]')
-parser.add_argument('--batch_size', type=int, default=128, help='batch size')
-parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
-parser.add_argument('--weight_decay', type=float, default=0.01,
-                    help='weight decay')
-parser.add_argument('--nhid', type=int, default=128, help='hidden layer size')
-parser.add_argument('--epochs', type=int, default=60,
-                    help='maximum number of epochs')
-parser.add_argument('--concat', type=bool, default=True,
-                    help='whether concat news embedding and graph embedding')
-parser.add_argument('--multi_gpu', type=bool, default=False,
-                    help='multi-gpu mode')
-parser.add_argument('--benchmark', type=bool, default=True,
-                    help='whether using fixed data split')
-parser.add_argument('--feature', type=str, default='spacy',
-                    help='feature type, [profile, spacy, bert, content]')
-parser.add_argument('--model', type=str, default='gcn',
-                    help='model type, [gcn, gat, sage]')
-
-args = parser.parse_args()
-torch.manual_seed(args.seed)
-if torch.cuda.is_available():
-    torch.cuda.manual_seed(args.seed)
-
-dataset = UPFD(root='data', feature=args.feature, name=args.dataset,
-               transform=ToUndirected())
-
-args.num_classes = dataset.num_classes
-args.num_features = dataset.num_features
-
-print(args)
-
-if args.benchmark:
-    training_set = Subset(dataset, dataset.train_idx.numpy().tolist())
-    validation_set = Subset(dataset, dataset.val_idx.numpy().tolist())
-    test_set = Subset(dataset, dataset.test_idx.numpy().tolist())
-else:
-    num_training = int(len(dataset) * 0.2)
-    num_val = int(len(dataset) * 0.1)
-    num_test = len(dataset) - (num_training + num_val)
-    training_set, validation_set, test_set = \
-        random_split(dataset, [num_training, num_val, num_test])
-
-if args.multi_gpu:
-    loader = DataListLoader
-else:
-    loader = DataLoader
-
-train_loader = loader(training_set, batch_size=args.batch_size, shuffle=True)
-val_loader = loader(validation_set, batch_size=args.batch_size, shuffle=False)
-test_loader = loader(test_set, batch_size=args.batch_size, shuffle=False)
-
-model = Model(args, concat=args.concat)
-if args.multi_gpu:
-    model = DataParallel(model)
-model = model.to(args.device)
-optimizer = torch.optim.Adam(model.parameters(), lr=args.lr,
-                             weight_decay=args.weight_decay)
-
-if __name__ == '__main__':
-
-    t = time.time()
-    model.train()
-    for epoch in tqdm(range(args.epochs)):
-        loss_train = 0.0
-        out_log = []
-        for i, data in enumerate(train_loader):
-            optimizer.zero_grad()
-            if not args.multi_gpu:
-                data = data.to(args.device)
-            out = model(data)
-            if args.multi_gpu:
-                y = torch.cat([d.y.unsqueeze(0) for d in data]) \
-                    .squeeze().to(out.device)
-            else:
-                y = data.y
-            loss = F.nll_loss(out, y)
-            loss.backward()
-            optimizer.step()
-            loss_train += loss.item()
-            out_log.append([F.softmax(out, dim=1), y])
-        acc_train, _, _, _, recall_train, auc_train, _ = \
-            eval_deep(out_log, train_loader)
-        [acc_val, _, _, _, recall_val, auc_val, _], loss_val = \
-            compute_test(val_loader)
-        print(f'loss_train: {loss_train:.4f}, acc_train: {acc_train:.4f},'
-              f' recall_train: {recall_train:.4f}, auc_train: {auc_train:.4f},'
-              f' loss_val: {loss_val:.4f}, acc_val: {acc_val:.4f},'
-              f' recall_val: {recall_val:.4f}, auc_val: {auc_val:.4f}')
-
-    [acc_test, f1_macro_test, f1_micro_test, precision_test, recall_test,
-     auc_test, ap_test], test_loss = compute_test(test_loader, verbose=False)
-    print(f'Test set results: acc: {acc_test:.4f},'
-          f'f1_macro: {f1_macro_test:.4f}, f1_micro: {f1_micro_test:.4f}, '
-          f'precision: {precision_test:.4f}, recall: {recall_test:.4f},'
-          f' auc: {auc_test:.4f}, ap: {ap_test:.4f}')
+for epoch in range(1, 61):
+    loss = train()
+    train_acc = test(train_loader)
+    val_acc = test(val_loader)
+    test_acc = test(test_loader)
+    print(f'Epoch: {epoch:02d}, Loss: {loss:.4f}, Train: {train_acc:.4f}, '
+          f'Val: {val_acc:.4f}, Test: {test_acc:.4f}')
--- torch_geometric/datasets/upfd.py
+++ torch_geometric/datasets/upfd.py
@@ -1,36 +1,42 @@
+import os
 import os.path as osp
-import numpy as np
-import scipy.sparse as sp
-import shutil
-from google_drive_downloader import GoogleDriveDownloader as gdd
 
 import torch
-from torch_geometric.data import Data, InMemoryDataset
-from torch_geometric.utils import add_self_loops
+import numpy as np
+import scipy.sparse as sp
+from google_drive_downloader import GoogleDriveDownloader as gdd
+
 from torch_sparse import coalesce
 from torch_geometric.io import read_txt_array
+from torch_geometric.data import Data, InMemoryDataset
 
 
 class UPFD(InMemoryDataset):
-    r"""The tree-structured fake news propagation graph classification
-    dataset from the `"User Preference-aware Fake News Detection"
+    r"""The tree-structured fake news propagation graph classification dataset
+    from the `"User Preference-aware Fake News Detection"
     <https://arxiv.org/abs/2104.12259>`_ paper.
-    It includes two sets of tree-structured fake&real news propagation
-    graphs extracted from Twitter.
-    For a single graph, the root node represents the source news,
-    leaf nodes represent Twitter users who retweeted the same root news.
-    A user node has an edge to the news node if and only if the user
-    retweeted the root news directly.
-    Two user nodes have an edge if and only if one user retweeted
-    the root news from the other user.
+    It includes two sets of tree-structured fake & real news propagation graphs
+    extracted from Twitter.
+    For a single graph, the root node represents the source news, and leaf
+    nodes represent Twitter users who retweeted the same root news.
+    A user node has an edge to the news node if and only if the user retweeted
+    the root news directly.
+    Two user nodes have an edge if and only if one user retweeted the root news
+    from the other user.
     Four different node features are encoded using different encoders.
-    Refer to `GNN-FakeNews <https://github.com/safe-graph/GNN-FakeNews>`_
-    repo for more details.
+    Please refer to `GNN-FakeNews
+    <https://github.com/safe-graph/GNN-FakeNews>`_ repo for more details.
+
+    .. note::
+
+        For an example of using UPFD, see `examples/upfd.py
+        <https://github.com/rusty1s/pytorch_geometric/blob/master/examples/
+        upfd.py>`_.
 
     Args:
         root (string): Root directory where the dataset should be saved.
-        name (string): The name of the graph set, (:obj:`"politifact"`,
-         :obj:`"gossipcop"`).
+        name (string): The name of the graph set (:obj:`"politifact"`,
+            :obj:`"gossipcop"`).
         feature (string): The node feature type (:obj:`"profile"`,
             :obj:`"spacy"`, :obj:`"bert"`, :obj:`"content"`).
             If set to :obj:`"profile"`, the 10-dimensional node feature
@@ -45,6 +51,11 @@ class UPFD(InMemoryDataset):
             If set to :obj:`"content"`, the 310-dimensional node feature is
             composed of a 300-dimensional "spacy" vector plus a
             10-dimensional "profile" vector.
+        split (string, optional): If :obj:`"train"`, loads the training
+            dataset.
+            If :obj:`"val"`, loads the validation dataset.
+            If :obj:`"test"`, loads the test dataset.
+            (default: :obj:`"train"`)
         transform (callable, optional): A function/transform that takes in an
             :obj:`torch_geometric.data.Data` object and returns a transformed
             version. The data object will be transformed before every access.
@@ -59,134 +70,86 @@ class UPFD(InMemoryDataset):
             final dataset. (default: :obj:`None`)
     """
 
-    pol_id = '1KOmSrlGcC50PjkvRVbyb_WoWHVql06J-'
-    gos_id = '1VskhAQ92PrT4sWEKQ2v2-AJhEcpp4A81'
+    ids = {
+        'politifact': '1KOmSrlGcC50PjkvRVbyb_WoWHVql06J-',
+        'gossipcop': '1VskhAQ92PrT4sWEKQ2v2-AJhEcpp4A81',
+    }
 
-    def __init__(self, root, name, feature,
-                 transform=None, pre_transform=None, pre_filter=None):
+    def __init__(self, root, name, feature, split="train", transform=None,
+                 pre_transform=None, pre_filter=None):
         self.root = root
         self.name = name
         self.feature = feature
         super(UPFD, self).__init__(root, transform, pre_transform, pre_filter)
 
-        self.data, self.slices, self.train_idx, self.val_idx, self.test_idx\
-            = torch.load(self.processed_paths[0])
+        assert split in ['train', 'val', 'test']
+        path = self.processed_paths[['train', 'val', 'test'].index(split)]
+        self.data, self.slices = torch.load(path)
 
     @property
     def raw_dir(self):
-        name = 'raw/'
-        return osp.join(self.root, self.name, name)
+        return osp.join(self.root, self.name, 'raw')
 
     @property
     def processed_dir(self):
-        name = 'processed/'
-        return osp.join(self.root, self.name, name)
+        return osp.join(self.root, self.name, 'processed', self.feature)
 
     @property
     def raw_file_names(self):
-        names = ['node_graph_id', 'graph_labels']
-        return ['{}.npy'.format(name) for name in names] + \
-               ['A.txt', f'new_{self.feature}_feature.npz']
+        return [
+            'node_graph_id.npy', 'graph_labels.npy', 'A.txt', 'train_idx.npy',
+            'val_idx.npy', 'test_idx.npy', f'new_{self.feature}_feature.npz'
+        ]
 
     @property
     def processed_file_names(self):
-        if self.pre_filter is None:
-            return f'{self.name[:3]}_data_{self.feature}.pt'
-        else:
-            return f'{self.name[:3]}_data_{self.feature}_prefiler.pt'
+        return ['train.pt', 'val.pt', 'test.pt']
 
     def download(self):
-        shutil.rmtree(self.raw_dir)
-        if self.name == 'politifact':
-            gdd.download_file_from_google_drive(self.pol_id, self.raw_dir +
-                                                f'{self.name}.zip', unzip=True)
-        else:
-            gdd.download_file_from_google_drive(self.gos_id, self.raw_dir +
-                                                f'{self.name}.zip', unzip=True)
+        gdd.download_file_from_google_drive(
+            self.ids[self.name], osp.join(self.raw_dir, f'{self.name}.zip'),
+            unzip=True)
+        os.remove(osp.join(self.raw_dir, f'{self.name}.zip'))
 
     def process(self):
+        x = sp.load_npz(
+            osp.join(self.raw_dir, f'new_{self.feature}_feature.npz'))
+        x = torch.from_numpy(x.todense()).to(torch.float)
 
-        self.data, self.slices = read_graph_data(self.raw_dir, self.feature)
+        edge_index = read_txt_array(osp.join(self.raw_dir, 'A.txt'), sep=',',
+                                    dtype=torch.long).t()
+        edge_index, _ = coalesce(edge_index, None, x.size(0), x.size(0))
 
-        if self.pre_filter is not None:
-            data_list = [self.get(idx) for idx in range(len(self))]
-            data_list = [data for data in data_list if self.pre_filter(data)]
-            self.data, self.slices = self.collate(data_list)
+        y = np.load(osp.join(self.raw_dir, 'graph_labels.npy'))
+        y = torch.from_numpy(y).to(torch.long)
+        _, y = y.unique(sorted=True, return_inverse=True)
 
-        if self.pre_transform is not None:
-            data_list = [self.get(idx) for idx in range(len(self))]
-            data_list = [self.pre_transform(data) for data in data_list]
-            self.data, self.slices = self.collate(data_list)
+        batch = np.load(osp.join(self.raw_dir, 'node_graph_id.npy'))
+        batch = torch.from_numpy(batch).to(torch.long)
 
-        # The fixed data split for benchmarking evaluation
-        # train-val-test split is 20%-10%-70%
-        self.train_idx = torch.from_numpy(np.load(
-            self.raw_dir + 'train_idx.npy')).to(torch.long)
-        self.val_idx = torch.from_numpy(np.load(
-            self.raw_dir + 'val_idx.npy')).to(torch.long)
-        self.test_idx = torch.from_numpy(np.load(
-            self.raw_dir + 'test_idx.npy')).to(torch.long)
+        node_slice = torch.cumsum(batch.bincount(), 0)
+        node_slice = torch.cat([torch.tensor([0]), node_slice])
+        edge_slice = torch.cumsum(batch[edge_index[0]].bincount(), 0)
+        edge_slice = torch.cat([torch.tensor([0]), edge_slice])
+        graph_slice = torch.arange(y.size(0) + 1)
+        self.slices = {
+            'x': node_slice,
+            'edge_index': edge_slice,
+            'y': graph_slice
+        }
 
-        torch.save((self.data, self.slices, self.train_idx,
-                    self.val_idx, self.test_idx), self.processed_paths[0])
+        edge_index -= node_slice[batch[edge_index[0]]].view(1, -1)
+        self.data = Data(x=x, edge_index=edge_index, y=y)
+
+        for path, split in zip(self.processed_paths, ['train', 'val', 'test']):
+            idx = np.load(osp.join(self.raw_dir, f'{split}_idx.npy')).tolist()
+            data_list = [self.get(i) for i in idx]
+            if self.pre_filter is not None:
+                data_list = [d for d in data_list if self.pre_filter(d)]
+            if self.pre_transform is not None:
+                data_list = [self.pre_transform(d) for d in data_list]
+            torch.save(self.collate(data_list), path)
 
     def __repr__(self):
-        return '{}({})'.format(self.name, len(self))
-
-
-def split(data, batch):
-    """
-    Util code to create graph batches
-    """
-
-    node_slice = torch.cumsum(torch.from_numpy(np.bincount(batch)), 0)
-    node_slice = torch.cat([torch.tensor([0]), node_slice])
-
-    row, _ = data.edge_index
-    edge_slice = torch.cumsum(torch.from_numpy(np.bincount(batch[row])), 0)
-    edge_slice = torch.cat([torch.tensor([0]), edge_slice])
-
-    # Edge indices should start at zero for every graph.
-    data.edge_index -= node_slice[batch[row]].unsqueeze(0)
-    data.__num_nodes__ = torch.bincount(batch).tolist()
-
-    slices = {'edge_index': edge_slice}
-    if data.x is not None:
-        slices['x'] = node_slice
-    if data.edge_attr is not None:
-        slices['edge_attr'] = edge_slice
-    if data.y is not None:
-        if data.y.size(0) == batch.size(0):
-            slices['y'] = node_slice
-        else:
-            slices['y'] = torch.arange(0, batch[-1] + 2, dtype=torch.long)
-
-    return data, slices
-
-
-def read_graph_data(folder, feature):
-    """
-    Util code to create PyG data instance from raw graph data
-    """
-
-    node_attributes = sp.load_npz(folder + f'new_{feature}_feature.npz')
-    edge_index = read_txt_array(osp.join(folder, 'A.txt'),
-                                sep=',', dtype=torch.long).t()
-    node_graph_id = np.load(folder + 'node_graph_id.npy')
-    graph_labels = np.load(folder + 'graph_labels.npy')
-
-    edge_attr = None
-    x = torch.from_numpy(node_attributes.todense()).to(torch.float)
-    node_graph_id = torch.from_numpy(node_graph_id).to(torch.long)
-    y = torch.from_numpy(graph_labels).to(torch.long)
-    _, y = y.unique(sorted=True, return_inverse=True)
-
-    num_nodes = edge_index.max().item() + 1 if x is None else x.size(0)
-    edge_index, edge_attr = add_self_loops(edge_index, edge_attr)
-    edge_index, edge_attr = coalesce(edge_index, edge_attr,
-                                     num_nodes, num_nodes)
-
-    data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr, y=y)
-    data, slices = split(data, node_graph_id)
-
-    return data, slices
+        return (f'{self.__class__.__name__}({len(self)}, name={self.name}, '
+                f'feature={self.feature})')
