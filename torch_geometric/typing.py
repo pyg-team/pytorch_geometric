737fca8e6aaab10b84d118279bdc356a660ee6c8
rusty1s <matthias.fey@tu-dortmund.de>
2021-05-21 09:01:14 +0200

    add comments to type hint definitions

--- torch_geometric/typing.py
+++ torch_geometric/typing.py
@@ -1,15 +1,23 @@
-from typing import Tuple, Optional, Union, Callable
+from typing import Tuple, Optional, Union
 
 from torch import Tensor
 from torch_sparse import SparseTensor
 
-# Data Types
-NodeType = str
-EdgeType = Tuple[str, str, str]
-QueryType = Union[str, NodeType, EdgeType, Tuple[str, str]]
-AttrType = Union[str, Tuple[str, Callable]]
+# Types for accesing data:
 
-# Message Passing Types
+# Node-types are denoted by a single string, e.g.: `data['paper']`
+NodeType = str
+
+# Edge-types are denotes by a tripled of strings, e.g.:
+# `data[('author', 'writes', 'paper')]
+EdgeType = Tuple[str, str, str]
+
+# There exist some short-cuts to query edge-types (given that the full triplet
+# can be uniquely reconstructed, e.g.:
+# `data['writes']` or `data[('author', 'paper')]`
+QueryType = Union[str, NodeType, EdgeType, Tuple[str, str]]
+
+# Types for message passing:
 Adj = Union[Tensor, SparseTensor]
 OptTensor = Optional[Tensor]
 PairTensor = Tuple[Tensor, Tensor]
