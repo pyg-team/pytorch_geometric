47b0c9380758154408dd7e93d179bc14827c8c4c
Yulong Lin <linyulong97@gmail.com>
2021-05-22 15:13:18 +0100

    Add PyTorch dataset and dataloader classes for TemporalData

---
+++ test/data/test_temporal_dataloader.py
@@ -0,0 +1,29 @@
+import torch
+from torch_geometric.data import TemporalData, TemporalDataset, TemporalDataLoader
+
+
+def test_temporal_dataloader():
+    src = torch.tensor([1, 2, 0])
+    dst = torch.tensor([3, 1, 2])
+    t = torch.Tensor([0., 5., 29.])
+    msg = torch.Tensor([[564., 8997., 22., 5.],
+                        [8965., 53., 5., 892.],
+                        [879., 23., 564., 88.]])
+    y = torch.tensor([0, 1, 0])
+    data = TemporalData(src=src, dst=dst, t=t, msg=msg, y=y)
+
+    assert data.__repr__() == ('TemporalData(dst=[3], msg=[3, 4], src=[3], t=[3], y=[3])')
+
+    dataset = TemporalDataset(data)
+
+    loader = TemporalDataLoader(dataset, batch_size=2, shuffle=False)
+
+    for batch in loader:
+        assert len(batch) == len(batch.keys)
+        assert batch.src.tolist() == [1, 2]
+        assert batch.dst.tolist() == [3, 1]
+        assert batch.t.tolist() == [0., 5.]
+        assert batch.msg.tolist() == [[564., 8997., 22., 5.],
+                                     [8965., 53., 5., 892.]]
+        assert batch.y.tolist() == [0, 1]
+        break
---
+++ torch_geometric/data/temporal_dataloader.py
@@ -0,0 +1,53 @@
+import torch
+from torch.utils.data.dataloader import default_collate
+from torch_geometric.data import TemporalData
+
+
+class TemporalDataset(torch.utils.data.Dataset):
+    """
+    Converts a TemporalData object to a PyTorch Dataset
+    """
+    def __init__(self, temporal_data: TemporalData):
+        self.temporal_data = temporal_data
+
+    def __len__(self):
+        return len(self.temporal_data.src)
+
+    def __getitem__(self, idx):
+        return self.temporal_data[idx]
+
+
+class TemporalDataCollater(object):
+    """
+    Collates TemporalData objects for batching
+    """
+    def collate(self, temporal_data_list):
+        batch = TemporalData()
+        for key in temporal_data_list[0].keys:
+            batch[key] = default_collate([d[key] for d in temporal_data_list
+                                          ]).squeeze(1)
+        return batch
+
+    def __call__(self, batch):
+        return self.collate(batch)
+
+
+class TemporalDataLoader(torch.utils.data.DataLoader):
+    """
+    PyTorch DataLoader for TemporalData objects
+    """
+    def __init__(self,
+                 dataset: TemporalDataset,
+                 batch_size=1,
+                 shuffle=False,
+                 **kwargs):
+
+        if "collate_fn" in kwargs:
+            del kwargs["collate_fn"]
+
+        super(TemporalDataLoader,
+              self).__init__(dataset,
+                             batch_size,
+                             shuffle,
+                             collate_fn=TemporalDataCollater(),
+                             **kwargs)
