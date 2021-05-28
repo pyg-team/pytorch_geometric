487bd91635200d4de4b60c997060c489653825fe
rusty1s <matthias.fey@tu-dortmund.de>
2021-05-28 09:49:04 +0200

    mag docstring

--- torch_geometric/datasets/ogb_mag.py
+++ torch_geometric/datasets/ogb_mag.py
@@ -13,7 +13,19 @@ from torch_geometric.data import (InMemoryDataset, HeteroData, download_url,
 
 
 class OGB_MAG(InMemoryDataset):
-    r"""
+    r"""The ogbn-mag dataset from the `"Open Graph Benchmark: Datasets for
+    Machine Learning on Graphs" <https://arxiv.org/abs/2005.00687>`_ paper.
+    ogbn-mag is a heterogeneous graph composed of a subset of the Microsoft
+    Academic Graph (MAG).
+    It contains four types of entities—papers (736,389 nodes), authors
+    (1,134,649 nodes), institutions (8,740 nodes), and fields of study
+    (59,965 nodes) — as well as four types of directed relations connecting two
+    types of entities.
+    Each paper is associated with a 128-dimensional :obj:`word2vec` feature
+    vector, while all other node types are not associated with any input
+    features.
+    The task is to predict the venue (conference or journal) of each paper.
+    In total, there are 349 different venues.
 
     Args:
         root (string): Root directory where the dataset should be saved.
