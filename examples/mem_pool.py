49f86c9dd21edd92d73b1a8fd493bd1abb85ac98
wsad1 <jinu.sunil@gmail.com>
2021-06-24 15:57:52 +0530

    added comments

--- examples/mem_pool.py
+++ examples/mem_pool.py
@@ -11,7 +11,7 @@ from torch import optim
 path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data',
                 'PROTEINS_full')
 dataset = TUDataset(path, name="PROTEINS_full", use_node_attr=True)
-dataset.data.x = dataset.data.x[:, :-3]
+dataset.data.x = dataset.data.x[:, :-3]  # only use non-binary features.
 dataset = dataset.shuffle()
 n = (len(dataset)) // 10
 
