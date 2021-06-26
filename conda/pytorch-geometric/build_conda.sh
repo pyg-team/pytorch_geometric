e7d3b8627401fc73e22c11fc8d51ff653dddfef3
rusty1s <matthias.fey@tu-dortmund.de>
2021-06-26 10:16:52 +0200

    [ci skip]

--- conda/pytorch-geometric/build_conda.sh
+++ conda/pytorch-geometric/build_conda.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
 
+export PYTHON_VERSION=$1
+export TORCH_VERSION=$2
+export CUDA_VERSION=$3
+
+export CONDA_PYTORCH_CONSTRAINT="pytorch==${TORCH_VERSION%.*}.*"
+
+if [ "${CUDA_VERSION}" = "cpu" ]; then
+  export CONDA_CUDATOOLKIT_CONSTRAINT="cpuonly"
+else
+  case $CUDA_VERSION in
+    cu111)
+      export CONDA_CUDATOOLKIT_CONSTRAINT="cudatoolkit==11.1.*"
+      ;;
+    cu102)
+      export CONDA_CUDATOOLKIT_CONSTRAINT="cudatoolkit==10.2.*"
+      ;;
+    cu101)
+      export CONDA_CUDATOOLKIT_CONSTRAINT="cudatoolkit==10.1.*"
+      ;;
+    *)
+      echo "Unrecognized CUDA_VERSION=$CUDA_VERSION"
+      exit 1
+      ;;
+  esac
+fi
+
+echo "PyTorch $TORCH_VERSION+$CUDA_VERSION"
+echo "- $CONDA_PYTORCH_CONSTRAINT"
+echo "- $CONDA_CUDATOOLKIT_CONSTRAINT"
+
 conda build . -c defaults -c pytorch -c conda-forge -c rusty1s --output-folder "$HOME/conda-bld"
