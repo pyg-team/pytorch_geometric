3c6392d8f46d469c15f17ce17eb5c8ce724df28d
rusty1s <matthias.fey@tu-dortmund.de>
2021-07-08 07:39:28 +0200

    rename

--- torch_geometric/nn/__init__.py
+++ torch_geometric/nn/__init__.py
@@ -2,7 +2,7 @@ from .sequential import Sequential
 from .meta import MetaLayer
 from .data_parallel import DataParallel
 from .reshape import Reshape
-from .to_hetero import to_hetero
+from .to_hetero_transformer import to_hetero
 from .to_hetero_with_bases_transformer import to_hetero_with_bases
 
 from .conv import *  # noqa
