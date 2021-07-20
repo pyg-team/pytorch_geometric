132f7e66aad0a8896ec3dc7edf3d22dfed1bca3c
rusty1s <matthias.fey@tu-dortmund.de>
2021-07-20 09:14:03 +0200

    fix removeisolatednodes transform in case 'data.num_nodes' is present

--- torch_geometric/transforms/remove_isolated_nodes.py
+++ torch_geometric/transforms/remove_isolated_nodes.py
@@ -6,11 +6,13 @@ from torch_geometric.utils import remove_isolated_nodes
 
 class RemoveIsolatedNodes(object):
     r"""Removes isolated nodes from the graph."""
-
     def __call__(self, data):
         num_nodes = data.num_nodes
         out = remove_isolated_nodes(data.edge_index, data.edge_attr, num_nodes)
         data.edge_index, data.edge_attr, mask = out
+
+        if hasattr(data, '__num_nodes__'):
+            data.num_nodes = int(mask.sum())
 
         for key, item in data:
             if bool(re.search('edge', key)):
