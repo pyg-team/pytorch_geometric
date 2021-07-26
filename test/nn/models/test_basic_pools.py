7179a54c9ffd2cc66278a1e68827abe0f2000d82
PabloAMC <pmorenocf@alumnos.unex.es>
2021-07-26 20:12:37 +0200

    Update test_basic_pools.py

--- test/nn/models/test_basic_pools.py
+++ test/nn/models/test_basic_pools.py
@@ -12,7 +12,7 @@ acts = [None, torch.relu_, F.elu, ReLU()]
 norms = [None, BatchNorm1d(16), LayerNorm(16)]
 jks = ['last', 'cat', 'max', 'lstm']
 glob_pools = ['mean', 'max', 'add']
-ratios = [.8, .5]
+ratios = [.75, .5]
 
 
 @pytest.mark.parametrize('dropout,act,norm,jk,glob_pool,ratio',
@@ -20,45 +20,57 @@ ratios = [.8, .5]
 def test_asapool(dropout, act, norm, jk, glob_pool, ratio):
     x = torch.randn(3, 8)
     edge_index = torch.tensor([[0, 1, 1, 2], [1, 0, 2, 1]])
-    out_channels = 16 if jk != 'cat' else 32
+    in_channels = 8
+    hidden_channels = 16
+    num_layers = 2
+    out_channels = hidden_channels if jk != 'cat' else num_layers*hidden_channels
 
-    model = ASAPool(in_channels=8, hidden_channels=16, num_layers=2,
+    model = ASAPool(in_channels=in_channels, hidden_channels=hidden_channels, num_layers=num_layers,
                 dropout=dropout, act=act, norm=norm, jk=jk, glob_pool=glob_pool, ratio=ratio)
     assert str(model) == f'ASAPool(8, {out_channels}, num_layers=2)' #todo:change out_channels
-    assert model(x, edge_index).size() == (3, out_channels)
+    assert model(x, edge_index).size() == (1, out_channels)
 
 @pytest.mark.parametrize('dropout,act,norm,jk,glob_pool',
                          product(dropouts, acts, norms, jks, glob_pools))
 def test_edgepool(dropout, act, norm, jk, glob_pool):
     x = torch.randn(3, 8)
     edge_index = torch.tensor([[0, 1, 1, 2], [1, 0, 2, 1]])
-    out_channels = 16 if jk != 'cat' else 32
+    in_channels = 8
+    hidden_channels = 16
+    num_layers = 2
+    out_channels = hidden_channels if jk != 'cat' else num_layers*hidden_channels
 
-    model = EdgePool(in_channels=8, hidden_channels=16, num_layers=2,
+    model = EdgePool(in_channels=in_channels, hidden_channels=hidden_channels, num_layers=num_layers,
                 dropout=dropout, act=act, norm=norm, jk=jk, glob_pool=glob_pool)
     assert str(model) == f'EdgePool(8, {out_channels}, num_layers=2)' #todo:change out_channels
-    assert model(x, edge_index).size() == (3, out_channels)
+    assert model(x, edge_index).size() == (1, out_channels)
 
 @pytest.mark.parametrize('dropout,act,norm,jk,glob_pool,ratio',
                          product(dropouts, acts, norms, jks, glob_pools, ratios))
 def test_sagpool(dropout, act, norm, jk, glob_pool, ratio):
     x = torch.randn(3, 8)
     edge_index = torch.tensor([[0, 1, 1, 2], [1, 0, 2, 1]])
-    out_channels = 16 if jk != 'cat' else 32
+    in_channels = 8
+    hidden_channels = 16
+    num_layers = 2
+    out_channels = hidden_channels if jk != 'cat' else num_layers*hidden_channels
 
-    model = SAGPool(in_channels=8, hidden_channels=16, num_layers=2,
+    model = SAGPool(in_channels=in_channels, hidden_channels=hidden_channels, num_layers=num_layers,
                 dropout=dropout, act=act, norm=norm, jk=jk, glob_pool=glob_pool, ratio=ratio)
     assert str(model) == f'SAGPool(8, {out_channels}, num_layers=2)' #todo:change out_channels
-    assert model(x, edge_index).size() == (3, out_channels)
+    assert model(x, edge_index).size() == (1, out_channels)
 
 @pytest.mark.parametrize('dropout,act,norm,jk,glob_pool',
                          product(dropouts, acts, norms, jks, glob_pools))
 def test_graclus(dropout, act, norm, jk, glob_pool):
     x = torch.randn(3, 8)
     edge_index = torch.tensor([[0, 1, 1, 2], [1, 0, 2, 1]])
-    out_channels = 16 if jk != 'cat' else 32
+    in_channels = 8
+    hidden_channels = 16
+    num_layers = 2
+    out_channels = hidden_channels if jk != 'cat' else num_layers*hidden_channels
 
-    model = Graclus(in_channels=8, hidden_channels=16, num_layers=2,
+    model = Graclus(in_channels=in_channels, hidden_channels=hidden_channels, num_layers=num_layers,
                 dropout=dropout, act=act, norm=norm, jk=jk, glob_pool=glob_pool)
     assert str(model) == f'Graclus(8, {out_channels}, num_layers=2)' #todo:change out_channels
-    assert model(x, edge_index).size() == (3, out_channels)
+    assert model(x, edge_index).size() == (1, out_channels)
