de2769894b7a2ea605e46da4b575fa9a655ebb46
benedekrozemberczki <benedek.rozemberczki@gmail.com>
2021-08-07 20:26:40 +0100

    PDN WWW 2021

--- test/nn/conv/test_pdn_conv.py
+++ test/nn/conv/test_pdn_conv.py
@@ -1,5 +1,4 @@
 import torch
-from torch_sparse import SparseTensor
 from torch_geometric.nn import PDNConv
 
 
@@ -13,6 +12,7 @@ def test_pdn_conv():
     out = conv(x, edge_index, edge_weight)
     assert out.size() == (4, 32)
 
+
 def test_pdn_conv_with_sparse_node_input_feature():
     x = torch.sparse_coo_tensor(indices=torch.tensor([[0, 0], [0, 1]]),
                                 values=torch.tensor([1., 1.]),
@@ -24,28 +24,33 @@ def test_pdn_conv_with_sparse_node_input_feature():
 
     out = conv(x, edge_index, edge_weight)
     assert out.size() == (4, 32)
-    
+
+
 def test_pdn_conv_with_sparse_edge_input_feature():
     x = torch.randn(4, 16)
-    edge_weight = torch.sparse_coo_tensor(indices=torch.tensor([[0, 0], [0, 1]]),
+    edge_weight = torch.sparse_coo_tensor(indices=torch.tensor([[0, 0],
+                                                                [0, 1]]),
                                 values=torch.tensor([1., 1.]),
-                                size=torch.Size([6, 8]))       
+                                size=torch.Size([6, 8]))
+
     edge_index = torch.tensor([[0, 0, 0, 1, 2, 3], [1, 2, 3, 0, 0, 0]])
 
     conv = PDNConv(16, 32, 8, 128)
 
     out = conv(x, edge_index, edge_weight)
     assert out.size() == (4, 32)
-    
+
+
 def test_pdn_conv_with_sparse_node_and_edge_input_feature():
     x = torch.sparse_coo_tensor(indices=torch.tensor([[0, 0], [0, 1]]),
                                 values=torch.tensor([1., 1.]),
                                 size=torch.Size([4, 16]))
-                                
-    edge_weight = torch.sparse_coo_tensor(indices=torch.tensor([[0, 0], [0, 1]]),
+
+    edge_weight = torch.sparse_coo_tensor(indices=torch.tensor([[0, 0],
+                                                                [0, 1]]),
                                 values=torch.tensor([1., 1.]),
                                 size=torch.Size([6, 8]))
-                                
+
     edge_index = torch.tensor([[0, 0, 0, 1, 2, 3], [1, 2, 3, 0, 0, 0]])
 
     conv = PDNConv(16, 32, 8, 128)
