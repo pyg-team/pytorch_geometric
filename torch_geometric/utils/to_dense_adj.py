8004ac4aa74ce2823d99f1cda79bfc3ff4c6eb09
rusty1s <matthias.fey@tu-dortmund.de>
2021-08-20 13:55:40 +0200

    allow smaller adjs in to_dense_adj

--- torch_geometric/utils/to_dense_adj.py
+++ torch_geometric/utils/to_dense_adj.py
@@ -26,19 +26,26 @@ def to_dense_adj(edge_index, batch=None, edge_attr=None, max_num_nodes=None):
     num_nodes = scatter(one, batch, dim=0, dim_size=batch_size, reduce='add')
     cum_nodes = torch.cat([batch.new_zeros(1), num_nodes.cumsum(dim=0)])
 
-    if edge_attr is None:
-        edge_attr = torch.ones(edge_index.size(1), device=edge_index.device)
+    idx0 = batch[edge_index[0]]
+    idx1 = edge_index[0] - cum_nodes[batch][edge_index[0]]
+    idx2 = edge_index[1] - cum_nodes[batch][edge_index[1]]
 
     if max_num_nodes is None:
         max_num_nodes = num_nodes.max().item()
 
+    elif idx1.max() >= max_num_nodes or idx2.max() >= max_num_nodes:
+        mask = (idx1 < max_num_nodes) & (idx2 < max_num_nodes)
+        idx0 = idx0[mask]
+        idx1 = idx1[mask]
+        idx2 = idx2[mask]
+        edge_attr = None if edge_attr is None else edge_attr[mask]
+
+    if edge_attr is None:
+        edge_attr = torch.ones(idx0.numel(), device=edge_index.device)
+
     size = [batch_size, max_num_nodes, max_num_nodes]
     size += list(edge_attr.size())[1:]
     adj = torch.zeros(size, dtype=edge_attr.dtype, device=edge_index.device)
-
-    idx0 = batch[edge_index[0]]
-    idx1 = edge_index[0] - cum_nodes[batch][edge_index[0]]
-    idx2 = edge_index[1] - cum_nodes[batch][edge_index[1]]
 
     flattened_size = batch_size * max_num_nodes * max_num_nodes
     adj = adj.view([flattened_size] + list(adj.size())[3:])
