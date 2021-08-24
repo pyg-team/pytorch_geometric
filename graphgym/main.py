4ccecd86ee4c0334b8dda1afc8ab51a1a5a0d10d
Dong Wang <dongw89@gmail.com>
2021-08-23 21:36:57 -0700

    create optimizer config and scheduler config separately (#113)
    * create optimizer config and scheduler config separately
    
    * fix format
    
    * import explicitly
    
    Co-authored-by: Dong Wang <dongwang@yannis-air.lan>

--- graphgym/main.py
+++ graphgym/main.py
@@ -13,13 +13,30 @@ from torch_geometric.graphgym.config import (cfg, assert_cfg, dump_cfg,
 from torch_geometric.graphgym.loader import create_loader
 from torch_geometric.graphgym.logger import setup_printing, create_logger
 from torch_geometric.graphgym.optimizer import create_optimizer, \
-    create_scheduler
+    create_scheduler, OptimizerConfig, SchedulerConfig
 from torch_geometric.graphgym.model_builder import create_model
 from torch_geometric.graphgym.train import train
 from torch_geometric.graphgym.utils.agg_runs import agg_runs
 from torch_geometric.graphgym.utils.comp_budget import params_count
 from torch_geometric.graphgym.utils.device import auto_select_device
 from torch_geometric.graphgym.register import train_dict
+
+
+def new_optimizer_config(cfg):
+    return OptimizerConfig(
+        optimizer=cfg.optim.optimizer,
+        base_lr=cfg.optim.base_lr,
+        weight_decay=cfg.optim.weight_decay,
+        momentum=cfg.optim.momentum)
+
+
+def new_scheduler_config(cfg):
+    return SchedulerConfig(
+        scheduler=cfg.optim.scheduler,
+        steps=cfg.optim.steps,
+        lr_decay=cfg.optim.lr_decay,
+        max_epoch=cfg.optim.max_epoch)
+
 
 if __name__ == '__main__':
     # Load cmd line args
@@ -45,8 +62,10 @@ if __name__ == '__main__':
         loaders = create_loader()
         meters = create_logger()
         model = create_model()
-        optimizer = create_optimizer(model.parameters())
-        scheduler = create_scheduler(optimizer)
+        optimizer = create_optimizer(
+            model.parameters(), new_optimizer_config(cfg))
+        scheduler = create_scheduler(
+            optimizer, new_scheduler_config(cfg))
         # Print model info
         logging.info(model)
         logging.info(cfg)
