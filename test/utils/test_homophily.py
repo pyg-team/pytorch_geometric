ebba8201d32b1a61fbed1853a0ecd36c727adf91
wsad1 <jinu.sunil@gmail.com>
2021-08-28 11:23:29 +0530

    adding test for sparse edge index

--- test/utils/test_homophily.py
+++ test/utils/test_homophily.py
@@ -1,4 +1,5 @@
 import torch
+from torch_sparse import SparseTensor
 from torch_geometric.utils import homophily
 
 
@@ -6,8 +7,13 @@ def test_homophily():
     edge_index = torch.tensor([[0, 1, 2, 3], [1, 2, 0, 4]])
     y = torch.tensor([0, 0, 0, 0, 1])
     batch = torch.tensor([0, 0, 0, 1, 1])
+    row, col = edge_index
+    adj = SparseTensor(row=row, col=col, sparse_sizes=(5, 5))
+
     assert homophily(edge_index, y, method='edge') == 0.75
+    assert homophily(adj, y, method='edge') == 0.75
     assert homophily(edge_index, y, batch, method='edge').tolist() == [1., 0.]
     assert homophily(edge_index, y.unsqueeze(1), method='node') == 0.6
+    assert homophily(adj, y.unsqueeze(1), method='node') == 0.6
     assert homophily(edge_index, y.unsqueeze(1), batch,
                      method='node').tolist() == [1., 0.]
