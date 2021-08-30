582dc2b2bf490c9a1a924f211ac35c7c44fe65bf
omril <olevi@corephotonics.com>
2021-08-30 22:38:49 +0300

    meshcnn FAUST dataset example

---
+++ examples/meshcnn_faust.py
@@ -0,0 +1,21 @@
+import os.path as osp
+
+import torch
+import torch.nn.functional as F
+from torch_geometric.datasets import FAUST
+import torch_geometric.transforms as T
+from torch_geometric.data import DataLoader
+
+path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'FAUST')
+pre_transform = T.Compose([T.MeshCNNPrepare()])
+train_dataset = FAUST(path, True, T.Cartesian(), pre_transform)
+test_dataset = FAUST(path, False, T.Cartesian(), pre_transform)
+train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=1)
+d = train_dataset[0]
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+target = torch.arange(d.num_nodes, dtype=torch.long, device=device)
+
+for data in train_loader:
+    print('This is a test for FAUST dataset with MeshCNN structure')
