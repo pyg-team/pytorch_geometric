19a7c69338f45a4b3127ee34cee21ea31ad0ad60
omril <olevi@corephotonics.com>
2021-09-07 15:36:30 +0300

    FAUST MeshCNN example - augmentation options

--- examples/meshcnn_faust.py
+++ examples/meshcnn_faust.py
@@ -6,9 +6,15 @@ import torch_geometric.transforms as T
 from torch_geometric.data import DataLoader
 
 path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'FAUST')
-pre_transform = T.Compose([T.MeshCNNPrepare()])
-train_dataset = FAUST(path, True, T.Cartesian(), pre_transform)
-test_dataset = FAUST(path, False, T.Cartesian(), pre_transform)
+aug_slide_verts = 0.0
+aug_scale_verts = True
+aug_flip_edges = 0.5
+
+pre_transform_train = T.Compose(
+    [T.MeshCNNPrepare(aug_slide_verts, aug_scale_verts, aug_flip_edges)])
+pre_transform_test = T.Compose([T.MeshCNNPrepare()])
+train_dataset = FAUST(path, True, T.Cartesian(), pre_transform_train)
+test_dataset = FAUST(path, False, T.Cartesian(), pre_transform_test)
 train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=1)
 d = train_dataset[0]
