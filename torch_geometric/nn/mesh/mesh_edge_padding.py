79e5c415f0758eaadb6b5de3415cb49c56937b91
omril <olevi@corephotonics.com>
2021-09-08 11:45:01 +0300

    change mesh padding value

--- torch_geometric/nn/mesh/mesh_edge_padding.py
+++ torch_geometric/nn/mesh/mesh_edge_padding.py
@@ -6,7 +6,7 @@ def get_meshes_edge_index(meshes):
         [mesh.mesh_data.edge_index.shape[1] for mesh in meshes])
     edge_index = []
     for mesh in meshes:
-        padding = torch.zeros(2,
+        padding = -1*torch.ones(2,
                               num_edges_len - mesh.mesh_data.edge_index.shape[
                                   1])
         edge_index.append(
