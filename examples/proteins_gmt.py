dfd32d08a3d8191d6290e53458d4eda515d04fd6
rusty1s <matthias.fey@tu-dortmund.de>
2021-09-20 12:03:15 +0200

    clean up

--- examples/proteins_gmt.py
+++ examples/proteins_gmt.py
@@ -1,6 +1,7 @@
 import os.path as osp
 
 import torch
+from torch.nn import Linear
 import torch.nn.functional as F
 from torch_geometric.datasets import TUDataset
 from torch_geometric.loader import DataLoader
@@ -8,91 +9,89 @@ from torch_geometric.nn import GCNConv, GraphMultisetTransformer
 
 path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'PROTEINS')
 dataset = TUDataset(path, name='PROTEINS').shuffle()
-average_nodes = int(dataset.data.x.size(0) / len(dataset))
+avg_num_nodes = int(dataset.data.num_nodes / len(dataset))
 n = (len(dataset) + 9) // 10
 test_dataset = dataset[:n]
 val_dataset = dataset[n:2 * n]
 train_dataset = dataset[2 * n:]
 test_loader = DataLoader(test_dataset, batch_size=128)
 val_loader = DataLoader(val_dataset, batch_size=128)
-train_loader = DataLoader(train_dataset, batch_size=128)
+train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True)
 
 
 class Net(torch.nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
+        super().__init__()
 
         self.conv1 = GCNConv(dataset.num_features, 32)
         self.conv2 = GCNConv(32, 32)
         self.conv3 = GCNConv(32, 32)
 
         self.pool = GraphMultisetTransformer(
-            96, 64, 32,
-            conv=GCNConv, num_nodes=average_nodes, pooling_ratio=0.25,
+            in_channels=96,
+            hidden_channels=64,
+            out_channels=32,
+            Conv=GCNConv,
+            num_nodes=avg_num_nodes,
+            pooling_ratio=0.25,
             pool_sequences=['GMPool_G', 'SelfAtt', 'GMPool_I'],
-            num_heads=4, ln=False
+            num_heads=4,
+            layer_norm=False,
         )
 
-        self.lin1 = torch.nn.Linear(32, 16)
-        self.lin2 = torch.nn.Linear(16, dataset.num_classes)
+        self.lin1 = Linear(32, 16)
+        self.lin2 = Linear(16, dataset.num_classes)
 
-    def forward(self, data):
-        x, edge_index, batch = data.x, data.edge_index, data.batch
+    def forward(self, x0, edge_index, batch):
+        x1 = self.conv1(x0, edge_index).relu()
+        x2 = self.conv2(x1, edge_index).relu()
+        x3 = self.conv3(x2, edge_index).relu()
+        x = torch.cat([x1, x2, x3], dim=-1)
 
-        x1 = F.relu(self.conv1(x, edge_index))
-        x2 = F.relu(self.conv2(x1, edge_index))
-        x3 = F.relu(self.conv3(x2, edge_index))
-
-        x = torch.cat([x1, x2, x3], dim=1)
         x = self.pool(x, batch, edge_index)
 
-        x = F.relu(self.lin1(x))
+        x = self.lin1(x).relu()
         x = F.dropout(x, p=0.5, training=self.training)
-        x = F.log_softmax(self.lin2(x), dim=-1)
+        x = self.lin2(x)
 
-        return x
+        return x.log_softmax(dim=-1)
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = Net().to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-4)
 
-print(model)
 
-
-def train(epoch):
+def train():
     model.train()
 
-    loss_all = 0
+    total_loss = 0
     for data in train_loader:
         data = data.to(device)
         optimizer.zero_grad()
-        output = model(data)
-        loss = F.nll_loss(output, data.y)
+        out = model(data.x, data.edge_index, data.batch)
+        loss = F.nll_loss(out, data.y)
         loss.backward()
-        loss_all += data.num_graphs * loss.item()
+        total_loss += data.num_graphs * float(loss)
         optimizer.step()
-    return loss_all / len(train_dataset)
+    return total_loss / len(train_dataset)
 
 
 @torch.no_grad()
 def test(loader):
     model.eval()
-    correct = 0
 
+    total_correct = 0
     for data in loader:
         data = data.to(device)
-        pred = model(data).max(dim=1)[1]
-        correct += pred.eq(data.y).sum().item()
-    return correct / len(loader.dataset)
+        out = model(data.x, data.edge_index, data.batch)
+        total_correct += int((out.argmax(dim=-1) == data.y).sum())
+    return total_correct / len(loader.dataset)
 
 
-best_val_acc = test_acc = 0
 for epoch in range(1, 201):
-    train_loss = train(epoch)
+    train_loss = train()
     val_acc = test(val_loader)
-    if val_acc > best_val_acc:
-        test_acc = test(test_loader)
-        best_val_acc = val_acc
-    print(f'Epoch: {epoch:03d}, Train Loss: {train_loss:.4f}, '
+    test_acc = test(test_loader)
+    print(f'Epoch: {epoch:03d}, Loss: {train_loss:.4f}, '
           f'Val Acc: {val_acc:.4f}, Test Acc: {test_acc:.4f}')
--- torch_geometric/nn/glob/gmt.py
+++ torch_geometric/nn/glob/gmt.py
@@ -1,40 +1,60 @@
-from typing import List, Callable
-
-import torch
-from torch import Tensor
-from torch_geometric.nn import GCNConv
-from torch_geometric.utils import to_dense_batch
-from torch_geometric.typing import OptTensor
+from typing import List, Type, Optional, Tuple
 
 import math
 
+import torch
+from torch import Tensor
+from torch.nn import Linear, LayerNorm
+
+from torch_geometric.nn import GCNConv
+from torch_geometric.utils import to_dense_batch
+
 
 class MAB(torch.nn.Module):
-    def __init__(self, dim_Q, dim_K, dim_V, num_heads, conv=None, ln=False):
-        super(MAB, self).__init__()
+    def __init__(self, dim_Q: int, dim_K: int, dim_V: int, num_heads: int,
+                 Conv: Optional[Type] = None, layer_norm: bool = False):
+        super().__init__()
         self.dim_V = dim_V
         self.num_heads = num_heads
+        self.layer_norm = layer_norm
 
-        self.fc_q = torch.nn.Linear(dim_Q, dim_V)
+        self.fc_q = Linear(dim_Q, dim_V)
 
-        if conv is None:
-            self.layer_k = torch.nn.Linear(dim_K, dim_V)
-            self.layer_v = torch.nn.Linear(dim_K, dim_V)
+        if Conv is None:
+            self.layer_k = Linear(dim_K, dim_V)
+            self.layer_v = Linear(dim_K, dim_V)
         else:
-            self.layer_k = conv(dim_K, dim_V)
-            self.layer_v = conv(dim_K, dim_V)
+            self.layer_k = Conv(dim_K, dim_V)
+            self.layer_v = Conv(dim_K, dim_V)
 
-        if ln:
-            self.ln0 = torch.nn.LayerNorm(dim_V)
-            self.ln1 = torch.nn.LayerNorm(dim_V)
+        if layer_norm:
+            self.ln0 = LayerNorm(dim_V)
+            self.ln1 = LayerNorm(dim_V)
 
-        self.fc_o = torch.nn.Linear(dim_V, dim_V)
+        self.fc_o = Linear(dim_V, dim_V)
 
-    def forward(self, Q, K, graph=None, attention_mask=None):
+    def reset_parameters(self):
+        self.fc_q.reset_parameters()
+        self.layer_k.reset_parameters()
+        self.layer_v.reset_parameters()
+        if self.layer_norm:
+            self.ln0.reset_parameters()
+            self.ln1.reset_parameters()
+        self.fc_o.reset_parameters()
+        pass
+
+    def forward(
+        self,
+        Q: Tensor,
+        K: Tensor,
+        graph: Optional[Tuple[Tensor, Tensor, Tensor]] = None,
+        mask: Optional[Tensor] = None,
+    ) -> Tensor:
+
         Q = self.fc_q(Q)
 
         if graph is not None:
-            (x, edge_index, batch) = graph
+            x, edge_index, batch = graph
             K, V = self.layer_k(x, edge_index), self.layer_v(x, edge_index)
             K, _ = to_dense_batch(K, batch)
             V, _ = to_dense_batch(V, batch)
@@ -42,170 +62,186 @@ class MAB(torch.nn.Module):
             K, V = self.layer_k(K), self.layer_v(K)
 
         dim_split = self.dim_V // self.num_heads
-        Q_ = torch.cat(Q.split(dim_split, 2), 0)
-        K_ = torch.cat(K.split(dim_split, 2), 0)
-        V_ = torch.cat(V.split(dim_split, 2), 0)
+        Q_ = torch.cat(Q.split(dim_split, 2), dim=0)
+        K_ = torch.cat(K.split(dim_split, 2), dim=0)
+        V_ = torch.cat(V.split(dim_split, 2), dim=0)
 
-        if attention_mask is not None:
-            attention_mask = torch.cat(
-                [attention_mask for _ in range(self.num_heads)], 0
-            )
+        if mask is not None:
+            mask = torch.cat([mask for _ in range(self.num_heads)], 0)
             attention_score = Q_.bmm(K_.transpose(1, 2))
             attention_score = attention_score / math.sqrt(self.dim_V)
-            A = torch.softmax(attention_mask + attention_score, 1)
+            A = torch.softmax(mask + attention_score, 1)
         else:
             A = torch.softmax(
-                Q_.bmm(K_.transpose(1, 2)) / math.sqrt(self.dim_V), 1
-            )
+                Q_.bmm(K_.transpose(1, 2)) / math.sqrt(self.dim_V), 1)
 
         out = torch.cat((Q_ + A.bmm(V_)).split(Q.size(0), 0), 2)
-        out = out if getattr(self, 'ln0', None) is None else self.ln0(out)
-        out = out + torch.nn.functional.relu(self.fc_o(out))
-        out = out if getattr(self, 'ln1', None) is None else self.ln1(out)
+
+        if self.layer_norm:
+            out = self.ln0(out)
+
+        out = out + self.fc_o(out).relu()
+
+        if self.layer_norm:
+            out = self.ln1(out)
 
         return out
 
 
 class SAB(torch.nn.Module):
-    def __init__(self, dim_in, dim_out, num_heads,
-                 conv=None, ln=False):
-        super(SAB, self).__init__()
-        self.mab = MAB(dim_in, dim_in, dim_out, num_heads, conv=conv, ln=ln)
+    def __init__(self, in_channels: int, out_channels: int, num_heads: int,
+                 Conv: Optional[Type] = None, layer_norm: bool = False):
+        super().__init__()
+        self.mab = MAB(in_channels, in_channels, out_channels, num_heads,
+                       Conv=Conv, layer_norm=layer_norm)
 
-    def forward(self, X, graph=None, attention_mask=None):
-        return self.mab(X, X, graph, attention_mask)
+    def reset_parameters(self):
+        self.mab.reset_parameters()
+
+    def forward(
+        self,
+        x: Tensor,
+        graph: Optional[Tuple[Tensor, Tensor, Tensor]] = None,
+        mask: Optional[Tensor] = None,
+    ) -> Tensor:
+        return self.mab(x, x, graph, mask)
 
 
 class PMA(torch.nn.Module):
-    def __init__(self, dim, num_heads,
-                 num_seeds, conv=None, ln=False):
-        super(PMA, self).__init__()
-        self.S = torch.nn.Parameter(torch.Tensor(1, num_seeds, dim))
+    def __init__(self, channels: int, num_heads: int, num_seeds: int,
+                 Conv: Optional[Type] = None, layer_norm: bool = False):
+        super().__init__()
+        self.S = torch.nn.Parameter(torch.Tensor(1, num_seeds, channels))
+        self.mab = MAB(channels, channels, channels, num_heads, Conv=Conv,
+                       layer_norm=layer_norm)
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
         torch.nn.init.xavier_uniform_(self.S)
+        self.mab.reset_parameters()
 
-        self.mab = MAB(dim, dim, dim, num_heads, conv=conv, ln=ln)
-
-    def forward(self, X, graph=None, attention_mask=None):
-        return self.mab(
-            self.S.repeat(X.size(0), 1, 1), X, graph, attention_mask
-        )
+    def forward(
+        self,
+        x: Tensor,
+        graph: Optional[Tuple[Tensor, Tensor, Tensor]] = None,
+        mask: Optional[Tensor] = None,
+    ) -> Tensor:
+        return self.mab(self.S.repeat(x.size(0), 1, 1), x, graph, mask)
 
 
 class GraphMultisetTransformer(torch.nn.Module):
-    r"""The global pooling operator, Graph Multiset Transformer,
-    from the `"Accurate Learning of Graph Representations
+    r"""The global Graph Multiset Transformer pooling operator from the
+    `"Accurate Learning of Graph Representations
     with Graph Multiset Pooling" <https://arxiv.org/abs/2102.11533>`_ paper.
 
-    In short, nodes of the entire graph are clustered via attention-based
-    pooling operations (GMPool_G or GMPool_I), and self-attention (SelfAtt)
-    could be used to calculate the inter-relationships among nodes.
+    The Graph Multiset Transformer clusters nodes of the entire graph via
+    attention-based pooling operations (:obj:`"GMPool_G"` or
+    :obj:`"GMPool_I"`).
+    In addition, self-attention (:obj:`"SelfAtt"`) can be used to calculate
+    the inter-relationships among nodes.
 
     Args:
         in_channels (int): Size of each input sample.
         hidden_channels (int): Size of each hidden sample.
         out_channels (int): Size of each output sample.
-        conv (torch.nn.Module, optional): A graph neural network layer
-            for calculating hidden representations of nodes for GMPool_G
-            (one of :class:`torch_geometric.nn.conv.GraphConv`,
-            :class:`torch_geometric.nn.conv.GCNConv` or
-            :class:`torch_geometric.nn.conv.GATConv`). (default:
-            :class:`torch_geometric.nn.conv.GCNConv`)
+        conv (Type, optional): A graph neural network layer
+            for calculating hidden representations of nodes for
+            :obj:`"GMPool_G"` (one of
+            :class:`~torch_geometric.nn.conv.GCNConv`,
+            :class:`~torch_geometric.nn.conv.GraphConv` or
+            :class:`~torch_geometric.nn.conv.GATConv`).
+            (default: :class:`~torch_geometric.nn.conv.GCNConv`)
         num_nodes (int, optional): The number of average
             or maximum nodes. (default: :obj:`300`)
         pooling_ratio (float, optional): Graph pooling ratio
             for each pooling. (default: :obj:`0.25`)
         pool_sequences ([str], optional): A sequence of pooling layers
-            consisting of Graph Multiset Transformer (one of
-            :obj:`['GMPool_I']`,
-            :obj:`['GMPool_G']`,
-            :obj:`['GMPool_G', 'GMPool_I']`,
-            :obj:`['GMPool_G', 'SelfAtt', 'GMPool_I']` or
-            :obj:`['GMPool_G', 'SelfAtt', 'SelfAtt', 'GMPool_I']`).
-            (default: :obj:`['GMPool_G', 'SelfAtt', 'GMPool_I']`)
-        num_heads (int, optional): Number of heads in attention.
+            consisting of Graph Multiset Transformer submodules (one of
+            :obj:`["GMPool_I"]`,
+            :obj:`["GMPool_G"]`,
+            :obj:`["GMPool_G", "GMPool_I"]`,
+            :obj:`["GMPool_G", "SelfAtt", "GMPool_I"]` or
+            :obj:`["GMPool_G", "SelfAtt", "SelfAtt", "GMPool_I"]`).
+            (default: :obj:`["GMPool_G", "SelfAtt", "GMPool_I"]`)
+        num_heads (int, optional): Number of attention heads.
             (default: :obj:`4`)
-        ln (bool, optional): If set to :obj:`True`, will make use of
+        layer_norm (bool, optional): If set to :obj:`True`, will make use of
             layer normalization. (default: :obj:`False`)
     """
-    def __init__(self, in_channels: int, hidden_channels: int,
-                 out_channels: int, conv: Callable = None,
-                 num_nodes: int = 300, pooling_ratio: float = 0.25,
-                 pool_sequences: List[str] = [
-                     'GMPool_G', 'SelfAtt', 'GMPool_I'],
-                 num_heads: int = 4, ln: bool = False):
-        super(GraphMultisetTransformer, self).__init__()
+    def __init__(
+        self,
+        in_channels: int,
+        hidden_channels: int,
+        out_channels: int,
+        Conv: Optional[Type] = None,
+        num_nodes: int = 300,
+        pooling_ratio: float = 0.25,
+        pool_sequences: List[str] = ['GMPool_G', 'SelfAtt', 'GMPool_I'],
+        num_heads: int = 4,
+        layer_norm: bool = False,
+    ):
+        super().__init__()
         self.in_channels = in_channels
         self.hidden_channels = hidden_channels
         self.out_channels = out_channels
+        self.Conv = Conv or GCNConv
         self.num_nodes = num_nodes
         self.pooling_ratio = pooling_ratio
         self.pool_sequences = pool_sequences
         self.num_heads = num_heads
-        self.ln = ln
+        self.layer_norm = layer_norm
 
-        self.conv = GCNConv if conv is None else conv
+        self.lin1 = Linear(in_channels, hidden_channels)
+        self.lin2 = Linear(hidden_channels, out_channels)
 
         self.pools = torch.nn.ModuleList()
-        n_out_nodes = math.ceil(self.num_nodes * self.pooling_ratio)
-        self.pools.append(torch.nn.Linear(in_channels, hidden_channels))
-        for index, pool_type in enumerate(pool_sequences):
+        num_out_nodes = math.ceil(num_nodes * pooling_ratio)
+        for i, pool_type in enumerate(pool_sequences):
             if pool_type not in ['GMPool_G', 'GMPool_I', 'SelfAtt']:
-                raise ValueError(
-                    "The element in 'pools'",
-                    "should be one of 'GMPool_G', 'GMPool_I', 'SelfAtt'"
-                )
-            if (index == len(pool_sequences) - 1):
-                n_out_nodes = 1
+                raise ValueError("Elements in 'pool_sequences' should be one "
+                                 "of 'GMPool_G', 'GMPool_I', or 'SelfAtt'")
+
+            if i == len(pool_sequences) - 1:
+                num_out_nodes = 1
 
             if pool_type == 'GMPool_G':
                 self.pools.append(
-                    PMA(hidden_channels, num_heads,
-                        n_out_nodes, conv=self.conv, ln=ln)
-                )
-                n_out_nodes = math.ceil(n_out_nodes * self.pooling_ratio)
+                    PMA(hidden_channels, num_heads, num_out_nodes,
+                        Conv=self.Conv, layer_norm=layer_norm))
+                num_out_nodes = math.ceil(num_out_nodes * self.pooling_ratio)
+
             elif pool_type == 'GMPool_I':
                 self.pools.append(
-                    PMA(hidden_channels, num_heads,
-                        n_out_nodes, conv=None, ln=ln)
-                )
-                n_out_nodes = math.ceil(n_out_nodes * self.pooling_ratio)
+                    PMA(hidden_channels, num_heads, num_out_nodes, Conv=None,
+                        layer_norm=layer_norm))
+                num_out_nodes = math.ceil(num_out_nodes * self.pooling_ratio)
+
             elif pool_type == 'SelfAtt':
                 self.pools.append(
-                    SAB(hidden_channels, hidden_channels,
-                        num_heads, conv=None, ln=ln)
-                )
-        self.pools.append(torch.nn.Linear(hidden_channels, out_channels))
+                    SAB(hidden_channels, hidden_channels, num_heads, Conv=None,
+                        layer_norm=layer_norm))
+
+    def reset_parameters(self):
+        self.lin1.reset_parameters()
+        self.lin2.reset_parameters()
+        for pool in self.pools:
+            pool.reset_parameters()
 
     def forward(self, x: Tensor, batch: Tensor,
-                edge_index: OptTensor = None) -> Tensor:
+                edge_index: Optional[Tensor] = None) -> Tensor:
         """"""
-        x = self.pools[0](x)
+        x = self.lin1(x)
         batch_x, mask = to_dense_batch(x, batch)
+        mask = (~mask).unsqueeze(1).to(dtype=x.dtype) * -1e9
 
-        extended_attention_mask = mask.unsqueeze(1)
-        extended_attention_mask = extended_attention_mask.to(
-            dtype=next(self.parameters()).dtype)
-        extended_attention_mask = (1.0 - extended_attention_mask) * -1e9
+        for i, (name, pool) in enumerate(zip(self.pool_sequences, self.pools)):
+            graph = (x, edge_index, batch) if name == 'GMPool_G' else None
+            batch_x = pool(batch_x, graph, mask)
+            mask = None
 
-        for index, pool_type in enumerate(self.pool_sequences):
-            if pool_type == 'GMPool_G':
-                batch_x = self.pools[index + 1](
-                    batch_x,
-                    graph=(x, edge_index, batch),
-                    attention_mask=extended_attention_mask
-                )
-            else:
-                batch_x = self.pools[index + 1](
-                    batch_x,
-                    graph=None,
-                    attention_mask=extended_attention_mask
-                )
-            extended_attention_mask = None
-        batch_x = self.pools[-1](batch_x)
+        return self.lin2(batch_x.squeeze(1))
 
-        out = batch_x.squeeze(1)
-        return out
-
-    def __repr__(self):
-        return '{}(pool={})'.format(self.__class__.__name__,
-                                    self.pools)
+    def __repr__(self) -> str:
+        return (f'{self.__class__.__name__}({self.in_channels}, '
+                f'{self.out_channels}, pool_sequences={self.pool_sequences})')
