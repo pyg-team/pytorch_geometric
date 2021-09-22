e7b2da181109568fc39922ca64aecf7e80142e72
rusty1s <matthias.fey@tu-dortmund.de>
2021-09-22 15:39:05 +0000

    eval

--- examples/transe_fb15k_237.py
+++ examples/transe_fb15k_237.py
@@ -14,13 +14,26 @@ model = TransE(train_data.num_nodes, train_data.num_edge_types,
                hidden_channels=50, p_norm=1.0, sparse=True).to(device)
 
 loader = model.loader(train_data.edge_index, train_data.edge_type,
-                      batch_size=70000, shuffle=True, drop_last=True)
+                      batch_size=10000, shuffle=True, drop_last=True)
 optimizer = torch.optim.SparseAdam(model.parameters(), lr=0.01)
 
 for epoch in range(1, 1001):
+    total_loss = total_examples = 0
     for edge_index, edge_type, pos_mask in loader:
         optimizer.zero_grad()
         loss = model.loss(edge_index, edge_type, pos_mask)
         loss.backward()
         optimizer.step()
-        print(f'Epoch: {epoch:04d}, Loss: {loss:.4f}')
+        total_loss += float(loss) * edge_index.size(1)
+        total_examples += edge_index.size(1)
+    loss = total_loss / total_examples
+    print(f'Epoch: {epoch:04d}, Loss: {loss:.4f}')
+
+    if epoch % 100 == 0:
+        rank, hits = model.test(val_data.edge_index, val_data.edge_type,
+                                batch_size=20000, k=10)
+        print(f'Epoch: {epoch:04d}, Rank: {rank:.2f}, Hits: {hits:.4f}')
+
+rank, hits = model.test(test_data.edge_index, test_data.edge_type,
+                        batch_size=20000, k=10)
+print(f'Rank: {rank:.2f}, Hits: {hits:.4f}')
--- torch_geometric/nn/models/ke_model.py
+++ torch_geometric/nn/models/ke_model.py
@@ -1,6 +1,7 @@
 from typing import List, Tuple
 
 import math
+from tqdm import tqdm
 
 import torch
 from torch import Tensor
@@ -36,7 +37,62 @@ class KEModel(torch.nn.Module):
 
     def loader(self, edge_index: Tensor, edge_type: Tensor,
                add_negative_samples: bool = True, **kwargs):
-        raise NotImplementedError
+        return DataLoader(self.num_nodes, edge_index, edge_type,
+                          add_negative_samples, **kwargs)
+
+    @torch.no_grad()
+    def test(self, edge_index: Tensor, edge_type: Tensor, batch_size: int,
+             filtered: bool = False, k: int = 10) -> Tuple[float, float]:
+
+        if filtered:
+            raise NotImplementedError("'Filtered' inference not yet supported")
+
+        device = self.node_emb.weight.device
+
+        mean_ranks = []
+        hits_at_k = []
+
+        for j in tqdm(range(edge_index.size(1))):
+            (src, dst), rel = edge_index[:, j], edge_type[j]
+
+            src_edge_index = torch.stack([
+                torch.arange(self.num_nodes, device=src.device),
+                dst.expand(self.num_nodes),
+            ], dim=0)
+            src_edge_type = rel.expand(self.num_nodes).to(device)
+
+            src_scores = []
+            for i in range(0, self.num_nodes, batch_size):
+                score = self(src_edge_index[:, i:i + batch_size].to(device),
+                             src_edge_type[i:i + batch_size].to(device))
+                src_scores.append(score)
+            src_score = torch.cat(src_scores, dim=0)
+            rank = (src_score.argsort() == src).nonzero(as_tuple=False)
+            rank = int(rank.view(-1)[0])
+            mean_ranks.append(rank)
+            hits_at_k.append(rank < k)
+
+            dst_edge_index = torch.stack([
+                src.expand(self.num_nodes),
+                torch.arange(self.num_nodes, device=dst.device),
+            ], dim=0)
+            dst_edge_type = rel.expand(self.num_nodes).to(device)
+
+            dst_scores = []
+            for i in range(0, self.num_nodes, batch_size):
+                score = self(dst_edge_index[:, i:i + batch_size].to(device),
+                             dst_edge_type[i:i + batch_size].to(device))
+                dst_scores.append(score)
+            dst_score = torch.cat(dst_scores, dim=0)
+            rank = (dst_score.argsort() == dst).nonzero(as_tuple=False)
+            rank = int(rank.view(-1)[0])
+            mean_ranks.append(rank)
+            hits_at_k.append(rank < k)
+
+        mean_rank = float(torch.tensor(mean_ranks, dtype=torch.float).mean())
+        hits_at_k = int(torch.tensor(hits_at_k).sum()) / len(hits_at_k)
+
+        return mean_rank, hits_at_k
 
     def __repr__(self) -> str:
         return (f'{self.__class__.__name__}({self.num_nodes}, '
@@ -111,11 +167,6 @@ class TransE(KEModel):
         dst = F.normalize(dst, p=self.p_norm, dim=-1)
 
         return src.add_(rel).sub_(dst).norm(p=self.p_norm, dim=-1).view(-1)
-
-    def loader(self, edge_index: Tensor, edge_type: Tensor,
-               add_negative_samples: bool = True, **kwargs):
-        return DataLoader(self.num_nodes, edge_index, edge_type,
-                          add_negative_samples, **kwargs)
 
     def loss(self, edge_index: Tensor, edge_type: Tensor, pos_mask: Tensor,
              margin: float = 1.0) -> Tensor:
