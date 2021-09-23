635154b1de79332bcbd16c7ec135ae26ab98e91c
rusty1s <matthias.fey@tu-dortmund.de>
2021-09-23 12:16:09 +0000

    set persistent_workers=True in case num_workers>0

--- torch_geometric/loader/neighbor_loader.py
+++ torch_geometric/loader/neighbor_loader.py
@@ -131,6 +131,7 @@ class NeighborLoader(torch.utils.data.DataLoader):
     ):
         if kwargs.get('num_workers', 0) > 0:
             torch.multiprocessing.set_sharing_strategy('file_system')
+            kwargs['persistent_workers'] = True
 
         if 'collate_fn' in kwargs:
             del kwargs['collate_fn']
