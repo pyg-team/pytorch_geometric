7a04d4afc529f640833fedb4b07db49a1d25156e
wlhuang <huangwuliang19b@ict.ac.cn>
2021-09-23 17:36:01 +0800

    Add bipartite unit test cases.

--- torch_geometric/utils/negative_sampling.py
+++ torch_geometric/utils/negative_sampling.py
@@ -253,8 +253,8 @@ def vector_to_edge_index(idx: Tensor, size: Tuple[int, int], bipartite: bool,
         return torch.stack([row, col], dim=0)
 
 
-def is_neg_samp_feasible(edge_index, sample_method: str, num_nodes = None,
-                         bipartite: bool = False,
+def is_neg_samp_feasible(edge_index:Tensor, sample_method: str,
+                         num_nodes = None, bipartite: bool = False,
                          contains_neg_self_loops: bool = True,
                          force_undirected: bool = False) -> bool:
     r"""Check feasibility of negative sampling.
