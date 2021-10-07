bfcc5c5b5c4f0427cc6fa2b5ce450209a2e67c73
Julian Förster <fjulian@users.noreply.github.com>
2021-10-07 06:07:51 +0000

    graphgym: fix clash of run_dir naming (#3201)
    * Fix clash of run_dir naming. Save config and log for each repetition.
    
    
    Move config file one level up from run_dir
    
    * Update graphgym multi-seed fix
    
    Co-authored-by: JiaxuanYou <youjiaxuan@gmail.com>

--- graphgym/main.py
+++ graphgym/main.py
@@ -41,13 +41,14 @@ if __name__ == '__main__':
     # Set Pytorch environment
     torch.set_num_threads(cfg.num_threads)
     dump_cfg(cfg)
-    set_printing()
     # Repeat for different random seeds
     for i in range(args.repeat):
-        # Set configurations for each run
-        seed_everything(cfg.seed + i)
-        auto_select_device()
         set_run_dir(cfg.out_dir, args.cfg_file)
+        set_printing()
+        # Set configurations for each run
+        cfg.seed = cfg.seed + 1
+        seed_everything(cfg.seed)
+        auto_select_device()
         # Set machine learning pipeline
         loaders = create_loader()
         loggers = create_logger()
