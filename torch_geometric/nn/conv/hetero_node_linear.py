63b13b385e964e91a4ad2840fecf2f531085edf0
Xiaoyu006 <46880093+Xiaoyu006@users.noreply.github.com>
2021-10-17 14:49:34 +0800

    Update torch_geometric/nn/conv/hetero_node_linear.py
    Co-authored-by: Matthias Fey <matthias.fey@tu-dortmund.de>

--- torch_geometric/nn/conv/hetero_node_linear.py
+++ torch_geometric/nn/conv/hetero_node_linear.py
@@ -19,7 +19,7 @@ class HeteroLinear(torch.nn.Module):
     
     def forward(self, x: Tensor, node_type: Tensor) -> Tensor:
         emb_x = torch.zeros(x.shape[0], self.node_emb_size).to(x.device)
-        for node_type in range(self.num_node_types): # node_type is an integer, so that node_type_list should be a list of integers.
+        for idx in range(self.num_node_types):
             node_mask = node_type == idx
             emb_x[node_mask] = self.HeteroNodeEmbedLinears[node_type](x[node_mask])
         return emb_x
