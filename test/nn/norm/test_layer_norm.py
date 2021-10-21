475565ac9387228d898ef4851efbe846c12e1e14
rusty1s <matthias.fey@tu-dortmund.de>
2021-10-21 11:13:59 +0200

    fix layer norm test

--- test/nn/norm/test_layer_norm.py
+++ test/nn/norm/test_layer_norm.py
@@ -13,8 +13,8 @@ def test_layer_norm(affine):
     torch.jit.script(norm)
     out1 = norm(x)
     assert out1.size() == (100, 16)
-    assert torch.allclose(norm(x, batch), out1)
+    assert torch.allclose(norm(x, batch), out1, atol=1e-6)
 
     out2 = norm(torch.cat([x, x], dim=0), torch.cat([batch, batch + 1], dim=0))
-    assert torch.allclose(out1, out2[:100])
-    assert torch.allclose(out1, out2[100:])
+    assert torch.allclose(out1, out2[:100], atol=1e-6)
+    assert torch.allclose(out1, out2[100:], atol=1e-6)
