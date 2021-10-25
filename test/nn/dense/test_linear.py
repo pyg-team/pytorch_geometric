cb7d1ed9d448d7f8e3c2b5eb569242ed8ec1d2e7
rusty1s <matthias.fey@tu-dortmund.de>
2021-10-25 08:54:27 +0200

    fix docstring + jit test

--- test/nn/dense/test_linear.py
+++ test/nn/dense/test_linear.py
@@ -8,7 +8,6 @@ from torch_geometric.nn.dense.linear import HeteroLinear
 from torch.nn import Linear as PTLinear
 from torch.nn.parameter import UninitializedParameter
 
-
 weight_inits = ['glorot', 'kaiming_uniform', None]
 bias_inits = ['zeros', None]
 
@@ -74,8 +73,16 @@ def test_copy_linear(lazy):
     assert copied_lin.bias.data_ptr() != lin.bias.data_ptr()
     assert torch.allclose(copied_lin.bias, lin.bias, atol=1e-6)
 
+
 def test_hetero_linear():
     x = torch.randn((3, 16))
     node_type = torch.tensor([0, 1, 2])
-    het_lin = HeteroLinear(in_channels=16, out_channels=32, num_node_types=3)
-    assert het_lin(x, node_type).size() == (3, 32)
+
+    lin = HeteroLinear(in_channels=16, out_channels=32, num_node_types=3)
+    assert str(lin) == 'HeteroLinear(16, 32, bias=True)'
+
+    out = lin(x, node_type)
+    assert out.size() == (3, 32)
+
+    jit = torch.jit.script(lin)
+    assert torch.allclose(jit(x, node_type), out)
