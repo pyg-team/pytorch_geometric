369b862fb9039df20235a5450085660343cfe174
CaypoH <caypoh_wb@mail.ru>
2021-10-25 09:29:23 +0300

    added linear transformation of edge features so they match nodes feat… (#3372)
    * added linear transformation of edge features so they match nodes features dimention
    
    * edge_attr argument added to save backward compatibility
    
    * update
    
    Co-authored-by: r.nikiforov <r.nikiforov@okko.tv>
    Co-authored-by: rusty1s <matthias.fey@tu-dortmund.de>

--- torch_geometric/nn/conv/gin_conv.py
+++ torch_geometric/nn/conv/gin_conv.py
@@ -1,11 +1,11 @@
-from typing import Callable, Union
+from typing import Callable, Union, Optional
 from torch_geometric.typing import OptPairTensor, Adj, OptTensor, Size
 
 import torch
 from torch import Tensor
-import torch.nn.functional as F
 from torch_sparse import SparseTensor, matmul
 from torch_geometric.nn.conv import MessagePassing
+from torch_geometric.nn.dense.linear import Linear
 
 from ..inits import reset
 
@@ -103,11 +103,15 @@ class GINEConv(MessagePassing):
             (default: :obj:`0.`)
         train_eps (bool, optional): If set to :obj:`True`, :math:`\epsilon`
             will be a trainable parameter. (default: :obj:`False`)
+        edge_dim (int, optional): Edge feature dimensionality. If set to
+            :obj:`None`, node and edge feature dimensionality is expected to
+            match. Other-wise, edge features are linearly transformed to match
+            node feature dimensionality. (default: :obj:`None`)
         **kwargs (optional): Additional arguments of
             :class:`torch_geometric.nn.conv.MessagePassing`.
     """
     def __init__(self, nn: Callable, eps: float = 0., train_eps: bool = False,
-                 **kwargs):
+                 edge_dim: Optional[int] = None, **kwargs):
         kwargs.setdefault('aggr', 'add')
         super(GINEConv, self).__init__(**kwargs)
         self.nn = nn
@@ -116,24 +120,27 @@ class GINEConv(MessagePassing):
             self.eps = torch.nn.Parameter(torch.Tensor([eps]))
         else:
             self.register_buffer('eps', torch.Tensor([eps]))
+        if edge_dim is not None:
+            if hasattr(self.nn[0], 'in_features'):
+                in_channels = self.nn[0].in_features
+            else:
+                in_channels = self.nn[0].in_channels
+            self.lin = Linear(edge_dim, in_channels)
+        else:
+            self.lin = None
         self.reset_parameters()
 
     def reset_parameters(self):
         reset(self.nn)
         self.eps.data.fill_(self.initial_eps)
+        if self.lin is not None:
+            self.lin.reset_parameters()
 
     def forward(self, x: Union[Tensor, OptPairTensor], edge_index: Adj,
                 edge_attr: OptTensor = None, size: Size = None) -> Tensor:
         """"""
         if isinstance(x, Tensor):
             x: OptPairTensor = (x, x)
-
-        # Node and edge feature dimensionalites need to match.
-        if isinstance(edge_index, Tensor):
-            assert edge_attr is not None
-            assert x[0].size(-1) == edge_attr.size(-1)
-        elif isinstance(edge_index, SparseTensor):
-            assert x[0].size(-1) == edge_index.size(-1)
 
         # propagate_type: (x: OptPairTensor, edge_attr: OptTensor)
         out = self.propagate(edge_index, x=x, edge_attr=edge_attr, size=size)
@@ -145,7 +152,15 @@ class GINEConv(MessagePassing):
         return self.nn(out)
 
     def message(self, x_j: Tensor, edge_attr: Tensor) -> Tensor:
-        return F.relu(x_j + edge_attr)
+        if self.lin is None and x_j.size(-1) != edge_attr.size(-1):
+            raise ValueError("Node and edge feature dimensionalities do not "
+                             "match. Consider setting the 'edge_dim' "
+                             "attribute of 'GINEConv'")
+
+        if self.lin is not None:
+            edge_attr = self.lin(edge_attr)
+
+        return (x_j + edge_attr).relu()
 
     def __repr__(self):
         return '{}(nn={})'.format(self.__class__.__name__, self.nn)
