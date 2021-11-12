db18ef8289658aede2f7bb01222b9022d085f791
wsad1 <jinu.sunil@gmail.com>
2021-11-12 10:14:44 +0530

    updated test, removed a test

--- test/nn/conv/test_han_conv.py
+++ test/nn/conv/test_han_conv.py
@@ -4,25 +4,20 @@ from torch_geometric.nn import HANConv
 
 
 def test_han_conv_same_dimensions():
+
     x_dict = {
         'author': torch.randn(6, 16),
+        'paper': torch.randn(5, 12),
+        'term': torch.randn(4, 3)
     }
-    metapaths = [[('author', 'paper'), ('paper', 'author')],
-                 [('author', 'paper'), ('paper', 'conference'),
-                  ('conference', 'paper'), ('paper', 'author')],
-                 [('author', 'paper'), ('paper', 'term'),
-                  ('term', 'paper'), ('paper', 'author')]]
-    edge_index_dict = dict()
-    for path in metapaths:
-        index1 = torch.randint(0, 6, (20, ), dtype=torch.long)
-        index2 = torch.randint(0, 6, (20, ), dtype=torch.long)
-        src_type = path[0][0]
-        rel_type = ''.join([edge_type[0][0] for edge_type in path])\
-                   + path[-1][-1][0]
-        dst_type = path[-1][-1]
-        edge_index_dict[(src_type, rel_type, dst_type)] = torch.stack(
-            [index1, index2]
-        )
+    edge1 = torch.randint(0, 6, (2, 7), dtype=torch.long)
+    edge2 = torch.randint(0, 5, (2, 4), dtype=torch.long)
+    edge3 = torch.randint(0, 3, (2, 5), dtype=torch.long)
+    edge_index_dict = {
+        ('author', 'metapath1', 'author'): edge1,
+        ('paper', 'matapath2', 'paper'): edge2,
+        ('paper', 'matapath3', 'paper'): edge3,
+    }
 
     adj_t_dict = {}
     for edge_type, edge_index in edge_index_dict.items():
@@ -33,85 +28,35 @@ def test_han_conv_same_dimensions():
                           x_dict[dst_type].size(0))).t()
 
     metadata = (list(x_dict.keys()), list(edge_index_dict.keys()))
+    in_channels = {'author': 16, 'paper': 12, 'term': 3}
 
-    conv = HANConv(16, 16, metadata, heads=2)
+    conv = HANConv(in_channels, 16, metadata, heads=2)
     assert str(conv) == 'HANConv(16, heads=2)'
     out_dict1 = conv(x_dict, edge_index_dict)
-    assert len(out_dict1) == 1
+    assert len(out_dict1) == 3
     assert out_dict1['author'].size() == (6, 16)
-    out_dict2 = conv(x_dict, adj_t_dict)
-    for out1, out2 in zip(out_dict1.values(), out_dict2.values()):
-        assert torch.allclose(out1, out2, atol=1e-6)
+    assert out_dict1['paper'].size() == (5, 16)
+    assert out_dict1['term'] is None
+    del x_dict['term']
+    del out_dict1['term']
 
-
-def test_han_conv_different_dimensions():
-    x_dict = {
-        'author': torch.randn(4, 16),
-        'paper': torch.randn(6, 32),
-    }
-    metapaths = [[('author', 'paper'), ('paper', 'author')],
-                 [('author', 'paper'), ('paper', 'conference'),
-                  ('conference', 'paper'), ('paper', 'author')],
-                 [('paper', 'author'), ('author', 'paper')],
-                 [('paper', 'author'), ('author', 'conference'),
-                  ('conference', 'author'), ('author', 'paper')], ]
-    edge_index_dict = dict()
-    for path in metapaths:
-        src_type = path[0][0]
-        dst_type = path[-1][-1]
-        src_size = x_dict[src_type].size()[0]
-        dst_size = x_dict[dst_type].size()[0]
-        index1 = torch.randint(0, src_size, (20,), dtype=torch.long)
-        index2 = torch.randint(0, dst_size, (20,), dtype=torch.long)
-        src_type = path[0][0]
-        rel_type = ''.join([edge_type[0][0] for edge_type in path])\
-                   + path[-1][-1][0]
-        dst_type = path[-1][-1]
-        edge_index_dict[(src_type, rel_type, dst_type)] = torch.stack(
-            [index1, index2]
-        )
-
-    adj_t_dict = {}
-    for edge_type, edge_index in edge_index_dict.items():
-        src_type, _, dst_type = edge_type
-        adj_t_dict[edge_type] = SparseTensor(
-            row=edge_index[0], col=edge_index[1],
-            sparse_sizes=(x_dict[src_type].size(0),
-                          x_dict[dst_type].size(0))).t()
-
-    metadata = (list(x_dict.keys()), list(edge_index_dict.keys()))
-
-    conv = HANConv({'author': 16, 'paper': 32}, 16, metadata, heads=2)
-    assert str(conv) == 'HANConv(16, heads=2)'
-    out_dict1 = conv(x_dict, edge_index_dict)
-    assert len(out_dict1) == 2
-    assert out_dict1['author'].size() == (4, 16)
-    assert out_dict1['paper'].size() == (6, 16)
     out_dict2 = conv(x_dict, adj_t_dict)
     for out1, out2 in zip(out_dict1.values(), out_dict2.values()):
         assert torch.allclose(out1, out2, atol=1e-6)
 
 
 def test_hgt_conv_lazy():
+
     x_dict = {
         'author': torch.randn(6, 16),
+        'paper': torch.randn(5, 12),
     }
-    metapaths = [[('author', 'paper'), ('paper', 'author')],
-                 [('author', 'paper'), ('paper', 'conference'),
-                  ('conference', 'paper'), ('paper', 'author')],
-                 [('author', 'paper'), ('paper', 'term'),
-                  ('term', 'paper'), ('paper', 'author')]]
-    edge_index_dict = dict()
-    for path in metapaths:
-        index1 = torch.randint(0, 6, (20,), dtype=torch.long)
-        index2 = torch.randint(0, 6, (20,), dtype=torch.long)
-        src_type = path[0][0]
-        rel_type = ''.join([edge_type[0][0] for edge_type in path])\
-                   + path[-1][-1][0]
-        dst_type = path[-1][-1]
-        edge_index_dict[(src_type, rel_type, dst_type)] = torch.stack(
-            [index1, index2]
-        )
+    edge1 = torch.randint(0, 6, (2, 8), dtype=torch.long)
+    edge2 = torch.randint(0, 5, (2, 6), dtype=torch.long)
+    edge_index_dict = {
+        ('author', 'metapath0', 'author'): edge1,
+        ('paper', 'metapath1', 'paper'): edge2,
+    }
 
     adj_t_dict = {}
     for edge_type, edge_index in edge_index_dict.items():
@@ -125,8 +70,9 @@ def test_hgt_conv_lazy():
     conv = HANConv(-1, 16, metadata, heads=2)
     assert str(conv) == 'HANConv(16, heads=2)'
     out_dict1 = conv(x_dict, edge_index_dict)
-    assert len(out_dict1) == 1
+    assert len(out_dict1) == 2
     assert out_dict1['author'].size() == (6, 16)
+    assert out_dict1['paper'].size() == (5, 16)
     out_dict2 = conv(x_dict, adj_t_dict)
     for out1, out2 in zip(out_dict1.values(), out_dict2.values()):
         assert torch.allclose(out1, out2, atol=1e-6)
