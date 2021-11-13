1c9bac4ef16f39f7ee6a1b6d1bf66f914388ef26
Padarn Wilson <padarn.wilson@grabtaxi.com>
2021-11-13 12:33:45 +0000

    v1 of edge message passing

--- examples/edge_update.py
+++ examples/edge_update.py
@@ -1,17 +1,32 @@
+import os.path as osp
+
 from torch_geometric.nn import MessagePassing
 from torch_geometric.data import Data
 
+from torch_geometric.datasets import Planetoid
+import torch_geometric.transforms as T
+
+dataset = 'Cora'
+transform = T.Compose([
+    T.RandomNodeSplit(num_val=500, num_test=500),
+    T.TargetIndegree(),
+])
+path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', dataset)
+dataset = Planetoid(path, dataset, transform=transform)
+data = dataset[0]
 
 class EdgeMessagePassingExample(MessagePassing):
 
     def forward(self, data: Data):
-        x = self.propagate(data.edge_index, data.x)
-        edge_attr = self.update_edges(data.edge_index, data.x, data.edge_attr)
+        x = self.propagate(data.edge_index, edge_attr=data.edge_attr, x=data.x)
+        edge_attr = self.edges_propagate(data.edge_index, edge_attr=data.edge_attr, x=data.x)
+        return x, edge_attr
 
+    def edge_message_and_update(self, edge_attr, x_i, x_j):
+        return edge_attr
 
-    def edge_update(self, x_i, x_j, edge_features):
-        return edge_features
-
-
-    def message(self, x_i):
+    def message(self, edge_attr, x_i, x_j):
         return x_i
+
+model = EdgeMessagePassingExample()
+print(model.forward(data))
