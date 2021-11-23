9c9295ac6a6aea83ec94c57c8195f144f836d597
QuanticDisaster <58271711+QuanticDisaster@users.noreply.github.com>
2021-11-23 11:20:53 +0100

    Point Transformer examples (#3344)
    * first commit
    
    * format cleanup
    
    * part segmentation
    
    * fix forgotten transformer + format
    
    * update graphgym model import
    
    Co-authored-by: Matthias Fey <matthias.fey@tu-dortmund.de>
    
    * move ReLu
    
    * fix scatter_max error + format
    
    * fix hardcoded dimensions
    
    Co-authored-by: Jinu Sunil <jinu.sunil@gmail.com>
    
    * reorder block visually
    
    * fix blocks visually
    
    * auto add dummy feature if none is given
    
    * use classes instead of functions
    
    * format
    
    * Apply suggestions from code review
    
    Co-authored-by: Matthias Fey <matthias.fey@tu-dortmund.de>
    
    * push reviews
    
    * push reviews
    
    * mlp
    
    * format
    
    * format
    
    * explicit arguments
    
    * code reviews
    
    Co-authored-by: Matthias Fey <matthias.fey@tu-dortmund.de>
    Co-authored-by: Jinu Sunil <jinu.sunil@gmail.com>

---
+++ examples/point_transformer_segmentation.py
@@ -0,0 +1,235 @@
+from point_transformer_classification import TransitionDown, TransformerBlock
+from point_transformer_classification import MLP
+
+import os.path as osp
+
+import torch
+import torch.nn.functional as F
+from torch.nn import Sequential as Seq, Linear as Lin, ReLU
+
+import torch_geometric.transforms as T
+from torch_geometric.datasets import ShapeNet
+from torch_geometric.loader import DataLoader
+from torch_geometric.utils import intersection_and_union as i_and_u
+from torch_geometric.nn.unpool import knn_interpolate
+
+from torch_cluster import knn_graph
+
+category = 'Airplane'  # Pass in `None` to train on all categories.
+path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'ShapeNet')
+transform = T.Compose([
+    T.RandomTranslate(0.01),
+    T.RandomRotate(15, axis=0),
+    T.RandomRotate(15, axis=1),
+    T.RandomRotate(15, axis=2),
+])
+pre_transform = T.NormalizeScale()
+train_dataset = ShapeNet(path, category, split='trainval', transform=transform,
+                         pre_transform=pre_transform)
+test_dataset = ShapeNet(path, category, split='test',
+                        pre_transform=pre_transform)
+train_loader = DataLoader(train_dataset, batch_size=10, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=10, shuffle=False)
+
+
+class TransitionUp(torch.nn.Module):
+    '''
+        Reduce features dimensionnality and interpolate back to higher
+        resolution and cardinality
+    '''
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.mlp_sub = MLP([in_channels, out_channels])
+        self.mlp = MLP([out_channels, out_channels])
+
+    def forward(self, x, x_sub, pos, pos_sub, batch=None, batch_sub=None):
+        # transform low-res features and reduce the number of features
+        x_sub = self.mlp_sub(x_sub)
+
+        # interpolate low-res feats to high-res points
+        x_interpolated = knn_interpolate(
+            x_sub, pos_sub, pos, k=3, batch_x=batch_sub, batch_y=batch)
+
+        x = self.mlp(x) + x_interpolated
+
+        return x
+
+
+class Net(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, dim_model, k=16):
+        super().__init__()
+        self.k = k
+
+        # dummy feature is created if there is none given
+        in_channels = max(in_channels, 1)
+
+        # first block
+        self.mlp_input = MLP([in_channels, dim_model[0]])
+
+        self.transformer_input = TransformerBlock(in_channels=dim_model[0],
+                                                  out_channels=dim_model[0],
+                                                  )
+
+        # backbone layers
+        self.transformers_up = torch.nn.ModuleList()
+        self.transformers_down = torch.nn.ModuleList()
+        self.transition_up = torch.nn.ModuleList()
+        self.transition_down = torch.nn.ModuleList()
+
+        for i in range(0, len(dim_model) - 1):
+
+            # Add Transition Down block followed by a Point Transformer block
+            self.transition_down.append(
+                TransitionDown(in_channels=dim_model[i],
+                               out_channels=dim_model[i + 1],
+                               k=self.k)
+            )
+
+            self.transformers_down.append(
+                TransformerBlock(in_channels=dim_model[i + 1],
+                                 out_channels=dim_model[i + 1])
+            )
+
+            # Add Transition Up block followed by Point Transformer block
+            self.transition_up.append(
+                TransitionUp(in_channels=dim_model[i + 1],
+                             out_channels=dim_model[i])
+            )
+
+            self.transformers_up.append(
+                TransformerBlock(in_channels=dim_model[i],
+                                 out_channels=dim_model[i])
+            )
+
+        # summit layers
+        self.mlp_summit = MLP([dim_model[-1], dim_model[-1]], batch_norm=False)
+
+        self.transformer_summit = TransformerBlock(
+            in_channels=dim_model[-1],
+            out_channels=dim_model[-1],
+        )
+
+        # class score computation
+        self.mlp_output = Seq(
+            Lin(dim_model[0], 64),
+            ReLU(),
+            Lin(64, 64),
+            ReLU(),
+            Lin(64, out_channels)
+        )
+
+    def forward(self, x, pos, batch=None):
+
+        # add dummy features in case there is none
+        if x is None:
+            x = torch.ones((pos.shape[0], 1)).to(pos.get_device())
+
+        out_x = []
+        out_pos = []
+        out_batch = []
+
+        # first block
+        x = self.mlp_input(x)
+        edge_index = knn_graph(pos, k=self.k, batch=batch)
+        x = self.transformer_input(x, pos, edge_index)
+
+        # save outputs for skipping connections
+        out_x.append(x)
+        out_pos.append(pos)
+        out_batch.append(batch)
+
+        # backbone down : #reduce cardinality and augment dimensionnality
+        for i in range(len(self.transformers_down)):
+            x, pos, batch = self.transition_down[i](
+                x, pos, batch=batch)
+            edge_index = knn_graph(pos, k=self.k, batch=batch)
+            x = self.transformers_down[i](x, pos, edge_index)
+
+            out_x.append(x)
+            out_pos.append(pos)
+            out_batch.append(batch)
+
+        # summit
+        x = self.mlp_summit(x)
+        edge_index = knn_graph(pos, k=self.k, batch=batch)
+        x = self.transformer_summit(x, pos, edge_index)
+
+        # backbone up : augment cardinality and reduce dimensionnality
+        n = len(self.transformers_down)
+        for i in range(n):
+            x = self.transition_up[- i - 1](
+                x=out_x[- i - 2],
+                x_sub=x,
+                pos=out_pos[- i - 2],
+                pos_sub=out_pos[- i - 1],
+                batch_sub=out_batch[- i - 1],
+                batch=out_batch[- i - 2]
+            )
+
+            edge_index = knn_graph(out_pos[- i - 2], k=self.k,
+                                   batch=out_batch[- i - 2])
+            x = self.transformers_up[- i - 1](x, out_pos[- i - 2], edge_index)
+
+        # Class score
+        out = self.mlp_output(x)
+
+        return F.log_softmax(out, dim=-1)
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = Net(3, train_dataset.num_classes, dim_model=[
+    32, 64, 128, 256, 512], k=16).to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
+
+
+def train():
+    model.train()
+
+    total_loss = correct_nodes = total_nodes = 0
+    for i, data in enumerate(train_loader):
+        data = data.to(device)
+        optimizer.zero_grad()
+        out = model(data.x, data.pos, data.batch)
+        loss = F.nll_loss(out, data.y)
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+        correct_nodes += out.argmax(dim=1).eq(data.y).sum().item()
+        total_nodes += data.num_nodes
+
+        if (i + 1) % 10 == 0:
+            print(f'[{i+1}/{len(train_loader)}] Loss: {total_loss / 10:.4f} '
+                  f'Train Acc: {correct_nodes / total_nodes:.4f}')
+            total_loss = correct_nodes = total_nodes = 0
+
+
+def test(loader):
+    model.eval()
+
+    y_mask = loader.dataset.y_mask
+    ious = [[] for _ in range(len(loader.dataset.categories))]
+
+    for data in loader:
+        data = data.to(device)
+        pred = model(data.x, data.pos, data.batch).argmax(dim=1)
+
+        i, u = i_and_u(pred, data.y, loader.dataset.num_classes, data.batch)
+        iou = i.cpu().to(torch.float) / u.cpu().to(torch.float)
+        iou[torch.isnan(iou)] = 1
+
+        # Find and filter the relevant classes for each category.
+        for iou, category in zip(iou.unbind(), data.category.unbind()):
+            ious[category.item()].append(iou[y_mask[category]])
+
+    # Compute mean IoU.
+    ious = [torch.stack(iou).mean(0).mean(0) for iou in ious]
+    return torch.tensor(ious).mean().item()
+
+
+for epoch in range(1, 100):
+    train()
+    iou = test(test_loader)
+    print('Epoch: {:02d}, Test IoU: {:.4f}'.format(epoch, iou))
+    scheduler.step()
