383eec8975124fec518df189ab2940eb757d4e1f
rusty1s <matthias.fey@tu-dortmund.de>
2021-11-26 12:27:45 +0000

    update

--- torch_geometric/nn/reshape.py
+++ torch_geometric/nn/reshape.py
@@ -1,4 +1,5 @@
 import torch
+from torch import Tensor
 
 
 class Reshape(torch.nn.Module):
@@ -6,11 +7,11 @@ class Reshape(torch.nn.Module):
         super(Reshape, self).__init__()
         self.shape = shape
 
-    def forward(self, x):
+    def forward(self, x: Tensor) -> Tensor:
         """"""
         x = x.view(*self.shape)
         return x
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         shape = ', '.join([str(dim) for dim in self.shape])
         return f'{self.__class__.__name__}({shape})'
