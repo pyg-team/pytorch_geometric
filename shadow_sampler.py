9ffa595dbd2d1d19365eb9ebc5d10ca2f9beb3d7
SDC <duttacho@usc.edu>
2021-11-27 18:24:42 -0800

    Update shadow_sampler.py

--- shadow_sampler.py
+++ shadow_sampler.py
@@ -8,21 +8,16 @@ from torch_geometric.loader import GraphSAINTRandomWalkSampler
 from torch_geometric.loader import ShaDowKHopSampler
 from torch_geometric.nn import GraphConv
 from torch_geometric.utils import degree
+from torch_geometric.nn import global_mean_pool
 
 path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'Flickr')
 dataset = Flickr(path)
 data = dataset[0]
 row, col = data.edge_index
-data.edge_weight = 1. / degree(col, data.num_nodes)[col]  # Norm by in-degree.
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--use_normalization', action='store_true')
-args = parser.parse_args()
-
-
-indices = data.train_mask.nonzero()
-index = torch.flatten(indices)
-loader = ShaDowKHopSampler(data, depth=2, num_neighbors=5, node_idx=index)
+loader = ShaDowKHopSampler(data, depth=2, num_neighbors=5, node_idx=data.train_mask)
+test_loader = ShaDowKHopSampler(data, depth=2, num_neighbors=5, node_idx = data.test_mask)
+val_loader = ShaDowKHopSampler(data, depth=2, num_neighbors=5, node_idx = data.val_mask)
 
 
 class Net(torch.nn.Module):
@@ -30,24 +25,20 @@ class Net(torch.nn.Module):
         super(Net, self).__init__()
         in_channels = dataset.num_node_features
         out_channels = dataset.num_classes
-        self.conv1 = GraphConv(in_channels, hidden_channels)
-        self.conv2 = GraphConv(hidden_channels, hidden_channels)
-        self.conv3 = GraphConv(hidden_channels, hidden_channels)
-        self.lin = torch.nn.Linear(3 * hidden_channels, out_channels)
-
-    def set_aggr(self, aggr):
-        self.conv1.aggr = aggr
-        self.conv2.aggr = aggr
-        self.conv3.aggr = aggr
-
-    def forward(self, x0, edge_index, edge_weight=None):
-        x1 = F.relu(self.conv1(x0, edge_index, edge_weight))
-        x1 = F.dropout(x1, p=0.2, training=self.training)
-        x2 = F.relu(self.conv2(x1, edge_index, edge_weight))
-        x2 = F.dropout(x2, p=0.2, training=self.training)
-        x3 = F.relu(self.conv3(x2, edge_index, edge_weight))
-        x3 = F.dropout(x3, p=0.2, training=self.training)
-        x = torch.cat([x1, x2, x3], dim=-1)
+        self.conv1 = SAGEConv(in_channels, hidden_channels)
+        self.conv2 = SAGEConv(hidden_channels, hidden_channels)
+        self.conv3 = SAGEConv(hidden_channels, hidden_channels)
+        self.lin = torch.nn.Linear(hidden_channels, out_channels)
+    def forward(self, data):
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+        x = F.relu(self.conv1(x, edge_index))
+        x = F.dropout(x, p=0.2)
+        x = F.relu(self.conv2(x, edge_index))
+        x = F.dropout(x, p=0.2, training=self.training)
+        x = F.relu(self.conv3(x, edge_index))
+        x = F.dropout(x, p=0.2, training=self.training)
+        
+        x = global_mean_pool(x,batch)
         x = self.lin(x)
         return x.log_softmax(dim=-1)
 
@@ -56,24 +47,17 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = Net(hidden_channels=256).to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-
 def train():
     model.train()
-    model.set_aggr('add' if args.use_normalization else 'mean')
 
     total_loss = total_examples = 0
     for data in loader:
         data = data.to(device)
         optimizer.zero_grad()
+        
+        out = model(data)
+        loss = F.nll_loss(out, data.y)
 
-        if args.use_normalization:
-            edge_weight = data.edge_norm * data.edge_weight
-            out = model(data.x, data.edge_index, edge_weight)
-            loss = F.nll_loss(out[data.root_n_id], data.y, reduction='none')
-            loss = (loss * data.node_norm)[data.root_n_id].sum()
-        else:
-            out = model(data.x, data.edge_index)
-            loss = F.nll_loss(out[data.root_n_id], data.y)
 
         loss.backward()
         optimizer.step()
@@ -85,20 +69,32 @@ def train():
 @torch.no_grad()
 def test():
     model.eval()
-    model.set_aggr('mean')
-
-    out = model(data.x.to(device), data.edge_index.to(device))
-    pred = out.argmax(dim=-1)
-    correct = pred.eq(data.y.to(device))
-
-    accs = []
-    for _, mask in data('train_mask', 'val_mask', 'test_mask'):
-        accs.append(correct[mask].sum().item() / mask.sum().item())
-    return accs
+    test_correct = 0
+    test_total = 0
+    for data in test_loader:
+        data = data.to(device)
+        out = model(data)
+        pred = out.argmax(dim=-1)
+        test_correct += pred.eq(data.y.to(device)).item()
+        test_total += 1
+    accs_test = test_correct / test_total
+    
+    val_correct = 0
+    val_total = 0
+    for data in val_loader:
+        data = data.to(device)
+        out = model(data)
+        pred = out.argmax(dim=-1)
+        val_correct += pred.eq(data.y.to(device)).item()
+        val_total += 1
+    accs_val = val_correct / val_total
+    
+    
+    
+    return accs_test, accs_val
 
 
 for epoch in range(1, 10):
     loss = train()
-    accs = test()
-    print(f'Epoch: {epoch:02d}, Loss: {loss:.4f}, Train: {accs[0]:.4f}, '
-          f'Val: {accs[1]:.4f}, Test: {accs[2]:.4f}')
+    test_acc, val_acc = test()
+    print(f'Epoch: {epoch:02d}, Loss: {loss:.4f}, Val: {val_acc:.4f} Test: {test_acc:.4f}')
