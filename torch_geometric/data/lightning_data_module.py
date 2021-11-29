d92a2d261eb6d4737d757862eed0a48f7442013a
rusty1s <matthias.fey@tu-dortmund.de>
2021-11-29 14:17:46 +0000

    update

--- torch_geometric/data/lightning_data_module.py
+++ torch_geometric/data/lightning_data_module.py
@@ -3,51 +3,32 @@ from typing import Optional
 from torch_geometric.data import Dataset
 from torch_geometric.loader import DataLoader
 
-import pytorch_lightning as pl
 
+def LightningDataset(self, train_dataset: Dataset, val_dataset: Dataset,
+                     test_dataset: Optional[Dataset] = None,
+                     batch_size: int = 1, num_workers: int = 0, **kwargs):
 
-class LightningDataset(pl.LightningDataModule):
-    def __init__(self, train_dataset: Dataset, val_dataset: Dataset,
-                 test_dataset: Optional[Dataset] = None, **kwargs):
-        super().__init__()
-        self.train_dataset = train_dataset
-        self.val_dataset = val_dataset
-        self.test_dataset = test_dataset
-        self.kwargs = kwargs
-
-    def prepare_data(self):
-        print(self.trainer)
-        print("PREPARE DATA")
+    from pytorch_lightning import LightningDataModule
+    from pytorch_lightning.plugins import DDPSpawnPlugin
 
     def setup(self, stage: Optional[str] = None):
-        if not isinstance(self.trainer.training_type_plugin,
-                          pl.plugins.DDPSpawnPlugin):
+        if not isinstance(self.trainer.training_type_plugin, DDPSpawnPlugin):
             raise NotImplementedError
 
-    def train_dataloader(self):
-        print("TRAIN DATALOADER")
-        x = self.train_dataset.data.x[0]
-        x.add_(1)
-        print(x)
-        print(x.is_shared())
-        # TODO: shuffle=True
-        # TODO: pin_memory=True?
-        loader = DataLoader(self.train_dataset, **self.kwargs)
-        print(loader.batch_size)
-        return loader
+    def dataloader(dataset: Dataset, shuffle: bool, **kwargs):
+        # shuffle &= not isinstance(dataset, IterableDataset)
+        return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle,
+                          pin_memory=True, num_workers=num_workers,
+                          persistent_workers=num_workers > 0, **kwargs)
 
-    def val_dataloader(self):
-        print("VAL DATALOADER")
-        # TODO: pin_memory=True?
-        loader = DataLoader(self.val_dataset, **self.kwargs)
-        print(loader.batch_size)
-        return loader
+    data_module = LightningDataModule()
+    data_module.train_dataset = train_dataset
+    data_module.val_dataset = val_dataset
+    data_module.test_dataset = test_dataset
 
-    def test_dataloader(self):
-        print("TEST DATALOADER")
-        # TODO: pin_memory=True?
-        loader = DataLoader(self.test_dataset, **self.kwargs)
-        print(loader.batch_size)
-        return loader
+    data_module.train_dataloader = dataloader
+    data_module.val_dataloader = dataloader
+    if test_dataset is not None:
+        data_module.train_dataloader = dataloader
 
-    # TODO: replicate dataset functionality
+    return data_module
