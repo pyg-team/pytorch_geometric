298ee65ca67e623bfb99b069032d3db62ac632c0
QuanticDisaster <QuanticDisaster@gmail.com>
2021-12-03 13:25:40 +0100

    replace code to ease future commit comparisons

--- examples/point_transformer_s3dis.py
+++ examples/point_transformer_s3dis.py
@@ -1,20 +1,225 @@
-from point_transformer_classification import TransitionDown, TransformerBlock
-from point_transformer_classification import MLP
-from point_transformer_segmentation import TransitionUp, Net
-
 import os.path as osp
 
 import torch
 import torch.nn.functional as F
 from torch.nn import Sequential as Seq, Linear as Lin, BatchNorm1d as BN, ReLU
+from torch.nn import Identity
 
 import torch_geometric.transforms as T
 from torch_geometric.datasets import S3DIS
 from torch_geometric.loader import DataLoader
-from torch_geometric.utils import intersection_and_union as i_and_u, mean_iou
+from torch_geometric.utils import intersection_and_union as i_and_u
 from torch_geometric.nn.unpool import knn_interpolate
+from torch_geometric.nn.pool import knn
+from torch_geometric.nn.conv import PointTransformerConv
+
 
 from torch_cluster import knn_graph
+from torch_cluster import fps
+from torch_scatter import scatter_max
+
+
+class TransformerBlock(torch.nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.lin_in = Lin(in_channels, in_channels)
+        self.lin_out = Lin(out_channels, out_channels)
+
+        self.pos_nn = MLP([3, 64, out_channels], batch_norm=False)
+
+        self.attn_nn = MLP([out_channels, 64, out_channels], batch_norm=False)
+
+        self.transformer = PointTransformerConv(
+            in_channels,
+            out_channels,
+            pos_nn=self.pos_nn,
+            attn_nn=self.attn_nn
+        )
+
+    def forward(self, x, pos, edge_index):
+        x = self.lin_in(x).relu()
+        x = self.transformer(x, pos, edge_index)
+        x = self.lin_out(x).relu()
+        return x
+
+
+class TransitionDown(torch.nn.Module):
+    '''
+        Samples the input point cloud by a ratio percentage to reduce
+        cardinality and uses an mlp to augment features dimensionnality
+    '''
+
+    def __init__(self, in_channels, out_channels, ratio=0.25, k=16):
+        super().__init__()
+        self.k = k
+        self.ratio = ratio
+        self.mlp = MLP([in_channels, out_channels])
+
+    def forward(self, x, pos, batch):
+        # FPS sampling
+        id_clusters = fps(pos, ratio=self.ratio, batch=batch)
+
+        # compute for each cluster the k nearest points
+        sub_batch = batch[id_clusters] if batch is not None else None
+
+        # beware of self loop
+        id_k_neighbor = knn(pos, pos[id_clusters], k=self.k,
+                            batch_x=batch, batch_y=sub_batch)
+
+        # transformation of features through a simple MLP
+        x = self.mlp(x)
+
+        # Max pool onto each cluster the features from knn in points
+        x_out, _ = scatter_max(x[id_k_neighbor[1]],
+                               id_k_neighbor[0],
+                               dim_size=id_clusters.size(0),
+                               dim=0)
+
+        # keep only the clusters and their max-pooled features
+        sub_pos, out = pos[id_clusters], x_out
+        return out, sub_pos, sub_batch
+
+
+def MLP(channels, batch_norm=True):
+    return Seq(*[
+        Seq(
+            Lin(channels[i - 1], channels[i]),
+            BN(channels[i]) if batch_norm else Identity(),
+            ReLU()
+        )
+        for i in range(1, len(channels))
+    ])
+
+class TransitionUp(torch.nn.Module):
+    '''
+        Reduce features dimensionnality and interpolate back to higher
+        resolution and cardinality
+    '''
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.mlp_sub = MLP([in_channels, out_channels])
+        self.mlp = MLP([out_channels, out_channels])
+
+    def forward(self, x, x_sub, pos, pos_sub, batch=None, batch_sub=None):
+        # transform low-res features and reduce the number of features
+        x_sub = self.mlp_sub(x_sub)
+
+        # interpolate low-res feats to high-res points
+        x_interpolated = knn_interpolate(x_sub, pos_sub, pos, k=3,
+                                         batch_x=batch_sub, batch_y=batch)
+
+        x = self.mlp(x) + x_interpolated
+
+        return x
+
+
+class Net(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, dim_model, k=16):
+        super().__init__()
+        self.k = k
+
+        # dummy feature is created if there is none given
+        in_channels = max(in_channels, 1)
+
+        # first block
+        self.mlp_input = MLP([in_channels, dim_model[0]])
+
+        self.transformer_input = TransformerBlock(
+            in_channels=dim_model[0],
+            out_channels=dim_model[0],
+        )
+
+        # backbone layers
+        self.transformers_up = torch.nn.ModuleList()
+        self.transformers_down = torch.nn.ModuleList()
+        self.transition_up = torch.nn.ModuleList()
+        self.transition_down = torch.nn.ModuleList()
+
+        for i in range(0, len(dim_model) - 1):
+
+            # Add Transition Down block followed by a Point Transformer block
+            self.transition_down.append(
+                TransitionDown(in_channels=dim_model[i],
+                               out_channels=dim_model[i + 1], k=self.k))
+
+            self.transformers_down.append(
+                TransformerBlock(in_channels=dim_model[i + 1],
+                                 out_channels=dim_model[i + 1]))
+
+            # Add Transition Up block followed by Point Transformer block
+            self.transition_up.append(
+                TransitionUp(in_channels=dim_model[i + 1],
+                             out_channels=dim_model[i]))
+
+            self.transformers_up.append(
+                TransformerBlock(in_channels=dim_model[i],
+                                 out_channels=dim_model[i]))
+
+        # summit layers
+        self.mlp_summit = MLP([dim_model[-1], dim_model[-1]], batch_norm=False)
+
+        self.transformer_summit = TransformerBlock(
+            in_channels=dim_model[-1],
+            out_channels=dim_model[-1],
+        )
+
+        # class score computation
+        self.mlp_output = Seq(Lin(dim_model[0], 64), ReLU(), Lin(64, 64),
+                              ReLU(), Lin(64, out_channels))
+
+    def forward(self, x, pos, batch=None):
+
+        # add dummy features in case there is none
+        if x is None:
+            x = torch.ones((pos.shape[0], 1)).to(pos.get_device())
+
+        out_x = []
+        out_pos = []
+        out_batch = []
+
+        # first block
+        x = self.mlp_input(x)
+        edge_index = knn_graph(pos, k=self.k, batch=batch)
+        x = self.transformer_input(x, pos, edge_index)
+
+        # save outputs for skipping connections
+        out_x.append(x)
+        out_pos.append(pos)
+        out_batch.append(batch)
+
+        # backbone down : #reduce cardinality and augment dimensionnality
+        for i in range(len(self.transformers_down)):
+            x, pos, batch = self.transition_down[i](x, pos, batch=batch)
+            edge_index = knn_graph(pos, k=self.k, batch=batch)
+            x = self.transformers_down[i](x, pos, edge_index)
+
+            out_x.append(x)
+            out_pos.append(pos)
+            out_batch.append(batch)
+
+        # summit
+        x = self.mlp_summit(x)
+        edge_index = knn_graph(pos, k=self.k, batch=batch)
+        x = self.transformer_summit(x, pos, edge_index)
+
+        # backbone up : augment cardinality and reduce dimensionnality
+        n = len(self.transformers_down)
+        for i in range(n):
+            x = self.transition_up[-i - 1](x=out_x[-i - 2], x_sub=x,
+                                           pos=out_pos[-i - 2],
+                                           pos_sub=out_pos[-i - 1],
+                                           batch_sub=out_batch[-i - 1],
+                                           batch=out_batch[-i - 2])
+
+            edge_index = knn_graph(out_pos[-i - 2], k=self.k,
+                                   batch=out_batch[-i - 2])
+            x = self.transformers_up[-i - 1](x, out_pos[-i - 2], edge_index)
+
+        # Class score
+        out = self.mlp_output(x)
+
+        return F.log_softmax(out, dim=-1)
+
 
 def train():
     model.train()
