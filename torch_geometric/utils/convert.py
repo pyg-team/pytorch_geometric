560df3322e9156ddf0a501f23cb0b34b70ffdefc
max-zipfl-fzi <58517124+max-zipfl-fzi@users.noreply.github.com>
2021-12-08 15:30:08 +0100

    Fix key output of networkx edge getter: (#3646)
    * Fix key output of networkx edge getter:
    Removed the key output, which resulted in a three dimensional tensor, when using MultiDiGraphs.
    Three dimensional edge tuples could not be converted into the required [2,-1] COO format.
    
    * Fix key output of networkx edge getter
    Check: networkx graphtype
    
    * Fix Linting errors:
    Line too Long
    
    * FIx Linting errors:
    remove blank space
    
    * update
    
    Co-authored-by: rusty1s <matthias.fey@tu-dortmund.de>

--- torch_geometric/utils/convert.py
+++ torch_geometric/utils/convert.py
@@ -132,7 +132,13 @@ def from_networkx(G, group_node_attrs: Optional[Union[List[str], all]] = None,
 
     G = nx.convert_node_labels_to_integers(G)
     G = G.to_directed() if not nx.is_directed(G) else G
-    edge_index = torch.LongTensor(list(G.edges)).t().contiguous()
+
+    if isinstance(G, (nx.MultiGraph, nx.MultiDiGraph)):
+        edges = list(G.edges(keys=False))
+    else:
+        edges = list(G.edges)
+
+    edge_index = torch.tensor(edges, dtype=torch.long).t().contiguous()
 
     data = defaultdict(list)
 
