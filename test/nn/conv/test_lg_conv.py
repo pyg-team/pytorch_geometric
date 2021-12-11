801471735b2ac5a25f32b720e8f5152e520c5cbf
Lukas <lukhaas@stanford.edu>
2021-12-11 23:39:02 +0100

    Test Fixes

--- test/nn/conv/test_lg_conv.py
+++ test/nn/conv/test_lg_conv.py
@@ -1,13 +1,10 @@
 import torch
-from torch_sparse import SparseTensor
 from torch_geometric.nn import LGConv
 
 
 def test_light_conv():
     x1 = torch.randn(4, 8)
     edge_index = torch.tensor([[0, 1, 2, 3], [0, 0, 1, 1]])
-    row, col = edge_index
-    adj1 = SparseTensor(row=row, col=col, sparse_sizes=(4, 4))
 
     conv = LGConv()
     assert conv.__repr__() == 'LGConv()'
