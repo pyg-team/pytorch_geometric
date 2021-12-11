1bffee02dc8d509c937d46dbf54111581616f870
Lukas <lukhaas@stanford.edu>
2021-12-11 20:07:33 +0100

    Removed unnecessary variables in tests

--- test/nn/conv/test_lg_conv.py
+++ test/nn/conv/test_lg_conv.py
@@ -5,12 +5,10 @@ from torch_geometric.nn import LGConv
 
 def test_light_conv():
     x1 = torch.randn(4, 8)
-    x2 = torch.randn(2, 16)
     edge_index = torch.tensor([[0, 1, 2, 3], [0, 0, 1, 1]])
     row, col = edge_index
     value = torch.randn(edge_index.size(1))
     adj1 = SparseTensor(row=row, col=col, sparse_sizes=(4, 4))
-    adj2 = SparseTensor(row=row, col=col, value=value, sparse_sizes=(4, 4))
 
     conv = LGConv()
     assert conv.__repr__() == 'LGConv()'
