351c2fc8b7a0b49b7be37ce711bf3b1e776d5ec4
rusty1s <matthias.fey@tu-dortmund.de>
2021-12-11 19:25:52 +0100

    update

--- torch_geometric/graphgym/register.py
+++ torch_geometric/graphgym/register.py
@@ -1,5 +1,20 @@
 from typing import Dict, Any, Union, Callable
 
+act_dict: Dict[str, Any] = {}
+node_encoder_dict: Dict[str, Any] = {}
+edge_encoder_dict: Dict[str, Any] = {}
+stage_dict: Dict[str, Any] = {}
+head_dict: Dict[str, Any] = {}
+layer_dict: Dict[str, Any] = {}
+pooling_dict: Dict[str, Any] = {}
+network_dict: Dict[str, Any] = {}
+config_dict: Dict[str, Any] = {}
+loader_dict: Dict[str, Any] = {}
+optimizer_dict: Dict[str, Any] = {}
+scheduler_dict: Dict[str, Any] = {}
+loss_dict: Dict[str, Any] = {}
+train_dict: Dict[str, Any] = {}
+
 
 def register_base(mapping: Dict[str, Any], key: str,
                   module: Any = None) -> Union[None, Callable]:
@@ -26,92 +41,71 @@ def register_base(mapping: Dict[str, Any], key: str,
     return wrapper
 
 
-act_dict: Dict[str, Any] = {}
-node_encoder_dict: Dict[str, Any] = {}
-edge_encoder_dict: Dict[str, Any] = {}
-stage_dict: Dict[str, Any] = {}
-head_dict: Dict[str, Any] = {}
-layer_dict: Dict[str, Any] = {}
-pooling_dict: Dict[str, Any] = {}
-network_dict: Dict[str, Any] = {}
-config_dict: Dict[str, Any] = {}
-loader_dict: Dict[str, Any] = {}
-optimizer_dict: Dict[str, Any] = {}
-scheduler_dict: Dict[str, Any] = {}
-loss_dict: Dict[str, Any] = {}
-train_dict: Dict[str, Any] = {}
-
-
 def register_act(key: str, module: Any = None):
     r"""Registers an activation function in GraphGym."""
-    register_base(act_dict, key, module)
+    return register_base(act_dict, key, module)
 
 
 def register_node_encoder(key: str, module: Any = None):
     r"""Registers a node feature encoder in GraphGym."""
-    register_base(node_encoder_dict, key, module)
+    return register_base(node_encoder_dict, key, module)
 
 
 def register_edge_encoder(key: str, module: Any = None):
     r"""Registers an edge feature encoder in GraphGym."""
-    register_base(edge_encoder_dict, key, module)
+    return register_base(edge_encoder_dict, key, module)
 
 
 def register_stage(key: str, module: Any = None):
     r"""Registers a customized GNN stage in GraphGym."""
-    register_base(stage_dict, key, module)
+    return register_base(stage_dict, key, module)
 
 
 def register_head(key: str, module: Any = None):
-    r"""
-    Registers a GNN prediction head in GraphGym.
-
-    """
-    register_base(head_dict, key, module)
+    r"""Registers a GNN prediction head in GraphGym."""
+    return register_base(head_dict, key, module)
 
 
 def register_layer(key: str, module: Any = None):
-    r"""
-    Registers a GNN layer in GraphGym.
-    """
-    register_base(layer_dict, key, module)
+    r"""Registers a GNN layer in GraphGym."""
+    return register_base(layer_dict, key, module)
 
 
 def register_pooling(key: str, module: Any = None):
     r"""Registers a GNN global pooling/readout layer in GraphGym."""
-    register_base(pooling_dict, key, module)
+    return register_base(pooling_dict, key, module)
 
 
 def register_network(key: str, module: Any = None):
     r"""Registers a GNN model in GraphGym."""
-    register_base(network_dict, key, module)
+    return register_base(network_dict, key, module)
 
 
 def register_config(key: str, module: Any = None):
     r"""Registers a configuration group in GraphGym."""
-    register_base(config_dict, key, module)
+    return register_base(config_dict, key, module)
 
 
 def register_loader(key: str, module: Any = None):
     r"""Registers a data loader in GraphGym."""
-    register_base(loader_dict, key, module)
+    return register_base(loader_dict, key, module)
 
 
 def register_optimizer(key: str, module: Any = None):
     r"""Registers an optimizer in GraphGym."""
-    register_base(optimizer_dict, key, module)
+    return register_base(optimizer_dict, key, module)
 
 
 def register_scheduler(key: str, module: Any = None):
     r"""Registers a learning rate scheduler in GraphGym."""
-    register_base(scheduler_dict, key, module)
+    return register_base(scheduler_dict, key, module)
 
 
 def register_loss(key: str, module: Any = None):
     r"""Registers a loss function in GraphGym."""
-    register_base(loss_dict, key, module)
+    return register_base(loss_dict, key, module)
 
 
 def register_train(key: str, module: Any = None):
     r"""Registers a training function in GraphGym."""
-    register_base(train_dict, key, module)
+    return register_base(train_dict, key, module)
