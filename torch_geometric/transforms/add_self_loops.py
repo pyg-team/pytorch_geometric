152e805b063cf961e1bfadfd53d78458e2d87859
Dongkwan Kim <todoaskit@gmail.com>
2021-12-15 18:10:57 +0900

    Add attr arg of edge feature in AddSelfLoops

--- torch_geometric/transforms/add_self_loops.py
+++ torch_geometric/transforms/add_self_loops.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, Optional
 
 from torch_geometric.utils import add_self_loops
 from torch_geometric.data import Data, HeteroData
@@ -6,7 +6,17 @@ from torch_geometric.transforms import BaseTransform
 
 
 class AddSelfLoops(BaseTransform):
-    r"""Adds self-loops to the given homogeneous or heterogeneous graph."""
+    r"""Adds self-loops to the given homogeneous or heterogeneous graph.
+
+    Args:
+        attr: (str, optional): The name of the attribute of edge weights
+            or multi-dimensional edge features, to pass it to the
+            :meth:`torch_geometric.utils.add_self_loops`.
+            (default: :obj:`edge_weight`)
+    """
+    def __init__(self, attr: Optional[str] = 'edge_weight'):
+        self.attr = attr
+
     def __call__(self, data: Union[Data, HeteroData]):
         for store in data.edge_stores:
             if store.is_bipartite() or 'edge_index' not in store:
@@ -14,7 +24,7 @@ class AddSelfLoops(BaseTransform):
 
             store.edge_index, store.edge_weight = add_self_loops(
                 store.edge_index,
-                store.edge_weight if 'edge_weight' in store else None,
+                getattr(store, self.attr, None),
                 num_nodes=store.size(0))
 
         return data
