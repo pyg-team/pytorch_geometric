11babb0b6f8aee2a3ae5850c99a4ea2ff07f743b
Stefano Roncelli <45285915+saiden89@users.noreply.github.com>
2021-12-21 22:28:09 +0100

    removed unused Size import

--- torch_geometric/nn/conv/gatv2_conv.py
+++ torch_geometric/nn/conv/gatv2_conv.py
@@ -1,5 +1,5 @@
 from typing import Union, Tuple, Optional
-from torch_geometric.typing import (Adj, Size, OptTensor, PairTensor)
+from torch_geometric.typing import (Adj, OptTensor, PairTensor)
 
 import torch
 from torch import Tensor
