7be54d9484cc88b298b92b154cae35479f69725c
rusty1s <matthias.fey@tu-dortmund.de>
2021-12-25 13:57:45 +0100

    update

--- torch_geometric/graphgym/cmd_args.py
+++ torch_geometric/graphgym/cmd_args.py
@@ -1,28 +1,17 @@
 import argparse
-import sys
 
 
-def parse_args():
-    r"""
-    Parses the command line arguments.
+def parse_args() -> argparse.Namespace:
+    r"""Parses the command line arguments."""
+    parser = argparse.ArgumentParser(description='GraphGym')
 
-    """
-    parser = argparse.ArgumentParser(
-        description='Train a classification model')
-    parser.add_argument('--cfg', dest='cfg_file', help='Config file path',
-                        required=True, type=str)
-    parser.add_argument('--repeat', dest='repeat',
-                        help='Repeat how many random seeds', default=1,
-                        type=int)
-    parser.add_argument(
-        '--mark_done',
-        dest='mark_done',
-        action='store_true',
-        help='mark yaml as yaml_done after a job has finished',
-    )
-    parser.add_argument('opts', help='See graphgym/config.py for all options',
-                        default=None, nargs=argparse.REMAINDER)
-    if len(sys.argv) == 1:
-        parser.print_help()
-        sys.exit(1)
+    parser.add_argument('--cfg', type=str, required=True,
+                        help='The configuration file path.')
+    parser.add_argument('--repeat', type=int, default=1,
+                        help='The number of repeated jobs.')
+    parser.add_argument('--mark_done', action='store_true',
+                        help='Mark yaml as done after a job has finished.')
+    parser.add_argument('opts', default=None, nargs=argparse.REMAINDER,
+                        help='See graphgym/config.py for remaining options.')
+
     return parser.parse_args()
