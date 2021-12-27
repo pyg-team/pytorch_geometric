3fc1ca06949a4449f5436d848e87b99f27aa4025
rusty1s <matthias.fey@tu-dortmund.de>
2021-12-27 18:13:33 +0100

    ckpt refactor

--- torch_geometric/graphgym/checkpoint.py
+++ torch_geometric/graphgym/checkpoint.py
@@ -8,6 +8,10 @@ import torch
 
 from torch_geometric.graphgym.config import cfg
 
+MODEL_STATE = 'model_state'
+OPTIMIZER_STATE = 'optimizer_state'
+SCHEDULER_STATE = 'scheduler_state'
+
 
 def load_ckpt(
     model: torch.nn.Module,
@@ -16,19 +20,18 @@ def load_ckpt(
     epoch: int = -1,
 ) -> int:
     r"""Loads the model checkpoint at a given epoch."""
-    if epoch < 0:
-        epochs = get_ckpt_epochs()
-        epoch = epochs[epoch] if len(epochs) > 0 else 0
+    epoch = get_ckpt_epoch(epoch)
+    path = get_ckpt_path(epoch)
 
-    if not osp.exists(get_ckpt_path(epoch)):
+    if not osp.exists(path):
         return 0
 
-    ckpt = torch.load(get_ckpt_path(epoch))
-    model.load_state_dict(ckpt['model_state'])
-    if optimizer is not None and 'optimizer_state' in ckpt:
-        optimizer.load_state_dict(ckpt['optimizer_state'])
-    if scheduler is not None and 'scheduler_state' in ckpt:
-        scheduler.load_state_dict(ckpt['scheduler_state'])
+    ckpt = torch.load(path)
+    model.load_state_dict(ckpt[MODEL_STATE])
+    if optimizer is not None and OPTIMIZER_STATE in ckpt:
+        optimizer.load_state_dict(ckpt[OPTIMIZER_STATE])
+    if scheduler is not None and SCHEDULER_STATE in ckpt:
+        scheduler.load_state_dict(ckpt[SCHEDULER_STATE])
 
     return epoch + 1
 
@@ -41,23 +44,19 @@ def save_ckpt(
 ):
     r"""Saves the model checkpoint at a given epoch."""
     ckpt: Dict[str, Any] = {}
-    ckpt['model_state'] = model.state_dict()
+    ckpt[MODEL_STATE] = model.state_dict()
     if optimizer is not None:
-        ckpt['optimizer_state'] = optimizer.state_dict()
+        ckpt[OPTIMIZER_STATE] = optimizer.state_dict()
     if scheduler is not None:
-        ckpt['scheduler_state'] = scheduler.state_dict()
+        ckpt[SCHEDULER_STATE] = scheduler.state_dict()
 
     os.makedirs(get_ckpt_dir(), exist_ok=True)
-    torch.save(ckpt, get_ckpt_path(epoch))
+    torch.save(ckpt, get_ckpt_path(get_ckpt_epoch(epoch)))
 
 
 def remove_ckpt(epoch: int = -1):
     r"""Removes the model checkpoint at a given epoch."""
-    if epoch < 0:
-        epochs = get_ckpt_epochs()
-        epoch = epochs[epoch] if len(epochs) > 0 else 0
-
-    os.remove(get_ckpt_path(epoch))
+    os.remove(get_ckpt_path(get_ckpt_epoch(epoch)))
 
 
 def clean_ckpt():
@@ -73,8 +72,8 @@ def get_ckpt_dir() -> str:
     return osp.join(cfg.run_dir, 'ckpt')
 
 
-def get_ckpt_path(name: Union[int, str]) -> str:
-    return osp.join(get_ckpt_dir(), f'{name}.ckpt')
+def get_ckpt_path(epoch: Union[int, str]) -> str:
+    return osp.join(get_ckpt_dir(), f'{epoch}.ckpt')
 
 
 def get_ckpt_epochs() -> List[int]:
@@ -82,5 +81,8 @@ def get_ckpt_epochs() -> List[int]:
     return sorted([int(osp.basename(path).split('.')[0]) for path in paths])
 
 
-def get_last_ckpt_epoch() -> int:
-    return get_ckpt_epochs[-1]
+def get_ckpt_epoch(epoch: int) -> int:
+    if epoch < 0:
+        epochs = get_ckpt_epochs()
+        epoch = epochs[epoch] if len(epochs) > 0 else 0
+    return epoch
