9b5b308f313c18ac53973ff8b8b445cb639cc8f2
rusty1s <matthias.fey@tu-dortmund.de>
2021-12-31 13:37:27 +0100

    reset

--- graphgym/custom_graphgym/loader/example.py
+++ graphgym/custom_graphgym/loader/example.py
@@ -1,5 +1,12 @@
 from torch_geometric.datasets import QM7b
 
-from torch_geometric.graphgym.register import register_dataset
+from torch_geometric.graphgym.register import register_loader
 
-register_dataset('qm7b', QM7b)
+
+@register_loader('example')
+def load_dataset_example(format, name, dataset_dir):
+    dataset_dir = f'{dataset_dir}/{name}'
+    if format == 'PyG':
+        if name == 'QM7b':
+            dataset_raw = QM7b(dataset_dir)
+            return dataset_raw
