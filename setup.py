27ef60e86b97411396fa4c36eb95b73198745dee
Itmom <53969314+Cho-Geonwoo@users.noreply.github.com>
2022-01-05 09:57:21 +0900

    Update setup.py
    Co-authored-by: Matthias Fey <matthias.fey@tu-dortmund.de>

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     install_requires=install_requires,
     extras_require={
         'test': tests_require,
-        'dev': dev_requires
+        'dev': dev_requires,
     },
     packages=find_packages(),
     include_package_data=True,
