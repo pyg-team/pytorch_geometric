1e24b3a16fa2e92f9ad558ec2544c29132f7129b
Matthias Fey <matthias.fey@tu-dortmund.de>
2022-01-26 16:26:24 +0100

    Refactor: `MLP` initialization (#3957)
    * update MLP
    
    * docstring
    
    * fix test
    
    * typo
    
    * typo

--- torch_geometric/nn/models/mlp.py
+++ torch_geometric/nn/models/mlp.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional, Union
 
 import torch
 import torch.nn.functional as F
@@ -9,12 +9,39 @@ from torch_geometric.nn.dense.linear import Linear
 
 
 class MLP(torch.nn.Module):
-    r"""A multi-layer perception (MLP) model.
+    r"""A Multi-Layer Perception (MLP) model.
+    There exists two ways to instantiate an :class:`MLP`:
+
+    1. By specifying explicit channel sizes, *e.g.*,
+
+       .. code-block:: python
+
+          mlp = MLP([16, 32, 64, 128])
+
+       creates a three-layer MLP with **differently** sized hidden layers.
+
+    1. By specifying fixed hidden channel sizes over a number of layers,
+       *e.g.*,
+
+       .. code-block:: python
+
+          mlp = MLP(in_channels=16, hidden_channels=32,
+                    out_channels=128, num_layers=3)
+
+       creates a three-layer MLP with **equally** sized hidden layers.
 
     Args:
-        channel_list (List[int]): List of input, intermediate and output
-            channels. :obj:`len(channel_list) - 1` denotes the number of layers
-            of the MLP.
+        channel_list (List[int] or int, optional): List of input, intermediate
+            and output channels. :obj:`len(channel_list) - 1` denotes the
+            number of layers of the MLP (default: :obj:`None`)
+        in_channels (int, optional): Size of each input sample.
+            Will override :attr:`channel_list`. (default: :obj:`None`)
+        hidden_channels (int, optional): Size of each hidden sample.
+            Will override :attr:`channel_list`. (default: :obj:`None`)
+        out_channels (int, optional): Size of each output sample.
+            Will override :attr:`channel_list`. (default: :obj:`None`)
+        num_layers (int, optional): The number of layers.
+            Will override :attr:`channel_list`. (default: :obj:`None`)
         dropout (float, optional): Dropout probability of each hidden
             embedding. (default: :obj:`0.`)
         batch_norm (bool, optional): If set to :obj:`False`, will not make use
@@ -22,9 +49,29 @@ class MLP(torch.nn.Module):
         relu_first (bool, optional): If set to :obj:`True`, ReLU activation is
             applied before batch normalization. (default: :obj:`False`)
     """
-    def __init__(self, channel_list: List[int], dropout: float = 0.,
-                 batch_norm: bool = True, relu_first: bool = False):
+    def __init__(
+        self,
+        channel_list: Optional[Union[List[int], int]] = None,
+        *,
+        in_channels: Optional[int] = None,
+        hidden_channels: Optional[int] = None,
+        out_channels: Optional[int] = None,
+        num_layers: Optional[int] = None,
+        dropout: float = 0.,
+        batch_norm: bool = True,
+        relu_first: bool = False,
+    ):
         super().__init__()
+
+        if not isinstance(channel_list, (tuple, list)):
+            in_channels = channel_list
+
+        if in_channels is not None:
+            assert num_layers >= 1
+            channel_list = [hidden_channels] * (num_layers - 1)
+            channel_list = [in_channels] + channel_list + [out_channels]
+
+        assert isinstance(channel_list, (tuple, list))
         assert len(channel_list) >= 2
         self.channel_list = channel_list
         self.dropout = dropout
@@ -39,6 +86,21 @@ class MLP(torch.nn.Module):
             self.norms.append(BatchNorm1d(dim) if batch_norm else Identity())
 
         self.reset_parameters()
+
+    @property
+    def in_channels(self) -> int:
+        r"""Size of each input sample."""
+        return self.channel_list[0]
+
+    @property
+    def out_channels(self) -> int:
+        r"""Size of each output sample."""
+        return self.channel_list[-1]
+
+    @property
+    def num_layers(self) -> int:
+        r"""The number of layers."""
+        return len(self.channel_list) - 1
 
     def reset_parameters(self):
         for lin in self.lins:
