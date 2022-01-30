b679e74f40049e0b5a8785155c303f4e077e6d25
Amit Ambardekar <amitamb@gmail.com>
2022-01-31 02:30:26 +0530

    Updated var_misuse implementation

--- torch_geometric/datasets/var_misuse.py
+++ torch_geometric/datasets/var_misuse.py
@@ -1,25 +1,31 @@
-from typing import Optional, Callable
-
+from typing import Optional, Callable, List
 import os
-import datetime
-
+import json
 import torch
 
-from torch_geometric.data import (InMemoryDataset, Data, download_url,
-                                  extract_gz)
+from torch_geometric.data import (Data, download_url,
+                                  extract_gz, Dataset)
 
+BACKWARD_EDGE_TYPE_NAME_SUFFIX = "_Bkwd"
+__PROGRAM_GRAPH_EDGES_TYPES = ["Child", "NextToken", "LastUse", "LastWrite", "LastLexicalUse", "ComputedFrom",
+                               "GuardedByNegation", "GuardedBy", "FormalArgName", "ReturnsTo"]
+__PROGRAM_GRAPH_EDGES_TYPES_WITH_BKWD = \
+    __PROGRAM_GRAPH_EDGES_TYPES + [edge_type_name + BACKWARD_EDGE_TYPE_NAME_SUFFIX
+                                   for edge_type_name in __PROGRAM_GRAPH_EDGES_TYPES]
+PROGRAM_GRAPH_EDGES_TYPES_VOCAB = {edge_type_name: idx
+                                   for idx, edge_type_name in enumerate(__PROGRAM_GRAPH_EDGES_TYPES_WITH_BKWD)}
 
-class VarMisuse(InMemoryDataset):
-    r"""The Bitcoin-OTC dataset from the `"EvolveGCN: Evolving Graph
-    Convolutional Networks for Dynamic Graphs"
-    <https://arxiv.org/abs/1902.10191>`_ paper, consisting of 138
-    who-trusts-whom networks of sequential time steps.
+class VarMisuse(Dataset):
+    r"""The VarMisuse dataset from the `"Learning to Represent Programs "
+    with Graphs. <https://arxiv.org/abs/1711.00740>`_ paper, consisting of
+    code from files from different open source projects represented as 
+    graphs for learning to identify misuse of variable and naming of
+    variables.
 
     Args:
         root (string): Root directory where the dataset should be saved.
-        edge_window_size (int, optional): The window size for the existence of
-            an edge in the graph sequence since its initial creation.
-            (default: :obj:`10`)
+        split (int, optional): Split of the data from either `train`, `valid`
+            or `test` (default: :str:`train`)
         transform (callable, optional): A function/transform that takes in an
             :obj:`torch_geometric.data.Data` object and returns a transformed
             version. The data object will be transformed before every access.
@@ -28,77 +34,120 @@ class VarMisuse(InMemoryDataset):
             an :obj:`torch_geometric.data.Data` object and returns a
             transformed version. The data object will be transformed before
             being saved to disk. (default: :obj:`None`)
+        pre_filter (callable, optional): A function that takes in an
+            :obj:`torch_geometric.data.Data` object and returns a boolean
+            value, indicating whether the data object should be included in the
+            final dataset. (default: :obj:`None`)
     """
+    url = 'https://github.com/amitamb-ml/varmisuse-data/raw/main/graphs-{}/{}.jsonl.gz'
 
-    url = ('https://github.com/amitamb/varmisuse-data/blob/main/graphs-{}-raw/{}')
-
-    # number of files for each repo
-    REPOS_MAP = {
-      akka: 26
+    windows_count = {
+        'train': 20,
+        'valid': 4,
+        'test': 4
     }
 
+    chunks_per_window = 10
+
     def __init__(self, root: str,
+                 split='train',
                  transform: Optional[Callable] = None,
-                 pre_transform: Optional[Callable] = None):
-        self.edge_window_size = edge_window_size
-        super().__init__(root, transform, pre_transform)
-        self.data, self.slices = torch.load(self.processed_paths[0])
+                 pre_transform: Optional[Callable] = None,
+                 pre_filter=None):
+        self.split = split
+        super().__init__(root, transform, pre_transform, pre_filter)
 
     @property
-    def raw_file_names(self) -> str:
-        return 'soc-sign-bitcoinotc.csv'
+    def raw_file_names(self) -> List[str]:
+        return [ f'{chunk_name}.jsonl' for chunk_name in self.chunk_names ]
 
     @property
-    def processed_file_names(self) -> str:
-        return 'data.pt'
+    def processed_file_names(self) -> List[str]:
+        return [ f'{chunk_name}.pt' for chunk_name in self.chunk_names ]
 
     @property
-    def num_nodes(self) -> int:
-        return self.data.edge_index.max().item() + 1
+    def chunk_names(self) -> List[str]:
+        chunk_names = []
+        total_windows = self.windows_count[self.split]
+        for window in range(total_windows):
+            for chunk in range(self.chunks_per_window):
+                chunk_names.append(f'chunk_{window}-{chunk}')
+        return chunk_names
 
     def download(self):
-        path = download_url(self.url, self.raw_dir)
-        extract_gz(path, self.raw_dir)
-        os.unlink(path)
+        for chunk_name in self.chunk_names:
+            full_url = self.url.format(self.split, chunk_name)
+            path = download_url(full_url, self.raw_dir)
+            extract_gz(path, self.raw_dir)
+            os.unlink(path)
 
     def process(self):
-        with open(self.raw_paths[0], 'r') as f:
-            data = f.read().split('\n')[:-1]
-            data = [[x for x in line.split(',')] for line in data]
+        for idx, raw_path in enumerate(self.raw_paths):
+            with open(self.raw_paths[0], 'r') as f:
+                graphs_json = f.read().split('\n')[:-1]
+                data_list = []
+                for graph_json in graphs_json:
+                    raw_sample = json.loads(graph_json)
+                    
+                    slot_node_id = raw_sample['SlotDummyNode']
+                    distractor_candidate_ids = []
+                    for candidate in raw_sample['SymbolCandidates']:
+                        if candidate['IsCorrect']:
+                            correct_candidate_id = candidate['SymbolDummyNode']
+                        else:
+                            distractor_candidate_ids.append(candidate['SymbolDummyNode'])
+                    assert correct_candidate_id is not None
+                    # 0th candidate_id will always be correct
+                    # candidate_node_ids = [correct_candidate_id] + distractor_candidate_ids
+                    
+                    edge_indexes = []
+                    edge_types = []
+                    
+                    raw_edges = raw_sample['ContextGraph']['Edges']
+                    for e_type, e_type_edges in raw_edges.items():
+                        if len(e_type_edges) > 0:
+                            e_type_bkwd = e_type + BACKWARD_EDGE_TYPE_NAME_SUFFIX
+                            e_type_idx = PROGRAM_GRAPH_EDGES_TYPES_VOCAB[e_type]
+                            e_type_bkwd_idx = PROGRAM_GRAPH_EDGES_TYPES_VOCAB[e_type_bkwd]
 
-            edge_index = [[int(line[0]), int(line[1])] for line in data]
-            edge_index = torch.tensor(edge_index, dtype=torch.long)
-            edge_index = edge_index - edge_index.min()
-            edge_index = edge_index.t().contiguous()
-            num_nodes = edge_index.max().item() + 1
+                            fwd_edges = torch.tensor(e_type_edges, dtype=torch.int32).t().contiguous()
+                            fwd_edge_type = torch.tensor([e_type_idx] * fwd_edges.size(1), dtype=torch.int8)
+                            bkwd_edges = torch.flip(fwd_edges, [0])
+                            bkwd_edge_type = torch.tensor([e_type_bkwd_idx] * bkwd_edges.size(1), dtype=torch.int8)
+                            
+                            edge_indexes.append(fwd_edges)
+                            edge_types.append(fwd_edge_type)
+                            edge_indexes.append(bkwd_edges)
+                            edge_types.append(bkwd_edge_type)
 
-            edge_attr = [float(line[2]) for line in data]
-            edge_attr = torch.tensor(edge_attr, dtype=torch.long)
+                    edge_index = torch.cat(edge_indexes, dim=1)
+                    edge_type = torch.cat(edge_types, dim=-1)
+                    
+                    num_nodes = len(raw_sample['ContextGraph']['NodeLabels'])
+                    node_labels = [""] * num_nodes
+                    for (node, label) in raw_sample['ContextGraph']['NodeLabels'].items():
+                        node_labels[int(node)] = label
+                    
+                    data = Data(edge_index=edge_index, edge_type=edge_type,
+                                node_labels=node_labels, slot_node_id=slot_node_id,
+                                correct_candidate_id=correct_candidate_id,
+                                distractor_candidate_ids=distractor_candidate_ids)
+                    
+                    data_list.append(data)
+                
+                if self.pre_filter is not None:
+                    data_list = [d for d in data_list if self.pre_filter(d)]
 
-            stamps = [int(float(line[3])) for line in data]
-            stamps = [datetime.datetime.fromtimestamp(x) for x in stamps]
+                if self.pre_transform is not None:
+                    data_list = [self.pre_transform(d) for d in data_list]
+                torch.save(data_list, self.processed_paths[idx])
 
-        offset = datetime.timedelta(days=13.8)  # Results in 138 time steps.
-        graph_idx, factor = [], 1
-        for t in stamps:
-            factor = factor if t < stamps[0] + factor * offset else factor + 1
-            graph_idx.append(factor - 1)
-        graph_idx = torch.tensor(graph_idx, dtype=torch.long)
+    def len(self):
+        return len(self.processed_file_names) * 100
 
-        data_list = []
-        for i in range(graph_idx.max().item() + 1):
-            mask = (graph_idx > (i - self.edge_window_size)) & (graph_idx <= i)
-            data = Data()
-            data.edge_index = edge_index[:, mask]
-            data.edge_attr = edge_attr[mask]
-            data.num_nodes = num_nodes
-            data_list.append(data)
-
-        if self.pre_filter is not None:
-            data_list = [d for d in data_list if self.pre_filter(d)]
-
-        if self.pre_transform is not None:
-            data_list = [self.pre_transform(d) for d in data_list]
-
-        data, slices = self.collate(data_list)
-        torch.save((data, slices), self.processed_paths[0])
+    def get(self, idx):
+        file_idx,data_idx = divmod(idx,100)
+        window,chunk = divmod(file_idx, 10)
+        data_list = torch.load(os.path.join(self.processed_dir, f'chunk_{window}-{chunk}.pt'))
+        data = data_list[data_idx]
+        return data
