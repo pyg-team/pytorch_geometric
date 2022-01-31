fc58f3cf581533fb670112f54cb7931fd2e6e662
Amit Ambardekar <amitamb@gmail.com>
2022-01-31 11:09:36 +0530

    Fixed few bugs and flex8 issues

--- torch_geometric/datasets/var_misuse.py
+++ torch_geometric/datasets/var_misuse.py
@@ -6,32 +6,16 @@ import torch
 
 from torch_geometric.data import Data, Dataset, download_url, extract_gz
 
-BACKWARD_EDGE_TYPE_NAME_SUFFIX = "_Bkwd"
-__PROGRAM_GRAPH_EDGES_TYPES = [
-    "Child", "NextToken", "LastUse", "LastWrite", "LastLexicalUse",
-    "ComputedFrom", "GuardedByNegation", "GuardedBy", "FormalArgName",
-    "ReturnsTo"
-]
-__PROGRAM_GRAPH_EDGES_TYPES_WITH_BKWD = \
-    __PROGRAM_GRAPH_EDGES_TYPES + [edge_type_name + BACKWARD_EDGE_TYPE_NAME_SUFFIX
-                                   for edge_type_name in __PROGRAM_GRAPH_EDGES_TYPES]
-PROGRAM_GRAPH_EDGES_TYPES_VOCAB = {
-    edge_type_name: idx
-    for idx, edge_type_name in enumerate(__PROGRAM_GRAPH_EDGES_TYPES_WITH_BKWD)
-}
-
-
 class VarMisuse(Dataset):
     r"""The VarMisuse dataset from the `"Learning to Represent Programs "
-    with Graphs. <https://arxiv.org/abs/1711.00740>`_ paper, consisting of
-    code from files from different open source projects represented as 
+    with Graphs. <https://arxiv.org/abs/1
+    711.00740>`_ paper, consisting of
+    code from files from different open source projects represented as
     graphs for learning to identify misuse of variable and naming of
     variables.
 
     Args:
         root (string): Root directory where the dataset should be saved.
-        name (string): The name of the dataset (:obj:`"CS"`,
-            :obj:`"Physics"`).
         split (string, optional): If :obj:`"train"`, loads the training
             dataset.
             If :obj:`"valid"`, loads the validation dataset.
@@ -50,11 +34,23 @@ class VarMisuse(Dataset):
             value, indicating whether the data object should be included in the
             final dataset. (default: :obj:`None`)
     """
-    url = 'https://github.com/amitamb-ml/varmisuse-data/raw/main/graphs-{}/{}.jsonl.gz'
+    url = ('https://github.com/amitamb-ml/'
+           'varmisuse-data/raw/main/graphs-{}/{}.jsonl.gz')
 
     windows_count = {'train': 20, 'valid': 4, 'test': 4}
 
     chunks_per_window = 10
+
+    graph_edge_types = [
+        "Child", "NextToken", "LastUse", "LastWrite", "LastLexicalUse",
+        "ComputedFrom", "GuardedByNegation", "GuardedBy", "FormalArgName",
+        "ReturnsTo"
+    ]
+
+    BACKWARD_EDGE_TYPE_NAME_SUFFIX = "_Bkwd"
+
+    graph_edge_types_vocab = [edge_type_name + BACKWARD_EDGE_TYPE_NAME_SUFFIX
+                              for edge_type_name in graph_edge_types]
 
     def __init__(self, root: str, split='train',
                  transform: Optional[Callable] = None,
@@ -88,75 +84,79 @@ class VarMisuse(Dataset):
 
     def process(self):
         for idx, raw_path in enumerate(self.raw_paths):
-            with open(self.raw_paths[0], 'r') as f:
-                graphs_json = f.read().split('\n')[:-1]
-                data_list = []
-                for graph_json in graphs_json:
-                    raw_sample = json.loads(graph_json)
+            self.process_path(self.raw_paths[idx])
 
-                    slot_node_id = raw_sample['SlotDummyNode']
-                    distractor_candidate_ids = []
-                    for candidate in raw_sample['SymbolCandidates']:
-                        if candidate['IsCorrect']:
-                            correct_candidate_id = candidate['SymbolDummyNode']
-                        else:
-                            distractor_candidate_ids.append(
-                                candidate['SymbolDummyNode'])
-                    assert correct_candidate_id is not None
-                    # 0th candidate_id will always be correct
-                    # candidate_node_ids = [correct_candidate_id] + distractor_candidate_ids
+    def process_path(self, raw_path):
+        with open(raw_path, 'r') as f:
+            graphs_json = f.read().split('\n')[:-1]
+            data_list = []
+            for graph_json in graphs_json:
+                raw_sample = json.loads(graph_json)
 
-                    edge_indexes = []
-                    edge_types = []
+                slot_node_id = raw_sample['SlotDummyNode']
+                distractor_candidate_ids = []
+                for candidate in raw_sample['SymbolCandidates']:
+                    if candidate['IsCorrect']:
+                        correct_candidate_id = candidate['SymbolDummyNode']
+                    else:
+                        distractor_candidate_ids.append(
+                            candidate['SymbolDummyNode'])
+                assert correct_candidate_id is not None
+                # 0th candidate_id will always be correct
+                # candidate_node_ids = [correct_candidate_id] +
+                #                      distractor_candidate_ids
 
-                    raw_edges = raw_sample['ContextGraph']['Edges']
-                    for e_type, e_type_edges in raw_edges.items():
-                        if len(e_type_edges) > 0:
-                            e_type_bkwd = e_type + BACKWARD_EDGE_TYPE_NAME_SUFFIX
-                            e_type_idx = PROGRAM_GRAPH_EDGES_TYPES_VOCAB[
-                                e_type]
-                            e_type_bkwd_idx = PROGRAM_GRAPH_EDGES_TYPES_VOCAB[
-                                e_type_bkwd]
+                edge_indexes = []
+                edge_types = []
 
-                            fwd_edges = torch.tensor(
-                                e_type_edges,
-                                dtype=torch.int32).t().contiguous()
-                            fwd_edge_type = torch.tensor([e_type_idx] *
-                                                         fwd_edges.size(1),
-                                                         dtype=torch.int8)
-                            bkwd_edges = torch.flip(fwd_edges, [0])
-                            bkwd_edge_type = torch.tensor([e_type_bkwd_idx] *
-                                                          bkwd_edges.size(1),
-                                                          dtype=torch.int8)
+                raw_edges = raw_sample['ContextGraph']['Edges']
+                for e_type, e_type_edges in raw_edges.items():
+                    if len(e_type_edges) > 0:
+                        e_type_bkwd = (e_type +
+                                       self.BACKWARD_EDGE_TYPE_NAME_SUFFIX)
+                        e_type_idx = self.graph_edge_types_vocab[e_type]
+                        e_type_bkwd_idx = self.graph_edge_types_vocab[
+                            e_type_bkwd]
 
-                            edge_indexes.append(fwd_edges)
-                            edge_types.append(fwd_edge_type)
-                            edge_indexes.append(bkwd_edges)
-                            edge_types.append(bkwd_edge_type)
+                        fwd_edges = torch.tensor(
+                            e_type_edges,
+                            dtype=torch.int32).t().contiguous()
+                        fwd_edge_type = torch.tensor([e_type_idx] *
+                                                     fwd_edges.size(1),
+                                                     dtype=torch.int8)
+                        bkwd_edges = torch.flip(fwd_edges, [0])
+                        bkwd_edge_type = torch.tensor([e_type_bkwd_idx] *
+                                                      bkwd_edges.size(1),
+                                                      dtype=torch.int8)
 
-                    edge_index = torch.cat(edge_indexes, dim=1)
-                    edge_type = torch.cat(edge_types, dim=-1)
+                        edge_indexes.append(fwd_edges)
+                        edge_types.append(fwd_edge_type)
+                        edge_indexes.append(bkwd_edges)
+                        edge_types.append(bkwd_edge_type)
 
-                    num_nodes = len(raw_sample['ContextGraph']['NodeLabels'])
-                    node_labels = [""] * num_nodes
-                    for (node, label
-                         ) in raw_sample['ContextGraph']['NodeLabels'].items():
-                        node_labels[int(node)] = label
+                edge_index = torch.cat(edge_indexes, dim=1)
+                edge_type = torch.cat(edge_types, dim=-1)
 
-                    data = Data(
-                        edge_index=edge_index, edge_type=edge_type,
-                        node_labels=node_labels, slot_node_id=slot_node_id,
-                        correct_candidate_id=correct_candidate_id,
-                        distractor_candidate_ids=distractor_candidate_ids)
+                num_nodes = len(raw_sample['ContextGraph']['NodeLabels'])
+                node_labels = [""] * num_nodes
+                node_labels = raw_sample['ContextGraph']['NodeLabels']
+                for (node, label) in node_labels.items():
+                    node_labels[int(node)] = label
 
-                    data_list.append(data)
+                data = Data(
+                    edge_index=edge_index, edge_type=edge_type,
+                    node_labels=node_labels, slot_node_id=slot_node_id,
+                    correct_candidate_id=correct_candidate_id,
+                    distractor_candidate_ids=distractor_candidate_ids)
 
-                if self.pre_filter is not None:
-                    data_list = [d for d in data_list if self.pre_filter(d)]
+                data_list.append(data)
 
-                if self.pre_transform is not None:
-                    data_list = [self.pre_transform(d) for d in data_list]
-                torch.save(data_list, self.processed_paths[idx])
+            if self.pre_filter is not None:
+                data_list = [d for d in data_list if self.pre_filter(d)]
+
+            if self.pre_transform is not None:
+                data_list = [self.pre_transform(d) for d in data_list]
+            torch.save(data_list, self.processed_paths[idx])
 
     def len(self):
         return len(self.processed_file_names) * 100
