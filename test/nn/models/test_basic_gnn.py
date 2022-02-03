a631cbd3b6cca21132efaaab7a1f3a264ef787b3
rusty1s <matthias.fey@tu-dortmund.de>
2022-02-03 14:45:08 +0100

    test packaging

--- test/nn/models/test_basic_gnn.py
+++ test/nn/models/test_basic_gnn.py
@@ -1,3 +1,4 @@
+import os
 from itertools import product
 
 import pytest
@@ -89,3 +90,29 @@ def test_pna(out_dim, dropout, act, norm, jk):
                 scalers=scalers, deg=deg)
     assert str(model) == f'PNA(8, {out_channels}, num_layers=2)'
     assert model(x, edge_index).size() == (3, out_channels)
+
+
+def test_packaging():
+    x = torch.randn(3, 8)
+    edge_index = torch.tensor([[0, 1, 1, 2], [1, 0, 2, 1]])
+
+    model = GraphSAGE(8, 16, num_layers=2)
+    torch.save(model, 'model.pt')
+
+    model = torch.load('model.pt')
+    with torch.no_grad():
+        assert model(x, edge_index).size() == (3, 16)
+    os.remove('model.pt')
+
+    model = GraphSAGE(8, 16, num_layers=2)
+    with torch.package.PackageExporter('package.pt') as pe:
+        pe.extern('torch_geometric.nn.**')
+        pe.extern('_operator')
+        pe.save_pickle('models', 'model.pkl', model)
+
+    pi = torch.package.PackageImporter('package.pt')
+    model = pi.load_pickle('models', 'model.pkl')
+    with torch.no_grad():
+        assert model(x, edge_index).size() == (3, 16)
+
+    os.remove('package.pt')
