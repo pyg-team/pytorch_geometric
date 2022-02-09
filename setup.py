87803b531ad04125990c8faa83b2cf1bcbacb569
rusty1s <matthias.fey@tu-dortmund.de>
2022-02-09 12:19:35 +0100

    update python version

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
         'graph-neural-networks',
         'graph-convolutional-networks',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     install_requires=install_requires,
     extras_require={
         'full': full_install_requires,
