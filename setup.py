53a06d0c7f7c7cf13d97806b1f48be40235af26f
rusty1s <matthias.fey@tu-dortmund.de>
2022-02-09 11:23:41 +0100

    add class_resolver

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ install_requires = [
     'pyparsing',
     'hydra-core',
     'scikit-learn',
+    'class-resolver',
     'googledrivedownloader',
 ]
 
--- test/nn/models/test_mlp.py
+++ test/nn/models/test_mlp.py
@@ -6,13 +6,13 @@ import torch
 from torch_geometric.nn import MLP
 
 
-@pytest.mark.parametrize('batch_norm,relu_first',
+@pytest.mark.parametrize('batch_norm,act_first',
                          product([False, True], [False, True]))
-def test_mlp(batch_norm, relu_first):
+def test_mlp(batch_norm, act_first):
     x = torch.randn(4, 16)
 
     torch.manual_seed(12345)
-    mlp = MLP([16, 32, 32, 64], batch_norm=batch_norm, relu_first=relu_first)
+    mlp = MLP([16, 32, 32, 64], batch_norm=batch_norm, act_first=act_first)
     assert str(mlp) == 'MLP(16, 32, 32, 64)'
     out = mlp(x)
     assert out.size() == (4, 64)
@@ -22,5 +22,5 @@ def test_mlp(batch_norm, relu_first):
 
     torch.manual_seed(12345)
     mlp = MLP(16, hidden_channels=32, out_channels=64, num_layers=3,
-              batch_norm=batch_norm, relu_first=relu_first)
+              batch_norm=batch_norm, act_first=act_first)
     assert torch.allclose(mlp(x), out)
