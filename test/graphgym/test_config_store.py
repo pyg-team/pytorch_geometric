4479e67db737c384eadd76ba8bb326fc261d96ab
rusty1s <matthias.fey@tu-dortmund.de>
2022-02-09 13:44:14 +0100

    fix test

--- test/graphgym/test_config_store.py
+++ test/graphgym/test_config_store.py
@@ -39,7 +39,7 @@ def test_config_store():
     assert cfg.dataset.transform.AddSelfLoops.fill_value is None
 
     # Check `cfg.model`:
-    assert len(cfg.model) == 10
+    assert len(cfg.model) == 11
     assert cfg.model._target_.split('.')[-1] == 'GCN'
     assert cfg.model.in_channels == 34
     assert cfg.model.out_channels == 4
@@ -49,6 +49,7 @@ def test_config_store():
     assert cfg.model.act == 'relu'
     assert cfg.model.norm is None
     assert cfg.model.jk is None
+    assert not cfg.model.act_first
     assert cfg.model.act_kwargs is None
 
     # Check `cfg.optim`:
