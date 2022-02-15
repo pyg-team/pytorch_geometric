7ce277f2cd4c822eaaf3294e4555146a82696a76
Matthias Fey <matthias.fey@tu-dortmund.de>
2022-02-15 10:08:23 +0100

    `Sequential`: `OrderedDict` support (#4075)
    * sequential ordereddict support
    
    * linting

--- test/nn/test_sequential.py
+++ test/nn/test_sequential.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 import torch
 import torch.fx
 from torch.nn import Dropout, Linear, ReLU
@@ -115,3 +117,20 @@ def test_sequential_with_multiple_return_values():
     x1, x2 = model(x, edge_index)
     assert x1.size() == (4, 32)
     assert x2.size() == (4, 64)
+
+
+def test_sequential_with_ordered_dict():
+    x = torch.randn(4, 16)
+    edge_index = torch.tensor([[0, 0, 0, 1, 2, 3], [1, 2, 3, 0, 0, 0]])
+
+    model = Sequential(
+        'x, edge_index', modules=OrderedDict([
+            ('conv1', (GCNConv(16, 32), 'x, edge_index -> x')),
+            ('conv2', (GCNConv(32, 64), 'x, edge_index -> x')),
+        ]))
+
+    assert isinstance(model.conv1, GCNConv)
+    assert isinstance(model.conv2, GCNConv)
+
+    x = model(x, edge_index)
+    assert x.size() == (4, 64)
