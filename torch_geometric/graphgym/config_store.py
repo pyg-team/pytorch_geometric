431a0747872e48711cd85c96ebf1b8d9e386b01e
Matthias Fey <matthias.fey@tu-dortmund.de>
2022-02-16 10:54:45 +0100

    Updates to `config_store` (#4085)
    * update
    
    * update
    
    * update
    
    * fix test

--- torch_geometric/graphgym/config_store.py
+++ torch_geometric/graphgym/config_store.py
@@ -1,6 +1,6 @@
 import inspect
 from dataclasses import dataclass, field, make_dataclass
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, Dict, List, Optional, Tuple, Union
 
 import torch
 from hydra.core.config_store import ConfigStore
@@ -18,8 +18,13 @@ MAPPING = {
 }
 
 
-def to_dataclass(cls: Any, base: Optional[Any] = None,
-                 exclude: Optional[List[str]] = None) -> Any:
+def to_dataclass(
+    cls: Any,
+    base: Optional[Any] = None,
+    map_args: Optional[Dict[str, Tuple]] = None,
+    exclude_args: Optional[List[str]] = None,
+    strict: bool = False,
+) -> Any:
     r"""Converts the input arguments of a given class :obj:`cls` to a
     :obj:`dataclass` schema, *e.g.*,
 
@@ -42,29 +47,43 @@ def to_dataclass(cls: Any, base: Optional[Any] = None,
         cls (Any): The class to generate a schema for.
         base: (Any, optional): The base class of the schema.
             (default: :obj:`None`)
-        exclude (List[str], optional): Arguments to exclude.
+        map_args (Dict[str, Tuple], optional): Arguments for which annotation
+            and default values should be overriden. (default: :obj:`None`)
+        exclude_args (List[str or int], optional): Arguments to exclude.
             (default: :obj:`None`)
+        strict (bool, optional): If set to :obj:`True`, ensures that all
+            arguments in either :obj:`map_args` and :obj:`exclude_args` are
+            present in the input parameters. (default: :obj:`False`)
     """
     fields = []
 
-    for name, arg in inspect.signature(cls.__init__).parameters.items():
+    params = inspect.signature(cls.__init__).parameters
+
+    if strict:  # Check that keys in map_args or exlcude_args are present.
+        args = set() if map_args is None else set(map_args.keys())
+        if exclude_args is not None:
+            args |= set([arg for arg in exclude_args if isinstance(arg, str)])
+        diff = args - set(params.keys())
+        if len(diff) > 0:
+            raise ValueError(f"Expected input argument(s) {diff} in "
+                             f"'{cls.__name__}'")
+
+    for i, (name, arg) in enumerate(params.items()):
         if name in EXCLUDE:
             continue
-        if exclude is not None and name in exclude:
+        if exclude_args is not None:
+            if name in exclude_args or i in exclude_args:
+                continue
+        if base is not None:
+            if name in base.__dataclass_fields__:
+                continue
+
+        if map_args is not None and name in map_args:
+            fields.append((name, ) + map_args[name])
             continue
-        if base is not None and name in base.__dataclass_fields__.keys():
-            continue
 
-        item = (name, )
-
-        annotation = arg.annotation
-        default = arg.default
-
+        annotation, default = arg.annotation, arg.default
         annotation = map_annotation(annotation, mapping=MAPPING)
-
-        if str(default) == "<required parameter>":
-            # Fix `torch.optim.SGD.lr = _RequiredParameter()`
-            default = inspect.Parameter.empty
 
         if annotation != inspect.Parameter.empty:
             # `Union` types are not supported (except for `Optional`).
@@ -80,17 +99,25 @@ def to_dataclass(cls: Any, base: Optional[Any] = None,
                 annotation = Optional[Any]
             else:
                 annotation = Any
-        item = item + (annotation, )
 
-        if default != inspect.Parameter.empty:
+        if str(default) == "<required parameter>":
+            # Fix `torch.optim.SGD.lr = _RequiredParameter()`:
+            # https://github.com/pytorch/hydra-torch/blob/main/
+            # hydra-configs-torch/hydra_configs/torch/optim/sgd.py
+            default = field(default=MISSING)
+        elif default != inspect.Parameter.empty:
             if isinstance(default, (list, dict)):
-                item = item + (field(default_factory=lambda: default), )
-            else:
-                item = item + (default, )
-        else:
-            item = item + (field(default=MISSING), )
+                # Avoid late binding of default values inside a loop:
+                # https://stackoverflow.com/questions/3431676/
+                # creating-functions-in-a-loop
+                def wrapper(default):
+                    return lambda: default
 
-        fields.append(item)
+                default = field(default_factory=wrapper(default))
+        else:
+            default = field(default=MISSING)
+
+        fields.append((name, annotation, default))
 
     full_cls_name = f'{cls.__module__}.{cls.__qualname__}'
     fields.append(('_target_', str, field(default=full_cls_name)))
@@ -102,13 +129,8 @@ def to_dataclass(cls: Any, base: Optional[Any] = None,
 config_store = ConfigStore.instance()
 
 
-@dataclass
-class BaseConfig:
-    _target_: str = MISSING
-
-
 @dataclass  # Register `torch_geometric.transforms` ###########################
-class Transform(BaseConfig):
+class Transform:
     pass
 
 
@@ -126,43 +148,49 @@ for cls_name in set(transforms.__all__) - set([
 
 
 @dataclass  # Register `torch_geometric.datasets` #############################
-class Dataset(BaseConfig):
-    transform: Dict[str, Transform] = field(default_factory=dict)
-    pre_transform: Dict[str, Transform] = field(default_factory=dict)
+class Dataset:
+    pass
 
+
+map_dataset_args = {
+    'transform': (Dict[str, Transform], field(default_factory=dict)),
+    'pre_transform': (Dict[str, Transform], field(default_factory=dict)),
+}
 
 for cls_name in set(datasets.__all__) - set([]):
     cls = to_dataclass(getattr(datasets, cls_name), base=Dataset,
-                       exclude=['pre_filter'])
+                       map_args=map_dataset_args, exclude_args=['pre_filter'])
     config_store.store(group='dataset', name=cls_name, node=cls)
 
 
 @dataclass  # Register `torch_geometric.models` ###############################
-class Model(BaseConfig):
+class Model:
     pass
 
 
 for cls_name in set(models.__all__) - set([]):
-    cls = to_dataclass(getattr(models, cls_name), base=Model, exclude=[])
+    cls = to_dataclass(getattr(models, cls_name), base=Model)
     config_store.store(group='model', name=cls_name, node=cls)
 
 
 @dataclass  # Register `torch.optim.Optimizer` ################################
-class Optimizer(BaseConfig):
+class Optimizer:
     pass
 
 
 for cls_name in set([
         key for key, cls in torch.optim.__dict__.items()
         if inspect.isclass(cls) and issubclass(cls, torch.optim.Optimizer)
-]) - set(['Optimizer']):
+]) - set([
+        'Optimizer',
+]):
     cls = to_dataclass(getattr(torch.optim, cls_name), base=Optimizer,
-                       exclude=['params'])
+                       exclude_args=['params'])
     config_store.store(group='optimizer', name=cls_name, node=cls)
 
 
 @dataclass  # Register `torch.optim.lr_scheduler` #############################
-class LRScheduler(BaseConfig):
+class LRScheduler:
     pass
 
 
@@ -170,16 +198,23 @@ for cls_name in set([
         key for key, cls in torch.optim.lr_scheduler.__dict__.items()
         if inspect.isclass(cls)
 ]) - set([
-        'Optimizer', '_LRScheduler', 'Counter', 'SequentialLR',
-        'ChainedScheduler'
+        'Optimizer',
+        '_LRScheduler',
+        'Counter',
+        'SequentialLR',
+        'ChainedScheduler',
 ]):
     cls = to_dataclass(getattr(torch.optim.lr_scheduler, cls_name),
-                       base=LRScheduler, exclude=['optimizer'])
+                       base=LRScheduler, exclude_args=['optimizer'])
     config_store.store(group='lr_scheduler', name=cls_name, node=cls)
 
 
 @dataclass  # Register global schema ##########################################
 class Config:
     dataset: Dataset = MISSING
+    model: Model = MISSING
     optim: Optimizer = MISSING
     lr_scheduler: Optional[LRScheduler] = None
+
+
+config_store.store(name='config', node=Config)
