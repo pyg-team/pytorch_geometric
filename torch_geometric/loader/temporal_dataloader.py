2327db70c94753f9c481100140d21f0ec4f40a67
Otávio Calaça <otaviocx@gmail.com>
2022-02-25 22:20:47 -0300

    Update docs

--- torch_geometric/loader/temporal_dataloader.py
+++ torch_geometric/loader/temporal_dataloader.py
@@ -23,7 +23,7 @@ class TemporalDataLoader(DataLoader):
     :class:`torch_geometric.data.TemporalData` to a mini-batch.
 
     Args:
-        dataset (TemporalData): The :obj:`temporalData` from which to load 
+        dataset (TemporalData): The :obj:`temporalData` from which to load
             the data.
         batch_size (int, optional): How many samples per batch to load.
             (default: :obj:`1`)
