cad103c6f8aaca901a262268d1dd3e9d59b44ecf
Unyi Daniel <daniel.unyi.42@gmail.com>
2022-03-03 14:22:29 +0000

    formula in the documentation of get_mesh_Laplacian

--- torch_geometric/utils/get_mesh_laplacian.py
+++ torch_geometric/utils/get_mesh_laplacian.py
@@ -5,18 +5,17 @@ from torch_geometric.utils import add_self_loops
 
 
 def get_mesh_laplacian(pos, face):
-    r""" Computes the mesh Laplacian of the mesh given by :obj:`pos`
-    and :obj:`face`. It is computed as :math:`\mathbf{L_{ij}} =
-    \begin{cases} 
+    """ Computes the mesh Laplacian of the mesh given by :obj:`pos`
+    and :obj:`face`.
+    It is computed as :math:`\mathbf{L}_{ij} = \begin{cases} 
         \frac{\cot \angle_{ikj} + \cot \angle_{ilj}}{2 a_{ij}} &
-        \mbox{if } \{i, j\} \mbox{ is an edge} \\
+        \mbox{if } i, j \mbox{ is an edge,} \\
         \sum_{j \in N(i)}{L_{ij}} &
-        \mbox{if } \{i\} \mbox{ is in the diagonal}
-        \0 \mbox{ otherwise}
-    \end{cases}`
-    where :math:`a_{ij}` is the local area element,
+        \mbox{if } i \mbox{ is in the diagonal,} \\
+        0 \mbox{ otherwise.}
+    \end{cases}` where :math:`a_{ij}` is the local area element,
     i.e. one-third of the neighbouring triangle's area.
-    
+
     Args:
         pos (Tensor): The node positions.
         face (LongTensor): The face indices.
