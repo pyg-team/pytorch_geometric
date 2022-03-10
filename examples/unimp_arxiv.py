e5b0d621d057e7216ff6966f805cc7ee8c62b80c
Padarn Wilson <padarn.wilson@grabtaxi.com>
2022-03-10 10:00:02 +0000

    replicated modell

--- examples/unimp_arxiv.py
+++ examples/unimp_arxiv.py
@@ -8,8 +8,12 @@ import torch_geometric.transforms as T
 import torch_geometric.utils.mask as mask_util
 from torch_geometric.nn import MaskLabel, TransformerConv
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 logger = logging.getLogger(__file__)
 logging.basicConfig(level=logging.INFO)
+
+logger.info(f"using device: {device}")
 
 # Get dataset
 root = os.path.join("..", "data", "OGB")
@@ -19,6 +23,7 @@ dataset = PygNodePropPredDataset(
     transform=T.Compose([T.ToUndirected()]),
 )
 data = dataset[0]
+data.to(device)
 num_classes = 40
 
 # Form masks for labels: mask gives labels being predicted
@@ -28,19 +33,51 @@ valid_mask = mask_util.index_to_mask(split_idx["valid"], size=data.num_nodes)
 test_mask = mask_util.index_to_mask(split_idx["test"], size=data.num_nodes)
 
 # Model parameters
-inner_dim = 16
+inner_dim = 64
 heads = 2
 
 
 class UnimpNet(torch.nn.Module):
-    def __init__(self, feature_size: int, num_classes: int, inner_dim: int,
-                 heads: int):
+    def __init__(
+        self,
+        feature_size: int,
+        num_classes: int,
+        inner_dim: int,
+        heads: int,
+        dropout: float = 0.3,
+    ):
         super().__init__()
 
         self.label_embedding = MaskLabel(num_classes, feature_size)
-        self.conv = TransformerConv(feature_size, inner_dim // heads,
-                                    heads=heads)
-        self.linear = torch.nn.Linear(inner_dim, num_classes)
+
+        self.conv1 = TransformerConv(
+            feature_size,
+            inner_dim // heads,
+            heads=heads,
+            dropout=dropout,
+            beta=True,
+            concat=True
+        )
+        self.norm1 = torch.nn.LayerNorm(inner_dim)
+
+        self.conv2 = TransformerConv(
+            inner_dim,
+            inner_dim // heads,
+            heads=heads,
+            dropout=dropout,
+            beta=True,
+            concat=True
+        )
+        self.norm2 = torch.nn.LayerNorm(inner_dim)
+
+        self.conv3 = TransformerConv(
+            inner_dim,
+            num_classes,
+            heads=heads,
+            dropout=dropout,
+            beta=True,
+            concat=False
+        )
 
     def forward(
         self,
@@ -50,9 +87,14 @@ class UnimpNet(torch.nn.Module):
         label_mask: torch.Tensor,
     ):
         x = self.label_embedding(x, y, label_mask)
-        x = self.conv(x, edge_index)
-        out = self.linear(x)
-        return out
+        x = self.conv1(x, edge_index)
+        x = self.norm1(x)
+        x = torch.nn.functional.relu(x)
+        x = self.conv2(x, edge_index)
+        x = self.norm2(x)
+        x = torch.nn.functional.relu(x)
+        x = self.conv3(x, edge_index)
+        return x
 
     def predictions(self, out: torch.Tensor):
         probs = torch.nn.functional.softmax(out, dim=1)
@@ -64,11 +106,11 @@ class UnimpNet(torch.nn.Module):
 
     def accuracy(self, out: torch.Tensor, labels: torch.Tensor,
                  mask: torch.Tensor):
-        return ((self.predictions(out[mask]) == labels[mask]).sum().float() /
-                float(labels[mask].size(0)))
+        return ((self.predictions(out[mask]) == labels[mask]).sum().float()
+                / float(labels[mask].size(0)))
 
 
-def train(model, optim, epochs=200, label_rate=0.95):
+def train(model, optim, epochs=500, label_rate=0.65):
 
     y = data.y.squeeze()
     for epoch in range(epochs):
@@ -95,6 +137,7 @@ def train(model, optim, epochs=200, label_rate=0.95):
 
         # no grad ops
         with torch.no_grad():
+            model.eval()
             epoc_mask_valid = train_mask
             epoch_mask_test = train_mask | valid_mask
 
@@ -118,6 +161,9 @@ def train(model, optim, epochs=200, label_rate=0.95):
 
 
 model = UnimpNet(data.x.shape[1], num_classes, inner_dim, heads)
-optim = torch.optim.Adam(model.parameters(), lr=0.01)
+model.to(device)
+
+optim = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=0.0005)
+
 
 train(model, optim)
