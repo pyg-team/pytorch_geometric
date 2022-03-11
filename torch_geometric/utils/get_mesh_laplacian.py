2ae2ac95f4929887e62abf19ad453533d079a92c
rusty1s <matthias.fey@tu-dortmund.de>
2022-03-11 23:05:32 +0100

    re-format

--- torch_geometric/utils/get_mesh_laplacian.py
+++ torch_geometric/utils/get_mesh_laplacian.py
@@ -8,28 +8,27 @@ from torch_geometric.utils import add_self_loops, coalesce
 
 
 def get_mesh_laplacian(pos: Tensor, face: Tensor) -> Tuple[Tensor, Tensor]:
-    r"""Computes the mesh Laplacian of the mesh given by
-    :obj:`pos` and :obj:`face`. It is computed as
-    
+    r"""Computes the mesh Laplacian of a mesh given by :obj:`pos` and
+    :obj:`face`.
+    It is computed as
+
     .. math::
-      \mathbf{L}_{ij} = \begin{cases}
-        \frac{\cot \angle_{ikj} + \cot \angle_{ilj}}{2 a_{ij}} &
-        \mbox{if } i, j \mbox{ is an edge,} \\
-        \sum_{j \in N(i)}{L_{ij}} &
-        \mbox{if } i \mbox{ is in the diagonal,} \\
-        0 \mbox{ otherwise.}
+        \mathbf{L}_{ij} = \begin{cases}
+            \frac{\cot \angle_{ikj} + \cot \angle_{ilj}}{2 a_{ij}} &
+            \mbox{if } i, j \mbox{ is an edge} \\
+            \sum_{j \in N(i)}{L_{ij}} &
+            \mbox{if } i \mbox{ is in the diagonal} \\
+            0 \mbox{ otherwise}
       \end{cases}
 
-    where :math:`a_{ij}` is the local area element,
-    i.e. one-third of the neighbouring triangle's area.
+    where :math:`a_{ij}` is the local area element, *i.e.* one-third of the
+    neighbouring triangle's area.
 
     Args:
         pos (Tensor): The node positions.
         face (LongTensor): The face indices.
     """
-
-    assert pos.shape[1] == 3
-    assert face.shape[0] == 3
+    assert pos.size(1) == 3 and face.size(0) == 3
 
     num_nodes = pos.shape[0]
 
@@ -43,7 +42,7 @@ def get_mesh_laplacian(pos: Tensor, face: Tensor) -> Tuple[Tensor, Tensor]:
         area = cross / 6.0  # one-third of a triangle's area is cross / 6.0
         return cot / 2.0, area / 2.0
 
-    # for each triangle face, add all 3 angles
+    # For each triangle face, add all three angles:
     cot_201, area_201 = add_angles(face[2], face[0], face[1])
     cot_012, area_012 = add_angles(face[0], face[1], face[2])
     cot_120, area_120 = add_angles(face[1], face[2], face[0])
@@ -62,14 +61,15 @@ def get_mesh_laplacian(pos: Tensor, face: Tensor) -> Tuple[Tensor, Tensor]:
         torch.stack([face[0], face[1]], dim=0)
     ], dim=1)
 
-    _, cot_weight = coalesce(edge_index, cot_weight)
-    edge_index, area_weight = coalesce(edge_index, area_weight)
+    edge_index, weight = coalesce(edge_index, [cot_weight, area_weight])
+    cot_weight, area_weight = weight
 
-    # compute the diagonal part
+    # Compute the diagonal part:
     row, col = edge_index
     cot_deg = scatter_add(cot_weight, row, dim=0, dim_size=num_nodes)
     area_deg = scatter_add(area_weight, row, dim=0, dim_size=num_nodes)
     deg = cot_deg / area_deg
     edge_index, _ = add_self_loops(edge_index, num_nodes=num_nodes)
     edge_weight = torch.cat([-cot_weight, deg], dim=0)
+
     return edge_index, edge_weight
