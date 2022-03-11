7c7851f617b342534f0f0d7d5ff38213c8622636
rusty1s <matthias.fey@tu-dortmund.de>
2022-03-11 20:53:02 +0100

    typo

--- torch_geometric/testing.py
+++ torch_geometric/testing.py
@@ -2,14 +2,10 @@ import os
 from typing import Callable
 
 
-def onlyFullTest(func: Callable):
-    import pytest
-
-    return pytest.mark.skipif(
-        os.getenv('FULL_TEST', '0') != '1',
-        reason="Skip for fast testing run",
-    )(func)
-
-
 def is_full_test():
     return os.getenv('FULL_TEST', '0') == '1'
+
+
+def onlyFullTest(func: Callable):
+    import pytest
+    return pytest.mark.skipif(not is_full_test(), reason="Fast test run")(func)
