2df40fc153140b348c2eb225fd810aa5ec937cf0
rusty1s <matthias.fey@tu-dortmund.de>
2022-03-19 21:35:06 +0100

    update

--- examples/molecule_datapipe.py
+++ examples/molecule_datapipe.py
@@ -1,46 +1,14 @@
-import time
-from typing import Dict
+import torchdata
 
-import torchdata.datapipes as dp
-
-from torch_geometric.utils import from_smiles
+import torch_geometric.data.datapipes  # noqa
 
 path = '/Users/rusty1s/Downloads/HIV.csv'
 
-
-@dp.functional_datapipe('parse_smiles')
-class SMILESParser(dp.iter.IterDataPipe):
-    def __init__(self, dp):
-        self.dp = dp
-
-    def __iter__(self):
-        for d in self.dp:
-            # TODO: str -> from_smiles
-            # TODO: Dict -> from_smiles(data['smiles']
-            # TODO: NAN values?
-            yield from_smiles(d['smiles'])
-
-
-def filter_fn(data: Dict[str, str]) -> bool:
-    return len(data) > 0
-
-
-datapipe = dp.iter.FileOpener([path])
+datapipe = torchdata.datapipes.iter.FileOpener([path])
 datapipe = datapipe.parse_csv_as_dict()
 datapipe = datapipe.parse_smiles()
 datapipe = datapipe.shuffle()
-datapipe = datapipe.batch_graphs(batch_size=4)
-# datapipe = datapipe.filter(filter_fn)
-# datapipe = datapipe.hello()
+datapipe = datapipe.batch_graphs(batch_size=128)
 
-t = time.perf_counter()
-for i, bla in enumerate(datapipe):
-    print(bla)
-    # if i > 5:
-    #     break
-print(time.perf_counter() - t)
-
-# dp = IterableWrapper([(path, io.StringIO(path))])
-
-# for example in dp.readlines():
-#     print(example)
+for batch in datapipe:
+    print(batch)
--- torch_geometric/data/datapipes.py
+++ torch_geometric/data/datapipes.py
@@ -1,12 +1,12 @@
-import torch
-from torch.utils.data.datapipe import IterDataPipe
-from torch.utils.data.datapipes._decorator import functional_datapipe
+import torchdata
+from torchdata.datapipes.iter import IterDataPipe
 
 from torch_geometric.data import Batch
+from torch_geometric.utils import from_smiles
 
 
-@functional_datapipe('batch_graphs')
-class Batcher(torch.utils.data.datapipe.Batcher):
+@torchdata.datapipes.functional_datapipe('batch_graphs')
+class Batcher(torchdata.datapipes.iter.Batcher):
     def __init__(
         self,
         datapipe: IterDataPipe,
@@ -19,3 +19,16 @@ class Batcher(torch.utils.data.datapipe.Batcher):
             drop_last,
             wrapper_class=Batch.from_data_list,
         )
+
+
+@torchdata.datapipes.functional_datapipe('parse_smiles')
+class SMILESParser(IterDataPipe):
+    def __init__(self, dp: IterDataPipe):
+        self.dp = dp
+
+    def __iter__(self):
+        for d in self.dp:
+            # TODO: str -> from_smiles
+            # TODO: Dict -> from_smiles(data['smiles']
+            # TODO: NAN values?
+            yield from_smiles(d['smiles'])
