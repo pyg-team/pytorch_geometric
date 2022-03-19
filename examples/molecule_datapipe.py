66301c4916bf67939de797ee49935c94631a3399
rusty1s <matthias.fey@tu-dortmund.de>
2022-03-19 21:56:34 +0100

    doc

--- examples/molecule_datapipe.py
+++ examples/molecule_datapipe.py
@@ -1,8 +1,23 @@
+# In this example, you will find a data loading implementation using PyTorch
+# DataPipes (https://pytorch.org/data/) for molecular graph datasets.
+# Here, we make use of two PyG-related DataPipes
+# (implemented in `torch_geometric.data.datapipes`):
+# * parse_smiles: Converts a SMILES string (and an optional target) into a
+#   molecular graph representation of PyG.
+# * batch_graphs: Batches muliple PyG data objects together.
+
+import os.path as osp
+
 import torchdata
 
+# Register functional datapipes:
 import torch_geometric.data.datapipes  # noqa
+from torch_geometric.data import download_url
 
-path = '/Users/rusty1s/Downloads/HIV.csv'
+# Download HIV dataset from 'https://moleculenet.org':
+url = 'https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/HIV.csv'
+root = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data')
+path = download_url(url, root)
 
 datapipe = torchdata.datapipes.iter.FileOpener([path])
 datapipe = datapipe.parse_csv_as_dict()
