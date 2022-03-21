c2dfd0657b42b202cbf5796b4c3a15491a8cb04e
rusty1s <matthias.fey@tu-dortmund.de>
2022-03-21 09:21:49 +0000

    update

---
+++ examples/cpp/main.cpp
@@ -0,0 +1,10 @@
+/* #include <iostream> */
+#include <torch/script.h>
+#include <torch/torch.h>
+/* #include <torchscatter/scatter.h> */
+/* #include <torchsparse/sparse.h> */
+
+#include <iostream>
+/* #include <memory> */
+
+int main() { std::cout << "ok\n"; }
