6eff7f2c38c68ce92434c57a49e543091cea4f0b
Shravankumar <shravankumar147@gmail.com>
2022-03-22 12:25:33 +0530

    'added_test_for_elliptic_dataset' (#4313)
    * 'added_test_for_elliptic_dataset'
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * update
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: rusty1s <matthias.fey@tu-dortmund.de>

---
+++ test/datasets/test_elliptic.py
@@ -0,0 +1,24 @@
+from torch_geometric.testing import onlyFullTest
+
+
+@onlyFullTest
+def test_elliptic_bitcoin_dataset(get_dataset):
+    dataset = get_dataset(name='EllipticBitcoinDataset')
+    assert str(dataset) == 'EllipticBitcoinDataset()'
+    assert len(dataset) == 1
+    assert dataset.num_features == 165
+    assert dataset.num_classes == 2
+
+    data = dataset[0]
+    assert len(data) == 5
+    assert data.x.size() == (203769, 165)
+    assert data.edge_index.size() == (2, 234355)
+    assert data.y.size() == (203769, )
+
+    assert data.train_mask.size() == (203769, )
+    assert data.train_mask.sum() > 0
+    assert data.test_mask.size() == (203769, )
+    assert data.test_mask.sum() > 0
+    assert data.train_mask.sum() + data.test_mask.sum() == 4545 + 42019
+    assert data.y[data.test_mask | data.train_mask].min() == 0
+    assert data.y[data.test_mask | data.train_mask].max() == 1
