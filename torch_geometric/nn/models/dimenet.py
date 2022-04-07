069294130f87f40a797ff90a65eb937ad4779237
arunppsg <arun.palaniappan1999@gmail.com>
2022-04-07 22:36:17 +0530

    added interaction plusplus block

--- torch_geometric/nn/models/dimenet.py
+++ torch_geometric/nn/models/dimenet.py
@@ -219,6 +219,96 @@ class InteractionBlock(torch.nn.Module):
         return h
 
 
+class InteractionPPBlock(torch.nn.Module):
+    """
+    The interaction block transforms each message embedding using
+    multiple residual blocks.
+    """
+    def __init__(self, hidden_channels, int_emb_size, basis_emb_size,
+                 num_spherical, num_radial, num_before_skip, num_after_skip,
+                 act=swish):
+        super(InteractionPPBlock, self).__init__()
+        self.act = act
+
+        # Transformation of Bessel and spherical basis representations
+        self.lin_rbf1 = Linear(num_radial, basis_emb_size, bias=False)
+        self.lin_rbf2 = Linear(basis_emb_size, hidden_channels, bias=False)
+
+        self.lin_sbf1 = Linear(num_spherical * num_radial, basis_emb_size,
+                               bias=False)
+        self.lin_sbf2 = Linear(basis_emb_size, int_emb_size, bias=False)
+
+        # Hidden transformation of input message
+        self.lin_kj = Linear(hidden_channels, hidden_channels)
+        self.lin_ji = Linear(hidden_channels, hidden_channels)
+
+        # Embedding projections for interaction triplets
+        self.lin_down = Linear(hidden_channels, int_emb_size, bias=False)
+        self.lin_up = Linear(int_emb_size, hidden_channels, bias=False)
+        # Residual layers before and after skip connection
+        self.layers_before_skip = torch.nn.ModuleList([
+            ResidualLayer(hidden_channels, act) for _ in range(num_before_skip)
+        ])
+        self.lin = Linear(hidden_channels, hidden_channels)
+        self.layers_after_skip = torch.nn.ModuleList([
+            ResidualLayer(hidden_channels, act) for _ in range(num_before_skip)
+        ])
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        glorot_orthogonal(self.lin_rbf1.weight, scale=2.0)
+        glorot_orthogonal(self.lin_rbf2.weight, scale=2.0)
+        glorot_orthogonal(self.lin_sbf1.weight, scale=2.0)
+        glorot_orthogonal(self.lin_sbf2.weight, scale=2.0)
+
+        glorot_orthogonal(self.lin_kj.weight, scale=2.0)
+        self.lin_kj.bias.data.fill_(0)
+        glorot_orthogonal(self.lin_ji.weight, scale=2.0)
+        self.lin_ji.bias.data.fill_(0)
+
+        glorot_orthogonal(self.lin_down.weight, scale=2.0)
+        glorot_orthogonal(self.lin_up.weight, scale=2.0)
+
+        for res_layer in self.layers_before_skip:
+            res_layer.reset_parameters()
+        glorot_orthogonal(self.lin.weight, scale=2.0)
+        self.lin.bias.data.fill_(0)
+        for res_layer in self.layers_before_skip:
+            res_layer.reset_parameters()
+
+    def forward(self, x, rbf, sbf, idx_kj, idx_ji):
+        # Initial transformation
+        x_ji = self.act(self.lin_ji(x))
+        x_kj = self.act(self.lin_kj(x))
+
+        # Transformation via Bessel basis
+        rbf = self.lin_rbf1(rbf)
+        rbf = self.lin_rbf2(rbf)
+        x_kj = x_kj * rbf
+
+        # Down project embedding and generating triple-interactions
+        x_kj = self.act(self.lin_down(x_kj))
+
+        # Transform via 2D spherical basis
+        sbf = self.lin_sbf1(sbf)
+        sbf = self.lin_sbf2(sbf)
+        x_kj = x_kj[idx_kj] * sbf
+
+        # Aggregate interactions and up-project embeddings
+        x_kj = scatter(x_kj, idx_ji, dim=0, dim_size=x.size(0))
+        x_kj = self.act(self.lin_up(x_kj))
+
+        h = x_ji + x_kj
+        for layer in self.layers_before_skip:
+            h = layer(h)
+        h = self.act(self.lin(h)) + x
+        for layer in self.layers_after_skip:
+            h = layer(h)
+
+        return h
+
+
 class OutputPPBlock(torch.nn.Module):
     def __init__(self, num_radial, hidden_channels, out_emb_channels,
                  out_channels, num_layers, act=swish):
