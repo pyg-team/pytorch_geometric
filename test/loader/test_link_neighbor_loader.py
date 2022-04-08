1fe556a44c793ea5c7f765832184ffc20f706eb1
rusty1s <matthias.fey@tu-dortmund.de>
2022-04-08 20:09:20 +0200

    fix test

--- test/loader/test_link_neighbor_loader.py
+++ test/loader/test_link_neighbor_loader.py
@@ -12,15 +12,16 @@ def get_edge_index(num_src_nodes, num_dst_nodes, num_edges):
 
 
 def unique_edge_pairs(edge_index):
-    return set(map(tuple, edge_index.T.tolist()))
+    return set(map(tuple, edge_index.t().tolist()))
 
 
 @pytest.mark.parametrize('directed', [True, False])
 def test_homogeneous_link_neighbor_loader(directed):
     torch.manual_seed(12345)
 
-    pos_edge_index = get_edge_index(100, 100, 500)
-    neg_edge_index = get_edge_index(100, 100, 500)
+    pos_edge_index = get_edge_index(100, 50, 500)
+    neg_edge_index = get_edge_index(100, 50, 500)
+    neg_edge_index[1, :] += 50
 
     edge_label_index = torch.cat([pos_edge_index, neg_edge_index], dim=-1)
     edge_label = torch.cat([torch.ones(500), torch.zeros(500)], dim=0)
@@ -31,7 +32,7 @@ def test_homogeneous_link_neighbor_loader(directed):
     data.x = torch.arange(100)
     data.edge_attr = torch.arange(500)
 
-    loader = LinkNeighborLoader(data, num_neighbors=[5] * 2, batch_size=20,
+    loader = LinkNeighborLoader(data, num_neighbors=[-1] * 2, batch_size=20,
                                 edge_label_index=edge_label_index,
                                 edge_label=edge_label, directed=directed,
                                 shuffle=True)
@@ -50,17 +51,17 @@ def test_homogeneous_link_neighbor_loader(directed):
         assert batch.edge_attr.min() >= 0
         assert batch.edge_attr.max() < 500
 
-        # Assert positive samples were present in original graph:
+        # Assert positive samples were present in the original graph:
         edge_index = unique_edge_pairs(batch.edge_index)
         edge_label_index = batch.edge_label_index[:, batch.edge_label == 1]
         edge_label_index = unique_edge_pairs(edge_label_index)
-        # assert len(edge_index | edge_label_index) == len(edge_index)
+        assert len(edge_index | edge_label_index) == len(edge_index)
 
-        # Assert negative samples were not present in original graph
+        # Assert negative samples were not present in the original graph:
         edge_index = unique_edge_pairs(batch.edge_index)
         edge_label_index = batch.edge_label_index[:, batch.edge_label == 0]
         edge_label_index = unique_edge_pairs(edge_label_index)
-        # assert len(edge_index & edge_label_index) == len(edge_label_index)
+        assert len(edge_index & edge_label_index) == 0
 
 
 @pytest.mark.parametrize('directed', [True, False])
