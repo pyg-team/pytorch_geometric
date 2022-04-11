ea33500317484b7f1862f323c5272b27c0da013a
tim <dbwtimteo@outlook.com>
2022-04-11 21:37:45 +0800

    lint for flake8

--- torch_geometric/nn/conv/disen_conv.py
+++ torch_geometric/nn/conv/disen_conv.py
@@ -7,7 +7,7 @@ from torch_sparse import SparseTensor, matmul
 
 from torch_geometric.nn.conv import MessagePassing
 from torch_geometric.nn.dense.linear import Linear
-from torch_geometric.typing import OptPairTensor, Size
+from torch_geometric.typing import Size, OptPairTensor
 
 
 class DisenConv(MessagePassing):
@@ -19,7 +19,8 @@ class DisenConv(MessagePassing):
     vector disentangled into :obj:`K` factors:
 
     .. math::
-        \mathbf{y}_{u}=\left[\mathbf{c}_{1}, \mathbf{c}_{2}, \ldots, \mathbf{c}_{K}\right].
+        \mathbf{y}_{u}=\left[\mathbf{c}_{1}, \mathbf{c}_{2},
+        \ldots, \mathbf{c}_{K}\right].
 
     DisenConv scans the input feature vectors from :obj:`K`
     perspectives:
@@ -35,14 +36,16 @@ class DisenConv(MessagePassing):
     The :math:`\mathbf{c}_{k}` is initialized with :math:`\mathbf{z}_{u, k}`
     Later, the following process is iterated for :obj:`T` times.
 
-    First, the propagation weight from :math:`v\in \mathcal{N}(u)` is computed via:
+    First, the propagation weight from :math:`v\in \mathcal{N}(u)`
+    is computed via:
 
     .. math::
         &p_{v, k} \leftarrow \mathbf{z}_{v, k}^{\top}
         \mathbf{c}_{k} / \tau, \forall k=1,2, \ldots, K
 
         &{\left[p_{v, 1} \ldots p_{v, K}\right] \leftarrow
-        \operatorname{softmax}\left(\left[p_{v, 1} \ldots p_{v, K}\right]\right)}
+        \operatorname{softmax}\left(\left[p_{v, 1}
+        \ldots p_{v, K}\right]\right)}
 
     Then, the message passing is conducted for :obj:`K` factors:
 
@@ -50,20 +53,20 @@ class DisenConv(MessagePassing):
         &\mathbf{c}_{k} \leftarrow \mathbf{Z}_{u, k}+
         \sum_{v\in\mathcal{N}(u)} p_{v, k} \mathbf{Z}_{v, k}
 
-        &\mathbf{c}_{k} \leftarrow \mathbf{c}_{k} /
-        \left\|\mathbf{c}_{k}\right\|_{2} \forall k=1,2, \ldots, K.
+        &\mathbf{c}_{k} \leftarrow \mathbf{c}_{k} / \left\|
+        \mathbf{c}_{k}\right\|_{2} \forall k=1,2, \ldots, K .
 
 
     Args:
-      in_channels (int): The number of input channels.
-      out_channels (int): The number of channels of the output feature.
-      K (int): The number of channels in the output.
-      T (int): The number of iterations. Defaults to 5
-      tao (float): The time decay parameter.
-      separate_channels (List[int]): This is a list of integers, which
-                specifies the number of channels for each factor.
-                 If set to :obj:`None`, the channels will be automatically
-                 split as even as possible.
+        in_channels (int): The number of input channels.
+        out_channels (int): The number of channels of the output feature.
+        K (int): The number of channels in the output.
+        T (int): The number of iterations. Defaults to 5
+        tao (float): The time decay parameter.
+        separate_channels (List[int]): This is a list of integers, which
+            specifies the number of channels for each factor.
+            If set to :obj:`None`, the channels will be automatically
+            split as even as possible.
 
     Shapes:
         - **input:**
@@ -76,20 +79,21 @@ class DisenConv(MessagePassing):
         - **output:**
             node features :math:`(|\mathcal{V}|, F_{out})`
             if you want the embeddings of all nodes in :math:`\mathcal{V}` ;
-            :math:`(|\mathcal{V_t}|, F_{out}))`
+            :math:`(|\mathcal{V_t}|, F_{out})`
             if you only want the embeddings of the target nodes in
             :math:`\mathcal{V_t}` .
 
     """
+
     def __init__(
-        self,
-        in_channels: int,
-        out_channels: int,
-        K: int = None,
-        T: int = 5,
-        tao: float = 1.0,
-        separate_channels: List[int] = None,
-        **kwargs,
+            self,
+            in_channels: int,
+            out_channels: int,
+            K: int = None,
+            T: int = 5,
+            tao: float = 1.0,
+            separate_channels: List[int] = None,
+            **kwargs,
     ):
         kwargs.setdefault("aggr", "add")
         super().__init__(**kwargs)
@@ -106,7 +110,8 @@ class DisenConv(MessagePassing):
         if separate_channels is None:
             if K is None:
                 raise ValueError(
-                    "`K` must be assigned when `separate_channels` is None")
+                    "You have to choose a `K` when `separate_channels` is None"
+                )
             self.separate_channels = [out_channels // K] * K
             self.separate_channels[-1] += out_channels % K
             self.K = K
@@ -122,7 +127,7 @@ class DisenConv(MessagePassing):
         self.lin_projector.reset_parameters()
 
     def get_projector(
-        self, separate=False
+            self, separate: bool = False
     ) -> Union[Tuple[Tensor, Tensor], Tuple[Tuple[Tensor], Tuple[Tensor]]]:
         projector = self.lin_projector.weight
         projector_bias = self.lin_projector.bias
@@ -139,27 +144,28 @@ class DisenConv(MessagePassing):
         return zs
 
     def update_p(
-        self,
-        zs: List[Tensor],
-        cs: List[Tensor],
-        dst_nodes: Union[Tensor, List[int]],
-        src_nodes: Union[Tensor, List[int]],
+            self,
+            zs: List[Tensor],
+            cs: List[Tensor],
+            dst_nodes: Union[Tensor, List[int]],
+            src_nodes: Union[Tensor, List[int]],
     ) -> Tensor:
         assert len(dst_nodes) == len(src_nodes)
         pout = [
-            torch.sum(cs[k][dst_nodes] * zs[k][src_nodes],
-                      dim=-1).div_(self.tao) for k in range(self.K)
+            torch.sum(cs[k][dst_nodes] *
+                      zs[k][src_nodes], dim=-1).div_(self.tao)
+            for k in range(self.K)
         ]
         pout = F.softmax(torch.stack(pout), dim=0)
         return pout
 
     def forward(
-        self,
-        x: Union[Tensor, OptPairTensor],
-        edge_index: Union[SparseTensor, Tensor],
-        size: Size = None,
+            self,
+            x: Union[Tensor, OptPairTensor],
+            edge_index: Union[SparseTensor, Tensor],
+            size: Size = None,
     ) -> Tensor:
-
+        """"""
         if isinstance(edge_index, Tensor):
             src_nodes, dst_nodes = edge_index
         elif isinstance(edge_index, SparseTensor):
@@ -185,10 +191,14 @@ class DisenConv(MessagePassing):
         for i in range(self.T):
             p = self.update_p(z_src, c_dst, dst_nodes, src_nodes)
             for k in range(self.K):
-                graph = (edge_index.set_value(p[k], layout="coo") if
-                         isinstance(edge_index, SparseTensor) else edge_index)
-                ck = self.propagate(graph, x=(z_src[k], None),
-                                    edge_weight=p[k], size=size)
+                graph = (
+                    edge_index.set_value(p[k], layout="coo")
+                    if isinstance(edge_index, SparseTensor)
+                    else edge_index
+                )
+                ck = self.propagate(
+                    graph, x=(z_src[k], None), edge_weight=p[k], size=size
+                )
                 ck = ck + z_dst[k]
                 c_dst[k] = ck / ck.norm(dim=-1, keepdim=True)
 
@@ -203,6 +213,8 @@ class DisenConv(MessagePassing):
         return edge_weight.view(-1, 1) * x_j
 
     def __repr__(self) -> str:
-        return (f"{self.__class__.__name__}({self.in_channels}, "
-                f"{self.out_channels}, "
-                f"[{self.K}]disentangled_channels={self.separate_channels})")
+        return (
+            f"{self.__class__.__name__}({self.in_channels}, "
+            f"{self.out_channels}, "
+            f"[{self.K}]disentangled_channels={self.separate_channels})"
+        )
