e48cc18d19d9dea2cd96c4d9acc4b4121223961c
tim <dbwtimteo@outlook.com>
2022-04-11 10:25:35 +0800

    feat(nn.conv): support `DisenConv`

--- torch_geometric/nn/conv/disen_conv.py
+++ torch_geometric/nn/conv/disen_conv.py
@@ -1,4 +1,4 @@
-from typing import List, Optional, Tuple, Union
+from typing import List, Tuple, Union
 
 import torch
 import torch.nn.functional as F
@@ -7,12 +7,52 @@ from torch_sparse import SparseTensor, matmul
 
 from torch_geometric.nn.conv import MessagePassing
 from torch_geometric.nn.dense.linear import Linear
+from torch_geometric.typing import Size, OptPairTensor
 
 
 class DisenConv(MessagePassing):
     r"""The graph convolutional operator from the `"Disentangled graph
     convolutional networks"
-    <http://proceedings.mlr.press/v97/ma19a.html>`_ paper
+    <http://proceedings.mlr.press/v97/ma19a.html>`_ paper。
+    Given the feature vectors of centarl node :obj:`u` and its neighbors
+    :math:`\mathcal{N}(u)`, this layer outputs the central node feature
+    vector disentangled into :obj:`K` factors:
+
+    .. math::
+        \mathbf{y}_{u}=\left[\mathbf{c}_{1}, \mathbf{c}_{2}, \ldots, \mathbf{c}_{K}\right].
+
+    DisenConv scans the input feature vectors from :obj:`K`
+    perspectives:
+
+    .. math::
+        \mathbf{z}_{i, k} \leftarrow \sigma\left(\mathbf{W}_{k}^{\top}
+        \mathbf{x}_{i}+\mathbf{b}_{k}\right), i \in\{u\} \cup \mathcal{N}(u)
+
+    where :math:`\mathbf{W}_{k}` and :math:`\mathbf{b}_{k}` project the
+    features :math:`\mathbf{x}_{i}` of node :math:`i` into the k-th
+    aspect.
+
+    The :math:`\mathbf{c}_{k}` is initialized with :math:`\mathbf{z}_{u, k}`
+    Later, the following process is iterated for :obj:`T` times.
+
+    First, the propagation weight from :math:`v\in \mathcal{N}(u)` is computed via:
+
+    .. math::
+        &p_{v, k} \leftarrow \mathbf{z}_{v, k}^{\top}
+        \mathbf{c}_{k} / \tau, \forall k=1,2, \ldots, K
+
+        &{\left[p_{v, 1} \ldots p_{v, K}\right] \leftarrow
+        \operatorname{softmax}\left(\left[p_{v, 1} \ldots p_{v, K}\right]\right)}
+
+    Then, the message passing is conducted for :obj:`K` factors:
+
+    .. math::
+        &\mathbf{c}_{k} \leftarrow \mathbf{Z}_{u, k}+
+        \sum_{v\in\mathcal{N}(u)} p_{v, k} \mathbf{Z}_{v, k}
+
+        &\mathbf{c}_{k} \leftarrow \mathbf{c}_{k} /
+        \left\|\mathbf{c}_{k}\right\|_{2} \forall k=1,2, \ldots, K.
+
 
     Args:
       in_channels (int): The number of input channels.
@@ -22,30 +62,35 @@ class DisenConv(MessagePassing):
       tao (float): The time decay parameter.
       separate_channels (List[int]): This is a list of integers, which
                 specifies the number of channels for each factor.
-      weight_initializer (Optional[str]): The initializer for
-                the weights of the linear projector.
-      bias_initializer (Optional[str]): Initializer for the bias vector.
-
+                 If set to :obj:`None`, the channels will be automatically
+                 split as even as possible.
 
     Shapes:
         - **input:**
-            node features :math:`(|\mathcal{V}|, F_{in})`,
+            node features :math:`(|\mathcal{V}|, F_{in})`
+            if you want the embeddings of all nodes in :math:`\mathcal{V}` ;
+            :math:`((|\mathcal{V_s}|, F_{in}), (|\mathcal{V_t}|, F_{in}))`
+            if you only want the embeddings of the target nodes in
+            :math:`\mathcal{V_t}`
             edge indices :math:`(2, |\mathcal{E}|)`,
-            batch_size :math:`B`
-        - **output:** node features :math:`(B, F_{out})`
+        - **output:**
+            node features :math:`(|\mathcal{V}|, F_{out})`
+            if you want the embeddings of all nodes in :math:`\mathcal{V}` ;
+            :math:`(|\mathcal{V_t}|, F_{out}))`
+            if you only want the embeddings of the target nodes in
+            :math:`\mathcal{V_t}` .
+
     """
 
     def __init__(
-            self,
-            in_channels: int,
-            out_channels: int,
-            K: int = None,
-            T: int = 5,
-            tao: float = 1.0,
-            separate_channels: List[int] = None,
-            weight_initializer: Optional[str] = None,
-            bias_initializer: Optional[str] = None,
-            **kwargs,
+        self,
+        in_channels: int,
+        out_channels: int,
+        K: int = None,
+        T: int = 5,
+        tao: float = 1.0,
+        separate_channels: List[int] = None,
+        **kwargs,
     ):
         kwargs.setdefault("aggr", "add")
         super().__init__(**kwargs)
@@ -73,27 +118,19 @@ class DisenConv(MessagePassing):
             self.separate_channels = separate_channels
             self.K = len(separate_channels)
 
-        self.lin_projector = Linear(
-            in_channels,
-            out_channels,
-            bias=True,
-            weight_initializer=weight_initializer,
-            bias_initializer=bias_initializer,
-        )
+        self.lin_projector = Linear(in_channels, out_channels, bias=True)
 
     def reset_parameters(self):
         self.lin_projector.reset_parameters()
 
     def get_projector(
-            self, separate=False
-    ) -> Tuple[Union[Tuple[Tensor], Tensor], Union[Tuple[Tensor], Tensor]]:
+        self, separate=False
+    ) -> Union[Tuple[Tensor, Tensor], Tuple[Tuple[Tensor], Tuple[Tensor]]]:
         projector = self.lin_projector.weight
         projector_bias = self.lin_projector.bias
         if separate:
-            projector = torch.split(projector,
-                                    self.separate_channels, dim=0)
-            projector_bias = torch.split(projector_bias,
-                                         self.separate_channels, dim=0)
+            projector = torch.split(projector, self.separate_channels, dim=0)
+            projector_bias = torch.split(projector_bias, self.separate_channels, dim=0)
         return projector, projector_bias
 
     def project2aspects(self, features: Tensor) -> List[Tensor]:
@@ -103,22 +140,25 @@ class DisenConv(MessagePassing):
         return zs
 
     def update_p(
-            self,
-            zs: List[Tensor],
-            cs: List[Tensor],
-            dst_nodes: Union[Tensor, List[int]],
-            src_nodes: Union[Tensor, List[int]],
+        self,
+        zs: List[Tensor],
+        cs: List[Tensor],
+        dst_nodes: Union[Tensor, List[int]],
+        src_nodes: Union[Tensor, List[int]],
     ) -> Tensor:
         assert len(dst_nodes) == len(src_nodes)
-        pout = [torch.sum(cs[k][dst_nodes] * zs[k][src_nodes],
-                          dim=-1).div_(self.tao) for k in range(self.K)]
+        pout = [
+            torch.sum(cs[k][dst_nodes] * zs[k][src_nodes], dim=-1).div_(self.tao)
+            for k in range(self.K)
+        ]
         pout = F.softmax(torch.stack(pout), dim=0)
         return pout
 
     def forward(
-            self, x: Tensor,
-            edge_index: Union[SparseTensor, Tensor],
-            batch_size: int = None
+        self,
+        x: Union[Tensor, OptPairTensor],
+        edge_index: Union[SparseTensor, Tensor],
+        size: Size = None,
     ) -> Tensor:
 
         if isinstance(edge_index, Tensor):
@@ -127,34 +167,43 @@ class DisenConv(MessagePassing):
             dst_nodes, src_nodes, _ = edge_index.coo()
         else:
             raise TypeError(f"{type(edge_index)} not a valid graph type")
-        bs = batch_size if batch_size is not None \
-            else dst_nodes.unique().numel()
 
-        Z = self.project2aspects(x)
-        # target nodes always placed first
-        C = [chunk[:bs].clone() for chunk in Z]
+        if isinstance(x, Tensor):
+            x_src, x_dst = x, x
+        else:  # x_src includes the features of central and neighboring nodes
+            x_src, x_dst = x[0], x[1]
+
+        z_src = self.project2aspects(x_src)
+        if x_dst is not None:
+            z_dst = self.project2aspects(x_dst)
+        else:
+            unique_dst_nodes = torch.unique(dst_nodes, sorted=True)
+            z_dst = [chunk[unique_dst_nodes] for chunk in z_src]
+
+        c_dst = [ck.clone() for ck in z_dst]
+        size = (z_src[0].shape[0], z_dst[0].shape[0]) if size is None else size
 
         for i in range(self.T):
-            p = self.update_p(Z, C, dst_nodes, src_nodes)
+            p = self.update_p(z_src, c_dst, dst_nodes, src_nodes)
             for k in range(self.K):
                 graph = (
-                    edge_index[:bs].set_value(p[k], layout="coo")
+                    edge_index.set_value(p[k], layout="coo")
                     if isinstance(edge_index, SparseTensor)
                     else edge_index
                 )
-                ck = (
-                    self.propagate(graph, x=Z[k], edge_weight=p[k])[:bs]
-                    + Z[k][:bs]
+                ck = self.propagate(
+                    graph, x=(z_src[k], None), edge_weight=p[k], size=size
                 )
-                C[k] = ck / ck.norm(dim=-1, keepdim=True)
+                ck = ck + z_dst[k]
+                c_dst[k] = ck / ck.norm(dim=-1, keepdim=True)
 
-        out = torch.cat([C[k] for k in range(self.K)], dim=1)
+        out = torch.cat(c_dst, dim=1)
         return out
 
-    def message_and_aggregate(self, adj_t: SparseTensor, x: Tensor) -> Tensor:  # noqa
-        return matmul(adj_t, x, reduce=self.aggr)
+    def message_and_aggregate(self, adj_t: SparseTensor, x: OptPairTensor) -> Tensor:
+        return matmul(adj_t, x[0], reduce=self.aggr)
 
-    def message(self, x_j: Tensor, edge_weight: Tensor) -> Tensor:  # noqa
+    def message(self, x_j: Tensor, edge_weight: Tensor) -> Tensor:
         return edge_weight.view(-1, 1) * x_j
 
     def __repr__(self) -> str:
