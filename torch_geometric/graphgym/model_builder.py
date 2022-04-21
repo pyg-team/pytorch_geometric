6a2b04741a0d511e8599bd648291e659179da5d9
Aniket Maurya <theaniketmaurya@gmail.com>
2022-04-21 16:40:09 +0530

    Implement Lightning module for GraphGym (#4511)
    * add LitModule
    
    * format
    
    * add pl dep
    
    * add pl to min deps
    
    * add type hint
    
    * apply suggestions
    
    * apply suggestions
    
    * Update setup.py
    
    * fix tests
    
    * graphgym_requires
    
    * graphgym install
    
    Co-authored-by: Matthias Fey <matthias.fey@tu-dortmund.de>

--- torch_geometric/graphgym/model_builder.py
+++ torch_geometric/graphgym/model_builder.py
@@ -1,10 +1,45 @@
+import warnings
+
 import torch
 
 from torch_geometric.graphgym.config import cfg
 from torch_geometric.graphgym.models.gnn import GNN
 from torch_geometric.graphgym.register import network_dict, register_network
 
+try:
+    from pytorch_lightning import LightningModule
+except ImportError:
+    LightningModule = object
+    warnings.warn("Please install 'pytorch_lightning' for using the GraphGym "
+                  "experiment manager via 'pip install pytorch_lightning'")
+
 register_network('gnn', GNN)
+
+
+class GraphGymModule(LightningModule):
+    def __init__(self, dim_in, dim_out, cfg):
+        super().__init__()
+        self.model = network_dict[cfg.model.type](dim_in=dim_in,
+                                                  dim_out=dim_out)
+
+    def forward(self, *args, **kwargs):
+        return self.model(*args, **kwargs)
+
+    @property
+    def encoder(self) -> torch.nn.Module:
+        return self.model.encoder
+
+    @property
+    def mp(self) -> torch.nn.Module:
+        return self.model.mp
+
+    @property
+    def post_mp(self) -> torch.nn.Module:
+        return self.model.post_mp
+
+    @property
+    def pre_mp(self) -> torch.nn.Module:
+        return self.model.pre_mp
 
 
 def create_model(to_device=True, dim_in=None, dim_out=None):
@@ -22,7 +57,7 @@ def create_model(to_device=True, dim_in=None, dim_out=None):
     if 'classification' in cfg.dataset.task_type and dim_out == 2:
         dim_out = 1
 
-    model = network_dict[cfg.model.type](dim_in=dim_in, dim_out=dim_out)
+    model = GraphGymModule(dim_in, dim_out, cfg)
     if to_device:
         model.to(torch.device(cfg.device))
     return model
