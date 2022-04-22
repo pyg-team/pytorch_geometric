bfff998acb3556e482625b78969864511ee0a201
Aniket Maurya <theaniketmaurya@gmail.com>
2022-04-22 23:05:36 +0530

    fix test

--- test/graphgym/test_logger.py
+++ test/graphgym/test_logger.py
@@ -1,6 +1,9 @@
 from torch_geometric.graphgym.logger import Logger, LoggerCallback
+from torch_geometric.testing import withPackage
 
 
+@withPackage('yacs')
+@withPackage('pytorch_lightning')
 def test_logger_callback():
     logger = LoggerCallback()
     assert isinstance(logger.train_logger, Logger)
