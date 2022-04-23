325b51c9337c8c45e8d5a4f7578fea5503b42164
Dongkwan Kim <todoaskit@gmail.com>
2022-04-24 00:30:07 +0900

    Update docs & linting

--- torch_geometric/transforms/add_positional_encoding.py
+++ torch_geometric/transforms/add_positional_encoding.py
@@ -1,4 +1,4 @@
-from typing import Optional, Union
+from typing import Optional
 
 from scipy.sparse.linalg import eigs, eigsh
 import numpy as np
@@ -10,19 +10,12 @@ from torch_sparse import spspmm
 from torch_geometric.data import Data
 from torch_geometric.data.datapipes import functional_transform
 from torch_geometric.transforms import BaseTransform
-from torch_geometric.utils import get_laplacian, to_scipy_sparse_matrix, remove_self_loops
 from torch_geometric.utils.num_nodes import maybe_num_nodes
-
-
-def diagonal_weight(edge_index, edge_weight, num_nodes=None):
-    loop_mask = edge_index[0] == edge_index[1]
-    loop_index = edge_index[:, loop_mask][0]
-    loop_edge_attr = edge_weight[loop_mask]
-
-    N = maybe_num_nodes(edge_index, num_nodes)
-    diag_attr = torch.zeros((N,), dtype=edge_weight.dtype)
-    diag_attr[loop_index] = loop_edge_attr
-    return diag_attr
+from torch_geometric.utils import (
+    get_laplacian,
+    to_scipy_sparse_matrix,
+    remove_self_loops,
+)
 
 
 @functional_transform('add_positional_encoding')
@@ -33,12 +26,11 @@ class AddPositionalEncoding(BaseTransform):
     Args:
         name: (str): The type of the positional encoding
             (:obj:`"laplacian_eigenvector_pe"`, :obj:`"random_walk_pe"`).
-            If set to :obj:`"laplacian_eigenvector_pe"`, the Laplacian
-            eigenvector positional encoding from the `"Benchmarking
-            Graph Neural Networks" <https://arxiv.org/abs/2003.00982>`_
-            paper.
-            If set to :obj:`"random_walk_pe"`, the random walk positional
-            encoding from the `"Graph Neural Networks with Learnable
+            If set to :obj:`"laplacian_eigenvector_pe"`, will add the
+            Laplacian eigenvector positional encoding from the `"Benchmarking
+            Graph Neural Networks" <https://arxiv.org/abs/2003.00982>`_ paper.
+            If set to :obj:`"random_walk_pe"`, will add the random walk
+            positional encoding from the `"Graph Neural Networks with Learnable
             Structural and Positional Representations"
             <https://arxiv.org/abs/2110.07875>`_ paper.
         num_channels: (int): The number of channels. If :attr:`name` is
@@ -47,8 +39,8 @@ class AddPositionalEncoding(BaseTransform):
             :obj:`"random_walk_pe"`, it will be the number of the random walk
             steps.
         method: (str, optional): The method to add positional encoding.
-            If set to :obj:`"attr"`, a new attribute will be added to the
-                data object.
+            If set to :obj:`"attr"`, will be added to the data object
+                as a new attribute.
             If set to :obj:`"cat"`, will be concatenated to the existing
                 :obj:`x` in the data object. (default: :obj:`attr`)
         is_undirected (bool, optional): If set to :obj:`True`, this transform
@@ -56,7 +48,8 @@ class AddPositionalEncoding(BaseTransform):
             computation of the eigenvectors. (default: :obj:`False`)
     """
 
-    def __init__(self, name: str, num_channels: int, method: Optional[str] = 'attr',
+    def __init__(self, name: str, num_channels: int,
+                 method: Optional[str] = 'attr',
                  is_undirected: Optional[bool] = False):
         self.name = name
         self.num_channels = num_channels
@@ -76,19 +69,32 @@ class AddPositionalEncoding(BaseTransform):
             L = to_scipy_sparse_matrix(edge_index, edge_weight, N)
             eig_vals, eig_vecs = eig_fn(L, k=self.num_channels + 1, which='SR',
                                         return_eigenvectors=True)
-            eig_vecs = eig_vecs[:, eig_vals.argsort()]
-            pe = torch.from_numpy(np.real(eig_vecs[:, 1:self.num_channels + 1]))
+            eig_vecs = np.real(eig_vecs[:, eig_vals.argsort()])
+            pe = torch.from_numpy(eig_vecs[:, 1:self.num_channels + 1])
 
-            sign = 2 * torch.randint(0, 2, (self.num_channels, ), dtype=torch.float) - 1
+            sign = -1. + 2 * torch.randint(0, 2, (self.num_channels,),
+                                           dtype=torch.float)
             pe *= sign
 
         elif self.name == 'random_walk_pe':
+
+            def diagonal_weight(edge_index, edge_weight, num_nodes=None):
+                loop_mask = edge_index[0] == edge_index[1]
+                loop_index = edge_index[:, loop_mask][0]
+                loop_edge_attr = edge_weight[loop_mask]
+
+                N = maybe_num_nodes(edge_index, num_nodes)
+                diag_attr = torch.zeros((N,), dtype=edge_weight.dtype)
+                diag_attr[loop_index] = loop_edge_attr
+                return diag_attr
+
             # Compute D^{-1} A.
-            edge_index, edge_weight = remove_self_loops(data.edge_index, data.edge_weight)
+            edge_index, edge_weight = remove_self_loops(
+                data.edge_index, data.edge_weight)
             if edge_weight is None:
                 edge_weight = torch.ones(edge_index.size(1), dtype=torch.float,
                                          device=edge_index.device)
-            row, col = edge_index[0], edge_index[1]
+            row = edge_index[0]
             deg_inv = 1.0 / scatter_add(edge_weight, row, dim=0, dim_size=N)
             deg_inv.masked_fill_(deg_inv == float('inf'), 0)
             edge_weight = deg_inv[row] * edge_weight
@@ -96,8 +102,9 @@ class AddPositionalEncoding(BaseTransform):
             rw_index, rw_weight = edge_index, edge_weight
             pe_list = [diagonal_weight(rw_index, rw_weight)]
             for _ in range(self.num_channels - 1):
-                rw_index, rw_weight = spspmm(rw_index, rw_weight, edge_index, edge_weight,
-                                             N, N, N, coalesced=True)
+                rw_index, rw_weight = spspmm(
+                    rw_index, rw_weight, edge_index, edge_weight,
+                    N, N, N, coalesced=True)
                 pe_list.append(diagonal_weight(rw_index, rw_weight))
             pe = torch.stack(pe_list, dim=-1)
 
@@ -108,3 +115,6 @@ class AddPositionalEncoding(BaseTransform):
                 x = store.x.view(-1, 1) if store.x.dim() == 1 else store.x
                 store.x = torch.cat([x, pe.to(x.device, x.dtype)], dim=-1)
         return data
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}(name={self.name})'
