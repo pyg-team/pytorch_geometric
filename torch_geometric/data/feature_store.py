a5c4f1c48bab91060089094368fa5793e03ea069
rusty1s <matthias.fey@tu-dortmund.de>
2022-04-29 13:36:10 +0200

    pass

--- torch_geometric/data/feature_store.py
+++ torch_geometric/data/feature_store.py
@@ -1,16 +1,15 @@
 r"""
 This class defines the abstraction for a backend-agnostic feature store. The
-goal of a feature store is to abstract away all node and edge feature memory
+goal of the feature store is to abstract away all node and edge feature memory
 management so that varying implementations can allow for independent scale-out.
 
 This particular feature store abstraction makes a few key assumptions:
-    * The features we care about storing are node and edge features of a graph.
-        To this end, the attributes that the feature store supports include a
-        group_name (e.g. a heterogeneous node name, a heterogeneous edge type,
-        etc.), an attr_name (which defines the name of the feature tensor,
-        e.g. `x`, `edge_attr`, etc.), and an index.
-    * A feature can be uniquely identified from any associated attributes
-        specified in :class:`TensorAttr`.
+* The features we care about storing are node and edge features of a graph.
+  To this end, the attributes that the feature store supports include a
+  `group_name` (e.g. a heterogeneous node name or a heterogeneous edge type),
+  an `attr_name` (e.g. `x` or `edge_attr`), and an index.
+* A feature can be uniquely identified from any associated attributes specified
+  in `TensorAttr`.
 
 It is the job of a feature store implementor class to handle these assumptions
 properly. For example, a simple in-memory feature store implementation may
@@ -43,14 +42,14 @@ IndexType = Union[torch.Tensor, np.ndarray, slice, int]
 
 @dataclass
 class TensorAttr(CastMixin):
-    r"""Defines the attributes of a :obj:`FeatureStore` tensor; in particular,
+    r"""Defines the attributes of a class:`FeatureStore` tensor; in particular,
     all the parameters necessary to uniquely identify a tensor from the feature
     store.
 
     Note that the order of the attributes is important; this is the order in
     which attributes must be provided for indexing calls. Feature store
     implementor classes can define a different ordering by overriding
-    TensorAttr.__init__.
+    :meth:`TensorAttr.__init__`.
     """
 
     # The group name that the tensor corresponds to. Defaults to None.
@@ -64,32 +63,28 @@ class TensorAttr(CastMixin):
 
     # Convenience methods #####################################################
 
-    def is_set(self, attr):
+    def is_set(self, key: str) -> bool:
         r"""Whether an attribute is set in :obj:`TensorAttr`."""
-        assert attr in self.__dataclass_fields__
-        return getattr(self, attr) != _field_status.UNSET
+        assert key in self.__dataclass_fields__
+        return getattr(self, key) != _field_status.UNSET
 
-    def is_fully_specified(self):
+    def is_fully_specified(self) -> bool:
         r"""Whether the :obj:`TensorAttr` has no unset fields."""
-        return all([
-            getattr(self, field) != _field_status.UNSET
-            for field in self.__dataclass_fields__
-        ])
+        return all([self.is_set(key) for key in self.__dataclass_fields__])
 
     def fully_specify(self):
-        r"""Sets all UNSET fields to None."""
-        for field in self.__dataclass_fields__:
-            if getattr(self, field) == _field_status.UNSET:
-                setattr(self, field, None)
+        r"""Sets all :obj:`UNSET` fields to :obj:`None`."""
+        for key in self.__dataclass_fields__:
+            if not self.is_set(key):
+                setattr(self, key, None)
         return self
 
     def update(self, attr: 'TensorAttr'):
-        r"""Updates an :obj:`TensorAttr` with set attributes from another
-        :obj:`TensorAttr`."""
-        for field in self.__dataclass_fields__:
-            val = getattr(attr, field)
-            if val != _field_status.UNSET:
-                setattr(self, field, val)
+        r"""Updates an :class:`TensorAttr` with set attributes from another
+        :class:`TensorAttr`."""
+        for key in self.__dataclass_fields__:
+            if attr.is_set(key):
+                setattr(self, key, getattr(attr, key))
 
 
 class AttrView(CastMixin):
@@ -98,9 +93,9 @@ class AttrView(CastMixin):
     reference to the backing feature store as well as a :class:`TensorAttr`
     object that represents the view's state.
 
-    Users can create views either using the :obj:`AttrView` constructor,
-    :obj:`FeatureStore.view`, or by incompletely indexing a feature store. For
-    example, the following calls all create views:
+    Users can create views either using the :class:`AttrView` constructor,
+    :meth:`FeatureStore.view`, or by incompletely indexing a feature store.
+    For example, the following calls all create views:
 
     .. code-block:: python
 
@@ -116,7 +111,6 @@ class AttrView(CastMixin):
         store[group_name]()
         store[group_name].feat[index]
         store[group_name, feat][index]
-
     """
     def __init__(self, store: 'FeatureStore', attr: TensorAttr):
         self.__dict__['_store'] = store
@@ -124,11 +118,11 @@ class AttrView(CastMixin):
 
     # Advanced indexing #######################################################
 
-    def __getattr__(self, key: Any) -> 'AttrView':
-        r"""Sets the first unset field of the backing :obj:`TensorAttr` object
-        to the attribute. This allows for :obj:`AttrView` to be indexed by
+    def __getattr__(self, key: Any) -> Union['AttrView', FeatureTensorType]:
+        r"""Sets the first unset field of the backing :class:`TensorAttr` object
+        to the attribute. This allows for :class:`AttrView` to be indexed by
         different values of attributes, in order. In particular, for a feature
-        store that we want to index by `group_name` group and `attr_name` attr,
+        store that we want to index by :obj:`group_name` and :obj:`attr_name`,
         the following code will do so:
 
         .. code-block:: python
@@ -136,33 +130,34 @@ class AttrView(CastMixin):
             store[group, attr]
             store[group].attr
             store.group.attr
-
         """
         out = copy.copy(self)
 
-        # First attribute that is UNSET
-        attr_name = None
+        # Find the first attribute name that is UNSET:
+        attr_name: Optional[str] = None
         for field in out._attr.__dataclass_fields__:
             if getattr(out._attr, field) == _field_status.UNSET:
                 attr_name = field
                 break
 
         if attr_name is None:
-            raise AttributeError(f"Cannot access attribute {key} on view "
-                                 f"{out} as all attributes have already been "
-                                 f"set in this view.")
+            raise AttributeError(f"Cannot access attribute '{key}' on view "
+                                 f"'{out}' as all attributes have already "
+                                 f"been set in this view")
 
         setattr(out._attr, attr_name, key)
+
         if out._attr.is_fully_specified():
             return out._store.get_tensor(out._attr)
+
         return out
 
     def __getitem__(self, key: Any) -> Union['AttrView', FeatureTensorType]:
-        r"""Sets the first unset field of the backing :obj:`TensorAttr` object
-        to the attribute via indexing. This allows for :obj:`AttrView` to be
+        r"""Sets the first unset field of the backing :class:`TensorAttr` object
+        to the attribute via indexing. This allows for :class:`AttrView` to be
         indexed by different values of attributes, in order. In particular, for
-        a feature store that we want to index by `group_name` group and
-        `attr_name` attr, the following code will do so:
+        a feature store that we want to index by :obj:`group_name` and
+        :obj:`attr_name`, the following code will do so:
 
         .. code-block:: python
 
@@ -174,72 +169,69 @@ class AttrView(CastMixin):
 
     # Setting attributes ######################################################
 
-    def __setattr__(self, key, value):
-        r"""Supports attribute assignment to the backing :obj:`TensorAttr` of
-        an :obj:`AttrView`. This allows for :obj:`AttrView` objects to set
+    def __setattr__(self, key: str, value: Any):
+        r"""Supports attribute assignment to the backing :class:`TensorAttr` of
+        an :class:`AttrView`. This allows for :class:`AttrView` objects to set
         their backing attribute values. In particular, the following operation
-        sets the `index` of an :obj:`AttrView`:
+        sets the :obj:`index` of an :class:`AttrView`:
 
         .. code-block:: python
 
-            view = store.view(TensorAttr(group_name))
-            view.index = torch.Tensor([1, 2, 3])
-
+            view = store.view(group_name)
+            view.index = torch.tensor([1, 2, 3])
         """
         if key not in self._attr.__dataclass_fields__:
-            raise ValueError(f"Attempted to set nonexistent attribute {key} "
+            raise ValueError(f"Attempted to set nonexistent attribute '{key}' "
                              f"(acceptable attributes are "
-                             f"{self._attr.__dataclass_fields__}).")
+                             f"{self._attr.__dataclass_fields__})")
 
         setattr(self._attr, key, value)
 
-    def __setitem__(self, key, value):
-        r"""Supports attribute assignment to the backing :obj:`TensorAttr` of
-        an :obj:`AttrView` via indexing. This allows for :obj:`AttrView`
+    def __setitem__(self, key: str, value: Any):
+        r"""Supports attribute assignment to the backing :class:`TensorAttr` of
+        an :class:`AttrView` via indexing. This allows for :class:`AttrView`
         objects to set their backing attribute values. In particular, the
-        following operation sets the `index` of an :obj:`AttrView`:
+        following operation sets the `index` of an :class:`AttrView`:
 
         .. code-block:: python
 
             view = store.view(TensorAttr(group_name))
             view['index'] = torch.Tensor([1, 2, 3])
-
         """
         self.__setattr__(key, value)
 
     # Miscellaneous built-ins #################################################
 
     def __call__(self) -> FeatureTensorType:
-        r"""Supports :obj:`AttrView` as a callable to force retrieval from
+        r"""Supports :class:`AttrView` as a callable to force retrieval from
         the currently specified attributes. In particular, this passes the
-        current :obj:`TensorAttr` object to a GET call, regardless of whether
-        all attributes have been specified. It returns the result of this
-        call. In particular, the following operation returns a Tensor by
-        performing a GET operation on the backing feature store:
+        current :class:`TensorAttr` object to a GET call, regardless of whether
+        all attributes have been specified. It returns the result of this call.
+        In particular, the following operation returns a tensor by performing a
+        GET operation on the backing feature store:
 
         .. code-block:: python
 
             store[group_name, attr_name]()
-
         """
-        # Set all UNSET values to None if forced execution
+        # Set all UNSET values to None:
         out = copy.copy(self)
         out._attr.fully_specify()
         return out._store.get_tensor(out._attr)
 
-    def __copy__(self):
+    def __copy__(self) -> 'AttrView':
         out = self.__class__.__new__(self.__class__)
         for key, value in self.__dict__.items():
             out.__dict__[key] = value
+        out.__dict__['_attr'] = copy.copy(out.__dict__['_attr'])
         return out
 
-    def __eq__(self, __o: object) -> bool:
-        r"""Compares two :obj:`AttrView` objects by checking equality of their
-        :obj:`FeatureStore` references and :obj:`TensorAttr` attributes."""
-        if not isinstance(__o, AttrView):
+    def __eq__(self, obj: Any) -> bool:
+        r"""Compares two :class:`AttrView` objects by checking equality of their
+        :class:`FeatureStore` references and :class:`TensorAttr` attributes."""
+        if not isinstance(obj, AttrView):
             return False
-
-        return self._store == __o._store and self._attr == __o._attr
+        return self._store == obj._store and self._attr == obj._attr
 
     def __repr__(self) -> str:
         return (f'{self.__class__.__name__}(store={self._store}, '
