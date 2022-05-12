0c5a4ffbb2033f963a67afedd10e5bbce8d86580
Vijay Prakash Dwivedi <vijaydwivedi75@gmail.com>
2022-05-12 12:10:21 +0800

    formatting

--- torch_geometric/datasets/aqsol.py
+++ torch_geometric/datasets/aqsol.py
@@ -15,18 +15,23 @@ from torch_geometric.data import (
 
 
 class AQSOL(InMemoryDataset):
-    r"""The AQSOL dataset from `Benchmarking GNNs <http://arxiv.org/abs/2003.00982>`_,
+    r"""The AQSOL dataset from `Benchmarking GNNs 
+    <http://arxiv.org/abs/2003.00982>`_,
     based on `AqSolDB <https://www.nature.com/articles/s41597-019-0151-1>`
     which is a standardized database of 9,982 molecular graphs with
-    their aqueous solubility values, collected from 9 different data sources.
+    their aqueous solubility values, collected from 9 different data
+    sources.
 
-    The aqueous solubility targets are collected from experimental measurements and standardized
-    to LogS units in AqSolDB. These final values as the property to regress in the AQSOL dataset
-    which is the resultant collection in Benchmarking GNNs after filtering out few graphs
-    with no bonds/edges and a small number of graphs with missing node feature values.
+    The aqueous solubility targets are collected from experimental 
+    measurements and standardized to LogS units in AqSolDB. These final
+    values as the property to regress in the AQSOL dataset which is the
+    resultant collection in Benchmarking GNNs after filtering out few graphs
+    with no bonds/edges and a small number of graphs with missing node 
+    feature values.
 
-    Thus, the total molecular graphs are 9,823. For each molecular graph, the node features are the
-    types of heavy atoms and the edge features are the types of bonds between them, similar as ZINC.
+    Thus, the total molecular graphs are 9,823. For each molecular graph,
+    the node features are the types of heavy atoms and the edge features
+    are the types of bonds between them, similar as ZINC.
 
     Size of Dataset: 9,982 molecules.
     Split: Scaffold split (8:1:1) following same code as OGB.
@@ -35,14 +40,19 @@ class AQSOL(InMemoryDataset):
     Number of (unique) bonds: 5
     Performance Metric: MAE, same as ZINC
 
-    Atom Dict: {'Br': 0, 'C': 1, 'N': 2, 'O': 3, 'Cl': 4, 'Zn': 5, 'F': 6, 'P': 7, 'S': 8, 'Na': 9, 'Al': 10,
-    'Si': 11, 'Mo': 12, 'Ca': 13, 'W': 14, 'Pb': 15, 'B': 16, 'V': 17, 'Co': 18, 'Mg': 19, 'Bi': 20, 'Fe': 21,
-    'Ba': 22, 'K': 23, 'Ti': 24, 'Sn': 25, 'Cd': 26, 'I': 27, 'Re': 28, 'Sr': 29, 'H': 30, 'Cu': 31, 'Ni': 32,
-    'Lu': 33, 'Pr': 34, 'Te': 35, 'Ce': 36, 'Nd': 37, 'Gd': 38, 'Zr': 39, 'Mn': 40, 'As': 41, 'Hg': 42, 'Sb':
-    43, 'Cr': 44, 'Se': 45, 'La': 46, 'Dy': 47, 'Y': 48, 'Pd': 49, 'Ag': 50, 'In': 51, 'Li': 52, 'Rh': 53,
-    'Nb': 54, 'Hf': 55, 'Cs': 56, 'Ru': 57, 'Au': 58, 'Sm': 59, 'Ta': 60, 'Pt': 61, 'Ir': 62, 'Be': 63, 'Ge': 64}
+    Atom Dict: {'Br': 0, 'C': 1, 'N': 2, 'O': 3, 'Cl': 4, 'Zn': 5, 'F': 6,
+    'P': 7, 'S': 8, 'Na': 9, 'Al': 10, 'Si': 11, 'Mo': 12, 'Ca': 13, 'W': 14,
+    'Pb': 15, 'B': 16, 'V': 17, 'Co': 18, 'Mg': 19, 'Bi': 20, 'Fe': 21, 
+    'Ba': 22, 'K': 23, 'Ti': 24, 'Sn': 25, 'Cd': 26, 'I': 27, 'Re': 28,
+    'Sr': 29, 'H': 30, 'Cu': 31, 'Ni': 32, 'Lu': 33, 'Pr': 34, 'Te': 35,
+    'Ce': 36, 'Nd': 37, 'Gd': 38, 'Zr': 39, 'Mn': 40, 'As': 41, 'Hg': 42,
+    'Sb': 43, 'Cr': 44, 'Se': 45, 'La': 46, 'Dy': 47, 'Y': 48, 'Pd': 49,
+    'Ag': 50, 'In': 51, 'Li': 52, 'Rh': 53, 'Nb': 54, 'Hf': 55, 'Cs': 56,
+    'Ru': 57, 'Au': 58, 'Sm': 59, 'Ta': 60, 'Pt': 61, 'Ir': 62, 'Be': 63,
+    'Ge': 64}
 
-    Bond Dict: {'NONE': 0, 'SINGLE': 1, 'DOUBLE': 2, 'AROMATIC': 3, 'TRIPLE': 4}
+    Bond Dict: {'NONE': 0, 'SINGLE': 1, 'DOUBLE': 2, 'AROMATIC': 3, 
+    'TRIPLE': 4}
 
     Args:
         root (string): Root directory where the dataset should be saved.
@@ -56,14 +66,14 @@ class AQSOL(InMemoryDataset):
             being saved to disk. (default: :obj:`None`)
         pre_filter (callable, optional): A function that takes in an
             :obj:`torch_geometric.data.Data` object and returns a boolean
-            value, indicating whether the data object should be included in the
-            final dataset. (default: :obj:`None`)
+            value, indicating whether the data object should be included in
+            the final dataset. (default: :obj:`None`)
     """
 
     url = 'https://www.dropbox.com/s/lzu9lmukwov12kt/aqsol_graph_raw.zip?dl=1'
 
-    def __init__(self, root, split='train', transform=None, pre_transform=None,
-                 pre_filter=None):
+    def __init__(self, root, split='train', transform=None, 
+                 pre_transform=None, pre_filter=None):
         self.name = "AQSOL"
         assert split in ['train', 'val', 'test']
         super().__init__(root, transform, pre_transform, pre_filter)
@@ -106,17 +116,19 @@ class AQSOL(InMemoryDataset):
                 #     Shape of y : [1]
 
                 x = torch.LongTensor(graph[0]).unsqueeze(-1)
-                edge_attr = torch.LongTensor(graph[1])  #.unsqueeze(-1)
+                edge_attr = torch.LongTensor(graph[1])  
                 edge_index = torch.LongTensor(graph[2])
                 y = torch.tensor(graph[3])
 
                 data = Data(edge_index=edge_index)
 
                 if edge_index.shape[1] == 0:
-                    continue  # skipping for graphs with no bonds/edges
+                    continue
+                    # skipping for graphs with no bonds/edges
 
                 if data.num_nodes != len(x):
-                    continue  # cleaning <10 graphs with this discrepancy
+                    continue
+                    # cleaning <10 graphs with this discrepancy
 
                 data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr,
                             y=y)
