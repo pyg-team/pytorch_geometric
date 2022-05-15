09bb8715c749160f218086d72247a7e9f3e09d5a
Tzu-Han Chang <57966875+tzuhanchang@users.noreply.github.com>
2022-05-15 18:30:49 +0100

    Add unbatch to utils
    For unbatching node features, for example, use ```src=data.x``` and ```index =data.x_batch``` (assume ```follow_batch``` is set to ```x```).

---
+++ torch_geometric/utils/unbatch.py
@@ -0,0 +1,17 @@
+import torch
+
+from torch_geometric.utils import degree
+
+def unbatch(src: torch.tensor, index: torch.tensor, dim: int = -1) -> list:
+    r"""Unbatches batched data from DataLoader.
+
+    Args:
+        src (Tensor): source tensor.
+        index (LongTensor): index tensor.
+        dim (int): dimension along which to split the tensor.
+
+    :rtype: :class:`list`
+    """
+    split_sizes = list(map(int,degree(index).tolist()))
+    unbatched = src.split(split_sizes,dim)
+    return unbatched
