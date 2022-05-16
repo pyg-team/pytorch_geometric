d1331a04c0d00c2a8e6505033fe9c732948d8ffe
rusty1s <matthias.fey@tu-dortmund.de>
2022-05-16 08:45:04 -0700

    typo

--- torch_geometric/utils/unbatch.py
+++ torch_geometric/utils/unbatch.py
@@ -15,7 +15,8 @@ def unbatch(src: Tensor, batch: Tensor, dim: int = 0) -> List[Tensor]:
         batch (LongTensor): The batch vector
             :math:`\mathbf{b} \in {\{ 0, \ldots, B-1\}}^N`, which assigns each
             entry in :obj:`src` to a specific example. Must be ordered.
-        dim (int): The dimension along which to split the :obj:`src` tensor.
+        dim (int, optional): The dimension along which to split the :obj:`src`
+            tensor. (default: :obj:`0`)
 
     :rtype: :class:`List[Tensor]`
     """
