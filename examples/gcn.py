811f0b1c1ec79090ff95999c6984bc2dd78efd25
rusty1s <matthias.fey@tu-dortmund.de>
2022-05-16 09:52:44 -0700

    update

--- examples/gcn.py
+++ examples/gcn.py
@@ -6,47 +6,52 @@ import torch.nn.functional as F
 
 import torch_geometric.transforms as T
 from torch_geometric.datasets import Planetoid
-from torch_geometric.nn import ChebConv, GCNConv  # noqa
+from torch_geometric.nn import GCNConv
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', type=str, default='Cora',
+                    choices=['Cora', 'CiteSeer', 'PubMed'])
 parser.add_argument('--use_gdc', action='store_true',
-                    help='Use GDC preprocessing.')
+                    help="Use GDC preprocessing")
+parser.add_argument('--wandb', action='store_true',
+                    help="Track experiment via Weights & Biases")
 args = parser.parse_args()
 
-dataset = 'Cora'
-path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', dataset)
-dataset = Planetoid(path, dataset, transform=T.NormalizeFeatures())
+path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'Planetoid')
+dataset = Planetoid(path, args.dataset, transform=T.NormalizeFeatures())
 data = dataset[0]
 
 if args.use_gdc:
-    gdc = T.GDC(self_loop_weight=1, normalization_in='sym',
-                normalization_out='col',
-                diffusion_kwargs=dict(method='ppr', alpha=0.05),
-                sparsification_kwargs=dict(method='topk', k=128,
-                                           dim=0), exact=True)
-    data = gdc(data)
+    transform = T.GDC(
+        self_loop_weight=1,
+        normalization_in='sym',
+        normalization_out='col',
+        diffusion_kwargs=dict(method='ppr', alpha=0.05),
+        sparsification_kwargs=dict(method='topk', k=128, dim=0),
+        exact=True,
+    )
+    data = transform(data)
 
 
-class Net(torch.nn.Module):
-    def __init__(self):
+class GCN(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, hidden_channels):
         super().__init__()
-        self.conv1 = GCNConv(dataset.num_features, 16, cached=True,
+        self.conv1 = GCNConv(in_channels, hidden_channels, cached=True,
                              normalize=not args.use_gdc)
-        self.conv2 = GCNConv(16, dataset.num_classes, cached=True,
+        self.conv2 = GCNConv(hidden_channels, out_channels, cached=True,
                              normalize=not args.use_gdc)
-        # self.conv1 = ChebConv(data.num_features, 16, K=2)
-        # self.conv2 = ChebConv(16, data.num_features, K=2)
 
-    def forward(self):
-        x, edge_index, edge_weight = data.x, data.edge_index, data.edge_attr
-        x = F.relu(self.conv1(x, edge_index, edge_weight))
+    def forward(self, x, edge_index, edge_weight=None):
+        x = F.dropout(x, training=self.training)
+        x = self.conv1(x, edge_index, edge_weight).relu()
         x = F.dropout(x, training=self.training)
         x = self.conv2(x, edge_index, edge_weight)
-        return F.log_softmax(x, dim=1)
+        return x
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model, data = Net().to(device), data.to(device)
+model = GCN(dataset.num_features, dataset.num_classes, hidden_channels=16)
+model, data = model.to(device), data.to(device)
 optimizer = torch.optim.Adam([
     dict(params=model.conv1.parameters(), weight_decay=5e-4),
     dict(params=model.conv2.parameters(), weight_decay=0)
@@ -56,27 +61,31 @@ optimizer = torch.optim.Adam([
 def train():
     model.train()
     optimizer.zero_grad()
-    F.nll_loss(model()[data.train_mask], data.y[data.train_mask]).backward()
+    out = model(data.x, data.edge_index, data.edge_weight)
+    loss = F.cross_entropy(out[data.train_mask], data.y[data.train_mask])
+    loss.backward()
     optimizer.step()
+    return float(loss)
 
 
 @torch.no_grad()
 def test():
     model.eval()
-    logits, accs = model(), []
-    for _, mask in data('train_mask', 'val_mask', 'test_mask'):
-        pred = logits[mask].max(1)[1]
-        acc = pred.eq(data.y[mask]).sum().item() / mask.sum().item()
-        accs.append(acc)
+    out = model(data.x, data.edge_index, data.edge_weight)
+
+    accs = []
+    for mask in [data.train_mask, data.val_mask, data.test_mask]:
+        pred = out[mask].argmax(dim=-1)
+        accs.append(int((pred == data.y[mask]).sum()) / int(mask.sum()))
     return accs
 
 
 best_val_acc = test_acc = 0
 for epoch in range(1, 201):
-    train()
+    loss = train()
     train_acc, val_acc, tmp_test_acc = test()
     if val_acc > best_val_acc:
         best_val_acc = val_acc
         test_acc = tmp_test_acc
-    print(f'Epoch: {epoch:03d}, Train: {train_acc:.4f}, '
+    print(f'Epoch: {epoch:03d}, Loss: {loss:.4f}, Train: {train_acc:.4f}, '
           f'Val: {best_val_acc:.4f}, Test: {test_acc:.4f}')
