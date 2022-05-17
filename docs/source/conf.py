6c87390d72c81f5df54534b3e9a4a50bf32902ba
Matthias Fey <matthias.fey@tu-dortmund.de>
2022-05-17 06:52:02 -0700

    Integration of `pyg_sphinx_theme` (#4664)
    * update
    
    * readme
    
    * update
    
    * update
    
    * update
    
    * changelog
    
    * reset
    
    * update
    
    * update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,77 +1,42 @@
 import datetime
-import doctest
-
-import sphinx_rtd_theme
 
 import torch_geometric
+
+author = 'PyG Team'
+project = 'pytorch_geometric'
+version = torch_geometric.__version__
+copyright = f'{datetime.datetime.now().year}, {author}'
 
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
-    'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
 ]
 
-autosummary_generate = True
-templates_path = ['_templates']
+html_theme = 'pyg_sphinx_theme'
+html_logo = ('https://raw.githubusercontent.com/pyg-team/pyg_sphinx_theme/'
+             'master/pyg_sphinx_theme/static/img/pyg_logo.png')
+html_favicon = ('https://raw.githubusercontent.com/pyg-team/pyg_sphinx_theme/'
+                'master/pyg_sphinx_theme/static/img/favicon.png')
 
-source_suffix = '.rst'
 master_doc = 'index'
-
-author = 'Matthias Fey'
-project = 'pytorch_geometric'
-copyright = f'{datetime.datetime.now().year}, {author}'
-
-version = torch_geometric.__version__
-release = torch_geometric.__version__
-
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-doctest_default_flags = doctest.NORMALIZE_WHITESPACE
+add_module_names = False
 autodoc_member_order = 'bysource'
+
 intersphinx_mapping = {
     'python': ('https://docs.python.org/', None),
-    # TODO "unknown or unsupported inventory version" error for numpy doc.
-    # 'numpy': ('http://docs.scipy.org/doc/numpy', None),
+    'numpy': ('http://docs.scipy.org/doc/numpy', None),
     'pandas': ('http://pandas.pydata.org/pandas-docs/dev', None),
     'torch': ('https://pytorch.org/docs/master', None),
 }
 
-html_theme_options = {
-    'collapse_navigation': False,
-    'display_version': True,
-    'logo_only': True,
-    'navigation_depth': 2,
-}
-
-html_logo = '_static/img/pyg2.png'
-html_static_path = ['_static']
-html_css_files = ['css/custom.css']
-rst_context = {'torch_geometric': torch_geometric}
-
-add_module_names = False
-
 
 def setup(app):
-    def skip(app, what, name, obj, skip, options):
-        members = [
-            '__init__',
-            '__repr__',
-            '__weakref__',
-            '__dict__',
-            '__module__',
-        ]
-        return True if name in members else skip
+    def rst_jinja_render(app, _, source):
+        rst_context = {'torch_geometric': torch_geometric}
+        source[0] = app.builder.templates.render_string(source[0], rst_context)
 
-    def rst_jinja_render(app, docname, source):
-        src = source[0]
-        rendered = app.builder.templates.render_string(src, rst_context)
-        source[0] = rendered
-
-    app.connect('autodoc-skip-member', skip)
-    app.connect("source-read", rst_jinja_render)
+    app.connect('source-read', rst_jinja_render)
--- docs/source/modules/loader.rst
+++ docs/source/modules/loader.rst
@@ -10,4 +10,3 @@ torch_geometric.loader
 
 .. automodule:: torch_geometric.loader
     :members:
-    :special-members:
