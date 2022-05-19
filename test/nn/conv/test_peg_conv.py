611b7e8b974fb1148b7cd6c4f2ca5d140cf34e92
ZoomWang666 <hr_wang@whu.edu.cn>
2022-05-20 02:45:53 +0800

    update the code

--- test/nn/conv/test_peg_conv.py
+++ test/nn/conv/test_peg_conv.py
@@ -6,37 +6,36 @@ from torch_geometric.testing import is_full_test
 
 
 def test_peg_conv():
-    node_feature = torch.randn(4, 16)
+    x = torch.randn(4, 16) #node_feature
     pos_encoding = torch.randn(4, 16)
-    x = torch.cat((pos_encoding, node_feature), 1)
     edge_index = torch.tensor([[0, 0, 0, 1, 2, 3], [1, 2, 3, 0, 0, 0]])
     row, col = edge_index
     value = torch.rand(row.size(0))
     adj2 = SparseTensor(row=row, col=col, value=value, sparse_sizes=(4, 4))
     adj1 = adj2.set_value(None)
 
-    conv = PEGConv(in_feats_dim=16, pos_dim=16, out_feats_dim=32)
-    assert conv.__repr__() == 'PEGConv(16,16,32)'
-    out1 = conv(x, edge_index)
-    assert out1.size() == (4, 48)
-    assert torch.allclose(conv(x, adj1.t()), out1, atol=1e-6)
-    out2 = conv(x, edge_index, value)
-    assert out2.size() == (4, 48)
-    assert torch.allclose(conv(x, adj2.t()), out2, atol=1e-6)
+    conv = PEGConv(in_channels=16, out_channels=8)
+    assert conv.__repr__() == 'PEGConv(16,8)'
+    out1 = conv(x, pos_encoding, edge_index)
+    assert out1.size() == (4, 8)
+    assert torch.allclose(conv(x, pos_encoding, adj1.t()), out1, atol=1e-6)
+    out2 = conv(x, pos_encoding, edge_index, value)
+    assert out2.size() == (4, 8)
+    assert torch.allclose(conv(x, pos_encoding, adj2.t()), out2, atol=1e-6)
 
     if is_full_test():
-        t = '(Tensor, Tensor, OptTensor) -> Tensor'
+        t = '(Tensor, Tensor, Tensor, OptTensor) -> Tensor'
         jit = torch.jit.script(conv.jittable(t))
-        assert jit(x, edge_index).tolist() == out1.tolist()
-        assert jit(x, edge_index, value).tolist() == out2.tolist()
+        assert jit(x, pos_encoding, edge_index).tolist() == out1.tolist()
+        assert jit(x, pos_encoding, edge_index, value).tolist() == out2.tolist()
 
-        t = '(Tensor, SparseTensor, OptTensor) -> Tensor'
+        t = '(Tensor, Tensor, SparseTensor, OptTensor) -> Tensor'
         jit = torch.jit.script(conv.jittable(t))
-        assert torch.allclose(jit(x, adj1.t()), out1, atol=1e-6)
-        assert torch.allclose(jit(x, adj2.t()), out2, atol=1e-6)
+        assert torch.allclose(jit(x, pos_encoding, adj1.t()), out1, atol=1e-6)
+        assert torch.allclose(jit(x, pos_encoding, adj2.t()), out2, atol=1e-6)
 
     conv.cached = True
-    conv(x, edge_index)
-    assert conv(x, edge_index).tolist() == out1.tolist()
-    conv(x, adj1.t())
-    assert torch.allclose(conv(x, adj1.t()), out1, atol=1e-6)
+    conv(x, pos_encoding, edge_index)
+    assert conv(x, pos_encoding, edge_index).tolist() == out1.tolist()
+    conv(x, pos_encoding, adj1.t())
+    assert torch.allclose(conv(x, pos_encoding, adj1.t()), out1, atol=1e-6)
--- torch_geometric/nn/conv/peg_conv.py
+++ torch_geometric/nn/conv/peg_conv.py
@@ -4,10 +4,11 @@ import torch
 from torch import Tensor, nn
 from torch.nn import Parameter
 from torch_sparse import SparseTensor
-
+from torch_geometric.nn.dense.linear import Linear
 from torch_geometric.nn.conv import MessagePassing
 from torch_geometric.nn.conv.gcn_conv import gcn_norm
 from torch_geometric.typing import Adj, OptTensor, Size, Tensor
+from torch_geometric.nn.inits import zeros, glorot
 
 
 class PEGConv(MessagePassing):
@@ -18,9 +19,8 @@ class PEGConv(MessagePassing):
 
 
     Args:
-        in_feats_dim (int): Size of input node features.
-        pos_dim (int): Size of positional encoding.
-        out_feats_dim (int): Size of output node features.
+        in_channels (int): Size of input node features.
+        out_channels (int): Size of output node features.
         edge_mlp_dim (int):
             We use MLP to make one to one mapping between the
             relative information and edge weight. edge_mlp_dim
@@ -41,24 +41,20 @@ class PEGConv(MessagePassing):
             (default: :obj:`True`)
         bias (bool, optional): If set to :obj:`False`, the layer will not learn
             an additive bias. (default: :obj:`True`)
-        use_formerinfo (bool):
-            Whether to use previous layer's output to update node features.
+
         **kwargs (optional): Additional arguments of
             :class:`torch_geometric.nn.conv.MessagePassing`.
     """
-    def __init__(self, in_feats_dim: int, pos_dim: int, out_feats_dim: int,
+    def __init__(self, in_channels: int, out_channels: int,
                  edge_mlp_dim: int = 32, improved: bool = False,
                  cached: bool = False, add_self_loops: bool = True,
-                 normalize: bool = True, bias: bool = True,
-                 use_formerinfo: bool = False, **kwargs):
+                 normalize: bool = True, bias: bool = True, **kwargs):
 
         kwargs.setdefault('aggr', 'add')
         super(PEGConv, self).__init__(**kwargs)
 
-        self.in_feats_dim = in_feats_dim
-        self.out_feats_dim = out_feats_dim
-        self.pos_dim = pos_dim
-        self.use_formerinfo = use_formerinfo
+        self.in_channels = in_channels
+        self.out_channels = out_channels
         self.improved = improved
         self.cached = cached
         self.add_self_loops = add_self_loops
@@ -68,39 +64,37 @@ class PEGConv(MessagePassing):
         self._cached_edge_index = None
         self._cached_adj_t = None
 
-        self.weight_withformer = Parameter(
-            torch.Tensor(in_feats_dim + in_feats_dim, out_feats_dim))
-        self.weight_noformer = Parameter(
-            torch.Tensor(in_feats_dim, out_feats_dim))
         self.edge_mlp = nn.Sequential(nn.Linear(1, edge_mlp_dim),
                                       nn.Linear(edge_mlp_dim, 1), nn.Sigmoid())
 
+        self.lin = Linear(in_channels, out_channels, bias=False,
+                          weight_initializer='glorot')
+
         if bias:
-            self.bias = Parameter(torch.Tensor(out_feats_dim))
+            self.bias = Parameter(torch.Tensor(out_channels))
         else:
             self.register_parameter('bias', None)
 
         self.reset_parameters()
 
     def reset_parameters(self):
-        self.glorot(self.weight_withformer)
-        self.glorot(self.weight_noformer)
-        self.zeros(self.bias)
+        self.lin.reset_parameters()
+        zeros(self.bias)
         self._cached_edge_index = None
         self._cached_adj_t = None
 
-    def forward(self, x: Tensor, edge_index: Adj,
+    def forward(self, x: Tensor, pos_encoding: Tensor,  edge_index: Adj,
                 edge_weight: OptTensor = None) -> Tensor:
         """"""
 
-        coors, feats = x[:, :self.pos_dim], x[:, self.pos_dim:]
+        #coors, feats = x[:, :self.pos_dim], x[:, self.pos_dim:]
 
         if self.normalize:
             if isinstance(edge_index, Tensor):
                 cache = self._cached_edge_index
                 if cache is None:
                     edge_index, edge_weight = gcn_norm(  # yapf: disable
-                        edge_index, edge_weight, feats.size(self.node_dim),
+                        edge_index, edge_weight, x.size(self.node_dim),
                         self.improved, self.add_self_loops)
                     if self.cached:
                         self._cached_edge_index = (edge_index, edge_weight)
@@ -111,34 +105,32 @@ class PEGConv(MessagePassing):
                 cache = self._cached_adj_t
                 if cache is None:
                     edge_index = gcn_norm(  # yapf: disable
-                        edge_index, edge_weight, feats.size(self.node_dim),
+                        edge_index, edge_weight, x.size(self.node_dim),
                         self.improved, self.add_self_loops)
                     if self.cached:
                         self._cached_adj_t = edge_index
                 else:
                     edge_index = cache
-        else:
-            print('We normalize the adjacency matrix in PEG.')
 
         if isinstance(edge_index, Tensor):
-            rel_coors = coors[edge_index[0]] - coors[edge_index[1]]
+            rel_coors = pos_encoding[edge_index[0]] - pos_encoding[edge_index[1]]
         elif isinstance(edge_index, SparseTensor):
-            rel_coors = coors[
-                edge_index.to_torch_sparse_coo_tensor()._indices()[0]] - coors[
+            rel_coors = pos_encoding[
+                edge_index.to_torch_sparse_coo_tensor()._indices()[0]] - pos_encoding[
                     edge_index.to_torch_sparse_coo_tensor()._indices()[1]]
         rel_dist = (rel_coors**2).sum(dim=-1, keepdim=True)
 
-        # propagate_type: (x: Tensor, edge_weight: OptTensor)
+        
         # pos: l2 norms
-        hidden_out, coors_out = self.propagate(edge_index, x=feats,
-                                               edge_weight=edge_weight,
-                                               pos=rel_dist, coors=coors,
-                                               size=None)
+        out = self.propagate(edge_index, x=x,
+                                        edge_weight=edge_weight,
+                                        pos=rel_dist, size=None)
 
+        out = self.lin(out)
         if self.bias is not None:
-            hidden_out += self.bias
+            out += self.bias
 
-        return torch.cat([coors_out, hidden_out], dim=-1)
+        return out
 
     def message(self, x_i: Tensor, x_j: Tensor, edge_weight: OptTensor,
                 pos) -> Tensor:
@@ -146,51 +138,7 @@ class PEGConv(MessagePassing):
         return x_j if edge_weight is None else (PE_edge_weight *
                                                 edge_weight.view(-1, 1) * x_j)
 
-    def propagate(self, edge_index: Adj, size: Size = None, **kwargs):
-        r"""The initial call to start propagating messages.
-
-        Args:
-            `edge_index` holds the indices of a general (sparse)
-                assignment matrix of shape :obj:`[N, M]`.
-            size (tuple, optional) if none, the size will be inferred
-                and assumed to be quadratic.
-            **kwargs: Any additional data which is needed to construct and
-                aggregate messages, and to update node embeddings.
-        """
-        size = self.__check_input__(edge_index, size)
-        coll_dict = self.__collect__(self.__user_args__, edge_index, size,
-                                     kwargs)
-        msg_kwargs = self.inspector.distribute('message', coll_dict)
-        aggr_kwargs = self.inspector.distribute('aggregate', coll_dict)
-        update_kwargs = self.inspector.distribute('update', coll_dict)
-
-        # get messages
-        m_ij = self.message(**msg_kwargs)
-
-        m_i = self.aggregate(m_ij, **aggr_kwargs)
-
-        coors_out = kwargs["coors"]
-        hidden_feats = kwargs["x"]
-        if self.use_formerinfo:
-            hidden_out = torch.cat([hidden_feats, m_i], dim=-1)
-            hidden_out = hidden_out @ self.weight_withformer
-        else:
-            hidden_out = m_i
-            hidden_out = hidden_out @ self.weight_noformer
-
-        # return tuple
-        return self.update((hidden_out, coors_out), **update_kwargs)
-
-    def glorot(self, tensor):
-        if tensor is not None:
-            stdv = math.sqrt(6.0 / (tensor.size(-2) + tensor.size(-1)))
-            tensor.data.uniform_(-stdv, stdv)
-
-    def zeros(self, tensor):
-        if tensor is not None:
-            tensor.data.fill_(0)
-
     def __repr__(self):
-        return '{}({},{},{})'.format(self.__class__.__name__,
-                                     self.in_feats_dim, self.pos_dim,
-                                     self.out_feats_dim)
+        return '{}({},{})'.format(self.__class__.__name__,
+                                     self.in_channels,
+                                     self.out_channels)
