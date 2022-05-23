91ceef8f33ff5c9864f3d9a527ad6080f52c363c
rusty1s <matthias.fey@tu-dortmund.de>
2022-05-23 09:32:20 +0000

    update

--- examples/pytorch_ignite/gin.py
+++ examples/pytorch_ignite/gin.py
@@ -4,8 +4,13 @@ import ignite
 import ignite.contrib.handlers.tensorboard_logger
 import ignite.contrib.handlers.tqdm_logger
 import torch
+import torch.nn.functional as F
 
-import torch_geometric as pyg
+import torch_geometric.transforms as T
+from torch_geometric import seed_everything
+from torch_geometric.datasets import TUDataset
+from torch_geometric.loader import DataLoader
+from torch_geometric.nn import GIN, MLP, global_add_pool
 
 
 class Model(torch.nn.Module):
@@ -13,103 +18,77 @@ class Model(torch.nn.Module):
                  hidden_channels: int = 64, num_layers: int = 3,
                  dropout: float = 0.5):
         super().__init__()
-        self.gnn = pyg.nn.GIN(in_channels, hidden_channels, num_layers,
-                              dropout=dropout, jk='cat')
 
-        self.classifier = pyg.nn.MLP(
-            [hidden_channels, hidden_channels, out_channels], batch_norm=True,
-            dropout=dropout)
+        self.gnn = GIN(in_channels, hidden_channels, num_layers,
+                       dropout=dropout, jk='cat')
+
+        self.classifier = MLP([hidden_channels, hidden_channels, out_channels],
+                              batch_norm=True, dropout=dropout)
 
     def forward(self, data):
-        x, edge_index, batch = data.x, data.edge_index, data.batch
-        x = self.gnn(x, edge_index)
-        x = pyg.nn.global_add_pool(x, batch)
+        x = self.gnn(data.x, data.edge_index)
+        x = global_add_pool(x, data.batch)
         x = self.classifier(x)
         return x
 
 
 def main():
-    pyg.seed_everything(42)
+    seed_everything(42)
 
-    device = torch.device('cuda')
     root = osp.join('data', 'TUDataset')
-    log_dir = 'runs/example/'
-    dataset = pyg.datasets.TUDataset(
-        root, 'IMDB-BINARY', pre_transform=pyg.transforms.OneHotDegree(135))
-
-    amp_mode = 'amp'
-    batch_size = 16
+    dataset = TUDataset(root, 'IMDB-BINARY', pre_transform=T.OneHotDegree(135))
 
     dataset = dataset.shuffle()
     test_dataset = dataset[:len(dataset) // 10]
     val_dataset = dataset[len(dataset) // 10:2 * len(dataset) // 10]
     train_dataset = dataset[2 * len(dataset) // 10:]
-    loader_trn = pyg.data.DataLoader(
-        dataset=train_dataset,
-        batch_size=batch_size,
-        shuffle=True,
-        num_workers=0,
-        pin_memory=torch.cuda.is_available(),
-    )
-    loader_val = pyg.data.DataLoader(
-        dataset=val_dataset,
-        batch_size=batch_size,
-        shuffle=False,
-        num_workers=0,
-        pin_memory=torch.cuda.is_available(),
-    )
-    loader_test = pyg.data.DataLoader(
-        dataset=test_dataset,
-        batch_size=batch_size,
-        shuffle=False,
-        num_workers=0,
-        pin_memory=torch.cuda.is_available(),
-    )
 
+    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True,
+                              pin_memory=True)
+    val_loader = DataLoader(val_dataset, batch_size=64, pin_memory=True)
+    test_loader = DataLoader(test_dataset, batch_size=64, pin_memory=True)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = Model(dataset.num_node_features, dataset.num_classes).to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
     metrics = {'acc': ignite.metrics.Accuracy()}
 
     def prepare_batch_fn(batch, device, non_blocking):
-        # return a tuple of (x, y) that can be directly runned as
-        # `loss_fn(model(x), y)`
-        # consequently model will have to accept the whole `Data` object
         return (batch.to(device, non_blocking=non_blocking),
                 batch.y.to(device, non_blocking=non_blocking))
 
     trainer = ignite.engine.create_supervised_trainer(
         model=model,
         optimizer=optimizer,
-        loss_fn=torch.nn.functional.cross_entropy,
+        loss_fn=F.cross_entropy,
         device=device,
         prepare_batch=prepare_batch_fn,
         output_transform=lambda x, y, y_pred, loss: loss.item(),
-        amp_mode=amp_mode,
+        amp_mode='amp',
     )
-    # progress bar for each epoch
+
+    # Progress bar for each epoch:
     pbar = ignite.contrib.handlers.tqdm_logger.ProgressBar()
-    pbar.attach(trainer, output_transform=lambda x: {"loss": x})
+    pbar.attach(trainer, output_transform=lambda x: {'loss': x})
 
     def log_metrics(evaluator, loader, tag):
-        assert isinstance(tag, str)
-
         def logger(trainer):
             evaluator.run(loader)
-            metrics = trn_evaluator.state.metrics
-            print(f"{tag:10} Results - Epoch: {trainer.state.epoch:4} ",
-                  end='')
-            for key, value in metrics.items():
-                print(f"{key}: {value:.2f} ", end='')
-            print()
+            print(f'{tag:10} Epoch: {trainer.state.epoch:02d}, '
+                  f'Acc: {evaluator.state.metrics["acc"]:.4f}')
 
         return logger
 
-    trn_evaluator = ignite.engine.create_supervised_evaluator(
-        model=model, metrics=metrics, device=device,
-        prepare_batch=prepare_batch_fn, output_transform=lambda x, y, y_pred:
-        (y_pred, y), amp_mode=amp_mode)
+    train_evaluator = ignite.engine.create_supervised_evaluator(
+        model=model,
+        metrics=metrics,
+        device=device,
+        prepare_batch=prepare_batch_fn,
+        output_transform=lambda x, y, y_pred: (y_pred, y),
+        amp_mode='amp',
+    )
     trainer.on(ignite.engine.Events.EPOCH_COMPLETED(every=1))(log_metrics(
-        trn_evaluator, loader_trn, 'Training'))
+        train_evaluator, train_loader, 'Training'))
 
     val_evaluator = ignite.engine.create_supervised_evaluator(
         model=model,
@@ -117,10 +96,10 @@ def main():
         device=device,
         prepare_batch=prepare_batch_fn,
         output_transform=lambda x, y, y_pred: (y_pred, y),
-        amp_mode=amp_mode,
+        amp_mode='amp',
     )
     trainer.on(ignite.engine.Events.EPOCH_COMPLETED(every=1))(log_metrics(
-        val_evaluator, loader_val, 'Validation'))
+        val_evaluator, val_loader, 'Validation'))
 
     test_evaluator = ignite.engine.create_supervised_evaluator(
         model=model,
@@ -128,74 +107,57 @@ def main():
         device=device,
         prepare_batch=prepare_batch_fn,
         output_transform=lambda x, y, y_pred: (y_pred, y),
-        amp_mode=amp_mode,
+        amp_mode='amp',
     )
     trainer.on(ignite.engine.Events.EPOCH_COMPLETED(every=1))(log_metrics(
-        test_evaluator, loader_test, 'Test'))
+        test_evaluator, test_loader, 'Test'))
 
-    # create an handler to save checkpoints of the
-    # model based on Accuracy on the training set
+    # Save checkpoint of the model based on Accuracy on the validation set:
     checkpoint_handler = ignite.handlers.Checkpoint(
-        {'model': model}, log_dir, n_saved=2,
+        {'model': model},
+        'runs/gin',
+        n_saved=2,
         score_name=list(metrics.keys())[0],
-        filename_pattern="best_trn_epoch-{global_step}" +
-        "-{score_name}-{score}.pt",
-        global_step_transform=ignite.handlers.global_step_from_engine(trainer))
-    trn_evaluator.add_event_handler(ignite.engine.Events.EPOCH_COMPLETED,
-                                    checkpoint_handler)
-
-    # create an handler to save checkpoints of the
-    # model based on Accuracy on the validation set
-    checkpoint_handler = ignite.handlers.Checkpoint(
-        {'model': model}, log_dir, n_saved=2,
-        score_name=list(metrics.keys())[0],
-        filename_pattern="best_val_epoch-{global_step}" +
-        "-{score_name}-{score}.pt",
-        global_step_transform=ignite.handlers.global_step_from_engine(trainer))
+        filename_pattern='best-{global_step}-{score_name}-{score}.pt',
+        global_step_transform=ignite.handlers.global_step_from_engine(trainer),
+    )
     val_evaluator.add_event_handler(ignite.engine.Events.EPOCH_COMPLETED,
                                     checkpoint_handler)
 
-    # create a tensorboard logger to write logs
+    # Create a tensorboard logger to write logs:
     tb_logger = ignite.contrib.handlers.tensorboard_logger.TensorboardLogger(
-        log_dir=osp.join(log_dir, "tb_logs"))
-    # Attach the logger to the trainer to log training loss at each iteration
+        log_dir=osp.join('runs/example', 'tb_logs'))
+
     tb_logger.attach_output_handler(
         trainer, event_name=ignite.engine.Events.ITERATION_COMPLETED,
-        tag="training", output_transform=lambda loss: {"loss_iteration": loss})
-    # Attach the logger to the trainer to log training loss at each epoch
+        tag='training', output_transform=lambda loss: {'loss_iteration': loss})
     tb_logger.attach_output_handler(
         trainer, event_name=ignite.engine.Events.EPOCH_COMPLETED,
-        tag="training", output_transform=lambda loss: {"loss_epoch": loss})
-    # Attach the logger to the train evaluator
-    # to log training metrics at each epoch
+        tag='training', output_transform=lambda loss: {'loss_epoch': loss})
     tb_logger.attach_output_handler(
-        trn_evaluator,
+        train_evaluator,
         event_name=ignite.engine.Events.EPOCH_COMPLETED,
-        tag="training",
+        tag='training',
         metric_names='all',
         global_step_transform=ignite.handlers.global_step_from_engine(trainer),
     )
-    # Attach the logger to the validation evaluator
-    # to log validation metrics at each epoch
     tb_logger.attach_output_handler(
         val_evaluator,
         event_name=ignite.engine.Events.EPOCH_COMPLETED,
-        tag="validation",
+        tag='validation',
         metric_names='all',
         global_step_transform=ignite.handlers.global_step_from_engine(trainer),
     )
-    # Attach the logger to the test evaluator to log test metrics at each epoch
     tb_logger.attach_output_handler(
         test_evaluator,
         event_name=ignite.engine.Events.EPOCH_COMPLETED,
-        tag="test",
+        tag='test',
         metric_names='all',
         global_step_transform=ignite.handlers.global_step_from_engine(trainer),
     )
     tb_logger.close()
 
-    # run the trainer
-    trainer.run(loader_trn, max_epochs=50)
+    trainer.run(train_loader, max_epochs=50)
 
 
 if __name__ == '__main__':
