c6fe443945ad9b4386d71b8b48bab2fc64ba5512
lightaime <lightaime@gmail.com>
2022-05-25 17:56:06 +0300

    Formatted

--- torch_geometric/nn/aggr/__init__.py
+++ torch_geometric/nn/aggr/__init__.py
@@ -1,5 +1,14 @@
-from .aggr import (Aggr, MeanAggr, MaxAggr, MinAggr, SumAggr, SoftmaxAggr,
-                   PowermeanAggr, VarAggr, StdAggr)
+from .aggr import (
+    Aggr,
+    MeanAggr,
+    MaxAggr,
+    MinAggr,
+    SumAggr,
+    SoftmaxAggr,
+    PowermeanAggr,
+    VarAggr,
+    StdAggr,
+)
 
 __all__ = classes = [
     'Aggr',
