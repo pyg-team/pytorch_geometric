d578b15d352d38cd5189b45675468c2151e83e68
Alexandria Barghi <abarghi@nvidia.com>
2022-06-03 19:06:15 +0000

    add example

--- examples/cugraph_data.py
+++ examples/cugraph_data.py
@@ -1,28 +1,30 @@
 import sys
 sys.path += ['/work/pytorch_geometric', '/work/gaas/python']
-
-from torch_geometric.data import Data
-from torch_geometric.data.cugraph import CuGraphData
-from torch_geometric.nn import GCNConv
-
-import numpy as np
-
-import torch
-import torch.nn.functional as F
+# assumes cugraph package is installed
 
 from gaas_client import GaasClient
+from torch_geometric.data import Data
+from torch_geometric.data.cugraph import CuGraphData
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
+import numpy as np
+
 
 client = GaasClient()
-client.load_csv_as_edge_data('/work/cugraph/datasets/karate.csv', ['int32','int32','float32'], ['0','1'])
-
+client.load_csv_as_vertex_data('/tmp/snap/email-Eu-core-department-labels.txt', dtypes=['int32','int32'],vertex_col_name='id', names=['id','dept'])
+client.load_csv_as_edge_data('/tmp/snap/email-Eu-core.txt', ['int32','int32'], ['0','1'])
 cd = CuGraphData(client)
 
+N = 42 # The number of classes
+C = N*4
 
 class GCN(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = GCNConv(8, 16)
-        self.conv2 = GCNConv(16, 2)
+        self.conv1 = GCNConv(cd.num_node_features - 1, C)
+        self.conv2 = GCNConv(C, int(C/2))
+        self.conv3 = GCNConv(int(C/2), N)
 
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
@@ -31,6 +33,10 @@ class GCN(torch.nn.Module):
         x = F.relu(x)
         x = F.dropout(x, training=self.training)
         x = self.conv2(x, edge_index)
+        x = F.relu(x)
+        x = F.dropout(x, training=self.training)
+        x = self.conv3(x, edge_index)
+        x = F.relu(x)
 
         return F.log_softmax(x, dim=1)
 
@@ -39,8 +45,8 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = GCN().to(device)
 
 data = cd.to(device)
-data.x = torch.Tensor(np.ones((34,8))).type(torch.FloatTensor).to(device)
-data.y = torch.Tensor(np.zeros(34)).type(torch.LongTensor).to(device)
+data.x = data.id[-1] # for now have to convert to local torch Tensor
+data.y = data.dept[-1][:,0].to(torch.long) # for now have to convert to local torch Tensor
 
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
 
@@ -55,5 +61,5 @@ for epoch in range(200):
 model.eval()
 pred = model(data).argmax(dim=1)
 correct = (pred == data.y).sum()
-acc = int(correct) / 34
+acc = int(correct) / cd.num_nodes
 print(f'Accuracy: {acc:.4f}')
