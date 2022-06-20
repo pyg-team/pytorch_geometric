4ea680758013c55f4b1df29f4ab1703d34ecd53e
rusty1s <matthias.fey@tu-dortmund.de>
2022-06-20 14:22:12 +0200

    typo

--- test/transforms/test_random_jitter.py
+++ test/transforms/test_random_jitter.py
@@ -21,7 +21,7 @@ def test_random_jitter():
     assert data.pos.max().item() <= 0.1
 
     data = Data(pos=pos)
-    data = RandomJitter(0.1)(data)
+    data = RandomJitter([0.1, 1])(data)
     assert len(data) == 1
     assert data.pos[:, 0].min().item() >= -0.1
     assert data.pos[:, 0].max().item() <= 0.1
