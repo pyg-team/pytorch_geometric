e8c4799c6f1bac4fee89423844dc6b8123acf8a4
Gennaro Gala <gennarogala1396@gmail.com>
2022-06-28 15:06:16 +0200

    Update autoencoder.py
    Handling self-loops is only useful when negative edges are not provided.

--- torch_geometric/nn/models/autoencoder.py
+++ torch_geometric/nn/models/autoencoder.py
@@ -94,10 +94,10 @@ class GAE(torch.nn.Module):
         pos_loss = -torch.log(
             self.decoder(z, pos_edge_index, sigmoid=True) + EPS).mean()
 
-        # Do not include self-loops in negative samples
-        pos_edge_index, _ = remove_self_loops(pos_edge_index)
-        pos_edge_index, _ = add_self_loops(pos_edge_index)
         if neg_edge_index is None:
+            # Do not include self-loops in negative samples
+            pos_edge_index, _ = remove_self_loops(pos_edge_index)
+            pos_edge_index, _ = add_self_loops(pos_edge_index)
             neg_edge_index = negative_sampling(pos_edge_index, z.size(0))
         neg_loss = -torch.log(1 -
                               self.decoder(z, neg_edge_index, sigmoid=True) +
