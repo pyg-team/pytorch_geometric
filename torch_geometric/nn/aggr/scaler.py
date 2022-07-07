5f77394f6293d5e51374e35d93c32cabce8155e1
Matthias Fey <matthias.fey@tu-dortmund.de>
2022-07-07 10:38:07 +0200

    Add TorchScript support to `DegreeScalerAggregation` (#4934)
    * update
    
    * changelog
    
    * update
    
    * update
    
    * update

--- torch_geometric/nn/aggr/scaler.py
+++ torch_geometric/nn/aggr/scaler.py
@@ -4,45 +4,54 @@ import torch
 from torch import Tensor
 
 from torch_geometric.nn.aggr import Aggregation, MultiAggregation
+from torch_geometric.nn.resolver import aggregation_resolver as aggr_resolver
 from torch_geometric.utils import degree
 
 
 class DegreeScalerAggregation(Aggregation):
-    """
-    Class that combines together one or more aggregators and then transforms
-    the result with one or more scalers. The scalers are normalised by the
-    in-degree of the training set and so must be provided at construction.
+    r"""Combines one or more aggregators and transforms its output with one or
+    more scalers as introduced in the `"Principal Neighbourhood Aggregation for
+    Graph Nets" <https://arxiv.org/abs/2004.05718>`_ paper.
+    The scalers are normalised by the in-degree of the training set and so must
+    be provided at time of construction.
+    See :class:`torch_geometric.nn.conv.PNAConv` for more information.
 
     Args:
-        aggrs (list of string or list or Aggregation): The list of
-            aggregations given as :class:`~torch_geometric.nn.aggr.Aggregation`
-            (or any string that automatically resolves to it).
-        scalers (list of str): Set of scaling function identifiers, namely
-                :obj:`"identity"`, :obj:`"amplification"`,
-                :obj:`"attenuation"`, :obj:`"linear"` and
-                :obj:`"inverse_linear"`.
+        aggr (string or list or Aggregation): The aggregation scheme to use.
+            See :class:`~torch_geometric.nn.conv.MessagePassing` for more
+            information.
+        scaler (str or list): Set of scaling function identifiers, namely one
+            or more of :obj:`"identity"`, :obj:`"amplification"`,
+            :obj:`"attenuation"`, :obj:`"linear"` and :obj:`"inverse_linear"`.
         deg (Tensor): Histogram of in-degrees of nodes in the training set,
             used by scalers to normalize.
-        aggr_kwargs (List[Dict[str, Any]], optional): Arguments passed to the
-            respective aggregation functions in case it gets automatically
+        aggr_kwargs (Dict[str, Any], optional): Arguments passed to the
+            respective aggregation function in case it gets automatically
             resolved. (default: :obj:`None`)
     """
-    def __init__(self, aggrs: List[Union[Aggregation, str]],
-                 scalers: List[str], deg: Tensor,
-                 aggrs_kwargs: Optional[List[Dict[str, Any]]] = None):
-
+    def __init__(
+        self,
+        aggr: Union[str, List[str], Aggregation],
+        scaler: Union[str, List[str]],
+        deg: Tensor,
+        aggr_kwargs: Optional[List[Dict[str, Any]]] = None,
+    ):
         super().__init__()
 
-        # TODO: Support non-lists
-        if not isinstance(aggrs, list):
-            raise RuntimeError("`aggrs` must be a list of aggregations ")
+        if isinstance(aggr, (str, Aggregation)):
+            self.aggr = aggr_resolver(aggr, **(aggr_kwargs or {}))
+        elif isinstance(aggr, (tuple, list)):
+            self.aggr = MultiAggregation(aggr, aggr_kwargs)
+        else:
+            raise ValueError(f"Only strings, list, tuples and instances of"
+                             f"`torch_geometric.nn.aggr.Aggregation` are "
+                             f"valid aggregation schemes (got '{type(aggr)}')")
 
-        self.aggr = MultiAggregation(aggrs, aggrs_kwargs)
-        self.scalers = scalers
+        self.scaler = [scaler] if isinstance(aggr, str) else scaler
 
         deg = deg.to(torch.float)
         num_nodes = int(deg.sum())
-        bin_degrees = torch.arange(deg.numel())
+        bin_degrees = torch.arange(deg.numel(), device=deg.device)
         self.avg_deg: Dict[str, float] = {
             'lin': float((bin_degrees * deg).sum()) / num_nodes,
             'log': float(((bin_degrees + 1).log() * deg).sum()) / num_nodes,
@@ -53,16 +62,19 @@ class DegreeScalerAggregation(Aggregation):
                 ptr: Optional[Tensor] = None, dim_size: Optional[int] = None,
                 dim: int = -2) -> Tensor:
 
+        # TODO Currently, `degree` can only operate on `index`:
         self.assert_index_present(index)
 
         out = self.aggr(x, index, ptr, dim_size, dim)
-        deg = degree(index, dtype=out.dtype).clamp_(1)
 
+        assert index is not None
+        deg = degree(index, dtype=out.dtype).clamp_(1)
         size = [1] * len(out.size())
         size[dim] = -1
-        deg = deg.view(*size)
+        deg = deg.view(size)
+
         outs = []
-        for scaler in self.scalers:
+        for scaler in self.scaler:
             if scaler == 'identity':
                 pass
             elif scaler == 'amplification':
@@ -74,6 +86,7 @@ class DegreeScalerAggregation(Aggregation):
             elif scaler == 'inverse_linear':
                 out = out * (self.avg_deg['lin'] / deg)
             else:
-                raise ValueError(f'Unknown scaler "{scaler}".')
+                raise ValueError(f"Unknown scaler '{scaler}'")
             outs.append(out)
+
         return torch.cat(outs, dim=-1) if len(outs) > 1 else outs[0]
