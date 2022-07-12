c0e714dd594206fc0e0ec4e43937a393f0a05789
padarn.wilson <padarn.wilson@grabtaxi.com>
2022-07-12 21:43:11 +0800

    new deprecation approach

--- torch_geometric/nn/glob/__init__.py
+++ torch_geometric/nn/glob/__init__.py
@@ -16,14 +16,18 @@ classes = __all__
 
 from torch_geometric.deprecation import deprecated  # noqa
 from torch_geometric.nn.aggr import Set2Set  # noqa
-from torch_geometric.nn.glob import sort  # noqa
+from torch_geometric.nn.aggr import SortAggr  # noqa
 
 Set2Set = deprecated(
     details="use 'nn.aggr.Set2Set' instead",
     func_name='nn.glob.Set2Set',
 )(Set2Set)
 
-global_sort_pool = deprecated(
+
+@deprecated(
     details="use 'nn.aggr.GlobalSortAggr' instead",
     func_name='nn.glob.global_sort_pool',
-)(sort.global_sort_pool)
+)
+def global_sort_pool(x, index, k):
+    module = SortAggr(k=k)
+    return module(x, index=index)
