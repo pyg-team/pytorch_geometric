022edec1ddbb439ffff5547918432e4d175e705e
Julian Förster <fjulian@users.noreply.github.com>
2022-07-13 19:45:45 +0200

    fix test

--- test/graphgym/test_graphgym.py
+++ test/graphgym/test_graphgym.py
@@ -136,7 +136,7 @@ def test_graphgym_module(tmpdir):
     assert isinstance(scheduler[0], torch.optim.lr_scheduler.CosineAnnealingLR)
 
     cfg.params = params_count(model)
-    assert cfg.params == 23880
+    assert cfg.params == 23883
 
     keys = {"loss", "true", "pred_score", "step_end_time"}
     # test training step
