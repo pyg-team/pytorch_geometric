8d403c4b08a9b4a1151e2cf18236c5bc4c832639
rusty1s <matthias.fey@tu-dortmund.de>
2022-07-14 14:57:36 +0000

    fix test

--- torch_geometric/testing/__init__.py
+++ torch_geometric/testing/__init__.py
@@ -1,4 +1,4 @@
-from .decorators import (is_full_test, onlyFullTest, withPackage,
+from .decorators import (is_full_test, onlyFullTest, withPython, withPackage,
                          withRegisteredOp, withCUDA)
 
 __all__ = [
