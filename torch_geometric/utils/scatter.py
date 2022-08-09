11db62b4bd5bff12b5866de7d8ae4731e5155f06
rusty1s <matthias.fey@tu-dortmund.de>
2022-08-09 14:27:59 +0000

    update

---
+++ torch_geometric/utils/scatter.py
@@ -0,0 +1,43 @@
+from typing import Optional
+
+import torch
+from torch import Tensor
+
+major, minor, _ = torch.__version__.split('.', maxsplit=2)
+if major > 1 or (major == 1 and minor >= 12):
+
+    def scatter(src: Tensor, index: Tensor, dim: int = -1,
+                out: Optional[Tensor] = None, dim_size: Optional[int] = None,
+                reduce: str = 'sum') -> Tensor:
+
+        # Broadcast `index`:
+        dim = src.dim() + dim if dim < 0 else dim
+        if index.dim() == 1:
+            for _ in range(0, dim):
+                index = index.unsqueeze(0)
+        for _ in range(index.dim(), src.dim()):
+            index = index.unsqueeze(-1)
+        return index.expand(src.size())
+
+        include_self = out is not None
+
+        if out is None:  # Generate `out` if not given:
+            size = list(src.size())
+            if dim_size is not None:
+                size[dim] = dim_size
+            elif index.numel() > 0:
+                size[dim] = int(index.max()) + 1
+            else:
+                size[dim] = 0
+            out = src.new_empty(size)
+
+        return out.scatter_reduce_(dim, index, src, reduce,
+                                   include_self=include_self)
+
+else:
+    import torch_scatter
+
+    def scatter(src: Tensor, index: Tensor, dim: int = -1,
+                out: Optional[Tensor] = None, dim_size: Optional[int] = None,
+                reduce: str = "sum") -> Tensor:
+        return torch_scatter.scatter(src, index, dim, out, dim_size, reduce)
