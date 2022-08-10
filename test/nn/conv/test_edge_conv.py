1382231446e371068e96c370dd1206baf5c77efb
rusty1s <matthias.fey@tu-dortmund.de>
2022-08-10 11:39:11 +0000

    reset

--- test/nn/conv/test_edge_conv.py
+++ test/nn/conv/test_edge_conv.py
@@ -25,35 +25,35 @@ def test_edge_conv_conv():
         '))')
     out1 = conv(x1, edge_index)
     assert out1.size() == (4, 32)
-    assert torch.allclose(conv((x1, x1), edge_index), out1)
-    assert torch.allclose(conv(x1, adj.t()), out1)
-    assert torch.allclose(conv((x1, x1), adj.t()), out1)
+    assert conv((x1, x1), edge_index).tolist() == out1.tolist()
+    assert conv(x1, adj.t()).tolist() == out1.tolist()
+    assert conv((x1, x1), adj.t()).tolist() == out1.tolist()
 
     adj = adj.sparse_resize((4, 2))
     out2 = conv((x1, x2), edge_index)
     assert out2.size() == (2, 32)
-    assert torch.allclose(conv((x1, x2), adj.t()), out2)
+    assert conv((x1, x2), adj.t()).tolist() == out2.tolist()
 
     if is_full_test():
         t = '(Tensor, Tensor) -> Tensor'
         jit = torch.jit.script(conv.jittable(t))
-        assert torch.allclose(jit(x1, edge_index), out1)
+        assert jit(x1, edge_index).tolist() == out1.tolist()
 
         t = '(PairTensor, Tensor) -> Tensor'
         jit = torch.jit.script(conv.jittable(t))
-        assert torch.allclose(jit((x1, x1), edge_index), out1)
-        assert torch.allclose(jit((x1, x2), edge_index), out2)
+        assert jit((x1, x1), edge_index).tolist() == out1.tolist()
+        assert jit((x1, x2), edge_index).tolist() == out2.tolist()
 
         adj = adj.sparse_resize((4, 4))
         t = '(Tensor, SparseTensor) -> Tensor'
         jit = torch.jit.script(conv.jittable(t))
-        assert torch.allclose(jit(x1, adj.t()), out1)
+        assert jit(x1, adj.t()).tolist() == out1.tolist()
 
         t = '(PairTensor, SparseTensor) -> Tensor'
         jit = torch.jit.script(conv.jittable(t))
-        assert torch.allclose(jit((x1, x1), adj.t()), out1)
+        assert jit((x1, x1), adj.t()).tolist() == out1.tolist()
         adj = adj.sparse_resize((4, 2))
-        assert torch.allclose(jit((x1, x2), adj.t()), out2)
+        assert jit((x1, x2), adj.t()).tolist() == out2.tolist()
 
 
 @withPackage('torch_cluster')
