b5363590f9b347623311f8a00610ca6a58241673
Josiah Bjorgaard <josiahbjorgaard@gmail.com>
2022-08-10 13:17:33 -0700

    fix Iterable type hint

--- torch_geometric/data/iterable_dataset.py
+++ torch_geometric/data/iterable_dataset.py
@@ -161,7 +161,7 @@ class IterableDataset(torch.utils.data.IterableDataset):
 
         print("Done!", file=sys.stderr)
 
-    def __iter__(self) -> Iterable[Data]:
+    def __iter__(self) -> Iterable:
         r"""Creates an iterator from an implementation of `iter`
         (and transforms each iter in case :obj:`transform` is
         present)."""
