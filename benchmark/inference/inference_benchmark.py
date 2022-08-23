e2e9204a077de57ed008519ae75275e564997092
Matthias Fey <matthias.fey@tu-dortmund.de>
2022-08-23 11:11:12 +0200

    `SparseTensor` inference benchmark (#5242)
    * update
    
    * changelog

--- benchmark/inference/inference_benchmark.py
+++ benchmark/inference/inference_benchmark.py
@@ -24,7 +24,8 @@ def run(args: argparse.ArgumentParser) -> None:
         assert dataset_name in supported_sets.keys(
         ), f"Dataset {dataset_name} isn't supported."
         print(f'Dataset: {dataset_name}')
-        dataset, num_classes = get_dataset(dataset_name, args.root)
+        dataset, num_classes = get_dataset(dataset_name, args.root,
+                                           args.use_sparse_tensor)
         data = dataset.to(device)
         hetero = True if dataset_name == 'ogbn-mag' else False
         mask = ('paper', None) if dataset_name == 'ogbn-mag' else None
@@ -65,8 +66,7 @@ def run(args: argparse.ArgumentParser) -> None:
                         )
 
                     for hidden_channels in args.num_hidden_channels:
-                        print(
-                            '-----------------------------------------------')
+                        print('----------------------------------------------')
                         print(
                             f'Batch size={batch_size}, '
                             f'Layers amount={layers}, '
@@ -111,6 +111,9 @@ if __name__ == '__main__':
                            default=['ogbn-mag', 'ogbn-products',
                                     'Reddit'], type=str)
     argparser.add_argument(
+        '--use-sparse-tensor', action='store_true',
+        help='use torch_sparse.SparseTensor as graph storage format')
+    argparser.add_argument(
         '--models', nargs='+',
         default=['edge_cnn', 'gat', 'gcn', 'pna', 'rgat', 'rgcn'], type=str)
     argparser.add_argument('--root', default='../../data', type=str,
@@ -124,7 +127,7 @@ if __name__ == '__main__':
         '--num-heads', default=2, type=int,
         help='number of hidden attention heads, applies only for gat and rgat')
     argparser.add_argument(
-        '--hetero-num-neighbors', default=-1, type=int,
+        '--hetero-num-neighbors', default=10, type=int,
         help='number of neighbors to sample per layer for hetero workloads')
     argparser.add_argument('--num-workers', default=2, type=int)
     argparser.add_argument('--experimental-mode', action='store_true',
