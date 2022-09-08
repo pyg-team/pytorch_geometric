b01f0201f91f6536dd9b9b4d22d1ef3f121777f3
rusty1s <matthias.fey@tu-dortmund.de>
2022-09-08 14:05:47 +0000

    update

--- torch_geometric/sampler/neighbor_sampler.py
+++ torch_geometric/sampler/neighbor_sampler.py
@@ -16,7 +16,7 @@ from torch_geometric.typing import NumNeighbors
 
 try:
     import pyg_lib  # noqa
-    _WITH_PYG_LIB = False  # TODO: Set to `True` once all tests pass.
+    _WITH_PYG_LIB = True
 except ImportError:
     _WITH_PYG_LIB = False
 
