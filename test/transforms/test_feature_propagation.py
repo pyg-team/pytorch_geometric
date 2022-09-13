bf00d1e4e36cc48ef03666b7600f394702a9af08
EdisonLeeeee <cnljt@outlook.com>
2022-09-13 16:58:15 +0800

    fix test

--- test/transforms/test_feature_propagation.py
+++ test/transforms/test_feature_propagation.py
@@ -5,8 +5,6 @@ from torch_geometric.transforms import FeaturePropagation, ToSparseTensor
 
 
 def test_feature_propagation():
-    assert str(FeaturePropagation()) == ('FeaturePropagation('
-                                         'num_iterations=40)')
     x = torch.randn(6, 4)
     x[0, 1] = float('nan')
     x[2, 3] = float('nan')
@@ -14,15 +12,18 @@ def test_feature_propagation():
     edge_index = torch.tensor([[0, 1, 0, 4, 1, 4, 2, 3, 3, 5],
                                [1, 0, 4, 0, 4, 1, 3, 2, 5, 3]])
 
+    assert str(FeaturePropagation()) == ('FeaturePropagation('
+                                         'num_iterations=40)')
+
     data = Data(x=x, edge_index=edge_index)
     assert torch.any(torch.isnan(data.x))
 
-    data = FeaturePropagation(missing_mask=missing_mask)(data)
+    data = FeaturePropagation(missing_mask)(data)
     assert torch.all(~torch.isnan(data.x))
     assert data.x.size() == x.size()
 
     data = Data(x=x, edge_index=edge_index)
     data = ToSparseTensor()(data)
-    data = FeaturePropagation(missing_mask=missing_mask)(data)
+    data = FeaturePropagation(missing_mask)(data)
     assert torch.all(~torch.isnan(data.x))
     assert data.x.size() == x.size()
