e69dc363d2097d846675a848db911fa5b571382d
Hatem Helal <hatemh@graphcore.ai>
2022-09-14 21:05:53 +0100

    make desc private

--- torch_geometric/data/summary.py
+++ torch_geometric/data/summary.py
@@ -19,66 +19,66 @@ class Summary:
 
         iter = map(map_data, tqdm(dataset))
         df = pd.DataFrame(list(iter), columns=["nodes", "edges"])
-        self.desc = df.describe()
+        self._desc = df.describe()
 
     @property
     def num_graphs(self) -> int:
         r"""The number of graphs in the dataset"""
         # note can use either nodes or edges, the counts are the same
-        return int(self.desc['nodes']['count'])
+        return int(self._desc['nodes']['count'])
 
     @property
     def min_num_nodes(self) -> int:
         r"""The minimum number of nodes"""
-        return int(self.desc['nodes']['min'])
+        return int(self._desc['nodes']['min'])
 
     @property
     def max_num_nodes(self) -> int:
         r"""The maximum number of nodes"""
-        return int(self.desc['nodes']['max'])
+        return int(self._desc['nodes']['max'])
 
     @property
     def median_num_nodes(self) -> int:
         r"""The median number of nodes"""
-        return int(self.desc['nodes']['median'])
+        return int(self._desc['nodes']['median'])
 
     @property
     def mean_num_nodes(self) -> float:
         r"""The mean number of nodes"""
-        return self.desc['nodes']['mean']
+        return self._desc['nodes']['mean']
 
     @property
     def std_num_nodes(self) -> float:
         r"""The standard deviation of the number of nodes"""
-        return self.desc['nodes']['std']
+        return self._desc['nodes']['std']
 
     @property
     def min_num_edges(self) -> int:
         r"""The minimum number of edges"""
-        return int(self.desc['edges']['min'])
+        return int(self._desc['edges']['min'])
 
     @property
     def max_num_edges(self) -> int:
         r"""The maximum number of edges"""
-        return int(self.desc['edges']['max'])
+        return int(self._desc['edges']['max'])
 
     @property
     def median_num_edges(self) -> int:
         r"""The median number of edges"""
-        return int(self.desc['edges']['median'])
+        return int(self._desc['edges']['median'])
 
     @property
     def mean_num_edges(self) -> float:
         r"""The mean number of edges"""
-        return self.desc['edges']['mean']
+        return self._desc['edges']['mean']
 
     @property
     def std_num_edges(self) -> float:
         r"""The standard deviation of the number of edges"""
-        return self.desc['edges']['std']
+        return self._desc['edges']['std']
 
     def __repr__(self) -> str:
         prefix = self.__class__.__name__ + " " + self.dataset_str + "\n"
-        t = self.desc.drop('count')
+        t = self._desc.drop('count')
         body = tabulate(t, headers=t.columns)
         return prefix + body
