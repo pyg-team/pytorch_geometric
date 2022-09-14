4b6c62b011ab8b98baa28b50610a79060344c879
Francesco Landolfi <flandolfi@users.noreply.github.com>
2022-09-14 12:40:06 +0200

    Fix node_attributes shape

--- torch_geometric/io/tu.py
+++ torch_geometric/io/tu.py
@@ -27,6 +27,8 @@ def read_tu_data(folder, prefix):
     node_attributes = torch.empty((batch.size(0), 0))
     if 'node_attributes' in names:
         node_attributes = read_file(folder, prefix, 'node_attributes')
+        if node_attributes.dim() == 1:
+            node_attributes = node_attributes.unsqueeze(-1)
 
     node_labels = torch.empty((batch.size(0), 0))
     if 'node_labels' in names:
