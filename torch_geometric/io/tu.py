fb25c865a763cbf2a5d4ee8a9557105ad8abe57b
Matthias Fey <matthias.fey@tu-dortmund.de>
2022-09-14 16:54:31 +0200

    Update tu.py

--- torch_geometric/io/tu.py
+++ torch_geometric/io/tu.py
@@ -43,6 +43,8 @@ def read_tu_data(folder, prefix):
     edge_attributes = torch.empty((edge_index.size(1), 0))
     if 'edge_attributes' in names:
         edge_attributes = read_file(folder, prefix, 'edge_attributes')
+        if edge_attributes.dim() == 1:
+            edge_attributes = edge_attributes.unsqueeze(-1)
 
     edge_labels = torch.empty((edge_index.size(1), 0))
     if 'edge_labels' in names:
