b79f98078cfe390fd75ea174ab39f0102499664f
rusty1s <matthias.fey@tu-dortmund.de>
2022-09-15 15:30:57 +0000

    update

--- test/transforms/test_mask_transform.py
+++ test/transforms/test_mask_transform.py
@@ -1,110 +1,91 @@
-import pytest
+import copy
+
 import torch
 
 from torch_geometric.data import Data, HeteroData
 from torch_geometric.transforms import IndexToMask, MaskToIndex
 
 
-def check_attr(data, attr, expected_numel, expected_dtype):
-    assert hasattr(data, attr)
-    assert data[attr].numel() == expected_numel
-    assert data[attr].dtype == expected_dtype
-
-
 def test_index_to_mask():
-    assert IndexToMask().__repr__(
-    ) == 'IndexToMask(attrs=None, size=None, replace=False)'
+    assert str(IndexToMask()) == ('IndexToMask(attrs=None, sizes=None, '
+                                  'replace=False)')
 
-    train_idx = torch.arange(0, 10)
-    val_idx = torch.arange(10, 15)
-    test_idx = torch.arange(15, 20)
-    data = Data(train_idx=train_idx, val_idx=val_idx, test_idx=test_idx,
-                num_nodes=20)
+    train_index = torch.arange(0, 3)
+    test_index = torch.arange(3, 5)
+    data = Data(train_index=train_index, test_index=test_index, num_nodes=5)
 
-    out = IndexToMask()(data.clone())
-    assert len(out) == len(data) + 3
-
-    for attr in ["train_mask", "val_mask", "test_mask"]:
-        check_attr(out, attr, 20, torch.bool)
-
-    out = IndexToMask(sizes=30)(data.clone())
-    assert len(out) == len(data) + 3
-
-    for attr in ["train_mask", "val_mask", "test_mask"]:
-        check_attr(out, attr, 30, torch.bool)
-
-    out = IndexToMask(replace=True)(data.clone())
+    out = IndexToMask(replace=True)(copy.copy(data))
     assert len(out) == len(data)
+    assert out.train_mask.tolist() == [True, True, True, False, False]
+    assert out.test_mask.tolist() == [False, False, False, True, True]
 
-    for attr in ["train_idx", "val_idx", "test_idx"]:
-        check_attr(out, attr, 20, torch.bool)
+    out = IndexToMask(replace=False)(copy.copy(data))
+    assert len(out) == len(data) + 2
 
-    out = IndexToMask(attrs="train_idx")(data.clone())
+    out = IndexToMask(sizes=6, replace=True)(copy.copy(data))
+    assert out.train_mask.tolist() == [True, True, True, False, False, False]
+    assert out.test_mask.tolist() == [False, False, False, True, True, False]
+
+    out = IndexToMask(attrs='train_index')(copy.copy(data))
     assert len(out) == len(data) + 1
-    check_attr(out, "train_mask", 20, torch.bool)
-
-    with pytest.raises(ValueError):
-        IndexToMask(sizes=(1, 2))(data)
+    assert 'train_index' in out
+    assert 'train_mask' in out
+    assert 'test_index' in out
+    assert 'test_mask' not in out
 
 
 def test_mask_to_index():
-    assert MaskToIndex().__repr__() == 'MaskToIndex(attrs=None, replace=False)'
+    assert str(MaskToIndex()) == 'MaskToIndex(attrs=None, replace=False)'
 
-    train_mask = torch.tensor([1, 0, 1, 1, 0, 0, 0]).bool()
-    val_mask = torch.tensor([0, 1, 0, 0, 1, 0, 0]).bool()
-    test_mask = torch.tensor([0, 0, 0, 0, 0, 1, 1]).bool()
-    data = Data(train_mask=train_mask, val_mask=val_mask, test_mask=test_mask)
+    train_mask = torch.tensor([True, True, True, False, False])
+    test_mask = torch.tensor([False, False, False, True, True])
+    data = Data(train_mask=train_mask, test_mask=test_mask)
 
-    out = MaskToIndex()(data.clone())
-    assert len(out) == len(data) + 3
-    check_attr(out, "train_idx", 3, torch.long)
-    check_attr(out, "val_idx", 2, torch.long)
-    check_attr(out, "test_idx", 2, torch.long)
-
-    out = MaskToIndex(attrs="train_mask")(data.clone())
-    assert len(out) == len(data) + 1
-    check_attr(out, "train_idx", 3, torch.long)
-
-    out = MaskToIndex(replace=True)(data.clone())
+    out = MaskToIndex(replace=True)(copy.copy(data))
     assert len(out) == len(data)
-    check_attr(out, "train_mask", 3, torch.long)
+    assert out.train_index.tolist() == [0, 1, 2]
+    assert out.test_index.tolist() == [3, 4]
+
+    out = MaskToIndex(replace=False)(copy.copy(data))
+    assert len(out) == len(data) + 2
+
+    out = MaskToIndex(attrs='train_mask')(copy.copy(data))
+    assert len(out) == len(data) + 1
+    assert 'train_mask' in out
+    assert 'train_index' in out
+    assert 'test_mask' in out
+    assert 'test_index' not in out
 
 
 def test_hetero_index_to_mask():
     data = HeteroData()
-    data['u'].train_idx = torch.arange(0, 10)
-    data['u'].val_idx = torch.arange(10, 15)
-    data['u'].test_idx = torch.arange(15, 25)
-    data['u'].num_nodes = 25
+    data['u'].train_index = torch.arange(0, 3)
+    data['u'].test_index = torch.arange(3, 5)
+    data['u'].num_nodes = 5
 
-    data['v'].train_idx = torch.arange(0, 10)
-    data['v'].val_idx = torch.arange(10, 15)
-    data['v'].test_idx = torch.arange(15, 20)
-    data['v'].num_nodes = 20
+    data['v'].train_index = torch.arange(0, 3)
+    data['v'].test_index = torch.arange(3, 5)
+    data['v'].num_nodes = 5
 
-    out = IndexToMask()(data.clone())
-    assert len(out) == len(data) + 3
-
-    for attr in ["train_mask", "val_mask", "test_mask"]:
-        check_attr(out['u'], attr, 25, torch.bool)
-        check_attr(out['v'], attr, 20, torch.bool)
+    out = IndexToMask()(copy.copy(data))
+    assert len(out) == len(data) + 2
+    assert 'train_mask' in out['u']
+    assert 'test_mask' in out['u']
+    assert 'train_mask' in out['v']
+    assert 'test_mask' in out['v']
 
 
 def test_hetero_mask_to_index():
     data = HeteroData()
+    data['u'].train_mask = torch.tensor([True, True, True, False, False])
+    data['u'].test_mask = torch.tensor([False, False, False, True, True])
 
-    data['u'].train_mask = torch.tensor([1, 0, 1, 1, 0, 0, 0]).bool()
-    data['u'].val_mask = torch.tensor([0, 1, 0, 0, 1, 0, 0]).bool()
-    data['u'].test_mask = torch.tensor([0, 0, 0, 0, 0, 1, 1]).bool()
+    data['v'].train_mask = torch.tensor([True, True, True, False, False])
+    data['v'].test_mask = torch.tensor([False, False, False, True, True])
 
-    data['v'].train_mask = torch.tensor([1, 0, 1, 1, 0, 0, 0]).bool()
-    data['v'].val_mask = torch.tensor([0, 1, 0, 0, 1, 0, 0]).bool()
-    data['v'].test_mask = torch.tensor([0, 0, 0, 0, 0, 1, 1]).bool()
-
-    out = MaskToIndex()(data.clone())
-    assert len(out) == len(data) + 3
-
-    for type in ['u', 'v']:
-        check_attr(out[type], "train_idx", 3, torch.long)
-        check_attr(out[type], "val_idx", 2, torch.long)
-        check_attr(out[type], "test_idx", 2, torch.long)
+    out = MaskToIndex()(copy.copy(data))
+    assert len(out) == len(data) + 2
+    assert 'train_index' in out['u']
+    assert 'test_index' in out['u']
+    assert 'train_index' in out['v']
+    assert 'test_index' in out['v']
--- torch_geometric/transforms/mask.py
+++ torch_geometric/transforms/mask.py
@@ -9,28 +9,31 @@ from torch_geometric.utils import index_to_mask, mask_to_index
 AnyData = Union[Data, HeteroData]
 
 
-def transform_attrs(attrs: Optional[List[str]], store: BaseStorage,
-                    suffix: str):
+def get_attrs_with_suffix(
+    attrs: Optional[List[str]],
+    store: BaseStorage,
+    suffix: str,
+) -> List[str]:
     if attrs is not None:
         return attrs
-    return tuple(filter(lambda s: s.endswith(suffix), store.keys()))
+    return [key for key in store.keys() if key.endswith(suffix)]
 
 
-def update_suffix(name: str, old_suffix: str, new_suffix: str):
-    if name.endswith(old_suffix):
-        name = name[:-len(old_suffix)]
-
-    return name + new_suffix
+def get_mask_size(attr: str, store: BaseStorage, size: Optional[int]) -> int:
+    if size is not None:
+        return size
+    return store.num_edges if store.is_edge_attr(attr) else store.num_nodes
 
 
 @functional_transform('index_to_mask')
 class IndexToMask(BaseTransform):
     r"""Converts indices to a mask representation
+    (functional name: :obj:`index_to_mask`).
 
     Args:
         attrs (str, [str], optional): If given, will only perform index to mask
             conversion for the given attributes. If omitted, will infer the
-            attributes from the suffix `_idx`. (default: :obj:`None`)
+            attributes from the suffix :obj:`_index`. (default: :obj:`None`)
         sizes (int, [int], optional). The size of the mask. If set to
             :obj:`None`, an automatically sized tensor is returned. The number
             of nodes will be used by default, except for edge attributes which
@@ -39,80 +42,82 @@ class IndexToMask(BaseTransform):
         replace (bool, optional): if set to :obj:`True` replaces the index
             attributes with mask tensors. (default: :obj:`False`)
     """
-    def __init__(self, attrs: Optional[Union[str, List[str]]] = None,
-                 sizes: Optional[Union[int, List[int]]] = None,
-                 replace: bool = False):
-        self.attrs = (attrs, ) if isinstance(attrs, str) else attrs
+    def __init__(
+        self,
+        attrs: Optional[Union[str, List[str]]] = None,
+        sizes: Optional[Union[int, List[int]]] = None,
+        replace: bool = False,
+    ):
+        self.attrs = [attrs] if isinstance(attrs, str) else attrs
         self.sizes = sizes
         self.replace = replace
 
-    def __call__(self, data: AnyData) -> AnyData:
-        suffix = "_idx"
-
+    def __call__(
+        self,
+        data: Union[Data, HeteroData],
+    ) -> Union[Data, HeteroData]:
         for store in data.stores:
-            attrs = transform_attrs(self.attrs, store, suffix)
-            sizes = self.sizes if self.sizes else (None, ) * len(attrs)
+            attrs = get_attrs_with_suffix(self.attrs, store, '_index')
 
+            sizes = self.sizes or ([None] * len(attrs))
             if isinstance(sizes, int):
-                sizes = (sizes, ) * len(attrs)
+                sizes = [self.sizes] * len(attrs)
 
             if len(attrs) != len(sizes):
                 raise ValueError(
-                    f"The number of attributes {len(attrs)} must match "
-                    f"the number of sizes provided {len(sizes)}.")
+                    f"The number of attributes (got {len(attrs)}) must match "
+                    f"the number of sizes provided (got {len(sizes)}).")
 
-            for index_attr, size in zip(attrs, sizes):
-                size = self.auto_size(store, index_attr, size)
-                mask = index_to_mask(store[index_attr], size=size)
-
-                if not self.replace:
-                    index_attr = update_suffix(index_attr, suffix, "_mask")
-
-                store[index_attr] = mask
+            for attr, size in zip(attrs, sizes):
+                if attr not in store:
+                    continue
+                size = get_mask_size(attr, store, size)
+                mask = index_to_mask(store[attr], size=size)
+                store[f'{attr[:-6]}_mask'] = mask
+                if self.replace:
+                    del store[attr]
 
         return data
 
     def __repr__(self) -> str:
         return (f'{self.__class__.__name__}(attrs={self.attrs}, '
-                f'size={self.sizes}, replace={self.replace})')
-
-    @staticmethod
-    def auto_size(data: AnyData, key: str, size: Optional[int]):
-        if size is not None:
-            return size
-
-        return data.num_edges if data.is_edge_attr(key) else data.num_nodes
+                f'sizes={self.sizes}, replace={self.replace})')
 
 
 @functional_transform('mask_to_index')
 class MaskToIndex(BaseTransform):
-    r"""Converts a mask to an index representation.
+    r"""Converts a mask to an index representation
+    (functional name: :obj:`mask_to_index`).
 
     Args:
         attrs (str, [str], optional): If given, will only perform mask to index
             conversion for the given attributes.  If omitted, will infer the
-            attributes from the suffix `_mask` (default: :obj:`None`)
+            attributes from the suffix :obj:`_mask` (default: :obj:`None`)
         replace (bool, optional): if set to :obj:`True` replaces the mask
             attributes with index tensors. (default: :obj:`False`)
     """
-    def __init__(self, attrs: Optional[Union[str, List[str]]] = None,
-                 replace: bool = False):
-        self.attrs = (attrs, ) if isinstance(attrs, str) else attrs
+    def __init__(
+        self,
+        attrs: Optional[Union[str, List[str]]] = None,
+        replace: bool = False,
+    ):
+        self.attrs = [attrs] if isinstance(attrs, str) else attrs
         self.replace = replace
 
-    def __call__(self, data: AnyData) -> AnyData:
-        suffix = "_mask"
-
+    def __call__(
+        self,
+        data: Union[Data, HeteroData],
+    ) -> Union[Data, HeteroData]:
         for store in data.stores:
-            attrs = transform_attrs(self.attrs, store, suffix)
+            attrs = get_attrs_with_suffix(self.attrs, store, '_mask')
 
-            for mask_attr in attrs:
-                index = mask_to_index(store[mask_attr])
-
-                if not self.replace:
-                    mask_attr = update_suffix(mask_attr, suffix, "_idx")
-
-                store[mask_attr] = index
+            for attr in attrs:
+                if attr not in store:
+                    continue
+                index = mask_to_index(store[attr])
+                store[f'{attr[:-5]}_index'] = index
+                if self.replace:
+                    del store[attr]
 
         return data
 
