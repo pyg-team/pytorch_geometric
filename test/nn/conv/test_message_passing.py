5457388180cf2e36b04e5f5cfd0193c8a0bfad53
Jakub Pietrak <jakub.pietrak@intel.com>
2022-09-19 15:53:51 +0200

    better porting using complete torch.sparse.matmul

--- test/nn/conv/test_message_passing.py
+++ test/nn/conv/test_message_passing.py
@@ -7,9 +7,8 @@ from torch import Tensor
 from torch.nn import Linear
 from torch_scatter import scatter
 from torch_sparse import SparseTensor
-from torch_sparse.matmul import spmm as spmm
-from torch.sparse.matmul import spmm as _spmm
-
+#from torch_sparse.matmul import spmm as spmm
+from torch.sparse.matmul import spmm
 from torch_geometric.nn import MessagePassing, aggr
 from torch_geometric.typing import Adj, OptPairTensor, OptTensor, Size
 
@@ -46,13 +45,10 @@ class MyConv(MessagePassing):
 
     def message_and_aggregate(self, adj_t: SparseTensor,
                               x: OptPairTensor) -> Tensor:
-        if self.aggr in ['sum', 'add']:
-            return _spmm(adj_t, x[0], reduce=self.aggr)
-        else:
-            return spmm(adj_t, x[0], reduce=self.aggr)
+        return spmm(adj_t, x[0], reduce=self.aggr)
 
 
-def allclose(t1 : torch.Tensor, t2: torch.Tensor, atol=1e-2):
+def allclose(t1 : torch.Tensor, t2: torch.Tensor, atol=1e-4):
     return torch.allclose(t1, t2, atol)
 
 
@@ -67,10 +63,10 @@ def test_my_conv():
     conv = MyConv(8, 32)
     out = conv(x1, edge_index, value)
     assert out.size() == (4, 32)
-    assert conv(x1, edge_index, value, (4, 4)).tolist() == out.tolist()
-    assert conv(x1, adj.t()).tolist() == out.tolist()
+    assert allclose(conv(x1, edge_index, value, (4, 4)), out)
+    assert allclose(conv(x1, adj.t()), out)
     conv.fuse = False
-    assert conv(x1, adj.t()).tolist() == out.tolist()
+    assert allclose(conv(x1, adj.t()), out)
     conv.fuse = True
 
     adj = adj.sparse_resize((4, 2))
@@ -79,12 +75,12 @@ def test_my_conv():
     out2 = conv((x1, None), edge_index, value, (4, 2))
     assert out1.size() == (2, 32)
     assert out2.size() == (2, 32)
-    assert conv((x1, x2), edge_index, value, (4, 2)).tolist() == out1.tolist()
-    assert conv((x1, x2), adj.t()).tolist() == out1.tolist()
-    assert conv((x1, None), adj.t()).tolist() == out2.tolist()
+    assert allclose(conv((x1, x2), edge_index, value, (4, 2)), out1)
+    assert allclose(conv((x1, x2), adj.t()), out1)
+    assert allclose(conv((x1, None), adj.t()), out2)
     conv.fuse = False
-    assert conv((x1, x2), adj.t()).tolist() == out1.tolist()
-    assert conv((x1, None), adj.t()).tolist() == out2.tolist()
+    assert allclose(conv((x1, x2), adj.t()), out1)
+    assert allclose(conv((x1, None), adj.t()), out2)
     conv.fuse = True
 
 
@@ -116,14 +112,14 @@ def test_my_conv_jittable():
 
     t = '(Tensor, Tensor, OptTensor, Size) -> Tensor'
     jit = torch.jit.script(conv.jittable(t))
-    assert jit(x1, edge_index, value).tolist() == out.tolist()
-    assert jit(x1, edge_index, value, (4, 4)).tolist() == out.tolist()
+    assert allclose(jit(x1, edge_index, value), out)
+    assert allclose(jit(x1, edge_index, value, (4, 4)), out)
 
     t = '(Tensor, SparseTensor, OptTensor, Size) -> Tensor'
     jit = torch.jit.script(conv.jittable(t))
-    assert jit(x1, adj.t()).tolist() == out.tolist()
+    assert allclose(jit(x1, adj.t()), out)
     jit.fuse = False
-    assert jit(x1, adj.t()).tolist() == out.tolist()
+    assert allclose(jit(x1, adj.t()), out)
     jit.fuse = True
 
     adj = adj.sparse_resize((4, 2))
@@ -133,17 +129,17 @@ def test_my_conv_jittable():
 
     t = '(OptPairTensor, Tensor, OptTensor, Size) -> Tensor'
     jit = torch.jit.script(conv.jittable(t))
-    assert jit((x1, x2), edge_index, value).tolist() == out1.tolist()
-    assert jit((x1, x2), edge_index, value, (4, 2)).tolist() == out1.tolist()
-    assert jit((x1, None), edge_index, value, (4, 2)).tolist() == out2.tolist()
+    assert allclose(jit((x1, x2), edge_index, value), out1)
+    assert allclose(jit((x1, x2), edge_index, value, (4, 2)), out1)
+    assert allclose(jit((x1, None), edge_index, value, (4, 2)), out2)
 
     t = '(OptPairTensor, SparseTensor, OptTensor, Size) -> Tensor'
     jit = torch.jit.script(conv.jittable(t))
-    assert jit((x1, x2), adj.t()).tolist() == out1.tolist()
-    assert jit((x1, None), adj.t()).tolist() == out2.tolist()
+    assert allclose(jit((x1, x2), adj.t()), out1)
+    assert allclose(jit((x1, None), adj.t()), out2)
     jit.fuse = False
-    assert jit((x1, x2), adj.t()).tolist() == out1.tolist()
-    assert jit((x1, None), adj.t()).tolist() == out2.tolist()
+    assert allclose(jit((x1, x2), adj.t()), out1)
+    assert allclose(jit((x1, None), adj.t()), out2)
     jit.fuse = True
 
 
@@ -169,18 +165,18 @@ def test_my_static_graph_conv():
     conv = MyConv(8, 32)
     out = conv(x1, edge_index, value)
     assert out.size() == (3, 4, 32)
-    assert conv(x1, edge_index, value, (4, 4)).tolist() == out.tolist()
-    assert conv(x1, adj.t()).tolist() == out.tolist()
+    assert allclose(conv(x1, edge_index, value, (4, 4)), out)
+    assert allclose(conv(x1, adj.t()), out)
 
     adj = adj.sparse_resize((4, 2))
     conv = MyConv((8, 16), 32)
     out1 = conv((x1, x2), edge_index, value)
     assert out1.size() == (3, 2, 32)
-    assert conv((x1, x2), edge_index, value, (4, 2)).tolist() == out1.tolist()
-    assert conv((x1, x2), adj.t()).tolist() == out1.tolist()
+    assert allclose(conv((x1, x2), edge_index, value, (4, 2)), out1)
+    assert allclose(conv((x1, x2), adj.t()), out1)
     out2 = conv((x1, None), edge_index, value, (4, 2))
     assert out2.size() == (3, 2, 32)
-    assert conv((x1, None), adj.t()).tolist() == out2.tolist()
+    assert allclose(conv((x1, None), adj.t()), out2)
 
 
 class MyMultipleAggrConv(MessagePassing):
