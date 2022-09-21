afb60a8f338ebec1f5572867a6c84a67e81ba8d2
EdisonLeeeee <cnljt@outlook.com>
2022-09-21 16:19:44 +0800

    test

--- test/utils/test_dropout.py
+++ test/utils/test_dropout.py
@@ -1,6 +1,6 @@
 import torch
 
-from torch_geometric.utils import dropout_adj
+from torch_geometric.utils import dropout_adj, dropout_node
 
 
 def test_dropout_adj():
@@ -23,3 +23,21 @@ def test_dropout_adj():
     out = dropout_adj(edge_index, edge_attr, force_undirected=True)
     assert out[0].tolist() == [[0, 1, 1, 2], [1, 2, 0, 1]]
     assert out[1].tolist() == [1, 3, 1, 3]
+
+
+def test_dropout_node():
+    edge_index = torch.tensor([
+        [0, 1, 1, 2, 2, 3],
+        [1, 0, 2, 1, 3, 2],
+    ])
+
+    out = dropout_node(edge_index, training=False)
+    assert edge_index.tolist() == out[0].tolist()
+    assert out[1].tolist() == [False, False, False, False, False, False]
+    assert out[2].tolist() == [False, False, False, False]
+
+    torch.manual_seed(5)
+    out = dropout_node(edge_index)
+    assert out[0].tolist() == [[2, 3], [3, 2]]
+    assert out[1].tolist() == [False, False, False, False, True, True]
+    assert out[2].tolist() == [True, False, True, True]
