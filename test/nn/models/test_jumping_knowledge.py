b9c2c8f62a4c76487404ea486c6889bb54496c4a
Matthias Fey <matthias.fey@tu-dortmund.de>
2022-09-22 17:57:16 +0200

    Update test/nn/models/test_jumping_knowledge.py

--- test/nn/models/test_jumping_knowledge.py
+++ test/nn/models/test_jumping_knowledge.py
@@ -29,8 +29,8 @@ def test_jumping_knowledge():
         assert torch.allclose(jit(xs), out)
 
     model = JumpingKnowledge('lstm', channels, num_layers)
-    assert model.__repr__() == 'JumpingKnowledge(lstm, Channels: ' +\
-                               f'{channels}, Layers: {num_layers})'
+    assert model.__repr__() == (f'JumpingKnowledge(lstm, channels='
+                                f'{channels}, layers={num_layers})')
 
     out = model(xs)
     assert out.size() == (num_nodes, channels)
