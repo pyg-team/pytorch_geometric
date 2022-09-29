b5c9a1fccbf37906b8fc1738c119622d932d8408
BenGutteridge <benpgutteridge@gmail.com>
2022-09-29 15:27:46 +0100

    updated bash and yamls before running

--- graphgym/run_experiments_29.09.sh
+++ graphgym/run_experiments_29.09.sh
@@ -3,26 +3,48 @@
 # we want one for regular vanilla GCN on ogb-molhiv
 python main.py --cfg configs/configs_29.09/vanilla_GCN_2_layer.yaml --repeat 3
 
-# then tensorboard
+# then one for delay GCN on ogb-molhiv
+python main.py --cfg configs/configs_29.09/delay_GCN_2_layer.yaml --repeat 3
+
+# vanilla GCN on ogb-molhiv: 5 layers
+python main.py --cfg configs/configs_29.09/vanilla_GCN_5_layer.yaml --repeat 3
+
+# delay GCN on ogb-molhiv: 5 layers
+python main.py --cfg configs/configs_29.09/delay_GCN_5_layer.yaml --repeat 3
+
+# vanilla GCN on ogb-molhiv: 10 layers
+python main.py --cfg configs/configs_29.09/vanilla_GCN_10_layer.yaml --repeat 3
+
+# delay GCN on ogb-molhiv: 10 layers
+python main.py --cfg configs/configs_29.09/delay_GCN_10_layer.yaml --repeat 3
+
+# then tensorboards
 tensorboard dev upload --logdir ./results/vanilla_GCN_2_layer \
   --name "VanillaGCN 2MP layers" \
   --description "Results for vanilla GCN with GraphGym/OGB defaults (except GCN not GeneralConv and fewer epochs) to compare with DelayGCN version" \
   --one_shot
 
-# then one for delay GCN on ogb-molhiv
-python main.py --cfg configs/configs_29.09/delay_GCN_2_layer.yaml --repeat 3
-
-# then tensorboard
 tensorboard dev upload --logdir ./results/delay_GCN_2_layer \
   --name "DelayGCN 2MP layers" \
   --description "Results for delay GCN with GraphGym/OGB defaults" \
   --one_shot
 
-#--- COME TO THESE LATER ---
+tensorboard dev upload --logdir ./results/vanilla_GCN_5_layer \
+  --name "VanillaGCN 5MP layers" \
+  --description "Results for vanilla GCN with GraphGym/OGB defaults (except GCN not GeneralConv and fewer epochs) to compare with DelayGCN version" \
+  --one_shot
 
-# then one for regular vanilla GCN with more layers on ogb-molhiv
+tensorboard dev upload --logdir ./results/delay_GCN_5_layer \
+  --name "DelayGCN 5MP layers" \
+  --description "Results for delay GCN with GraphGym/OGB defaults" \
+  --one_shot
 
-# then tensorboard
+tensorboard dev upload --logdir ./results/vanilla_GCN_10_layer \
+  --name "VanillaGCN 10MP layers" \
+  --description "Results for vanilla GCN with GraphGym/OGB defaults (except GCN not GeneralConv and fewer epochs) to compare with DelayGCN version" \
+  --one_shot
 
-# then one for delay GCN with more layers on ogb-molhiv
-
+tensorboard dev upload --logdir ./results/delay_GCN_10_layer \
+  --name "DelayGCN 10MP layers" \
+  --description "Results for delay GCN with GraphGym/OGB defaults" \
+  --one_shot
