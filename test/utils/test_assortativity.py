22d351c8a1e508d216be0f08812d7a68a07663e9
EdisonLeeeee <cnljt@outlook.com>
2022-10-02 13:30:37 +0800

    update test

--- test/utils/test_assortativity.py
+++ test/utils/test_assortativity.py
@@ -1,15 +1,18 @@
 import pytest
 import torch
 
-from torch_geometric.utils import assortativity, coalesce
+from torch_geometric.utils import assortativity
 
 
 def test_assortativity():
-    torch.manual_seed(12345)
-
-    num_nodes = 100
-    num_edges = 1000
-    edge_index = torch.randint(0, num_nodes, size=(2, num_edges))
-    edge_index = coalesce(edge_index)
+    # completely assortative graph
+    edge_index = torch.tensor([[0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 5],
+                               [1, 2, 3, 0, 2, 3, 0, 1, 3, 0, 1, 2, 5, 4]])
     out = assortativity(edge_index)
-    assert pytest.approx(out, abs=1e-5) == -0.05669079348444939
+    assert pytest.approx(out, abs=1e-5) == 1.0
+
+    # completely disassortative graph
+    edge_index = torch.tensor([[0, 1, 2, 3, 4, 5, 5, 5, 5, 5],
+                               [5, 5, 5, 5, 5, 0, 1, 2, 3, 4]])
+    out = assortativity(edge_index)
+    assert pytest.approx(out, abs=1e-5) == -1.0
