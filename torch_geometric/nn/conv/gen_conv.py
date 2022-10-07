9b19402e45715128ea83f4673baad00687c6ff06
Guohao Li <lightaime@gmail.com>
2022-10-07 20:05:13 +0300

    update
    Co-authored-by: Matthias Fey <matthias.fey@tu-dortmund.de>

--- torch_geometric/nn/conv/gen_conv.py
+++ torch_geometric/nn/conv/gen_conv.py
@@ -64,6 +64,8 @@ class GENConv(MessagePassing):
     Args:
         in_channels (int or tuple): Size of each input sample, or :obj:`-1` to
             derive the size from the first input(s) to the forward method.
+            A tuple corresponds to the sizes of source and target
+            dimensionalities.
         out_channels (int): Size of each output sample.
         aggr (str, optional): The aggregation scheme to use (:obj:`"softmax"`,
             :obj:`"powermean"`, :obj:`"add"`, :obj:`"mean"`, :obj:`max`).
