872d8c684322f0ba200d73d6dfd3e3fae364a6d7
Matthias Fey <matthias.fey@tu-dortmund.de>
2022-10-13 12:36:24 +0200

    Update torch_geometric/datasets/ppi.py

--- torch_geometric/datasets/ppi.py
+++ torch_geometric/datasets/ppi.py
@@ -63,7 +63,7 @@ class PPI(InMemoryDataset):
     def __init__(self, root: str, split: str = 'train',
                  transform: Optional[Callable] = None,
                  pre_transform: Optional[Callable] = None,
-                 pre_filter: Optional[Callable] = None):
+                 pre_filter: Optional[Callable] = None, ):
 
         assert split in ['train', 'val', 'test']
 
