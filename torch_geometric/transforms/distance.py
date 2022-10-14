5f3a2b2672c6e09740744b51725171880ed89354
Matthias Fey <matthias.fey@tu-dortmund.de>
2022-10-14 10:02:59 +0200

    Update torch_geometric/transforms/distance.py

--- torch_geometric/transforms/distance.py
+++ torch_geometric/transforms/distance.py
@@ -1,5 +1,4 @@
 from typing import Optional
-from torch import tensor
 import torch
 from torch_geometric.data.datapipes import functional_transform
 from torch_geometric.transforms import BaseTransform
