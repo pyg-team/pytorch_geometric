9bbc557eb853238b0628f3d5d3dcb5938b29f94b
akash-vartak <cg82493@umbc.edu>
2022-10-14 14:34:44 -0400

    Adding type hints without test script since TorchScript support not possible

--- torch_geometric/nn/pool/max_pool.py
+++ torch_geometric/nn/pool/max_pool.py
@@ -82,7 +82,7 @@ def max_pool(cluster, data, transform=None):
     return data
 
 
-def max_pool_neighbor_x(data, flow='source_to_target'):
+def max_pool_neighbor_x(data: Data, flow: Optional[str] = 'source_to_target') -> Data:
     r"""Max pools neighboring node features, where each feature in
     :obj:`data.x` is replaced by the feature value with the maximum value from
     the central node and its neighbors.
