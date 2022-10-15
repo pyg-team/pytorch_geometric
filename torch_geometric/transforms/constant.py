007f053270ed3071d3618b2262453da534a08390
EdisonLeeeee <cnljt@outlook.com>
2022-10-15 16:20:03 +0800

    Add node_types argument to transforms.Constant

--- torch_geometric/transforms/constant.py
+++ torch_geometric/transforms/constant.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import List, Optional, Union
 
 import torch
 
@@ -14,15 +14,28 @@ class Constant(BaseTransform):
 
     Args:
         value (float, optional): The value to add. (default: :obj:`1.0`)
-        cat (bool, optional): If set to :obj:`False`, all existing node
+        cat (bool, optional): If set to :obj:`False`, the node
             features will be replaced. (default: :obj:`True`)
+        node_types (str or List[str], optional): The specified node types to
+            add constants, which is used only for heterogeneous graphs.
+            If set to :obj:`None`, constants will be added to each node feature
+            :obj:`x` of all existing node types. (default: :obj:`None`)
     """
-    def __init__(self, value: float = 1.0, cat: bool = True):
+    def __init__(self, value: float = 1.0, cat: bool = True,
+                 node_types: Optional[Union[str, List[str]]] = None):
         self.value = value
         self.cat = cat
+        self.node_types = node_types
 
-    def __call__(self, data: Union[Data, HeteroData]):
-        for store in data.node_stores:
+    def __call__(self, data: Union[Data,
+                                   HeteroData]) -> Union[Data, HeteroData]:
+        if isinstance(data, HeteroData) and self.node_types is not None:
+            node_types = [self.node_types] if isinstance(
+                self.node_types, str) else self.node_types
+            stores = [data[node_type] for node_type in node_types]
+        else:
+            stores = data.node_stores
+        for store in stores:
             c = torch.full((store.num_nodes, 1), self.value, dtype=torch.float)
 
             if hasattr(store, 'x') and self.cat:
