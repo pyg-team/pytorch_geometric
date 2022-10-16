1a5babd09b7710f4dd5ce9b557172d12f820175e
Matthias Fey <matthias.fey@tu-dortmund.de>
2022-10-16 10:58:44 +0200

    Update test/nn/norm/test_graph_size_norm.py

--- test/nn/norm/test_graph_size_norm.py
+++ test/nn/norm/test_graph_size_norm.py
@@ -7,9 +7,10 @@ from torch_geometric.testing import is_full_test
 def test_graph_size_norm():
     batch = torch.repeat_interleave(torch.full((10, ), 10, dtype=torch.long))
     norm = GraphSizeNorm()
+    x = torch.randn(100, 16)
+    out = norm(x, batch)
+    assert out.size() == (100, 16)
 
     if is_full_test():
-        torch.jit.script(norm)
-
-    out = norm(torch.randn(100, 16), batch)
-    assert out.size() == (100, 16)
+        jit = torch.jit.script(norm)
+        assert torch.allclose(jit(x, batch), out)
