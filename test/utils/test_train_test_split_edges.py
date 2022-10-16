b513dc736540d3c20daccf3e22fb44e55f778ea6
Shweta Jacob <shwetaaj59@gmail.com>
2022-10-16 15:17:50 -0400

    Add type hints for train_test_split_edges

--- test/utils/test_train_test_split_edges.py
+++ test/utils/test_train_test_split_edges.py
@@ -2,6 +2,7 @@ import pytest
 import torch
 
 from torch_geometric.data import Data
+from torch_geometric.testing import is_full_test
 from torch_geometric.utils import train_test_split_edges
 
 
@@ -26,3 +27,24 @@ def test_train_test_split_edges():
     assert data.train_pos_edge_attr.size() == (10, )
     assert data.val_pos_edge_attr.size() == (2, )
     assert data.test_pos_edge_attr.size() == (3, )
+
+    if is_full_test():
+        data = Data(edge_index=edge_index, edge_attr=edge_attr)
+        data.num_nodes = edge_index.max().item() + 1
+
+        jit = torch.jit.script(train_test_split_edges)
+        with pytest.warns(UserWarning, match='deprecated'):
+            data = jit(data, val_ratio=0.2, test_ratio=0.3)
+
+        assert len(data) == 10
+        assert data.val_pos_edge_index.size() == (2, 2)
+        assert data.val_neg_edge_index.size() == (2, 2)
+        assert data.test_pos_edge_index.size() == (2, 3)
+        assert data.test_neg_edge_index.size() == (2, 3)
+        assert data.train_pos_edge_index.size() == (2, 10)
+        assert data.train_neg_adj_mask.size() == (11, 11)
+        assert data.train_neg_adj_mask.sum().item(
+        ) == (11**2 - 11) / 2 - 4 - 6 - 5
+        assert data.train_pos_edge_attr.size() == (10, )
+        assert data.val_pos_edge_attr.size() == (2, )
+        assert data.test_pos_edge_attr.size() == (3, )
