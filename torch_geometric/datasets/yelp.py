6743c28890b3fc4ed9d45d01d5d498f2dc29fdba
sachinsharma9780 <sachinsharma9780@gmail.com>
2022-10-17 16:51:19 +0530

    adding type hint support for yelp dataset

--- torch_geometric/datasets/yelp.py
+++ torch_geometric/datasets/yelp.py
@@ -1,7 +1,7 @@
+from typing import Optional, Callable
 import json
 import os
 import os.path as osp
-
 import numpy as np
 import scipy.sparse as sp
 import torch
@@ -46,19 +46,19 @@ class Yelp(InMemoryDataset):
     class_map_id = '1VUcBGr0T0-klqerjAjxRmAqFuld_SMWU'
     role_id = '1NI5pa5Chpd-52eSmLW60OnB3WS5ikxq_'
 
-    def __init__(self, root, transform=None, pre_transform=None):
+    def __init__(self, root: str, transform: Optional[Callable] = None, pre_transform: Optional[Callable] = None):
         super().__init__(root, transform, pre_transform)
         self.data, self.slices = torch.load(self.processed_paths[0])
 
     @property
-    def raw_file_names(self):
+    def raw_file_names(self) -> list:
         return ['adj_full.npz', 'feats.npy', 'class_map.json', 'role.json']
 
     @property
-    def processed_file_names(self):
+    def processed_file_names(self) -> str:
         return 'data.pt'
 
-    def download(self):
+    def download(self) -> None:
         path = download_url(self.url.format(self.adj_full_id), self.raw_dir)
         os.rename(path, osp.join(self.raw_dir, 'adj_full.npz'))
 
@@ -71,7 +71,7 @@ class Yelp(InMemoryDataset):
         path = download_url(self.url.format(self.role_id), self.raw_dir)
         os.rename(path, osp.join(self.raw_dir, 'role.json'))
 
-    def process(self):
+    def process(self) -> None:
         f = np.load(osp.join(self.raw_dir, 'adj_full.npz'))
         adj = sp.csr_matrix((f['data'], f['indices'], f['indptr']), f['shape'])
         adj = adj.tocoo()
