6db41a549cdae04ca7a9af77d066cad66da0e974
Jinu Sunil <jinu.sunil@gmail.com>
2022-10-18 11:35:00 +0530

    Update test_diff_pool.py

--- test/nn/dense/test_diff_pool.py
+++ test/nn/dense/test_diff_pool.py
@@ -11,22 +11,16 @@ def test_dense_diff_pool():
     s = torch.randn((batch_size, num_nodes, num_clusters))
     mask = torch.randint(0, 2, (batch_size, num_nodes), dtype=torch.bool)
 
-    x, adj, link_loss, ent_loss = dense_diff_pool(x, adj, s, mask)
-    assert x.size() == (2, 10, 16)
-    assert adj.size() == (2, 10, 10)
+    x_out, adj_out, link_loss, ent_loss = dense_diff_pool(x, adj, s, mask)
+    assert x_out.size() == (2, 10, 16)
+    assert adj_out.size() == (2, 10, 10)
     assert link_loss.item() >= 0
     assert ent_loss.item() >= 0
 
     if is_full_test():
-        batch_size, num_nodes, channels, num_clusters = (2, 20, 16, 10)
-        x = torch.randn((batch_size, num_nodes, channels))
-        adj = torch.rand((batch_size, num_nodes, num_nodes))
-        s = torch.randn((batch_size, num_nodes, num_clusters))
-        mask = torch.randint(0, 2, (batch_size, num_nodes), dtype=torch.bool)
-
         jit = torch.jit.script(dense_diff_pool)
-        x, adj, link_loss, ent_loss = jit(x, adj, s, mask)
-        assert x.size() == (2, 10, 16)
-        assert adj.size() == (2, 10, 10)
+        x_out, adj_out, link_loss, ent_loss = jit(x, adj, s, mask)
+        assert x_out.size() == (2, 10, 16)
+        assert adj_out.size() == (2, 10, 10)
         assert link_loss.item() >= 0
         assert ent_loss.item() >= 0
