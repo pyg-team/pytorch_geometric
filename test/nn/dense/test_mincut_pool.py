776a0f422497ce4e4a46bba98ad86a4ee3184a97
Akash Vartak <cg82493@umbc.edu>
2022-10-18 13:14:28 -0400

    [Type Hints] `nn.dense_mincut_pool` (#5756)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Matthias Fey <matthias.fey@tu-dortmund.de>

--- test/nn/dense/test_mincut_pool.py
+++ test/nn/dense/test_mincut_pool.py
@@ -1,6 +1,7 @@
 import torch
 
 from torch_geometric.nn import dense_mincut_pool
+from torch_geometric.testing import is_full_test
 
 
 def test_dense_mincut_pool():
@@ -10,8 +11,18 @@ def test_dense_mincut_pool():
     s = torch.randn((batch_size, num_nodes, num_clusters))
     mask = torch.randint(0, 2, (batch_size, num_nodes), dtype=torch.bool)
 
-    x, adj, mincut_loss, ortho_loss = dense_mincut_pool(x, adj, s, mask)
-    assert x.size() == (2, 10, 16)
-    assert adj.size() == (2, 10, 10)
+    x_out, adj_out, mincut_loss, ortho_loss = dense_mincut_pool(
+        x, adj, s, mask)
+    assert x_out.size() == (2, 10, 16)
+    assert adj_out.size() == (2, 10, 10)
     assert -1 <= mincut_loss <= 0
     assert 0 <= ortho_loss <= 2
+
+    if is_full_test():
+        jit = torch.jit.script(dense_mincut_pool)
+
+        x_jit, adj_jit, mincut_loss, ortho_loss = jit(x, adj, s, mask)
+        assert x_jit.size() == (2, 10, 16)
+        assert adj_jit.size() == (2, 10, 10)
+        assert -1 <= mincut_loss <= 0
+        assert 0 <= ortho_loss <= 2
