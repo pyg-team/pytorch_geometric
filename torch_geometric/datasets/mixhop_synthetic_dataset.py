83f516a902bf2a5f2d48514da48e92a116800a7b
Matthias Fey <matthias.fey@tu-dortmund.de>
2022-10-21 21:52:29 +0200

    Update torch_geometric/datasets/mixhop_synthetic_dataset.py

--- torch_geometric/datasets/mixhop_synthetic_dataset.py
+++ torch_geometric/datasets/mixhop_synthetic_dataset.py
@@ -37,7 +37,7 @@ class MixHopSyntheticDataset(InMemoryDataset):
 
     def __init__(self, root: str, homophily: float,
                  transform: Optional[Callable] = None,
-                 pre_transform: Optional[Callable] = None):
+                 pre_transform: Optional[Callable] = None, ):
         self.homophily = homophily
         assert homophily in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
         super().__init__(root, transform, pre_transform)
