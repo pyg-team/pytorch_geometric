4706a5f1c253969293f9b4a52d6d03987286cf1c
Paul Louis <iampaullouis@gmail.com>
2022-10-23 18:16:14 -0400

    Add jit tests for SAGPooling

--- test/nn/pool/test_sag_pool.py
+++ test/nn/pool/test_sag_pool.py
@@ -1,4 +1,5 @@
 import torch
+from torch_geometric.testing import is_full_test
 
 from torch_geometric.nn import (
     GATConv,
@@ -17,23 +18,33 @@ def test_sag_pooling():
     x = torch.randn((num_nodes, in_channels))
 
     for GNN in [GraphConv, GCNConv, GATConv, SAGEConv]:
-        pool = SAGPooling(in_channels, ratio=0.5, GNN=GNN)
-        assert pool.__repr__() == (f'SAGPooling({GNN.__name__}, 16, '
-                                   f'ratio=0.5, multiplier=1.0)')
-        out = pool(x, edge_index)
-        assert out[0].size() == (num_nodes // 2, in_channels)
-        assert out[1].size() == (2, 2)
+        pool1 = SAGPooling(in_channels, ratio=0.5, GNN=GNN)
+        assert pool1.__repr__() == (f'SAGPooling({GNN.__name__}, 16, '
+                                    f'ratio=0.5, multiplier=1.0)')
+        out1 = pool1(x, edge_index)
+        assert out1[0].size() == (num_nodes // 2, in_channels)
+        assert out1[1].size() == (2, 2)
 
-        pool = SAGPooling(in_channels, ratio=None, GNN=GNN, min_score=0.1)
-        assert pool.__repr__() == (f'SAGPooling({GNN.__name__}, 16, '
-                                   f'min_score=0.1, multiplier=1.0)')
-        out = pool(x, edge_index)
-        assert out[0].size(0) <= x.size(0) and out[0].size(1) == (16)
-        assert out[1].size(0) == 2 and out[1].size(1) <= edge_index.size(1)
+        pool2 = SAGPooling(in_channels, ratio=None, GNN=GNN, min_score=0.1)
+        assert pool2.__repr__() == (f'SAGPooling({GNN.__name__}, 16, '
+                                    f'min_score=0.1, multiplier=1.0)')
+        out2 = pool2(x, edge_index)
+        assert out2[0].size(0) <= x.size(0) and out2[0].size(1) == (16)
+        assert out2[1].size(0) == 2 and out2[1].size(1) <= edge_index.size(1)
 
-        pool = SAGPooling(in_channels, ratio=2, GNN=GNN)
-        assert pool.__repr__() == (f'SAGPooling({GNN.__name__}, 16, '
-                                   f'ratio=2, multiplier=1.0)')
-        out = pool(x, edge_index)
-        assert out[0].size() == (2, in_channels)
-        assert out[1].size() == (2, 2)
+        pool3 = SAGPooling(in_channels, ratio=2, GNN=GNN)
+        assert pool3.__repr__() == (f'SAGPooling({GNN.__name__}, 16, '
+                                    f'ratio=2, multiplier=1.0)')
+        out3 = pool3(x, edge_index)
+        assert out3[0].size() == (2, in_channels)
+        assert out3[1].size() == (2, 2)
+
+        if is_full_test():
+            jit1 = torch.jit.script(pool1)
+            assert torch.allclose(jit1(x, edge_index)[0], out1[0])
+
+            jit2 = torch.jit.script(pool2)
+            assert torch.allclose(jit2(x, edge_index)[0], out2[0])
+
+            jit3 = torch.jit.script(pool3)
+            assert torch.allclose(jit3(x, edge_index)[0], out3[0])
