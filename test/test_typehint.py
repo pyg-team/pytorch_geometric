d6b2374fb03f0ba898198d4b33013b6772df3b16
Arian Jamasb <arjamasb@gmail.com>
2022-10-24 05:47:29 +0100

    Convert notebook contents into tests

---
+++ test/test_typehint.py
@@ -0,0 +1,88 @@
+import pytest
+import torch
+from torchtyping import TensorType
+
+from torch_geometric.data import Batch, Data
+from torch_geometric.tp import Graph, GraphBatch, typecheck
+
+
+def get_sample_tensor_data() -> Data:
+    d = Data()
+    d.x = torch.randn((1, 2, 3))
+    return d
+
+
+def get_sample_string_data() -> Data:
+    d = Data()
+    d.x = "not a tensor"
+    return d
+
+
+def get_sample_mixed_data() -> Data:
+    d = Data()
+    d.x = torch.randn((1, 2, 3))
+    d.y = "not a tensor"
+    return d
+
+
+def test_typecheck_attribute():
+    d = get_sample_data()
+
+    @typecheck
+    def forward(d: Graph["x": torch.Tensor]):
+        return d
+
+    assert d == forward(
+        get_sample_tensor_data), "Return value is not the same as input"
+
+    with pytest.raises(TypeError):
+        forward(get_sample_string_data())
+
+
+def test_fully_specified():
+    d = get_sample_mixed_data()
+
+    @typecheck
+    def forward(d: Graph["x": torch.Tensor]):
+        return d
+
+    with pytest.raises(TypeError):
+        forward(d)
+
+
+def test_fuzzy_specification():
+    d = get_sample_mixed_data()
+
+    @typecheck
+    def forward(d: Graph["x": torch.Tensor, ...]):
+        return d
+
+    assert d == forward(d)
+
+
+def test_tensor_shape_specification():
+    d = get_sample_mixed_data()
+
+    @typecheck
+    def forward(d: Graph["x": TensorType[-1, -1, 3], ...]):
+        return d
+
+    assert d == forward(d)
+
+    @typecheck
+    def forward(d: Graph["x": TensorType[-1, 20, 3], ...]):
+        return d
+
+    with pytest.raises(TypeError):
+        forward(d)
+
+
+def test_batch_shape_specification():
+    d = get_sample_mixed_data()
+    b = Batch.from_data_list([d])
+
+    @typecheck
+    def forward(d: GraphBatch["x": TensorType[-1, -1, 3], ...]):
+        return d
+
+    assert b == forward(b)
