f579fa7a5ba93b6f80f642bc24e4b723ab41480f
rusty1s <matthias.fey@tu-dortmund.de>
2022-10-24 06:58:48 +0000

    update

--- test/nn/pool/test_sag_pool.py
+++ test/nn/pool/test_sag_pool.py
@@ -19,32 +19,35 @@ def test_sag_pooling():
 
     for GNN in [GraphConv, GCNConv, GATConv, SAGEConv]:
         pool1 = SAGPooling(in_channels, ratio=0.5, GNN=GNN)
-        assert pool1.__repr__() == (f'SAGPooling({GNN.__name__}, 16, '
-                                    f'ratio=0.5, multiplier=1.0)')
+        assert str(pool1) == (f'SAGPooling({GNN.__name__}, 16, '
+                              f'ratio=0.5, multiplier=1.0)')
         out1 = pool1(x, edge_index)
         assert out1[0].size() == (num_nodes // 2, in_channels)
         assert out1[1].size() == (2, 2)
 
         pool2 = SAGPooling(in_channels, ratio=None, GNN=GNN, min_score=0.1)
-        assert pool2.__repr__() == (f'SAGPooling({GNN.__name__}, 16, '
-                                    f'min_score=0.1, multiplier=1.0)')
+        assert str(pool2) == (f'SAGPooling({GNN.__name__}, 16, '
+                              f'min_score=0.1, multiplier=1.0)')
         out2 = pool2(x, edge_index)
         assert out2[0].size(0) <= x.size(0) and out2[0].size(1) == (16)
         assert out2[1].size(0) == 2 and out2[1].size(1) <= edge_index.size(1)
 
         pool3 = SAGPooling(in_channels, ratio=2, GNN=GNN)
-        assert pool3.__repr__() == (f'SAGPooling({GNN.__name__}, 16, '
-                                    f'ratio=2, multiplier=1.0)')
+        assert str(pool3) == (f'SAGPooling({GNN.__name__}, 16, '
+                              f'ratio=2, multiplier=1.0)')
         out3 = pool3(x, edge_index)
         assert out3[0].size() == (2, in_channels)
         assert out3[1].size() == (2, 2)
 
         if is_full_test():
+            pool1.gnn = pool1.gnn.jittable()
             jit1 = torch.jit.script(pool1)
             assert torch.allclose(jit1(x, edge_index)[0], out1[0])
 
+            pool2.gnn = pool2.gnn.jittable()
             jit2 = torch.jit.script(pool2)
             assert torch.allclose(jit2(x, edge_index)[0], out2[0])
 
+            pool3.gnn = pool3.gnn.jittable()
             jit3 = torch.jit.script(pool3)
             assert torch.allclose(jit3(x, edge_index)[0], out3[0])
--- torch_geometric/nn/pool/sag_pool.py
+++ torch_geometric/nn/pool/sag_pool.py
@@ -5,6 +5,7 @@ from torch import Tensor
 
 from torch_geometric.nn import GraphConv
 from torch_geometric.nn.pool.topk_pool import filter_adj, topk
+from torch_geometric.typing import OptTensor
 from torch_geometric.utils import softmax
 
 
@@ -67,10 +68,16 @@ class SAGPooling(torch.nn.Module):
         **kwargs (optional): Additional parameters for initializing the graph
             neural network layer.
     """
-    def __init__(self, in_channels: int, ratio: Union[float, int] = 0.5,
-                 GNN: Callable = GraphConv, min_score: Optional[float] = None,
-                 multiplier: float = 1.0, nonlinearity: Callable = torch.tanh,
-                 **kwargs):
+    def __init__(
+        self,
+        in_channels: int,
+        ratio: Union[float, int] = 0.5,
+        GNN: torch.nn.Module = GraphConv,
+        min_score: Optional[float] = None,
+        multiplier: float = 1.0,
+        nonlinearity: Callable = torch.tanh,
+        **kwargs,
+    ):
         super().__init__()
 
         self.in_channels = in_channels
@@ -86,10 +93,13 @@ class SAGPooling(torch.nn.Module):
         self.gnn.reset_parameters()
 
     def forward(
-        self, x: Tensor, edge_index: Tensor,
-        edge_attr: Optional[Tensor] = None, batch: Optional[Tensor] = None,
-        attn: Optional[Tensor] = None
-    ) -> Tuple[Tensor, Tensor, Optional[Tensor], Tensor, Tensor, Tensor]:
+        self,
+        x: Tensor,
+        edge_index: Tensor,
+        edge_attr: OptTensor = None,
+        batch: OptTensor = None,
+        attn: OptTensor = None,
+    ) -> Tuple[Tensor, Tensor, OptTensor, Tensor, Tensor, Tensor]:
         """"""
         if batch is None:
             batch = edge_index.new_zeros(x.size(0))
