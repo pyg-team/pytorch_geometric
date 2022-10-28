73769d611985ef9f6374a6b80454e58097274300
akash-vartak <cg82493@umbc.edu>
2022-10-28 17:54:16 -0400

    Adding type hint for nn.MessageNorm

--- test/nn/norm/test_msg_norm.py
+++ test/nn/norm/test_msg_norm.py
@@ -1,6 +1,8 @@
 import torch
 
 from torch_geometric.nn import MessageNorm
+from torch_geometric.testing import is_full_test
+
 
 
 def test_message_norm():
@@ -10,8 +12,16 @@ def test_message_norm():
     msg = torch.randn(100, 16)
     assert norm(x, msg).size() == (100, 16)
 
+    if is_full_test():
+        jit = torch.jit.script(norm)
+        assert jit(x, msg).size() == (100, 16)
+
     norm = MessageNorm(False)
     assert norm.__repr__() == ('MessageNorm(learn_scale=False)')
     x = torch.randn(100, 16)
     msg = torch.randn(100, 16)
     assert norm(x, msg).size() == (100, 16)
+
+    if is_full_test():
+        jit = torch.jit.script(norm)
+        assert jit(x, msg).size() == (100, 16)
