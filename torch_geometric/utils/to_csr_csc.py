446a9dea7bc5264e5d46af6647654ec9e78b50f3
MathsCode <nthaxjm@126.com>
2022-11-01 13:22:07 +0000

    change the annotation

--- torch_geometric/utils/to_csr_csc.py
+++ torch_geometric/utils/to_csr_csc.py
@@ -2,26 +2,29 @@ import torch
 
 
 def to_csr_csc(edge_index):
-    r"""Generate csr and csc tensor based on the coordinates: obj: `edge_index` of the non-zero
-    numbers of the sparse matrix
+    r"""Generate csr and csc tensor based on the coordinates: 
+    obj: `edge_index` of the non-zero numbers of the sparse matrix
 
-    The method returns the csr and csc tensor : obj: `csr_csr` (tuple)
+    The method returns the csr and csc tensor : obj: `csr_csr` (tuple).
+    The data at each position of the tuple is represented as follows.
         (1) row_ptr: crow_indices in csr
         (2) col_idx: col_indices in csr
         (3) col_ptr: crow_indices in csc
         (4) row_idx: col_indices in csc
-        (5) premute: sort order / permute matrix
+        (5) premute: sort order
 
     Arg:
-        edge_index(list or : obj:`torch.tensor`): The coordinates of the non-zero
-        numbers of the sparse matrix
+        edge_index(list or : obj:`torch.tensor`): 
+        The coordinates of the non-zero numbers of the sparse matrix
 
     Examples:
+     
         >>> edge_index = torch.tensor([[0, 1, 0], [1, 0, 0]])
         >>> # (0,1),(1,0) (0,0) are the non-zero numbers
         >>> dgnn_adj = to_csr_csc(edge_index)
         >>> dgnn_adj
         ([0,2,3],[0, 1, 0],[0, 2, 3],[0, 1, 0],[2, 1, 0])
+
     """
     numlist = torch.arange(edge_index.size(1), dtype=torch.int32,
                            device=edge_index.device)
