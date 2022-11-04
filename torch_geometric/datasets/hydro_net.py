aa8bfb1eb2696c4ad5a956a987f47b196cb7b310
EdisonLeeeee <cnljt@outlook.com>
2022-11-05 01:29:06 +0800

    nit

--- torch_geometric/datasets/hydro_net.py
+++ torch_geometric/datasets/hydro_net.py
@@ -2,6 +2,7 @@ import copy
 import os
 import os.path as osp
 from dataclasses import dataclass
+from functools import lru_cache
 from glob import glob
 from pathlib import Path
 from typing import Callable, List, Optional, Tuple, Union
@@ -20,8 +21,9 @@ from torch_geometric.data import (
 try:
     from functools import cached_property
 except ImportError:  # Python 3.7 support.
-    cached_property = None
-from functools import lru_cache
+
+    def cached_property(func):
+        return property(fget=lru_cache(maxsize=1)(func))
 
 
 class HydroNet(InMemoryDataset):
@@ -164,6 +166,7 @@ class HydroNet(InMemoryDataset):
 
         return range(3, 26)
 
+    @cached_property
     def _dataset(self) -> ConcatDataset:
         dataset = ConcatDataset(self._partitions)
 
@@ -171,11 +174,6 @@ class HydroNet(InMemoryDataset):
             dataset = self._load_small_split(dataset)
 
         return dataset
-
-    if cached_property is not None:
-        _dataset = cached_property(_dataset)
-    else:
-        _dataset = property(fget=lru_cache(maxsize=1)(_dataset))
 
     def _load_small_split(self, dataset: ConcatDataset) -> Subset:
         split_file = osp.join(self.processed_dir, 'split_00_small.npz')
@@ -314,14 +312,10 @@ class Partition(InMemoryDataset):
         self._data_list = num_graphs * [None]
         self.is_loaded = True
 
+    @cached_property
     def num_graphs(self) -> int:
         with np.load(self.processed_paths[0]) as npzfile:
             return int(npzfile['num_graphs'])
-
-    if cached_property is not None:
-        num_graphs = cached_property(num_graphs)
-    else:
-        num_graphs = property(fget=lru_cache(maxsize=1)(num_graphs))
 
     def len(self) -> int:
         return self.num_graphs
