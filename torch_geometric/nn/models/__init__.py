83a150ff397f07f6cadcb34c5801d2f1ae5723e8
wsad1 <jinu.sunil@gmail.com>
2022-11-10 11:51:24 +0000

    added to_captum_input and captum_attr_to_dicts

--- torch_geometric/nn/models/__init__.py
+++ torch_geometric/nn/models/__init__.py
@@ -9,7 +9,8 @@ from .re_net import RENet
 from .graph_unet import GraphUNet
 from .schnet import SchNet
 from .dimenet import DimeNet, DimeNetPlusPlus
-from .explainer import Explainer, to_captum, to_captum_input
+from .explainer import (Explainer, to_captum, to_captum_input,
+                        captum_output_to_dicts)
 from .gnn_explainer import GNNExplainer
 from .metapath2vec import MetaPath2Vec
 from .deepgcn import DeepGCNLayer
@@ -48,6 +49,7 @@ __all__ = [
     'Explainer',
     'to_captum',
     'to_captum_input',
+    'captum_output_to_dicts',
     'GNNExplainer',
     'MetaPath2Vec',
     'DeepGCNLayer',
