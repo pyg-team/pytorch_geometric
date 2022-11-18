6502aecf26952ad45abe79c7bab00bce8e7e26f9
rusty1s <matthias.fey@tu-dortmund.de>
2022-11-18 14:32:11 +0000

    update

--- examples/hetero/bipartite_sage.py
+++ examples/hetero/bipartite_sage.py
@@ -1,4 +1,3 @@
-import argparse
 import os.path as osp
 
 import torch
@@ -8,22 +7,16 @@ from torch.nn import Embedding, Linear
 import torch_geometric.transforms as T
 from torch_geometric.datasets import MovieLens
 from torch_geometric.nn import SAGEConv
-from torch_geometric.utils import to_dense_adj
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--use_weighted_loss', action='store_true',
-                    help='Whether to use weighted MSE loss.')
-args = parser.parse_args()
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+from torch_geometric.nn.conv.gcn_conv import gcn_norm
 
 path = osp.join(osp.dirname(osp.realpath(__file__)), '../../data/MovieLens')
-user_sim_dict_path = 'swing_user_sim_dict.json'
 dataset = MovieLens(path, model_name='all-MiniLM-L6-v2')
 data = dataset[0]
 data['user'].x = torch.arange(data['user'].num_nodes)
+data['user', 'movie'].edge_label = data['user', 'movie'].edge_label.float()
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 data = data.to(device)
-del data['user'].num_nodes
 
 # Add a reverse ('movie', 'rev_rates', 'user') relation for message passing:
 data = T.ToUndirected()(data)
@@ -38,115 +31,62 @@ train_data, val_data, test_data = T.RandomLinkSplit(
     rev_edge_types=[('movie', 'rev_rates', 'user')],
 )(data)
 
+# Generate the co-occurence matrix of movies<>movies:
+metapath = [('movie', 'rev_rates', 'user'), ('user', 'rates', 'movie')]
+train_data = T.AddMetaPaths(metapaths=[metapath])(train_data)
 
-def to_u2i_mat(edge_index, i_num, u_num):
-    # Convert bipartite edge_index format to matrix format
-    u2imat = to_dense_adj(edge_index)[0]
+# Apply normalization to filter the metapath:
+_, edge_weight = gcn_norm(
+    train_data['movie', 'movie'].edge_index,
+    num_nodes=train_data['movie'].num_nodes,
+    add_self_loops=False,
+)
+edge_index = train_data['movie', 'movie'].edge_index[:, edge_weight > 0.002]
 
-    return u2imat[:u_num, :i_num]
-
-
-def get_coocur_mat(train_mat, num_neighbors):
-    # Generate the co-occurrence matrix with normalisation and top-k filtering
-    A = train_mat.T @ train_mat
-    n_items = A.shape[0]
-    res_mat = torch.zeros((n_items, num_neighbors))
-    res_sim_mat = torch.zeros((n_items, num_neighbors))
-
-    items_D = torch.sum(A, axis=0).reshape(-1)
-    users_D = torch.sum(A, axis=1).reshape(-1)
-    beta_uD = (1 / torch.sqrt(users_D + 1)).reshape(-1, 1)
-    beta_iD = (1 / torch.sqrt(items_D + 1)).reshape(1, -1)
-    all_ii_constraint_mat = beta_uD @ beta_iD
-
-    norm_A = all_ii_constraint_mat * A
-    res_sim_mat, res_mat = torch.topk(norm_A, num_neighbors, dim=1)
-
-    return res_mat.long(), res_sim_mat.float()
+train_data['movie', 'metapath_0', 'movie'].edge_index = edge_index
+val_data['movie', 'metapath_0', 'movie'].edge_index = edge_index
+test_data['movie', 'metapath_0', 'movie'].edge_index = edge_index
 
 
-def get_i2i_sim_graph(i2imat, i2isimmat):
-    # Generate the i2i graph from the co-occurrence matrix with filters
-    mask, len = i2isimmat > 1e-5, i2imat[0].size(0)
-    mask = torch.tensor([
-        torch.zeros(len, dtype=torch.bool).tolist()
-        if v[0] != i else mask[i].tolist() for i, v in enumerate(i2imat)
-    ])
-    i2imat[~mask] = -1
-    i2i_edge_idx = [[i, j] for i, v in enumerate(i2imat[:, 1:]) for j in v
-                    if j >= 0]
-
-    return torch.tensor(i2i_edge_idx).T
-
-
-u2i_mat = to_u2i_mat(train_data.edge_index_dict[('user', 'rates', 'movie')],
-                     train_data['movie'].x.shape[0],
-                     train_data['user'].x.shape[0])
-i2i_mat, i2i_sim_mat = get_coocur_mat(u2i_mat, 9)
-i2i = get_i2i_sim_graph(i2i_mat, i2i_sim_mat)
-
-# Add the generated i2i graph for high-order information
-train_data[('movie', 'sims', 'movie')].edge_index = i2i.to(device)
-val_data[('movie', 'sims', 'movie')].edge_index = i2i.to(device)
-test_data[('movie', 'sims', 'movie')].edge_index = i2i.to(device)
-
-# We have an unbalanced dataset with many labels for rating 3 and 4, and very
-# few for 0 and 1. Therefore we use a weighted MSE loss.
-if args.use_weighted_loss:
-    weight = torch.bincount(train_data['user', 'movie'].edge_label)
-    weight = weight.max() / weight
-else:
-    weight = None
-
-
-def weighted_mse_loss(pred, target, weight=None):
-    weight = 1. if weight is None else weight[target].to(pred.dtype)
-    return (weight * (pred - target.to(pred.dtype)).pow(2)).mean()
-
-
-class ItemGNNEncoder(torch.nn.Module):
+class MovieGNNEncoder(torch.nn.Module):
     def __init__(self, hidden_channels, out_channels):
         super().__init__()
 
         self.conv1 = SAGEConv(-1, hidden_channels)
-        self.conv2 = SAGEConv(-1, hidden_channels)
-        self.lin1 = Linear(hidden_channels, hidden_channels)
-        self.lin2 = Linear(hidden_channels, out_channels)
+        self.conv2 = SAGEConv(hidden_channels, hidden_channels)
+        self.lin = Linear(hidden_channels, out_channels)
 
-    def forward(self, h, edge_index):
-        h = self.conv1(h, edge_index).relu()
-        h = self.lin1(h).relu()
-        h = self.conv2(h, edge_index).relu()
-        h = self.lin2(h)
-        return h
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index).relu()
+        x = self.conv2(x, edge_index).relu()
+        return self.lin(x)
 
 
 class UserGNNEncoder(torch.nn.Module):
     def __init__(self, hidden_channels, out_channels):
         super().__init__()
-
         self.conv1 = SAGEConv((-1, -1), hidden_channels)
         self.conv2 = SAGEConv((-1, -1), hidden_channels)
         self.conv3 = SAGEConv((-1, -1), hidden_channels)
-        self.lin1 = Linear(hidden_channels, hidden_channels)
-        self.lin2 = Linear(hidden_channels, hidden_channels)
-        self.lin3 = Linear(hidden_channels, out_channels)
+        self.lin = Linear(hidden_channels, out_channels)
 
     def forward(self, x_dict, edge_index_dict):
-        h = x_dict['movie']
-        h = self.conv1(h, edge_index_dict[('movie', 'sims', 'movie')]).relu()
-        h = self.lin1(h).relu()
+        movie_x = self.conv1(
+            x_dict['movie'],
+            edge_index_dict[('movie', 'metapath_0', 'movie')],
+        ).relu()
 
-        x_dict['user'] = self.conv2([x_dict['movie'], x_dict['user']],
-                                    edge_index_dict[('movie', 'rev_rates',
-                                                     'user')]).relu()
-        x_dict['user'] = self.lin2(x_dict['user']).relu()
+        user_x = self.conv2(
+            (x_dict['movie'], x_dict['user']),
+            edge_index_dict[('movie', 'rev_rates', 'user')],
+        ).relu()
 
-        x_dict['user'] = self.conv3([h, x_dict['user']],
-                                    edge_index_dict[('movie', 'rev_rates',
-                                                     'user')]).relu()
-        x_dict['user'] = self.lin3(x_dict['user'])
-        return x_dict['user']
+        user_x = self.conv3(
+            (movie_x, user_x),
+            edge_index_dict[('movie', 'rev_rates', 'user')],
+        ).relu()
+
+        return self.lin(user_x)
 
 
 class EdgeDecoder(torch.nn.Module):
@@ -155,9 +95,9 @@ class EdgeDecoder(torch.nn.Module):
         self.lin1 = Linear(2 * hidden_channels, hidden_channels)
         self.lin2 = Linear(hidden_channels, 1)
 
-    def forward(self, z_dict, edge_label_index):
+    def forward(self, z_src, z_dst, edge_label_index):
         row, col = edge_label_index
-        z = torch.cat([z_dict['user'][row], z_dict['movie'][col]], dim=-1)
+        z = torch.cat([z_src[row], z_dst[col]], dim=-1)
 
         z = self.lin1(z).relu()
         z = self.lin2(z)
@@ -165,37 +105,39 @@ class EdgeDecoder(torch.nn.Module):
 
 
 class Model(torch.nn.Module):
-    def __init__(self, input_size, hidden_channels, out_channels):
+    def __init__(self, num_users, hidden_channels, out_channels):
         super().__init__()
-        self.user_emb = Embedding(input_size, hidden_channels, device=device)
-        self.item_encoder = ItemGNNEncoder(hidden_channels, out_channels)
+        self.user_emb = Embedding(num_users, hidden_channels)
         self.user_encoder = UserGNNEncoder(hidden_channels, out_channels)
+        self.movie_encoder = MovieGNNEncoder(hidden_channels, out_channels)
         self.decoder = EdgeDecoder(hidden_channels)
 
     def forward(self, x_dict, edge_index_dict, edge_label_index):
         z_dict = {}
         x_dict['user'] = self.user_emb(x_dict['user'])
-        z_dict['movie'] = self.item_encoder(
-            x_dict['movie'], edge_index_dict[('movie', 'sims', 'movie')])
         z_dict['user'] = self.user_encoder(x_dict, edge_index_dict)
+        z_dict['movie'] = self.movie_encoder(
+            x_dict['movie'],
+            edge_index_dict[('movie', 'metapath_0', 'movie')],
+        )
+        return self.decoder(z_dict['user'], z_dict['movie'], edge_label_index)
 
-        return self.decoder(z_dict, edge_label_index)
 
-
-model = Model(input_size=data['user'].x.size(0), hidden_channels=64,
-              out_channels=64).to(device)
-
+model = Model(data['user'].num_nodes, hidden_channels=64, out_channels=64)
+model = model.to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.0003)
 
 
 def train():
     model.train()
     optimizer.zero_grad()
-    pred = model(train_data.x_dict, train_data.edge_index_dict,
-                 train_data['user', 'movie'].edge_label_index)
-    target = train_data['user', 'movie'].edge_label
-    loss = weighted_mse_loss(pred, target, weight)
-    loss.backward(retain_graph=True)
+    out = model(
+        train_data.x_dict,
+        train_data.edge_index_dict,
+        train_data['user', 'movie'].edge_label_index,
+    )
+    loss = F.mse_loss(out, train_data['user', 'movie'].edge_label)
+    loss.backward()
     optimizer.step()
     return float(loss)
 
@@ -203,11 +145,12 @@ def train():
 @torch.no_grad()
 def test(data):
     model.eval()
-    pred = model(data.x_dict, data.edge_index_dict,
-                 data['user', 'movie'].edge_label_index)
-    pred = pred.clamp(min=0, max=5)
-    target = data['user', 'movie'].edge_label.float()
-    rmse = F.mse_loss(pred, target).sqrt()
+    out = model(
+        data.x_dict,
+        data.edge_index_dict,
+        data['user', 'movie'].edge_label_index,
+    ).clamp(min=0, max=5)
+    rmse = F.mse_loss(out, data['user', 'movie'].edge_label).sqrt()
     return float(rmse)
 
 
