984af573f4b560c74069f0ef3e0c052400acb010
Rishi Puri <riship@riship-mlt.client.nvidia.com>
2022-11-18 12:13:39 -0800

    wip

--- torch_geometric/nn/to_hetero_transformer.py
+++ torch_geometric/nn/to_hetero_transformer.py
@@ -177,7 +177,7 @@ class ToHeteroModule(Module):
                     f"This may lead to unexpected behaviour.")
             heteromodule = {}
             for edge_type in self.edge_types:
-                heteromodule[edge_type] = copy.deepcopy(module).to(list(module.parameters())[0].device)
+p                heteromodule[edge_type] = copy.deepcopy(module)
                 if hasattr(module, 'reset_parameters'):
                     module.reset_parameters()
                 elif sum([p.numel() for p in module.parameters()]) > 0:
