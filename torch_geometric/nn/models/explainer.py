a5647fb277b370314909d09731764f662424ed15
Jinu Sunil <jinu.sunil@gmail.com>
2022-11-19 10:25:47 +0530

    Update torch_geometric/nn/models/explainer.py
    Co-authored-by: Matthias Fey <matthias.fey@tu-dortmund.de>

--- torch_geometric/nn/models/explainer.py
+++ torch_geometric/nn/models/explainer.py
@@ -204,7 +204,7 @@ def to_captum_input(data: Union[HeteroData, Data], mask_type: str,
                     *args) -> Tuple[Tuple[Tensor], Tuple[Tensor]]:
     r"""Given :obj:`data` and :obj:`mask_type`, converts it to a format to use
     in `Captum.ai <https://captum.ai/>`_ attribution methods. Returns :obj:`inputs`
-    and `additional_forward_args` required for `Captum`s `attribute` functions.
+    and :obj:`additional_forward_args` required for `Captum's :obj:`attribute` functions.
     See :obj:`torch_geometric.nn.to_captum_model` for example usage.
 
     Args:
