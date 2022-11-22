7f6eb1da3ce8b84735b4336229f5640027d4de11
rusty1s <matthias.fey@tu-dortmund.de>
2022-11-22 15:52:10 +0000

    update

--- test/nn/aggr/test_fused.py
+++ test/nn/aggr/test_fused.py
@@ -1,27 +1,28 @@
+import pytest
 import torch
 
 from torch_geometric.nn.aggr.fused import FusedAggregation
 from torch_geometric.nn.resolver import aggregation_resolver
 
 
-def test_fused_aggregation():
+@pytest.mark.parametrize('aggrs', [
+    ['sum', 'mean', 'min', 'max', 'mul', 'var', 'std'],
+    ['sum', 'min', 'max', 'mul', 'var', 'std'],
+    ['min', 'max', 'mul', 'var', 'std'],
+    ['mean', 'min', 'max', 'mul', 'var', 'std'],
+    ['sum', 'min', 'max', 'mul', 'std'],
+    ['mean', 'min', 'max', 'mul', 'std'],
+    ['min', 'max', 'mul', 'std'],
+])
+def test_fused_aggregation(aggrs):
+    aggrs = [aggregation_resolver(aggr) for aggr in aggrs]
+
     x = torch.randn(6, 1)
     y = x.clone()
+    index = torch.tensor([0, 0, 1, 1, 1, 2])
 
     x.requires_grad_(True)
     y.requires_grad_(True)
-
-    index = torch.tensor([0, 0, 1, 1, 1, 2])
-
-    aggrs = [
-        aggregation_resolver('sum'),
-        aggregation_resolver('mean'),
-        aggregation_resolver('min'),
-        aggregation_resolver('max'),
-        aggregation_resolver('mul'),
-        aggregation_resolver('var'),
-        aggregation_resolver('std'),
-    ]
 
     aggr = FusedAggregation(aggrs)
     assert str(aggr) == 'FusedAggregation()'
