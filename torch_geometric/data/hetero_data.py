584b02bb98bf7a56a2adaeb01cb44eee8c926898
rusty1s <matthias.fey@tu-dortmund.de>
2022-11-23 17:37:47 +0000

    update

--- torch_geometric/data/hetero_data.py
+++ torch_geometric/data/hetero_data.py
@@ -479,12 +479,7 @@ class HeteroData(BaseData, FeatureStore, GraphStore):
         return mapping
 
     def _check_type_name(self, name: str):
-        if not name.isidentifier() and not name.isdigit():
-            warnings.warn(f"The type '{name}' contains invalid characters "
-                          f"which might lead to unexpected behavior down the "
-                          f"line. To avoid any issues, ensure that your type "
-                          f"only contains letters, numbers and underscores.")
-        elif '__' in name:
+        if '__' in name:
             warnings.warn(f"The type '{name}' contains double underscores "
                           f"('__') which might lead to unexpected behavior "
                           f"down the line. To avoid any issues, ensure that "
