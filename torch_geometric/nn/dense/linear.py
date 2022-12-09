8d7d891cb06ed72867cd96e77a37703861745c38
jan.toenshoff <jan.toenshoff@rwth-aachen.de>
2022-12-09 18:37:36 +0100

    Fixed sorting of vertex output in HeteroLinear. If "is_sorted=False", the output will be unsorted to the original vertex order of the input.

--- torch_geometric/nn/dense/linear.py
+++ torch_geometric/nn/dense/linear.py
@@ -250,16 +250,24 @@ class HeteroLinear(torch.nn.Module):
         if torch_geometric.typing.WITH_PYG_LIB:
             assert self.weight is not None
 
+            needs_unsort = False
             if not self.is_sorted:
                 if (type_vec[1:] < type_vec[:-1]).any():
                     type_vec, perm = type_vec.sort()
                     x = x[perm]
+                    needs_unsort = True
 
             type_vec_ptr = torch.ops.torch_sparse.ind2ptr(
                 type_vec, self.num_types)
             out = pyg_lib.ops.segment_matmul(x, type_vec_ptr, self.weight)
             if self.bias is not None:
                 out += self.bias[type_vec]
+
+            # restore original order of vertices if necessary
+            if needs_unsort:
+                out_unsorted = torch.empty_like(out)
+                out_unsorted[perm] = out
+                out = out_unsorted
         else:
             assert self.lins is not None
             out = x.new_empty(x.size(0), self.out_channels)
