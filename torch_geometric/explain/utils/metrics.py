1f57eb4c22d08eecb7b0b8b232092db6badf7a70
Emanuel Seemann <3380606+seemanne@users.noreply.github.com>
2022-12-11 14:10:35 +0100

    improve docstrings

--- torch_geometric/explain/utils/metrics.py
+++ torch_geometric/explain/utils/metrics.py
@@ -2,48 +2,60 @@ import torch
 from torch import Tensor
 
 
-def get_characterization_score(fidelity: Tensor,
+def get_characterization_score(fidelities: Tensor,
                                weights: Tensor = torch.tensor([1, 1])) \
         -> 'Tensor':
-    r"""Returns the componentwise characterization score where fidelity[0]
-    is the tensor containing F_{+} and fidelity[1] is the tensor
-    containing F_{-}.
+    r"""Returns the componentwise characterization score from the `"GraphFram
+    Ex:Towards Systematic Evaluation of Explainability Methods for Graph Neur
+    al Networks" <https://arxiv.org/abs/2206.09677>`_ paper.
 
-    The characterization score is calculated as
-     .. math::
+    :obj:`fidelities[0]` is the tensor containing :math:`fid_{+}` and
+    :obj:`fidelities[1]` is the tensor containing :math:`fid_{-}`.
+
+    The characterization score is calculated as:
+
+    .. math::
       charac = \frac{w_+ + w_-}{\frac{w_+}{fid_+}+\frac{w_-}{1-fid_-}}
 
 
     Args:
-        weights (Tensor): Tensor containing the two weights of shape [2]
-        fidelity (Tensor): Tensor containing fidelities of shape [2, n]
+        fidelities (Tensor): Tensor containing fidelities of shape [2, n]
+        weights (Tensor, optional): Tensor containing the two weights of
+            shape [2] (default: :obj:`Tensor(1,1)`)
+
 
     Returns:
-        Tensor: Tensor containing componentwise characterization
+        :class:`Tensor`: Tensor containing componentwise characterization
         score of shape [n]
     """
-    components = torch.stack([fidelity[0], 1 - fidelity[1]])
+    components = torch.stack([fidelities[0], 1 - fidelities[1]])
     normed_weights = weights.div(weights.sum())
 
     return _weighted_harmonic_mean(normed_weights, components)
 
 
 def get_fidelity_curve_auc(fidelities: Tensor, x: Tensor) -> 'Tensor':
-    r"""Returns the AUC for the fidelity curve
-    More precisely returns the AUC for:
+    r"""Returns the AUC for the fidelity curve from the `"GraphFramEx:
+    Towards Systematic Evaluation of Explainability Methods for Graph
+    Neural Networks" <https://arxiv.org/abs/2206.09677>`_ paper.
 
-     .. math::
-        y = \frac{Fid_{+}}{1 - Fid_{-}}
+    :obj:`fidelities[0]` is the tensor containing :math:`fid_{+}` and
+    :obj:`fidelities[1]` is the tensor containing :math:`fid_{-}`.
 
-    Expects the input to be sorted, i.e. x should be an ascending sequence.
+    More precisely, returns the AUC of:
+
+    .. math::
+        f(x) = \frac{fid_{+}}{1 - fid_{-}}
+
+    .. note::
+        Expects the input to be sorted, i.e. x should be an ascending sequence.
 
     Args:
         fidelities (Tensor): Tensor containing fidelities
         x (Tensor): Tensor containing x-axis
 
 
-    Returns:
-        Tensor: area under the fidelity curve
+    :rtype: :class:`Tensor`
     """
 
     if torch.any(fidelities[1] == 1):
@@ -74,7 +86,9 @@ def _weighted_harmonic_mean(weights: Tensor, components: Tensor) -> 'Tensor':
         components (Tensor): Tensor containing components of shape [2, n]
 
     Returns:
-        Tensor: Tensor containing pairwise weighted harmonic means of shape [n]
+        :class:`Tensor`: Tensor containing pairwise weighted harmonic means
+            of shape [n]
+
     """
     if round(weights.sum().item(), 2) != 1:
         raise ValueError('weights must add up to 1')
