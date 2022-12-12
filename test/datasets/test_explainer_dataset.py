529a9f7d3e4794013bcb9756d1f809745619f26e
rusty1s <matthias.fey@tu-dortmund.de>
2022-12-12 10:07:14 +0000

    update

--- test/datasets/test_explainer_dataset.py
+++ test/datasets/test_explainer_dataset.py
@@ -2,25 +2,22 @@ import pytest
 import torch
 
 from torch_geometric import seed_everything
-from torch_geometric.data import Data
 from torch_geometric.datasets import ExplainerDataset
-from torch_geometric.datasets.graph_generator import GraphGenerator
+from torch_geometric.datasets.graph_generator import BAGraph
 from torch_geometric.datasets.motif_generator import HouseMotif
-from torch_geometric.utils import barabasi_albert_graph
 
 
-# TODO Remove after the BAGraph is merged.
-class BAGraph(GraphGenerator):
-    def __call__(self):
-        edge_index = barabasi_albert_graph(num_nodes=80, num_edges=5)
-        return Data(num_nodes=80, edge_index=edge_index)
-
-
-@pytest.mark.parametrize('graph_generator', [BAGraph()])
-@pytest.mark.parametrize('motif_generator', [HouseMotif(), 'house'])
+@pytest.mark.parametrize('graph_generator', [
+    BAGraph(num_nodes=80, num_edges=5),
+])
+@pytest.mark.parametrize('motif_generator', [
+    HouseMotif(),
+    'house',
+])
 def test_explainer_dataset_ba_house(graph_generator, motif_generator):
     dataset = ExplainerDataset(graph_generator, motif_generator, num_motifs=2)
-    assert str(dataset) == ('ExplainerDataset(1, graph_generator=BAGraph(), '
+    assert str(dataset) == ('ExplainerDataset(1, graph_generator='
+                            'BAGraph(num_nodes=80, num_edges=5), '
                             'motif_generator=HouseMotif(), num_motifs=2)')
     assert len(dataset) == 1
 
@@ -40,11 +37,11 @@ def test_explainer_dataset_ba_house(graph_generator, motif_generator):
 
 def test_explainer_dataset_reproducibility():
     seed_everything(12345)
-    dataset = ExplainerDataset(BAGraph(), HouseMotif(), num_motifs=2)
-    data1 = dataset[0]
+    data1 = ExplainerDataset(BAGraph(num_nodes=80, num_edges=5), HouseMotif(),
+                             num_motifs=2)[0]
 
     seed_everything(12345)
-    dataset = ExplainerDataset(BAGraph(), HouseMotif(), num_motifs=2)
-    data2 = dataset[0]
+    data2 = ExplainerDataset(BAGraph(num_nodes=80, num_edges=5), HouseMotif(),
+                             num_motifs=2)[0]
 
     assert torch.equal(data1.edge_index, data2.edge_index)
--- torch_geometric/datasets/graph_generator/ba_graph.py
+++ torch_geometric/datasets/graph_generator/ba_graph.py
@@ -1,34 +1,25 @@
-from typing import Callable, Optional
-
+from torch_geometric.data import Data
+from torch_geometric.datasets.graph_generator import GraphGenerator
 from torch_geometric.utils import barabasi_albert_graph
-
-from .graph_generator import GraphGenerator
 
 
 class BAGraph(GraphGenerator):
-    r"""Generate base graph for Barabasi-Albert (BA) graph.
+    r"""Generates random Barabasi-Albert (BA) graphs.
+    See :meth:`~torch_geometric.utils.barabasis_albert_graph` for more
+    information.
 
     Args:
-        num_nodes (int, optional): Specifies the number of the nodes in the
-            base graph. (default: :obj:`300`)
-        num_edges: number of edges
-        num_motifs: number of motifs to attach to the base graph
-        motif (Motif, optional): the Motif object
+        num_nodes (int): The number of nodes.
+        num_edges (int): The number of edges from a new node to existing nodes.
     """
-    def __init__(
-        self,
-        num_nodes: int = 300,
-        num_edges: int = 5,
-        num_motifs: int = 80,
-        motif: Optional[Callable] = None,
-        seed: int = None,
-    ):
+    def __init__(self, num_nodes: int, num_edges: int):
+        self.num_nodes = num_nodes
         self.num_edges = num_edges
-        self.num_motifs = num_motifs
-        super().__init__(num_nodes, motif, seed)
 
-    def generate_base_graph(self) -> None:
-        self._edge_index = barabasi_albert_graph(self.num_nodes,
-                                                 self.num_edges)
-        self.attach_motif(self.num_motifs)
-        self.generate_feature(num_features=10)
+    def __call__(self) -> Data:
+        edge_index = barabasi_albert_graph(self.num_nodes, self.num_edges)
+        return Data(num_nodes=self.num_nodes, edge_index=edge_index)
+
+    def __repr__(self) -> str:
+        return (f'{self.__class__.__name__}(num_nodes={self.num_nodes}, '
+                f'num_edges={self.num_edges})')
