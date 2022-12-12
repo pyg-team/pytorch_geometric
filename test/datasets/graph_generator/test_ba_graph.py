5692983bc31432641081306a3e0f01313355e2ed
rusty1s <matthias.fey@tu-dortmund.de>
2022-12-12 10:17:41 +0000

    update

--- test/datasets/graph_generator/test_ba_graph.py
+++ test/datasets/graph_generator/test_ba_graph.py
@@ -1,23 +1,9 @@
-from torch_geometric.datasets import ExplainerDataset
-from torch_geometric.datasets.generators import BAGraph, Motif
+from torch_geometric.datasets.graph_generator import BAGraph
 
 
-def test_base_graph():
-    generator = BAGraph()
-    dataset = ExplainerDataset(generator)
-    data = dataset[0]
-    assert data.x.size() == (300, 10)
-    assert data.edge_index.size(1) >= 1120
-
-
-def test_graph_with_motif():
-    motif = Motif('house')
-    generator = BAGraph(num_nodes=300, num_motifs=80, motif=motif)
-    dataset = ExplainerDataset(generator)
-    data = dataset[0]
-
-    assert data.edge_index.size(1) >= 1120
-    assert data.x.size() == (700, 10)
-    assert data.y.size() == (700, )
-    assert data.expl_mask.sum() == 60
-    assert data.edge_label.sum() == 960
+def test_ba_graph():
+    graph_generator = BAGraph(num_nodes=300, num_edges=5)
+    data = graph_generator()
+    assert len(data) == 2
+    assert data.num_nodes == 300
+    assert data.num_edges <= 2 * 300 * 5
