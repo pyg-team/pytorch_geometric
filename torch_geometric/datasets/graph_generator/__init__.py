f59841000b04564e48054755671917badc99c833
Matthias Fey <matthias.fey@tu-dortmund.de>
2022-12-13 10:34:25 +0100

    Update torch_geometric/datasets/graph_generator/__init__.py

--- torch_geometric/datasets/graph_generator/__init__.py
+++ torch_geometric/datasets/graph_generator/__init__.py
@@ -3,4 +3,4 @@ from .ba_graph import BAGraph
 from .er_graph import ERGraph
 from .grid_graph import GridGraph
 
-__all__ = classes = ['GraphGenerator', 'BAGraph', 'ERGraph', 'GridGraph']
+__all__ = classes = ['GraphGenerator', 'BAGraph', 'ERGraph', 'GridGraph', ]
