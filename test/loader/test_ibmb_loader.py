8fa3e16400cdaec3f8f2d1f971536600ceec3ce6
Chendi Qian <1049653012@qq.com>
2022-12-13 22:07:34 +0800

    add test

--- test/loader/test_ibmb_loader.py
+++ test/loader/test_ibmb_loader.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import numpy as np
 import torch
 
 from torch_geometric.data import Data
@@ -16,24 +17,56 @@ def test_graph_ibmb():
     y = torch.arange(num_nodes) % 3
 
     graph = Data(x=x, y=y, edge_index=edge_index)
+
     torch.manual_seed(42)
     train_indices = torch.unique(torch.randint(high=33, size=(20, )))
+
     batch_loader = IBMBBatchLoader(
         graph, batch_order='order', num_partitions=4,
         output_indices=train_indices, return_edge_index_type='edge_index',
         batch_expand_ratio=1., metis_output_weight=None, batch_size=1,
         shuffle=False)
-    batches1 = [b for b in batch_loader]
-    assert len(batches1) == 4
+    batches = [b for b in batch_loader]
+    assert len(batches) == 4
     assert sum([b.output_node_mask.sum()
-                for b in batches1]) == len(train_indices)
+                for b in batches]) == len(train_indices)
+
     node_loader = IBMBNodeLoader(graph, batch_order='order',
                                  output_indices=train_indices,
                                  return_edge_index_type='edge_index',
                                  num_auxiliary_node_per_output=4,
                                  num_output_nodes_per_batch=4, batch_size=1,
                                  shuffle=False)
-    batches2 = [b for b in node_loader]
-    assert len(batches2) == 4
+    batches = [b for b in node_loader]
+    assert len(batches) == 4
     assert sum([b.output_node_mask.sum()
-                for b in batches2]) == len(train_indices)
+                for b in batches]) == len(train_indices)
+
+    batch_loader = IBMBBatchLoader(
+        graph, batch_order='order', num_partitions=8,
+        output_indices=train_indices, return_edge_index_type='edge_index',
+        batch_expand_ratio=1., metis_output_weight=None, batch_size=1,
+        shuffle=False)
+
+    batches = [b for b in batch_loader]
+    assert len(batches) == 7
+    assert sum([b.output_node_mask.sum()
+                for b in batches]) == len(train_indices)
+
+    node_loader = IBMBNodeLoader(graph, batch_order='order',
+                                 output_indices=train_indices,
+                                 return_edge_index_type='edge_index',
+                                 num_auxiliary_node_per_output=4,
+                                 num_output_nodes_per_batch=2, batch_size=1,
+                                 shuffle=False)
+
+    batches = [b for b in node_loader]
+    assert len(batches) == 8
+    assert sum([b.output_node_mask.sum()
+                for b in batches]) == len(train_indices)
+
+    for out, aux in node_loader.node_wise_out_aux_pairs:
+        assert np.all(np.in1d(out, aux))
+
+    for out, aux in batch_loader.batch_wise_out_aux_pairs:
+        assert np.all(np.in1d(out, aux))
