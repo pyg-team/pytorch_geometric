518fe332a22223d3327a9649fd6dfe5a31716937
Shweta Jacob <shwetaaj59@gmail.com>
2022-12-12 23:20:34 -0500

    Update test for groundtruth metrics

--- test/explain/test_metrics.py
+++ test/explain/test_metrics.py
@@ -18,20 +18,54 @@ def data():
     )
 
 
-def create_random_explanation(
+def create_groundtruth(
     data: Data,
     node_mask: bool = True,
     edge_mask: bool = True,
     node_feat_mask: bool = True,
     edge_feat_mask: bool = True,
 ):
-    node_mask = torch.rand(data.x.size(0)) if node_mask else None
-    edge_mask = torch.rand(data.edge_index.size(1)) if edge_mask else None
-    node_feat_mask = torch.rand_like(data.x) if node_feat_mask else None
-    edge_feat_mask = (torch.rand_like(data.edge_attr)
-                      if edge_feat_mask else None)
+    node_mask = torch.Tensor([0.123, 0.617, 0.815, 0.900
+                              ]) if node_mask else None
+    edge_mask = torch.Tensor([0.243, 0.660, 0.780, 0.000, 0.000, 1.000
+                              ]) if edge_mask else None
+    node_feat_mask = torch.Tensor(
+        [[0.000, 0.450, 1.000], [0.234, 0.100, 0.000], [1.000, 1.000, 0.000],
+         [0.000, 0.000, 0.100]]) if node_feat_mask else None
+    edge_feat_mask = torch.Tensor(
+        [[0.600, 0.400, 0.000], [0.234, 0.100, 0.000], [1.000, 0.000, 0.000],
+         [0.000, 0.000, 0.100]]) if edge_feat_mask else None
 
-    return Explanation(  # Create explanation.
+    return Explanation(
+        node_mask=node_mask,
+        edge_mask=edge_mask,
+        node_feat_mask=node_feat_mask,
+        edge_feat_mask=edge_feat_mask,
+        x=data.x,
+        edge_index=data.edge_index,
+        edge_attr=data.edge_attr,
+    )
+
+
+def create_explanation(
+    data: Data,
+    node_mask: bool = True,
+    edge_mask: bool = True,
+    node_feat_mask: bool = True,
+    edge_feat_mask: bool = True,
+):
+    node_mask = torch.Tensor([0.245, 0.312, 0.670, 0.899
+                              ]) if node_mask else None
+    edge_mask = torch.Tensor([0.342, 0.660, 0.780, 0.000, 1.000, 0.000
+                              ]) if edge_mask else None
+    node_feat_mask = torch.Tensor(
+        [[0.000, 0.450, 0.990], [0.234, 0.100, 0.000], [1.000, 1.000, 0.000],
+         [0.000, 0.000, 0.100]]) if node_feat_mask else None
+    edge_feat_mask = torch.Tensor(
+        [[0.600, 0.400, 0.000], [0.234, 0.100, 0.000], [1.000, 0.500, 0.000],
+         [0.100, 0.000, 0.100]]) if edge_feat_mask else None
+
+    return Explanation(
         node_mask=node_mask,
         edge_mask=edge_mask,
         node_feat_mask=node_feat_mask,
@@ -43,35 +77,23 @@ def create_random_explanation(
 
 
 def test_masks(data):
-
-    ex_node_mask = [True, False]
-    ex_edge_mask = [True, False]
-    ex_node_feat_mask = [True, False]
-    ex_edge_feat_mask = [True, False]
-    gt_node_mask = [True, False]
-    gt_edge_mask = [True, False]
-    gt_node_feat_mask = [True, False]
-    gt_edge_feat_mask = [True, False]
-
-    explanation = create_random_explanation(
+    ground_truth = create_groundtruth(
         data,
-        node_mask=ex_node_mask,
-        edge_mask=ex_edge_mask,
-        node_feat_mask=ex_node_feat_mask,
-        edge_feat_mask=ex_edge_feat_mask,
+        node_mask=True,
+        edge_mask=True,
+        node_feat_mask=True,
+        edge_feat_mask=False,
     )
-
-    groundtruth = create_random_explanation(
+    explanation = create_explanation(
         data,
-        node_mask=gt_node_mask,
-        edge_mask=gt_edge_mask,
-        node_feat_mask=gt_node_feat_mask,
-        edge_feat_mask=gt_edge_feat_mask,
+        node_mask=True,
+        edge_mask=True,
+        node_feat_mask=True,
+        edge_feat_mask=False,
     )
-
-    accuracy_metrics = get_groundtruth_metrics(explanation, groundtruth)
-    assert accuracy_metrics[0] == 1.0
-    assert accuracy_metrics[1] == 1.0
-    assert accuracy_metrics[2] == 1.0
-    assert accuracy_metrics[3] == 1.0
-    assert accuracy_metrics[4] == 0.5
+    accuracy_metrics = get_groundtruth_metrics(explanation, ground_truth)
+    assert torch.round(accuracy_metrics[0], decimals=4) == torch.tensor(0.9091)
+    assert torch.round(accuracy_metrics[1], decimals=4) == torch.tensor(0.9333)
+    assert torch.round(accuracy_metrics[2], decimals=4) == torch.tensor(0.9333)
+    assert torch.round(accuracy_metrics[3], decimals=4) == torch.tensor(0.9333)
+    assert torch.round(accuracy_metrics[4], decimals=4) == torch.tensor(1.0)
