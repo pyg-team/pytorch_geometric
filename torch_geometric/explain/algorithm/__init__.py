631528664761fc86a072bc78335d87a03c42277b
Nikhil Shenoy <shenoy@login01.sockeye.arc.ubc.ca>
2022-12-13 22:52:11 -0800

    updated the __init__ method

--- torch_geometric/explain/algorithm/__init__.py
+++ torch_geometric/explain/algorithm/__init__.py
@@ -1,9 +1,11 @@
 from .base import ExplainerAlgorithm
 from .dummy_explainer import DummyExplainer
 from .gnn_explainer import GNNExplainer
+from .subgraphX_explainer import SubgraphXExplainer
 
 __all__ = classes = [
     'ExplainerAlgorithm',
     'DummyExplainer',
     'GNNExplainer',
+    'SubgraphXExplainer'
 ]
