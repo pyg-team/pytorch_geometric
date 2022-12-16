c97693f2cc40d79a902953ad3abc93bf2751c7c3
rusty1s <matthias.fey@tu-dortmund.de>
2022-12-16 09:21:43 +0000

    update

--- test/explain/metric/test_basic.py
+++ test/explain/metric/test_basic.py
@@ -1,184 +1,43 @@
-import pytest
 import torch
 
-from torch_geometric.data import Data
-from torch_geometric.explain import Explanation
-from torch_geometric.explain.metric.basic import get_groundtruth_metrics
+from torch_geometric.explain import groundtruth_metrics
 
 
-@pytest.fixture
-def data1():
-    return Data(
-        x=torch.randn(4, 3),
-        edge_index=torch.tensor([
-            [0, 0, 0, 1, 1, 2],
-            [1, 2, 3, 2, 3, 3],
-        ]),
-        edge_attr=torch.randn(6, 3),
-    )
+def test_groundtruth_metrics():
+    pred_mask = torch.rand(10)
+    target_mask = torch.rand(10)
+
+    accuracy, recall, precision, f1_score, auroc = groundtruth_metrics(
+        pred_mask, target_mask)
+
+    assert accuracy >= 0.0 and accuracy <= 1.0
+    assert recall >= 0.0 and recall <= 1.0
+    assert precision >= 0.0 and precision <= 1.0
+    assert f1_score >= 0.0 and f1_score <= 1.0
+    assert auroc >= 0.0 and auroc <= 1.0
 
 
-@pytest.fixture
-def data2():
-    return Data(
-        x=torch.randn(3, 2),
-        edge_index=torch.tensor([
-            [0, 0, 0],
-            [1, 2, 3],
-        ]),
-        edge_attr=torch.randn(3, 2),
-    )
+def test_perfect_groundtruth_metrics():
+    pred_mask = target_mask = torch.rand(10)
+
+    accuracy, recall, precision, f1_score, auroc = groundtruth_metrics(
+        pred_mask, target_mask)
+
+    assert accuracy == 1.0
+    assert recall == 1.0
+    assert precision == 1.0
+    assert f1_score == 1.0
+    assert auroc == 1.0
 
 
-def create_groundtruth(
-    data: Data,
-    node_mask: bool = True,
-    edge_mask: bool = True,
-    node_feat_mask: bool = True,
-    edge_feat_mask: bool = True,
-):
-    node_mask = torch.Tensor([0.123, 0.617, 0.815, 0.900
-                              ]) if node_mask else None
-    edge_mask = torch.Tensor([0.243, 0.660, 0.780, 0.000, 0.000, 1.000
-                              ]) if edge_mask else None
-    node_feat_mask = torch.Tensor(
-        [[0.000, 0.450, 1.000], [0.234, 0.100, 0.000], [1.000, 1.000, 0.000],
-         [0.000, 0.000, 0.100]]) if node_feat_mask else None
-    edge_feat_mask = torch.Tensor(
-        [[0.600, 0.400, 0.000], [0.234, 0.100, 0.000], [1.000, 0.000, 0.000],
-         [0.000, 0.000, 0.100]]) if edge_feat_mask else None
+def test_groundtruth_true_negative():
+    pred_mask = target_mask = torch.zeros(10)
 
-    return Explanation(
-        node_mask=node_mask,
-        edge_mask=edge_mask,
-        node_feat_mask=node_feat_mask,
-        edge_feat_mask=edge_feat_mask,
-        x=data.x,
-        edge_index=data.edge_index,
-        edge_attr=data.edge_attr,
-    )
+    accuracy, recall, precision, f1_score, auroc = groundtruth_metrics(
+        pred_mask, target_mask)
 
-
-def create_explanation(
-    data: Data,
-    node_mask: bool = True,
-    edge_mask: bool = True,
-    node_feat_mask: bool = True,
-    edge_feat_mask: bool = True,
-):
-    node_mask = torch.Tensor([0.245, 0.312, 0.670, 0.899
-                              ]) if node_mask else None
-    edge_mask = torch.Tensor([0.342, 0.660, 0.780, 0.000, 1.000, 0.000
-                              ]) if edge_mask else None
-    node_feat_mask = torch.Tensor(
-        [[0.000, 0.450, 0.990], [0.234, 0.100, 0.000], [1.000, 1.000, 0.000],
-         [0.000, 0.000, 0.100]]) if node_feat_mask else None
-    edge_feat_mask = torch.Tensor(
-        [[0.600, 0.400, 0.000], [0.234, 0.100, 0.000], [1.000, 0.500, 0.000],
-         [0.100, 0.000, 0.100]]) if edge_feat_mask else None
-
-    return Explanation(
-        node_mask=node_mask,
-        edge_mask=edge_mask,
-        node_feat_mask=node_feat_mask,
-        edge_feat_mask=edge_feat_mask,
-        x=data.x,
-        edge_index=data.edge_index,
-        edge_attr=data.edge_attr,
-    )
-
-
-def create_groundtruth_with_only_tn(
-    data: Data,
-    node_mask: bool = True,
-    edge_mask: bool = True,
-    node_feat_mask: bool = True,
-    edge_feat_mask: bool = True,
-):
-    node_mask = torch.Tensor([0.000, 0.000, 0.000]) if node_mask else None
-    edge_mask = torch.Tensor([0.000, 0.000, 0.000]) if edge_mask else None
-    node_feat_mask = torch.Tensor([[0.000, 0.000], [0.000, 0.000],
-                                   [0.000, 0.000]]) if node_feat_mask else None
-    edge_feat_mask = torch.Tensor([[0.000, 0.000], [0.000, 0.000],
-                                   [0.000, 0.000]]) if edge_feat_mask else None
-
-    return Explanation(
-        node_mask=node_mask,
-        edge_mask=edge_mask,
-        node_feat_mask=node_feat_mask,
-        edge_feat_mask=edge_feat_mask,
-        x=data.x,
-        edge_index=data.edge_index,
-        edge_attr=data.edge_attr,
-    )
-
-
-def create_explanation_with_only_tn(
-    data: Data,
-    node_mask: bool = True,
-    edge_mask: bool = True,
-    node_feat_mask: bool = True,
-    edge_feat_mask: bool = True,
-):
-    node_mask = torch.Tensor([0.000, 0.000, 0.000]) if node_mask else None
-    edge_mask = torch.Tensor([0.000, 0.000, 0.000]) if edge_mask else None
-    node_feat_mask = torch.Tensor([[0.000, 0.000], [0.000, 0.000],
-                                   [0.000, 0.000]]) if node_feat_mask else None
-    edge_feat_mask = torch.Tensor([[0.000, 0.000], [0.000, 0.000],
-                                   [0.000, 0.000]]) if edge_feat_mask else None
-
-    return Explanation(
-        node_mask=node_mask,
-        edge_mask=edge_mask,
-        node_feat_mask=node_feat_mask,
-        edge_feat_mask=edge_feat_mask,
-        x=data.x,
-        edge_index=data.edge_index,
-        edge_attr=data.edge_attr,
-    )
-
-
-def test_accuracy_metrics(data1):
-    ground_truth = create_groundtruth(
-        data1,
-        node_mask=True,
-        edge_mask=True,
-        node_feat_mask=True,
-        edge_feat_mask=False,
-    )
-    explanation = create_explanation(
-        data1,
-        node_mask=True,
-        edge_mask=True,
-        node_feat_mask=True,
-        edge_feat_mask=False,
-    )
-    accuracy_metrics = get_groundtruth_metrics(explanation, ground_truth)
-    assert torch.round(accuracy_metrics[0], decimals=4) == torch.tensor(0.9091)
-    assert torch.round(accuracy_metrics[1], decimals=4) == torch.tensor(0.9333)
-    assert torch.round(accuracy_metrics[2], decimals=4) == torch.tensor(0.9333)
-    assert torch.round(accuracy_metrics[3], decimals=4) == torch.tensor(0.9333)
-    assert torch.round(accuracy_metrics[4], decimals=4) == torch.tensor(0.8381)
-
-
-def test_accuracy_metrics_when_precision_and_recall_is_zero(data2):
-    ground_truth = create_groundtruth_with_only_tn(
-        data2,
-        node_mask=True,
-        edge_mask=True,
-        node_feat_mask=False,
-        edge_feat_mask=False,
-    )
-    explanation = create_explanation_with_only_tn(
-        data2,
-        node_mask=True,
-        edge_mask=True,
-        node_feat_mask=False,
-        edge_feat_mask=False,
-    )
-    accuracy_metrics = get_groundtruth_metrics(explanation, ground_truth)
-    assert torch.round(accuracy_metrics[0], decimals=4) == torch.tensor(1.)
-    assert torch.round(accuracy_metrics[1], decimals=4) == torch.tensor(0.)
-    assert torch.round(accuracy_metrics[2], decimals=4) == torch.tensor(0.)
-    assert torch.round(accuracy_metrics[3], decimals=4) == torch.tensor(0.)
-    assert torch.round(accuracy_metrics[4], decimals=4) == torch.tensor(0.)
+    assert accuracy == 1.0
+    assert recall == 0.0
+    assert precision == 0.0
+    assert f1_score == 0.0
+    assert auroc == 0.0
--- torch_geometric/explain/metric/__init__.py
+++ torch_geometric/explain/metric/__init__.py
@@ -1,6 +1,8 @@
+from .basic import groundtruth_metrics
 from .fidelity import characterization_score, fidelity_curve_auc
 
 __all__ = classes = [
+    'groundtruth_metrics',
     'characterization_score',
     'fidelity_curve_auc',
 ]
