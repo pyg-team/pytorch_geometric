fdac23417d6378dfb1c6e968ee1e31a490e235b5
fork123aniket <fork123aniket>
2022-12-22 18:43:09 +0530

    __init__.py file for GraphMask Implementation

--- torch_geometric/explain/algorithm/__init__.py
+++ torch_geometric/explain/algorithm/__init__.py
@@ -2,10 +2,12 @@ from .base import ExplainerAlgorithm
 from .dummy_explainer import DummyExplainer
 from .gnn_explainer import GNNExplainer
 from .attention_explainer import AttentionExplainer
+from .graphmask_explainer import GraphMaskExplainer
 
 __all__ = classes = [
     'ExplainerAlgorithm',
     'DummyExplainer',
     'GNNExplainer',
+    'GraphMaskExplainer'
     'AttentionExplainer',
 ]
