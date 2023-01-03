4b24568af3c4bc6f17c498ab35eb7ed816bdb94b
rusty1s <matthias.fey@tu-dortmund.de>
2023-01-03 10:21:55 +0000

    update

--- test/utils/test_nested.py
+++ test/utils/test_nested.py
@@ -3,22 +3,32 @@ import torch
 from torch_geometric.utils import from_nested_tensor, to_nested_tensor
 
 
-def test_to_and_from_nested_tensor():
-    x = torch.randn(5, 5)
-    batch = torch.tensor([0, 0, 1, 1, 1])
+def test_to_nested_tensor():
+    x = torch.randn(5, 4, 3)
 
-    nested = to_nested_tensor(x, batch)
-    # print(nested)
-    # print(nested.is_nested)
-    # print(out)
-    # # print(out.__class__.__dict__)
-    # print(out.__class__)
-    # print(out.storage()._storage.__dict__)
-    # out = out.contiguous()
-    # # print(out.storage().__dict__)
+    out = to_nested_tensor(x, batch=torch.tensor([0, 0, 1, 1, 1]))
+    out = out.to_padded_tensor(padding=0)
+    assert out.size() == (2, 3, 4, 3)
+    assert torch.allclose(out[0, :2], x[0:2])
+    assert torch.allclose(out[1, :3], x[2:5])
 
-    # bla = torch.tensor(out.storage())
-    # print(bla)
-    # print(bla.shape)
+    out = to_nested_tensor(x, ptr=torch.tensor([0, 2, 5]))
+    out = out.to_padded_tensor(padding=0)
+    assert out.size() == (2, 3, 4, 3)
+    assert torch.allclose(out[0, :2], x[0:2])
+    assert torch.allclose(out[1, :3], x[2:5])
 
-    from_nested_tensor(nested)
+    out = to_nested_tensor(x)
+    out = out.to_padded_tensor(padding=0)
+    assert out.size() == (1, 5, 4, 3)
+    assert torch.allclose(out[0], x)
+
+
+def test_from_nested_tensor():
+    x = torch.randn(5, 4, 3)
+
+    nested = to_nested_tensor(x, batch=torch.tensor([0, 0, 1, 1, 1]))
+    out, batch = from_nested_tensor(nested)
+
+    assert torch.equal(x, out)
+    assert batch.tolist() == [0, 0, 1, 1, 1]
