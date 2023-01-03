18277ee9f9eb5ce448bf07fd2698f8dd0095e3ba
rusty1s <matthias.fey@tu-dortmund.de>
2023-01-03 19:26:33 +0000

    update<

--- torch_geometric/nn/aggr/set_transformer.py
+++ torch_geometric/nn/aggr/set_transformer.py
@@ -1,97 +1,29 @@
-import math
 from typing import Optional
 
 import torch
-import torch.nn.functional as F
-from torch import Tensor, nn
+from torch import Tensor
 
 from torch_geometric.nn.aggr import Aggregation
-
-# Set Transformer code as provided in
-# https://github.com/juho-lee/set_transformer
+from torch_geometric.nn.aggr.gmt import PMA, SAB
 
 
-class MAB(nn.Module):
-    def __init__(self, dim_Q, dim_K, dim_V, num_heads, ln=False):
-        super(MAB, self).__init__()
-        self.dim_V = dim_V
-        self.num_heads = num_heads
-        self.fc_q = nn.Linear(dim_Q, dim_V)
-        self.fc_k = nn.Linear(dim_K, dim_V)
-        self.fc_v = nn.Linear(dim_K, dim_V)
-        if ln:
-            self.ln0 = nn.LayerNorm(dim_V)
-            self.ln1 = nn.LayerNorm(dim_V)
-        self.fc_o = nn.Linear(dim_V, dim_V)
-
-    def forward(self, Q, K):
-        Q = self.fc_q(Q)
-        K, V = self.fc_k(K), self.fc_v(K)
-
-        dim_split = self.dim_V // self.num_heads
-        Q_ = torch.cat(Q.split(dim_split, 2), 0)
-        K_ = torch.cat(K.split(dim_split, 2), 0)
-        V_ = torch.cat(V.split(dim_split, 2), 0)
-
-        A = torch.softmax(
-            Q_.bmm(K_.transpose(1, 2)) / math.sqrt(self.dim_V), 2)
-        Out = torch.cat((Q_ + A.bmm(V_)).split(Q.size(0), 0), 2)
-        Out = Out if getattr(self, 'ln0', None) is None else self.ln0(Out)
-        Out = Out + F.relu(self.fc_o(Out))
-        Out = Out if getattr(self, 'ln1', None) is None else self.ln1(Out)
-        return Out
-
-
-class SAB(nn.Module):
-    def __init__(self, dim_in, dim_out, num_heads, ln=False):
-        super(SAB, self).__init__()
-        self.mab = MAB(dim_in, dim_in, dim_out, num_heads, ln=ln)
-
-    def forward(self, X):
-        return self.mab(X, X)
-
-
-class ISAB(nn.Module):
-    def __init__(self, dim_in, dim_out, num_heads, num_inds, ln=False):
-        super(ISAB, self).__init__()
-        self.Induced = nn.Parameter(torch.Tensor(1, num_inds, dim_out))
-        nn.init.xavier_uniform_(self.Induced)
-        self.mab0 = MAB(dim_out, dim_in, dim_out, num_heads, ln=ln)
-        self.mab1 = MAB(dim_in, dim_out, dim_out, num_heads, ln=ln)
-
-    def forward(self, X):
-        H = self.mab0(self.Induced.repeat(X.size(0), 1, 1), X)
-        return self.mab1(X, H)
-
-
-class PMA(nn.Module):
-    def __init__(self, dim, num_heads, num_seeds, ln=False):
-        super(PMA, self).__init__()
-        self.S = nn.Parameter(torch.Tensor(1, num_seeds, dim))
-        nn.init.xavier_uniform_(self.S)
-        self.mab = MAB(dim, dim, dim, num_heads, ln=ln)
-
-    def forward(self, X):
-        return self.mab(self.S.repeat(X.size(0), 1, 1), X)
-
-
-class SetTransformer(nn.Module):
+class SetTransformer(torch.nn.Module):
     def __init__(self, dim_input: int, num_outputs: int, dim_output: int,
                  num_enc_SABs: int, num_dec_SABs: int, dim_hidden: int = 128,
                  num_heads: int = 4, ln: bool = False):
-        super(SetTransformer, self).__init__()
+        super().__init__()
 
-        self.enc = nn.Sequential(
-            SAB(dim_input, dim_hidden, num_heads, ln=ln), *[
-                SAB(dim_hidden, dim_hidden, num_heads, ln=ln)
+        self.enc = torch.nn.Sequential(
+            SAB(dim_input, dim_hidden, num_heads, layer_norm=ln), *[
+                SAB(dim_hidden, dim_hidden, num_heads, layer_norm=ln)
                 for _ in range(num_enc_SABs)
             ])
 
-        self.dec = nn.Sequential(
-            PMA(dim_hidden, num_heads, num_outputs, ln=ln), *[
-                SAB(dim_hidden, dim_hidden, num_heads, ln=ln)
+        self.dec = torch.nn.Sequential(
+            PMA(dim_hidden, num_heads, num_outputs, layer_norm=ln), *[
+                SAB(dim_hidden, dim_hidden, num_heads, layer_norm=ln)
                 for _ in range(num_dec_SABs)
-            ], nn.Linear(dim_hidden, dim_output))
+            ], torch.nn.Linear(dim_hidden, dim_output))
 
     def forward(self, X):
         return self.dec(self.enc(X))
