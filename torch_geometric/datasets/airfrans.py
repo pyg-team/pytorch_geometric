cfc8b7a2a8236a70c73f8c5be6073d507b293177
Matthias Fey <matthias.fey@tu-dortmund.de>
2023-01-05 08:35:12 +0100

    Update torch_geometric/datasets/airfrans.py

--- torch_geometric/datasets/airfrans.py
+++ torch_geometric/datasets/airfrans.py
@@ -37,7 +37,7 @@ class AirfRANS(InMemoryDataset):
     A library for manipulang simulations of the dataset is available `here
     <https://airfrans.readthedocs.io/en/latest/index.html>`_.
 
-    The dataset is under the `ODbL v1.0 License
+    The dataset is released under the `ODbL v1.0 License
     <https://opendatacommons.org/licenses/odbl/1-0/>`_.
 
     .. note::
