4f9c00d85936f74dd1d6cfe05bd2942ddb30feaf
rusty1s <matthias.fey@tu-dortmund.de>
2023-01-09 08:35:36 +0100

    update

--- test/explain/conftest.py
+++ test/explain/conftest.py
@@ -11,27 +11,25 @@ def get_edge_index(num_src_nodes, num_dst_nodes, num_edges):
 
 
 @pytest.fixture
+def data():
+    return Data(
+        x=torch.randn(4, 3),
+        edge_index=get_edge_index(4, 4, num_edges=6),
+        edge_attr=torch.randn(6, 3),
+    )
+
+
+@pytest.fixture
 def hetero_data():
     data = HeteroData()
     data['paper'].x = torch.randn(8, 16)
     data['author'].x = torch.randn(10, 8)
 
-    data['paper', 'paper'].edge_index = get_edge_index(8, 8, 10)
+    data['paper', 'paper'].edge_index = get_edge_index(8, 8, num_edges=10)
     data['paper', 'paper'].edge_attr = torch.randn(10, 16)
-    data['author', 'paper'].edge_index = get_edge_index(10, 8, 10)
-    data['paper', 'author'].edge_index = get_edge_index(8, 10, 10)
+    data['paper', 'author'].edge_index = get_edge_index(8, 10, num_edges=10)
     data['paper', 'author'].edge_attr = torch.randn(10, 8)
+    data['author', 'paper'].edge_index = get_edge_index(10, 8, num_edges=10)
+    data['author', 'paper'].edge_attr = torch.randn(10, 8)
 
     return data
-
-
-@pytest.fixture
-def data():
-    return Data(
-        x=torch.randn(4, 3),
-        edge_index=torch.tensor([
-            [0, 0, 0, 1, 1, 2],
-            [1, 2, 3, 2, 3, 3],
-        ]),
-        edge_attr=torch.randn(6, 3),
-    )
