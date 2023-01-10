19e33725f5f4ca9852a93c9f1ae7aedf5d92d6e4
Rishi Puri <riship@riship-mlt.client.nvidia.com>
2023-01-10 15:22:17 -0800

    debugging

--- torch_geometric/nn/conv/hgt_conv.py
+++ torch_geometric/nn/conv/hgt_conv.py
@@ -355,7 +355,7 @@ class HGTConv(MessagePassing):
             inc_counter = 0
             for i, src_type in enumerate(src_types):
                 increment_dict[src_type] = inc_counter
-                inc_counter += k_out[:,trans_ptr[i]:trans_ptr[i + 1],:].shape[0]
+                inc_counter += k_out[trans_ptr[i]:trans_ptr[i + 1]].shape[0]
             print('increment_dict=', increment_dict)
 
         #combine edge_index dict into single tensor
