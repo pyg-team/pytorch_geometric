53c70cf177c09b053f34e454b90d7d09ef535837
Matthias Fey <matthias.fey@tu-dortmund.de>
2023-01-18 16:25:19 +0100

    Re-structure `torch_geometric.nn.unpool/kge/functional` documentation (#6458)

--- torch_geometric/nn/unpool/knn_interpolate.py
+++ torch_geometric/nn/unpool/knn_interpolate.py
@@ -23,23 +23,23 @@ def knn_interpolate(x: torch.Tensor, pos_x: torch.Tensor, pos_y: torch.Tensor,
     to :math:`y`.
 
     Args:
-        x (Tensor): Node feature matrix
+        x (torch.Tensor): Node feature matrix
             :math:`\mathbf{X} \in \mathbb{R}^{N \times F}`.
-        pos_x (Tensor): Node position matrix
+        pos_x (torch.Tensor): Node position matrix
             :math:`\in \mathbb{R}^{N \times d}`.
-        pos_y (Tensor): Upsampled node position matrix
+        pos_y (torch.Tensor): Upsampled node position matrix
             :math:`\in \mathbb{R}^{M \times d}`.
-        batch_x (LongTensor, optional): Batch vector
+        batch_x (torch.Tensor, optional): Batch vector
             :math:`\mathbf{b_x} \in {\{ 0, \ldots, B-1\}}^N`, which assigns
             each node from :math:`\mathbf{X}` to a specific example.
             (default: :obj:`None`)
-        batch_y (LongTensor, optional): Batch vector
+        batch_y (torch.Tensor, optional): Batch vector
             :math:`\mathbf{b_y} \in {\{ 0, \ldots, B-1\}}^N`, which assigns
             each node from :math:`\mathbf{Y}` to a specific example.
             (default: :obj:`None`)
         k (int, optional): Number of neighbors. (default: :obj:`3`)
-        num_workers (int): Number of workers to use for computation. Has no
-            effect in case :obj:`batch_x` or :obj:`batch_y` is not
+        num_workers (int, optional): Number of workers to use for computation.
+            Has no effect in case :obj:`batch_x` or :obj:`batch_y` is not
             :obj:`None`, or the input lies on the GPU. (default: :obj:`1`)
     """
 
