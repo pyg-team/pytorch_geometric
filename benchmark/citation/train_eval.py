57220f8c74f59c2bcc00f39aaaa9a1b619558abd
rusty1s <matthias.fey@tu-dortmund.de>
2023-01-21 10:22:02 +0000

    update

--- benchmark/citation/train_eval.py
+++ benchmark/citation/train_eval.py
@@ -150,13 +150,13 @@ def evaluate(model, data):
     model.eval()
 
     with torch.no_grad():
-        logits = model(data)
+        out = model(data)
 
     outs = {}
     for key in ['train', 'val', 'test']:
         mask = data[f'{key}_mask']
-        loss = F.nll_loss(logits[mask], data.y[mask]).item()
-        pred = logits[mask].max(1)[1]
+        loss = F.nll_loss(out[mask], data.y[mask]).item()
+        pred = out[mask].max(1)[1]
         acc = pred.eq(data.y[mask]).sum().item() / mask.sum().item()
 
         outs[f'{key}_loss'] = loss
