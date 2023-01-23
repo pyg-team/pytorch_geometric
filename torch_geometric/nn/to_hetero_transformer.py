240ae67a254cfd44737847e9111e8b8a2cbf87d7
Rishi Puri <riship@riship-mlt.client.nvidia.com>
2023-01-23 11:40:40 -0800

    debugging

--- torch_geometric/nn/to_hetero_transformer.py
+++ torch_geometric/nn/to_hetero_transformer.py
@@ -323,7 +323,8 @@ class ToHeteroTransformer(Transformer):
             print("inserting dict extraction functions to graph")
             for key in self.metadata[int(self.is_edge_level(node))]:
                 print("inserting for key:", key)
-                print("target =",f'{target}.extract_from_dict')
+                print("target =", f'{target}.extract_from_dict')
+                print("name =", f'{name}__{key2str(key)}')
                 out = self.graph.create_node(
                     'call_method', target=f'{target}.extract_from_dict',
                     args=(key, ), name=f'{name}__{key2str(key)}')
