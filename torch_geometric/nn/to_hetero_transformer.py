6f28e567294efdc38112f0c7d80883235deef9ed
Rishi Puri <riship@riship-mlt.client.nvidia.com>
2023-01-24 13:02:27 -0800

    debugging

--- torch_geometric/nn/to_hetero_transformer.py
+++ torch_geometric/nn/to_hetero_transformer.py
@@ -416,12 +416,12 @@ class ToHeteroTransformer(Transformer):
             self.find_by_target(f'{target}.{key2str(self.metadata[0][0])}')
         ) or bool(
             self.find_by_name(
-                f'{target}__hetero_get_{key2str(self.metadata[0][0])}'))
+                f'{target}_hetero_get_{key2str(self.metadata[0][0])}'))
         has_edge_level_target = bool(
             self.find_by_target(f'{target}.{key2str(self.metadata[1][0])}')
         ) or bool(
             self.find_by_name(
-                f'{target}__hetero_get_{key2str(self.metadata[1][0])}'))
+                f'{target}_hetero_get_{key2str(self.metadata[1][0])}'))
         print("has_node_level_target:", has_node_level_target)
         print("has_edge_level_target:", has_edge_level_target)
         if not has_node_level_target and not has_edge_level_target:
