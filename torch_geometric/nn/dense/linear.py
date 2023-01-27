2ec68d4fca6cd2339139c1ea64e1556df80750ed
Rishi Puri <riship@riship-mlt.client.nvidia.com>
2023-01-26 19:27:10 -0800

    debugging

--- torch_geometric/nn/dense/linear.py
+++ torch_geometric/nn/dense/linear.py
@@ -284,7 +284,7 @@ class HeteroLinear(torch.nn.Module):
     @torch.no_grad()
     def initialize_parameters(self, module, input):
         if is_uninitialized_parameter(self.weight):
-            self.in_channels = max([i.size(-1) for in in input])
+            self.in_channels = max([i.size(-1) for i in input])
             self.weight.materialize((self.num_types, self.in_channels, self.out_channels))
             self.reset_parameters()
         self._hook.remove()
