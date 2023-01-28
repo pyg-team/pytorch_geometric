5fc0958ba36ba6dbf1ee03e538697a4648adf4d8
rusty1s <matthias.fey@tu-dortmund.de>
2023-01-28 20:30:47 +0100

    update

--- torch_geometric/datasets/ba_multi_shapes.py
+++ torch_geometric/datasets/ba_multi_shapes.py
@@ -8,22 +8,20 @@ from torch_geometric.data import Data, InMemoryDataset, download_url
 
 
 class BAMultiShapesDataset(InMemoryDataset):
-    r"""The synthetic BAMultiShapes graph classification dataset for evaluating
-    explainabilty algorithms, as described in the `"Global Explainability of
-    GNNs via Logic Combination of Learned Concepts"
+    r"""The synthetic BA-Multi-Shapes graph classification dataset for
+    evaluating explainabilty algorithms, as described in the
+    `"Global Explainability of GNNs via Logic Combination of Learned Concepts"
     <https://arxiv.org/abs/2210.07147>`_ paper.
     Given three atomic motifs, namely House (H), Wheel (W), and Grid (G),
-    :class:`~torch_geometric.datasets.BAMultiShapesDataset` contains 1000
-    graphs where each graph is obtained by attaching to a random
-    Barabasi-Albert (BA) the motifs as follows:
+    :class:`~torch_geometric.datasets.BAMultiShapesDataset` contains 1,000
+    graphs where each graph is obtained by attaching the motifs to a random
+    Barabasi-Albert (BA) as follows:
 
-        class 0: :math:`\emptyset` :math:`\lor` H :math:`\lor` W :math:`\lor`
-            G :math:`\lor` :math:`\{H, W, G\}`
-        class 1: H :math:`\land` W :math:`\lor` H :math:`\land` G :math:`\lor`
-            W :math:`\land` G
+    * class 0: :math:`\emptyset \lor` H \lor W \lor G \lor \{ H, W, G \}`
+
+    * class 1: :math:`H \land W \lor H \land G \lor W  \land G`
 
     This dataset is pre-computed from the official implementation.
-
 
     Args:
         root (str): Root directory where the dataset should be saved.
@@ -35,6 +33,10 @@ class BAMultiShapesDataset(InMemoryDataset):
             an :obj:`torch_geometric.data.Data` object and returns a
             transformed version. The data object will be transformed before
             being saved to disk. (default: :obj:`None`)
+        pre_filter (callable, optional): A function that takes in an
+            :obj:`torch_geometric.data.Data` object and returns a boolean
+            value, indicating whether the data object should be included in the
+            final dataset. (default: :obj:`None`)
 
     **STATS:**
 
@@ -53,47 +55,43 @@ class BAMultiShapesDataset(InMemoryDataset):
           - 10
           - 2
     """
-    url = ("https://github.com/steveazzolin/gnn_logic_global_expl/raw/master/"
-           "datasets/BAMultiShapes")
-    filename = 'BAMultiShapes.pkl'
+    url = ('https://github.com/steveazzolin/gnn_logic_global_expl/raw/master/'
+           'datasets/BAMultiShapes/BAMultiShapes.pkl')
 
     def __init__(
         self,
         root: str,
         transform: Optional[Callable] = None,
         pre_transform: Optional[Callable] = None,
+        pre_filter: Optional[Callable] = None,
     ):
-        super().__init__(root, transform, pre_transform)
+        super().__init__(root, transform, pre_transform, pre_filter)
         self.data, self.slices = torch.load(self.processed_paths[0])
 
+    @property
     def raw_file_names(self) -> str:
-        return self.filename
+        return 'BAMultiShapes.pkl'
 
     @property
     def processed_file_names(self) -> str:
         return 'data.pt'
 
     def download(self):
-        download_url(f'{self.url}/{self.filename}', self.raw_dir)
+        download_url(self.url, self.raw_dir)
 
     def process(self):
         with open(self.raw_paths[0], 'rb') as f:
-            adj, x, y = pickle.load(f)
-
-        adj = np.array(adj)
-        x = np.array(x)
-
-        adjs = torch.tensor(adj)
-        xs = torch.from_numpy(x).to(torch.float)
-        ys = torch.tensor(y)
+            adjs, xs, ys = pickle.load(f)
 
         data_list: List[Data] = []
-        for i in range(xs.size(0)):
-            edge_index = adjs[i].nonzero().t()
-            x = xs[i]
-            y = int(ys[i])
+        for adj, x, y in zip(adjs, xs, ys):
+            edge_index = torch.from_numpy(adj).nonzero().t()
+            x = torch.from_numpy(np.array(x)).to(torch.float)
 
             data = Data(x=x, edge_index=edge_index, y=y)
+
+            if self.pre_filter is not None and not self.pre_filter(data):
+                continue
 
             if self.pre_transform is not None:
                 data = self.pre_transform(data)
