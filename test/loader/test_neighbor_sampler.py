c694cdde8d4abe9f05ec0187a0f68dba2dbaabe3
juyi <1263810658@qq.com>
2023-01-28 10:12:11 +0000

    update

--- test/loader/test_neighbor_sampler.py
+++ test/loader/test_neighbor_sampler.py
@@ -7,7 +7,6 @@ from torch_geometric.utils import erdos_renyi_graph
 from torch_sparse import SparseTensor
 
 
-
 def test_neighbor_sampler():
     torch.manual_seed(12345)
     edge_index = erdos_renyi_graph(num_nodes=10, edge_prob=0.5)
@@ -15,7 +14,8 @@ def test_neighbor_sampler():
     E = edge_index.size(1)
 
     loader = NeighborSampler(edge_index, sizes=[2, 4], batch_size=2)
-    sparse_loader = NeighborSampler(sparse_edge_index, sizes=[2, 4], batch_size=2,collate_fn="test_init",dataset="test_init")
+    sparse_loader = NeighborSampler(sparse_edge_index, sizes=[
+                                    2, 4], batch_size=2, collate_fn="test_init", dataset="test_init")
     assert loader.__repr__() == 'NeighborSampler(sizes=[2, 4])'
     assert len(loader) == 5
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -35,7 +35,6 @@ def test_neighbor_sampler():
     for batch_size, n_id, adjs in sparse_loader:
         for (edge_index, e_id, size) in adjs:
             edge_index.to(device)
-
     out = loader.sample([1, 2])
     assert len(out) == 3
 
