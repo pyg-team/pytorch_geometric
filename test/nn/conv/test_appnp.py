6e28541cb13033d03772ef87a91d7ea795f83976
Zecheng Zhang <zecheng@kumo.ai>
2023-02-01 04:49:08 -0800

    [Code Coverage] APPNP (#6558)
    Add some tests to APPNP
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: rusty1s <matthias.fey@tu-dortmund.de>

--- test/nn/conv/test_appnp.py
+++ test/nn/conv/test_appnp.py
@@ -11,17 +11,35 @@ def test_appnp():
     row, col = edge_index
     adj = SparseTensor(row=row, col=col, sparse_sizes=(4, 4))
 
-    conv = APPNP(K=10, alpha=0.1)
-    assert conv.__repr__() == 'APPNP(K=10, alpha=0.1)'
+    conv = APPNP(K=3, alpha=0.1, cached=True)
+    assert str(conv) == 'APPNP(K=3, alpha=0.1)'
     out = conv(x, edge_index)
     assert out.size() == (4, 16)
-    assert torch.allclose(conv(x, adj.t()), out, atol=1e-6)
+    assert torch.allclose(conv(x, adj.t()), out)
+
+    # Run again to test the cached functionality:
+    assert conv._cached_edge_index is not None
+    assert conv._cached_adj_t is not None
+    assert torch.allclose(conv(x, edge_index), conv(x, adj.t()))
+
+    conv.reset_parameters()
+    assert conv._cached_edge_index is None
+    assert conv._cached_adj_t is None
 
     if is_full_test():
         t = '(Tensor, Tensor, OptTensor) -> Tensor'
         jit = torch.jit.script(conv.jittable(t))
-        assert jit(x, edge_index).tolist() == out.tolist()
+        assert torch.allclose(jit(x, edge_index), out)
 
         t = '(Tensor, SparseTensor, OptTensor) -> Tensor'
         jit = torch.jit.script(conv.jittable(t))
-        assert torch.allclose(jit(x, adj.t()), out, atol=1e-6)
+        assert torch.allclose(jit(x, adj.t()), out)
+
+
+def test_appnp_dropout():
+    x = torch.randn(4, 16)
+    edge_index = torch.tensor([[0, 0, 0, 1, 2, 3], [1, 2, 3, 0, 0, 0]])
+
+    # With dropout probability of 1.0, the final output equals to alpha * x:
+    conv = APPNP(K=2, alpha=0.1, dropout=1.0)
+    assert torch.allclose(0.1 * x, conv(x, edge_index))
