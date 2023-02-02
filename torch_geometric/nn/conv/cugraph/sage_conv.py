0a552d65b3af8894e9f821955c3a528de1e4392d
rusty1s <matthias.fey@tu-dortmund.de>
2023-02-02 13:45:01 +0000

    update

--- torch_geometric/nn/conv/cugraph/sage_conv.py
+++ torch_geometric/nn/conv/cugraph/sage_conv.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 from torch import Tensor
 from torch.nn import Linear
 
-from torch_geometric.utils import index_sort
+from torch_geometric.nn.conv.cugraph.base import CuGraphModule
 
 try:
     from pylibcugraphops import make_fg_csr, make_mfg_csr
@@ -15,7 +15,7 @@ except ImportError:
     HAS_PYLIBCUGRAPHOPS = False
 
 
-class CuGraphSAGEConv(torch.nn.Module):
+class CuGraphSAGEConv(CuGraphModule):
     r"""The GraphSAGE operator from the `"Inductive Representation Learning on
     Large Graphs" <https://arxiv.org/abs/1706.02216>`_ paper.
 
@@ -65,46 +65,13 @@ class CuGraphSAGEConv(torch.nn.Module):
             self.pre_lin.reset_parameters()
         self.linear.reset_parameters()
 
-    @staticmethod
-    def to_csc(
-        edge_index: Tensor,
-        size: Optional[Tuple[int, int]] = None,
-    ) -> Tuple[Tensor, Tensor]:
-        r"""
-        Args:
-            edge_index (torch.Tensor): The edge indices.
-            size ((int, int), optional). The shape of :obj:`edge_index` in each
-                dimension. (default: :obj:`None`)
-        """
-        # TODO Consider out-sourcing to base class or utility function.
-        row, col = edge_index
-        num_target_nodes = size[1] if size is not None else int(col.max() + 1)
-        col, perm = index_sort(col, max_value=num_target_nodes)
-        row = row[perm]
-
-        colptr = torch._convert_indices_from_coo_to_csr(col, num_target_nodes)
-
-        return row, colptr
-
     def forward(
         self,
         x: Tensor,
         csc: Tuple[Tensor, Tensor],
         max_num_neighbors: Optional[int] = None,
     ) -> Tensor:
-        r"""
-        Args:
-            x (torch.Tensor): The node features.
-            csc ((torch.Tensor, torch.Tensor)): A tuple containing the CSC
-                representation of a graph, given as a tuple of
-                :obj:`(row, colptr)`. Use the :meth:`to_csc` method to convert
-                an :obj:`edge_index` representation to the desired format.
-            max_num_neighbors (int, optional): The maximum number of neighbors
-                of a target node. It is only effective when operating in a
-                bipartite graph.. When not given, the value will be computed
-                on-the-fly, leading to slightly worse performance.
-                (default: :obj:`None`)
-        """
+        """"""
         if not x.is_cuda:
             raise RuntimeError(f"'{self.__class__.__name__}' requires GPU-"
                                f"based processing (got CPU tensor)")
