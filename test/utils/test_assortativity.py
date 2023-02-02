c494389e43e2050bf1e3ac7676506f23b65c7336
akash-vartak <cg82493@umbc.edu>
2023-02-02 17:33:47 -0500

    Adding tests for SparseTensor

--- test/utils/test_assortativity.py
+++ test/utils/test_assortativity.py
@@ -2,6 +2,9 @@ import pytest
 import torch
 
 from torch_geometric.utils import assortativity
+from torch_geometric.data import Data
+from torch_geometric.transforms import ToSparseTensor
+from torch_sparse import SparseTensor
 
 
 def test_assortativity():
@@ -11,8 +14,33 @@ def test_assortativity():
     out = assortativity(edge_index)
     assert pytest.approx(out, abs=1e-5) == 1.0
 
+    # SpraseTensor version
+    edge_weight = torch.randn(edge_index.size(1))
+    edge_attr = torch.randn(edge_index.size(1), 8)
+    data = Data(edge_index=edge_index, edge_weight=edge_weight,
+                edge_attr=edge_attr, num_nodes=6)
+    data = ToSparseTensor()(data)
+    edge_index_as_sparse = SparseTensor(row = data.adj_t.storage.row(),
+                                col = data.adj_t.storage.col(),
+                                value = data.adj_t.storage.value())
+    out = assortativity(edge_index_as_sparse)
+    assert pytest.approx(out, abs=1e-5) == 1.0
+
+
     # completely disassortative graph
     edge_index = torch.tensor([[0, 1, 2, 3, 4, 5, 5, 5, 5, 5],
                                [5, 5, 5, 5, 5, 0, 1, 2, 3, 4]])
     out = assortativity(edge_index)
+    assert pytest.approx(out, abs=1e-5) == -1.0
+
+    # SpraseTensor version
+    edge_weight = torch.randn(edge_index.size(1))
+    edge_attr = torch.randn(edge_index.size(1), 8)
+    data = Data(edge_index=edge_index, edge_weight=edge_weight,
+                edge_attr=edge_attr, num_nodes=6)
+    data = ToSparseTensor()(data)
+    edge_index_as_sparse = SparseTensor(row = data.adj_t.storage.row(),
+                                col = data.adj_t.storage.col(),
+                                value = data.adj_t.storage.value())
+    out = assortativity(edge_index_as_sparse)
     assert pytest.approx(out, abs=1e-5) == -1.0
