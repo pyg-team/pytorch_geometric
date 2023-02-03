fb0e8dfcea6b7eace3a90707049b562e5b3f700c
rusty1s <matthias.fey@tu-dortmund.de>
2023-02-03 22:04:06 +0000

    update

--- test/profile/test_profiler.py
+++ test/profile/test_profiler.py
@@ -5,15 +5,21 @@ from torch_geometric.profile.profiler import Profiler
 from torch_geometric.testing import onlyFullTest, withCUDA
 
 
-@onlyFullTest
 @withCUDA
+@onlyFullTest
 def test_torch_profiler(get_dataset, device):
-    dataset = get_dataset(name='Cora')
-    use_cuda = torch.cuda.is_available()
-    data = dataset[0].to(device)
-    model = GraphSAGE(dataset.num_features, hidden_channels=64, num_layers=3,
-                      out_channels=dataset.num_classes).to(device)
-    model.train()
-    with Profiler(model, profile_memory=True, use_cuda=use_cuda) as prof:
-        model(data.x, data.edge_index)
-        print(prof)
+    x = torch.randn(10, 16, device=device)
+    edge_index = torch.tensor([
+        [0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9],
+        [1, 0, 2, 1, 3, 2, 4, 3, 5, 4, 6, 5, 7, 6, 8, 7, 9, 8],
+    ], device=device)
+
+    model = GraphSAGE(16, hidden_channels=32, num_layers=2).to(device)
+
+    with Profiler(model, profile_memory=True, use_cuda=x.is_cuda) as prof:
+        model(x, edge_index)
+    _, heading_list, raw_results, layer_names, layer_stats = prof.get_trace()
+
+    assert 'Self CPU total' in heading_list
+    assert 'aten::relu' in raw_results
+    assert '-act--aten::relu' in layer_names
