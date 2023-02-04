fe4f1ff8e98b70a91366c64be1653bd7f87a32bc
rusty1s <matthias.fey@tu-dortmund.de>
2023-02-04 11:38:47 +0000

    update

--- test/nn/conv/test_antisymmetric_conv.py
+++ test/nn/conv/test_antisymmetric_conv.py
@@ -5,27 +5,20 @@ from torch_geometric.nn import AntiSymmetricConv
 
 
 def test_antisymmetric_conv():
-    x1 = torch.randn(4, 8)
-    edge_index = torch.tensor([[0, 1, 2, 3], [0, 0, 1, 1]])
+    x = torch.randn(4, 8)
+    edge_index = torch.tensor([[0, 0, 0, 1, 2, 3], [1, 2, 3, 0, 0, 0]])
     row, col = edge_index
     value = torch.rand(row.size(0))
     adj = SparseTensor(row=row, col=col, value=value, sparse_sizes=(4, 4))
 
     conv = AntiSymmetricConv(8)
-    assert conv.__repr__() == (
-        'AntiSymmetricConv(in_channels=8, phi=GCNConv(8, 8), num_iters=1, '
-        'epsilon=0.1, gamma=0.1, activ_fun=tanh, bias=True)')
+    assert str(conv) == ('AntiSymmetricConv(8, phi=GCNConv(8, 8), '
+                         'num_iters=1, epsilon=0.1, gamma=0.1)')
 
-    out1 = conv(x1, edge_index, value)
-    assert out1.size() == (4, 8)
-    assert conv(x1, edge_index, value).tolist() == out1.tolist()
-    assert torch.allclose(conv(x1, edge_index, value), out1)
+    out = conv(x, edge_index, value)
+    assert out.size() == (4, 8)
+    assert torch.allclose(conv(x, adj.t()), out)
 
-    out2 = conv(x1, adj.t())
-    assert conv(x1, adj.t()).tolist() == out2.tolist()
-    assert torch.allclose(conv(x1, adj.t()), out2)
-
-    out3 = conv(x1, edge_index)
-    assert out3.size() == (4, 8)
-    assert conv(x1, edge_index).tolist() == out3.tolist()
-    assert torch.allclose(conv(x1, edge_index), out3)
+    out = conv(x, edge_index)
+    assert out.size() == (4, 8)
+    assert torch.allclose(conv(x, adj.t().set_value(None)), out)
--- torch_geometric/nn/conv/antisymmetric_conv.py
+++ torch_geometric/nn/conv/antisymmetric_conv.py
@@ -1,47 +1,51 @@
 import math
-from typing import Optional
+from typing import Any, Callable, Dict, Optional, Union
 
 import torch
-from torch.nn import Module, Parameter, init
+from torch import Tensor
+from torch.nn import Parameter
 
-from torch_geometric.nn.conv import GCNConv
+from torch_geometric.nn.conv import GCNConv, MessagePassing
+from torch_geometric.nn.inits import ones, zeros
+from torch_geometric.nn.resolver import activation_resolver
 from torch_geometric.typing import Adj, OptTensor
 
 
-class AntiSymmetricConv(Module):
-    r"""The anti-symmetric deep graph network operator from the
+class AntiSymmetricConv(torch.nn.Module):
+    r"""The anti-symmetric graph convolutional operator from the
     `"Anti-Symmetric DGN: a stable architecture for Deep Graph Networks"
     <https://openreview.net/forum?id=J3Y7cgZOOS>`_ paper
 
     .. math::
-        \mathbf{x}^{\prime}_i = \mathbf{x}_i + \epsilon \sigma \left(
+        \mathbf{x}^{\prime}_i = \mathbf{x}_i + \epsilon \cdot \sigma \left(
             (\mathbf{W}-\mathbf{W}^T-\gamma \mathbf{I}) \mathbf{x}_i +
             \Phi(\mathbf{X}, \mathcal{N}_i) + \mathbf{b}\right),
 
-    where :math:`\Phi(\mathbf{X}, \mathcal{N}_i)` denotes the aggregation
-    function for the states of the nodes in the neighborhood of :math:`i`.
-
+    where :math:`\Phi(\mathbf{X}, \mathcal{N}_i)` denotes a
+    :class:`~torch.nn.conv.MessagePassing` layer.
 
     Args:
         in_channels (int): Size of each input sample.
-        phi (torch.nn.Module, optional): the function that aggregates nodes
-            (and edges) information :math:`\Phi`. If set to :obj:`None`, then
-            the aggregator is instantiated as
-            :obj:`GCNConv(in_channels, in_channels)`. (default :obj:`None`)
-        num_iters (int, optional): the number of times the anti-symmetric deep
-            graph network operator is called leveraging weight sharing.
-            (default :obj:`1`)
-        epsilon (float, optional): the discretization step size
+        phi (MessagePassing, optional): The message passing module
+            :math:`\Phi`. If set to :obj:`None`, will use
+            :class:`~torch_geometric.nn.conv.GCNConv` as default.
+            (default :obj:`None`)
+        num_iters (int, optional): The number of times the anti-symmetric deep
+            graph network operator is called. (default :obj:`1`)
+        epsilon (float, optional): The discretization step size
             :math:`\epsilon`. (default :obj:`0.1`)
-        gamma (float, optional): the strength of the diffusion :math:`\gamma`.
+        gamma (float, optional): The strength of the diffusion :math:`\gamma`.
             It regulates the stability of the method. (default :obj:`0.1`)
-        activ_fun (str, optional): the monotonically non-decreasing activation
-            function :math:`\sigma`, e.g., :class:`torch.tanh`,
-            :class:`torch.relu`. (default :class:`torch.tanh`)
+        act (str, optional): The monotonically non-decreasing activation
+            function :math:`\sigma`, *e.g.*, :obj:`"tanh"` or :obj:`"relu"`.
+            (default :class:`torch.tanh`)
+        act (str or Callable, optional): The monotonically non-decreasing
+            activation function :math:`\sigma`. (default: :obj:`"tanh"`)
+        act_kwargs (Dict[str, Any], optional): Arguments passed to the
+            respective activation function defined by :obj:`act`.
+            (default: :obj:`None`)
         bias (bool, optional): If set to :obj:`False`, the layer will not learn
             an additive bias. (default: :obj:`True`)
-        **kwargs (optional): Additional arguments of
-            :class:`torch_geometric.nn.conv.MessagePassing`.
 
     Shapes:
         - **input:**
@@ -50,59 +54,68 @@ class AntiSymmetricConv(Module):
           edge weights :math:`(|\mathcal{E}|)` *(optional)*
         - **output:** node features :math:`(|\mathcal{V}|, F_{in})`
     """
-    def __init__(self, in_channels: int, phi: Optional[torch.nn.Module] = None,
-                 num_iters: int = 1, epsilon: float = 0.1, gamma: float = 0.1,
-                 activ_fun: str = 'tanh', bias: bool = True, **kwargs) -> None:
-        super().__init__(**kwargs)
-        self.W = Parameter(torch.empty((in_channels, in_channels)),
-                           requires_grad=True)
-        self.bias = (Parameter(torch.empty(in_channels), requires_grad=True)
-                     if bias else None)
-        self.eye = Parameter(torch.eye(in_channels), requires_grad=False)
-        self.phi = (GCNConv(in_channels, in_channels, bias=False)
-                    if phi is None else phi)
+    def __init__(
+        self,
+        in_channels: int,
+        phi: Optional[MessagePassing] = None,
+        num_iters: int = 1,
+        epsilon: float = 0.1,
+        gamma: float = 0.1,
+        act: Union[str, Callable, None] = 'tanh',
+        act_kwargs: Optional[Dict[str, Any]] = None,
+        bias: bool = True,
+    ) -> None:
+        super().__init__()
+
         self.in_channels = in_channels
         self.num_iters = num_iters
         self.gamma = gamma
         self.epsilon = epsilon
-        self.activation = getattr(torch, activ_fun)
-        self.activ_fun = activ_fun
-        self.b = bias
+        self.act = activation_resolver(act, **(act_kwargs or {}))
+
+        if phi is None:
+            phi = GCNConv(in_channels, in_channels, bias=False)
+
+        self.W = Parameter(torch.Tensor(in_channels, in_channels))
+        self.eye = Parameter(torch.Tensor(in_channels))
+        self.phi = phi
+
+        if bias:
+            self.bias = Parameter(torch.Tensor(in_channels))
+        else:
+            self.register_parameter('bias', None)
 
         self.reset_parameters()
 
-    def forward(self, x: torch.Tensor, edge_index: Adj,
-                edge_weight: OptTensor = None) -> torch.Tensor:
+    def reset_parameters(self):
+        torch.nn.init.kaiming_uniform_(self.W, a=math.sqrt(5))
+        ones(self.eye)
+        self.phi.reset_parameters()
+        zeros(self.bias)
+
+    def forward(self, x: Tensor, edge_index: Adj,
+                edge_weight: OptTensor = None) -> Tensor:
         """"""
-        antisymmetric_W = self.W - self.W.T - self.gamma * self.eye
+        antisymmetric_W = self.W - self.W.t() - self.gamma * self.eye
 
         for _ in range(self.num_iters):
-            neigh_x = self.phi(x=x, edge_index=edge_index,
-                               edge_weight=edge_weight)
-            conv = x @ antisymmetric_W.T + neigh_x
+            h = self.phi(x, edge_index, edge_weight=edge_weight)
+            h = x @ antisymmetric_W.t() + h
 
             if self.bias is not None:
-                conv += self.bias
+                h += self.bias
 
-            x = x + self.epsilon * self.activation(conv)
+            if self.act is not None:
+                h = self.act(h)
+
+            x = x + self.epsilon * h
+
         return x
-
-    def reset_parameters(self):
-        # Setting a=sqrt(5) in kaiming_uniform is the same as initializing with
-        # uniform(-1/sqrt(in_features), 1/sqrt(in_features)). For details, see
-        # https://github.com/pytorch/pytorch/issues/57109
-        init.kaiming_uniform_(self.W, a=math.sqrt(5))
-        if self.bias is not None:
-            fan_in, _ = init._calculate_fan_in_and_fan_out(self.W)
-            bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
-            init.uniform_(self.bias, -bound, bound)
 
     def __repr__(self) -> str:
         return (f'{self.__class__.__name__}('
-                f'in_channels={self.in_channels}, '
+                f'{self.in_channels}, '
                 f'phi={self.phi}, '
                 f'num_iters={self.num_iters}, '
                 f'epsilon={self.epsilon}, '
-                f'gamma={self.gamma}, '
-                f'activ_fun={self.activ_fun}, '
-                f'bias={self.b})')
+                f'gamma={self.gamma})')
