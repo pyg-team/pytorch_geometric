395c551edc4166f0b1964e163efa8721891c52e5
Gabriel VIGOT <56968382+gabyweg76@users.noreply.github.com>
2023-02-05 14:47:59 +0100

    Update torch_geometric/nn/conv/Bayesian_GCNConv.py
    Co-authored-by: Jintang Li <cnljt@outlook.com>

--- torch_geometric/nn/conv/Bayesian_GCNConv.py
+++ torch_geometric/nn/conv/Bayesian_GCNConv.py
@@ -67,7 +67,6 @@ class BGCNConv(MessagePassing):
         self.normalize = normalize
 
         self._cached_edge_index = None
-        self._cached_adj_t = None
 
         self.mu_weight = Parameter(torch.Tensor(out_channels, in_channels))
         self.rho_weight = Parameter(torch.Tensor(out_channels, in_channels))
