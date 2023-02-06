ad74a3994448b9b23bacd12fd3ea585a89acde66
akash-vartak <cg82493@umbc.edu>
2023-02-06 15:30:59 -0500

    Initial test for home.py

---
+++ test/test_home.py
@@ -0,0 +1,17 @@
+import os
+import os.path as osp
+from tempfile import gettempdir
+
+from torch_geometric import get_home_dir, set_home_dir
+
+ENV_PYG_HOME = 'PYG_HOME'
+DEFAULT_CACHE_DIR = osp.join('~', '.cache', 'pyg')
+
+def test_home():
+    home_dir = os.getenv(ENV_PYG_HOME, DEFAULT_CACHE_DIR)
+    home_dir = osp.expanduser(home_dir)
+    assert get_home_dir() == home_dir
+
+    temp_test_path = osp.join(gettempdir(), 'test_pyg')
+    set_home_dir(temp_test_path)
+    assert get_home_dir() == temp_test_path
