55567ac9556b839bbdcd8d9998dda6c1b248a5b5
akash-vartak <cg82493@umbc.edu>
2023-02-07 16:34:54 -0500

    Tests for different code paths

--- test/utils/test_loop.py
+++ test/utils/test_loop.py
@@ -92,9 +92,27 @@ def test_add_remaining_self_loops():
     edge_index = torch.tensor([[0, 1, 0], [1, 0, 0]])
     edge_weight = torch.tensor([0.5, 0.5, 0.5])
 
-    edge_index, edge_weight = add_remaining_self_loops(edge_index, edge_weight)
-    assert edge_index.tolist() == [[0, 1, 0, 1], [1, 0, 0, 1]]
-    assert edge_weight.tolist() == [0.5, 0.5, 0.5, 1]
+    edge_index_out, edge_weight_out = add_remaining_self_loops(edge_index, edge_weight)
+    assert edge_index_out.tolist() == [[0, 1, 0, 1], [1, 0, 0, 1]]
+    assert edge_weight_out.tolist() == [0.5, 0.5, 0.5, 1]
+
+    # fill_value as int
+    fill_value = 5
+    edge_index_out, edge_weight_out = add_remaining_self_loops(edge_index, edge_weight, fill_value)
+    assert edge_index_out.tolist() == [[0, 1, 0, 1], [1, 0, 0, 1]]
+    assert edge_weight_out.tolist() == [0.5, 0.5, 0.5, 5.0]
+
+    # fill_value as Tensor
+    fill_value = torch.Tensor([2])
+    edge_index_out, edge_weight_out = add_remaining_self_loops(edge_index, edge_weight, fill_value)
+    assert edge_index_out.tolist() == [[0, 1, 0, 1], [1, 0, 0, 1]]
+    assert edge_weight_out.tolist() == [0.5, 0.5, 0.5, 2.0]
+
+    # fill_value as str
+    fill_value = "add"
+    edge_index_out, edge_weight_out = add_remaining_self_loops(edge_index, edge_weight, fill_value)
+    assert edge_index_out.tolist() == [[0, 1, 0, 1], [1, 0, 0, 1]]
+    assert edge_weight_out.tolist() == [0.5, 0.5, 0.5, 0.5]
 
 
 def test_add_remaining_self_loops_without_initial_loops():
