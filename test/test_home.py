dd8601e0713991f84938cd5b4fed5ea39e9fda4c
rusty1s <matthias.fey@tu-dortmund.de>
2023-02-08 09:50:07 +0000

    update

--- test/test_home.py
+++ test/test_home.py
@@ -1,18 +1,19 @@
 import os
 import os.path as osp
-from tempfile import gettempdir
 
 from torch_geometric import get_home_dir, set_home_dir
-
-ENV_PYG_HOME = 'PYG_HOME'
-DEFAULT_CACHE_DIR = osp.join('~', '.cache', 'pyg')
+from torch_geometric.home import DEFAULT_CACHE_DIR
 
 
 def test_home():
-    home_dir = os.getenv(ENV_PYG_HOME, DEFAULT_CACHE_DIR)
-    home_dir = osp.expanduser(home_dir)
+    os.environ.pop('PYG_HOME', None)
+    home_dir = osp.expanduser(DEFAULT_CACHE_DIR)
     assert get_home_dir() == home_dir
 
-    temp_test_path = osp.join(gettempdir(), 'test_pyg')
-    set_home_dir(temp_test_path)
-    assert get_home_dir() == temp_test_path
+    home_dir = '/tmp/test_pyg1'
+    os.environ['PYG_HOME'] = home_dir
+    assert get_home_dir() == home_dir
+
+    home_dir = '/tmp/test_pyg2'
+    set_home_dir(home_dir)
+    assert get_home_dir() == home_dir
