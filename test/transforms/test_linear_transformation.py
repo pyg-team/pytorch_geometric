417a9bfee6f83f6466c08f29ffad812cf276cd09
akash-vartak <cg82493@umbc.edu>
2023-02-09 16:16:24 -0500

    Adding code comments

--- test/transforms/test_linear_transformation.py
+++ test/transforms/test_linear_transformation.py
@@ -5,6 +5,7 @@ from torch_geometric.transforms import LinearTransformation
 
 
 def test_linear_transformation():
+    # matrix as 2D array
     matrix = [[2, 0], [0, 2]]
     transform = LinearTransformation(matrix)
     assert str(transform) == ('LinearTransformation(\n'
@@ -12,6 +13,7 @@ def test_linear_transformation():
                               ' [0 2]]\n'
                               ')')
 
+    # matrix as Tensor
     matrix = torch.tensor([[2, 0], [0, 2]], dtype=torch.float)
     transform = LinearTransformation(matrix)
     assert str(transform) == ('LinearTransformation(\n'
@@ -19,11 +21,12 @@ def test_linear_transformation():
                               ' [0. 2.]]\n'
                               ')')
 
-
+    # matrix as Tensor without pos
     data = Data()
     data = transform(data)
     assert len(data) == 0
 
+    # matrix as Tensor with pos
     pos = torch.Tensor([[-1, 1], [-3, 0], [2, -1]])
     data = Data(pos=pos)
     data = transform(data)
