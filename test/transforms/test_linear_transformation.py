d8ed1715a901e1e0fc4ee3761c4f541a7e3dfdb0
rusty1s <matthias.fey@tu-dortmund.de>
2023-02-10 06:34:38 +0000

    update

--- test/transforms/test_linear_transformation.py
+++ test/transforms/test_linear_transformation.py
@@ -1,33 +1,26 @@
+import pytest
 import torch
 
 from torch_geometric.data import Data
 from torch_geometric.transforms import LinearTransformation
 
 
-def test_linear_transformation():
+@pytest.mark.parametrize('matrix', [
+    [[2., 0.], [0., 2.]],
+    torch.tensor([[2., 0.], [0., 2.]]),
+])
+def test_linear_transformation(matrix):
     pos = torch.Tensor([[-1, 1], [-3, 0], [2, -1]])
-    data = Data(pos=pos)
 
-    # expected __repr__ output
-    expected = ('LinearTransformation(\n'
-                '[[2. 0.]\n'
-                ' [0. 2.]]\n'
-                ')')
-
-    # Test with matrix as 2D array:
-    matrix = [[2., 0.], [0., 2.]]
     transform = LinearTransformation(matrix)
-    assert str(transform) == expected
+    assert str(transform) == ('LinearTransformation(\n'
+                              '[[2. 0.]\n'
+                              ' [0. 2.]]\n'
+                              ')')
 
-    # Test with matrix as Tensor:
-    matrix = torch.tensor([[2, 0], [0, 2]], dtype=torch.float)
-    transform = LinearTransformation(matrix)
-    assert str(transform) == expected
-    data = transform(data)
-    assert len(data) == 1
-    assert data.pos.tolist() == [[-2, 2], [-6, 0], [4, -2]]
+    out = transform(Data(pos=pos))
+    assert len(out) == 1
+    assert out.pos.tolist() == [[-2, 2], [-6, 0], [4, -2]]
 
-    # Test without pos:
-    data = Data()
-    data = transform(data)
-    assert len(data) == 0
+    out = transform(Data())
+    assert len(out) == 0
