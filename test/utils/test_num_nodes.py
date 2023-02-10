a8cefc63d33058d0d20574ba1e097da6b395bd07
Shweta Jacob <shwetaaj59@gmail.com>
2023-02-10 15:01:43 -0500

    Add test case for num nodes

--- test/utils/test_num_nodes.py
+++ test/utils/test_num_nodes.py
@@ -19,7 +19,10 @@ def test_maybe_num_nodes_dict():
         '1': torch.LongTensor([[0, 0, 1, 1, 2, 2, 2], [1, 2, 0, 2, 0, 1, 1]]),
         '2': torch.LongTensor([[0, 0, 1, 3], [1, 2, 0, 4]])
     }
+    num_nodes_dict = {'2': 5}
 
     expected_num_nodes_dict = {'1': 3, '2': 5}
 
     assert maybe_num_nodes_dict(edge_index_dict) == expected_num_nodes_dict
+    assert maybe_num_nodes_dict(edge_index_dict,
+                                num_nodes_dict) == expected_num_nodes_dict
