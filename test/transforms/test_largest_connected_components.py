4a5a93efdb863d95c7a584f8bd70090abe36d895
akash-vartak <cg82493@umbc.edu>
2023-02-10 18:21:36 -0500

    Adding tests for missing code paths

--- test/transforms/test_largest_connected_components.py
+++ test/transforms/test_largest_connected_components.py
@@ -13,10 +13,17 @@ def test_largest_connected_components():
     ])
     data = Data(edge_index=edge_index, num_nodes=10)
 
+    # Testing without `connection` specified
     transform = LargestConnectedComponents(num_components=2)
     out = transform(data)
     assert out.num_nodes == 8
     assert out.edge_index.tolist() == data.edge_index[:, :12].tolist()
+
+    # Testing with `connection = strong`
+    transform = LargestConnectedComponents(num_components=2, connection='strong')
+    out = transform(data)
+    assert out.num_nodes == 7
+    assert out.edge_index.tolist() == [[0, 0, 1, 1, 2, 2, 2, 3, 3, 4, 5, 6], [1, 2, 0, 2, 0, 1, 3, 2, 4, 3, 6, 5]]
 
     edge_index = torch.tensor([
         [0, 1, 2, 3, 3, 4],
@@ -24,7 +31,14 @@ def test_largest_connected_components():
     ])
     data = Data(edge_index=edge_index, num_nodes=5)
 
+    # Testing without `num_components` and `connection` specified
     transform = LargestConnectedComponents()
     out = transform(data)
     assert out.num_nodes == 3
     assert out.edge_index.tolist() == [[0, 1, 1, 2], [1, 0, 2, 1]]
+
+    # Testing with specified num_components more than scipy's num_components
+    transform = LargestConnectedComponents(num_components=3)
+    out = transform(data)
+    assert out.num_nodes == int(edge_index.max()) + 1
+    assert out.edge_index.tolist() == data.edge_index.tolist()
