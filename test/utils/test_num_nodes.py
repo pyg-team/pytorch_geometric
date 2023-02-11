36541e483852be326718f6655f9ac0a88549d9ad
Matthias Fey <matthias.fey@tu-dortmund.de>
2023-02-11 21:32:38 +0100

    Update test/utils/test_num_nodes.py

--- test/utils/test_num_nodes.py
+++ test/utils/test_num_nodes.py
@@ -16,8 +16,8 @@ def test_maybe_num_nodes():
 
 def test_maybe_num_nodes_dict():
     edge_index_dict = {
-        '1': torch.LongTensor([[0, 0, 1, 1, 2, 2, 2], [1, 2, 0, 2, 0, 1, 1]]),
-        '2': torch.LongTensor([[0, 0, 1, 3], [1, 2, 0, 4]])
+        '1': torch.tensor([[0, 0, 1, 1, 2, 2, 2], [1, 2, 0, 2, 0, 1, 1]]),
+        '2': torch.tensor([[0, 0, 1, 3], [1, 2, 0, 4]])
     }
     num_nodes_dict = {'2': 5}
 
