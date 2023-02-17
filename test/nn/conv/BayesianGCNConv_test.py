e7c27fc89bf7bd445456197ffc3bc443692c6ba9
Gabriel VIGOT <56968382+gabyweg76@users.noreply.github.com>
2023-02-17 18:00:09 +0100

    Modified the test functions

--- test/nn/conv/BayesianGCNConv_test.py
+++ test/nn/conv/BayesianGCNConv_test.py
@@ -1,341 +1,131 @@
-import copy
-from typing import Optional
-
 import torch
-import torch.nn.functional as F
-from torch import Tensor
-from torch.nn import Parameter
+import copy
 from torch_sparse import SparseTensor
-
-from torch_geometric.nn.conv import MessagePassing
-from torch_geometric.nn.conv.gcn_conv import gcn_norm
-# Import BayesianGCNConv once merged in the library
-# from torch_geometric.nn.conv.BayesianGCNConv import BayesianGCNConv
+from torch_geometric.nn.conv.BayesianGCNConv import BayesianGCNConv
 from torch_geometric.testing import is_full_test
-from torch_geometric.typing import Adj, OptPairTensor, OptTensor
-# for the last version of torch_geometric
-from torch_geometric.utils import spmm
 
-
-class BayesianGCNConv(MessagePassing):
-    r"""The Bayesian graph convolutional operator inspired
-    from the `"Improving model calibration with
-   accuracy versus uncertainty optimization"
-   <https://arxiv.org/abs/2012.07923>`_ paper
-   We first proceed wth linear transformation:
-   .. math::
-      \mathbf{X}^{\prime} = \mathbf{X} \mathbf{W}^{T} +  \mathbf{b}
-   where :math:\mathbf{W}^{T}  denotes the weights with a fully factorized
-   Gaussian distributions represented by :math:`\mu` and :math:`\sigma`, with
-   an unconstrained parameter :math:`\rho` for :math:`\sigma = log(1+exp(\rho))`
-   to avoid non-negative variance.
-   \mathbf{b} denotes the bias which is based on the same structure as the weights
-   CAUTION: the weights and `edge_weight`
-   are different parameters, since the `edge_weight``
-   are initialized to one, they are not considered as a Parameter.
-   NOTE : the weights and bias are initialized with
-   a Gaussian distribution of mean equal to
-   0.0 and a standard deviation of 0.1
-   Then propagate the message through the graph
-   with a graph convolution operator
-   from the `"Semi-supervised
-   Classification with Graph Convolutional Networks"
-   <https://arxiv.org/abs/1609.02907>`_ paper
-   .. math::
-        \mathbf{X}^{\prime} = \mathbf{\hat{D}}^{-1/2} \mathbf{\hat{A}}
-        \mathbf{\hat{D}}^{-1/2} \mathbf{X} \mathbf{\Theta},
-   where :math:`\mathbf{\hat{A}} = \mathbf{A} + \mathbf{I}` denotes the
-   adjacency matrix with inserted self-loops and
-   :math:`\hat{D}_{ii} = \sum_{j=0} \hat{A}_{ij}`
-   its diagonal degree matrix.
-   The adjacency matrix can include
-   other values than :obj:`1` representing
-   edge weights via the optional :obj:`edge_weight` tensor.
-   Its node-wise formulation is given by:
-   .. math::
-        \mathbf{x}^{\prime}_i = \mathbf{\Theta}^{\top} \sum_{j \in
-        \mathcal{N}(v) \cup \{ i \}} \frac{e_{j,i}}{\sqrt{\hat{d}_j
-        \hat{d}_i}} \mathbf{x}_j
-   with :math:`\hat{d}_i = 1 + \sum_{j \in \mathcal{N}(i)} e_{j,i}`
-   , where
-   :math:`e_{j,i}` denotes the edge weight
-   from source node :obj:`j` to target
-   node :obj:`i` (default: :obj:`1.0`)
-   Args:
-        in_channels (int): Size of each input sample,
-        or :obj:`-1` to derive
-            the size from the first input(s) to the forward method.
-        out_channels (int): Size of each output sample.
-        prior_mean (float): Mean of the prior
-        arbitrary distribution to be
-            used on the complexity cost (default: :obj:`0.0`)
-        prior_variance (float): Variance of
-        the prior arbitrary distribution
-            to be used on the complexity cost (default: :obj:`1.0`)
-        posterior_mu_init (float): Init trainable mu
-        parameter representing
-            mean of the approximate posterior (default: :obj:`-3.0`)
-        posterior_rho_init (float): Init trainable
-        rho parameter representing
-            the sigma of the approximate
-            posterior through softplus function
-            (default: :obj:`0.0`)
-        improved (bool, optional): If set to :obj:`True`, the layer computes
-            :math:`\mathbf{\hat{A}}` as :math:`\mathbf{A} + 2\mathbf{I}`.
-            (default: :obj:`False`)
-        cached (bool, optional): If set to :obj:`True`, the layer will cache
-            the computation of :math:`\mathbf{\hat{D}}^{-1/2} \mathbf{\hat{A}}
-            \mathbf{\hat{D}}^{-1/2}` on first execution, and will use the
-            cached version for further executions.
-            This parameter should only
-            be set to :obj:`True` in transductive
-            learning scenarios. (default: :obj:`False`)
-        normalize (bool, optional): Whether to add self-loops and compute
-            symmetric normalization coefficients on the fly.
-            (default: :obj:`True`)
-        bias (bool, optional): If set to :obj:`False`,
-        the layer will not learn
-            an additive bias. (default: :obj:`True`)
-        **kwargs (optional): Additional arguments of
-            :class:`torch_geometric.nn.conv.MessagePassing`.
-   Shapes:
-        - **input:**
-          node features :math:`(|\mathcal{V}|, F_{in})`,
-          edge indices :math:`(2, |\mathcal{E}|)`,
-          edge weights :math:`(|\mathcal{E}|)` *(optional)*
-        - **output:**
-          node features :math:`(|\mathcal{V}|, F_{out})`,
-          Kullback-Leibler divergence for weights and bias (optional):
-          math : `(\mathrm{KL}[q_{\theta}(w)||p(w)])`
-          with variational parameters math:`(\theta = (\mu, \rho))`
-          to make an approximation
-          of the variational posterior
-          math:`(q(\theta)=\mathcal{N}(\mu, log(1+e^{\rho})))`
-   """
-    _cached_edge_index: Optional[OptPairTensor]
-    _cached_adj_t: Optional[SparseTensor]
-
-    def __init__(self, in_channels: int, out_channels: int, prior_mean=0,
-                 prior_variance=1, posterior_mu_init=0,
-                 posterior_rho_init=-3.0, cached: bool = False,
-                 normalize: bool = False, bias: bool = True, **kwargs):
-
-        kwargs.setdefault('aggr', 'add')
-        super().__init__(**kwargs)
-
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.prior_mean = prior_mean
-        self.prior_variance = prior_variance
-        self.posterior_mu_init = posterior_mu_init
-        self.posterior_rho_init = posterior_rho_init
-        self.cached = cached
-        self.normalize = normalize
-
-        self.mu_weight = Parameter(torch.Tensor(out_channels, in_channels))
-        self.rho_weight = Parameter(torch.Tensor(out_channels, in_channels))
-
-        self.register_buffer('eps_weight',
-                             torch.Tensor(out_channels, in_channels),
-                             persistent=False)
-        self.register_buffer('prior_weight_mu',
-                             torch.Tensor(out_channels, in_channels),
-                             persistent=False)
-        self.register_buffer('prior_weight_sigma',
-                             torch.Tensor(out_channels, in_channels),
-                             persistent=False)
-
-        if bias:
-            self.mu_bias = Parameter(torch.Tensor(out_channels))
-            self.rho_bias = Parameter(torch.Tensor(out_channels))
-            self.register_buffer('eps_bias', torch.Tensor(out_channels),
-                                 persistent=False)
-            self.register_buffer('prior_bias_mu', torch.Tensor(out_channels),
-                                 persistent=False)
-            self.register_buffer('prior_bias_sigma',
-                                 torch.Tensor(out_channels), persistent=False)
-        else:
-            self.register_buffer('prior_bias_mu', None, persistent=False)
-            self.register_buffer('prior_bias_sigma', None, persistent=False)
-            self.register_parameter('mu_bias', None)
-            self.register_parameter('rho_bias', None)
-            self.register_buffer('eps_bias', None, persistent=False)
-
-        self.reset_parameters()
-
-    def reset_parameters(self):
-        self.prior_weight_mu.fill_(self.prior_mean)
-        self.prior_weight_sigma.fill_(self.prior_variance)
-
-        self.mu_weight.data.normal_(mean=self.posterior_mu_init, std=0.1)
-        self.rho_weight.data.normal_(mean=self.posterior_rho_init, std=0.1)
-        if self.mu_bias is not None:
-            self.prior_bias_mu.fill_(self.prior_mean)
-            self.prior_bias_sigma.fill_(self.prior_variance)
-            self.mu_bias.data.normal_(mean=self.posterior_mu_init, std=0.1)
-            self.rho_bias.data.normal_(mean=self.posterior_rho_init, std=0.1)
-        self._cached_edge_index = None
-        self._cached_adj_t = None
-
-    def kl_div(self, mu_q, sigma_q, mu_p, sigma_p):
-        """
-        Calculates kl divergence between two gaussians (Q || P)
-        Parameters:
-             * mu_q: torch.Tensor -> mu parameter of distribution Q
-             * sigma_q: torch.Tensor -> sigma parameter of distribution Q
-             * mu_p: float -> mu parameter of distribution P
-             * sigma_p: float -> sigma parameter of distribution P
-        returns torch.Tensor of shape 0
-        """
-        kl = torch.log(sigma_p) - torch.log(
-            sigma_q) + (sigma_q**2 + (mu_q - mu_p)**2) / (2 *
-                                                          (sigma_p**2)) - 0.5
-        return kl.mean()
-
-    def kl_loss(self):
-        sigma_weight = torch.log1p(torch.exp(self.rho_weight))
-        kl = self.kl_div(self.mu_weight, sigma_weight, self.prior_weight_mu,
-                         self.prior_weight_sigma)
-        if self.mu_bias is not None:
-            sigma_bias = torch.log1p(torch.exp(self.rho_bias))
-            kl += self.kl_div(self.mu_bias, sigma_bias, self.prior_bias_mu,
-                              self.prior_bias_sigma)
-        return kl
-
-    def forward(self, x: Tensor, edge_index: Adj,
-                edge_weight: OptTensor = None,
-                return_kl: bool = True) -> Tensor:
-        """"""
-
-        sigma_weight = torch.log1p(torch.exp(self.rho_weight))
-
-        weight = self.mu_weight + \
-            (sigma_weight * self.eps_weight.data.normal_())
-
-        if return_kl:
-            kl_weight = self.kl_div(self.mu_weight, sigma_weight,
-                                    self.prior_weight_mu,
-                                    self.prior_weight_sigma)
-
-        if self.normalize:
-            if isinstance(edge_index, Tensor):
-                cache = self._cached_edge_index
-                if cache is None:
-                    edge_index, edge_weight = gcn_norm(  # yapf: disable
-                        edge_index, edge_weight, x.size(self.node_dim),
-                        x.dtype)
-                    if self.cached:
-                        self._cached_edge_index = (edge_index, edge_weight)
-                else:
-                    edge_index, edge_weight = cache[0], cache[1]
-
-        elif isinstance(edge_index, SparseTensor):
-            cache = self._cached_adj_t
-            if cache is None:
-                edge_index = gcn_norm(  # yapf: disable
-                    edge_index, edge_weight, x.size(self.node_dim), x.dtype)
-                if self.cached:
-                    self._cached_adj_t = edge_index
-                else:
-                    edge_index = cache
-
-        sigma_weight = torch.log1p(torch.exp(self.rho_weight))
-        weight = self.mu_weight + \
-            (sigma_weight * self.eps_weight.data.normal_())
-        if return_kl:
-            kl_weight = self.kl_div(self.mu_weight, sigma_weight,
-                                    self.prior_weight_mu,
-                                    self.prior_weight_sigma)
-
-        bias = None  # Initialization of bias
-
-        if self.mu_bias is not None:
-            sigma_bias = torch.log1p(torch.exp(self.rho_bias))
-            bias = self.mu_bias + (sigma_bias * self.eps_bias.data.normal_())
-            if return_kl:
-                kl_bias = self.kl_div(self.mu_bias, sigma_bias,
-                                      self.prior_bias_mu,
-                                      self.prior_bias_sigma)
-
-        x = F.linear(x, weight, bias)
-
-        # propagate_type: (x: Tensor, edge_weight: OptTensor)
-        out = self.propagate(edge_index, x=x, edge_weight=edge_weight,
-                             size=None)
-
-        if return_kl:
-            if self.mu_bias is not None:
-                kl = kl_weight + kl_bias
-                return out, kl
-            else:
-                kl = kl_weight
-                return out
-
-    def message(self, x_j: Tensor, edge_weight: OptTensor) -> Tensor:
-        return x_j if edge_weight is None else edge_weight.view(-1, 1) * x_j
-
-    def message_and_aggregate(self, adj_t: SparseTensor, x: Tensor) -> Tensor:
-        return spmm(adj_t, x, reduce=self.aggr)
-
-
-def test_gcn_conv_with_sparse_input_feature():
-    x = torch.sparse_coo_tensor(indices=torch.tensor([[0, 0], [0, 1]]),
-                                values=torch.tensor([1., 1.]),
-                                size=torch.Size([4, 16]))
-    edge_index = torch.tensor([[0, 0, 0, 1, 2, 3], [1, 2, 3, 0, 0, 0]])
+def test_bayesian_conv_norm():
+    x = torch.randn(4, 16)
+    edge_index = torch.tensor([[0, 0, 0], [1, 2, 3]])
+    row, col = edge_index
     conv = BayesianGCNConv(16, 32)
-    out, kl = conv(x, edge_index)
-    print(out.shape, kl.detach())
-    assert out.size() == (4, 32)
-
-
-def test_static_gcn_conv():
+    out1, kl_out1 = conv(x, edge_index)
+    conv.flow = "target_to_source"
+    out2, kl_out2 = conv(x, edge_index.flip(0))
+    # Reminder when declared, the default values for bayesian conv is
+    # prior_mean=0, prior_variance=1
+    # posterior_mu_init = 0
+    # posterior_rho_init = -3.0
+    # The outputs of the Bayesian conv layer should have a mean close to 0
+    # The variance should not exceed 1
+    # The KL divergence should be the same for both convs
+    assert torch.allclose(out1.mean(), out2.mean(), atol=0.1)
+    assert torch.allclose(torch.var(out1), torch.var(out2), atol=1)
+    assert torch.allclose(kl_out1, kl_out2, atol=1e-6)
+    
+def test_static_bayesian_conv():
     x = torch.randn(3, 4, 16)
     edge_index = torch.tensor([[0, 0, 0, 1, 2, 3], [1, 2, 3, 0, 0, 0]])
     conv = BayesianGCNConv(16, 32)
     out, kl = conv(x, edge_index)
-    print(out.shape, kl.detach().cpu())
     assert out.size() == (3, 4, 32)
-
-
+    
+def test_gcn_conv_with_sparse_input_feature():
+    x = torch.sparse_coo_tensor(indices=torch.tensor([[0, 0], [0, 1]]),
+                                    values=torch.tensor([1., 1.]),
+                                    size=torch.Size([4, 16]))
+    edge_index = torch.tensor([[0, 0, 0, 1, 2, 3], [1, 2, 3, 0, 0, 0]])
+    conv = BayesianGCNConv(16, 32)
+    out, kl = conv(x, edge_index)
+    assert out.size() ==(4, 32)
+    
 def test_gcn_conv_with_decomposed_layers():
     x = torch.randn(4, 16)
     edge_index = torch.tensor([[0, 0, 0, 1, 2, 3], [1, 2, 3, 0, 0, 0]])
     conv = BayesianGCNConv(16, 32)
     decomposed_conv = copy.deepcopy(conv)
     decomposed_conv.decomposed_layers = 2
-    out1, kl = conv(x, edge_index)
-    out2, kl_2 = decomposed_conv(x, edge_index)
-    print(out1.shape, out2.shape)
-    # the weight differs from out1 to out2 because different weight and biases
-    # assert torch.allclose(out1, out2, atol=1.0)
-
+    out1, kl_out1 = conv(x, edge_index)
+    out2, kl_out2 = decomposed_conv(x, edge_index)
+    # Reminder when declared, the default values for bayesian conv is
+    # prior_mean=0, prior_variance=1
+    # posterior_mu_init = 0
+    # posterior_rho_init = -3.0
+    # The outputs of the Bayesian conv layer should have a mean close to 0
+    # The variance should not exceed 1
+    # The KL divergence should be the same for both convs
+    assert torch.allclose(out1.mean(), out2.mean(), atol=0.1)
+    assert torch.allclose(torch.var(out1), torch.var(out2), atol=1)
+    assert torch.allclose(kl_out1, kl_out2, atol=1e-6)
+    
     if is_full_test():
         t = '(Tensor, Tensor, OptTensor) -> Tensor'
         jit = torch.jit.script(decomposed_conv.jittable(t))
         assert jit(x, edge_index).tolist() == out1.tolist()
-
-
+        
 def test_gcn_conv():
     x = torch.randn(4, 16)
     edge_index = torch.tensor([[0, 0, 0, 1, 2, 3], [1, 2, 3, 0, 0, 0]])
     row, col = edge_index
     value = torch.rand(row.size(0))
-    # zeros_values = torch.zeros(value.size()).to(device)
-    # adj1 = torch.sparse_coo_tensor(edge_index, value, device=x.device)
-    # adj2 = torch.sparse_coo_tensor(edge_index, zeros_value, device=x.device)
+    adj2 = SparseTensor(row=row, col=col, value=value, sparse_sizes=(4, 4))
+    adj1 = adj2.set_value(None)
+    adj3 = adj1.to_torch_sparse_coo_tensor()
+    adj4 = adj2.to_torch_sparse_coo_tensor()
     conv = BayesianGCNConv(16, 32)
     assert conv.__repr__() == 'BayesianGCNConv(16, 32)'
-    out1, kl = conv(x, edge_index)
-    print(out1.shape, kl.detach())
+    out1, kl_out1 = conv(x, edge_index)
     assert out1.size() == (4, 32)
-
-    out2, kl = conv(x, edge_index, value)
-    print(out2.shape, kl.detach())
+    out_1_adj1, kl_adj1 = conv(x, adj1.t())
+    assert torch.allclose(out1.mean(), out_1_adj1.mean(), atol=0.1)
+    assert torch.allclose(torch.var(out1), torch.var(out_1_adj1), atol=1)
+    assert torch.allclose(kl_out1, kl_adj1, atol=1e-6)
+    out2, kl_out2 = conv(x, edge_index, value)
     assert out2.size() == (4, 32)
+    out_2_adj2, kl_adj2 = conv(x, adj2.t())
+    assert torch.allclose(out2.mean(), out_2_adj2.mean(), atol=0.1)
+    assert torch.allclose(torch.var(out2), torch.var(out_2_adj2), atol=1)
+    assert torch.allclose(kl_out2, kl_adj2, atol=1e-6)
 
     if is_full_test():
         t = '(Tensor, Tensor, OptTensor) -> Tensor'
         jit = torch.jit.script(conv.jittable(t))
         assert jit(x, edge_index).tolist() == out1.tolist()
         assert jit(x, edge_index, value).tolist() == out2.tolist()
+
+        t = '(Tensor, SparseTensor, OptTensor) -> Tensor'
+        jit = torch.jit.script(conv.jittable(t))
+        assert torch.allclose(jit(x, adj1.t()), out1, atol=1e-6)
+        assert torch.allclose(jit(x, adj2.t()), out2, atol=1e-6)
+
+    conv.cached = True
+    out_f1 , kl_f1 = conv(x, edge_index)
+    for i in range(len(out1.tolist())):
+        assert torch.allclose(out_f1[i].mean(), out1[i].mean(), atol=0.1)
+        assert torch.allclose(torch.var(out_f1[i]), torch.var(out1[i]), atol=1)
+    assert torch.allclose(kl_f1, kl_out1, atol=1e-6)
+
+
+    out_f2, kl_f2 = conv(x, adj1.t())
+    assert torch.allclose(out_f2.mean(), out1.mean(), atol=0.1)
+    assert torch.allclose(torch.var(out_f2), torch.var(out1), atol=1)
+    assert torch.allclose(kl_f2, kl_out1, atol=1e-6)
+
+    out3, kl_3 = conv(x, adj3.t())
+    assert torch.allclose(out3.mean(), out1.mean(), atol=0.1)
+    assert torch.allclose(torch.var(out3), torch.var(out1), atol=1)
+    assert torch.allclose(kl_out1, kl_3, atol=1e-6)
+
+    out4, kl_4 = conv(x, adj4.t())
+    assert torch.allclose(out4.mean(), out4.mean(), atol=0.1)
+    assert torch.allclose(torch.var(out4), torch.var(out4), atol=1)
+    assert torch.allclose(kl_out1, kl_4, atol=1e-6)
+
+
+   
+ 
+
+ 
+
+
+ 
