4e5f9d915193de2257dcd015829e3311f2d9c9bc
Gabriel VIGOT <56968382+gabyweg76@users.noreply.github.com>
2023-02-18 23:18:55 +0100

    Update BayesianGCNConv_test.py

--- test/nn/conv/BayesianGCNConv_test.py
+++ test/nn/conv/BayesianGCNConv_test.py
@@ -75,7 +75,9 @@ def test_gcn_conv():
     edge_index = torch.tensor([[0, 0, 0, 1, 2, 3], [1, 2, 3, 0, 0, 0]])
     row, col = edge_index
     value = torch.rand(row.size(0))
-    adj2 = SparseTensor(row=row, col=col, value=value, sparse_sizes=(4, 4))
+    adj2 = torch.sparse_coo_tensor(indices=edge_index,
+                                   values=value,
+                                   size=torch.Size([4, 4]))
     adj1 = adj2.set_value(None)
     adj3 = adj1.to_torch_sparse_coo_tensor()
     adj4 = adj2.to_torch_sparse_coo_tensor()
@@ -93,18 +95,7 @@ def test_gcn_conv():
     assert torch.allclose(out2.mean(), out_2_adj2.mean(), atol=0.1)
     assert torch.allclose(torch.var(out2), torch.var(out_2_adj2), atol=1)
     assert torch.allclose(kl_out2, kl_adj2, atol=1e-6)
-
-    if is_full_test():
-        t = '(Tensor, Tensor, OptTensor) -> Tensor'
-        jit = torch.jit.script(conv.jittable(t))
-        assert jit(x, edge_index).tolist() == out1.tolist()
-        assert jit(x, edge_index, value).tolist() == out2.tolist()
-
-        t = '(Tensor, SparseTensor, OptTensor) -> Tensor'
-        jit = torch.jit.script(conv.jittable(t))
-        assert torch.allclose(jit(x, adj1.t()), out1, atol=1e-6)
-        assert torch.allclose(jit(x, adj2.t()), out2, atol=1e-6)
-
+    
     conv.cached = True
     out_f1, kl_f1 = conv(x, edge_index)
     for i in range(len(out1.tolist())):
