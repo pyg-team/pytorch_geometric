ab1c9b85da48eeac2bcac812d129944354c70817
rusty1s <matthias.fey@tu-dortmund.de>
2023-02-27 09:49:57 +0000

    changelog

--- test/data/test_datapipes.py
+++ test/data/test_datapipes.py
@@ -1,3 +1,4 @@
+import pytest
 import torch
 
 from torch_geometric.data import Data
@@ -7,46 +8,42 @@ from torch_geometric.testing import withPackage
 from torch_geometric.utils import to_smiles
 
 
-def create_dataset_adapter() -> DatasetAdapter:
+@pytest.fixture()
+def dataset_adapter() -> DatasetAdapter:
     x = torch.randn(3, 8)
     edge_index = torch.tensor([[0, 1, 1], [1, 0, 2]])
     data = Data(x=x, edge_index=edge_index)
-    return DatasetAdapter([data, data, data, data, data])
+    return DatasetAdapter([data, data, data, data])
 
 
-def test_dataset_adapter():
-    dp = create_dataset_adapter()
-
-    # Test DataLoader with the datapipe:
-    loader = DataLoader(dp, batch_size=2, drop_last=True)
+def test_dataset_adapter(dataset_adapter):
+    loader = DataLoader(dataset_adapter, batch_size=2)
     batch = next(iter(loader))
     assert batch.x.shape == (6, 8)
     assert len(loader) == 2
 
-    # Test apply sharding:
-    dp.apply_sharding(2, 0)
-    num_examples = sum(1 for _ in iter(dp))
-    assert num_examples == 3
+    # Test sharding:
+    dataset_adapter.apply_sharding(2, 0)
+    assert len([data for data in dataset_adapter]) == 2
 
 
-def test_datapipe_batch_graphs():
-    dp = create_dataset_adapter()
-    batch_dp = dp.batch_graphs(batch_size=2, drop_last=True)
-    batch = next(iter(batch_dp))
+def test_datapipe_batch_graphs(dataset_adapter):
+    dp = dataset_adapter.batch_graphs(batch_size=2)
+    assert len(dp) == 2
+    batch = next(iter(dp))
     assert batch.x.shape == (6, 8)
-    assert len(batch_dp) == 2
 
 
-def test_functional_transform():
-    dp = create_dataset_adapter()
-    assert next(iter(dp)).is_directed()
-    dp = dp.to_undirected()
-    assert next(iter(dp)).is_undirected()
+def test_functional_transform(dataset_adapter):
+    assert next(iter(dataset_adapter)).is_directed()
+    dataset_adapter = dataset_adapter.to_undirected()
+    assert next(iter(dataset_adapter)).is_undirected()
 
 
 @withPackage('rdkit')
 def test_datapipe_parse_smiles():
     smiles = 'F/C=C/F'
+
     dp = DatasetAdapter([smiles])
     dp = dp.parse_smiles()
     assert to_smiles(next(iter(dp))) == smiles
