73fc5117e2158b7d16187c528150f1a06781ba21
rusty1s <matthias.fey@tu-dortmund.de>
2023-02-27 11:50:15 +0000

    update

--- torch_geometric/utils/get_mesh_laplacian.py
+++ torch_geometric/utils/get_mesh_laplacian.py
@@ -3,7 +3,12 @@ from typing import Optional, Tuple
 import torch
 from torch import Tensor
 
-from torch_geometric.utils import add_self_loops, coalesce, to_undirected
+from torch_geometric.utils import (
+    add_self_loops,
+    coalesce,
+    scatter,
+    to_undirected,
+)
 
 
 def get_mesh_laplacian(
@@ -70,47 +75,40 @@ def get_mesh_laplacian(
 
     # Face to edge:
     cot_index = torch.cat([face[:2], face[1:], face[::2]], dim=1)
-    cot_index, cot_weight = to_undirected(*coalesce(cot_index, cot_weight))
+    cot_index, cot_weight = to_undirected(cot_index, cot_weight)
 
     # Compute the diagonal part:
-    row, col = cot_index
-    cot_deg = torch.zeros(num_nodes).scatter_add_(0, row, cot_weight)
+    cot_deg = scatter(cot_weight, cot_index[0], 0, num_nodes, reduce='sum')
     edge_index, _ = add_self_loops(cot_index, num_nodes=num_nodes)
     edge_weight = torch.cat([cot_weight, -cot_deg], dim=0)
 
     if normalization is not None:
 
         def get_areas(left, centre, right):
-            left_pos, central_pos, right_pos = pos[left], pos[centre], pos[
-                right]
-            left_vec = left_pos - central_pos
-            right_vec = right_pos - central_pos
+            central_pos = pos[centre]
+            left_vec = pos[left] - central_pos
+            right_vec = pos[right] - central_pos
             cross = torch.norm(torch.cross(left_vec, right_vec, dim=1), dim=1)
             area = cross / 6.0  # one-third of a triangle's area is cross / 6.0
             return area / 2.0  # since each corresponding area is counted twice
 
-        # Like before, but here we only need the diagonal (i.e. the mass matrix):
+        # Like before, but here we only need the diagonal (the mass matrix):
         area_021 = get_areas(face[0], face[2], face[1])
         area_102 = get_areas(face[1], face[0], face[2])
         area_012 = get_areas(face[0], face[1], face[2])
         area_weight = torch.cat([area_021, area_102, area_012])
         area_index = torch.cat([face[:2], face[1:], face[::2]], dim=1)
-        area_index, area_weight = to_undirected(
-            *coalesce(area_index, area_weight))
-        row, col = area_index
-        area_deg = torch.zeros(num_nodes).scatter_add_(0, row, area_weight)
+        area_index, area_weight = to_undirected(area_index, area_weight)
+        area_deg = scatter(area_weight, area_index[0], 0, num_nodes, 'sum')
 
         if normalization == 'sym':
             area_deg_inv_sqrt = area_deg.pow_(-0.5)
-            area_deg_inv_sqrt.masked_fill_(area_deg_inv_sqrt == float('inf'),
-                                           0)
-            row, col = edge_index
-            edge_weight = area_deg_inv_sqrt[
-                row] * edge_weight * area_deg_inv_sqrt[col]
+            area_deg_inv_sqrt[area_deg_inv_sqrt == float('inf')] = 0.0,
+            edge_weight = (area_deg_inv_sqrt[edge_index[0]] * edge_weight *
+                           area_deg_inv_sqrt[edge_index[1]])
         elif normalization == 'rw':
             area_deg_inv = 1.0 / area_deg
-            area_deg_inv.masked_fill_(area_deg_inv == float('inf'), 0)
-            row, col = edge_index
-            edge_weight = area_deg_inv[row] * edge_weight
+            area_deg_inv[area_deg_inv == float('inf')] = 0.0
+            edge_weight = area_deg_inv[edge_index[0]] * edge_weight
 
     return edge_index, edge_weight
