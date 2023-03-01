c3db6c3642da322b04f598cbde67623b4048c622
rusty1s <matthias.fey@tu-dortmund.de>
2023-03-01 16:27:46 +0000

    changelog

--- torch_geometric/loader/zip_loader.py
+++ torch_geometric/loader/zip_loader.py
@@ -9,9 +9,27 @@ from torch_geometric.loader.base import DataLoaderIterator
 
 
 class ZipLoader(torch.utils.data.DataLoader):
+    r"""A loader that returns a tuple of data objects by sampling from multiple
+    :class:`NodeLoader` or :class:`LinkLoader` instances.
+
+    Args:
+        loaders (List[NodeLoader] or List[LinkLoader]): The loader instances.
+        filter_per_worker (bool, optional): If set to :obj:`True`, will filter
+            the returning data in each worker's subprocess rather than in the
+            main process.
+            Setting this to :obj:`True` for in-memory datasets is generally not
+            recommended:
+            (1) it may result in too many open file handles,
+            (2) it may slown down data loading,
+            (3) it requires operating on CPU tensors.
+            (default: :obj:`False`)
+        **kwargs (optional): Additional arguments of
+            :class:`torch.utils.data.DataLoader`, such as :obj:`batch_size`,
+            :obj:`shuffle`, :obj:`drop_last` or :obj:`num_workers`.
+    """
     def __init__(
         self,
-        loaders: List[Union[NodeLoader, LinkLoader]],
+        loaders: Union[List[NodeLoader], List[LinkLoader]],
         filter_per_worker: bool = False,
         **kwargs,
     ):
@@ -28,12 +46,13 @@ class ZipLoader(torch.utils.data.DataLoader):
             if not callable(getattr(loader, 'filter_fn', None)):
                 raise ValueError("'{loader.__class__.__name__}' does not have "
                                  "a 'filter_fn' method")
-            loader = loader.filter_per_worker = filter_per_worker
+            loader.filter_per_worker = filter_per_worker
 
         iterator = range(min([len(loader.dataset) for loader in loaders]))
         super().__init__(iterator, collate_fn=self.collate_fn, **kwargs)
 
         self.loaders = loaders
+        self.filter_per_worker = filter_per_worker
 
     def collate_fn(self, index: List[int]) -> Tuple[Any, ...]:
         if not isinstance(index, Tensor):
