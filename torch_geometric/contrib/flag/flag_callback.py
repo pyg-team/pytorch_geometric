b469c3cfc9a9a1df9346e9d81685b32ffd3f1f6f
jreisler <julia92796@gmail.com>
2023-03-10 20:32:44 -0800

    added doc strings for flag and callback

--- torch_geometric/contrib/flag/flag_callback.py
+++ torch_geometric/contrib/flag/flag_callback.py
@@ -1,14 +1,71 @@
+from typing import Optional
+
+import torch
+
+
 class FLAGCallback:
-  def on_ascent_step_begin(self, step_index, loss):
-    pass
+    """
+    Base class for callbacks to use in order to hook into the various stages
+    of the model training with the
+    `FLAG data augmentation strategy
+    <https://github.com/pyg-team/pytorch_geometric/blob/master/
+    torch_geometric/contrib/flag/flag.py>`_.
 
-  def on_ascent_step_end(self, step_index, loss, perturb_data):
-    pass
+    To create a custom callback, subclass
+    `torch_geometric.contrib.flag.FLAGCallback` and override the method
+    associated with the stage of interest.
+    """
+    def on_ascent_step_begin(self, step_index: int,
+                             loss: Optional[torch.nn.Module]):
+        """
+        Called at the start of an ascent step.
 
-  def on_optimizer_step_begin(self, loss):
-    pass
+        Subclasses should override for any actions to run. This function
+        should only be called during TRAIN mode.
 
-  def on_optimizer_step_end(self, loss):
-    pass
+        Args:
+            step_index (int): The index of step.
+            loss (Optional[torch.nn.Module]): The loss before the ascent
+                step. Will be None before the first ascent step.
+        """
+        pass
 
+    def on_ascent_step_end(self, step_index: int, loss: torch.nn.Module,
+                           perturb_data: torch.nn.Parameter):
+        """
+        Called at the end of an ascent step.
 
+        Subclasses should override for any actions to run. This function
+        should only be called during TRAIN mode.
+
+        Args:
+            step_index (int): The index of step.
+            loss (torch.nn.Module): The loss after the ascent step.
+            perturb_data (torch.nn.Parameter): The tensor containing the input
+                perturbations after the ascent step.
+        """
+        pass
+
+    def on_optimizer_step_begin(self, loss: torch.nn.Module):
+        """
+        Called at the start of an optimizer step.
+
+        Subclasses should override for any actions to run. This function
+        should only be called during TRAIN mode.
+
+        Args:
+            loss (torch.nn.Module): The loss after the ascent step.
+        """
+        pass
+
+    def on_optimizer_step_end(self, loss: torch.nn.Module):
+        """
+        Called at the end of an optimizer step.
+
+        Subclasses should override for any actions to run. This function
+        should only be called during TRAIN mode.
+
+        Args:
+            loss (torch.nn.Module): The loss after the ascent step.
+        """
+        pass
