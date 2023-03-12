e8b41cfeec0b70b692e1a042e6f1e542f1fe446d
Matthias Fey <matthias.fey@tu-dortmund.de>
2023-03-12 17:59:31 +0100

    Fix `DBLP` download, move `get_edge_index` to `testing` package (#6901)

--- test/conftest.py
+++ test/conftest.py
@@ -27,20 +27,15 @@ def load_dataset(root: str, name: str, *args, **kwargs) -> Dataset:
     if name.lower() in ['bashapes']:
         from torch_geometric.datasets import BAShapes
         return BAShapes(*args, **kwargs)
-    if name.lower() in ['dblp']:
-        from torch_geometric.datasets import DBLP
-        path = osp.join(root, 'DBLP')
-        return DBLP(path, *args, **kwargs)
     if name in ['citationCiteseer', 'illc1850']:
         from torch_geometric.datasets import SuiteSparseMatrixCollection
         path = osp.join(root, 'SuiteSparseMatrixCollection')
         return SuiteSparseMatrixCollection(path, name=name, *args, **kwargs)
-    if 'elliptic' in name.lower():
-        from torch_geometric.datasets import EllipticBitcoinDataset
-        path = osp.join(root, 'EllipticBitcoinDataset')
-        return EllipticBitcoinDataset(path, *args, **kwargs)
+    if name.lower() in ['hetero']:
+        from torch_geometric.testing import FakeHeteroDataset
+        return FakeHeteroDataset(*args, **kwargs)
 
-    raise NotImplementedError
+    raise ValueError(f"Cannot load dataset with name '{name}'")
 
 
 @pytest.fixture(scope='session')
