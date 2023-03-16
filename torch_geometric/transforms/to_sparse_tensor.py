74c3eaa82a66dc19dd7c43871a7280e4ea3c77c5
EdisonLeeeee <cnljt@outlook.com>
2023-03-16 21:16:04 +0800

    update

--- torch_geometric/transforms/to_sparse_tensor.py
+++ torch_geometric/transforms/to_sparse_tensor.py
@@ -41,6 +41,7 @@ class ToSparseTensor(BaseTransform):
             a :class:`~torch_sparse.SparseTensor` object. If set to
             :obj:`'torch'`, the :obj:`edge_index`_index will be converted to a
             :class:`torch.spase.Tensor` object.
+            (default: :obj:`'torch_sparse'`)
     """
     def __init__(
         self,
