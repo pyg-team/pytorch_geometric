78a0698bfa6b12009a533240cc9c96cafcd19801
jreisler <julia92796@gmail.com>
2023-03-18 15:23:55 -0700

    wip

---
+++ examples/contrib/flag_node_classification.py
@@ -0,0 +1,226 @@
+import os.path as osp
+
+import torch
+import torch.nn.functional as F
+from ogb.nodeproppred import Evaluator, PygNodePropPredDataset
+from tqdm import tqdm
+
+from torch_geometric.loader import NeighborSampler
+from torch_geometric.nn import SAGEConv
+from torch_geometric.contrib.flag.flag import FLAG
+
+root = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'products')
+dataset = PygNodePropPredDataset('ogbn-products', root)
+split_idx = dataset.get_idx_split()
+evaluator = Evaluator(name='ogbn-products')
+data = dataset[0]
+
+train_idx = split_idx['train']
+
+# TODO: change this back 
+train_loader = NeighborSampler(data.edge_index, node_idx=train_idx,
+                               sizes=[15, 10, 5], batch_size=1024,
+                               shuffle=True, num_workers=12)
+subgraph_loader = NeighborSampler(data.edge_index, node_idx=None, sizes=[-1],
+                                  batch_size=4096, shuffle=False,
+                                  num_workers=12)
+# train_loader = NeighborSampler(data.edge_index, node_idx=train_idx,
+#                                sizes=[15, 10, 5], batch_size=1024,
+#                                shuffle=True)
+# subgraph_loader = NeighborSampler(data.edge_index, node_idx=None, sizes=[-1],
+#                                   batch_size=4096, shuffle=False)
+
+
+class SAGE(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels, num_layers):
+        super().__init__()
+
+        self.num_layers = num_layers
+
+        self.convs = torch.nn.ModuleList()
+        self.convs.append(SAGEConv(in_channels, hidden_channels))
+        for _ in range(num_layers - 2):
+            self.convs.append(SAGEConv(hidden_channels, hidden_channels))
+        self.convs.append(SAGEConv(hidden_channels, out_channels))
+
+    def reset_parameters(self):
+        for conv in self.convs:
+            conv.reset_parameters()
+
+    def forward(self, x, adjs):
+        # `train_loader` computes the k-hop neighborhood of a batch of nodes,
+        # and returns, for each layer, a bipartite graph object, holding the
+        # bipartite edges `edge_index`, the index `e_id` of the original edges,
+        # and the size/shape `size` of the bipartite graph.
+        # Target nodes are also included in the source nodes so that one can
+        # easily apply skip-connections or add self-loops.
+        for i, (edge_index, _, size) in enumerate(adjs):
+            x_target = x[:size[1]]  # Target nodes are always placed first.
+            x = self.convs[i]((x, x_target), edge_index)
+            if i != self.num_layers - 1:
+                x = F.relu(x)
+                x = F.dropout(x, p=0.5, training=self.training)
+        return x.log_softmax(dim=-1)
+
+    def inference(self, x_all):
+        pbar = tqdm(total=x_all.size(0) * self.num_layers)
+        pbar.set_description('Evaluating')
+
+        # Compute representations of nodes layer by layer, using *all*
+        # available edges. This leads to faster computation in contrast to
+        # immediately computing the final representations of each batch.
+        total_edges = 0
+        for i in range(self.num_layers):
+            xs = []
+            for batch_size, n_id, adj in subgraph_loader:
+                edge_index, _, size = adj.to(device)
+                total_edges += edge_index.size(1)
+                x = x_all[n_id].to(device)
+                x_target = x[:size[1]]
+                x = self.convs[i]((x, x_target), edge_index)
+                if i != self.num_layers - 1:
+                    x = F.relu(x)
+                xs.append(x.cpu())
+
+                pbar.update(batch_size)
+
+            x_all = torch.cat(xs, dim=0)
+
+        pbar.close()
+
+        return x_all
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = SAGE(dataset.num_features, 256, dataset.num_classes, num_layers=3)
+model = model.to(device)
+
+x = data.x.to(device)
+y = data.y.squeeze().to(device)
+
+# FLAG parameters
+step_size_labeled = 8e-03
+step_size_unlabeled = 1.6e-2
+M = 3
+
+
+def train(epoch):
+    model.train()
+
+    pbar = tqdm(total=train_idx.size(0))
+    pbar.set_description(f'Epoch {epoch:02d}')
+
+    total_loss = 0
+    for batch_size, n_id, adjs in train_loader:
+
+        print("starting batch")
+        loss, _ = flag.forward(x[n_id], adjs, torch.unsqueeze(y[n_id[:batch_size]], 1), torch.tensor([i for i in range(batch_size)], device=device), step_size_labeled, step_size_unlabeled, n_ascent_steps=M)
+
+        print(loss)
+
+        # # `adjs` holds a list of `(edge_index, e_id, size)` tuples.
+        # adjs = [adj.to(device) for adj in adjs]
+
+        # optimizer.zero_grad()
+        # out = model(x[n_id], adjs)
+        # loss = F.nll_loss(out, y[n_id[:batch_size]])
+
+
+        # loss.backward()
+        # optimizer.step()
+
+        total_loss += float(loss)
+        pbar.update(batch_size)
+
+    pbar.close()
+
+    loss = total_loss / len(train_loader)
+
+    return loss
+
+
+def train_normal(epoch):
+    model.train()
+
+    pbar = tqdm(total=train_idx.size(0))
+    pbar.set_description(f'Epoch {epoch:02d}')
+
+    total_loss = 0
+    for batch_size, n_id, adjs in train_loader:
+        # `adjs` holds a list of `(edge_index, e_id, size)` tuples.
+        adjs = [adj.to(device) for adj in adjs]
+
+        optimizer.zero_grad()
+        out = model(x[n_id], adjs)
+        loss = F.nll_loss(out, y[n_id[:batch_size]])
+        loss.backward()
+        optimizer.step()
+
+        total_loss += float(loss)
+        pbar.update(batch_size)
+
+    pbar.close()
+
+    loss = total_loss / len(train_loader)
+
+    return loss
+
+
+
+@torch.no_grad()
+def test():
+    model.eval()
+
+    out = model.inference(x)
+
+    y_true = y.cpu().unsqueeze(-1)
+    y_pred = out.argmax(dim=-1, keepdim=True)
+
+    train_acc = evaluator.eval({
+        'y_true': y_true[split_idx['train']],
+        'y_pred': y_pred[split_idx['train']],
+    })['acc']
+    val_acc = evaluator.eval({
+        'y_true': y_true[split_idx['valid']],
+        'y_pred': y_pred[split_idx['valid']],
+    })['acc']
+    test_acc = evaluator.eval({
+        'y_true': y_true[split_idx['test']],
+        'y_pred': y_pred[split_idx['test']],
+    })['acc']
+
+    return train_acc, val_acc, test_acc
+
+
+## Test acc for 1 iteration: 0.7868
+
+test_accs = []
+for run in range(1, 11):
+    print('')
+    print(f'Run {run:02d}:')
+    print('')
+
+    model.reset_parameters()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.003)
+    flag = FLAG(model, optimizer, F.nll_loss, device)
+
+    best_val_acc = final_test_acc = 0
+
+    # Since FLAG runs M forward and backwards passes, we can reduce the
+    # number of epochs by a factor of M
+    for epoch in range(1, 22):
+        loss = train_normal(epoch)
+        print(f'Epoch {epoch:02d}, Loss: {loss:.4f}')
+
+        train_acc, val_acc, test_acc = test()
+        print(f'Train: {train_acc:.4f}, Val: {val_acc:.4f}, '
+              f'Test: {test_acc:.4f}')
+
+        if val_acc > best_val_acc:
+            best_val_acc = val_acc
+            final_test_acc = test_acc
+    test_accs.append(final_test_acc)
+
+test_acc = torch.tensor(test_accs)
+print('============================')
+print(f'Final Test: {test_acc.mean():.4f} ± {test_acc.std():.4f}')
