3e9ec360a35ee924c55aaed2ee09f3bf62e5cc3e
Matthias Fey <matthias.fey@tu-dortmund.de>
2023-03-19 01:48:45 +0100

    Evaluate `torch.compile` against custom GNN layer (#6963)
    Co-authored-by: wsad1 <jinu.sunil@gmail.com>

--- test/nn/test_compile_conv.py
+++ test/nn/test_compile_conv.py
@@ -1,10 +1,24 @@
 import pytest
 import torch
+from torch import Tensor
 
 from torch_geometric import enable_compile
 from torch_geometric.nn import GATConv, GCNConv, SAGEConv
 from torch_geometric.profile import benchmark
 from torch_geometric.testing import onlyLinux, withCUDA, withPackage
+from torch_geometric.utils import scatter
+
+
+class MySAGEConv(torch.nn.Module):
+    def __init__(self, in_channels: int, out_channels: int):
+        super().__init__()
+        self.lin_src = torch.nn.Linear(in_channels, out_channels)
+        self.lin_dst = torch.nn.Linear(in_channels, out_channels)
+
+    def forward(self, x: Tensor, edge_index: Tensor) -> Tensor:
+        x_j = x[edge_index[0]]
+        out = scatter(x_j, edge_index[1], dim_size=x.size(0), reduce='mean')
+        return self.lin_src(out) + self.lin_dst(x)
 
 
 @withCUDA
@@ -20,6 +34,8 @@ def test_compile_conv(device, Conv):
     out = torch.compile(conv)(x, edge_index)
     if not x.is_cuda or Conv != GATConv:  # TODO Fix compiled GATConv on GPU.
         assert torch.allclose(conv(x, edge_index), out, atol=1e-6)
+    else:
+        assert not torch.allclose(conv(x, edge_index), out, atol=1e-2)
 
 
 if __name__ == '__main__':
@@ -34,7 +50,7 @@ if __name__ == '__main__':
     x = torch.randn(num_nodes, 64, device=args.device)
     edge_index = torch.randint(num_nodes, (2, num_edges), device=args.device)
 
-    for Conv in [GCNConv, SAGEConv, GATConv]:
+    for Conv in [GCNConv, SAGEConv, MySAGEConv, GATConv]:
         print(f'Conv: {Conv.__name__}')
 
         conv = Conv(64, 64).to(args.device)
