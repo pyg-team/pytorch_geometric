a09400cfeaf394bca4ce19301db3672e80019d71
rusty1s <matthias.fey@tu-dortmund.de>
2023-03-20 08:19:05 +0000

    update

--- examples/explain/captum_explainer.py
+++ examples/explain/captum_explainer.py
@@ -3,14 +3,13 @@ import os.path as osp
 import torch
 import torch.nn.functional as F
 
-import torch_geometric.transforms as T
 from torch_geometric.datasets import Planetoid
 from torch_geometric.explain import CaptumExplainer, Explainer
 from torch_geometric.nn import GCNConv
 
 dataset = 'Cora'
 path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'Planetoid')
-dataset = Planetoid(path, dataset, transform=T.NormalizeFeatures())
+dataset = Planetoid(path, dataset)
 data = dataset[0]
 
 
@@ -40,18 +39,10 @@ for _ in range(1, 201):
     loss.backward()
     optimizer.step()
 
-# Select the output index of a node to be explained:
-index = 10
-
-# Instantiate the CaptumExplainer algorithm:
-algorithm = CaptumExplainer(attribution_method='IntegratedGradients', )
-
-# Create an instance of the Explainer class with the GNN model
-# and the CaptumExplainer algorithm:
 explainer = Explainer(
     model=model,
-    algorithm=algorithm,
-    explanation_type='model',  # or phenomenon
+    algorithm=CaptumExplainer('IntegratedGradients'),
+    explanation_type='model',
     model_config=dict(
         mode='multiclass_classification',
         task_level='node',
@@ -65,20 +56,14 @@ explainer = Explainer(
     ),
 )
 
-# Compute the explanation:
-explanation = explainer(
-    x=data.x,
-    edge_index=data.edge_index,
-    index=index,
-)
+node_index = 10
+explanation = explainer(data.x, data.edge_index, index=node_index)
 print(f'Generated explanations in {explanation.available_explanations}')
 
-# Visualize the node feature importance:
 path = 'feature_importance.png'
 explanation.visualize_feature_importance(path, top_k=10)
 print(f"Feature importance plot has been saved to '{path}'")
 
-# Visualize the subgraph:
 path = 'subgraph.pdf'
 explanation.visualize_graph(path)
 print(f"Subgraph plot has been saved to '{path}'")
