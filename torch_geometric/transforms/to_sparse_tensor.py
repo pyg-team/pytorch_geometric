43dffde155af42929fffb693a77cea1c2fd721ec
rusty1s <matthias.fey@tu-dortmund.de>
2023-03-20 07:08:58 +0000

    changelog

--- torch_geometric/transforms/to_sparse_tensor.py
+++ torch_geometric/transforms/to_sparse_tensor.py
@@ -1,27 +1,34 @@
 from typing import Optional, Union
 
+import torch
+from torch import Tensor
+
+import torch_geometric.typing
 from torch_geometric.data import Data, HeteroData
 from torch_geometric.data.datapipes import functional_transform
 from torch_geometric.transforms import BaseTransform
 from torch_geometric.typing import SparseTensor
-from torch_geometric.utils import sort_edge_index, to_torch_csr_tensor
+from torch_geometric.utils import (
+    sort_edge_index,
+    to_torch_coo_tensor,
+    to_torch_csr_tensor,
+)
 
 
 @functional_transform('to_sparse_tensor')
 class ToSparseTensor(BaseTransform):
     r"""Converts the :obj:`edge_index` attributes of a homogeneous or
-    heterogeneous data object into a (transposed)
-    :class:`torch_sparse.SparseTensor` or a native PyTorch sparse tensor
-    object with key :obj:`adj_t`
+    heterogeneous data object into a **transposed**
+    :class:`torch_sparse.SparseTensor` or a :pytorch:`PyTorch`
+    :class:`torch.sparse.Tensor` object with key :obj:`adj_t`
     (functional name: :obj:`to_sparse_tensor`).
 
     .. note::
 
         In case of composing multiple transforms, it is best to convert the
-        :obj:`data` object to a :class:`~torch_sparse.SparseTensor` or a
-        :class:`torch.sparse.Tensor` as late as possible,
-        since there exist some transforms that are only able to
-        operate on :obj:`data.edge_index` for now.
+        :obj:`data` object via :class:`ToSparseTensor` as late as possible,
+        since there exist some transforms that are only able to operate on
+        :obj:`data.edge_index` for now.
 
     Args:
         attr (str, optional): The name of the attribute to add as a value to
@@ -31,30 +38,35 @@ class ToSparseTensor(BaseTransform):
         remove_edge_index (bool, optional): If set to :obj:`False`, the
             :obj:`edge_index` tensor will not be removed.
             (default: :obj:`True`)
-        fill_cache (bool, optional): If set to :obj:`False`, will not fill the
-            underlying :class:`~torch_sparse.SparseTensor` cache.
-            This does not work for :class:`torch.spase.Tensor` input.
+        fill_cache (bool, optional): If set to :obj:`True`, will fill the
+            underlying :class:`torch_sparse.SparseTensor` cache (if used).
             (default: :obj:`True`)
-        backend (str, optional): It specifies the backend library to be used
-            for converting the :obj:`edge_index` to a sparse tensor. If set to
-            :obj:`'torch_sparse'`, the :obj:`edge_index` will be converted to
-            a :class:`~torch_sparse.SparseTensor` object. If set to
-            :obj:`'torch'`, the :obj:`edge_index` will be converted to a
-            :class:`torch.sparse.Tensor` object.
-            (default: :obj:`'torch_sparse'`)
+        layout (torch.layout, optional): Specifies the layout of the returned
+            sparse tensor (:obj:`None`, :obj:`torch.sparse_coo` or
+            :obj:`torch.sparse_csr`).
+            If set to :obj:`None` and the :obj:`torch_sparse` dependency is
+            installed, will convert :obj:`edge_index` into a
+            :class:`torch_sparse.SparseTensor` object.
+            If set to :obj:`None` and the :obj:`torch_sparse` dependency is
+            not installed, will convert :obj:`edge_index` into a
+            :class:`torch.sparse.Tensor` object with layout
+            :obj:`torch.sparse_csr`. (default: :obj:`None`)
     """
     def __init__(
         self,
         attr: Optional[str] = 'edge_weight',
         remove_edge_index: bool = True,
         fill_cache: bool = True,
-        backend: str = 'torch_sparse',
+        layout: Optional[torch.layout] = None,
     ):
+        if layout not in {None, torch.sparse_coo, torch.sparse_csr}:
+            raise ValueError(f"Unexpected sparse tensor layout "
+                             f"(got '{layout}')")
+
         self.attr = attr
         self.remove_edge_index = remove_edge_index
         self.fill_cache = fill_cache
-        assert backend in ['torch_sparse', 'torch']
-        self.backend = backend
+        self.layout = layout
 
     def __call__(
         self,
@@ -83,25 +95,44 @@ class ToSparseTensor(BaseTransform):
             for key, value in zip(keys, values):
                 store[key] = value
 
-            if self.backend == 'torch_sparse':
+            layout = self.layout
+            size = store.size()[::-1]
+            value: Optional[Tensor] = None
+            if self.attr is not None and self.attr in store:
+                value = store[self.attr]
+
+            if layout is None and torch_geometric.typing.WITH_TORCH_SPARSE:
                 store.adj_t = SparseTensor(
-                    row=store.edge_index[1], col=store.edge_index[0],
-                    value=None if self.attr is None or self.attr not in store
-                    else store[self.attr], sparse_sizes=store.size()[::-1],
-                    is_sorted=True, trust_data=True)
-            else:
+                    row=store.edge_index[1],
+                    col=store.edge_index[0],
+                    value=value,
+                    sparse_sizes=size,
+                    is_sorted=True,
+                    trust_data=True,
+                )
+
+            # TODO Multi-dimensional edge attributes only support for COO.
+            elif ((value is not None and value.dim() > 1)
+                  or layout == torch.sparse_coo):
+                store.adj_t = to_torch_coo_tensor(
+                    store.edge_index.flip([0]),
+                    edge_attr=value,
+                    size=size,
+                )
+
+            elif layout is None or layout == torch.sparse_csr:
                 store.adj_t = to_torch_csr_tensor(
                     store.edge_index.flip([0]),
-                    edge_attr=None if self.attr is None
-                    or self.attr not in store else store[self.attr],
-                    size=store.size()[::-1])
+                    edge_attr=value,
+                    size=size,
+                )
 
             if self.remove_edge_index:
                 del store['edge_index']
                 if self.attr is not None and self.attr in store:
                     del store[self.attr]
 
-            if self.fill_cache and self.backend == 'torch_sparse':
+            if self.fill_cache and isinstance(store.adj_t, SparseTensor):
                 # Pre-process some important attributes.
                 store.adj_t.storage.rowptr()
                 store.adj_t.storage.csr2csc()
@@ -109,6 +140,5 @@ class ToSparseTensor(BaseTransform):
         return data
 
     def __repr__(self):
-        return (f'{self.__class__.__name__}(attr={self.attr},'
-                f' remove_edge_index={self.remove_edge_index},'
-                f' fill_cache={self.fill_cache}, backend={self.backend})')
+        return (f'{self.__class__.__name__}(attr={self.attr}, '
+                f'layout={self.layout})')
