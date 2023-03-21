d028a16afb183031b8c77ec23904f20d4c550413
Pratham Soni <psoni2002@gmail.com>
2023-03-21 14:01:12 -0700

    improving tests and Pep8 compat
    Signed-off-by: Pratham Soni <psoni2002@gmail.com>

--- examples/contrib/graph_lottery_ticket.py
+++ examples/contrib/graph_lottery_ticket.py
@@ -1,9 +1,9 @@
 from argparse import ArgumentParser
 
 import torch
+import torch.nn.functional as F
 import tqdm
 from torch.nn import Module
-from torch.nn.functional import nll_loss
 
 import torch_geometric
 from torch_geometric.contrib.nn import GLTSearch
@@ -13,7 +13,7 @@ from torch_geometric.contrib.nn.models.graph_lottery_ticket import (
 )
 from torch_geometric.data import Data
 from torch_geometric.datasets import Planetoid
-from torch_geometric.nn import GAT, GCN, GIN
+from torch_geometric.nn import GAT, GCN
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -31,6 +31,7 @@ def generate_edge_data(dataset):
 
 class LinkPredictor(Module):
     """helper model to get dot product interaction on edges"""
+
     def __init__(self, model):
         super().__init__()
         self.model = model
@@ -71,7 +72,8 @@ def baseline(model: Module, graph: Data, task, verbose: bool = False):
                                edges=graph.edges)
 
                 edge_mask = graph.train_mask[
-                    graph.edges[0]] & graph.train_mask[graph.edges[1]]
+                                graph.edges[0]] & graph.train_mask[
+                                graph.edges[1]]
                 loss = loss_fn(output[edge_mask],
                                graph.edge_labels[edge_mask].float())
             else:
@@ -114,49 +116,56 @@ def baseline(model: Module, graph: Data, task, verbose: bool = False):
 
 
 if __name__ == "__main__":
-    """ GLT Trainer example. To reproduce GLT models from paper run the following:
-        python graph_lottery_ticket.py --dataset Cora --model gcn --prune_rate_graph .1855 --prune_rate_model .5904 \
-        --task node_classification
-        python graph_lottery_ticket.py --dataset Cora --model gin --prune_rate_graph .05 --prune_rate_model .2 \
-        --task node_classification
-        python graph_lottery_ticket.py --dataset Cora --model gat --prune_rate_graph .4596 --prune_rate_model .9313 \
-        --task node_classification
+    """GLT Trainer example. To reproduce GLT models from paper run the 
+    following: 
+    
+        python graph_lottery_ticket.py --dataset Cora --model gcn 
+        --prune_rate_graph .1855 --prune_rate_model .5904 --task 
+        node_classification 
+    
+        python graph_lottery_ticket.py --dataset Cora --model gat 
+        --prune_rate_graph .4596 --prune_rate_model .9313 --task 
+        node_classification 
 
-        python graph_lottery_ticket.py --dataset CiteSeer --model gcn --prune_rate_graph .4867 --prune_rate_model .945 \
-        --task node_classification
-        python graph_lottery_ticket.py --dataset CiteSeer --model gin --prune_rate_graph .5819 --prune_rate_model .982 \
-        --task node_classification
-        python graph_lottery_ticket.py --dataset CiteSeer --model gat --prune_rate_graph .5123 --prune_rate_model .956 \
-        --task node_classification
+        python graph_lottery_ticket.py --dataset CiteSeer --model gcn 
+        --prune_rate_graph .4867 --prune_rate_model .945 --task 
+        node_classification 
 
-        python graph_lottery_ticket.py --dataset PubMed --model gcn --prune_rate_graph .5819 --prune_rate_model .9775 \
-        --task node_classification
-        python graph_lottery_ticket.py --dataset PubMed --model gin --prune_rate_graph .4867 --prune_rate_model .9450 \
-        --task node_classification
-        python graph_lottery_ticket.py --dataset PubMed --model gat --prune_rate_graph .5819 --prune_rate_model .9775 \
-        --task node_classification
+        python graph_lottery_ticket.py --dataset CiteSeer --model gat 
+        --prune_rate_graph .5123 --prune_rate_model .956 --task 
+        node_classification 
 
-        python graph_lottery_ticket.py --dataset Cora --model gcn --prune_rate_graph .2649 --prune_rate_model .7379 \
-        --task link_prediction
-        python graph_lottery_ticket.py --dataset Cora --model gin --prune_rate_graph .2262 --prune_rate_model .6723 \
-        --task link_prediction
-        python graph_lottery_ticket.py --dataset Cora --model gat --prune_rate_graph .4312 --prune_rate_model .9141 \
-        --task link_prediction
+        python graph_lottery_ticket.py --dataset PubMed --model gcn 
+        --prune_rate_graph .5819 --prune_rate_model .9775 --task 
+        node_classification 
 
-        python graph_lottery_ticket.py --dataset CiteSeer --model gcn --prune_rate_graph .3366 --prune_rate_model .8322\
-        --task link_prediction
-        python graph_lottery_ticket.py --dataset CiteSeer --model gin --prune_rate_graph .2649 --prune_rate_model .7379\
-        --task link_prediction
-        python graph_lottery_ticket.py --dataset CiteSeer --model gat --prune_rate_graph .4312 --prune_rate_model .9141\
-        --task link_prediction
+        python graph_lottery_ticket.py --dataset PubMed --model gat 
+        --prune_rate_graph .5819 --prune_rate_model .9775 --task 
+        node_classification 
 
-        python graph_lottery_ticket.py --dataset PubMed --model gcn --prune_rate_graph .4013 --prune_rate_model .8926 \
-        --task link_prediction
-        python graph_lottery_ticket.py --dataset PubMed --model gin --prune_rate_graph .1426 --prune_rate_model .488 \
-        --task link_prediction
-        python graph_lottery_ticket.py --dataset PubMed --model gat --prune_rate_graph .5599 --prune_rate_model .9719 \
-        --task link_prediction
-    """
+        python graph_lottery_ticket.py --dataset Cora --model gcn 
+        --prune_rate_graph .2649 --prune_rate_model .7379 --task 
+        link_prediction 
+        
+        python graph_lottery_ticket.py --dataset Cora --model gat 
+        --prune_rate_graph .4312 --prune_rate_model .9141 --task 
+        link_prediction 
+
+        python graph_lottery_ticket.py --dataset CiteSeer --model gcn 
+        --prune_rate_graph .3366 --prune_rate_model .8322 --task 
+        link_prediction 
+        
+        python graph_lottery_ticket.py --dataset CiteSeer --model gat 
+        --prune_rate_graph .4312 --prune_rate_model .9141 --task 
+        link_prediction 
+
+        python graph_lottery_ticket.py --dataset PubMed --model gcn 
+        --prune_rate_graph .4013 --prune_rate_model .8926 --task 
+        link_prediction 
+        
+        python graph_lottery_ticket.py --dataset PubMed --model gat 
+        --prune_rate_graph .5599 --prune_rate_model .9719 --task 
+        link_prediction """
     parser = ArgumentParser()
     parser.add_argument('-d', "--dataset", default="Cora")
     parser.add_argument('-m', "--model", default="gcn")
@@ -184,18 +193,13 @@ if __name__ == "__main__":
                   output_channels=hidden_channels,
                   hidden_channels=hidden_channels, num_layers=2).to(device)
 
-    elif args.model.lower() == "gin":
-        gnn = GIN(in_channels=dataset.num_node_features,
-                  output_channels=hidden_channels,
-                  hidden_channels=hidden_channels, num_layers=2).to(device)
-
     elif args.model.lower() == "gat":
         gnn = GAT(in_channels=dataset.num_node_features,
                   output_channels=hidden_channels,
                   hidden_channels=hidden_channels, num_layers=2).to(device)
 
     else:
-        raise ValueError("model must be one of gcn, gin, gat")
+        raise ValueError("model must be one of gcn, gat")
 
     if args.task == "link_prediction":
         gnn = LinkPredictor(gnn)
@@ -203,17 +207,16 @@ if __name__ == "__main__":
     # baseline(gnn, data, args.task, args.verbose)
 
     if args.task == "link_prediction":
-        loss_fn = torch.nn.functional.binary_cross_entropy_with_logits
+        loss_fn = F.binary_cross_entropy_with_logits
     else:
-        loss_fn = torch.nn.functional.cross_entropy
+        loss_fn = F.cross_entropy
 
     trainer = GLTSearch(task=args.task, module=gnn, graph=data, lr=8e-3,
                         reg_graph=args.reg_graph, reg_model=args.reg_model,
                         prune_rate_graph=args.prune_rate_graph,
                         prune_rate_model=args.prune_rate_model,
                         optim_args={"weight_decay":
-                                    8e-5}, seed=1234, verbose=args.verbose,
-                        ignore_keys={"eps"
-                                     }, max_train_epochs=200, loss_fn=loss_fn)
+                                        8e-5}, seed=1234, verbose=args.verbose
+                        , max_train_epochs=200, loss_fn=loss_fn)
 
     initial_params, mask_dict = trainer.prune()
--- test/contrib/nn/models/test_graph_lottery_ticket.py
+++ test/contrib/nn/models/test_graph_lottery_ticket.py
@@ -2,12 +2,14 @@ from copy import deepcopy
 
 import pytest
 import torch
+from torch_geometric.contrib.nn import GLTMask, GLTModel
 from torch.nn import Module
 
-from torch_geometric.contrib.nn import GLTMask, GLTModel, GLTSearch
 from torch_geometric.data import Data
-from torch_geometric.nn import GAT, GCN, GIN
+from torch_geometric.nn import GAT, GCN
 from torch_geometric.utils import negative_sampling
+
+device = torch.device('cpu')
 
 edge_index = torch.tensor([[0, 1], [1, 0], [1, 2], [2, 1], [3, 4], [4, 3],
                            [1, 3], [3, 1], [0, 5], [5, 0]], dtype=torch.long)
@@ -31,11 +33,12 @@ def generate_edge_data(dataset):
 test_data_node_classification = Data(x=x,
                                      edge_index=edge_index.t().contiguous())
 test_data_link_prediction = deepcopy(test_data_node_classification)
-generate_edge_data(test_data_node_classification)
+generate_edge_data(test_data_link_prediction)
 
 
 class LinkPredictor(Module):
     """helper model to get dot product interaction on edges"""
+
     def __init__(self, model):
         super().__init__()
         self.model = model
@@ -51,25 +54,120 @@ def build_test_model(architecture, link_prediction=False):
     input_dim = 1
     hidden_dim = 5
     if link_prediction:
-        model = architecture(input_dim, hidden_dim, hidden_dim)
+        model = architecture(in_channels=input_dim, hidden_channels=hidden_dim,
+                             out_channels=hidden_dim, num_layers=2)
         model = LinkPredictor(model)
     else:
-        model = architecture(input_dim, hidden_dim, 1)
+        model = architecture(in_channels=input_dim, hidden_channels=hidden_dim,
+                             out_channels=1, num_layers=2)
     return model
 
 
-@pytest.mark.parametrize('architecture', [GCN, GIN, GAT])
+@pytest.mark.parametrize('architecture', [GCN, GAT])
 @pytest.mark.parametrize('link_prediction', [False, True])
-def test_GLTModel_forward(architecture, link_prediction):
+def test_make_mask(architecture, link_prediction):
     if link_prediction:
         graph = test_data_link_prediction
-        input_shape = graph.x.shape
     else:
         graph = test_data_node_classification
-        input_shape = graph.edges.shape
+
+    adjacency_shape = graph.edge_index.shape[1]
 
     model = build_test_model(architecture, link_prediction)
-    model = GLTModel(model, graph)
-    output = model()
+    mask = GLTMask(model, graph, device).to_dict()
 
-    assert output.shape[0] == input_shape[0]
+    for name, param in model.named_parameters():
+        mask = mask[name + GLTModel.MASK]
+        assert param.shape == mask.shape
+
+    assert mask[GLTModel.EDGE_MASK + GLTModel.MASK].shape[0] == \
+           adjacency_shape
+
+
+@pytest.mark.parametrize('architecture', [GCN, GAT])
+@pytest.mark.parametrize('link_prediction', [False, True])
+def test_apply_mask(architecture, link_prediction):
+    if link_prediction:
+        graph = test_data_link_prediction
+    else:
+        graph = test_data_node_classification
+
+    model = build_test_model(architecture, link_prediction)
+    pruned_model = GLTModel(model, graph)
+    mask = GLTMask(model, graph, device).to_dict()
+
+    original_params = deepcopy(dict(model.named_parameters()))
+    pruned_model.apply_mask(mask)
+    new_params = dict(model.named_parameters())
+
+    for name, param in original_params.items():
+        assert param.shape == new_params[name + GLTModel.MASK].shape
+        assert param.shape == new_params[name + GLTModel.ORIG].shape
+
+
+@pytest.mark.parametrize('architecture', [GCN, GAT])
+@pytest.mark.parametrize('link_prediction', [False, True])
+def test_apply_mask_ignore_keys(architecture, link_prediction):
+    if link_prediction:
+        graph = test_data_link_prediction
+    else:
+        graph = test_data_node_classification
+
+    model = build_test_model(architecture, link_prediction)
+    ignore_keys = {'bias'}
+    pruned_model = GLTModel(model, graph, ignore_keys=ignore_keys)
+    mask = GLTMask(model, graph, device).to_dict()
+
+    original_params = deepcopy(dict(model.named_parameters()))
+    pruned_model.apply_mask(mask)
+    new_params = dict(model.named_parameters())
+
+    for name, param in original_params.items():
+        key = name.rpartition('.')[-1]
+        if key in ignore_keys:
+            assert param not in new_params
+
+
+@pytest.mark.parametrize('architecture', [GCN, GAT])
+@pytest.mark.parametrize('link_prediction', [False, True])
+def test_forward(architecture, link_prediction):
+    if link_prediction:
+        graph = test_data_link_prediction
+        input_len = graph.edges.shape[0]
+    else:
+        graph = test_data_node_classification
+        input_len = graph.x.shape[0]
+
+    model = build_test_model(architecture, link_prediction)
+    pruned_model = GLTModel(model, graph)
+    mask = GLTMask(model, graph, device).to_dict()
+
+    pruned_model.apply_mask(mask)
+    output = pruned_model()
+    assert output.shape[0] == input_len
+
+
+@pytest.mark.parametrize('architecture', [GCN, GAT])
+@pytest.mark.parametrize('link_prediction', [False, True])
+def test_rewind(architecture, link_prediction):
+    if link_prediction:
+        graph = test_data_link_prediction
+    else:
+        graph = test_data_node_classification
+
+    model = build_test_model(architecture, link_prediction)
+    initial_params = {
+        "module." + k + GLTModel.ORIG if k.rpartition(".")[
+            -1] else "module." + k: v.detach().clone()
+        for k, v in model.state_dict().items()
+    }
+    pruned_model = GLTModel(model, graph)
+    mask = GLTMask(model, graph, device)
+
+    pruned_model.apply_mask(mask.to_dict())
+    pruned_model.rewind(mask.to_dict(weight_prefix=True) | initial_params)
+
+    for param in (mask.to_dict(weight_prefix=True) |
+                  initial_params).values():
+        assert not param.requires_grad
+        assert param.grad is None
