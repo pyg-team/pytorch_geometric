adfc2d1c2fc188fa38bc6cbe2c80d4205d7338ad
puririshi98 <puririshi98@berkeley.edu>
2023-03-21 15:41:56 -0700

    rebased to_hetero

--- torch_geometric/nn/to_hetero_module.py
+++ torch_geometric/nn/to_hetero_module.py
@@ -6,8 +6,52 @@ import torch
 from torch import Tensor
 
 import torch_geometric
+from torch_geometric.nn.dense import HeteroLinear, Linear
 from torch_geometric.typing import EdgeType, NodeType, OptTensor
 from torch_geometric.utils import scatter
+
+
+class SubParam:
+    def __init__(self, param: torch.nn.Parameter, idx: int):
+        self.idx = idx
+        self.param = param
+
+    def get_data(self):
+        return self.param[self.idx].data
+
+    def set_data(self, data):
+        if self.param.data[self.idx].shape == data.t().shape:
+            self.param.data[self.idx] = data.t()
+        else:
+            self.param.data[self.idx] = data
+
+    data = property(get_data, set_data)
+
+
+class DummyLinear:
+    def __init__(self, get_type: Union[NodeType, EdgeType],
+                 types: Union[List[NodeType],
+                              List[EdgeType]], hetero_module: torch.nn.Module):
+        self.type = get_type
+        self.types = types
+        self.idx = types.index(get_type)
+        self.in_channels = hetero_module.in_channels
+        self.out_channels = hetero_module.out_channels
+        self.hetero_weight = hetero_module.weight
+        self.use_bias = hetero_module.kwargs.get('bias', True)
+
+        self.hetero_bias = hetero_module.bias
+
+    @property
+    def weight(self):
+        return SubParam(self.hetero_weight, self.idx)
+
+    @property
+    def bias(self):
+        if self.use_bias:
+            return SubParam(self.hetero_bias, self.idx)
+        else:
+            return None
 
 
 class ToHeteroLinear(torch.nn.Module):
@@ -16,33 +60,43 @@ class ToHeteroLinear(torch.nn.Module):
         module: torch.nn.Module,
         types: Union[List[NodeType], List[EdgeType]],
     ):
-        from torch_geometric.nn import HeteroLinear, Linear
-
         super().__init__()
 
         self.types = types
 
         if isinstance(module, Linear):
-            in_channels = module.in_channels
-            out_channels = module.out_channels
-            bias = module.bias is not None
+            self.in_channels = module.in_channels
+            self.out_channels = module.out_channels
 
         elif isinstance(module, torch.nn.Linear):
             in_channels = module.in_features
             out_channels = module.out_features
-            bias = module.bias is not None
 
         else:
             raise ValueError(f"Expected 'Linear' module (got '{type(module)}'")
 
-        # TODO We currently assume that `x` is sorted according to `type`.
+        self.bias = module.bias is not None
+        if self.in_channels == -1:
+            self.dim_dict = None
         self.hetero_module = HeteroLinear(
-            in_channels,
-            out_channels,
+            self.in_channels,
+            self.out_channels,
             num_types=len(types),
             is_sorted=True,
-            bias=bias,
+            bias=self.bias,
         )
+
+    def __getitem__(
+            self, get_type: Union[NodeType,
+                                  EdgeType]) -> Union[Linear, DummyLinear]:
+        # returns a Linear layer for type
+        # neccesary to avoid changing usage in the following examples:
+        # 1) model.lin[node_type].weight.data = conv.root.data.t()
+        # 2) model.lin[node_type].bias.data = conv.bias.data
+        if not torch_geometric.typing.WITH_PYG_LIB:
+            return self.hetero_module.lins[self.types.index(get_type)]
+        else:
+            return DummyLinear(get_type, self.types, self.hetero_module)
 
     def fused_forward(self, x: Tensor, type_vec: Tensor) -> Tensor:
         return self.hetero_module(x, type_vec)
@@ -58,13 +112,35 @@ class ToHeteroLinear(torch.nn.Module):
                 for i, key in enumerate(self.types)
             }
 
-        x = torch.cat([x_dict[key] for key in self.types], dim=0)
+        if self.in_channels == -1:
+            padded_list = self.pad_xs(x_dict)
+            x = torch.cat(padded_list, dim=0)
+        else:
+            x = torch.cat([x_dict[key] for key in self.types], dim=0)
         sizes = [x_dict[key].size(0) for key in self.types]
         type_vec = torch.arange(len(self.types), device=x.device)
         size = torch.tensor(sizes, device=x.device)
         type_vec = type_vec.repeat_interleave(size)
         outs = self.hetero_module(x, type_vec).split(sizes)
         return {key: out for key, out in zip(self.types, outs)}
+
+    def pad_xs(
+            self, x_dict: Dict[Union[NodeType, EdgeType],
+                               Tensor]) -> List[Tensor]:
+        if self.dim_dict is None:
+            self.dim_dict = {
+                key: x_dict[key].size(-1)
+                for key in x_dict.keys()
+            }
+            self.dims_tensor = torch.tensor(list(self.dim_dict.values()))
+            self.max_size = self.dims_tensor.max()
+        if not (self.dims_tensor == self.max_size).all():
+            for key, x in x_dict.items():
+                if x.shape[1] < self.max_size:
+                    x_dict[key] = torch.concat(
+                        (x, torch.zeros(x.shape[0],
+                                        self.max_size - x.shape[1])), dim=1)
+        return list(x_dict.values())
 
     def forward(
         self,
