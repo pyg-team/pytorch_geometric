2533caee5b6a11b2250f6ca730f31659778243c2
puririshi98 <puririshi98@berkeley.edu>
2023-03-22 12:11:32 -0700

    added heterodictlinear

--- torch_geometric/nn/dense/linear.py
+++ torch_geometric/nn/dense/linear.py
@@ -8,6 +8,7 @@ from torch import Tensor, nn
 from torch.nn.parameter import Parameter
 
 import torch_geometric.typing
+from torch_geometric.typing import EdgeType, NodeType
 from torch_geometric.nn import inits
 from torch_geometric.typing import pyg_lib
 from torch_geometric.utils import index_sort
