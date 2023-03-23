e0b284f65bcafd455b7c6e19ac0c25c80987e618
IshanSabane <sabaneishan@gmail.com>
2023-03-22 21:55:26 -0700

    resolved

--- torch_geometric/contrib/degree_quant/__init__.py
+++ torch_geometric/contrib/degree_quant/__init__.py
@@ -1,5 +1,5 @@
-from gcn_conv import GCNConvMultiQuant
-from gat_conv import GATConvMultiQuant
-from gin_conv import GINConvMultiQuant
+from .gcn_conv import GCNConvMultiQuant
+from .gat_conv import GATConvMultiQuant
+from .gin_conv import GINConvMultiQuant
 __all__ = classes = ['GCNConvMultiQuant', 'GATConvMultiQuant',
                      'GINConvMultiQuant']
