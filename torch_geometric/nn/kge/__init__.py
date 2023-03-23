09a6362dc04b57fc7e4f0f577cd2f35d860b3356
Riya Sinha <riya.v.sinha@gmail.com>
2023-03-23 16:50:13 -0500

    feat: add RotatE KGE model

--- torch_geometric/nn/kge/__init__.py
+++ torch_geometric/nn/kge/__init__.py
@@ -2,10 +2,12 @@ from .base import KGEModel
 from .transe import TransE
 from .complex import ComplEx
 from .distmult import DistMult
+from .rotate import RotatE
 
 __all__ = classes = [
     'KGEModel',
     'TransE',
     'ComplEx',
     'DistMult',
+    'RotatE',
 ]
