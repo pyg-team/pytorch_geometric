2d92ccc1c32b8870927f822db2f4cd4f76651145
Thejas <thejas@DN0a1ea576.SUNet>
2023-03-23 04:32:43 -0700

    Fix style

--- examples/contrib/graphwavenet/main.py
+++ examples/contrib/graphwavenet/main.py
@@ -128,7 +128,7 @@ def main():
 
         log = 'Epoch: {:03d}, Train Loss: {:.4f}, Train MAPE: {:.4f}, ' + \
             'Train RMSE: {:.4f}, Valid Loss: {:.4f}, Valid MAPE: {:.4f}, ' + \
-                'Valid RMSE: {:.4f}, Training Time: {:.4f}/epoch'
+            'Valid RMSE: {:.4f}, Training Time: {:.4f}/epoch'
         print(
             log.format(i, mtrain_loss, mtrain_mape, mtrain_rmse, mvalid_loss,
                        mvalid_mape, mvalid_rmse, (t2 - t1)), flush=True)
