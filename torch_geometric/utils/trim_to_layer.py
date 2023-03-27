4e901d3c7bab65a5b198d8e473f76f7e4f696ffe
andreazanetti <andrea.zanetti@intel.com>
2023-03-27 10:58:09 +0200

    It adds first step to support SparseTensor in trim_to_layer (only homo considered). sage works on ogbn-products; gcn not yet

--- torch_geometric/utils/trim_to_layer.py
+++ torch_geometric/utils/trim_to_layer.py
@@ -2,6 +2,7 @@ from typing import Dict, List, Optional, Tuple, Union
 
 import torch
 from torch import Tensor
+from torch_sparse import SparseStorage, SparseTensor
 
 from torch_geometric.typing import (
     EdgeType,
@@ -72,25 +73,63 @@ def trim_to_layer(
                 for k, v in edge_attr.items()
             }
         return x, edge_index, edge_attr
-
+    
+    # this point should be reached by homogeneous case only, hetero is handled above
+    # for homogeneous case, I add here support for the SparseTensor (from torch_sparse) case as well  
+    # x and edge_attr should be treated in same way, regardles we use SparseTensor or Tensor to represent adj matrix
     x = x.narrow(
         dim=0,
         start=0,
         length=x.size(0) - num_sampled_nodes_per_hop[-layer],
     )
-    edge_index = edge_index.narrow(
-        dim=1,
-        start=0,
-        length=edge_index.size(1) - num_sampled_edges_per_hop[-layer],
-    )
     if edge_attr is not None:
         edge_attr = edge_attr.narrow(
-            dim=0,
+                dim=0,
+                start=0,
+                length=edge_attr.size(0) - num_sampled_edges_per_hop[-layer],
+            )
+    # adj matrix as Tensor    
+    if isinstance(edge_index, Tensor):
+        edge_index = edge_index.narrow(
+            dim=1,
             start=0,
-            length=edge_attr.size(0) - num_sampled_edges_per_hop[-layer],
+            length=edge_index.size(1) - num_sampled_edges_per_hop[-layer],
         )
-    return x, edge_index, edge_attr
+        return x, edge_index, edge_attr
 
+    # adj matrix as SparseTensor
+    if isinstance(edge_index, SparseTensor):
+        new_num_rows = edge_index.storage._sparse_sizes[0] - \
+                num_sampled_nodes_per_hop[-layer]
+        sparse_sizes = (new_num_rows, new_num_rows)  # n_rows == n_cols
+
+        # rowptr
+        rowptr = torch.narrow(edge_index.storage._rowptr, 0, 0,
+                                new_num_rows + 1).clone()
+        active_nodes_num = new_num_rows - num_sampled_nodes_per_hop[-(layer + 1)]
+        rowptr[(active_nodes_num + 1):] = rowptr[active_nodes_num]
+
+        # col and value
+        col = torch.narrow(edge_index.storage._col, 0, 0, rowptr[-1])
+        value = torch.narrow(edge_index.storage._value, 0, 0, rowptr[-1]
+                                ) if edge_index.storage._value is not None else None
+
+        # indeces for conversion to csc
+        csr2csc = edge_index.storage._csr2csc[edge_index.storage._csr2csc < len(col)] \
+            if edge_index.storage._csr2csc is not None else None
+
+        # update storage and edge_index
+        storage = SparseStorage(row=None, rowptr=rowptr, col=col,
+                                value=value, sparse_sizes=sparse_sizes,
+                                rowcount=None, colptr=None,
+                                colcount=None, csr2csc=csr2csc,
+                                csc2csr=None, is_sorted=True,
+                                trust_data=False)
+        edge_index = SparseTensor.from_storage(storage)
+        return x, edge_index, edge_attr
+
+    raise NotImplemented
+    # end of trim_to_layer
 
 class TrimToLayer(torch.nn.Module):
     def forward(
