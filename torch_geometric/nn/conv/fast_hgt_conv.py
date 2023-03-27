ffc951fbe53c6520e53f1c338d4b1a840086fa53
puririshi98 <puririshi98@berkeley.edu>
2023-03-27 16:16:04 -0700

    address comments

--- torch_geometric/nn/conv/fast_hgt_conv.py
+++ torch_geometric/nn/conv/fast_hgt_conv.py
@@ -174,7 +174,7 @@ class FastHGTConv(MessagePassing):
                 out_dict[node_type] = None
 
         # Transform output node embeddings:
-        a_dict = self.out_lin({k: F.gelu(v) for k, v in out_dict.items()})
+        a_dict = self.out_lin({k: F.gelu(v) if v is not None else v for k, v in out_dict.items()})
 
         # Iterate over node types:
         for node_type, out in out_dict.items():
