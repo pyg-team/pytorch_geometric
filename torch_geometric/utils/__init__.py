4bce9a9fc8ccdc4af87e9b46dedefbe380e314d9
hbenedek <hben.0204@gmail.com>
2023-03-29 10:22:43 +0200

    fix unused import

--- torch_geometric/utils/__init__.py
+++ torch_geometric/utils/__init__.py
@@ -1,55 +1,79 @@
 import copy
 
-from .scatter import scatter
-from .segment import segment
-from .sort import index_sort
-from .degree import degree
-from .softmax import softmax
-from .dropout import dropout_adj, dropout_node, dropout_edge, dropout_path
-from .augmentation import shuffle_node, mask_feature, add_random_edge
-from .sort_edge_index import sort_edge_index
-from .coalesce import coalesce
-from .undirected import is_undirected, to_undirected
-from .loop import (contains_self_loops, remove_self_loops,
-                   segregate_self_loops, add_self_loops,
-                   add_remaining_self_loops, get_self_loop_attr)
-from .isolated import contains_isolated_nodes, remove_isolated_nodes
-from .subgraph import (get_num_hops, subgraph, k_hop_subgraph,
-                       bipartite_subgraph)
-from .homophily import homophily
 from .assortativity import assortativity
+from .augmentation import add_random_edge, mask_feature, shuffle_node
+from .coalesce import coalesce
+from .convert import (
+    from_cugraph,
+    from_dgl,
+    from_networkit,
+    from_networkx,
+    from_scipy_sparse_matrix,
+    from_trimesh,
+    to_cugraph,
+    to_dgl,
+    to_networkit,
+    to_networkx,
+    to_scipy_sparse_matrix,
+    to_trimesh,
+)
+from .degree import degree
+from .dropout import dropout_adj, dropout_edge, dropout_node, dropout_path
+from .embedding import get_embeddings
+from .geodesic import geodesic_distance
 from .get_laplacian import get_laplacian
 from .get_mesh_laplacian import get_mesh_laplacian
-from .mask import mask_select, index_to_mask, mask_to_index
-from .select import select, narrow
-from .to_dense_batch import to_dense_batch
-from .to_dense_adj import to_dense_adj
-from .nested import to_nested_tensor, from_nested_tensor
-from .sparse import (dense_to_sparse, is_sparse, is_torch_sparse_tensor,
-                     to_torch_coo_tensor, to_torch_csr_tensor,
-                     to_torch_csc_tensor, to_edge_index)
-from .spmm import spmm
-from .unbatch import unbatch, unbatch_edge_index
-from .one_hot import one_hot
-from .normalized_cut import normalized_cut
 from .grid import grid
-from .geodesic import geodesic_distance
-from .convert import to_scipy_sparse_matrix, from_scipy_sparse_matrix
-from .convert import to_networkx, from_networkx
-from .convert import to_networkit, from_networkit
-from .convert import to_trimesh, from_trimesh
-from .convert import to_cugraph, from_cugraph
-from .convert import to_dgl, from_dgl
+from .homophily import homophily
+from .isolated import contains_isolated_nodes, remove_isolated_nodes
+from .loop import (
+    add_remaining_self_loops,
+    add_self_loops,
+    contains_self_loops,
+    get_self_loop_attr,
+    remove_self_loops,
+    segregate_self_loops,
+)
+from .mask import index_to_mask, mask_select, mask_to_index
+from .negative_sampling import (
+    batched_negative_sampling,
+    negative_sampling,
+    structured_negative_sampling,
+    structured_negative_sampling_feasible,
+)
+from .nested import from_nested_tensor, to_nested_tensor
+from .normalized_cut import normalized_cut
+from .one_hot import one_hot
+from .random import (
+    barabasi_albert_graph,
+    erdos_renyi_graph,
+    stochastic_blockmodel_graph,
+)
+from .scatter import scatter
+from .segment import segment
+from .select import narrow, select
 from .smiles import from_smiles, to_smiles
-from .random import (erdos_renyi_graph, stochastic_blockmodel_graph,
-                     barabasi_albert_graph)
-from .negative_sampling import (negative_sampling, batched_negative_sampling,
-                                structured_negative_sampling,
-                                structured_negative_sampling_feasible)
-from .tree_decomposition import tree_decomposition
-from .embedding import get_embeddings
-from .trim_to_layer import trim_to_layer
+from .softmax import softmax
+from .sort import index_sort
+from .sort_edge_index import sort_edge_index
+from .sparse import (
+    dense_to_sparse,
+    is_sparse,
+    is_torch_sparse_tensor,
+    to_edge_index,
+    to_torch_coo_tensor,
+    to_torch_csc_tensor,
+    to_torch_csr_tensor,
+)
+from .spmm import spmm
+from .subgraph import bipartite_subgraph, get_num_hops, k_hop_subgraph, subgraph
+from .to_dense_adj import to_dense_adj
+from .to_dense_batch import to_dense_batch
 from .train_test_split_edges import train_test_split_edges
+from .tree_decomposition import tree_decomposition
+from .trim_to_layer import trim_to_layer
+from .unbatch import unbatch, unbatch_edge_index
+from .undirected import is_undirected, to_undirected
 
 __all__ = [
     'scatter',
@@ -130,6 +154,8 @@ __all__ = [
     'get_embeddings',
     'trim_to_layer',
     'train_test_split_edges',
+    'from_dgl',
+    'to_dgl',
 ]
 
 # `structured_negative_sampling_feasible` is a long name and thus destroys the
