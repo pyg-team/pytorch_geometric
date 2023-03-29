4f4866890e6ec4921cd31e48777167b44fa3f823
rusty1s <matthias.fey@tu-dortmund.de>
2023-03-29 05:18:51 +0000

    update

--- test/nn/conv/test_tag_conv.py
+++ test/nn/conv/test_tag_conv.py
@@ -1,30 +1,35 @@
 import torch
 
+import torch_geometric.typing
 from torch_geometric.nn import TAGConv
 from torch_geometric.testing import is_full_test
 from torch_geometric.typing import SparseTensor
+from torch_geometric.utils import to_torch_csc_tensor
 
 
 def test_tag_conv():
     x = torch.randn(4, 16)
     edge_index = torch.tensor([[0, 0, 0, 1, 2, 3], [1, 2, 3, 0, 0, 0]])
-    row, col = edge_index
-    value = torch.rand(row.size(0))
-    adj2 = SparseTensor(row=row, col=col, value=value, sparse_sizes=(4, 4))
-    adj1 = adj2.set_value(None)
-    adj3 = adj1.to_torch_sparse_csc_tensor()
-    adj4 = adj2.to_torch_sparse_csc_tensor()
+    value = torch.rand(edge_index.size(1))
+    adj1 = to_torch_csc_tensor(edge_index, size=(4, 4))
+    adj2 = to_torch_csc_tensor(edge_index, value, size=(4, 4))
 
     conv = TAGConv(16, 32)
     assert str(conv) == 'TAGConv(16, 32, K=3)'
+
     out1 = conv(x, edge_index)
     assert out1.size() == (4, 32)
     assert torch.allclose(conv(x, adj1.t()), out1, atol=1e-6)
-    assert torch.allclose(conv(x, adj3.t()), out1, atol=1e-6)
+
     out2 = conv(x, edge_index, value)
     assert out2.size() == (4, 32)
     assert torch.allclose(conv(x, adj2.t()), out2, atol=1e-6)
-    assert torch.allclose(conv(x, adj4.t()), out2, atol=1e-6)
+
+    if torch_geometric.typing.WITH_TORCH_SPARSE:
+        adj3 = SparseTensor.from_edge_index(edge_index, sparse_sizes=(4, 4))
+        adj4 = SparseTensor.from_edge_index(edge_index, value, (4, 4))
+        assert torch.allclose(conv(x, adj3.t()), out1, atol=1e-6)
+        assert torch.allclose(conv(x, adj4.t()), out2, atol=1e-6)
 
     if is_full_test():
         t = '(Tensor, Tensor, OptTensor) -> Tensor'
@@ -32,10 +37,11 @@ def test_tag_conv():
         assert jit(x, edge_index).tolist() == out1.tolist()
         assert jit(x, edge_index, value).tolist() == out2.tolist()
 
+    if is_full_test() and torch_geometric.typing.WITH_TORCH_SPARSE:
         t = '(Tensor, SparseTensor, OptTensor) -> Tensor'
         jit = torch.jit.script(conv.jittable(t))
-        assert torch.allclose(jit(x, adj1.t()), out1, atol=1e-6)
-        assert torch.allclose(jit(x, adj2.t()), out2, atol=1e-6)
+        assert torch.allclose(jit(x, adj3.t()), out1, atol=1e-6)
+        assert torch.allclose(jit(x, adj4.t()), out2, atol=1e-6)
 
 
 def test_static_tag_conv():
