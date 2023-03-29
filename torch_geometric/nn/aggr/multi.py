2bae84c9db81fc2a7e4d0db86271bdcad1575c60
Rishi Puri <puririshi98@berkeley.edu>
2023-03-29 13:16:02 -0700

    Update multi.py

--- torch_geometric/nn/aggr/multi.py
+++ torch_geometric/nn/aggr/multi.py
@@ -114,10 +114,7 @@ class MultiAggregation(Aggregation):
                 )
 
             elif mode == 'attn':
-                self.lin_heads = torch.nn.ModuleList([
-                    Linear(channels, self.out_channels)
-                    for channels in self.in_channels
-                ])
+                self.lin_heads =  HeteroDictLinear({i:channels for i, channels in enumerate(self.in_channels)}, self.out_channels)
                 num_heads = mode_kwargs.pop('num_heads', 1)
                 self.multihead_attn = MultiheadAttention(
                     self.out_channels,
@@ -181,10 +178,7 @@ class MultiAggregation(Aggregation):
             return self.lin(torch.cat(inputs, dim=-1))
 
         if hasattr(self, 'multihead_attn'):
-            x = torch.stack(
-                [head(inputs[i]) for i, head in enumerate(self.lin_heads)],
-                dim=0,
-            )
+            x = torch.stack(self.lin_heads({i:inputs[i] for i in range(len(inputs))}, dim=0)
             attn_out, _ = self.multihead_attn(x, x, x)
             return torch.mean(attn_out, dim=0)
 
