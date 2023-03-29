06e91a45a6cc1e018e83d3e3998fb63464cb9f12
Rishi Puri <puririshi98@berkeley.edu>
2023-03-29 15:41:03 -0700

    Update multi.py

--- torch_geometric/nn/aggr/multi.py
+++ torch_geometric/nn/aggr/multi.py
@@ -3,7 +3,7 @@ from typing import Any, Dict, List, Optional, Union
 
 import torch
 from torch import Tensor
-from torch.nn import Linear, MultiheadAttention
+from torch.nn import Linear, HeteroDictLinear, MultiheadAttention
 
 from torch_geometric.nn.aggr import Aggregation
 from torch_geometric.nn.aggr.fused import FusedAggregation
