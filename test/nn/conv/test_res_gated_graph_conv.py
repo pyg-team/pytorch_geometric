89178d173070f5fab0f5d3b0b4de1c37775e9cb4
rusty1s <matthias.fey@tu-dortmund.de>
2023-03-29 03:43:08 +0000

    update

--- test/nn/conv/test_res_gated_graph_conv.py
+++ test/nn/conv/test_res_gated_graph_conv.py
@@ -1,48 +1,59 @@
 import torch
 
+import torch_geometric.typing
 from torch_geometric.nn import ResGatedGraphConv
 from torch_geometric.testing import is_full_test
 from torch_geometric.typing import SparseTensor
+from torch_geometric.utils import to_torch_csc_tensor
 
 
 def test_res_gated_graph_conv():
     x1 = torch.randn(4, 8)
     x2 = torch.randn(2, 32)
     edge_index = torch.tensor([[0, 1, 2, 3], [0, 0, 1, 1]])
-    row, col = edge_index
-    adj1 = SparseTensor(row=row, col=col, sparse_sizes=(4, 4))
-    adj2 = adj1.to_torch_sparse_csc_tensor()
+    adj1 = to_torch_csc_tensor(edge_index, size=(4, 4))
 
     conv = ResGatedGraphConv(8, 32)
     assert str(conv) == 'ResGatedGraphConv(8, 32)'
+
     out = conv(x1, edge_index)
     assert out.size() == (4, 32)
     assert torch.allclose(conv(x1, adj1.t()), out, atol=1e-6)
-    assert torch.allclose(conv(x1, adj2.t()), out, atol=1e-6)
+
+    if torch_geometric.typing.WITH_TORCH_SPARSE:
+        adj2 = SparseTensor.from_edge_index(edge_index, sparse_sizes=(4, 4))
+        assert torch.allclose(conv(x1, adj2.t()), out, atol=1e-6)
 
     if is_full_test():
         t = '(Tensor, Tensor) -> Tensor'
         jit = torch.jit.script(conv.jittable(t))
         assert torch.allclose(jit(x1, edge_index), out, atol=1e-6)
 
+    if is_full_test() and torch_geometric.typing.WITH_TORCH_SPARSE:
         t = '(Tensor, SparseTensor) -> Tensor'
         jit = torch.jit.script(conv.jittable(t))
-        assert torch.allclose(jit(x1, adj1.t()), out, atol=1e-6)
+        assert torch.allclose(jit(x1, adj2.t()), out, atol=1e-6)
 
-    adj1 = adj1.sparse_resize((4, 2))
-    adj2 = adj1.to_torch_sparse_csc_tensor()
+    # Test bipartite message passing:
+    adj1 = to_torch_csc_tensor(edge_index, size=(4, 2))
+
     conv = ResGatedGraphConv((8, 32), 32)
     assert str(conv) == 'ResGatedGraphConv((8, 32), 32)'
+
     out = conv((x1, x2), edge_index)
     assert out.size() == (2, 32)
     assert torch.allclose(conv((x1, x2), adj1.t()), out, atol=1e-6)
-    assert torch.allclose(conv((x1, x2), adj2.t()), out, atol=1e-6)
+
+    if torch_geometric.typing.WITH_TORCH_SPARSE:
+        adj2 = SparseTensor.from_edge_index(edge_index, sparse_sizes=(4, 2))
+        assert torch.allclose(conv((x1, x2), adj2.t()), out, atol=1e-6)
 
     if is_full_test():
         t = '(PairTensor, Tensor) -> Tensor'
         jit = torch.jit.script(conv.jittable(t))
         assert torch.allclose(jit((x1, x2), edge_index), out, atol=1e-6)
 
+    if is_full_test() and torch_geometric.typing.WITH_TORCH_SPARSE:
         t = '(PairTensor, SparseTensor) -> Tensor'
         jit = torch.jit.script(conv.jittable(t))
-        assert torch.allclose(jit((x1, x2), adj1.t()), out, atol=1e-6)
+        assert torch.allclose(jit((x1, x2), adj2.t()), out, atol=1e-6)
--- test/nn/conv/test_rgat_conv.py
+++ test/nn/conv/test_rgat_conv.py
@@ -1,9 +1,11 @@
 import pytest
 import torch
 
+import torch_geometric.typing
 from torch_geometric.nn import RGATConv
 from torch_geometric.testing import is_full_test
 from torch_geometric.typing import SparseTensor
+from torch_geometric.utils import to_torch_coo_tensor
 
 
 @pytest.mark.parametrize('mod', [
@@ -38,11 +40,9 @@ def test_rgat_conv(mod, attention_mechanism, attention_mode):
 def test_rgat_conv_jittable():
     x = torch.randn(4, 8)
     edge_index = torch.tensor([[0, 1, 2, 3], [0, 0, 1, 1]])
-    row, col = edge_index
-    edge_attr = torch.randn((4, 8))
-    adj1 = SparseTensor(row=row, col=col, value=edge_attr, sparse_sizes=(4, 4))
-    adj2 = adj1.to_torch_sparse_coo_tensor()
+    edge_attr = torch.randn((edge_index.size(1), 8))
     edge_type = torch.tensor([0, 2, 1, 2])
+    adj1 = to_torch_coo_tensor(edge_index, edge_attr, size=(4, 4))
 
     conv = RGATConv(8, 20, num_relations=4, num_bases=4, mod='additive',
                     attention_mechanism='across-relation',
@@ -51,13 +51,15 @@ def test_rgat_conv_jittable():
 
     out = conv(x, edge_index, edge_type, edge_attr)
     assert out.size() == (4, 40)
-    assert torch.allclose(conv(x, adj1.t(), edge_type), out)
     # t() expects a tensor with <= 2 sparse and 0 dense dimensions
-    adj2_t = adj2.transpose(0, 1).coalesce()
-    assert torch.allclose(conv(x, adj2_t, edge_type), out)
+    adj1_t = adj1.transpose(0, 1).coalesce()
+    assert torch.allclose(conv(x, adj1_t, edge_type), out)
+
+    if torch_geometric.typing.WITH_TORCH_SPARSE:
+        adj2 = SparseTensor.from_edge_index(edge_index, edge_attr, (4, 4))
+        assert torch.allclose(conv(x, adj2.t(), edge_type), out)
 
     if is_full_test():
         t = '(Tensor, Tensor, OptTensor, OptTensor, Size, NoneType) -> Tensor'
         jit = torch.jit.script(conv.jittable(t))
-        assert torch.allclose(jit(x, edge_index, edge_type),
-                              conv(x, edge_index, edge_type))
+        assert torch.allclose(jit(x, edge_index, edge_type), out)
