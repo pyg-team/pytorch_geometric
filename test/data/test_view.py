1533f397a44f8e9185a6e1d2ac8e1f7d6769c551
ThePuzzlr <90777621+ThePuzzlr@users.noreply.github.com>
2023-03-31 19:51:34 -0500

    [Code Coverage] data/view.py

---
+++ test/data/test_view.py
@@ -0,0 +1,31 @@
+import copy
+import torch
+from collections.abc import Mapping
+from typing import Iterable, List
+from torch_geometric.data.view import MappingView, KeysView, ValuesView, ItemsView
+
+class M:
+    def keys(self):
+        return [1,2,3]
+    def __getitem__(self, x):
+        return x*2
+
+def test_mapping_view():
+    assert M().keys() == [1, 2, 3]
+    d = dict(M())
+    assert d == {1: 2, 2: 4, 3: 6}
+    mview = MappingView(mapping=M())
+    assert mview._keys() == [1, 2, 3]
+    assert mview.__len__() == 3
+def test_keys_view():
+    kview = KeysView(mapping=M())
+    for i, val in enumerate(kview.__iter__()):
+        assert i+1 == val
+def test_values_view():
+    vview = ValuesView(mapping=M())
+    for i, val in enumerate(vview.__iter__()):
+        assert 2 * (i+1) == val
+def test_items_view():
+    iview = ItemsView(mapping=M())
+    for i, val in enumerate(iview.__iter__()):
+        assert val == (i+1, 2*i + 2)
