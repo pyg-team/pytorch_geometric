c09121caac6e52f16a552fd83e04814cc601ebea
Rishi Puri <puririshi98@berkeley.edu>
2023-04-05 16:20:44 -0700

    Update convert.py

--- torch_geometric/utils/convert.py
+++ torch_geometric/utils/convert.py
@@ -202,7 +202,6 @@ def from_networkx(
 
     from torch_geometric.data import Data
 
-    G = nx.convert_node_labels_to_integers(G)
     G = G.to_directed() if not nx.is_directed(G) else G
 
     if isinstance(G, (nx.MultiGraph, nx.MultiDiGraph)):
