e68f0e284b04efe753f903c6a90b4589b7660ccb
rusty1s <matthias.fey@tu-dortmund.de>
2023-04-20 08:06:37 +0000

    update

--- torch_geometric/nn/models/lightgcn.py
+++ torch_geometric/nn/models/lightgcn.py
@@ -258,12 +258,8 @@ class BPRLoss(_Loss):
         super().__init__(None, None, "sum", **kwargs)
         self.lambda_reg = lambda_reg
 
-    def forward(
-        self,
-        positives: Tensor,
-        negatives: Tensor,
-        parameters: OptTensor = None,
-    ) -> Tensor:
+    def forward(self, positives: Tensor, negatives: Tensor,
+                parameters: Tensor = None) -> Tensor:
         r"""Compute the mean Bayesian Personalized Ranking (BPR) loss.
 
         .. note::
@@ -281,9 +277,9 @@ class BPRLoss(_Loss):
         """
         n_pairs = positives.size(0)
         log_prob = F.logsigmoid(positives - negatives).mean()
-
         regularization = 0
-        if self.lambda_reg != 0 and parameters is not None:
+
+        if self.lambda_reg != 0:
             regularization = self.lambda_reg * parameters.norm(p=2).pow(2)
 
-        return -log_prob + regularization / n_pairs
+        return (-log_prob + regularization) / n_pairs
