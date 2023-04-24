5420b2b184a04a7229a1b3fddc3979f50cf8f683
rusty1s <matthias.fey@tu-dortmund.de>
2023-04-24 10:25:53 +0000

    update

--- test/data/test_dataset_summary.py
+++ test/data/test_dataset_summary.py
@@ -1,18 +1,19 @@
 import torch
+from torch import Tensor
 
 from torch_geometric.data.summary import Stats, Summary
 from torch_geometric.datasets import FakeDataset, FakeHeteroDataset
 from torch_geometric.testing import withPackage
 
 
-def check_summary_item(summary_item, exp_item):
-    assert summary_item.mean == exp_item.mean().item()
-    assert summary_item.std == exp_item.std().item()
-    assert summary_item.min == exp_item.min().item()
-    assert summary_item.quantile25 == exp_item.quantile(0.25).item()
-    assert summary_item.median == exp_item.median().item()
-    assert summary_item.quantile75 == exp_item.quantile(0.75).item()
-    assert summary_item.max == exp_item.max().item()
+def check_stats(stats: Stats, expected: Tensor):
+    assert stats.mean == float(expected.mean())
+    assert stats.std == float(expected.std())
+    assert stats.min == float(expected.min())
+    assert stats.quantile25 == float(expected.quantile(0.25))
+    assert stats.median == float(expected.median())
+    assert stats.quantile75 == float(expected.quantile(0.75))
+    assert stats.max == float(expected.max())
 
 
 def test_dataset_summary():
@@ -25,17 +26,16 @@ def test_dataset_summary():
     assert summary.name == 'FakeDataset'
     assert summary.num_graphs == 10
 
-    check_summary_item(summary.num_nodes, num_nodes)
-    check_summary_item(summary.num_edges, num_edges)
+    check_stats(summary.num_nodes, num_nodes)
+    check_stats(summary.num_edges, num_edges)
 
 
 @withPackage('tabulate')
 def test_dataset_summary_representation():
     dataset = FakeDataset(num_graphs=10)
 
-    summary1 = Summary.from_dataset(dataset, per_type_breakdown=False)
-    summary2 = Summary.from_dataset(dataset, progress_bar=True,
-                                    per_type_breakdown=True)
+    summary1 = Summary.from_dataset(dataset, per_type=False)
+    summary2 = Summary.from_dataset(dataset, per_type=True)
 
     assert str(summary1) == str(summary2)
 
@@ -43,7 +43,7 @@ def test_dataset_summary_representation():
 @withPackage('tabulate')
 def test_dataset_summary_hetero():
     dataset1 = FakeHeteroDataset(num_graphs=10)
-    summary1 = Summary.from_dataset(dataset1, per_type_breakdown=False)
+    summary1 = Summary.from_dataset(dataset1, per_type=False)
 
     dataset2 = [data.to_homogeneous() for data in dataset1]
     summary2 = Summary.from_dataset(dataset2)
@@ -54,17 +54,16 @@ def test_dataset_summary_hetero():
 
 
 @withPackage('tabulate')
-def test_dataset_summary_hetero_representation_len():
+def test_dataset_summary_hetero_representation_length():
     dataset = FakeHeteroDataset(num_graphs=10)
-    summary = Summary.from_dataset(dataset, progress_bar=True)
-    summ_num_lines = len(str(summary).splitlines())
+    summary = Summary.from_dataset(dataset)
+    num_lines = len(str(summary).splitlines())
 
     stats_len = len(Stats.__dataclass_fields__)
-    header_and_border_len = 5
-    summ_tables_count = 3  # general, node per type, edge per type
-    exp_length = summ_tables_count * (stats_len + header_and_border_len)
+    len_header_and_border = 5
+    num_tables = 3  # general, stats per node type, stats per edge type
 
-    assert summ_num_lines == exp_length
+    assert num_lines == num_tables * (stats_len + len_header_and_border)
 
 
 def test_dataset_summary_hetero_per_type_check():
@@ -77,26 +76,23 @@ def test_dataset_summary_hetero_per_type_check():
     assert summary.name == 'FakeHeteroDataset'
     assert summary.num_graphs == 10
 
-    check_summary_item(summary.num_nodes, exp_num_nodes)
-    check_summary_item(summary.num_edges, exp_num_edges)
-
-    assert dataset.node_types
-    assert dataset.edge_types
+    check_stats(summary.num_nodes, exp_num_nodes)
+    check_stats(summary.num_edges, exp_num_edges)
 
     num_nodes_per_type = {}
     for node_type in dataset.node_types:
         num_nodes_per_type[node_type] = torch.Tensor(
-            [data.get_node_store(node_type).num_nodes for data in dataset])
+            [data[node_type].num_nodes for data in dataset])
 
     assert len(summary.num_nodes_per_type) == len(dataset.node_types)
-    for node_type, node_type_stats in summary.num_nodes_per_type:
-        check_summary_item(node_type_stats, num_nodes_per_type[node_type])
+    for node_type, stats in summary.num_nodes_per_type.items():
+        check_stats(stats, num_nodes_per_type[node_type])
 
     num_edges_per_type = {}
     for edge_type in dataset.edge_types:
         num_edges_per_type[edge_type] = torch.Tensor(
-            [data.get_edge_store(*edge_type).num_edges for data in dataset])
+            [data[edge_type].num_edges for data in dataset])
 
     assert len(summary.num_edges_per_type) == len(dataset.edge_types)
-    for edge_type, edge_type_stats in summary.num_edges_per_type:
-        check_summary_item(edge_type_stats, num_edges_per_type[edge_type])
+    for edge_type, stats in summary.num_edges_per_type.items():
+        check_stats(stats, num_edges_per_type[edge_type])
--- torch_geometric/data/summary.py
+++ torch_geometric/data/summary.py
@@ -1,10 +1,11 @@
+from collections import defaultdict
 from dataclasses import dataclass
-from typing import List, Optional, Tuple, Union
+from typing import Dict, List, Optional, Union
 
 import torch
 from tqdm import tqdm
 
-from torch_geometric.data import Dataset
+from torch_geometric.data import Dataset, HeteroData
 
 
 @dataclass
@@ -40,13 +41,16 @@ class Summary:
     num_graphs: int
     num_nodes: Stats
     num_edges: Stats
-    num_nodes_per_type: List[Tuple[str, Stats]]
-    num_edges_per_type: List[Tuple[str, Stats]]
+    num_nodes_per_type: Optional[Dict[str, Stats]] = None,
+    num_edges_per_type: Optional[Dict[str, Stats]] = None,
 
     @classmethod
-    def from_dataset(cls, dataset: Dataset,
-                     progress_bar: Optional[bool] = None,
-                     per_type_breakdown: Optional[bool] = True):
+    def from_dataset(
+        cls,
+        dataset: Dataset,
+        progress_bar: Optional[bool] = None,
+        per_type: bool = True,
+    ):
         r"""Creates a summary of a :class:`~torch_geometric.data.Dataset`
         object.
 
@@ -56,92 +60,95 @@ class Summary:
                 progress bar during stats computation. If set to :obj:`None`,
                 will automatically decide whether to show a progress bar based
                 on dataset size. (default: :obj:`None`)
-            per_type_breakdown (bool, optional). If set to :obj:`True`, it will
-                try to separate nodes and edges and calculate stats for each
-                type - can be usefull for i.e Hetero Dataset. If set to
-                :obj:`False`, only the general stats for nodes and columns will
-                be calculated. (default: :obj:`True`)
+            per_type (bool, optional). If set to :obj:`True`, will separate
+                statistics per node and edge type (only applicable in
+                heterogeneous graph datasets). (default: :obj:`True`)
         """
         name = dataset.__class__.__name__
 
         if progress_bar is None:
             progress_bar = len(dataset) >= 10000
 
-        node_types = []
-        edge_types = []
-        if hasattr(dataset, 'node_types'):
-            node_types = dataset.node_types
-        if hasattr(dataset, 'edge_types'):
-            edge_types = dataset.edge_types
-
         if progress_bar:
             dataset = tqdm(dataset)
 
-        num_nodes_list, num_edges_list = [], []
+        num_nodes, num_edges = [], []
+        num_nodes_per_type = defaultdict(list)
+        num_edges_per_type = defaultdict(list)
+
         for data in dataset:
-            num_nodes_list.append(data.num_nodes)
-            num_edges_list.append(data.num_edges)
+            num_nodes.append(data.num_nodes)
+            num_edges.append(data.num_edges)
 
-        nodes_per_type = []
-        edges_per_type = []
-        if per_type_breakdown:
-            for node_t in node_types:
-                node_list = []
-                for data in dataset:
-                    node_list.append(data.get_node_store(node_t).num_nodes)
-                nodes_per_type.append([node_t, Stats.from_data(node_list)])
+            if per_type and isinstance(data, HeteroData):
+                for node_type in data.node_types:
+                    num_nodes_per_type[node_type].append(
+                        data[node_type].num_nodes)
+                for edge_type in data.edge_types:
+                    num_edges_per_type[edge_type].append(
+                        data[edge_type].num_edges)
 
-            for edge_t in edge_types:
-                edge_list = []
-                for data in dataset:
-                    edge_list.append(data.get_edge_store(*edge_t).num_edges)
-                edges_per_type.append([edge_t, Stats.from_data(edge_list)])
+        if len(num_nodes_per_type) > 0:
+            num_nodes_per_type = {
+                node_type: Stats.from_data(num_nodes_list)
+                for node_type, num_nodes_list in num_nodes_per_type.items()
+            }
+        else:
+            num_nodes_per_type = None
 
-        return cls(name=name, num_graphs=len(dataset),
-                   num_nodes=Stats.from_data(num_nodes_list),
-                   num_edges=Stats.from_data(num_edges_list),
-                   num_nodes_per_type=nodes_per_type,
-                   num_edges_per_type=edges_per_type)
+        if len(num_edges_per_type) > 0:
+            num_edges_per_type = {
+                edge_type: Stats.from_data(num_edges_list)
+                for edge_type, num_edges_list in num_edges_per_type.items()
+            }
+        else:
+            num_edges_per_type = None
+
+        return cls(
+            name=name,
+            num_graphs=len(dataset),
+            num_nodes=Stats.from_data(num_nodes),
+            num_edges=Stats.from_data(num_edges),
+            num_nodes_per_type=num_nodes_per_type,
+            num_edges_per_type=num_edges_per_type,
+        )
 
     def __repr__(self) -> str:
         from tabulate import tabulate
 
-        prefix = f'{self.name} (#graphs={self.num_graphs}):\n'
+        body = f'{self.name} (#graphs={self.num_graphs}):\n'
 
         content = [['', '#nodes', '#edges']]
         stats = [self.num_nodes, self.num_edges]
         for field in Stats.__dataclass_fields__:
             row = [field] + [f'{getattr(s, field):.1f}' for s in stats]
             content.append(row)
-        body = tabulate(content, headers='firstrow', tablefmt='psql')
+        body += tabulate(content, headers='firstrow', tablefmt='psql')
 
-        if self.num_nodes_per_type:
+        if self.num_nodes_per_type is not None:
             content = [['']]
-            content[0] += [
-                f'#{node_type}' for node_type, _ in self.num_nodes_per_type
-            ]
+            content[0] += list(self.num_nodes_per_type.keys())
 
             for field in Stats.__dataclass_fields__:
                 row = [field] + [
                     f'{getattr(s, field):.1f}'
-                    for _, s in self.num_nodes_per_type
+                    for _, s in self.num_nodes_per_type.items()
                 ]
                 content.append(row)
-            body += "\nNumber of nodes per node type:\n" + \
-                tabulate(content, headers='firstrow', tablefmt='psql')
+            body += "\nNumber of nodes per node type:\n"
+            body += tabulate(content, headers='firstrow', tablefmt='psql')
 
-        if self.num_edges_per_type:
+        if self.num_edges_per_type is not None:
             content = [['']]
-            content[0] += [
-                f'#{edge_type}' for edge_type, _ in self.num_edges_per_type
-            ]
+            content[0] += [f"({', '.join(edge_type)})" for edge_type in self.num_edges_per_type.keys()]
+
             for field in Stats.__dataclass_fields__:
                 row = [field] + [
                     f'{getattr(s, field):.1f}'
-                    for _, s in self.num_edges_per_type
+                    for _, s in self.num_edges_per_type.items()
                 ]
                 content.append(row)
-            body += "\nNumber of edges per edge type:\n" +\
-                tabulate(content, headers='firstrow', tablefmt='psql')
+            body += "\nNumber of edges per edge type:\n"
+            body += tabulate(content, headers='firstrow', tablefmt='psql')
 
-        return prefix + body
+        return body
