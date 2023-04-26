ef44ffa4a843ad06f65c0737d1e3a5661fccc18c
puririshi98 <puririshi98@berkeley.edu>
2023-04-26 16:24:23 -0700

    debugging

--- torch_geometric/data/collate.py
+++ torch_geometric/data/collate.py
@@ -170,9 +170,13 @@ def _collate(
         slices = cumsum(repeats)
         if is_torch_sparse_tensor(elem):
             # (TODO) Replace w/ torch.cat once working upstream
-            e_idxs_to_cat = [to_edge_index(value)[0] for value in values]
+            sparse_sizes: List[int] = [0, 0]            
+            sparse_sizes[0] += value.sparse_size(0)
+            sparse_sizes[1] = max(sparse_sizes[1], value.sparse_size(1))
+            for value in values:
+                e_idxs_to_cat.append(to_edge_index(value)[0])
             print("e_idxs_to_cat.size()=",[e.size() for e in e_idxs_to_cat])
-            value = to_torch_sparse_tensor(torch.cat(e_idxs_to_cat, dim=1), layout=values[0].layout)
+            value = to_torch_sparse_tensor(torch.cat(e_idxs_to_cat, dim=1), size=sparse_sizes, layout=values[0].layout)
             print("value.size()=",value.size())
         else:
             value = torch_sparse.cat(values, dim=cat_dim)
