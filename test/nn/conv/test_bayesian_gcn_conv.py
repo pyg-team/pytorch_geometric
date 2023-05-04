0109f045033d4665d06066368eae9ef298dac282
Jintang Li <cnljt@outlook.com>
2023-05-05 00:39:54 +0800

    Update test_bayesian_gcn_conv.py

--- test/nn/conv/test_bayesian_gcn_conv.py
+++ test/nn/conv/test_bayesian_gcn_conv.py
@@ -1,51 +1,139 @@
 import torch
 
-# import torch_geometric.typing
-from torch_geometric.nn.conv import BayesianGCNConv
+import torch_geometric.typing
+from torch_geometric.nn import BayesianGCNConv
 from torch_geometric.testing import is_full_test
+from torch_geometric.typing import SparseTensor
+from torch_geometric.utils import to_torch_csc_tensor
 
 
-def test_bayesian_conv():
+def test_bayesian_gcn_conv():
     x = torch.randn(4, 16)
     edge_index = torch.tensor([[0, 0, 0, 1, 2, 3], [1, 2, 3, 0, 0, 0]])
-    row, col = edge_index
-    value = torch.rand(row.size(0))
-
-    adj1 = torch.randint(0, 4, (2, 6))
-    adj2 = torch.randint(0, 4, (2, 6))
+    value = torch.rand(edge_index.size(1))
+    adj1 = to_torch_csc_tensor(edge_index, size=(4, 4))
+    adj2 = to_torch_csc_tensor(edge_index, value, size=(4, 4))
 
     conv = BayesianGCNConv(16, 32)
-    assert conv.__repr__() == "BayesianGCNConv(16, 32)"
-    out1, kl_out1 = conv(x, edge_index)
+    assert str(conv) == "BayesianGCNConv(16, 32)"
+
+    out1 = conv(x, edge_index)
     assert out1.size() == (4, 32)
-    out_1_adj1, kl_adj1 = conv(x, adj1)
-    assert torch.allclose(torch.abs(out1.mean()), torch.abs(out_1_adj1.mean()),
-                          atol=1)
-    assert torch.allclose(torch.var(out1), torch.var(out_1_adj1), atol=1)
-    assert torch.allclose(kl_out1, kl_adj1, atol=1e-6)
-    out2, kl_out2 = conv(x, edge_index, value)
+    assert torch.allclose(
+        conv(x, adj1.t()).mean().abs(),
+        out1.mean().abs(), atol=1.0)
+    assert torch.allclose(
+        conv(x, adj1.t()).var().abs(),
+        out1.var().abs(), atol=1.0)
+
+    out2 = conv(x, edge_index, value)
     assert out2.size() == (4, 32)
-    out_2_adj2, kl_adj2 = conv(x, adj2)
-    assert torch.allclose(torch.abs(out2.mean()), torch.abs(out_2_adj2.mean()),
-                          atol=1)
-    assert torch.allclose(torch.var(out2), torch.var(out_2_adj2), atol=1)
-    assert torch.allclose(kl_out2, kl_adj2, atol=1e-6)
+    assert torch.allclose(
+        conv(x, adj2.t()).mean().abs(),
+        out2.mean().abs(), atol=1.0)
+    assert torch.allclose(
+        conv(x, adj2.t()).var().abs(),
+        out2.var().abs(), atol=1.0)
+
+    if torch_geometric.typing.WITH_TORCH_SPARSE:
+        adj3 = SparseTensor.from_edge_index(edge_index, sparse_sizes=(4, 4))
+        adj4 = SparseTensor.from_edge_index(edge_index, value, (4, 4))
+        assert torch.allclose(
+            conv(x, adj3.t()).mean().abs(),
+            out1.mean().abs(), atol=1.0)
+        assert torch.allclose(
+            conv(x, adj3.t()).var().abs(),
+            out1.var().abs(), atol=1.0)
+        assert torch.allclose(
+            conv(x, adj4.t()).mean().abs(),
+            out2.mean().abs(), atol=1.0)
+        assert torch.allclose(
+            conv(x, adj4.t()).var().abs(),
+            out2.var().abs(), atol=1.0)
 
     if is_full_test():
         t = "(Tensor, Tensor, OptTensor) -> Tensor"
         jit = torch.jit.script(conv.jittable(t))
-        assert jit(x, edge_index).tolist() == out1.tolist()
-        assert jit(x, edge_index, value).tolist() == out2.tolist()
+        assert torch.allclose(
+            jit(x, edge_index).mean().abs(),
+            out1.mean().abs(), atol=1.0)
+        assert torch.allclose(
+            jit(x, edge_index).var().abs(),
+            out1.var().abs(), atol=1.0)
+        assert torch.allclose(
+            jit(x, edge_index, value).mean().abs(),
+            out2.mean().abs(), atol=1.0)
+        assert torch.allclose(
+            jit(x, edge_index, value).var().abs(),
+            out2.var().abs(), atol=1.0)
 
-        t = "(Tensor, Tensor, OptTensor) -> Tensor"
+    if is_full_test() and torch_geometric.typing.WITH_TORCH_SPARSE:
+        t = "(Tensor, SparseTensor, OptTensor) -> Tensor"
         jit = torch.jit.script(conv.jittable(t))
-        assert torch.allclose(jit(x, adj1.t()), out1, atol=1e-6)
-        assert torch.allclose(jit(x, adj2.t()), out2, atol=1e-6)
+        assert torch.allclose(
+            jit(x, adj3.t()).mean().abs(),
+            out1.mean().abs(), atol=1.0)
+        assert torch.allclose(
+            jit(x, adj3.t()).var().abs(),
+            out1.var().abs(), atol=1.0)
+        assert torch.allclose(
+            jit(x, adj4.t()).mean().abs(),
+            out2.mean().abs(), atol=1.0)
+        assert torch.allclose(
+            jit(x, adj4.t()).var().abs(),
+            out2.var().abs(), atol=1.0)
 
     conv.cached = True
-    out_f1, kl_f1 = conv(x, edge_index)
-    for i in range(len(out1.tolist())):
-        assert torch.allclose(torch.abs(out_f1[i].mean()),
-                              torch.abs(out1[i].mean()), atol=1)
-        assert torch.allclose(torch.var(out_f1[i]), torch.var(out1[i]), atol=1)
-    assert torch.allclose(kl_f1, kl_out1, atol=1e-6)
+    conv(x, edge_index)
+    assert conv._cached_edge_index is not None
+    assert torch.allclose(
+        conv(x, edge_index).mean().abs(),
+        out1.mean().abs(), atol=1.0)
+    assert torch.allclose(
+        conv(x, adj1.t()).mean().abs(),
+        out1.var().abs(), atol=1.0)
+    assert torch.allclose(
+        conv(x, adj1.t()).var().abs(),
+        out1.mean().abs(), atol=1.0)
+    assert torch.allclose(
+        conv(x, edge_index).var().abs(),
+        out1.var().abs(), atol=1.0)
+
+    if torch_geometric.typing.WITH_TORCH_SPARSE:
+        conv(x, adj3.t())
+        assert conv._cached_adj_t is not None
+        assert torch.allclose(
+            conv(x, adj3.t()).mean().abs(),
+            out1.mean().abs(), atol=1.0)
+        assert torch.allclose(
+            conv(x, adj3.t()).var().abs(),
+            out1.var().abs(), atol=1.0)
+
+    # Test `return_kl_divergence`.
+    _, kl_div1 = conv(x, edge_index, return_kl_divergence=True)
+    assert kl_div1.dim() == 0
+
+    _, kl_div2 = conv(x, adj1.t(), return_kl_divergence=True)
+    assert kl_div2.dim() == 0
+    assert torch.allclose(kl_div1, kl_div2, atol=1e-6)
+
+    if torch_geometric.typing.WITH_TORCH_SPARSE:
+        _, kl_div3 = conv(x, adj2.t(), return_kl_divergence=True)
+        assert kl_div3.dim() == 0
+        assert torch.allclose(kl_div1, kl_div3, atol=1e-6)
+
+    if is_full_test():
+        t = ('(Tensor, Tensor, OptTensor, Size, bool) -> '
+             'Tuple[Tensor, Tuple[Tensor, Tensor]]')
+        jit = torch.jit.script(conv.jittable(t))
+        _, kl_div4 = jit(x, edge_index, return_kl_divergence=True)
+        assert kl_div3.dim() == 0
+        assert torch.allclose(kl_div1, kl_div4, atol=1e-6)
+
+    if is_full_test() and torch_geometric.typing.WITH_TORCH_SPARSE:
+        t = ('(Tensor, SparseTensor, OptTensor, Size, bool) -> '
+             'Tuple[Tensor, SparseTensor]')
+        jit = torch.jit.script(conv.jittable(t))
+        _, kl_div5 = jit(x, adj2.t(), return_kl_divergence=True)
+        assert kl_div5.dim() == 0
+        assert torch.allclose(kl_div1, kl_div5, atol=1e-6)
