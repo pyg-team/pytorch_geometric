fb1d855d5e3d12092ffaa8d456e3a46252cf9723
Matthias Fey <matthias.fey@tu-dortmund.de>
2023-05-04 14:07:32 +0200

    Allow tuples as keys in `ModuleDict`/`ParameterDict` (#7294)

--- torch_geometric/nn/module_dict.py
+++ torch_geometric/nn/module_dict.py
@@ -1,15 +1,20 @@
-from typing import Iterable, Mapping, Optional, Tuple
+from typing import Iterable, Mapping, Optional, Tuple, Union
 
 import torch
 from torch.nn import Module
+
+Key = Union[str, Tuple[str, ...]]
 
 
 # `torch.nn.ModuleDict` doesn't allow `.` to be used in key names.
 # This `ModuleDict` will support it by converting the `.` to `#` in the
 # internal representation and converts it back to `.` in the external
-# representation.
+# representation. It also allows passing tuples as keys.
 class ModuleDict(torch.nn.ModuleDict):
-    def __init__(self, modules: Optional[Mapping[str, Module]] = None):
+    def __init__(
+        self,
+        modules: Optional[Mapping[Union[str, Tuple[str, ...]], Module]] = None,
+    ):
         if modules is not None:  # Replace the keys in modules:
             modules = {
                 self.to_internal_key(key): module
@@ -18,28 +23,34 @@ class ModuleDict(torch.nn.ModuleDict):
         super().__init__(modules)
 
     @staticmethod
-    def to_internal_key(key: str) -> str:
-        return key.replace(".", "#")
+    def to_internal_key(key: Key) -> str:
+        if isinstance(key, tuple):
+            assert len(key) > 1
+            key = f"<{'___'.join(key)}>"
+        assert isinstance(key, str)
+        return key.replace('.', '#')
 
     @staticmethod
-    def to_external_key(key: str) -> str:
-        return key.replace("#", ".")
+    def to_external_key(key: str) -> Key:
+        key = key.replace('#', '.')
+        if key.startswith('<') and key.endswith('>') and '___' in key:
+            key = tuple(key[1:-1].split('___'))
+        return key
 
-    def __getitem__(self, key: str) -> Module:
+    def __getitem__(self, key: Key) -> Module:
         return super().__getitem__(self.to_internal_key(key))
 
-    def __setitem__(self, key: str, module: Module):
+    def __setitem__(self, key: Key, module: Module):
         return super().__setitem__(self.to_internal_key(key), module)
 
-    def __delitem__(self, key: str):
+    def __delitem__(self, key: Key):
         return super().__delitem__(self.to_internal_key(key))
 
-    def __contains__(self, key: str) -> bool:
+    def __contains__(self, key: Key) -> bool:
         return super().__contains__(self.to_internal_key(key))
 
-    def keys(self) -> Iterable[str]:
+    def keys(self) -> Iterable[Key]:
         return [self.to_external_key(key) for key in super().keys()]
 
-    def items(self) -> Iterable[Tuple[str, Module]]:
-        return [(self.to_external_key(key), value)
-                for key, value in super().items()]
+    def items(self) -> Iterable[Tuple[Key, Module]]:
+        return [(self.to_external_key(k), v) for k, v in super().items()]
