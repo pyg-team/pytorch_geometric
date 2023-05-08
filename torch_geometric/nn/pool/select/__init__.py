c566f5cee90bf319cbe9e86808a60b1f1d283c94
Jinu Sunil <jinu.sunil@gmail.com>
2023-05-08 13:37:06 +0530

    `Pooling`: Update base classes (#7307)
    Towards #6455.
    This PR makes the following changes
    1. Removes the `Pooling` base class. The current implementation isn't
    flexible enough to support all pooling operators. Instead pooling
    operators will implement their own forward method, using `Select` ,
    `Connect` and `Aggregate` operators.
    2. Updated the `Select` operator. It now returns `SelectOutput` which
    contains `node_index`, `cluster_index`, `num_clusters` and `weight`.
    Where `weight` is the weight given to a node assignment to a cluster.
    
    ---------
    
    Co-authored-by: rusty1s <matthias.fey@tu-dortmund.de>

--- torch_geometric/nn/pool/select/__init__.py
+++ torch_geometric/nn/pool/select/__init__.py
@@ -1,5 +1,6 @@
-from .base import Select
+from .base import Select, SelectOutput
 
 __all__ = [
     'Select',
+    'SelectOutput',
 ]
--- torch_geometric/nn/pool/select/base.py
+++ torch_geometric/nn/pool/select/base.py
@@ -1,38 +1,80 @@
-from typing import Optional, Tuple
+from dataclasses import dataclass
+from typing import Optional
 
 import torch
 from torch import Tensor
 
+from torch_geometric.utils.mixin import CastMixin
+
+
+@dataclass(init=False)
+class SelectOutput(CastMixin):
+    r"""The output of the :class:`Select` method, which holds an assignment
+    from selected nodes to their respective cluster(s).
+
+    Args:
+        node_index (torch.Tensor): The indices of the selected nodes.
+        cluster_index (torch.Tensor): The indices of the clusters each node in
+            :obj:`node_index` is assigned to.
+        num_clusters (int): The number of clusters.
+        weight (torch.Tensor, optional): A weight vector, denoting the strength
+            of the assignment of a node to its cluster. (default: :obj:`None`)
+    """
+    node_index: Tensor
+    cluster_index: Tensor
+    num_clusters: int
+    weight: Optional[Tensor] = None
+
+    def __init__(
+        self,
+        node_index: Tensor,
+        cluster_index: Tensor,
+        num_clusters: int,
+        weight: Optional[Tensor] = None,
+    ):
+        if node_index.dim() != 1:
+            raise ValueError(f"Expected 'node_index' to be one-dimensional "
+                             f"(got {node_index.dim()} dimensions)")
+
+        if cluster_index.dim() != 1:
+            raise ValueError(f"Expected 'cluster_index' to be one-dimensional "
+                             f"(got {cluster_index.dim()} dimensions)")
+
+        if node_index.numel() != cluster_index.numel():
+            raise ValueError(f"Expected 'node_index' and 'cluster_index' to "
+                             f"hold the same number of values (got "
+                             f"{node_index.numel()} and "
+                             f"{cluster_index.numel()} values)")
+
+        if weight is not None and weight.dim() != 1:
+            raise ValueError(f"Expected 'weight' vector to be one-dimensional "
+                             f"(got {weight.dim()} dimensions)")
+
+        if weight is not None and weight.numel() != node_index.numel():
+            raise ValueError(f"Expected 'weight' to hold {node_index.numel()} "
+                             f"values (got {weight.numel()} values)")
+
+        self.node_index = node_index
+        self.cluster_index = cluster_index
+        self.num_clusters = num_clusters
+        self.weight = weight
+
 
 class Select(torch.nn.Module):
-    r"""An abstract base class implementing custom node selections that map the
-    nodes of an input graph to supernodes of the pooled one.
+    r"""An abstract base class for implementing custom node selections as
+    described in the `"Understanding Pooling in Graph Neural Networks"
+    <https://arxiv.org/abs/1905.05178>`_ paper, which maps the nodes of an
+    input graph to supernodes in the coarsened graph.
 
-    Specifically, :class:`Select` returns a mapping
-    :math:`\mathbf{c} \in {\{ -1, \ldots, C - 1\}}^N`, which assigns each node
-    to one of :math:`C` super nodes.
-    In addition, :class:`Select` returns the number of super nodes.
+    Specifically, :class:`Select` returns a :class:`SelectOutput` output, which
+    holds a (sparse) mapping :math:`\mathbf{C} \in {[0, 1]}^{N \times C}` that
+    assigns selected nodes to one or more of :math:`C` super nodes.
     """
     def reset_parameters(self):
+        r"""Resets all learnable parameters of the module."""
         pass
 
-    def forward(
-        self,
-        x: Tensor,
-        edge_index: Tensor,
-        edge_attr: Optional[Tensor] = None,
-        batch: Optional[Tensor] = None,
-    ) -> Tuple[Tensor, int]:
-        r"""
-        Args:
-            x (torch.Tensor): The input node features.
-            edge_index (torch.Tensor): The edge indices.
-            edge_attr (torch.Tensor, optional): The edge features.
-                (default: :obj:`None`)
-            batch (torch.Tensor, optional): The batch vector
-                :math:`\mathbf{b} \in {\{ 0, \ldots, B-1\}}^N`, which assigns
-                each node to a specific graph. (default: :obj:`None`)
-        """
+    def forward(self, *args, **kwargs) -> SelectOutput:
         raise NotImplementedError
 
     def __repr__(self) -> str:
