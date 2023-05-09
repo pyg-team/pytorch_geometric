832e570505bffdeed047f671902a8d39420e35ee
Gabriel VIGOT <56968382+gabyweg76@users.noreply.github.com>
2023-05-09 12:48:02 +0200

    Update test_bayesian_gcn_conv.py
    Corrections

--- test/nn/conv/test_bayesian_gcn_conv.py
+++ test/nn/conv/test_bayesian_gcn_conv.py
@@ -17,15 +17,15 @@ def test_bayesian_gcn_conv():
     conv = BayesianGCNConv(16, 32)
     assert str(conv) == "BayesianGCNConv(16, 32)"
 
-    out1, kl1 = conv(x, edge_index)
+    out1, kl1 = conv(x, edge_index, return_kl_divergence=True)
     assert out1.size() == (4, 32)
 
-    out_adj1, kl_adj1 = conv(x, adj1.t())
+    out_adj1, kl_adj1 = conv(x, adj1.t(), return_kl_divergence=True)
     assert torch.allclose(out_adj1.mean().abs(), out1.mean().abs(), atol=1.0)
     assert torch.allclose(out_adj1.var().abs(), out1.var().abs(), atol=1.0)
 
-    out2, kl2 = conv(x, edge_index, value)
-    out_adj2, kl_adj2 = conv(x, adj2.t())
+    out2, kl2 = conv(x, edge_index, value, return_kl_divergence=True)
+    out_adj2, kl_adj2 = conv(x, adj2.t(),  return_kl_divergence=True)
     assert out2.size() == (4, 32)
     assert torch.allclose(out_adj2.mean().abs(), out2.mean().abs(), atol=1.0)
     assert torch.allclose(out_adj2.var().abs(), out2.var().abs(), atol=1.0)
@@ -34,8 +34,8 @@ def test_bayesian_gcn_conv():
         adj3 = SparseTensor.from_edge_index(edge_index, sparse_sizes=(4, 4))
         adj4 = SparseTensor.from_edge_index(edge_index, value, (4, 4))
 
-        out_adj3, kl_adj3 = conv(x, adj3.t())
-        out_adj4, kl_adj4 = conv(x, adj4.t())
+        out_adj3, kl_adj3 = conv(x, adj3.t(),  return_kl_divergence=True)
+        out_adj4, kl_adj4 = conv(x, adj4.t(),  return_kl_divergence=True)
 
         assert torch.allclose(out_adj3.mean().abs(),
                               out1.mean().abs(), atol=1.0)
@@ -72,7 +72,7 @@ def test_bayesian_gcn_conv():
         assert torch.allclose(out_adj4.var().abs(), out2.var().abs(), atol=1.0)
 
     conv.cached = True
-    out1_prime, kl1_prime = conv(x, edge_index)
+    out1_prime, kl1_prime = conv(x, edge_index,  return_kl_divergence=True)
 
     assert conv._cached_edge_index is not None
     assert torch.allclose(out1_prime.mean().abs(), out1.mean().abs(), atol=1.0)
@@ -81,7 +81,7 @@ def test_bayesian_gcn_conv():
     assert torch.allclose(out_adj1.var().abs(), out1.var().abs(), atol=1.0)
 
     if torch_geometric.typing.WITH_TORCH_SPARSE:
-        out3_prime, kl3_prime = conv(x, adj3.t())
+        out3_prime, kl3_prime = conv(x, adj3.t(), return_kl_divergence=True)
         assert conv._cached_adj_t is not None
         assert torch.allclose(out3_prime.mean().abs(),
                               out1.mean().abs(), atol=1.0)
