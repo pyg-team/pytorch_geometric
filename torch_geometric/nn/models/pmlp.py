ab930fc6ca155fe6e75e0e6beb9b4b2e4fd23652
wwymak <6398157+wwymak@users.noreply.github.com>
2023-05-15 18:41:30 +0100

    add docstrings

--- torch_geometric/nn/models/pmlp.py
+++ torch_geometric/nn/models/pmlp.py
@@ -6,6 +6,27 @@ from torch_geometric.nn.dense.linear import Linear
 
 
 class PMLP(torch.nn.Module):
+    r"""The PMLP model from the
+        `"Graph Neural Networks are Inherently Good Generalizers:
+        Insights by Bridging GNNs and MLPs"
+        <https://arxiv.org/pdf/2212.09034.pdf>`_ paper where message passing
+        layers are only used in inference
+
+
+        Args:
+            in_channels (int, optional): Size of each input sample.
+            hidden_channels (int, optional): Size of each hidden sample.
+            out_channels (int, optional): Size of each output sample.
+                Will override :attr:`channel_list`. (default: :obj:`None`)
+            num_layers (int, optional): The number of layers.
+                Will override :attr:`channel_list`. (default: :obj:`None`)
+            dropout (float, optional): Dropout probability of each
+                hidden embedding.
+            bias (bool, optional): If set to :obj:`False`, the module
+                will not learn additive biases.
+            **kwargs (optional): Extra parameters for the SimpleConv layers
+                used during inference
+        """
     def __init__(self, in_channels: int, hidden_channels: int,
                  out_channels: int, num_layers: int, dropout: float = 0.5,
                  bias: bool = True, **kwargs):
@@ -32,11 +53,17 @@ class PMLP(torch.nn.Module):
         self.reset_parameters()
 
     def reset_parameters(self):
+        r"""Resets all learnable parameters of the module."""
         for mlp in self.fcs:
             torch.nn.init.xavier_uniform_(mlp.weight, gain=1.414)
             torch.nn.init.zeros_(mlp.bias)
 
-    def forward(self, x, edge_index=None):
+    def forward(self, x: torch.Tensor, edge_index=None) -> torch.Tensor:
+        r"""
+            Args:
+            x (torch.Tensor): The source tensor.
+            edge_index (torch.Tensor, optional):
+        """
         for i in range(self.num_layers):
             x = x @ self.fcs[i].weight.t()
             if not self.training:
