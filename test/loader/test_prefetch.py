249681497fb6a3c9ebed25759cff6b7cdf28d3c5
Matthias Fey <matthias.fey@tu-dortmund.de>
2023-05-17 17:29:21 +0200

    `GPUPrefetcher` benchmark (#7378)
    ```
    Forward pass without prefetching...
    100%|██████| 200/200 [00:08<00:00, 22.94it/s]
    Forward pass with prefetching...
    100%|██████| 200/200 [00:07<00:00, 25.25it/s]
    ```

--- test/loader/test_prefetch.py
+++ test/loader/test_prefetch.py
@@ -1,6 +1,8 @@
 import torch
 
+from torch_geometric.loader import NeighborLoader
 from torch_geometric.loader.prefetch import GPUPrefetcher
+from torch_geometric.nn import GraphSAGE
 from torch_geometric.testing import onlyCUDA
 
 
@@ -17,3 +19,42 @@ def test_gpu_prefetcher():
         assert torch.equal(batch.cpu(), data[i])
         assert loader.idx > 0
     assert loader.idx == 0
+
+
+if __name__ == '__main__':
+    import argparse
+
+    from ogb.nodeproppred import PygNodePropPredDataset
+    from tqdm import tqdm
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_workers', type=int, default=8)
+    args = parser.parse_args()
+
+    data = PygNodePropPredDataset('ogbn-products', root='/tmp/ogb')[0]
+
+    model = GraphSAGE(
+        in_channels=data.x.size(-1),
+        hidden_channels=64,
+        num_layers=2,
+    ).cuda()
+
+    loader = NeighborLoader(
+        data,
+        input_nodes=torch.arange(1024 * 200),
+        batch_size=1024,
+        num_neighbors=[10, 10],
+        num_workers=args.num_workers,
+        filter_per_worker=True,
+    )
+
+    print('Forward pass without prefetching...')
+    for batch in tqdm(loader):
+        with torch.no_grad():
+            batch = batch.cuda()
+            model(batch.x, batch.edge_index)
+
+    print('Forward pass with prefetching...')
+    for batch in tqdm(GPUPrefetcher(loader, device='cuda')):
+        with torch.no_grad():
+            model(batch.x, batch.edge_index)
