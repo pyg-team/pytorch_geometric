9ff4d6a09e49ec69f8d4d99f637d8e816c893648
rusty1s <matthias.fey@tu-dortmund.de>
2023-05-24 06:37:58 +0000

    update

--- test/utils/test_map.py
+++ test/utils/test_map.py
@@ -7,8 +7,6 @@ def test_map_index():
     src = torch.tensor([2, 0, 1, 0, 3])
     index = torch.tensor([3, 2, 0, 1])
 
-    print(map_index(src, index))
-
     out, mask = map_index(src, index)
     assert out.tolist() == [1, 2, 3, 2, 0]
     assert mask.tolist() == [True, True, True, True, True]
