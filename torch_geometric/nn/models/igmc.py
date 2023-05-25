26726e4f44817f92ba63fae97f61e2504e5ef6e1
Emanuel Seemann <3380606+seemanne@users.noreply.github.com>
2023-05-25 23:21:22 +0200

    add igmc_node_label for homogenous data

---
+++ torch_geometric/nn/models/igmc.py
@@ -0,0 +1,22 @@
+import torch
+
+from torch_geometric.typing import EdgeType, NodeType
+
+
+def igmc_node_label(batch: dict[NodeType], edge_index: dict[EdgeType],
+                    num_sampled_edges: int):
+
+    if isinstance(edge_index, torch.Tensor):
+
+        label_list = [float('inf')] * len(batch)
+        for i in range(2):
+            for j in range(num_sampled_edges):
+                label_list[(i * num_sampled_edges) + j] = i
+
+        for i in range(len(edge_index[0])):
+            v = edge_index[0][i]
+            u = edge_index[1][i]
+            if v > u:
+                label_list[v] = min(label_list[u] + 2, label_list[v])
+
+        return torch.tensor(label_list)
