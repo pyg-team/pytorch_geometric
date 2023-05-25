a39153dab0683ac2bf7102a64fd66c7fbbf5d378
Emanuel Seemann <3380606+seemanne@users.noreply.github.com>
2023-05-25 23:56:09 +0200

    add HeteroData support

--- torch_geometric/nn/models/igmc.py
+++ torch_geometric/nn/models/igmc.py
@@ -1,11 +1,13 @@
-from typing import Union
+from typing import Optional, Union
 
 import torch
 
 from torch_geometric.data import Data, HeteroData
+from torch_geometric.typing import NodeType
 
 
-def igmc_node_label(batch: Union[Data, HeteroData], num_sampled_edges: int):
+def igmc_node_label(batch: Union[Data, HeteroData], num_sampled_edges: int,
+                    edge_direction: Optional[NodeType] = None):
 
     if isinstance(batch, Data):
 
@@ -20,4 +22,31 @@ def igmc_node_label(batch: Union[Data, HeteroData], num_sampled_edges: int):
             if v > u:
                 label_list[v] = min(label_list[u] + 2, label_list[v])
 
-        return torch.tensor(label_list)
+        batch.igmc_id = torch.tensor(label_list)
+
+    if isinstance(batch, HeteroData):
+
+        assert edge_direction is not None
+
+        origin = edge_direction[0]
+        for node_type in batch.node_types:
+
+            if node_type == origin:
+                start_id = 0
+            else:
+                start_id = 1
+
+            igmc_id = 0 + start_id
+            label_list = [float('inf')] * len(batch[node_type].batch)
+            prev = 0
+
+            for index, i in batch[node_type].batch:
+
+                if i < prev:
+                    igmc_id += 2
+                label_list[index] = igmc_id
+                prev = i
+
+            batch[node_type].igmc_id = torch.tensor(label_list)
+
+    return batch
