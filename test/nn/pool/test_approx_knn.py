3633b31e37bdcc28250e598de9276236766b9319
rusty1s <matthias.fey@tu-dortmund.de>
2023-05-27 17:46:40 +0000

    update

--- test/nn/pool/test_approx_knn.py
+++ test/nn/pool/test_approx_knn.py
@@ -1,15 +1,20 @@
+import warnings
+
 import torch
 
 from torch_geometric.nn import approx_knn, approx_knn_graph
-from torch_geometric.testing import withPackage
+from torch_geometric.testing import onlyFullTest, withPackage
 
 
 def to_set(edge_index):
     return set([(i, j) for i, j in edge_index.t().tolist()])
 
 
+@onlyFullTest  # JIT compile makes this test to slow :(
 @withPackage('pynndescent')
 def test_approx_knn():
+    warnings.filterwarnings('ignore', '.*find n_neighbors.*')
+
     x = torch.tensor([
         [-1.0, -1.0],
         [-1.0, +1.0],
@@ -34,19 +39,12 @@ def test_approx_knn():
     edge_index = approx_knn(x, y, 2, batch_x, batch_y)
     assert to_set(edge_index) == set([(0, 2), (0, 3), (1, 4), (1, 5)])
 
-    if x.is_cuda:
-        edge_index = approx_knn(x, y, 2, batch_x, batch_y, cosine=True)
-        assert to_set(edge_index) == set([(0, 2), (0, 3), (1, 4), (1, 5)])
 
-    # Skipping a batch:
-    batch_x = torch.tensor([0, 0, 0, 0, 2, 2, 2, 2])
-    batch_y = torch.tensor([0, 2])
-    edge_index = approx_knn(x, y, 2, batch_x, batch_y)
-    assert to_set(edge_index) == set([(0, 2), (0, 3), (1, 4), (1, 5)])
-
-
+@onlyFullTest  # JIT compile makes this test to slow :(
 @withPackage('pynndescent')
 def test_approx_knn_graph():
+    warnings.filterwarnings('ignore', '.*find n_neighbors.*')
+
     x = torch.tensor([
         [-1.0, -1.0],
         [-1.0, +1.0],
