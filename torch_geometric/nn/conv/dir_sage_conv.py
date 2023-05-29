fb9598e164cf19a1f33035bff58e3dd94e8efd7d
Emanuele Rossi <emanuele.rossi1909@gmail.com>
2023-05-29 11:20:43 +0100

    Add test for DirSage

--- torch_geometric/nn/conv/dir_sage_conv.py
+++ torch_geometric/nn/conv/dir_sage_conv.py
@@ -1,22 +1,27 @@
-from typing import Dict, List, Optional
+from typing import Optional
 
 import torch
 from torch import Tensor
 
 from torch_geometric.nn.conv import SAGEConv
-from torch_geometric.typing import Adj, Optional
 from torch_geometric.nn.dense.linear import Linear
+from torch_geometric.typing import Adj, Optional
 
 
 class DirSageConv(torch.nn.Module):
-    def __init__(self, in_channels: int, out_channels: int, alpha: Optional[float] = 0.5, **kwargs):
+    def __init__(self, in_channels: int, out_channels: int,
+                 alpha: Optional[float] = 0.5, **kwargs):
         super().__init__(**kwargs)
 
         self.in_channels = in_channels
         self.out_channels = out_channels
 
-        self.conv_src_to_dst = SAGEConv(in_channels, out_channels, flow="source_to_target", root_weight=False, bias=False)
-        self.conv_dst_to_src = SAGEConv(in_channels, out_channels, flow="target_to_source", root_weight=False, bias=False)
+        self.conv_src_to_dst = SAGEConv(in_channels, out_channels,
+                                        flow="source_to_target",
+                                        root_weight=False, bias=False)
+        self.conv_dst_to_src = SAGEConv(in_channels, out_channels,
+                                        flow="target_to_source",
+                                        root_weight=False, bias=False)
         self.lin_self = Linear(in_channels, out_channels)
         self.alpha = alpha
 
@@ -29,8 +34,12 @@ class DirSageConv(torch.nn.Module):
 
     def forward(self, x: Tensor, edge_index: Adj) -> Tensor:
 
-        return (
-            self.lin_self(x)
-            + (1 - self.alpha) * self.conv_src_to_dst(x, edge_index)
-            + self.alpha * self.conv_dst_to_src(x, edge_index)
-        )
+        return (self.lin_self(x) +
+                (1 - self.alpha) * self.conv_src_to_dst(x, edge_index) +
+                self.alpha * self.conv_dst_to_src(x, edge_index))
+
+    def __repr__(self) -> str:
+        if hasattr(self, 'in_channels') and hasattr(self, 'out_channels'):
+            return (f'{self.__class__.__name__}({self.in_channels}, '
+                    f'{self.out_channels})')
+        return f'{self.__class__.__name__}()'
