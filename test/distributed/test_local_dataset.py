89e2e7783db7fa248feacb5c68af75869e795b0a
ZhengHongming888 <hongming.zheng@intel.com>
2023-05-31 20:20:45 -0700

    correct test

--- test/distributed/test_local_dataset.py
+++ test/distributed/test_local_dataset.py
@@ -6,15 +6,9 @@ from tqdm import tqdm
 
 import torch_geometric.transforms as T
 from torch_geometric.datasets import OGB_MAG
-<<<<<<< HEAD
 from torch_geometric.nn import Linear, HGTConv
 
 from torch_geometric.distributed import LocalDataset
-=======
-from torch_geometric.distributed import Data, LocalDataset
-from torch_geometric.nn import HGTConv, Linear
-
->>>>>>> ab70838ab1f9c37f26f3f7042413fd336de24e60
 
 def test_local_dataset():
 
@@ -22,47 +16,52 @@ def test_local_dataset():
     transform = T.ToUndirected(merge=True)
     dataset = OGB_MAG(path, preprocess='metapath2vec', transform=transform)
     data = dataset[0]
-
+  
     # init edge_index and node_features.
     edge_dict, feature_dict, edge_ids_dict, node_ids_dict = {}, {}, {}, {}
     for etype in data.edge_types:
-        edge_dict[etype] = data[etype]['edge_index']
-        edge_ids_dict[etype] = torch.tensor([1, 2, 3, 5, 7, 4])
-
+      edge_dict[etype] = data[etype]['edge_index']
+      edge_ids_dict[etype] = torch.tensor([1, 2, 3, 5, 7, 4])
+    
     for ntype in data.node_types:
-        feature_dict[ntype] = data[ntype].x.clone(
-            memory_format=torch.contiguous_format)
-        node_ids_dict[ntype] = torch.tensor([1, 2, 3, 5, 8, 4])
-
+      feature_dict[ntype] = data[ntype].x.clone(memory_format=torch.contiguous_format)
+      node_ids_dict[ntype] = torch.tensor([1, 2, 3, 5, 8, 4])
+  
     # define local_dataset and init_graph() and init_node_features()
     pyg_dataset = LocalDataset()
-    pyg_dataset.init_graph(edge_index=edge_dict, edge_ids=edge_ids_dict)
-    pyg_dataset.init_node_features(node_feature_data=feature_dict,
-                                   ids=node_ids_dict)
+    pyg_dataset.init_graph(
+      edge_index=edge_dict,
+      edge_ids=edge_ids_dict
+    )
+    pyg_dataset.init_node_features(
+      node_feature_data=feature_dict,
+      ids= node_ids_dict
+    )
 
     # get the graph and node_features from the graphstore/featurestore in LocalDataset
-    edge_attrs = pyg_dataset.graph.get_all_edge_attrs()
+    edge_attrs=pyg_dataset.graph.get_all_edge_attrs()
 
-    edge_index = {}
-    edge_ids = {}
+    edge_index={}
+    edge_ids={}
     for item in edge_attrs:
         edge_index[item.edge_type] = pyg_dataset.graph.get_edge_index(item)
         edge_ids[item.edge_type] = pyg_dataset.graph.get_edge_id(item)
 
-    #verify the edge_index and edge_ids
-    assert (edge_index == edge_dict)
-    assert (edge_ids == edge_ids_dict)
 
+    #verify the edge_index and edge_ids
+    assert (edge_index==edge_dict)
+    assert (edge_ids==edge_ids_dict)
+    
     tensor_attrs = pyg_dataset.node_features.get_all_tensor_attrs()
-    node_feat = {}
-    node_ids = {}
-    node_id2index = {}
+    node_feat={}
+    node_ids={}
+    node_id2index={}
     for item in tensor_attrs:
-        node_feat[item.attr_name] = pyg_dataset.node_features.get_tensor(
-            item.group_name, item.attr_name)
-        node_ids[item.attr_name] = pyg_dataset.node_features.get_global_id(
-            item.group_name, item.attr_name)
+        node_feat[item.attr_name] = pyg_dataset.node_features.get_tensor(item.group_name, item.attr_name)
+        node_ids[item.attr_name] = pyg_dataset.node_features.get_global_id(item.group_name, item.attr_name)
 
     # verfiy the node features and node ids.
-    assert (node_feat == feature_dict)
-    assert (node_ids == node_ids_dict)
+    assert (node_feat==feature_dict)
+    assert (node_ids==node_ids_dict)
+
+
