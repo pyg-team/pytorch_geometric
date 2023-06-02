6cbec084832f8f7eba78d5668166fefd187024cb
Akihiro Nitta <nitta@akihironitta.com>
2023-06-02 15:47:44 +0100

    Update test

--- test/nn/test_encoding.py
+++ test/nn/test_encoding.py
@@ -28,27 +28,27 @@ def test_temporal_encoding(device):
 
 @withCUDA
 def test_link_encoding(device):
-    K = 3
+    num_edges_per_node = 3
     num_edge_features = 5
     hidden_channels = 7
     out_channels = 11
     time_channels = 13
+
     encoder = LinkEncoding(
-        K=K,
+        K=num_edges_per_node,
         num_edge_features=num_edge_features,
         hidden_channels=hidden_channels,
         out_channels=out_channels,
         time_channels=time_channels,
     ).to(device)
-    assert str(encoder) == (f'LinkEncoding(K={K}, '
+    assert str(encoder) == (f'LinkEncoding(K={num_edges_per_node}, '
                             f'num_edge_features={num_edge_features}, '
                             f'hidden_channels={hidden_channels}, '
                             f'out_channels={out_channels}, '
                             f'time_channels={time_channels})')
 
     num_nodes = 17
-    num_edges = 19
+    num_edges = num_nodes * num_edges_per_node
     edge_attr = torch.rand((num_edges, num_edge_features), device=device)
     edge_time = torch.rand((num_edges, ), device=device)
-    assert encoder(edge_attr, edge_time,
-                   num_nodes).size() == (num_nodes, out_channels)
+    assert encoder(edge_attr, edge_time).size() == (num_nodes, out_channels)
