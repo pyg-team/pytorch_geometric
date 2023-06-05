532961a2423bba70a251cf8b9a0d46da2c426f1f
rusty1s <matthias.fey@tu-dortmund.de>
2023-06-05 19:06:32 +0000

    fix empty

--- torch_geometric/utils/trim_to_layer.py
+++ torch_geometric/utils/trim_to_layer.py
@@ -53,6 +53,11 @@ def trim_to_layer(
             k: trim_feat(v, layer, num_sampled_nodes_per_hop[k])
             for k, v in x.items()
         }
+        print('=============================')
+        print('=============================')
+        print('= before trim adj============================')
+        for k, v in edge_index.items():
+            print(k, v)
         edge_index = {
             k: trim_adj(
                 v,
@@ -151,6 +156,8 @@ def trim_adj(
         )
 
     elif isinstance(edge_index, SparseTensor):
+        print("TRIM ADJ")
+        print(edge_index)
         size = (
             edge_index.size(0) - num_sampled_dst_nodes_per_hop[-layer],
             edge_index.size(1) - num_sampled_src_nodes_per_hop[-layer],
@@ -164,7 +171,7 @@ def trim_adj(
 
 
 def trim_sparse_tensor(src: SparseTensor, size: Tuple[int, int],
-                       num_seed_nodes: None) -> SparseTensor:
+                       num_seed_nodes: int) -> SparseTensor:
     r"""Trims a :class:`SparseTensor` along both dimensions to only contain
     the upper :obj:`num_nodes` in both dimensions.
 
@@ -178,19 +185,69 @@ def trim_sparse_tensor(src: SparseTensor, size: Tuple[int, int],
         num_seed_nodes (int): The number of seed nodes to compute
             representations.
     """
+    print('-trim_sparse_tensor--------------')
+    out = src[:size[0], :size[1]]
+    rowptr1, col1, value1 = out.csr()
+    row2, col2, value2 = out.coo()
+    print(row2)
+    return SparseTensor.from_storage(out.storage)
+    return SparseTensor(
+        rowptr=rowptr1,
+        col=col1,
+        value=value1,
+        sparse_sizes=size,
+    )
+    # torch.cuda.synchronize()
+    # print(src.sizes(), src.nnz(), size, num_seed_nodes)
+    # print(src.storage._row.size())
+    # print(src.storage._rowptr.size())
+    # print(src.storage._col.size())
+    # print(src.storage._row.cpu())
+    # print(src.storage._rowptr.cpu())
+    # print(src.storage._col.cpu())
     rowptr, col, value = src.csr()
+    print(rowptr)
+    print(col)
+    print(value)
 
     rowptr = torch.narrow(rowptr, 0, 0, size[0] + 1).clone()
     rowptr[num_seed_nodes + 1:] = rowptr[num_seed_nodes]
 
-    col = torch.narrow(col, 0, 0, rowptr[-1])
+    print(rowptr)
+
+    col = torch.narrow(col, 0, 0, int(rowptr[-1]))
+
+    print(col)
 
     if value is not None:
         value = torch.narrow(value, 0, 0, rowptr[-1])
 
-    csr2csc = src.storage._csr2csc
-    if csr2csc is not None:
-        csr2csc = csr2csc[csr2csc < len(col)]
+    print(value)
+
+    # csr2csc = src.storage._csr2csc
+    # if csr2csc is not None:
+    #     csr2csc = csr2csc[csr2csc < len(col)]
+
+    print('rowptr', rowptr)
+    print('rowptr_orig', rowptr1)
+    print('col', col)
+    print('col_orig', col1)
+    print('size', size)
+    if rowptr.numel() > 0:
+        assert rowptr.numel() - 1 == size[0]
+        assert rowptr.max() == col.numel()
+    if col.numel() > 0:
+        assert col.min() >= 0 and col.max() < size[1]
+    if col1.numel() > 0:
+        assert col1.min() >= 0 and col1.max() < size[1]
+
+    print(bla)
+    print('-------end--------')
+    return bla
+
+    return out
+
+    # return src
 
     storage = SparseStorage(
         row=None,
@@ -201,9 +258,11 @@ def trim_sparse_tensor(src: SparseTensor, size: Tuple[int, int],
         rowcount=None,
         colptr=None,
         colcount=None,
-        csr2csc=csr2csc,
+        # csr2csc=csr2csc,
         csc2csr=None,
-        is_sorted=True,
-        trust_data=True,
+        # is_sorted=True,
+        # trust_data=True,
     )
-    return src.from_storage(storage)
+    out = src.from_storage(storage)
+    print(out)
+    return out
