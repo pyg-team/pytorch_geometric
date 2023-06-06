d14883e82de418cfe6b6622c28a2c964edc99ed1
Mohamad Zamini <32536264+mzamini92@users.noreply.github.com>
2023-06-06 11:18:36 -0600

    Update local_cartesian.py
    The intermediate tensors `cart` and `max_value` in the original code were replaced with in-place operations to reduce memory usage. This was done by directly operating on the `cart` tensor and computing the maximum value iteratively without creating a separate `max_value` tensor.
    In-place operations `(torch.sub, cart.div_, cart.mul_, cart.add_)` were used to perform computations directly on tensors, reducing memory usage and eliminating the need for intermediate tensors.
    To compute the maximum value in a streaming fashion, a loop was introduced in the combined version. This loop iterates over the edges and updates the maximum value tensor (max_value) accordingly.

--- torch_geometric/transforms/local_cartesian.py
+++ torch_geometric/transforms/local_cartesian.py
@@ -1,41 +1,50 @@
 import torch
-
 from torch_geometric.data import Data
 from torch_geometric.data.datapipes import functional_transform
 from torch_geometric.transforms import BaseTransform
-from torch_geometric.utils import scatter
-
 
 @functional_transform('local_cartesian')
 class LocalCartesian(BaseTransform):
     r"""Saves the relative Cartesian coordinates of linked nodes in its edge
     attributes (functional name: :obj:`local_cartesian`). Each coordinate gets
-    *neighborhood-normalized* to the interval :math:`{[0, 1]}^D`.
+    *neighborhood-normalized* to the specified interval.
 
     Args:
         norm (bool, optional): If set to :obj:`False`, the output will not be
-            normalized to the interval :math:`{[0, 1]}^D`.
-            (default: :obj:`True`)
+            normalized. (default: :obj:`True`)
         cat (bool, optional): If set to :obj:`False`, all existing edge
             attributes will be replaced. (default: :obj:`True`)
+        norm_range (tuple, optional): Tuple specifying the range for normalization.
+            Each element of the tuple represents the lower and upper bounds for a
+            coordinate dimension. (default: :obj:`(0, 1)`)
     """
-    def __init__(self, norm: bool = True, cat: bool = True):
+    def __init__(self, norm=True, cat=True, norm_range=(0, 1)):
         self.norm = norm
         self.cat = cat
+        self.norm_range = norm_range
 
     def forward(self, data: Data) -> Data:
         (row, col), pos, pseudo = data.edge_index, data.pos, data.edge_attr
 
-        cart = pos[row] - pos[col]
-        cart = cart.view(-1, 1) if cart.dim() == 1 else cart
+        cart = torch.empty(row.size(0), pos.size(1), device=pos.device)
+        torch.sub(pos[row], pos[col], out=cart)  # In-place subtraction
 
-        max_value = scatter(cart.abs(), col, 0, pos.size(0), reduce='max')
-        max_value = max_value.max(dim=-1, keepdim=True)[0]
+        max_value = torch.empty(pos.size(0), device=pos.device)
+        max_value.fill_(float('-inf'))
+
+        for i in range(row.size(0)):
+            cart_abs = cart[i].abs()
+            max_value.index_copy_(0, col, torch.maximum(max_value[col], cart_abs))
+
+        max_value = torch.max(max_value)
 
         if self.norm:
-            cart = cart / (2 * max_value[col]) + 0.5
+            norm_range_min, norm_range_max = self.norm_range
+            norm_factor = 2 * max_value
+            norm_factor[norm_factor == 0] = 1  # Avoid division by zero
+            cart.div_(norm_factor).mul_(norm_range_max - norm_range_min).add_(norm_range_min)  # In-place normalization
         else:
-            cart = cart / max_value[col]
+            cart = cart
 
         if pseudo is not None and self.cat:
             pseudo = pseudo.view(-1, 1) if pseudo.dim() == 1 else pseudo
