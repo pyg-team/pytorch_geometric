6d9a03ac3eb062b5b914c83720f8b92987f05ac8
Rishi Puri <puririshi98@berkeley.edu>
2023-06-12 07:03:03 -0700

    `dense_diff_pool` benchmark (#7550)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: rusty1s <matthias.fey@tu-dortmund.de>

--- test/nn/dense/test_diff_pool.py
+++ test/nn/dense/test_diff_pool.py
@@ -1,6 +1,9 @@
+from itertools import product
+
 import torch
 
 from torch_geometric.nn import dense_diff_pool
+from torch_geometric.profile import benchmark
 from torch_geometric.testing import is_full_test
 
 
@@ -24,3 +27,38 @@ def test_dense_diff_pool():
         assert torch.allclose(adj_jit, adj_out)
         assert link_loss.item() >= 0
         assert ent_loss.item() >= 0
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--device', type=str, default='cuda')
+    args = parser.parse_args()
+
+    BS = [2**i for i in range(4, 8)]
+    NS = [2**i for i in range(4, 8)]
+    FS = [2**i for i in range(5, 9)]
+    CS = [2**i for i in range(5, 9)]
+
+    funcs = []
+    func_names = []
+    args_list = []
+    for B, N, F, C in product(BS, NS, FS, CS):
+        x = torch.randn(B, N, F, device=args.device)
+        adj = torch.randint(0, 2, (B, N, N), dtype=x.dtype, device=args.device)
+        s = torch.randn(B, N, C, device=args.device)
+
+        funcs.append(dense_diff_pool)
+        func_names.append(f'B={B}, N={N}, F={F}, C={C}')
+        args_list.append((x, adj, s))
+
+    benchmark(
+        funcs=funcs,
+        func_names=func_names,
+        args=args_list,
+        num_steps=50 if args.device == 'cpu' else 500,
+        num_warmups=10 if args.device == 'cpu' else 100,
+        per_step=True,
+        progress_bar=True,
+    )
