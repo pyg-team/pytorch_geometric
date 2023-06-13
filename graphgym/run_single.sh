2487536c81658dec060a67921eff10c1c89c8e1c
rusty1s <matthias.fey@tu-dortmund.de>
2023-06-13 12:45:55 +0000

    update

--- graphgym/run_single.sh
+++ graphgym/run_single.sh
@@ -2,5 +2,5 @@
 
 # Test for running a single experiment. --repeat means run how many different random seeds.
 python main.py --cfg configs/pyg/example_node.yaml --repeat 3 # node classification
-python main.py --cfg configs/pyg/example_link.yaml --repeat 3 # link prediction
-python main.py --cfg configs/pyg/example_graph.yaml --repeat 3 # graph classification
+# python main.py --cfg configs/pyg/example_link.yaml --repeat 3 # link prediction
+# python main.py --cfg configs/pyg/example_graph.yaml --repeat 3 # graph classification
