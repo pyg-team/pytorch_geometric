41493b3649dfd46b994b3b4dc02771e64a858018
Matthias Fey <matthias.fey@tu-dortmund.de>
2023-06-18 09:53:48 +0200

    Fix TorchScript support in `TopKPooling` (#7600)

--- torch_geometric/nn/pool/connect/filter_edges.py
+++ torch_geometric/nn/pool/connect/filter_edges.py
@@ -3,10 +3,9 @@ from typing import Optional, Tuple
 import torch
 from torch import Tensor
 
+from torch_geometric.nn.pool.connect import Connect, ConnectOutput
 from torch_geometric.nn.pool.select import SelectOutput
 from torch_geometric.utils.num_nodes import maybe_num_nodes
-
-from .base import Connect, ConnectOutput
 
 
 def filter_adj(
@@ -16,10 +15,13 @@ def filter_adj(
     cluster_index: Optional[Tensor] = None,
     num_nodes: Optional[int] = None,
 ) -> Tuple[Tensor, Optional[Tensor]]:
+
     num_nodes = maybe_num_nodes(edge_index, num_nodes)
+
     if cluster_index is None:
-        cluster_index = torch.arange(node_index.size(0), dtype=torch.long,
+        cluster_index = torch.arange(node_index.size(0),
                                      device=node_index.device)
+
     mask = node_index.new_full((num_nodes, ), -1)
     mask[node_index] = cluster_index
 
@@ -35,13 +37,13 @@ def filter_adj(
 
 
 class FilterEdges(Connect):
-    r"""Filter out edges if their incident nodes are not in any cluster.
-    It is assumed that each cluster contains only one node.
+    r"""Filters out edges if their incident nodes are not in any cluster
 
     .. math::
-            \mathbf{A}^{\prime} &= \mathbf{A}_{\mathbf{i},\mathbf{i}}
+            \mathbf{A}^{\prime} &= \mathbf{A}_{\mathbf{i},\mathbf{i}},
 
-    Where :math:`\mathbf{i}` are retained nodes.
+    where :math:`\mathbf{i}` denotes the set of retained nodes.
+    It is assumed that each cluster contains only one node.
     """
     def forward(
         self,
@@ -50,12 +52,19 @@ class FilterEdges(Connect):
         edge_attr: Optional[Tensor] = None,
         batch: Optional[Tensor] = None,
     ) -> ConnectOutput:
-        if select_output.num_clusters != select_output.cluster_index.size(0):
-            raise ValueError("'FilterEdges' requires each cluster to contain "
-                             "only one node.")
-        edge_index, edge_attr = filter_adj(edge_index, edge_attr,
-                                           select_output.node_index,
-                                           select_output.cluster_index,
-                                           num_nodes=select_output.num_nodes)
+
+        if (not torch.jit.is_scripting() and select_output.num_clusters !=
+                select_output.cluster_index.size(0)):
+            raise ValueError(f"'{self.__class__.__name__}' requires each "
+                             f"cluster to contain only one node")
+
+        edge_index, edge_attr = filter_adj(
+            edge_index,
+            edge_attr,
+            select_output.node_index,
+            select_output.cluster_index,
+            num_nodes=select_output.num_nodes,
+        )
         batch = self.get_pooled_batch(select_output, batch)
+
         return ConnectOutput(edge_index, edge_attr, batch)
