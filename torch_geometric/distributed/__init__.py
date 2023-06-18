489e0b7c7a5695942828543e4ca8d9a3df91656e
ZhengHongming888 <hongming.zheng@intel.com>
2023-06-18 14:21:53 -0700

    __init__.py

--- torch_geometric/distributed/__init__.py
+++ torch_geometric/distributed/__init__.py
@@ -1,9 +1,11 @@
 from .local_feature_store import LocalFeatureStore
 from .local_graph_store import LocalGraphStore
 from .partition import Partitioner
+from .dist_dataset import DistDataset
 
 __all__ = classes = [
     'LocalFeatureStore',
     'LocalGraphStore',
     'Partitioner',
+    'DistDataset',
 ]
