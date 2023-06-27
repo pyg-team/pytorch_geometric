65a9c1ec68a4ec319e550e93f43cb2534e9249cb
rusty1s <matthias.fey@tu-dortmund.de>
2023-06-27 23:41:42 +0200

    update

--- torch_geometric/__init__.py
+++ torch_geometric/__init__.py
@@ -1,23 +1,20 @@
-import torch_geometric.data
-import torch_geometric.datasets
-import torch_geometric.explain
-import torch_geometric.loader
-import torch_geometric.nn
-import torch_geometric.profile
-import torch_geometric.sampler
-import torch_geometric.transforms
 import torch_geometric.utils
+import torch_geometric.data
+import torch_geometric.sampler
+import torch_geometric.loader
+import torch_geometric.transforms
+import torch_geometric.datasets
+import torch_geometric.nn
+import torch_geometric.explain
+import torch_geometric.profile
 
-from .compile import compile
-from .debug import debug, is_debug_enabled, set_debug
-from .experimental import (
-    experimental_mode,
-    is_experimental_mode_enabled,
-    set_experimental_mode,
-)
-from .home import get_home_dir, set_home_dir
-from .lazy_loader import LazyLoader
 from .seed import seed_everything
+from .home import get_home_dir, set_home_dir
+from .compile import compile
+from .debug import is_debug_enabled, debug, set_debug
+from .experimental import (is_experimental_mode_enabled, experimental_mode,
+                           set_experimental_mode)
+from .lazy_loader import LazyLoader
 
 contrib = LazyLoader('contrib', globals(), 'torch_geometric.contrib')
 graphgym = LazyLoader('graphgym', globals(), 'torch_geometric.graphgym')
