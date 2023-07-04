f44191691065a08fbede31fb7841f814715b3a23
Matthias Fey <matthias.fey@tu-dortmund.de>
2023-07-05 06:47:49 +0700

    Update torch_geometric/graphgym/utils/agg_runs.py

--- torch_geometric/graphgym/utils/agg_runs.py
+++ torch_geometric/graphgym/utils/agg_runs.py
@@ -131,7 +131,7 @@ def agg_runs(dir, metric_best='auto'):
                         results_best[split] = [stats_best]
                     else:
                         results_best[split] += [stats_best]
-    results = {k: v for k, v in results.items() if v is not None}  # rm None
+    results = {k: v for k, v in results.items() if v is not None}
     results_best = {k: v for k, v in results_best.items() if v is not None}
     for key in results:
         for i in range(len(results[key])):
