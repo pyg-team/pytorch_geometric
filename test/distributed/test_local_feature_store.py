ceafe396cdbbd66a874b6ae42c6177079047355e
rusty1s <matthias.fey@tu-dortmund.de>
2023-07-06 17:58:04 +0700

    update

--- test/distributed/test_local_feature_store.py
+++ test/distributed/test_local_feature_store.py
@@ -6,16 +6,16 @@ from torch_geometric.distributed import LocalFeatureStore
 def test_local_feature_store_global_id():
     store = LocalFeatureStore()
 
-    feat = torch.Tensor([
-        [0, 0, 0],
-        [1, 1, 1],
-        [2, 2, 2],
-        [3, 3, 3],
-        [4, 4, 4],
-        [5, 5, 5],
-        [6, 6, 6],
-        [7, 7, 7],
-        [8, 8, 8],
+    feat = torch.tensor([
+        [0.0, 0.0, 0.0],
+        [1.0, 1.0, 1.0],
+        [2.0, 2.0, 2.0],
+        [3.0, 3.0, 3.0],
+        [4.0, 4.0, 4.0],
+        [5.0, 5.0, 5.0],
+        [6.0, 6.0, 6.0],
+        [7.0, 7.0, 7.0],
+        [8.0, 8.0, 8.0],
     ])
 
     paper_global_id = torch.tensor([1, 2, 3, 5, 8, 4])
@@ -26,22 +26,22 @@ def test_local_feature_store_global_id():
 
     out = store.get_tensor_from_global_id(group_name='paper', attr_name='feat',
                                           index=torch.tensor([3, 8, 4]))
-    assert torch.equal(out, feat[torch.tensor([3, 8, 4])])
+    assert torch.equal(out, feat[torch.tensor([3.0, 8.0, 4.0])])
 
 
 def test_local_feature_store_utils():
     store = LocalFeatureStore()
 
-    feat = torch.Tensor([
-        [0, 0, 0],
-        [1, 1, 1],
-        [2, 2, 2],
-        [3, 3, 3],
-        [4, 4, 4],
-        [5, 5, 5],
-        [6, 6, 6],
-        [7, 7, 7],
-        [8, 8, 8],
+    feat = torch.tensor([
+        [0.0, 0.0, 0.0],
+        [1.0, 1.0, 1.0],
+        [2.0, 2.0, 2.0],
+        [3.0, 3.0, 3.0],
+        [4.0, 4.0, 4.0],
+        [5.0, 5.0, 5.0],
+        [6.0, 6.0, 6.0],
+        [7.0, 7.0, 7.0],
+        [8.0, 8.0, 8.0],
     ])
 
     paper_global_id = torch.tensor([1, 2, 3, 5, 8, 4])
--- test/utils/test_geodesic.py
+++ test/utils/test_geodesic.py
@@ -10,17 +10,22 @@ from torch_geometric.utils import geodesic_distance
 @withPackage('gdist')
 @pytest.mark.skip(reason="No way of currently testing this")
 def test_geodesic_distance():
-    pos = torch.Tensor([[0, 0, 0], [2, 0, 0], [0, 2, 0], [2, 2, 0]])
+    pos = torch.tensor([
+        [0.0, 0.0, 0.0],
+        [2.0, 0.0, 0.0],
+        [0.0, 2.0, 0.0],
+        [2.0, 2.0, 0.0],
+    ])
     face = torch.tensor([[0, 1, 3], [0, 2, 3]]).t()
 
     out = geodesic_distance(pos, face)
-    expected = [
-        [0, 1, 1, sqrt(2)],
-        [1, 0, sqrt(2), 1],
-        [1, sqrt(2), 0, 1],
-        [sqrt(2), 1, 1, 0],
-    ]
-    assert torch.allclose(out, torch.tensor(expected))
+    expected = torch.tensor([
+        [0.0, 1.0, 1.0, sqrt(2)],
+        [1.0, 0.0, sqrt(2), 1.0],
+        [1.0, sqrt(2), 0.0, 1.0],
+        [sqrt(2), 1.0, 1.0, 0.0],
+    ])
+    assert torch.allclose(out, expected)
     assert torch.allclose(out, geodesic_distance(pos, face, num_workers=-1))
 
     out = geodesic_distance(pos, face, norm=False)
@@ -30,14 +35,14 @@ def test_geodesic_distance():
         [2, 2 * sqrt(2), 0, 2],
         [2 * sqrt(2), 2, 2, 0],
     ]
-    assert torch.allclose(out, torch.tensor(expected))
+    assert torch.allclose(out, expected)
 
     src = torch.tensor([0, 0, 0, 0])
     dest = torch.tensor([0, 1, 2, 3])
     out = geodesic_distance(pos, face, src=src, dest=dest)
-    expected = [0, 1, 1, sqrt(2)]
-    assert torch.allclose(out, torch.tensor(expected))
+    expected = torch.tensor([0.0, 1.0, 1.0, sqrt(2)])
+    assert torch.allclose(out, expected)
 
     out = geodesic_distance(pos, face, dest=dest)
-    expected = [0, 0, 0, 0]
-    assert torch.allclose(out, torch.Tensor(expected))
+    expected = torch.tensor([0.0, 0.0, 0.0, 0.0])
+    assert torch.allclose(out, expected)
