2f924fbc69903b914b790a1f4e7978533e99399a
rusty1s <matthias.fey@tu-dortmund.de>
2023-07-06 18:00:44 +0700

    update

--- test/transforms/test_random_jitter.py
+++ test/transforms/test_random_jitter.py
@@ -7,23 +7,23 @@ from torch_geometric.transforms import RandomJitter
 def test_random_jitter():
     assert str(RandomJitter(0.1)) == 'RandomJitter(0.1)'
 
-    pos = torch.Tensor([[0, 0], [0, 0], [0, 0], [0, 0]])
+    pos = torch.tensor([[0.0, 0.0], [0.0, 0.0], [0.0, 0.0], [0.0, 0.0]])
 
     data = Data(pos=pos)
     data = RandomJitter(0)(data)
     assert len(data) == 1
-    assert data.pos.tolist() == pos.tolist()
+    assert torch.allclose(data.pos, pos)
 
     data = Data(pos=pos)
     data = RandomJitter(0.1)(data)
     assert len(data) == 1
-    assert data.pos.min().item() >= -0.1
-    assert data.pos.max().item() <= 0.1
+    assert data.pos.min() >= -0.1
+    assert data.pos.max() <= 0.1
 
     data = Data(pos=pos)
     data = RandomJitter([0.1, 1])(data)
     assert len(data) == 1
-    assert data.pos[:, 0].min().item() >= -0.1
-    assert data.pos[:, 0].max().item() <= 0.1
-    assert data.pos[:, 1].min().item() >= -1
-    assert data.pos[:, 1].max().item() <= 1
+    assert data.pos[:, 0].min() >= -0.1
+    assert data.pos[:, 0].max() <= 0.1
+    assert data.pos[:, 1].min() >= -1
+    assert data.pos[:, 1].max() <= 1
