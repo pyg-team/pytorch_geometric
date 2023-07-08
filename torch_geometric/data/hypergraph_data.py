089b032404d7d98e3fa5780e9f62b8e4f234eaa9
CodeTalker <743238614@qq.com>
2023-07-08 19:30:03 +0800

    Add Comments for New Methods

--- torch_geometric/data/hypergraph_data.py
+++ torch_geometric/data/hypergraph_data.py
@@ -11,25 +11,102 @@ from torch_geometric.utils import hyper_subgraph, select
 
 
 class HyperGraphData(Data):
+    r"""A data object describing a hypergraph.
+    The data object can hold node-level, link-level and graph-level attributes.
+    This object differs from a standard data object by having hyperedges,
+    i.e. edges that connect more than two nodes
+
+    .. code-block:: python
+
+        from torch_geometric.data import HyperGraphData
+
+        x = torch.randn(4, 8)
+        edge_index = torch.tensor([
+                [0, 1, 0, 2, 1, 0, 1, 2, 3],
+                [0, 0, 1, 1, 1, 2, 2, 2, 2]
+            ])
+        data = HyperGraphData(x=x, edge_index=edge_index, ...)
+
+    Args:
+        x (torch.Tensor, optional): Node feature matrix with shape
+            :obj:`[num_nodes, num_node_features]`. (default: :obj:`None`)
+        edge_index (LongTensor, optional): Hyperedge list
+            with shape :obj:`[2, num_nodes*num_edges_per_node]`.
+            (default: :obj:`None`)
+        edge_attr (torch.Tensor, optional): Edge feature matrix with shape
+            :obj:`[num_edges, num_edge_features]`. (default: :obj:`None`)
+        y (torch.Tensor, optional): Graph-level or node-level ground-truth
+            labels with arbitrary shape. (default: :obj:`None`)
+        pos (torch.Tensor, optional): Node position matrix with shape
+            :obj:`[num_nodes, num_dimensions]`. (default: :obj:`None`)
+        **kwargs (optional): Additional attributes.
+    """
     def __init__(self, x: OptTensor = None, edge_index: OptTensor = None,
                  edge_attr: OptTensor = None, y: OptTensor = None,
                  pos: OptTensor = None, **kwargs):
         super().__init__(x=x, edge_index=edge_index, edge_attr=edge_attr, y=y,
-                         pos=pos)
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+                         pos=pos, **kwargs)
+
+    @property
+    def num_edges(self) -> int:
+        r"""Returns the number of hyperedges in the hypergraph.
+        """
+        return max(self.edge_index[1]) + 1
 
     def __inc__(self, key: str, value: Any, *args, **kwargs) -> Any:
-        if 'batch' in key:
-            return int(value.max()) + 1
-        elif key == 'face':
-            return self.num_nodes
-        elif key == 'edge_index':
+        r"""Returns the incremental count to cumulatively increase the value
+        :obj:`value` of the attribute :obj:`key` when creating mini-batches
+        using :class:`torch_geometric.loader.DataLoader`.
+
+        Examples:
+            >>> x = torch.randn(3, 16)
+            >>> edge_index = torch.tensor([
+            ...     [0, 1, 0, 2, 1],
+            ...     [0, 0, 1, 1, 1]
+            >>> ])
+            >>> data = HyperGraphData(x = x, edge_index = edge_index)
+            >>> data_list = [data, data]
+            >>> loader = Dataloader(data_list, batch_size = 2)
+            >>> batch = next(iter(loader))
+
+            >>> batch.edge_index
+            tensor([[0, 1, 0, 2, 1, 3, 4, 3, 5, 4],
+            [0, 0, 1, 1, 1, 2, 2, 3, 3, 3]])
+        """
+
+        if key == 'edge_index':
             return torch.tensor([[self.num_nodes], [self.num_edges]])
         else:
-            return 0
+            return super().__inc__(key, value, *args, **kwargs)
 
     def subgraph(self, subset: Tensor) -> 'HyperGraphData':
+        r"""Returns the induced subgraph given by the node indices
+        :obj:`subset`.
+
+        .. note::
+
+            If only a subset of a hyperedge's connected nodes are to be
+            selected in the subgraph, the hypergraph will remain in the
+            subgraph, but only the selected nodes will be connected by
+            the hyperedge. Hyperedges that only connects one node in the
+            subgraph will be removed.
+
+        Examples:
+            >>> x = torch.randn(4, 16)
+            >>> edge_index = torch.tensor([
+            ...     [0, 1, 0, 2, 1, 1, 2, 4],
+            ...     [0, 0, 1, 1, 1, 2, 2, 2]
+            >>> ])
+            >>> data = HyperGraphData(x = x, edge_index = edge_index)
+            >>> subset = torch.tensor([1, 2, 4])
+            >>> subgraph = data.subgraph(subset)
+            >>> subgraph.edge_index
+            tensor([[2, 1, 1, 2, 4],
+            [0, 0, 1, 1, 1]])
+
+        Args:
+            subset (LongTensor or BoolTensor): The nodes to keep.
+        """
         out = hyper_subgraph(subset, self.edge_index, relabel_nodes=True,
                              num_nodes=self.num_nodes, return_edge_mask=True)
         edge_index, _, edge_mask = out
--- torch_geometric/utils/hyperedge.py
+++ torch_geometric/utils/hyperedge.py
@@ -2,6 +2,31 @@ import torch
 
 
 def remove_single_hyperedge(edge_mask, edge_index):
+    r"""Takes a Hypergraph's edge_index, and remove the edges that only
+     connect one node. Used when computing a subgraph.
+
+     Examples:
+        >>> edge_index = torch.tensor([
+        ...     [0, 1, 0, 2, 1, 1, 2, 3],
+        ...     [0, 0, 1, 1, 1, 2, 2, 2]
+        >>> ])
+        >>> edge_mask = torch.tensor([
+        ... False, True, True, True, False, True, True, True
+        ... ])
+        >>> edge_mask, edge_index = remove_single_hyperedge(
+        ... edge_mask, edge_index
+        ... )
+        >>> edge_index
+        tensor([[0, 2, 1, 2, 3],
+        [1, 1, 2, 2, 2]])
+
+    .. note::
+
+        This method is for internal use only.
+    Args:
+        edge_mask (BoolTensor): The edges to keep.
+        edge_index (LongTensor or BoolTensor): The original hyperedges.
+    """
     _edge_index = edge_index[:, edge_mask]
     unique_elements, counts = torch.unique(_edge_index[1], return_counts=True)
     _edge_index = [[], []]
