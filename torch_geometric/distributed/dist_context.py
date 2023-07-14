047a249b305c8d248044383f576d4d6747da3df4
ZhengHongming888 <hongming.zheng@intel.com>
2023-07-13 22:33:02 -0700

    minor for dist_context/rpc

--- torch_geometric/distributed/dist_context.py
+++ torch_geometric/distributed/dist_context.py
@@ -1,3 +1,4 @@
+
 from enum import Enum
 from typing import Optional
 
@@ -8,32 +9,21 @@ class DistRole(Enum):
 
 class DistContext(object):
     r""" Context info for distributed Info in the current process."""
-    def __init__(self, role: DistRole, rank: int, group_name: str,
-                 world_size: int, global_world_size: int, global_rank: int):
-        self.role = role
-        self.rank = rank
-        self.group_name = group_name
-        self.world_size = world_size
-        self.global_world_size = global_world_size
-        self.global_rank = global_rank
-
+    def __init__(self,
+                 rank: int,
+                 global_rank: int,
+                 world_size: int,
+                 global_world_size: int,
+                 group_name: str,
+                 role: DistRole = DistRole.WORKER):
+      self.rank = rank
+      self.global_rank = global_rank
+      self.world_size = world_size
+      self.global_world_size = global_world_size
+      self.group_name = group_name
+      self.role = role
+    
     @property
     def worker_name(self) -> str:
         return f"{self.group_name}-{self.rank}"
 
-
-_dist_context: DistContext = None
-
-
-def get_context() -> DistContext:
-    return _dist_context
-
-
-def init_worker_group(world_size: int, rank: int,
-                      group_name: Optional[str] = None):
-    global _dist_context
-    _dist_context = DistContext(
-        role=DistRole.WORKER, world_size=world_size, rank=rank,
-        group_name=(group_name
-                    if group_name is not None else '_default_worker'),
-        global_world_size=world_size, global_rank=rank)
