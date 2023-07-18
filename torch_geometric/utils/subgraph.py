59352ea3e5c2a4cf13239b86a6686d536dce4728
Rishi Puri <puririshi98@berkeley.edu>
2023-07-18 15:15:06 -0700

    Update subgraph.py

--- torch_geometric/utils/subgraph.py
+++ torch_geometric/utils/subgraph.py
@@ -189,6 +189,7 @@ def bipartite_subgraph(
                 0) > 10**9 and edge_index.is_cuda:
             # if creating zeros for node idxs could cause GPU OOM, use CUDF
             import cudf
+            import cupy
             graph = cudf.DataFrame({
                 "u":
                 cupy.asarray(edge_index[0].reshape(-1)),
