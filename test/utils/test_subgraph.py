d5a0c8ae28481fb10d23102c97053c4ad334d8d3
Rishi Puri <puririshi98@berkeley.edu>
2023-07-18 15:03:42 -0700

    adding test

--- test/utils/test_subgraph.py
+++ test/utils/test_subgraph.py
@@ -72,6 +72,24 @@ def test_bipartite_subgraph():
         assert out[1].tolist() == [3.0, 4.0, 9.0, 10.0]
 
 
+def test_bipartite_subgraph_large_cudf():
+    edge_index = torch.tensor([[0, 5, 2, 3, 3, 4, 4, 3, 5, 5, 6],
+                               [0, 0, 3, 2, 0, 0, 2, 1, 2, 3, 1]])
+    num_nodes = 10**9
+    edge_index = torch.cat(
+        (edge_index, torch.randint(low=7, high=num_nodes, size=(2, 10**5))),
+        dim=-1).to(device)
+    idx = (torch.tensor([2, 3, 5]), torch.tensor([2, 3]))
+    mask = (index_to_mask(idx[0], 7), index_to_mask(idx[1], 4))
+    indices = (idx[0].tolist(), idx[1].tolist())
+    mixed = (mask[0], idx[1])
+
+    for subset in [idx, mask, indices, mixed]:
+        out = bipartite_subgraph(subset, edge_index,
+                                 relabel_nodes=True)
+        assert out[0].tolist() == [[0, 1, 2, 2], [1, 0, 0, 1]]
+
+
 def test_k_hop_subgraph():
     edge_index = torch.tensor([
         [0, 1, 2, 3, 4, 5],
