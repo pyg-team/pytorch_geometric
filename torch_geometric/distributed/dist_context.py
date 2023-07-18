f2dc85c14a975242510cd5d879a01a39a2086c8a
ZhengHongming888 <hongming.zheng@intel.com>
2023-07-18 15:51:47 -0700

    modify minor

--- torch_geometric/distributed/dist_context.py
+++ torch_geometric/distributed/dist_context.py
@@ -1,23 +1,30 @@
+
 from enum import Enum
 from typing import Optional
 
+from dataclasses import dataclass
 
 class DistRole(Enum):
     WORKER = 1  # for worker mode
 
-
-class DistContext(object):
+@dataclass
+class DistContext:
     r""" Context info for distributed Info in the current process."""
-    def __init__(self, rank: int, global_rank: int, world_size: int,
-                 global_world_size: int, group_name: str,
+    def __init__(self,
+                 rank: int,
+                 global_rank: int,
+                 world_size: int,
+                 global_world_size: int,
+                 group_name: str,
                  role: DistRole = DistRole.WORKER):
-        self.rank = rank
-        self.global_rank = global_rank
-        self.world_size = world_size
-        self.global_world_size = global_world_size
-        self.group_name = group_name
-        self.role = role
-
+      self.rank = rank
+      self.global_rank = global_rank
+      self.world_size = world_size
+      self.global_world_size = global_world_size
+      self.group_name = group_name
+      self.role = role
+    
     @property
     def worker_name(self) -> str:
         return f"{self.group_name}-{self.rank}"
+
