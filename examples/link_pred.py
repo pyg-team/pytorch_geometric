ad7e2044b6529e648dfaa74d6821c24000d07608
Matthias Fey <matthias.fey@tu-dortmund.de>
2023-07-19 13:33:59 +0200

    Update examples/link_pred.py

--- examples/link_pred.py
+++ examples/link_pred.py
@@ -14,6 +14,7 @@ elif hasattr(torch.backends, 'mps') and torch.backends.mps.is_available():
     device = torch.device('mps')
 else:
     device = torch.device('cpu')
+
 transform = T.Compose([
     T.NormalizeFeatures(),
     T.ToDevice(device),
