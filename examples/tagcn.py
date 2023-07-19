aa00b7fff045da2a03d595a018fdd408a7469504
Matthias Fey <matthias.fey@tu-dortmund.de>
2023-07-19 13:33:19 +0200

    Update examples/tagcn.py

--- examples/tagcn.py
+++ examples/tagcn.py
@@ -33,6 +33,7 @@ elif hasattr(torch.backends, 'mps') and torch.backends.mps.is_available():
     device = torch.device('mps')
 else:
     device = torch.device('cpu')
+
 model, data = Net().to(device), data.to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
 
