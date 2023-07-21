a71fcca49955cac8670e68d4627b4f096fbf9dd1
rusty1s <matthias.fey@tu-dortmund.de>
2023-07-21 09:37:10 +0200

    update

--- examples/explain/gnn_explainer_link_pred.py
+++ examples/explain/gnn_explainer_link_pred.py
@@ -9,14 +9,15 @@ from torch_geometric.datasets import Planetoid
 from torch_geometric.explain import Explainer, GNNExplainer, ModelConfig
 from torch_geometric.nn import GCNConv
 
-dataset = 'Cora'
-path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'Planetoid')
 if torch.cuda.is_available():
     device = torch.device('cuda')
 elif hasattr(torch.backends, 'mps') and torch.backends.mps.is_available():
     device = torch.device('mps')
 else:
     device = torch.device('cpu')
+
+dataset = 'Cora'
+path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'Planetoid')
 transform = T.Compose([
     T.NormalizeFeatures(),
     T.ToDevice(device),
