407618a364e7aca7fd103dd6bf29445bdd5b9769
Akihiro Nitta <nitta@akihironitta.com>
2023-07-27 14:37:08 +0000

    Update repro

--- test_temporal_link_neighbor_loader.py
+++ test_temporal_link_neighbor_loader.py
@@ -2,46 +2,60 @@ import torch
 from torch_geometric.data import Data
 from torch_geometric.loader import LinkNeighborLoader
 
-
 data = Data(
     x=torch.randn(10, 5),
-    edge_index=torch.randint(0, 10, (2, 123)),
-    edge_time=torch.arange(123, dtype=torch.long),
-    edge_label=torch.ones(123),
+    edge_index=torch.randint(0, 10, (2, 1234)),
+    edge_time=torch.arange(1234, dtype=torch.long),
+    edge_label=torch.ones(1234),
 )
-
 
 loader = LinkNeighborLoader(
     data,
-    num_neighbors=[2],
-    edge_label=torch.ones(data.num_edges),
+    num_neighbors=[10],
+    edge_label=data.edge_label,
     time_attr="edge_time",
     edge_label_time=data.edge_time,
-    batch_size=5,
+    batch_size=1,
     shuffle=True,
-    disjoint=False,
 )
 
 for sample in loader:
-    edge_index = sample.edge_index
-    edge_time = sample.edge_time
-
-    edge_label_index = sample.edge_label_index
-    edge_label_time = sample.edge_label_time
-    edge_label = sample.edge_label
-
-    print("===input===")
-    print(edge_index)
-    print(edge_time)
-
-    print("===target===")
-    print(edge_label_index)
-    print(edge_label_time)
-    print(edge_label)
-
-    print("===asdf===")
-    # src/dst share the same timestamp
-    edge_label_time2 = edge_label_time.repeat(2)
-    print(edge_time - edge_label_time2[edge_index[1]])
-    breakpoint()
+    print(sample)
+    # print(sample.edge_time <= sample.edge_label_time)
+    # assert torch.all(sample.edge_time <= sample.edge_label_time)
+    print(sample.input_id)
     break
+
+
+import torch
+from torch_geometric.data import Data
+from torch_geometric.loader import NeighborLoader
+
+data = Data(
+    x=torch.randn(10, 5),
+    node_time=torch.arange(10, dtype=torch.long),
+    edge_index=torch.randint(0, 10, (2, 1234)),
+    edge_label=torch.ones(1234),
+)
+
+loader = NeighborLoader(
+    data,
+    num_neighbors=[10, 10],
+    time_attr="node_time",
+    input_time=data.node_time,
+    batch_size=1,
+    shuffle=True,
+)
+
+for sample in loader:
+    print(sample)
+    print(sample.input_id)
+    # print(sample.node_time)
+    # print(sample.seed_time)
+    # assert torch.all(sample.node_time <= sample.node_label_time)
+    break
+
+"""
+Data(x=[18, 5], edge_index=[2, 20], edge_time=[20], edge_label=[1],  n_id=[18], e_id=[20], batch=[18], num_sampled_nodes=[2], num_sampled_edges=[1], input_id=[1], edge_label_index=[2, 1], edge_label_time=[1])
+Data(x=[9, 5],  edge_index=[2, 60], node_time=[9],  edge_label=[60], n_id=[9],  e_id=[60], batch=[9],  num_sampled_nodes=[3], num_sampled_edges=[2], input_id=[1], seed_time=[1], batch_size=1)
+"""
