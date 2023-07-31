e96711d79bfd6cd656bb8ef464519c41ccbf93ba
rusty1s <matthias.fey@tu-dortmund.de>
2023-07-31 19:09:59 +0000

    update

--- torch_geometric/datasets/ose_gvcs.py
+++ torch_geometric/datasets/ose_gvcs.py
@@ -25,7 +25,7 @@ class OSE_GVCS(InMemoryDataset):
 
     The dataset contains a heterogenous graphs with 50 :obj:`machine` nodes,
     composing the GVCS, and 290 directed edges, each representing one out of
-    three relationships pairs of machines.
+    three relationships of machines.
     """
     machines = [
         '3D Printer', '3D Scanner', 'Aluminum Extractor', 'Backhoe',
