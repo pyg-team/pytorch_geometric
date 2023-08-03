1086681fee28d1e5019d19e83b85effe2a4d7e93
rusty1s <matthias.fey@tu-dortmund.de>
2023-08-03 11:35:26 +0000

    update

--- test/utils/test_subgraph.py
+++ test/utils/test_subgraph.py
@@ -1,7 +1,7 @@
 import torch
 
 from torch_geometric.nn import GCNConv, Linear
-from torch_geometric.testing import withCUDA
+from torch_geometric.testing import withCUDA, withPackage
 from torch_geometric.utils import (
     bipartite_subgraph,
     get_num_hops,
@@ -74,6 +74,7 @@ def test_bipartite_subgraph():
 
 
 @withCUDA
+@withPackage('pandas')
 def test_bipartite_subgraph_large_index(device):
     subset = torch.tensor([50_000_000], device=device)
     edge_index = torch.tensor([[50_000_000], [50_000_000]], device=device)
