1d1583a6559a6781787d8f9945441d1fbbc7ce26
Rishi Puri <puririshi98@berkeley.edu>
2023-08-03 04:51:56 -0700

    Utilize `cudf` for `relabel_nodes` pathway in `utils.subgraph` (#7764)
    ```
    cd /opt/pyg; pip uninstall -y torch-geometric; rm -rf pytorch_geometric; git clone -b cudf-relabel https://github.com/pyg-team/pytorch_geometric.git; cd /opt/pyg/pytorch_geometric; pip install .; py.test -s /opt/pyg/pytorch_geometric/test/utils/test_subgraph.py -v
    test/utils/test_subgraph.py::test_get_num_hops PASSED
    test/utils/test_subgraph.py::test_subgraph PASSED
    test/utils/test_subgraph.py::test_subgraph_large_cudf PASSED
    test/utils/test_subgraph.py::test_bipartite_subgraph PASSED
    test/utils/test_subgraph.py::test_k_hop_subgraph PASSED
    
    ==================================================================================== 5 passed
    ```
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: rusty1s <matthias.fey@tu-dortmund.de>

--- torch_geometric/utils/subgraph.py
+++ torch_geometric/utils/subgraph.py
@@ -94,16 +94,20 @@ def subgraph(
     else:
         num_nodes = subset.size(0)
         node_mask = subset
+        subset = node_mask.nonzero().view(-1)
 
     edge_mask = node_mask[edge_index[0]] & node_mask[edge_index[1]]
     edge_index = edge_index[:, edge_mask]
     edge_attr = edge_attr[edge_mask] if edge_attr is not None else None
 
     if relabel_nodes:
-        node_idx = torch.zeros(node_mask.size(0), dtype=torch.long,
-                               device=device)
-        node_idx[subset] = torch.arange(node_mask.sum().item(), device=device)
-        edge_index = node_idx[edge_index]
+        edge_index, _ = map_index(
+            edge_index.view(-1),
+            subset,
+            max_index=num_nodes,
+            inclusive=True,
+        )
+        edge_index = edge_index.view(2, -1)
 
     if return_edge_mask:
         return edge_index, edge_attr, edge_mask
