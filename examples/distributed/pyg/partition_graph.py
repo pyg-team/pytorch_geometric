4e4f5c4c98cc467a53af01d6735f0e366a008b80
rusty1s <matthias.fey@tu-dortmund.de>
2023-08-04 17:52:23 +0200

    update

--- examples/distributed/pyg/partition_graph.py
+++ examples/distributed/pyg/partition_graph.py
@@ -8,14 +8,12 @@ from ogb.nodeproppred import PygNodePropPredDataset
 from torch_geometric.distributed import Partitioner
 
 
-def partition_dataset(ogbn_dataset: str, root_dir: str, num_partitions: int):
-    save_dir = root_dir + f'/{ogbn_dataset}-' + "partitions"
-    download_dir = f'{ogbn_dataset}'
-    dataset = PygNodePropPredDataset(ogbn_dataset, download_dir)
+def partition_dataset(ogbn_dataset: str, root_dir: str, num_parts: int):
+    save_dir = osp.join(root_dir, f'{ogbn_dataset}-partitions')
+    dataset = PygNodePropPredDataset(ogbn_dataset)
     data = dataset[0]
 
-    partitioner = Partitioner(root=save_dir, num_parts=num_partitions,
-                              data=data)
+    partitioner = Partitioner(data, num_parts, save_dir)
     partitioner.generate_partition()
     split_idx = dataset.get_idx_split()
 
@@ -24,51 +22,28 @@ def partition_dataset(ogbn_dataset: str, root_dir: str, num_partitions: int):
     os.makedirs(label_dir, exist_ok=True)
     torch.save(data.y.squeeze(), osp.join(label_dir, 'label.pt'))
 
-    print('-- Partitioning training idx ...')
+    print('-- Partitioning training indices ...')
     train_idx = split_idx['train']
-    train_idx = train_idx.split(train_idx.size(0) // num_partitions)
-    train_idx_partitions_dir = osp.join(root_dir,
-                                        f'{ogbn_dataset}-train-partitions')
-    os.makedirs(train_idx_partitions_dir, exist_ok=True)
-    for pidx in range(num_partitions):
-        torch.save(train_idx[pidx],
-                   osp.join(train_idx_partitions_dir, f'partition{pidx}.pt'))
+    train_idx = train_idx.split(train_idx.size(0) // num_parts)
+    train_part_dir = osp.join(root_dir, f'{ogbn_dataset}-train-partitions')
+    os.makedirs(train_part_dir, exist_ok=True)
+    for i in range(num_parts):
+        torch.save(train_idx[i], osp.join(train_part_dir, f'partition{i}.pt'))
 
-    print('-- Partitioning test idx ...')
+    print('-- Partitioning test indices ...')
     test_idx = split_idx['test']
-    test_idx = test_idx.split(test_idx.size(0) // num_partitions)
-    test_idx_partitions_dir = osp.join(root_dir,
-                                       f'{ogbn_dataset}-test-partitions')
-    os.makedirs(test_idx_partitions_dir, exist_ok=True)
-    for pidx in range(num_partitions):
-        torch.save(test_idx[pidx],
-                   osp.join(test_idx_partitions_dir, f'partition{pidx}.pt'))
+    test_idx = test_idx.split(test_idx.size(0) // num_parts)
+    test_part_dir = osp.join(root_dir, f'{ogbn_dataset}-test-partitions')
+    os.makedirs(test_part_dir, exist_ok=True)
+    for i in range(num_parts):
+        torch.save(test_idx[i], osp.join(test_part_dir, f'partition{i}.pt'))
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description="Arguments for ClusterData Partitioning.")
-    parser.add_argument(
-        "--dataset",
-        type=str,
-        default='ogbn-products',
-        help="The name of dataset.",
-    )
-    parser.add_argument(
-        "--root_dir",
-        type=str,
-        default='./data/products',
-        help="The root directory of input dataset and output partitions.",
-    )
-    parser.add_argument(
-        "--num_partitions",
-        type=int,
-        default=2,
-        help="Number of partitions",
-    )
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', type=str, default='ogbn-products')
+    parser.add_argument('--root_dir', type=str, default='./data/products')
+    parser.add_argument('--num_partitions', type=int, default=2)
     args = parser.parse_args()
 
-    partition_dataset(
-        ogbn_dataset=args.dataset,
-        root_dir=osp.join(osp.dirname(osp.realpath(__file__)),
-                          args.root_dir), num_partitions=args.num_partitions)
+    partition_dataset(args.dataset, args.root_dir, args.num_partitions)
--- examples/distributed/pyg/partition_hetero_graph.py
+++ examples/distributed/pyg/partition_hetero_graph.py
@@ -8,68 +8,41 @@ from torch_geometric.datasets import OGB_MAG
 from torch_geometric.distributed import Partitioner
 
 
-def partition_dataset(ogbn_dataset: str, root_dir: str, num_partitions: int):
-    save_dir = root_dir + f'/{ogbn_dataset}-' + "partitions"
+def partition_dataset(ogbn_dataset: str, root_dir: str, num_parts: int):
+    save_dir = osp.join(root_dir, f'{ogbn_dataset}-partitions')
     dataset = OGB_MAG(root=ogbn_dataset, preprocess='metapath2vec')
     data = dataset[0]
 
-    partitioner = Partitioner(root=save_dir, num_parts=num_partitions,
-                              data=data)
+    partitioner = Partitioner(data, num_parts, save_dir)
     partitioner.generate_partition()
-    # split_idx = data.get_idx_split()
-    n_nodes = data['paper'].x.shape[0]
-    n_train = int(n_nodes * 0.6)
-    n_val = int(n_nodes * 0.2)
 
     print('-- Saving label ...')
     label_dir = osp.join(root_dir, f'{ogbn_dataset}-label')
     os.makedirs(label_dir, exist_ok=True)
     torch.save(data['paper'].y.squeeze(), osp.join(label_dir, 'label.pt'))
-    print('-- Partitioning training idx ...')
-    train_idx = torch.arange(0, n_train)
-    train_idx = train_idx.split(train_idx.size(0) // num_partitions)
-    train_idx_partitions_dir = osp.join(root_dir,
-                                        f'{ogbn_dataset}-train-partitions')
-    os.makedirs(train_idx_partitions_dir, exist_ok=True)
-    for pidx in range(num_partitions):
-        torch.save(train_idx[pidx],
-                   osp.join(train_idx_partitions_dir, f'partition{pidx}.pt'))
 
-    print('-- Partitioning test idx ...')
-    test_idx = torch.arange(n_train + n_val, n_nodes)
-    test_idx = test_idx.split(test_idx.size(0) // num_partitions)
-    test_idx_partitions_dir = osp.join(root_dir,
-                                       f'{ogbn_dataset}-test-partitions')
-    os.makedirs(test_idx_partitions_dir, exist_ok=True)
-    for pidx in range(num_partitions):
-        torch.save(test_idx[pidx],
-                   osp.join(test_idx_partitions_dir, f'partition{pidx}.pt'))
+    print('-- Partitioning training indices ...')
+    train_idx = data['paper'].train_mask.nonzero().view(-1)
+    train_idx = train_idx.split(train_idx.size(0) // num_parts)
+    train_part_dir = osp.join(root_dir, f'{ogbn_dataset}-train-partitions')
+    os.makedirs(train_part_dir, exist_ok=True)
+    for i in range(num_parts):
+        torch.save(train_idx[i], osp.join(train_part_dir, f'partition{i}.pt'))
+
+    print('-- Partitioning test indices ...')
+    test_idx = data['paper'].test_mask.nonzero().view(-1)
+    test_idx = test_idx.split(test_idx.size(0) // num_parts)
+    test_part_dir = osp.join(root_dir, f'{ogbn_dataset}-test-partitions')
+    os.makedirs(test_part_dir, exist_ok=True)
+    for i in range(num_parts):
+        torch.save(test_idx[i], osp.join(test_part_dir, f'partition{i}.pt'))
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description="Arguments for ClusterData Partitioning.")
-    parser.add_argument(
-        "--dataset",
-        type=str,
-        default='ogbn-mags',
-        help="The name of dataset.",
-    )
-    parser.add_argument(
-        "--root_dir",
-        type=str,
-        default='./data/mags',
-        help="The root directory of input dataset and output partitions.",
-    )
-    parser.add_argument(
-        "--num_partitions",
-        type=int,
-        default=2,
-        help="Number of partitions",
-    )
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', type=str, default='ogbn-mag')
+    parser.add_argument('--root_dir', type=str, default='./data/mag')
+    parser.add_argument('--num_partitions', type=int, default=2)
     args = parser.parse_args()
 
-    partition_dataset(
-        ogbn_dataset=args.dataset,
-        root_dir=osp.join(osp.dirname(osp.realpath(__file__)),
-                          args.root_dir), num_partitions=args.num_partitions)
+    partition_dataset(args.dataset, args.root_dir, args.num_partitions)
