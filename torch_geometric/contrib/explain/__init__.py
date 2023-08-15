47ad5f09d86723b07fbc629287c0193cfa15ee43
Akihiro Nitta <nitta@akihironitta.com>
2023-08-15 10:16:57 +0000

    Remove contrib

--- torch_geometric/contrib/explain/__init__.py
+++ torch_geometric/contrib/explain/__init__.py
@@ -1,5 +1,12 @@
-from .graphmask_explainer import GraphMaskExplainer
 from .pgm_explainer import PGMExplainer
+from torch_geometric.explain.algorithm.graphmask_explainer import GraphMaskExplainer as NewGraphMaskExplainer
+from torch_geometric.deprecation import deprecated
+
+
+GraphMaskExplainer = deprecated(
+    details="use 'torch_geometric.explain.algorithm.GraphMaskExplainer' instead",
+)
+
 
 __all__ = classes = [
     'GraphMaskExplainer',
