24ae6fce303eb508928d17d35ac36bcafed475dc
rusty1s <matthias.fey@tu-dortmund.de>
2023-08-23 15:28:51 +0000

    update

--- torch_geometric/loader/prefetch.py
+++ torch_geometric/loader/prefetch.py
@@ -1,3 +1,4 @@
+import warnings
 from contextlib import nullcontext
 from functools import partial
 from typing import Any, Optional
@@ -6,6 +7,45 @@ import torch
 from torch.utils.data import DataLoader
 
 from torch_geometric.typing import WITH_IPEX
+
+
+class DeviceHelper:
+    def __init__(self, device: Optional[torch.device] = None):
+        with_cuda = torch.cuda.is_available()
+        with_xpu = torch.xpu.is_available() if WITH_IPEX else False
+
+        if device is None:
+            if with_cuda:
+                device = 'cuda'
+            elif with_xpu:
+                device = 'xpu'
+            else:
+                device = 'cpu'
+
+        self.device = torch.device(device)
+        self.is_gpu = self.device.type in ['cuda', 'xpu']
+
+        if ((self.device.type == 'cuda' and not with_cuda)
+                or (self.device.type == 'xpu' and not with_xpu)):
+            warnings.warn(f"Requested device '{self.device.type}' is not "
+                          f"available, falling back to CPU")
+            self.device = torch.device('cpu')
+
+        self.stream = None
+        self.stream_context = nullcontext
+        self.module = getattr(torch, self.device.type) if self.is_gpu else None
+
+    def maybe_init_stream(self) -> None:
+        if self.is_gpu:
+            self.stream = self.module.Stream()
+            self.stream_context = partial(
+                self.module.stream,
+                stream=self.stream,
+            )
+
+    def maybe_wait_stream(self) -> None:
+        if self.stream is not None:
+            self.module.current_stream().wait_stream(self.stream)
 
 
 class PrefetchLoader:
@@ -17,83 +57,32 @@ class PrefetchLoader:
         device (torch.device, optional): The device to load the data to.
             (default: :obj:`None`)
     """
-    class PrefetchLoaderDevice:
-        def __init__(self, device: Optional[torch.device] = None):
-            cuda_present = torch.cuda.is_available()
-            xpu_present = torch.xpu.is_available() if WITH_IPEX else False
-
-            if device is None:
-                if cuda_present:
-                    device = 'cuda'
-                elif xpu_present:
-                    device = 'xpu'
-                else:
-                    device = 'cpu'
-
-            self.device = torch.device(device)
-
-            if ((self.device.type == 'cuda' and not cuda_present)
-                    or (self.device.type == 'xpu' and not xpu_present)):
-                print(f'Requested device[{self.device.type}] is not available '
-                      '- fallback to CPU')
-                self.device = torch.device('cpu')
-
-            self.is_gpu = self.device.type in ['cuda', 'xpu']
-            self.stream = None
-            self.stream_context = nullcontext
-
-            gpu_module = None
-            if self.is_gpu:
-                if self.device.type == 'cuda':
-                    gpu_module = torch.cuda
-                else:
-                    gpu_module = torch.xpu
-
-            self.gpu_module = gpu_module
-
-        def maybe_init_stream(self) -> None:
-            if self.is_gpu:
-                self.stream = self.gpu_module.Stream()
-                self.stream_context = partial(self.gpu_module.stream,
-                                              stream=self.stream)
-
-        def maybe_wait_stream(self) -> None:
-            if self.stream is not None:
-                self.gpu_module.current_stream().wait_stream(self.stream)
-
-        def get_device(self) -> torch.device:
-            return self.device
-
-        def get_stream_context(self) -> Any:
-            return self.stream_context()
-
     def __init__(
         self,
         loader: DataLoader,
         device: Optional[torch.device] = None,
     ):
         self.loader = loader
-        self.device_mgr = self.PrefetchLoaderDevice(device)
+        self.device_helper = self.DeviceHelper(device)
 
     def non_blocking_transfer(self, batch: Any) -> Any:
-        if not self.device_mgr.is_gpu:
+        if not self.device_helper.is_gpu:
             return batch
         if isinstance(batch, (list, tuple)):
             return [self.non_blocking_transfer(v) for v in batch]
         if isinstance(batch, dict):
             return {k: self.non_blocking_transfer(v) for k, v in batch.items()}
 
-        device = self.device_mgr.get_device()
-        batch = batch.pin_memory(device)
-        return batch.to(device, non_blocking=True)
+        batch = batch.pin_memory(self.device_helper.device)
+        return batch.to(self.device_helper.device, non_blocking=True)
 
     def __iter__(self) -> Any:
         first = True
-        self.device_mgr.maybe_init_stream()
+        self.device_helper.maybe_init_stream()
 
         for next_batch in self.loader:
 
-            with self.device_mgr.get_stream_context():
+            with self.device_helper.stream_context():
                 next_batch = self.non_blocking_transfer(next_batch)
 
             if not first:
@@ -101,7 +90,7 @@ class PrefetchLoader:
             else:
                 first = False
 
-            self.device_mgr.maybe_wait_stream()
+            self.device_helper.maybe_wait_stream()
 
             batch = next_batch
 
