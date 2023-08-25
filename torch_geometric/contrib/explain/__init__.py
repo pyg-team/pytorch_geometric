3de818f940731ad9927eb6055ec954426bb3fc12
Aniket Saxena <92912434+fork123aniket@users.noreply.github.com>
2023-08-25 21:39:59 +0530

    Moved `GraphMaskExplainer` to `torch_geometric.explain` (#7779)
    Co-authored-by: Akihiro Nitta <nitta@akihironitta.com>

--- torch_geometric/contrib/explain/__init__.py
+++ torch_geometric/contrib/explain/__init__.py
@@ -1,5 +1,11 @@
-from .graphmask_explainer import GraphMaskExplainer
 from .pgm_explainer import PGMExplainer
+from torch_geometric.explain.algorithm.graphmask_explainer import (
+    GraphMaskExplainer as NewGraphMaskExplainer)
+from torch_geometric.deprecation import deprecated
+
+GraphMaskExplainer = deprecated(
+    "use 'torch_geometric.explain.algorithm.GraphMaskExplainer' instead", )(
+        NewGraphMaskExplainer)
 
 __all__ = classes = [
     'GraphMaskExplainer',
