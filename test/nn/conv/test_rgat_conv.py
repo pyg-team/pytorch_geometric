ae47cd36ca3bee12ea0251d50c03a4adb156fbd5
xnuohz <xnuohz@126.com>
2023-08-27 20:25:56 +0800

    update

--- test/nn/conv/test_rgat_conv.py
+++ test/nn/conv/test_rgat_conv.py
@@ -14,27 +14,57 @@ from torch_geometric.utils import to_torch_coo_tensor
     'f-additive',
     'f-scaled',
 ])
-@pytest.mark.parametrize('attention_mechanism', [
-    'within-relation',
-    'across-relation',
-])
-@pytest.mark.parametrize('attention_mode', [
-    'additive-self-attention',
-    'multiplicative-self-attention',
-])
-def test_rgat_conv(mod, attention_mechanism, attention_mode):
+@pytest.mark.parametrize('attention_mechanism',
+                         ['within-relation', 'across-relation'])
+@pytest.mark.parametrize(
+    'attention_mode',
+    ['additive-self-attention', 'multiplicative-self-attention'])
+@pytest.mark.parametrize('concat', [False])
+@pytest.mark.parametrize('out_channels', [20])
+@pytest.mark.parametrize('heads', [2])
+@pytest.mark.parametrize('edge_dim', [8, None])
+def test_rgat_conv(mod, attention_mechanism, attention_mode, concat,
+                   out_channels, heads, edge_dim):
     x = torch.randn(4, 8)
     edge_index = torch.tensor([[0, 1, 2, 3], [0, 0, 1, 1]])
     edge_type = torch.tensor([0, 2, 1, 2])
-    edge_attr = torch.randn((4, 8))
+    edge_attr = torch.randn((4, edge_dim)) if edge_dim else None
 
-    conv = RGATConv(8, 20, num_relations=4, num_bases=4, mod=mod,
-                    attention_mechanism=attention_mechanism,
-                    attention_mode=attention_mode, heads=2, dim=1, edge_dim=8)
-    assert str(conv) == 'RGATConv(8, 20, heads=2)'
+    # num_base is not None
+    conv0 = RGATConv(8, out_channels, num_relations=4, num_bases=4, mod=mod,
+                     attention_mechanism=attention_mechanism,
+                     attention_mode=attention_mode, heads=heads, dim=1,
+                     concat=concat, edge_dim=edge_dim)
 
-    out = conv(x, edge_index, edge_type, edge_attr)
-    assert out.size() == (4, 40)
+    # num_blocks is not None
+    conv1 = RGATConv(8, out_channels, num_relations=4, num_blocks=4, mod=mod,
+                     attention_mechanism=attention_mechanism,
+                     attention_mode=attention_mode, heads=heads, dim=1,
+                     concat=concat, edge_dim=edge_dim)
+
+    # both num_bases and num_blocks are None
+    conv2 = RGATConv(8, out_channels, num_relations=4, mod=mod,
+                     attention_mechanism=attention_mechanism,
+                     attention_mode=attention_mode, heads=heads, dim=1,
+                     concat=concat, edge_dim=edge_dim)
+
+    # dropout > 0 and mod is None
+    conv3 = RGATConv(8, out_channels, num_relations=4, mod=None,
+                     attention_mechanism=attention_mechanism,
+                     attention_mode=attention_mode, heads=heads, dim=1,
+                     concat=concat, edge_dim=edge_dim, dropout=0.5)
+
+    for conv in [conv0, conv1, conv2, conv3]:
+        assert str(conv) == f'RGATConv(8, {out_channels}, heads={heads})'
+
+        out = conv(x, edge_index, edge_type, edge_attr)
+        assert out.size() == (4, out_channels * 1 * (heads if concat else 1))
+
+        out, (adj, alpha) = conv(x, edge_index, edge_type, edge_attr,
+                                 return_attention_weights=True)
+        assert out.size() == (4, out_channels * 1 * (heads if concat else 1))
+        assert adj.size() == edge_index.size()
+        assert alpha.size() == (4, heads)
 
 
 def test_rgat_conv_jittable():
