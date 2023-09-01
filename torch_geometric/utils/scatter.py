a3eb4a63cf41c7828392a05b8cea73fb5ac0a5ba
Akihiro Nitta <nitta@akihironitta.com>
2023-09-01 21:37:56 +0000

    Add torch_geometrics.warnings

--- torch_geometric/utils/scatter.py
+++ torch_geometric/utils/scatter.py
@@ -6,6 +6,7 @@ from torch import Tensor
 
 import torch_geometric.typing
 from torch_geometric.typing import torch_scatter
+from torch_geometric.warnings import warn
 
 if torch_geometric.typing.WITH_PT112:  # pragma: no cover
 
@@ -85,9 +86,9 @@ if torch_geometric.typing.WITH_PT112:  # pragma: no cover
                     or not src.is_cuda or not src.requires_grad):
 
                 if src.is_cuda and src.requires_grad:
-                    warnings.warn(f"The usage of `scatter(reduce='{reduce}')` "
-                                  f"can be accelerated via the 'torch-scatter'"
-                                  f" package, but it was not found")
+                    warn(f"The usage of `scatter(reduce='{reduce}')` "
+                         f"can be accelerated via the 'torch-scatter'"
+                         f" package, but it was not found")
 
                 index = broadcast(index, src, dim)
                 return src.new_zeros(size).scatter_reduce_(
---
+++ torch_geometric/warnings.py
@@ -0,0 +1,17 @@
+import warnings
+from typing import Any
+
+import torch_geometric
+
+if torch_geometric.typing.WITH_PT20:  # pragma: no cover
+    from torch._dynamo import is_compiling
+else:
+    def is_compiling():
+        return False
+
+
+def warn(*args: Any, **kwargs: Any) -> None:
+    if is_compiling():
+        return
+
+    warnings.warn(*args, **kwargs)
