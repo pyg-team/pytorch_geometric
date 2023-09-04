71672c8d7e71211ad3f0c1fef5aebd347de38d11
Harshit Verma <100012454+harshit5674@users.noreply.github.com>
2023-09-04 16:32:37 +0530

    Update test_neural_fingerprint.py

--- test/nn/models/test_neural_fingerprint.py
+++ test/nn/models/test_neural_fingerprint.py
@@ -9,5 +9,5 @@ def test_neural_fingerprint():
     edge_index = torch.tensor([[0, 1, 1, 2], [1, 0, 2, 1]])
     model = NeuralFingerprint(num_features=7, fingerprint_length=5,
                               num_layers=4)
-    fingerprint = model.forward(x, edge_index)
+    fingerprint = model(x, edge_index)
     assert fingerprint.size() == torch.Size([5])
