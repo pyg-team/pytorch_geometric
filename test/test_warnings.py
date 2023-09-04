f0b38228b328d15f9bff6d898f182e3abe1e5156
Akihiro Nitta <nitta@akihironitta.com>
2023-09-04 21:25:12 +0000

    Update test comment

--- test/test_warnings.py
+++ test/test_warnings.py
@@ -13,7 +13,7 @@ def test_warn():
 
 @patch("torch_geometric.warnings._is_compiling", return_value=True)
 def test_no_warn_if_compiling(_):
-    """Test no warning if we are compiling to avoid graph breaks"""
+    """Test no warning raised to avoid graph breaks when compiling"""
     with warnings.catch_warnings():
         warnings.simplefilter("error")
         warn('test')
