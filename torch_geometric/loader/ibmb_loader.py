60f31883d9837493a2fd822959a90e97f632cc03
rusty1s <matthias.fey@tu-dortmund.de>
2023-09-12 12:30:02 +0000

    update

--- torch_geometric/loader/ibmb_loader.py
+++ torch_geometric/loader/ibmb_loader.py
@@ -1,6 +1,6 @@
 import logging
 import math
-from typing import Callable, List, NamedTuple, Optional, Tuple, Union
+from typing import Callable, Iterator, List, NamedTuple, Optional, Tuple, Union
 
 import numpy as np
 import scipy.sparse
@@ -9,10 +9,6 @@ from torch import Tensor
 from tqdm import tqdm
 
 from torch_geometric.data import Data
-from torch_geometric.loader.ibmb_sampler import (
-    IBMBOrderedSampler,
-    IBMBWeightedSampler,
-)
 from torch_geometric.typing import SparseTensor
 from torch_geometric.utils import get_ppr, is_undirected, subgraph
 
@@ -853,3 +849,65 @@ class IBMBNodeLoader(IBMBBaseLoader):
                 adj,
                 self.return_edge_index_type,
             )
+
+
+class IBMBOrderedSampler(torch.utils.data.Sampler[int]):
+    r"""A sampler with given order, specially for IBMB loaders.
+
+    Args:
+        data_source (np.ndarray, torch.Tensor, List): A :obj:`np.ndarray`,
+            :obj:`torch.Tensor`, or :obj:`List` data object. Contains the
+            order of the batches.
+    """
+    def __init__(self, data_source: Union[np.ndarray, torch.Tensor,
+                                          List]) -> None:
+        self.data_source = data_source
+        super().__init__(data_source)
+
+    def __iter__(self) -> Iterator[int]:
+        return iter(self.data_source)
+
+    def __len__(self) -> int:
+        return len(self.data_source)
+
+
+class IBMBWeightedSampler(torch.utils.data.Sampler[int]):
+    r"""A weighted sampler wrt the pair wise KL divergence.
+    The very first batch after initialization is sampled randomly,
+    with the next ones being sampled according to the last batch,
+    including the first batch in the next round.
+
+    Args:
+        batch_kl_div (np.ndarray, torch.Tensor): A :obj:`np.ndarray` or
+            :obj:`torch.Tensor`, each element [i, j] contains the pair wise
+            KL divergence between batch i and j.
+    """
+    def __init__(self, batch_kl_div: Union[np.ndarray, torch.Tensor]) -> None:
+        data_source = np.arange(batch_kl_div.shape[0])
+        self.data_source = data_source
+        self.batch_kl_div = batch_kl_div
+        self.last_train_batch_id = 0
+        super().__init__(data_source)
+
+    def __iter__(self) -> Iterator[int]:
+        probs = self.batch_kl_div.copy()
+
+        last = self.last_train_batch_id
+        num_batches = probs.shape[0]
+
+        fetch_idx = []
+
+        next_id = 0
+        while np.any(probs):
+            next_id = np.random.choice(num_batches, size=None, replace=False,
+                                       p=probs[last] / probs[last].sum())
+            last = next_id
+            fetch_idx.append(next_id)
+            probs[:, next_id] = 0.
+
+        self.last_train_batch_id = next_id
+
+        return iter(fetch_idx)
+
+    def __len__(self) -> int:
+        return len(self.data_source)
