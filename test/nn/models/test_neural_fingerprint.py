abffe0059f8716205ca2250f403c3c94249db10d
rusty1s <matthias.fey@tu-dortmund.de>
2023-09-12 07:26:17 +0000

    update

--- test/nn/models/test_neural_fingerprint.py
+++ test/nn/models/test_neural_fingerprint.py
@@ -1,21 +1,28 @@
+import pytest
 import torch
 
 import torch_geometric.typing
 from torch_geometric.nn import NeuralFingerprint
+from torch_geometric.testing import is_full_test
 from torch_geometric.typing import SparseTensor
 
 
-def test_neural_fingerprint():
+@pytest.mark.parametrize('batch', [None, torch.tensor([0, 1, 1])])
+def test_neural_fingerprint(batch):
     x = torch.randn(3, 7)
     edge_index = torch.tensor([[0, 1, 1, 2], [1, 0, 2, 1]])
-    model = NeuralFingerprint(num_features=7, fingerprint_length=5,
-                              num_layers=4)
-    jit = torch.jit.export(model)
-    fingerprint = model(x, edge_index)
-    assert fingerprint.size() == torch.Size([5])
 
-    assert torch.allclose(fingerprint, jit(x, edge_index))
+    model = NeuralFingerprint(7, 16, out_channels=5, num_layers=4)
+    assert str(model) == 'NeuralFingerprint(7, 5, num_layers=4)'
+    model.reset_parameters()
+
+    out = model(x, edge_index, batch)
+    assert out.size() == (1, 5) if batch is None else (2, 5)
 
     if torch_geometric.typing.WITH_TORCH_SPARSE:
-        adj = SparseTensor.from_edge_index(edge_index)
-        assert torch.allclose(model(x, adj.t()), fingerprint)
+        adj = SparseTensor.from_edge_index(edge_index, sparse_sizes=(3, 3))
+        assert torch.allclose(model(x, adj.t(), batch), out)
+
+    if is_full_test():
+        jit = torch.jit.export(model)
+        assert torch.allclose(jit(x, edge_index, batch), out)
--- torch_geometric/nn/models/neural_fingerprint.py
+++ torch_geometric/nn/models/neural_fingerprint.py
@@ -1,42 +1,72 @@
-import torch
-import torch.nn.functional as F
+from typing import Optional
 
-from torch_geometric.nn import Linear, MFConv
+import torch
+from torch import Tensor
+
+from torch_geometric.nn import Linear, MFConv, global_add_pool
 from torch_geometric.typing import Adj
 
 
 class NeuralFingerprint(torch.nn.Module):
     r"""The Nueral Fingerprint model from the
     `"Convolutional Networks on Graphs for Learning Molecular Fingerprints"
-    <https://arxiv.org/pdf/1509.09292.pdf>`__ paper to generate fingerprints
+    <https://arxiv.org/abs/1509.09292>`__ paper to generate fingerprints
     of molecules.
 
     Args:
-        num_features (int): The number of features of each node/atom.
-        fingerprint_length (int): The length of fingerprint vector required.
-        num_layers (int): Number of layers in the model (Radius in the paper).
+        in_channels (int): Size of each input sample.
+        hidden_channels (int): Size of each hidden sample.
+        out_channels (int): Size of each output fingerprint.
+        num_layers (int): Number of layers.
+        **kwargs (optional): Additional arguments of
+            :class:`torch_geometric.nn.conv.MFConv`.
     """
-    def __init__(self, num_features: int, fingerprint_length: int,
-                 num_layers: int):
+    def __init__(
+        self,
+        in_channels: int,
+        hidden_channels: int,
+        out_channels: int,
+        num_layers: int,
+        **kwargs,
+    ):
         super().__init__()
-        self.num_features = num_features
-        self.fingerprint_length = fingerprint_length
+
+        self.in_channels = in_channels
+        self.hidden_channels = hidden_channels
+        self.out_channels = out_channels
         self.num_layers = num_layers
-        self.layers = torch.nn.ModuleList()
+
+        self.convs = torch.nn.ModuleList()
         for i in range(self.num_layers):
-            self.layers.append(
-                MFConv(in_channels=self.num_features,
-                       out_channels=self.num_features))
-            self.layers.append(
-                Linear(in_channels=self.num_features,
-                       out_channels=self.fingerprint_length))
+            in_channels = self.in_channels if i == 0 else self.hidden_channels
+            self.convs.append(MFConv(in_channels, hidden_channels, **kwargs))
 
-    def forward(self, x: torch.Tensor, edge_index: Adj) -> torch.Tensor:
+        self.lins = torch.nn.ModuleList()
+        for _ in range(self.num_layers):
+            self.lins.append(Linear(hidden_channels, out_channels, bias=False))
 
-        fingerprint = x.new_zeros(self.fingerprint_length)
-        for i in range(0, 2 * self.num_layers, 2):
-            x = self.layers[i](x, edge_index)
-            x = torch.sigmoid(x)
-            y = F.softmax(self.layers[i + 1](x), dim=1)
-            fingerprint += torch.sum(y, dim=0)
-        return fingerprint
+    def reset_parameters(self):
+        r"""Resets all learnable parameters of the module."""
+        for conv in self.convs:
+            conv.reset_parameters()
+        for lin in self.lins:
+            lin.reset_parameters()
+
+    def forward(
+        self,
+        x: Tensor,
+        edge_index: Adj,
+        batch: Optional[Tensor] = None,
+        batch_size: Optional[int] = None,
+    ) -> Tensor:
+        """"""
+        outs = []
+        for conv, lin in zip(self.convs, self.lins):
+            x = conv(x, edge_index).sigmoid()
+            y = lin(x).softmax(dim=-1)
+            outs.append(global_add_pool(y, batch, batch_size))
+        return sum(outs)
+
+    def __repr__(self) -> str:
+        return (f'{self.__class__.__name__}({self.in_channels}, '
+                f'{self.out_channels}, num_layers={self.num_layers})')
