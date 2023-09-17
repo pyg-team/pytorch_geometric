ac8264e60841976e0ece8d449b1019a35fdc5faa
Matthias Fey <matthias.fey@tu-dortmund.de>
2023-09-17 10:36:59 +0200

    `Database` Updates and Tests (#8044)

--- torch_geometric/data/database.py
+++ torch_geometric/data/database.py
@@ -1,123 +1,167 @@
-import abc
-from collections import namedtuple
-from dataclasses import dataclass
 import io
-import itertools
-import json
-from typing import Iterable, Generator, Optional, Any
+from abc import ABC, abstractmethod
+from typing import Any, Iterable, List, Optional, Union
 
-import sqlite3
 import torch
-
-from torch_geometric.data.data import Data
-from torch_geometric.typing import OptTensor
+from torch import Tensor
 
 
-class GraphLabel:
+class Database(ABC):
+    r"""Base class for database."""
+    def connect(self):
+        pass
 
-    def __init__(self, id: str, *args, **kwargs):
-        self.id = id
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+    def close(self):
+        pass
 
-@dataclass
-class GraphRow:
-    id: str
-    data: Optional[dict[str, Optional[bytes]]]
+    @abstractmethod
+    def insert(self, index: int, data: Any):
+        raise NotImplementedError
 
+    def multi_insert(
+        self,
+        indices: Union[Iterable[int], Tensor],
+        data_list: Iterable[Any],
+        batch_size: Optional[int] = None,
+    ):
+        if batch_size is None:
+            batch_size = min(len(indices), len(data_list))
 
-def chunk(seq: Iterable, chunk_size: int) -> Generator[list, Any, None]:
-    "chunk data into chunks of chunk_size"
-    it = iter(seq)
-    while True:
-        batch = list(itertools.islice(it, chunk_size))
-        if not batch:
-            return
-        yield batch
+        for start in range(0, min(len(indices), len(data_list)), batch_size):
+            self._multi_insert(
+                indices[start:start + batch_size],
+                data_list[start:start + batch_size],
+            )
 
+    def _multi_insert(
+        self,
+        indices: Union[Iterable[int], Tensor],
+        data_list: Iterable[Any],
+    ):
+        if isinstance(indices, Tensor):
+            indices = indices.tolist()
+        for index, data in zip(indices, data_list):
+            self.insert(index, data)
 
-def namedtuple_factory(cursor, row):
-    """util function to create a namedtuple Row foe db results"""
-    fields = [column[0] for column in cursor.description]
-    cls = namedtuple("Row", fields)
-    return cls._make(row)
+    @abstractmethod
+    def get(self, index: int) -> Any:
+        raise NotImplementedError
 
+    def multi_get(
+        self,
+        indices: Union[Iterable[int], Tensor],
+        batch_size: Optional[int] = None,
+    ) -> List[Any]:
+        if batch_size is None:
+            batch_size = len(indices)
 
-class Database(abc.ABC):
+        data_list: List[Any] = []
+        for start in range(0, len(indices), batch_size):
+            chunk_indices = indices[start:start + batch_size]
+            data_list.extend(self._multi_get(chunk_indices))
+        return data_list
 
-    def __init__(self, credentials, *args, **kwargs):
-        self.connection = self._get_connection(credentials)
+    def _multi_get(self, indices: Union[Iterable[int], Tensor]) -> List[Any]:
+        if isinstance(indices, Tensor):
+            indices = indices.tolist()
+        return [self.get(index) for index in indices]
 
-    @abc.abstractmethod
-    def _initialize(self):
-        """initialize the database in some way if needed"""
-        raise NotImplementedError()
-
-    @abc.abstractmethod
-    def insert(self, labels: Iterable[GraphLabel], values: Iterable[Data], batch_size=10000) -> list[str]:
-        """insert data into a database"""
-        raise NotImplementedError()
-
-    @abc.abstractmethod
-    def serialize_data(self, data: Data) -> GraphRow:
-        """serialize the data"""
-        raise NotImplementedError()
-
-    @abc.abstractmethod
-    def get(self, key: str) -> Data:
-        """get data by key"""
-        raise NotImplementedError()
-
-    def multi_get(self, keys: list[str]) -> list[Data]:
-        """get multiple keys"""
-        return [self.get(key) for key in keys]
-
-
-class SQLiteDatabase(abc.ABC):
-
-    def __init__(self, credentials, table='pyg_database') -> None:
-        self.table = table
-        super().__init__(credentials)
-
-    def _initialize(self):
-        create = """CREATE TABLE ? (id TEXT, data, TEXT)"""
-        self.cursor.execute(create, self.table)
-
-    def insert(self, labels: Iterable[GraphLabel], values: Iterable[Data], batch_size=10000) -> list[GraphRow]:
-        for chunk_data in chunk(zip(labels, values), batch_size):
-            serialized = [self.serialize_data(label, value) for label, value in chunk_data]
-            query = f"""
-            INSERT INTO {self.table} (id, data) 
-              VALUES (?, ?)"""
-            self.cursor.executemany(query, [(row['id'], json.dumps(row['data'])) for row in serialized])
-
-    def get(self, label: GraphLabel):
-        query = f"""SELECT * FROM {self.table} where id = ?"""
-        self.cursor.execute(query, (label.id))
-        return self.cursor.fetchone()
-    
-    def multi_get(self, labels: Iterable[GraphLabel], batch_size=999):
-        for chunk_data in chunk(labels, batch_size):
-            query = f"SELECT * FROM {self.table} WHERE id IN ({','.join('?' * len(chunk_data))})"
-            self.cursor.execute(query, (label.id for label in chunk_data))
-
-    def serialize_data(self, label: GraphLabel, data: Data) -> GraphRow:
-        row_dict = {k: self._serialize_tensor(v) if isinstance(v, OptTensor) else v for k, v in vars(data).items()}
-        return GraphRow(
-            **vars(label),
-            **row_dict
-        )
+    # Helper functions ########################################################
 
     @staticmethod
-    def _serialize_tensor(t: OptTensor) -> bytes:
-        """convert a tensor into bytes"""
-        buff = io.BytesIO()
-        torch.save(t, buff)
-        return buff.getvalue()
+    def serialize(data: Any) -> bytes:
+        r"""Serializes :obj:`data` into bytes."""
+        buffer = io.BytesIO()
+        torch.save(data, buffer)
+        return buffer.getvalue()
 
-    def _get_connection(self, credentials):
-        """a method to get the db cursor to executor SQL"""
-        con = sqlite3.connect(credentials)
-        cursor = con.cursor()
-        cursor.row_factory = namedtuple_factory
-        return cursor
+    @staticmethod
+    def deserialize(data: bytes) -> Any:
+        r"""Deserializes bytes into the original data."""
+        return torch.load(io.BytesIO(data))
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}()'
+
+
+class SQLiteDatabase(Database):
+    def __init__(self, path: str, name: str):
+        super().__init__()
+
+        import sqlite3
+
+        self.path = path
+        self.name = name
+
+        self._connection: Optional[sqlite3.Connection] = None
+        self._cursor: Optional[sqlite3.Cursor] = None
+
+        self.connect()
+
+        query = (f'CREATE TABLE IF NOT EXISTS {self.name} (\n'
+                 f'  id INTEGER PRIMARY KEY,\n'
+                 f'  data BLOB NOT NULL\n'
+                 f')')
+        self.cursor.execute(query)
+
+    def connect(self):
+        import sqlite3
+        self._connection = sqlite3.connect(self.path)
+        self._cursor = self._connection.cursor()
+
+    def close(self):
+        self._connection.commit()
+        self._connection.close()
+        self._connection = None
+        self._cursor = None
+
+    @property
+    def cursor(self) -> Any:
+        if self._cursor is None:
+            raise RuntimeError("No open database connection")
+        return self._cursor
+
+    def insert(self, index: int, data: Any):
+        query = f'INSERT INTO {self.name} (id, data) VALUES (?, ?)'
+        self.cursor.execute(query, (index, self.serialize(data)))
+
+    def _multi_insert(
+        self,
+        indices: Union[Iterable[int], Tensor],
+        data_list: Iterable[Any],
+    ):
+        if isinstance(indices, Tensor):
+            indices = indices.tolist()
+        data_list = [self.serialize(data) for data in data_list]
+
+        query = f'INSERT INTO {self.name} (id, data) VALUES (?, ?)'
+        self.cursor.executemany(query, zip(indices, data_list))
+
+    def get(self, index: int) -> Any:
+        query = f'SELECT data FROM {self.name} WHERE id = ?'
+        self.cursor.execute(query, (index, ))
+        return self.deserialize(self.cursor.fetchone()[0])
+
+    def multi_get(
+        self,
+        indices: Union[Iterable[int], Tensor],
+        batch_size: Optional[int] = None,
+    ) -> List[Any]:
+        if isinstance(indices, Tensor):
+            indices = indices.tolist()
+
+        query = (f'SELECT data FROM {self.name} '
+                 f'WHERE id IN ({", ".join("?" * len(indices))})')
+        self.cursor.execute(query, indices)
+
+        if batch_size is None:
+            data_list = self.cursor.fetchall()
+        else:
+            data_list: List[Any] = []
+            while True:
+                chunk_list = self.cursor.fetchmany(size=batch_size)
+                if len(chunk_list) == 0:
+                    break
+                data_list.extend(chunk_list)
+
+        return [self.deserialize(data[0]) for data in data_list]
