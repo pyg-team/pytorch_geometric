911c90c6b70e3329308630e6edb5c70402bf5eb5
Matthias Fey <matthias.fey@tu-dortmund.de>
2023-09-18 10:58:23 +0200

    `RocksDatabase` implementation (#8052)

--- test/data/test_database.py
+++ test/data/test_database.py
@@ -3,7 +3,7 @@ import os.path as osp
 import pytest
 import torch
 
-from torch_geometric.data.database import SQLiteDatabase
+from torch_geometric.data.database import RocksDatabase, SQLiteDatabase
 from torch_geometric.profile import benchmark
 from torch_geometric.testing import withPackage
 
@@ -25,6 +25,32 @@ def test_sqlite_database(tmp_path, batch_size):
     data_list = torch.randn(2, 5)
     db.multi_insert(indices, data_list, batch_size=batch_size)
     assert len(db) == 3
+
+    out_list = db.multi_get(indices, batch_size=batch_size)
+    assert isinstance(out_list, list)
+    assert len(out_list) == 2
+    assert torch.equal(out_list[0], data_list[0])
+    assert torch.equal(out_list[1], data_list[1])
+
+    db.close()
+
+
+@withPackage('rocksdict')
+@pytest.mark.parametrize('batch_size', [None, 1])
+def test_rocks_database(tmp_path, batch_size):
+    path = osp.join(tmp_path, 'rocks.db')
+    db = RocksDatabase(path)
+    assert str(db) == 'RocksDatabase()'
+    with pytest.raises(NotImplementedError):
+        len(db)
+
+    data = torch.randn(5)
+    db.insert(0, data)
+    assert torch.equal(db.get(0), data)
+
+    indices = torch.tensor([1, 2])
+    data_list = torch.randn(2, 5)
+    db.multi_insert(indices, data_list, batch_size=batch_size)
 
     out_list = db.multi_get(indices, batch_size=batch_size)
     assert isinstance(out_list, list)
@@ -72,14 +98,19 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     data = torch.randn(args.numel, 128)
-
     tmp_dir = tempfile.TemporaryDirectory()
-    path = osp.join(tmp_dir.name, 'sqlite.db')
-    db = SQLiteDatabase(path, name='test_table')
 
+    path = osp.join(tmp_dir.name, 'sqlite.db')
+    sqlite_db = SQLiteDatabase(path, name='test_table')
     t = time.perf_counter()
-    db.multi_insert(range(args.numel), data, batch_size=100, log=True)
-    print(f'Initialized DB in {time.perf_counter() - t:.2f} seconds')
+    sqlite_db.multi_insert(range(args.numel), data, batch_size=100, log=True)
+    print(f'Initialized SQLiteDB in {time.perf_counter() - t:.2f} seconds')
+
+    path = osp.join(tmp_dir.name, 'rocks.db')
+    rocks_db = RocksDatabase(path)
+    t = time.perf_counter()
+    rocks_db.multi_insert(range(args.numel), data, batch_size=100, log=True)
+    print(f'Initialized RocksDB in {time.perf_counter() - t:.2f} seconds')
 
     def in_memory_get(data):
         index = torch.randint(0, args.numel, (128, ))
@@ -90,9 +121,9 @@ if __name__ == '__main__':
         return db[index]
 
     benchmark(
-        funcs=[in_memory_get, db_get],
-        func_names=['In-Memory', 'SQLite'],
-        args=[(data, ), (db, )],
+        funcs=[in_memory_get, db_get, db_get],
+        func_names=['In-Memory', 'SQLite', 'RocksDB'],
+        args=[(data, ), (sqlite_db, ), (rocks_db, )],
         num_steps=50,
         num_warmups=5,
     )
