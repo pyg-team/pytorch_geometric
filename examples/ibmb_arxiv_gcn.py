dd5744be4561a440012df3f805d98628720feb44
Chendi Qian <1049653012@qq.com>
2023-09-21 21:51:15 +0200

    add baseline and curve plot

--- examples/ibmb_arxiv_gcn.py
+++ examples/ibmb_arxiv_gcn.py
@@ -1,13 +1,88 @@
 import argparse
+import time
+from math import ceil
+from typing import List
 
+import matplotlib.pyplot as plt
 import torch
 from ogb.nodeproppred import PygNodePropPredDataset
-from torch_sparse import matmul
+from torch_sparse import SparseTensor, matmul
 
 from torch_geometric.data import Data
 from torch_geometric.loader import IBMBNodeLoader
 from torch_geometric.nn import GCNConv
 from torch_geometric.utils import add_remaining_self_loops, to_undirected
+
+
+def normalize_adjmat(adj: SparseTensor, normalization: str):
+    """
+    Normalize SparseTensor adjacency matrix.
+
+    :param normalization:
+    :return:
+    """
+
+    assert normalization in [
+        'sym', 'rw'
+    ], f"Unsupported normalization type {normalization}"
+    assert isinstance(
+        adj, SparseTensor), f"Expect SparseTensor type, got {type(adj)}"
+
+    adj = adj.fill_value(1.)
+    degree = adj.sum(0)
+
+    degree[degree == 0.] = 1e-12
+    deg_inv = 1 / degree
+
+    if normalization == 'sym':
+        deg_inv_sqrt = deg_inv**0.5
+        adj = adj * deg_inv_sqrt.reshape(1, -1)
+        adj = adj * deg_inv_sqrt.reshape(-1, 1)
+    elif normalization == 'rw':
+        adj = adj * deg_inv.reshape(-1, 1)
+
+    return adj
+
+
+class NeighborSamplingLoader(torch.utils.data.DataLoader):
+    def __init__(self, graph: Data, sizes: List[int], node_idx, **kwargs):
+
+        adj = SparseTensor.from_edge_index(
+            graph.edge_index, sparse_sizes=(graph.num_nodes, graph.num_nodes))
+        adj = normalize_adjmat(adj, 'sym')
+
+        self.node_idx = node_idx
+        self.batch_size = kwargs['batch_size']
+        self.sizes = sizes
+        self.adj_t = adj
+        self.original_graph = graph
+
+        super().__init__(node_idx, collate_fn=self.sample, **kwargs)
+
+    def __getitem__(self, idx):
+        return self.node_idx[idx]
+
+    def __len__(self):
+        return len(self.node_idx)
+
+    @property
+    def loader_len(self):
+        return ceil(len(self.node_idx) / self.batch_size)
+
+    def sample(self, batch):
+        batch = torch.tensor(batch)
+        adjs = []
+        n_id = batch
+        for size in self.sizes:
+            adj_t, n_id = self.adj_t.sample_adj(n_id, size, replace=False)
+            adjs.append(adj_t)
+
+        adjs = adjs[0] if len(adjs) == 1 else adjs[::-1]
+
+        subg = Data(x=self.original_graph.x[n_id],
+                    y=self.original_graph.y[batch], edge_index=adjs,
+                    output_node_mask=torch.ones(len(batch), dtype=torch.bool))
+        return subg
 
 
 class GraphPreprocess:
@@ -64,15 +139,34 @@ class GCN(torch.nn.Module):
 
         for i, l in enumerate(self.conv_layers):
             if i == self.num_layers - 1 and prime_index is not None:
-                x = l(x, adjs[prime_index, :])
+                if isinstance(adjs, SparseTensor):
+                    adj = adjs[prime_index, :]
+                elif isinstance(adjs, list):
+                    adj = adjs[i]
+                else:
+                    raise TypeError
             else:
-                x = l(x, adjs)
+                if isinstance(adjs, SparseTensor):
+                    adj = adjs
+                elif isinstance(adjs, list):
+                    adj = adjs[i]
+                else:
+                    raise TypeError
+
+            x = l(x, adj)
             x = self.bns[i](x)
             x = torch.nn.functional.dropout(torch.relu(x), p=0.5,
                                             training=self.training)
         x = self.lastlayer(x)
 
         return x.log_softmax(dim=-1)
+
+    def reset_parameters(self):
+        for c in self.conv_layers:
+            c.reset_parameters()
+        for c in self.bns:
+            c.reset_parameters()
+        self.lastlayer.reset_parameters()
 
 
 def train(model, train_loader, optimizer):
@@ -114,7 +208,7 @@ def test(model, loader, scheduler):
         if scheduler is not None:
             scheduler.step(loss)
         pred = torch.argmax(outputs.detach(), dim=1)
-        corrects += pred.eq(y).sum()
+        corrects += pred.eq(y).sum().item()
         total_loss += loss.item() * len(y)
         num_nodes += len(y)
     return total_loss / num_nodes, corrects / num_nodes
@@ -126,9 +220,14 @@ def args_parser():
                         help='number of auxiliary nodes per output node')
     parser.add_argument('--num_train_prime', type=int, default=9000,
                         help='number of output nodes in a training batch')
-    parser.add_argument('--num_val_prime', type=int, default=18000,
-                        help='number of output nodes in a validation batch')
-    parser.add_argument('--alpha', type=float, default=0.2,
+    parser.add_argument(
+        '--num_val_prime', type=int, default=18000,
+        help='number of output nodes in a validation batch,'
+        'typically larger than num_train_prime because '
+        'it requires no backward propagation')
+    parser.add_argument('--alpha', type=float, default=0.25,
+                        help='parameter controlling PPR calculation')
+    parser.add_argument('--eps', type=float, default=2.e-4,
                         help='parameter controlling PPR calculation')
     parser.add_argument('--hidden_channels', type=int, default=256,
                         help='hidden dimension for the GNN')
@@ -146,34 +245,88 @@ if __name__ == '__main__':
     split_idx = dataset.get_idx_split()
     graph = dataset[0]
 
-    train_loader = IBMBNodeLoader(
-        graph, batch_order='order', input_nodes=split_idx["train"],
-        return_edge_index_type='adj', num_auxiliary_nodes=args.num_aux,
-        num_nodes_per_batch=args.num_train_prime, alpha=args.alpha,
-        batch_size=1, shuffle=False)
-    val_loader = IBMBNodeLoader(graph, batch_order='order',
-                                input_nodes=split_idx["valid"],
-                                return_edge_index_type='adj',
-                                num_auxiliary_nodes=args.num_aux,
-                                num_nodes_per_batch=args.num_val_prime,
-                                alpha=args.alpha, batch_size=1, shuffle=False)
-    test_loader = IBMBNodeLoader(graph, batch_order='order',
-                                 input_nodes=split_idx["test"],
-                                 return_edge_index_type='adj',
-                                 num_auxiliary_nodes=args.num_aux,
-                                 num_nodes_per_batch=args.num_val_prime,
-                                 alpha=args.alpha, batch_size=1, shuffle=False)
-
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
     model = GCN(graph.num_node_features,
                 graph.y.max().item() + 1, args.hidden_channels,
                 args.num_layers).to(device)
+
+    ibmb_timing = []
+    ibmb_acc = []
+    baseline_timing = []
+    baseline_acc = []
+
+    model.reset_parameters()
     optimizer = torch.optim.Adam(model.parameters(), lr=1.e-3,
                                  weight_decay=args.weight_decay)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
         optimizer, mode='min', factor=0.33, patience=30, cooldown=10,
         min_lr=1.e-4)
 
+    if torch.cuda.is_available():
+        torch.cuda.synchronize()
+    ibmb_start_time = time.time()
+    # IBMB
+    train_loader = IBMBNodeLoader(
+        graph, batch_order='order', input_nodes=split_idx["train"],
+        return_edge_index_type='adj', num_auxiliary_nodes=args.num_aux,
+        num_nodes_per_batch=args.num_train_prime, alpha=args.alpha,
+        eps=args.eps, batch_size=1, shuffle=False)
+    val_loader = IBMBNodeLoader(
+        graph, batch_order='order', input_nodes=split_idx["valid"],
+        return_edge_index_type='adj', num_auxiliary_nodes=args.num_aux,
+        num_nodes_per_batch=args.num_val_prime, alpha=args.alpha, eps=args.eps,
+        batch_size=1, shuffle=False)
+    test_loader = IBMBNodeLoader(
+        graph, batch_order='order', input_nodes=split_idx["test"],
+        return_edge_index_type='adj', num_auxiliary_nodes=args.num_aux,
+        num_nodes_per_batch=args.num_val_prime, alpha=args.alpha, eps=args.eps,
+        batch_size=1, shuffle=False)
+
+    print("=========== Start IBMB training ===========")
+    for epoch in range(300):
+        train_loss, train_acc = train(model, train_loader, optimizer)
+        val_loss, val_acc = test(model, val_loader, scheduler)
+        test_loss, test_acc = test(model, test_loader, None)
+        print(f'Epoch: {epoch}, '
+              f'train loss: {train_loss:.4f}, train acc: {train_acc:.4f}, '
+              f'val loss: {val_loss:.4f}, val acc: {val_acc:.4f}, '
+              f'test loss: {test_loss:.4f}, test acc: {test_acc:.4f}')
+
+        if torch.cuda.is_available():
+            torch.cuda.synchronize()
+        ibmb_timing.append(time.time() - ibmb_start_time)
+        ibmb_acc.append(val_acc)
+
+    model.reset_parameters()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1.e-3,
+                                 weight_decay=args.weight_decay)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer, mode='min', factor=0.33, patience=30, cooldown=10,
+        min_lr=1.e-4)
+
+    if torch.cuda.is_available():
+        torch.cuda.synchronize()
+    baseline_start_time = time.time()
+    # some hyperparameters we used,
+    # in principle it should consume similar GPU as the IBMB loader
+    num_nodes = [3, 3, 3]
+    # Neighbor Loader
+    # the number of batches should be the same as IBMB loader
+    train_loader = NeighborSamplingLoader(
+        graph, sizes=num_nodes, node_idx=split_idx["train"],
+        batch_size=ceil(len(split_idx["train"]) / len(train_loader)),
+        shuffle=True)
+    val_loader = NeighborSamplingLoader(
+        graph, sizes=num_nodes, node_idx=split_idx["valid"],
+        batch_size=ceil(len(split_idx["train"]) / len(val_loader)),
+        shuffle=True)
+    test_loader = NeighborSamplingLoader(
+        graph, sizes=num_nodes, node_idx=split_idx["test"],
+        batch_size=ceil(len(split_idx["test"]) / len(test_loader)),
+        shuffle=True)
+
+    print("=========== Start baseline training ===========")
     for epoch in range(300):
         train_loss, train_acc = train(model, train_loader, optimizer)
         val_loss, val_acc = test(model, val_loader, scheduler)
@@ -182,3 +335,14 @@ if __name__ == '__main__':
               f'train loss: {train_loss:.4f}, train acc: {train_acc:.4f}, '
               f'val loss: {val_loss:.4f}, val acc: {val_acc:.4f}, '
               f'test loss: {best_loss:.4f}, test acc: {best_acc:.4f}')
+        if torch.cuda.is_available():
+            torch.cuda.synchronize()
+        baseline_timing.append(time.time() - baseline_start_time)
+        baseline_acc.append(val_acc)
+
+    fig = plt.figure()
+    plt.plot(ibmb_timing, ibmb_acc, label="IBMB")
+    plt.plot(baseline_timing, baseline_acc, label="Baseline")
+    plt.legend()
+    plt.xscale("log")
+    fig.savefig('curves.png', dpi=400)
