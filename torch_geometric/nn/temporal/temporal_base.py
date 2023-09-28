58d25e2405f20c8372afa2ba46e9aa6bd8bfe170
Rishi Puri <puririshi98@berkeley.edu>
2023-09-28 15:20:01 -0700

    Update temporal_base.py

--- torch_geometric/nn/temporal/temporal_base.py
+++ torch_geometric/nn/temporal/temporal_base.py
@@ -11,40 +11,37 @@ import warnings
 import torch
 
 
-class TemporalBase(object):
+class TemporalBase(torch.nn.Module):
     def __init__(
         self,
-        src: torch.tensor,
-        dst: torch.tensor,
-        ts: torch.tensor,
         memory_mode:
         str = 'unlimited',  # could be `unlimited` or `fixed_time_window`
         time_window_ratio: float = 0.15,
         pos_prob: float = 1.0,
     ):
         r"""
-        intialize edgebank and specify the memory mode
+        intialize module and specify the memory mode
         Parameters:
-            src: source node id of the edges for initialization
-            dst: destination node id of the edges for initialization
-            ts: timestamp of the edges for initialization
             memory_mode: 'unlimited' or 'fixed_time_window'
             time_window_ratio: the ratio of the time window length to the total time length
             pos_prob: the probability of the link existence for the edges in memory
         """
         raise NotImplementedError
 
-    def update_memory(self, src: torch.tensor, dst: torch.tensor,
-                      ts: torch.tensor):
+    def update_memory(self, edge_index: torch.tensor, ts: torch.tensor):
         r"""
         generate the current and correct state of the memory with the observed edges so far
         note that historical edges may include training, validation, and already observed test edges
         Parameters:
-            src: source node id of the edges
-            dst: destination node id of the edges
+            edge_index: [2, num_edges] tensor of edge indices
             ts: timestamp of the edges
         """
-        raise NotImplementedError
+        if self.memory_mode == 'unlimited':
+            self._update_unlimited_memory(edge_index)  #ignores time
+        elif self.memory_mode == 'fixed_time_window':
+            self._update_time_window_memory(edge_index, ts)
+        else:
+            raise ValueError("Invalide memory mode!")
 
     @property
     def start_time(self) -> int:
@@ -64,38 +61,32 @@ class TemporalBase(object):
         """
         raise NotImplementedError
 
-    def _update_unlimited_memory(self, update_src: torch.tensor,
-                                 update_dst: torch.tensor):
+    def _update_unlimited_memory(self, update_edge_index: torch.tensor):
         r"""
-        update self.memory with newly arrived src and dst
+        update self.memory with newly arrived edge indices
         Parameters:
-            src: source node id of the edges
-            dst: destination node id of the edges
+            update_edge_index: [2, num_edges] tensor of edge indices
         """
         raise NotImplementedError
 
-    def _update_time_window_memory(self, update_src: torch.tensor,
-                                   update_dst: torch.tensor,
+    def _update_time_window_memory(self, update_edge_index: torch.tensor,
                                    update_ts: torch.tensor) -> None:
         r"""
         move the time window forward until end of dst timestamp here
         also need to remove earlier edges from memory which is not in the time window
         Parameters:
-            update_src: source node id of the edges
-            update_dst: destination node id of the edges
+            update_edge_index: [2, num_edges] tensor of edge indices
             update_ts: timestamp of the edges
         """
 
         raise NotImplementedError
 
-    def predict_link(self, query_src: torch.tensor,
-                     query_dst: torch.tensor) -> torch.tensor:
+    def predict_link(self, query_edge_indices: torch.tensor) -> torch.tensor:
         r"""
         predict the probability from query src,dst pair given the current memory,
         all edges not in memory will return 0.0 while all observed edges in memory will return self.pos_prob
         Parameters:
-            query_src: source node id of the query edges
-            query_dst: destination node id of the query edges
+            query_edge_indices: [2, num_edges] tensor of edge indices
         Returns:
             pred: the prediction for all query edges
         """
