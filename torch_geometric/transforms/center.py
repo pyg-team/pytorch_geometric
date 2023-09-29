7fda04e5537772ac0d3577b16c4cb26af8003e70
Mohamad Zamini <32536264+mzamini92@users.noreply.github.com>
2023-09-29 17:51:22 -0600

    Added interval to center.py
    Following to PR5733 I've Added the interval argument to the BaseTransform class, and any transform that inherits from BaseTransform will now have access to the interval attribute in its forward method. @rusty1s

--- torch_geometric/transforms/center.py
+++ torch_geometric/transforms/center.py
@@ -1,18 +1,31 @@
-from typing import Union
+from typing import Union, Tuple
 
 from torch_geometric.data import Data, HeteroData
 from torch_geometric.data.datapipes import functional_transform
 from torch_geometric.transforms import BaseTransform
 
-
 @functional_transform('center')
 class Center(BaseTransform):
     r"""Centers node positions :obj:`data.pos` around the origin
-    (functional name: :obj:`center`)."""
+    (functional name: :obj:`center`).
+
+    Args:
+        interval (Tuple[float, float], optional): A tuple representing the
+            interval for the transformation. Defaults to (0.0, 1.0).
+    """
+    def __init__(
+        self,
+        interval: Tuple[float, float] = (0.0, 1.0),
+    ):
+        self.interval = interval
+
     def forward(
         self,
         data: Union[Data, HeteroData],
     ) -> Union[Data, HeteroData]:
+        # Pass the interval argument to the transformed data
+        data.interval = self.interval
+        
         for store in data.node_stores:
             if hasattr(store, 'pos'):
                 store.pos = store.pos - store.pos.mean(dim=-2, keepdim=True)
