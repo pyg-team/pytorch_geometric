f9598ef3154c3587cd4d46eb30edc29de4209e59
puririshi98 <puririshi98@berkeley.edu>
2023-10-05 09:08:39 -0700

    trying to get CI passing, works fine for my environments

--- torch_geometric/nn/to_hetero_transformer.py
+++ torch_geometric/nn/to_hetero_transformer.py
@@ -8,13 +8,10 @@ from torch import Tensor
 from torch.nn import Module
 
 import torch_geometric
-from torch_geometric.nn.dense import HeteroDictLinear
 from torch_geometric.nn.dense.linear import is_uninitialized_parameter
 from torch_geometric.nn.fx import Transformer, get_submodule
-from torch_geometric.nn.to_hetero_module import (
-    ToHeteroLinear,
-    get_linear_channels,
-)
+from torch_geometric.nn.to_hetero_module import ToHeteroLinear,
+
 from torch_geometric.typing import WITH_GMM, EdgeType, Metadata, NodeType
 from torch_geometric.utils.hetero import (
     check_add_self_loops,
