15ef0ea56468ff5a7b0133112987eb851f8cef37
rusty1s <matthias.fey@tu-dortmund.de>
2023-10-09 09:58:03 +0000

    update

--- torch_geometric/distributed/event_loop.py
+++ torch_geometric/distributed/event_loop.py
@@ -1,34 +1,37 @@
 import asyncio
 import logging
 from threading import BoundedSemaphore, Thread
+from typing import Callable, Optional
 
 import torch
 
 
-def wrap_torch_future(f: torch.futures.Future) -> asyncio.futures.Future:
-    r""" Convert a torch future to a standard asyncio future."""
+def to_asyncio_future(future: torch.futures.Future) -> asyncio.futures.Future:
+    r"""Convert a :class:`torch.futures.Future` to a standard asyncio future.
+    """
     loop = asyncio.get_event_loop()
-    aio_future = loop.create_future()
+    asyncio_future = loop.create_future()
 
     def on_done(*_):
         try:
-            result = f.wait()
+            result = future.wait()
         except Exception as e:
-            loop.call_soon_threadsafe(aio_future.set_exception, e)
+            loop.call_soon_threadsafe(asyncio_future.set_exception, e)
         else:
-            loop.call_soon_threadsafe(aio_future.set_result, result)
+            loop.call_soon_threadsafe(asyncio_future.set_result, result)
 
-    f.add_done_callback(on_done)
-    return aio_future
+    future.add_done_callback(on_done)
+
+    return asyncio_future
 
 
-class ConcurrentEventLoop(object):
-    r""" Concurrent event loop context.
+class ConcurrentEventLoop:
+    r"""Concurrent event loop context.
 
     Args:
         concurrency: max processing concurrency.
     """
-    def __init__(self, concurrency):
+    def __init__(self, concurrency: int):
         self._concurrency = concurrency
         self._sem = BoundedSemaphore(concurrency)
         self._loop = asyncio.new_event_loop()
@@ -46,23 +49,22 @@ class ConcurrentEventLoop(object):
             self._runner_t.join(timeout=1)
 
     def wait_all(self):
-        r""" Wait all pending tasks to be finished."""
+        r"""Wait for all pending tasks to be finished."""
         for _ in range(self._concurrency):
             self._sem.acquire()
         for _ in range(self._concurrency):
             self._sem.release()
 
-    def add_task(self, coro, callback=None):
-        r""" Add an asynchronized coroutine task to run.
+    def add_task(self, coro, callback: Optional[Callable] = None):
+        r"""Adds an asynchronized coroutine task to run.
 
         Args:
-        coro: the async coroutine func.
-        callback: the callback func applied on the returned results
-            after the coroutine task is finished.
+            coro: The asynchronous coroutine function.
+            callback (callable, optional): The callback function applied on the
+                returned results after the coroutine task is finished.
+                (default: :obj:`None`)
 
-        Note that any results returned by `callback` func will be ignored,
-        so it is preferable to handle all in your `callback` func and do
-        not return any results.
+        Note that any result returned by :obj:`callback` will be ignored.
         """
         def on_done(f: asyncio.futures.Future):
             try:
@@ -70,7 +72,7 @@ class ConcurrentEventLoop(object):
                 if callback is not None:
                     callback(res)
             except Exception as e:
-                logging.error("coroutine task failed: %s", e)
+                logging.error(f"Coroutine task failed with error: {e}")
             self._sem.release()
 
         self._sem.acquire()
@@ -78,7 +80,10 @@ class ConcurrentEventLoop(object):
         fut.add_done_callback(on_done)
 
     def run_task(self, coro):
-        r""" Run a coroutine task synchronously.
+        r"""Runs a coroutine task synchronously.
+
+        Args:
+            coro: The synchronous coroutine function.
         """
         with self._sem:
             fut = asyncio.run_coroutine_threadsafe(coro, self._loop)
--- torch_geometric/distributed/utils.py
+++ torch_geometric/distributed/utils.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Tuple
 
 import numpy as np
@@ -13,51 +13,28 @@ from torch_geometric.typing import Dict, NodeType, Optional
 
 @dataclass
 class NodeDict:
-    r"""Class used during hetero sampling. It contains fields that refer to
-    NodeDict in three situations:
-    1) the nodes are to serve as srcs in the next layer,
-    2) nodes with duplicates, that are further needed to create COO output
-       matrix,
-    3) output nodes without duplicates.
+    r"""Class used during heterogeneous sampling:
+    1) The nodes to serve as source nodes in the next layer
+    2) The nodes with duplicates that are further needed to create COO output
+    3) The output nodes without duplicates
     """
-    def __init__(self, node_types):
-        self.src: Dict[NodeType, Tensor] = {}
-        self.with_dupl: Dict[NodeType, Tensor] = {}
-        self.out: Dict[NodeType, np.array] = {}
-
-        for ntype in node_types:
-            self.src.update({ntype: torch.empty(0, dtype=torch.int64)})
-            self.with_dupl.update({ntype: torch.empty(0, dtype=torch.int64)})
-            self.out.update({ntype: np.empty(0, dtype=np.int64)})
+    src: Dict[NodeType, Tensor] = field(default_factory=dict)
+    with_dupl: Dict[NodeType, Tensor] = field(default_factory=dict)
+    out: Dict[NodeType, Tensor] = field(default_factory=dict)
 
 
 @dataclass
 class BatchDict:
-    r"""Class used during disjoint hetero sampling. It contains fields that
-    refer to BatchDict in three situations:
-    1) the batch is to serve as initial subgraph ids for src nodes in the next
-       layer,
-    2) subgraph ids with duplicates, that are further needed to create COO
-       output matrix,
-    3) output subgraph ids without duplicates.
+    r"""Class used during disjoint heterogeneous sampling:
+    1) The batch to serve as initial subgraph IDs for source nodes in the next
+       layer
+    2) The subgraph IDs with duplicates that are further needed to create COO
+       output
+    3) The output subgraph IDs without duplicates
     """
-    def __init__(self, node_types, disjoint):
-        self.src: Dict[NodeType, Tensor] = {}
-        self.with_dupl: Dict[NodeType, Tensor] = {}
-        self.out: Dict[NodeType, Tensor] = {}
-        self.disjoint = disjoint
-
-        if self.disjoint:
-            for ntype in node_types:
-                self.src.update({ntype: torch.empty(0, dtype=torch.int64)})
-                self.with_dupl.update(
-                    {ntype: torch.empty(0, dtype=torch.int64)})
-                self.out.update({ntype: np.empty(0, dtype=np.int64)})
-        else:
-            for ntype in node_types:
-                self.src.update({ntype: None})
-                self.with_dupl.update({ntype: None})
-                self.out.update({ntype: None})
+    src: Dict[NodeType, Tensor] = field(default_factory=dict)
+    with_duple: Dict[NodeType, Tensor] = field(default_factory=dict)
+    out: Dict[NodeType, Tensor] = field(default_factory=dict)
 
 
 def remove_duplicates(
