171e893df9e098a11c4bd158ab9baf5e84704162
Rishi Puri <puririshi98@berkeley.edu>
2023-10-26 13:34:28 -0700

    cleanup

--- examples/multi_gpu/multinode_multigpu_papers100m_gcn.py
+++ examples/multi_gpu/multinode_multigpu_papers100m_gcn.py
@@ -125,7 +125,7 @@ def run_train(device, data, world_size, ngpu_per_node, model, epochs,
                                             input_nodes=split_idx['test'],
                                             **kwargs)
     else:
-        from torch_geometric.loader import NeighborLoader
+        NeighborLoader = torch_geometric.loader.NeighborLoader
         num_work = get_num_workers(world_size)
         train_loader = NeighborLoader(data, input_nodes=split_idx['train'],
                                       num_workers=num_work, shuffle=True,
