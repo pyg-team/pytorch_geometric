b6b195dc2436794b0dc517c85bdfd41aadfc8cca
Ubuntu <ubuntu@ip-172-31-15-174.us-west-1.compute.internal>
2023-10-26 08:22:00 +0000

    Add alternative edge indices shape to input description

--- torch_geometric/nn/conv/gcn_conv.py
+++ torch_geometric/nn/conv/gcn_conv.py
@@ -159,7 +159,8 @@ class GCNConv(MessagePassing):
     Shapes:
         - **input:**
           node features :math:`(|\mathcal{V}|, F_{in})`,
-          edge indices :math:`(2, |\mathcal{E}|)`,
+          edge indices :math:`(2, |\mathcal{E}|)`
+          or :math:`(|\mathcal{V}|, |\mathcal{V}|)` *(sparse)*,
           edge weights :math:`(|\mathcal{E}|)` *(optional)*
         - **output:** node features :math:`(|\mathcal{V}|, F_{out})`
     """
