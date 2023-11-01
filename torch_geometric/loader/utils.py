`[NeighborLoader]` Fix `input_id` computation in case a `mask` is given (#8312)

torch_geometric/loader/utils.py
@@ -1,7 +1,6 @@
 import copy
 import logging
 import math
-from collections.abc import Sequence
 from typing import Any, Dict, Optional, Tuple, Union
 
 import numpy as np
@@ -249,24 +248,35 @@ def filter_custom_store(
 def get_input_nodes(
     data: Union[Data, HeteroData, Tuple[FeatureStore, GraphStore]],
     input_nodes: Union[InputNodes, TensorAttr],
-) -> Tuple[Optional[str], Sequence]:
-    def to_index(tensor):
-        if isinstance(tensor, Tensor) and tensor.dtype == torch.bool:
-            return tensor.nonzero(as_tuple=False).view(-1)
-        if not isinstance(tensor, Tensor):
-            return torch.tensor(tensor, dtype=torch.long)
-        return tensor
+    input_id: Optional[Tensor],
+) -> Tuple[Optional[str], Tensor, Optional[Tensor]]:
+    def to_index(nodes, input_id) -> Tuple[Tensor, Optional[Tensor]]:
+        if isinstance(nodes, Tensor) and nodes.dtype == torch.bool:
+            nodes = nodes.nonzero(as_tuple=False).view(-1)
+            if input_id is not None:
+                assert input_id.numel() == nodes.numel()
+            else:
+                input_id = nodes
+            return nodes, input_id
+
+        if not isinstance(nodes, Tensor):
+            nodes = torch.tensor(nodes, dtype=torch.long)
+
+        if input_id is not None:
+            assert input_id.numel() == nodes.numel()
+
+        return nodes, input_id
 
     if isinstance(data, Data):
         if input_nodes is None:
-            return None, torch.arange(data.num_nodes)
-        return None, to_index(input_nodes)
+            return None, torch.arange(data.num_nodes), None
+        return None, *to_index(input_nodes, input_id)
 
     elif isinstance(data, HeteroData):
         assert input_nodes is not None
 
         if isinstance(input_nodes, str):
-            return input_nodes, torch.arange(data[input_nodes].num_nodes)
+            return input_nodes, torch.arange(data[input_nodes].num_nodes), None
 
         assert isinstance(input_nodes, (list, tuple))
         assert len(input_nodes) == 2
@@ -274,20 +284,20 @@ def get_input_nodes(
 
         node_type, input_nodes = input_nodes
         if input_nodes is None:
-            return node_type, torch.arange(data[node_type].num_nodes)
-        return node_type, to_index(input_nodes)
+            return node_type, torch.arange(data[node_type].num_nodes), None
+        return node_type, *to_index(input_nodes, input_id)
 
     else:  # Tuple[FeatureStore, GraphStore]
         feature_store, graph_store = data
         assert input_nodes is not None
 
         if isinstance(input_nodes, Tensor):
-            return None, to_index(input_nodes)
+            return None, *to_index(input_nodes, input_id)
 
         if isinstance(input_nodes, str):
-            return input_nodes, torch.arange(
-                remote_backend_utils.num_nodes(feature_store, graph_store,
-                                               input_nodes))
+            num_nodes = remote_backend_utils.num_nodes(  #
+                feature_store, graph_store, input_nodes)
+            return input_nodes, torch.arange(num_nodes), None
 
         if isinstance(input_nodes, (list, tuple)):
             assert len(input_nodes) == 2
@@ -295,10 +305,11 @@ def get_input_nodes(
 
             node_type, input_nodes = input_nodes
             if input_nodes is None:
-                return node_type, torch.arange(
-                    remote_backend_utils.num_nodes(feature_store, graph_store,
-                                                   input_nodes))
-            return node_type, to_index(input_nodes)
+                num_nodes = remote_backend_utils.num_nodes(  #
+                    feature_store, graph_store, input_nodes)
+                return node_type, torch.arange(num_nodes), None
+
+            return node_type, *to_index(input_nodes, input_id)
 
 
 def get_edge_label_index(