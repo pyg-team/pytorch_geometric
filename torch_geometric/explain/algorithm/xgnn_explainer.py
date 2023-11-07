f58195f9808194627ae9458b027df213cf0b7bc8
Simon Bele <simonbele@Simons-MBP.fri1.uni-lj.si>
2023-11-07 10:28:49 +0100

    added base files

--- torch_geometric/explain/algorithm/xgnn_explainer.py
+++ torch_geometric/explain/algorithm/xgnn_explainer.py
@@ -1,10 +1,219 @@
-import logging
-from typing import List, Optional, Union
+from math import sqrt
+from typing import Optional, Tuple, Union
 
 import torch
 from torch import Tensor
+from torch.nn.parameter import Parameter
 
-from torch_geometric.explain import Explanation
+from torch_geometric.explain import ExplainerConfig, Explanation, ModelConfig
 from torch_geometric.explain.algorithm import ExplainerAlgorithm
-from torch_geometric.explain.config import ExplanationType, ModelTaskLevel
-from torch_geometric.nn.conv.message_passing import MessagePassing
+from torch_geometric.explain.algorithm.utils import clear_masks, set_masks
+from torch_geometric.explain.config import MaskType, ModelMode, ModelTaskLevel
+
+
+
+class XGNNExplainer(ExplainerAlgorithm):
+    r"""The XGNN-Explainer model from the `"XGNN: Towards Model-Level Explanations of Graph Neural Networks"
+    <https://arxiv.org/abs/2006.02587`_ paper for training a graph generator so that 
+    the generated graph patterns maximize a certain prediction of the model.
+
+    .. note::
+
+        For an example of using :class:`XGNNExplainer`, see
+        `examples/explain/gnn_explainer.py <https://github.com/pyg-team/
+        pytorch_geometric/blob/master/examples/explain/gnn_explainer.py>`_,
+        `examples/explain/gnn_explainer_ba_shapes.py <https://github.com/
+        pyg-team/pytorch_geometric/blob/master/examples/
+        explain/gnn_explainer_ba_shapes.py>`_, and `examples/explain/
+        gnn_explainer_link_pred.py <https://github.com/pyg-team/
+        pytorch_geometric/blob/master/examples/explain/gnn_explainer_link_pred.py>`_.
+
+    .. note::
+
+        The :obj:`edge_size` coefficient is multiplied by the number of nodes
+        in the explanation at every iteration, and the resulting value is added
+        to the loss as a regularization term, with the goal of producing
+        compact explanations.
+        A higher value will push the algorithm towards explanations with less
+        elements.
+        Consider adjusting the :obj:`edge_size` coefficient according to the
+        average node degree in the dataset, especially if this value is bigger
+        than in the datasets used in the original paper.
+
+    Args:
+        epochs (int, optional): The number of epochs to train.
+            (default: :obj:`100`)
+        lr (float, optional): The learning rate to apply.
+            (default: :obj:`0.01`)
+        **kwargs (optional): Additional hyper-parameters to override default
+            settings in
+            :attr:`~torch_geometric.explain.algorithm.GNNExplainer.coeffs`.
+    """
+
+    coeffs = {
+        'edge_size': 0.005,
+        'edge_reduction': 'sum',
+        'node_feat_size': 1.0,
+        'node_feat_reduction': 'mean',
+        'edge_ent': 1.0,
+        'node_feat_ent': 0.1,
+        'EPS': 1e-15,
+    }
+
+    def __init__(self, epochs: int = 100, lr: float = 0.01, **kwargs):
+        super().__init__()
+        self.epochs = epochs
+        self.lr = lr
+        self.coeffs.update(kwargs)
+
+        self.node_mask = self.hard_node_mask = None
+        self.edge_mask = self.hard_edge_mask = None
+
+    def forward(
+        self,
+        model: torch.nn.Module,
+        x: Tensor,
+        edge_index: Tensor,
+        *,
+        target: Tensor,
+        index: Optional[Union[int, Tensor]] = None,
+        **kwargs,
+    ) -> Explanation:
+        if isinstance(x, dict) or isinstance(edge_index, dict):
+            raise ValueError(f"Heterogeneous graphs not yet supported in "
+                             f"'{self.__class__.__name__}'")
+
+        self._train(model, x, edge_index, target=target, index=index, **kwargs)
+
+        node_mask = self._post_process_mask(
+            self.node_mask,
+            self.hard_node_mask,
+            apply_sigmoid=True,
+        )
+        edge_mask = self._post_process_mask(
+            self.edge_mask,
+            self.hard_edge_mask,
+            apply_sigmoid=True,
+        )
+
+        self._clean_model(model)
+
+        return Explanation(node_mask=node_mask, edge_mask=edge_mask)
+
+    def supports(self) -> bool:
+        return True
+
+    def _train(
+        self,
+        model: torch.nn.Module,
+        x: Tensor,
+        edge_index: Tensor,
+        *,
+        target: Tensor,
+        index: Optional[Union[int, Tensor]] = None,
+        **kwargs,
+    ):
+        self._initialize_masks(x, edge_index)
+
+        parameters = []
+        if self.node_mask is not None:
+            parameters.append(self.node_mask)
+        if self.edge_mask is not None:
+            set_masks(model, self.edge_mask, edge_index, apply_sigmoid=True)
+            parameters.append(self.edge_mask)
+
+        optimizer = torch.optim.Adam(parameters, lr=self.lr)
+
+        for i in range(self.epochs):
+            optimizer.zero_grad()
+
+            h = x if self.node_mask is None else x * self.node_mask.sigmoid()
+            y_hat, y = model(h, edge_index, **kwargs), target
+
+            if index is not None:
+                y_hat, y = y_hat[index], y[index]
+
+            loss = self._loss(y_hat, y)
+
+            loss.backward()
+            optimizer.step()
+
+            # In the first iteration, we collect the nodes and edges that are
+            # involved into making the prediction. These are all the nodes and
+            # edges with gradient != 0 (without regularization applied).
+            if i == 0 and self.node_mask is not None:
+                if self.node_mask.grad is None:
+                    raise ValueError("Could not compute gradients for node "
+                                     "features. Please make sure that node "
+                                     "features are used inside the model or "
+                                     "disable it via `node_mask_type=None`.")
+                self.hard_node_mask = self.node_mask.grad != 0.0
+            if i == 0 and self.edge_mask is not None:
+                if self.edge_mask.grad is None:
+                    raise ValueError("Could not compute gradients for edges. "
+                                     "Please make sure that edges are used "
+                                     "via message passing inside the model or "
+                                     "disable it via `edge_mask_type=None`.")
+                self.hard_edge_mask = self.edge_mask.grad != 0.0
+
+    def _initialize_masks(self, x: Tensor, edge_index: Tensor):
+        node_mask_type = self.explainer_config.node_mask_type
+        edge_mask_type = self.explainer_config.edge_mask_type
+
+        device = x.device
+        (N, F), E = x.size(), edge_index.size(1)
+
+        std = 0.1
+        if node_mask_type is None:
+            self.node_mask = None
+        elif node_mask_type == MaskType.object:
+            self.node_mask = Parameter(torch.randn(N, 1, device=device) * std)
+        elif node_mask_type == MaskType.attributes:
+            self.node_mask = Parameter(torch.randn(N, F, device=device) * std)
+        elif node_mask_type == MaskType.common_attributes:
+            self.node_mask = Parameter(torch.randn(1, F, device=device) * std)
+        else:
+            assert False
+
+        if edge_mask_type is None:
+            self.edge_mask = None
+        elif edge_mask_type == MaskType.object:
+            std = torch.nn.init.calculate_gain('relu') * sqrt(2.0 / (2 * N))
+            self.edge_mask = Parameter(torch.randn(E, device=device) * std)
+        else:
+            assert False
+
+    def _loss(self, y_hat: Tensor, y: Tensor) -> Tensor:
+        if self.model_config.mode == ModelMode.binary_classification:
+            loss = self._loss_binary_classification(y_hat, y)
+        elif self.model_config.mode == ModelMode.multiclass_classification:
+            loss = self._loss_multiclass_classification(y_hat, y)
+        elif self.model_config.mode == ModelMode.regression:
+            loss = self._loss_regression(y_hat, y)
+        else:
+            assert False
+
+        if self.hard_edge_mask is not None:
+            assert self.edge_mask is not None
+            m = self.edge_mask[self.hard_edge_mask].sigmoid()
+            edge_reduce = getattr(torch, self.coeffs['edge_reduction'])
+            loss = loss + self.coeffs['edge_size'] * edge_reduce(m)
+            ent = -m * torch.log(m + self.coeffs['EPS']) - (
+                1 - m) * torch.log(1 - m + self.coeffs['EPS'])
+            loss = loss + self.coeffs['edge_ent'] * ent.mean()
+
+        if self.hard_node_mask is not None:
+            assert self.node_mask is not None
+            m = self.node_mask[self.hard_node_mask].sigmoid()
+            node_reduce = getattr(torch, self.coeffs['node_feat_reduction'])
+            loss = loss + self.coeffs['node_feat_size'] * node_reduce(m)
+            ent = -m * torch.log(m + self.coeffs['EPS']) - (
+                1 - m) * torch.log(1 - m + self.coeffs['EPS'])
+            loss = loss + self.coeffs['node_feat_ent'] * ent.mean()
+
+        return loss
+
+    def _clean_model(self, model):
+        clear_masks(model)
+        self.node_mask = self.hard_node_mask = None
+        self.edge_mask = self.hard_edge_mask = None
---
+++ torch_geometric/explain/generative_explanation.py
@@ -0,0 +1,132 @@
+import copy
+from typing import Dict, List, Optional, Union
+
+import torch
+from torch import Tensor
+
+from torch_geometric.data.data import Data, warn_or_raise
+from torch_geometric.data.hetero_data import HeteroData
+from torch_geometric.explain.config import ThresholdConfig, ThresholdType
+from torch_geometric.visualization import visualize_graph
+
+
+class GenerativeExplanation(Data):
+    r"""Holds all the obtained explanations of a homogeneous graph.
+
+    The explanation object is a :obj:`~torch_geometric.data.Data` object and
+    can hold node attributions and edge attributions.
+    It can also hold the original graph if needed.
+
+    Args:
+        node_mask (Tensor, optional): Node-level mask with shape
+            :obj:`[num_nodes, 1]`, :obj:`[1, num_features]` or
+            :obj:`[num_nodes, num_features]`. (default: :obj:`None`)
+        edge_mask (Tensor, optional): Edge-level mask with shape
+            :obj:`[num_edges]`. (default: :obj:`None`)
+        **kwargs (optional): Additional attributes.
+    """
+    def validate(self, raise_on_error: bool = True) -> bool:
+        r"""Validates the correctness of the :class:`Explanation` object."""
+        status = super().validate(raise_on_error)
+        status &= self.validate_masks(raise_on_error)
+        return status
+
+    def get_explanation_subgraph(self) -> 'Explanation':
+        r"""Returns the induced subgraph, in which all nodes and edges with
+        zero attribution are masked out.
+        """
+        node_mask = self.get('node_mask')
+        if node_mask is not None:
+            node_mask = node_mask.sum(dim=-1) > 0
+        edge_mask = self.get('edge_mask')
+        if edge_mask is not None:
+            edge_mask = edge_mask > 0
+        return self._apply_masks(node_mask, edge_mask)
+
+    def get_complement_subgraph(self) -> 'Explanation':
+        r"""Returns the induced subgraph, in which all nodes and edges with any
+        attribution are masked out.
+        """
+        node_mask = self.get('node_mask')
+        if node_mask is not None:
+            node_mask = node_mask.sum(dim=-1) == 0
+        edge_mask = self.get('edge_mask')
+        if edge_mask is not None:
+            edge_mask = edge_mask == 0
+        return self._apply_masks(node_mask, edge_mask)
+
+    def _apply_masks(
+        self,
+        node_mask: Optional[Tensor] = None,
+        edge_mask: Optional[Tensor] = None,
+    ) -> 'Explanation':
+        out = copy.copy(self)
+
+        if edge_mask is not None:
+            for key, value in self.items():
+                if key == 'edge_index':
+                    out.edge_index = value[:, edge_mask]
+                elif self.is_edge_attr(key):
+                    out[key] = value[edge_mask]
+
+        if node_mask is not None:
+            out = out.subgraph(node_mask)
+
+        return out
+
+    def visualize_feature_importance(
+        self,
+        path: Optional[str] = None,
+        feat_labels: Optional[List[str]] = None,
+        top_k: Optional[int] = None,
+    ):
+        r"""Creates a bar plot of the node feature importances by summing up
+        the node mask across all nodes.
+
+        Args:
+            path (str, optional): The path to where the plot is saved.
+                If set to :obj:`None`, will visualize the plot on-the-fly.
+                (default: :obj:`None`)
+            feat_labels (List[str], optional): The labels of features.
+                (default :obj:`None`)
+            top_k (int, optional): Top k features to plot. If :obj:`None`
+                plots all features. (default: :obj:`None`)
+        """
+        node_mask = self.get('node_mask')
+        if node_mask is None:
+            raise ValueError(f"The attribute 'node_mask' is not available "
+                             f"in '{self.__class__.__name__}' "
+                             f"(got {self.available_explanations})")
+        if node_mask.dim() != 2 or node_mask.size(1) <= 1:
+            raise ValueError(f"Cannot compute feature importance for "
+                             f"object-level 'node_mask' "
+                             f"(got shape {node_mask.size()})")
+
+        if feat_labels is None:
+            feat_labels = range(node_mask.size(1))
+
+        score = node_mask.sum(dim=0)
+
+        return _visualize_score(score, feat_labels, path, top_k)
+
+    def visualize_graph(self, path: Optional[str] = None,
+                        backend: Optional[str] = None):
+        r"""Visualizes the explanation graph with edge opacity corresponding to
+        edge importance.
+
+        Args:
+            path (str, optional): The path to where the plot is saved.
+                If set to :obj:`None`, will visualize the plot on-the-fly.
+                (default: :obj:`None`)
+            backend (str, optional): The graph drawing backend to use for
+                visualization (:obj:`"graphviz"`, :obj:`"networkx"`).
+                If set to :obj:`None`, will use the most appropriate
+                visualization backend based on available system packages.
+                (default: :obj:`None`)
+        """
+        edge_mask = self.get('edge_mask')
+        if edge_mask is None:
+            raise ValueError(f"The attribute 'edge_mask' is not available "
+                             f"in '{self.__class__.__name__}' "
+                             f"(got {self.available_explanations})")
+        visualize_graph(self.edge_index, edge_mask, path, backend)
