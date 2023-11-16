ff00904be54b90d21a1acf28f9d379fd13e6e32a
rusty1s <matthias.fey@tu-dortmund.de>
2023-11-16 15:50:08 +0100

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -66,10 +66,12 @@ nbsphinx_thumbnails = {
 }
 
 
-def setup(app):
-    def rst_jinja_render(app, _, source):
+def rst_jinja_render(app, _, source):
+    if hasattr(app.builder, 'templates'):
         rst_context = {'torch_geometric': torch_geometric}
         source[0] = app.builder.templates.render_string(source[0], rst_context)
 
+
+def setup(app):
     app.connect('source-read', rst_jinja_render)
     app.add_js_file('js/version_alert.js')
