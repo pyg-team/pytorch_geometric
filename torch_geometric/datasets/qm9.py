da729791758a39b316829d620d2bdd451cd5f0bd
RafiBrent <105883594+RafiBrent@users.noreply.github.com>
2023-11-20 16:51:34 -0600

    Update qm9.py

--- torch_geometric/datasets/qm9.py
+++ torch_geometric/datasets/qm9.py
@@ -134,7 +134,7 @@ class QM9(InMemoryDataset):
     **STATS:**
 
     .. list-table::
-        :widths: 10 10 10 10 10
+        :widths: 10 10 10 10 10 10
         :header-rows: 1
         * - #featurize
           - #graphs
