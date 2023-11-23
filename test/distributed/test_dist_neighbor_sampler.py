0f7889ae2e4571da0a9c565440d6f0f15f0c585e
kgajdamo <kinga.gajdamowicz@intel.com>
2023-11-23 11:54:17 +0000

    enable distributed edge based temporal sampling

--- test/distributed/test_dist_neighbor_sampler.py
+++ test/distributed/test_dist_neighbor_sampler.py
@@ -15,9 +15,10 @@ from torch_geometric.distributed.rpc import init_rpc
 from torch_geometric.sampler import NeighborSampler, NodeSamplerInput
 from torch_geometric.sampler.neighbor_sampler import node_sample
 from torch_geometric.testing import onlyLinux, withPackage
+from torch_geometric.typing import WITH_EDGE_TIME_NEIGHBOR_SAMPLE
 
 
-def create_data(rank: int, world_size: int, temporal: bool = False):
+def create_data(rank: int, world_size: int, time_attr: str = 'time'):
     if rank == 0:  # Partition 0:
         node_id = torch.tensor([0, 1, 2, 3, 4, 5, 9])
         edge_index = torch.tensor([  # Sorted by destination.
@@ -30,7 +31,6 @@ def create_data(rank: int, world_size: int, temporal: bool = False):
             [5, 6, 7, 8, 9, 5, 0],
             [4, 5, 6, 7, 8, 9, 9],
         ])
-
     feature_store = LocalFeatureStore.from_data(node_id)
     graph_store = LocalGraphStore.from_data(
         edge_id=None,
@@ -50,9 +50,20 @@ def create_data(rank: int, world_size: int, temporal: bool = False):
     ])
     data = Data(x=None, y=None, edge_index=edge_index, num_nodes=10)
 
-    if temporal:  # Create time data:
+    if time_attr == 'time':  # Create time data:
         data.time = torch.tensor([5, 0, 1, 3, 3, 4, 4, 4, 4, 4])
-        feature_store.put_tensor(data.time, group_name=None, attr_name='time')
+        feature_store.put_tensor(data.time, group_name=None,
+                                 attr_name=time_attr)
+
+    else:  # time_attr = 'edge_time'
+        data.edge_time = torch.tensor([0, 1, 2, 3, 4, 5, 7, 7, 7, 7, 7, 11])
+
+        if rank == 0:
+            edge_time = torch.tensor([0, 1, 2, 3, 4, 5, 11])
+        if rank == 1:
+            edge_time = torch.tensor([4, 7, 7, 7, 7, 7, 11])
+        feature_store.put_tensor(edge_time, group_name=None,
+                                 attr_name=time_attr)
 
     return (feature_store, graph_store), data
 
@@ -144,8 +155,9 @@ def dist_neighbor_sampler_temporal(
     master_port: int,
     seed_time: torch.tensor = None,
     temporal_strategy: str = 'uniform',
+    time_attr: str = 'time',
 ):
-    dist_data, data = create_data(rank, world_size, temporal=True)
+    dist_data, data = create_data(rank, world_size, time_attr)
 
     current_ctx = DistContext(
         rank=rank,
@@ -172,7 +184,7 @@ def dist_neighbor_sampler_temporal(
         shuffle=False,
         disjoint=True,
         temporal_strategy=temporal_strategy,
-        time_attr='time',
+        time_attr=time_attr,
     )
 
     init_rpc(
@@ -211,7 +223,7 @@ def dist_neighbor_sampler_temporal(
         num_neighbors=num_neighbors,
         disjoint=True,
         temporal_strategy=temporal_strategy,
-        time_attr='time',
+        time_attr=time_attr,
     )
 
     # Evaluate node sample function:
@@ -269,12 +281,47 @@ def test_dist_neighbor_sampler_temporal(seed_time, temporal_strategy):
     world_size = 2
     w0 = mp_context.Process(
         target=dist_neighbor_sampler_temporal,
-        args=(world_size, 0, port, seed_time, temporal_strategy),
+        args=(world_size, 0, port, seed_time, temporal_strategy, 'time'),
     )
 
     w1 = mp_context.Process(
         target=dist_neighbor_sampler_temporal,
-        args=(world_size, 1, port, seed_time, temporal_strategy),
+        args=(world_size, 1, port, seed_time, temporal_strategy, 'time'),
+    )
+
+    w0.start()
+    w1.start()
+    w0.join()
+    w1.join()
+
+
+@onlyLinux
+@withPackage('pyg_lib')
+@pytest.mark.skipif(
+    not WITH_EDGE_TIME_NEIGHBOR_SAMPLE,
+    reason="Edge-level temporal sampling requires a more recent 'pyg-lib'"
+    " installation")
+@pytest.mark.parametrize(
+    'seed_time',
+    [torch.tensor([1, 1]), torch.tensor([3, 7])])
+@pytest.mark.parametrize('temporal_strategy', ['uniform', 'last'])
+def test_dist_neighbor_sampler_edge_level_temporal(seed_time,
+                                                   temporal_strategy):
+    mp_context = torch.multiprocessing.get_context('spawn')
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(('127.0.0.1', 0))
+    port = s.getsockname()[1]
+    s.close()
+
+    world_size = 2
+    w0 = mp_context.Process(
+        target=dist_neighbor_sampler_temporal,
+        args=(world_size, 0, port, seed_time, temporal_strategy, 'edge_time'),
+    )
+
+    w1 = mp_context.Process(
+        target=dist_neighbor_sampler_temporal,
+        args=(world_size, 1, port, seed_time, temporal_strategy, 'edge_time'),
     )
 
     w0.start()
