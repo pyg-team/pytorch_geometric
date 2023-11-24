c8e4a26a14f8ed021436cb9f97f865487c380328
Akihiro Nitta <nitta@akihironitta.com>
2023-11-24 16:46:00 +0000

    update

--- test/nn/aggr/test_multi.py
+++ test/nn/aggr/test_multi.py
@@ -3,11 +3,13 @@ import torch
 
 import torch_geometric
 from torch_geometric.nn import MultiAggregation
-from torch_geometric.testing import withCUDA
+from torch_geometric.testing import withCUDA, disableExtensions, withPackage
 
 
-@withCUDA
-@pytest.mark.parametrize('aggr_kwargs, expand', [
+# The 'cat' combine mode will expand the output dimensions by
+# the number of aggregators which is 3 here, while the other
+# modes keep output dimensions unchanged.
+parameters = [
     (pytest.param(dict(mode='cat'), 3, id='cat')),
     (pytest.param(
         dict(mode='proj', mode_kwargs=dict(in_channels=16, out_channels=16)),
@@ -22,11 +24,12 @@ from torch_geometric.testing import withCUDA
     (pytest.param(dict(mode='logsumexp'), 1, id='logsumexp')),
     (pytest.param(dict(mode='std'), 1, id='std')),
     (pytest.param(dict(mode='var'), 1, id='var')),
-])
+]
+
+
+@withCUDA
+@pytest.mark.parametrize('aggr_kwargs, expand', parameters)
 def test_multi_aggr(aggr_kwargs, expand, device):
-    # The 'cat' combine mode will expand the output dimensions by
-    # the number of aggregators which is 3 here, while the other
-    # modes keep output dimensions unchanged.
     x = torch.randn(7, 16, device=device)
     index = torch.tensor([0, 0, 1, 1, 1, 2, 3], device=device)
     ptr = torch.tensor([0, 2, 5, 6, 7], device=device)
@@ -52,7 +55,21 @@ def test_multi_aggr(aggr_kwargs, expand, device):
     jit = torch.jit.script(aggr)
     assert torch.allclose(out, jit(x, index))
 
-    if torch_geometric.typing.WITH_PT21:
-        opt = torch_geometric.compile(aggr)
-        atol = 1e-6 if aggr_kwargs['mode'] == 'std' else 1e-8
-        assert torch.allclose(out, opt(x, index), atol=atol)
+
+@withCUDA
+@disableExtensions
+@withPackage('torch>=2.1.0')
+@pytest.mark.parametrize('aggr_kwargs, _', parameters)
+def test_compile_multi_aggr(aggr_kwargs, _, device):
+    x = torch.randn(7, 16, device=device)
+    index = torch.tensor([0, 0, 1, 1, 1, 2, 3], device=device)
+
+    aggrs = ['mean', 'sum', 'max']
+    aggr = MultiAggregation(aggrs, **aggr_kwargs).to(device)
+    aggr.reset_parameters()
+    out1 = aggr(x, index)
+
+    opt = torch.compile(aggr)
+    out2 = opt(x, index)
+    atol = 1e-6 if aggr_kwargs['mode'] == 'std' else 1e-8
+    assert torch.allclose(out1, out2, atol=atol)
