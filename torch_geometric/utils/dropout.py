28573b905372442b9693e0db7cd23bd95a8490a1
Jintang Li <cnljt@outlook.com>
2023-12-05 11:36:42 +0800

    Update dropout.py

--- torch_geometric/utils/dropout.py
+++ torch_geometric/utils/dropout.py
@@ -91,8 +91,9 @@ def dropout_adj(
 
 
 def dropout_node(edge_index: Tensor, p: float = 0.5,
-                 num_nodes: Optional[int] = None, relabel_nodes: bool = False,
-                 training: bool = True) -> Tuple[Tensor, Tensor, Tensor]:
+                 num_nodes: Optional[int] = None, 
+                 training: bool = True,
+                 relabel_nodes: bool = False) -> Tuple[Tensor, Tensor, Tensor]:
     r"""Randomly drops nodes from the adjacency matrix
     :obj:`edge_index` with probability :obj:`p` using samples from
     a Bernoulli distribution.
@@ -106,11 +107,11 @@ def dropout_node(edge_index: Tensor, p: float = 0.5,
         p (float, optional): Dropout probability. (default: :obj:`0.5`)
         num_nodes (int, optional): The number of nodes, *i.e.*
             :obj:`max_val + 1` of :attr:`edge_index`. (default: :obj:`None`)
-        relabel_nodes (bool, optional): If set to `True`, the resulting
-            `edge_index` will be relabeled to hold consecutive indices
-            starting from zero.
         training (bool, optional): If set to :obj:`False`, this operation is a
             no-op. (default: :obj:`True`)
+        relabel_nodes (bool, optional): If set to `True`, the resulting
+            `edge_index` will be relabeled to hold consecutive indices
+            starting from zero.            
 
     :rtype: (:class:`LongTensor`, :class:`BoolTensor`, :class:`BoolTensor`)
 
