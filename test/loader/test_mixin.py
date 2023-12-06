209428bd7cd59998eac087076b30e2fc14d0f39f
JakubPietrakIntel <jakub.pietrak@intel.com>
2023-12-06 13:25:15 +0000

    test

--- test/loader/test_mixin.py
+++ test/loader/test_mixin.py
@@ -14,24 +14,29 @@ from torch_geometric.testing import onlyLinux, onlyNeighborSampler
 @pytest.mark.parametrize('loader_cores', [None, [1, 2]])
 def test_cpu_affinity_neighbor_loader(loader_cores, spawn_context):
     data = Data(x=torch.randn(1, 1))
-    loader = NeighborLoader(data, num_neighbors=[-1], batch_size=1,
-                            num_workers=2)
+    loader = NeighborLoader(
+        data, num_neighbors=[-1], batch_size=1, num_workers=2
+    )
     out = []
     with loader.enable_cpu_affinity(loader_cores):
         iterator = loader._get_iterator()
         workers = iterator._workers
+        sleep(3)  # Gives time for worker to initialize.
         for worker in workers:
-            sleep(3)  # Gives time for worker to initialize.
             process = subprocess.Popen(
-                ['taskset', '-c', '-p', f'{worker.pid}'],
-                stdout=subprocess.PIPE)
+                ['taskset', '-c', '-p', f'{worker.pid}'], stdout=subprocess.PIPE
+            )
             stdout = process.communicate()[0].decode('utf-8')
-            # returns "pid <pid>'s current affinity list: <n>-<m>"
+            # returns "pid <pid>'s current affinity list <n>-<m>"
             out.append(stdout.split(':')[1].strip())
         if loader_cores:
             out == ['[1]', '[2]']
         else:
-            assert out[0] != out[1]
+            n, m = out[0].split('-')
+            assert int(n) == 0
+            assert int(out[1].split('-')[0]) == int(m) + 1
+            # test if threads assigned to workers are in two consecutive ranges
+            # (0-n), (n+1, m)
 
 
 def init_fn(worker_id):
