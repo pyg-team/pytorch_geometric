125b5cfd1b4922418bde892b4fc5de0f751408ba
Simon Bele <simonbele@Simons-MacBook-Pro.local>
2023-12-07 10:15:00 +0100

    refactored

---
+++ examples/explain/xgnn/xgnn_model.py
@@ -0,0 +1,60 @@
+from torch_geometric.nn import GCNConv
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import global_mean_pool
+
+def reset_parameters(module):
+    if isinstance(module, torch.nn.Linear):
+        module.reset_parameters()
+        print(module.parameters())
+
+### GCN to predict graph property
+class GCN_Graph(torch.nn.Module):
+    def __init__(self, input_dim, output_dim, dropout, emb = False):
+        super(GCN_Graph, self).__init__()
+
+        self.dropout = dropout
+        self.convs = torch.nn.ModuleList([GCNConv(in_channels = input_dim, out_channels = 32),
+                                          GCNConv(in_channels = 32,        out_channels = 48),
+                                          GCNConv(in_channels = 48,        out_channels = 64)])
+
+        self.pool = global_mean_pool # global averaging to obtain graph representation
+
+        # self.post_mp = torch.nn.Sequential(torch.nn.Linear(64, 32),
+        #                                    torch.nn.Dropout(self.dropout),
+        #                                    torch.nn.Linear(32, output_dim))
+
+        self.fc1 = torch.nn.Linear(64, 32)
+        self.fc2 = torch.nn.Linear(32, output_dim)
+
+        self.loss = torch.nn.BCEWithLogitsLoss()
+        self.reset_parameters()
+
+    def reset_parameters(self):
+      for conv in self.convs:
+          conv.reset_parameters()
+      self.fc1.reset_parameters()
+      self.fc2.reset_parameters()
+
+    def forward(self, batched_data):
+        # Extract important attributes of our mini-batch
+        x, edge_index, batch = batched_data.x, batched_data.edge_index, batched_data.batch
+        embed = x.to(device)  # Ensure the embedding tensor is on the correct device
+
+        for i in range(len(self.convs)):
+            x = F.relu(self.convs[i](x, edge_index))
+            x = F.dropout(x, p=self.dropout, training=self.training)
+        # print(x)
+        # x = torch.mean(x, 1)
+        # print(x)
+        x = self.pool(x, batch)
+        # print(x)
+
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = self.fc2(x)
+        # print("size of x")
+        # print(x.size())
+        x = F.softmax(x, dim=1)
+        # print(x.size())
+        return x
---
+++ examples/explain/xgnn/xgnn_train.py
@@ -0,0 +1,114 @@
+from torch_geometric.data import Batch
+from torch_geometric.datasets import TUDataset
+import torch
+import torch.optim as optim
+from tqdm import trange
+import copy
+from tqdm.auto import trange
+
+from xgnn_model import GCN_Graph
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+def create_single_batch(dataset):
+    data_list = [data for data in dataset]
+    batched_data = Batch.from_data_list(data_list)
+    return batched_data
+
+def test(test_dataset, model):
+    model.eval()
+    correct = 0
+    total = test_dataset.num_graphs
+    with torch.no_grad():
+        pred = torch.round(model(test_dataset))
+        pred = pred.squeeze()
+        label = test_dataset.y.float()
+        labels_prob = torch.stack([1 - label, label], dim=1)
+        matches = (pred == labels_prob).float()
+
+    return torch.mean(matches)
+
+def train(dataset, args, train_indices, val_indices, test_indices):
+    # Split dataset into training and testing (validation is not used here)
+    train_dataset = create_single_batch([dataset[i] for i in train_indices]).to(device)
+    test_dataset = create_single_batch([dataset[i] for i in test_indices]).to(device)
+
+    # Model initialization
+    model = GCN_Graph(args.input_dim, output_dim=2, dropout=args.dropout).to(device)
+
+    opt = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay) # weight_decay=args.weight_decay
+
+    # Training loop
+    losses = []
+    test_accs = []
+    best_acc = 0
+    best_model = None
+    for epoch in trange(args.epochs, desc="Training", unit="Epoch"):
+        model.train()
+        opt.zero_grad()
+
+        pred = model(train_dataset)
+        label = train_dataset.y.float()
+        labels_prob = torch.stack([1 - label, label], dim=1)
+        loss = model.loss(pred, labels_prob)
+        loss.backward()
+        opt.step()
+        total_loss = loss.item()
+        losses.append(total_loss)
+
+        # Test accuracy
+        if epoch % 10 == 0:
+            test_acc = test(test_dataset, model)
+            test_accs.append(test_acc)
+            if test_acc > best_acc:
+                best_acc = test_acc
+                best_model = copy.deepcopy(model)
+        else:
+            test_accs.append(test_accs[-1])
+
+    return test_accs, losses, best_model, best_acc
+
+class objectview(object):
+    def __init__(self, d):
+        self.__dict__ = d
+
+args = {'batch_size': 32,
+        'dropout': 0.1,
+        'epochs': 1000,
+        'input_dim' : 7,
+        'opt': 'adam',
+        'opt_scheduler': 'none',
+        'opt_restart': 0,
+        'weight_decay': 5e-5,
+        'lr': 0.001}
+
+args = objectview(args)
+
+dataset = TUDataset(root='/tmp/MUTAG', name='MUTAG')
+num_graphs = len(dataset)
+
+# Define split percentages
+train_percentage = 0.7
+val_percentage = 0.0
+
+# Calculate split sizes
+train_size = int(num_graphs * train_percentage)
+val_size = int(num_graphs * val_percentage)
+test_size = num_graphs - train_size - val_size
+
+# Create shuffled indices
+indices = np.random.permutation(num_graphs)
+train_indices = indices[:train_size]
+val_indices = indices[train_size:train_size + val_size]
+test_indices = indices[train_size + val_size:]
+
+test_accs, losses, best_model, best_acc = train(dataset, args, train_indices, val_indices, test_indices)
+
+print("Maximum test set accuracy: {0}".format(max(test_accs)))
+print("Minimum loss: {0}".format(min(losses)))
+
+plt.title(dataset.name)
+plt.plot(losses, label="training loss")
+plt.plot(test_accs, label="test accuracy")
+plt.legend()
+plt.show()
