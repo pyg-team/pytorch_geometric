d92b5ce7fafc2dc5d711706ba0a379f5859446a3
rusty1s <matthias.fey@tu-dortmund.de>
2023-12-10 12:00:35 +0000

    update

--- torch_geometric/datasets/fake.py
+++ torch_geometric/datasets/fake.py
@@ -89,10 +89,10 @@ class FakeDataset(InMemoryDataset):
             x = torch.randn(num_nodes, self.num_channels)
             if self._num_classes > 0 and self.task == 'node':
                 assert isinstance(data.y, Tensor)
-                x = x + data.y
+                x = x + data.y.unsqueeze(1)
             elif self._num_classes > 0 and self.task == 'graph':
                 assert isinstance(data.y, Tensor)
-                x = x + data.y.unsqueeze(1)
+                x = x + data.y
             data.x = x
         else:
             data.num_nodes = num_nodes
