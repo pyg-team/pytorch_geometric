190a30f9fa883a65935031f83a09586141168d1d
ananjan-nandi-9 <tgk.ananjan@gmail.com>
2023-12-19 20:06:23 -0800

    Changed value for masked tails

--- torch_geometric/nn/kge/base.py
+++ torch_geometric/nn/kge/base.py
@@ -145,7 +145,7 @@ class KGEModel(torch.nn.Module):
                         continue
                     mask_indices.append(e_id)
                 mask_indices = torch.LongTensor(mask_indices).to(head_index.device)
-                flattened_scores.index_fill_(0, mask_indices, -1)
+                flattened_scores.index_fill_(0, mask_indices, -100)
             rank = int((flattened_scores.argsort(
                 descending=True) == t).nonzero().view(-1))
             mean_ranks.append(rank)
