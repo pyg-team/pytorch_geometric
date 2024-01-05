d575f6b5cb1a9ac59b86dce46a6ae859a512d609
rusty1s <matthias.fey@tu-dortmund.de>
2024-01-05 18:01:30 +0000

    update

--- torch_geometric/nn/conv/message_passing.py
+++ torch_geometric/nn/conv/message_passing.py
@@ -2,7 +2,6 @@ import inspect
 import os.path as osp
 import random
 import re
-import typing
 from inspect import Parameter
 from itertools import chain
 from typing import (
@@ -43,11 +42,6 @@ from torch_geometric.utils import (
     to_edge_index,
 )
 from torch_geometric.utils.sparse import ptr2index
-
-if typing.TYPE_CHECKING:
-    from typing import overload
-else:
-    from torch.jit import _overload_method as overload
 
 FUSE_AGGRS = {'add', 'sum', 'mean', 'min', 'max'}
 HookDict = OrderedDict[int, Callable]
@@ -203,23 +197,7 @@ class MessagePassing(torch.nn.Module):
 
     # Utilities ###############################################################
 
-    @overload
     def _check_input(
-        self,
-        edge_index: Tensor,
-        size: Size,
-    ) -> List[Optional[int]]:
-        pass
-
-    @overload
-    def _check_input(  # noqa: F811
-        self,
-        edge_index: SparseTensor,
-        size: Size,
-    ) -> List[Optional[int]]:
-        pass
-
-    def _check_input(  # noqa: F811
         self,
         edge_index: Union[Tensor, SparseTensor],
         size: Optional[Tuple[int, int]],
@@ -274,25 +252,7 @@ class MessagePassing(torch.nn.Module):
                 (f'Encountered tensor with size {src.size(self.node_dim)} in '
                  f'dimension {self.node_dim}, but expected size {the_size}.'))
 
-    @overload
     def _lift(
-        self,
-        src: Tensor,
-        edge_index: Tensor,
-        dim: int,
-    ) -> Tensor:
-        pass
-
-    @overload
-    def _lift(  # noqa: F811
-        self,
-        src: Tensor,
-        edge_index: SparseTensor,
-        dim: int,
-    ) -> Tensor:
-        pass
-
-    def _lift(  # noqa: F811
         self,
         src: Tensor,
         edge_index: Union[Tensor, SparseTensor],
@@ -364,27 +324,7 @@ class MessagePassing(torch.nn.Module):
              'shape `[2, num_messages]`, `torch_sparse.SparseTensor` '
              'or `torch.sparse.Tensor` for argument `edge_index`.'))
 
-    @overload
     def _collect(
-        self,
-        args: Set[str],
-        edge_index: Tensor,
-        size: List[Optional[int]],
-        kwargs: Dict[str, Any],
-    ) -> Dict[str, Any]:
-        pass
-
-    @overload
-    def _collect(  # noqa: F811
-        self,
-        args: Set[str],
-        edge_index: SparseTensor,
-        size: List[Optional[int]],
-        kwargs: Dict[str, Any],
-    ) -> Dict[str, Any]:
-        pass
-
-    def _collect(  # noqa: F811
         self,
         args: Set[str],
         edge_index: Union[Tensor, SparseTensor],
