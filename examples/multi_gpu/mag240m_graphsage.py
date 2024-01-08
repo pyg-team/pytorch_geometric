d3438d1361242a916b5c28467169e8fa2d09f5d5
puririshi98 <puririshi98@berkeley.edu>
2024-01-08 14:14:48 -0800

    trying undirected again

--- examples/multi_gpu/mag240m_graphsage.py
+++ examples/multi_gpu/mag240m_graphsage.py
@@ -172,12 +172,10 @@ def run(
         shuffle=True,
         drop_last=True,
     )
-    undirected_transform = torch_geometric.transforms.ToUndirected()
     train_loader = NeighborLoader(
         data,
         input_nodes=("paper", train_idx),
         num_neighbors=sizes,
-        transform=undirected_transform,
         **kwargs,
     )
 
@@ -186,14 +184,12 @@ def run(
             data,
             input_nodes=("paper", eval_idx),
             num_neighbors=sizes,
-            transform=undirected_transform,
             **kwargs,
         )
         test_loader = NeighborLoader(
             data,
             input_nodes=("paper", eval_idx),
             num_neighbors=sizes,
-            transform=undirected_transform,
             **kwargs,
         )
     if rank == 0:
