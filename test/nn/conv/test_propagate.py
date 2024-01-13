90883f00d2702e17030ef2331dfe7c3d237f7320
Matthias Fey <matthias.fey@tu-dortmund.de>
2024-01-13 09:44:51 +0100

    Implement `torch.sparse` and `SparseTensor` support in `propagate.jinja` (#8759)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- test/nn/conv/test_propagate.py
+++ test/nn/conv/test_propagate.py
@@ -1,11 +1,15 @@
+import pytest
 import torch
 
 from torch_geometric.nn import SAGEConv
+from torch_geometric.utils import to_torch_csc_tensor
 
 
-def test_propagate():
+@pytest.mark.parametrize('fuse', [True, False])
+def test_propagate(fuse):
     SAGEConv.jit_on_init = True
-    conv = SAGEConv(10, 16)
+    conv = SAGEConv(16, 32)
+    conv.fuse = fuse
     SAGEConv.jit_on_init = False
 
     old_propagate = conv.__class__.propagate
@@ -13,8 +17,17 @@ def test_propagate():
 
     try:
         conv.__class__.propagate = conv.propagate_jit
-        conv.__class__._collect = conv._collect_jit
-        torch.jit.script(conv)
+        conv.__class__._collect = conv.collect_jit
+        jit = torch.jit.script(conv)
+
+        x = torch.randn(4, 16)
+        edge_index = torch.tensor([[0, 0, 0, 1, 2, 3], [1, 2, 3, 0, 0, 0]])
+        adj = to_torch_csc_tensor(edge_index, size=(4, 4))
+
+        out1 = jit(x, edge_index)
+        out2 = jit(x, adj.t())
+        assert torch.allclose(out1, out2)
+
     finally:
         conv.__class__.propagate = old_propagate
         conv.__class__._collect = old_collect
