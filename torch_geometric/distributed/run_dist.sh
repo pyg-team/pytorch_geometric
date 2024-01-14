85343cecb54b04590e81c34cd5893a0602bfe6da
ZhengHongming888 <hongming.zheng@intel.com>
2024-01-13 21:47:33 -0800

    update launch.py & run_dist.sh

--- torch_geometric/distributed/run_dist.sh
+++ torch_geometric/distributed/run_dist.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 
-# assume user_name as XXX
-# anaconda virtual env used here.
-
-CONDA_ENV=/home/XXX/anaconda3/envs/PyGDistributed
-PYG_WORKSPACE=/home/XXX/01_distributed
+CONDA_ENV=/home/XXX/anaconda3/envs/pyg24
+PYG_WORKSPACE=$PWD 
 PY_EXEC=${CONDA_ENV}/bin/python
-EXEC_SCRIPT=${PYG_WORKSPACE}/e2e_homo.py
+EXEC_SCRIPT=${PYG_WORKSPACE}/distributed_cpu.py
 
 # node number
 NUM_NODES=2
 
 # dataset folder
-DATASET_ROOT_DIR="/home/XXX/partition_ds/products"
+DATASET_ROOT_DIR="/home/XXX/mag/2-parts"
 
 # process number for training
 NUM_TRAINING_PROCS=1
 
 # dataset name
-DATASET=ogbn-product
+DATASET=ogbn-mag
 
 # num epochs to run for
-EPOCHS=2
+NUM_EPOCHS=3
 
 BATCH_SIZE=1024
 
@@ -30,9 +27,10 @@ NUM_WORKERS=2
 CONCURRENCY=2
 
 #partition data directory
-PART_CONFIG="/home/XXX/partition_ds/products/ogbn-products-partitions/META.json"
+PART_CONFIG="/home/XXX/mag/2-parts/ogbn-products-partitions/META.json"
 NUMPART=2
 
+DDP_PORT=12351
 # fanout per layer
 NUM_NEIGHBORS="15,10,5"
 
@@ -48,5 +46,6 @@ echo $logname
 set -x
 
 # stdout stored in /logdir/logname.out
-python launch.py --workspace ${PYG_WORKSPACE} --num_nodes ${NUM_NODES} --num_neighbors ${NUM_NEIGHBORS} --num_training_procs ${NUM_TRAINING_PROCS} --dataset_root_dir ${DATASET_ROOT_DIR} --dataset ${DATASET} --epochs ${EPOCHS} --batch_size ${BATCH_SIZE} --num_workers ${NUM_WORKERS} --concurrency ${CONCURRENCY} --part_config ${PART_CONFIG} --ip_config ${IP_CONFIG} "cd /home/XXX; source anaconda3/envs/PyGDistributed/bin/activate; cd ${PYG_WORKSPACE}; ${PY_EXEC} ${EXEC_SCRIPT}" |& tee ${logdir}/${logname}.txt
+python launch.py --workspace ${PYG_WORKSPACE} --num_nodes ${NUM_NODES} --num_neighbors ${NUM_NEIGHBORS} --dataset_root_dir ${DATASET_ROOT_DIR} --dataset ${DATASET}  --num_epochs ${NUM_EPOCHS} --batch_size ${BATCH_SIZE} --num_workers ${NUM_WORKERS} --concurrency ${CONCURRENCY} --ddp_port ${DDP_PORT} --part_config ${PART_CONFIG} --ip_config ${IP_CONFIG} "cd /home/XXX; source ${CONDA_ENV}/bin/activate; cd ${PYG_WORKSPACE}; ${PY_EXEC} ${EXEC_SCRIPT}" |& tee ${logdir}/${logname}.txt
 set +x
+
