a2eda561c483bc78cec160657a4bdef2b32c3910
Matthias Fey <matthias.fey@tu-dortmund.de>
2024-01-15 16:43:05 +0100

    Check enabling `MessagePassing` JIT mode by default (#8768)

--- torch_geometric/nn/models/label_prop.py
+++ torch_geometric/nn/models/label_prop.py
@@ -80,7 +80,7 @@ class LabelPropagation(MessagePassing):
 
         res = (1 - self.alpha) * out
         for _ in range(self.num_layers):
-            # propagate_type: (y: Tensor, edge_weight: OptTensor)
+            # propagate_type: (x: Tensor, edge_weight: OptTensor)
             out = self.propagate(edge_index, x=out, edge_weight=edge_weight)
             out.mul_(self.alpha).add_(res)
             if post_step is not None:
