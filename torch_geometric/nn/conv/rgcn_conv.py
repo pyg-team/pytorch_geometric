2954911c89dfe6e4e03d9e7a0d149f69c568be07
Akihiro Nitta <nitta@akihironitta.com>
2024-01-17 07:26:47 +0000

    Fix compile

--- torch_geometric/nn/conv/rgcn_conv.py
+++ torch_geometric/nn/conv/rgcn_conv.py
@@ -221,7 +221,8 @@ class RGCNConv(MessagePassing):
 
         else:  # No regularization/Basis-decomposition ========================
 
-            use_segment_matmul = torch_geometric.backend.use_segment_matmul
+            use_segment_matmul: Optional[bool] = torch_geometric.backend.use_segment_matmul
+            use_segment_matmul = not is_compiling()
             # If `use_segment_matmul` is not specified, use a simple heuristic
             # to determine whether `segment_matmul` can speed up computation
             # given the observed input sizes:
@@ -241,7 +242,7 @@ class RGCNConv(MessagePassing):
                 use_segment_matmul = self._use_segment_matmul_heuristic_output
 
             if (use_segment_matmul and torch_geometric.typing.WITH_SEGMM
-                    and not is_compiling() and self.num_bases is None
+                    and self.num_bases is None
                     and x_l.is_floating_point()
                     and isinstance(edge_index, Tensor)):
 
