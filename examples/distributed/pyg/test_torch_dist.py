4ed50e268f96e4316331a945336667867b45ea44
JakubPietrakIntel <jakub.pietrak@intel.com>
2024-01-17 15:47:39 +0000

    db

--- examples/distributed/pyg/test_torch_dist.py
+++ examples/distributed/pyg/test_torch_dist.py
@@ -1,6 +1,8 @@
 import os
 import torch
 import argparse
+import intel_extension_for_pytorch as ipex
+import oneccl_bindings_for_pytorch  # noqa
 import torch.distributed as dist
 
 master_addr = '10.211.176.217'
@@ -11,7 +13,7 @@ os.environ["MASTER_PORT"] = master_port
 node_rank = int(os.environ.get("RANK", -1))
 mpi_rank = int(os.environ.get("PMI_RANK", -1))
 mpi_world_size = int(os.environ.get("PMI_SIZE", -1))
-rank = (node_rank * mpi_world_size + mpi_rank,)
+rank = node_rank * mpi_world_size + mpi_rank
 world_size = 2 * mpi_world_size
 os.environ["RANK"] = str(rank)
 os.environ["WORLD_SIZE"] = str(world_size)
@@ -22,7 +24,7 @@ print(
 device = torch.device(f'xpu:{rank}')
 
 dist.init_process_group(
-    backend='ccl',
+    backend="ccl",
     rank=node_rank * mpi_world_size + mpi_rank,
     world_size=2 * mpi_world_size,
     init_method='tcp://{}:{}'.format(master_addr, master_port),
