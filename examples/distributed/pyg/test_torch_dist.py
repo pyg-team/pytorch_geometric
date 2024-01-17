f1095276debf5c0484958b7619d5bf4a6153aa6c
JakubPietrakIntel <jakub.pietrak@intel.com>
2024-01-17 16:14:03 +0000

    test

--- examples/distributed/pyg/test_torch_dist.py
+++ examples/distributed/pyg/test_torch_dist.py
@@ -5,8 +5,10 @@ import intel_extension_for_pytorch as ipex
 import oneccl_bindings_for_pytorch  # noqa
 import torch.distributed as dist
 
-master_addr = '10.211.176.217'
-master_port = '11111'
+num_nodes = 1
+
+master_addr = "127.0.0.1" #'10.211.176.217'
+master_port = "29500"# '11111'
 os.environ["MASTER_ADDR"] = master_addr
 os.environ["MASTER_PORT"] = master_port
 
@@ -14,23 +16,29 @@ node_rank = int(os.environ.get("RANK", -1))
 mpi_rank = int(os.environ.get("PMI_RANK", -1))
 mpi_world_size = int(os.environ.get("PMI_SIZE", -1))
 rank = node_rank * mpi_world_size + mpi_rank
-world_size = 2 * mpi_world_size
+world_size = mpi_world_size * num_nodes
 os.environ["RANK"] = str(rank)
 os.environ["WORLD_SIZE"] = str(world_size)
-print(
-    f"node_rank: {node_rank}, mpi_rank: {mpi_rank} -> rank={rank}\nmpi_world_size:{mpi_world_size}"
-)
 
-device = torch.device(f'xpu:{rank}')
+print(
+    f"node_rank: {node_rank}, mpi_rank: {mpi_rank} -> rank={rank}"
+)
+print(f"num_nodes: {num_nodes}, mpi_world_size:{mpi_world_size} -> world_size={world_size}")
 
 dist.init_process_group(
     backend="ccl",
-    rank=node_rank * mpi_world_size + mpi_rank,
-    world_size=2 * mpi_world_size,
+    rank=rank,
+    world_size=world_size,
     init_method='tcp://{}:{}'.format(master_addr, master_port),
 )
-test_tensor = torch.tensor(rank)
+device = torch.device(f'xpu:{rank}')
+print(f"{device}: ddp connected")
+# dist.barrier()
 
-x = dist.all_reduce(test_tensor, op=dist.ReduceOp.SUM)
-print(x)
-print(f"Test value: {test_tensor.item()}, expected: {sum(range(world_size))}")
+# test_tensor = torch.tensor(rank)
+
+# x = dist.all_reduce(test_tensor, op=dist.ReduceOp.SUM)
+# print(x)
+# print(f"Test value: {test_tensor.item()}, expected: {sum(range(world_size))}")
+# dist.barrier()
+# dist.destroy_process_group()
