0934d61359079441eabcdfac2d32f6c8d5868c3d
JakubPietrakIntel <jakub.pietrak@intel.com>
2024-01-17 17:06:50 +0000

    sum

--- examples/distributed/pyg/test_torch_dist.py
+++ examples/distributed/pyg/test_torch_dist.py
@@ -5,14 +5,21 @@ import torch.distributed as dist
 
 os.environ['MASTER_ADDR'] = '10.211.176.217'
 os.environ['MASTER_PORT'] = '11111'
-rank = int(os.getenv('RANK'))
+node_rank = int(os.environ.get("RANK", -1))
+mpi_rank = int(os.environ.get("PMI_RANK", -1))
+mpi_world_size = int(os.environ.get("PMI_SIZE", -1))
+rank = (node_rank * mpi_world_size + mpi_rank,)
+world_size = 2 * mpi_world_size
+print(
+    f"node_rank: {node_rank}, mpi_rank: {mpi_rank} -> rank={rank}\nmpi_world_size:{mpi_world_size}"
+)
 dist.init_process_group(
     backend='ccl',
-    rank=rank,
-    world_size=4,
+    rank=node_rank * mpi_world_size + mpi_rank,
+    world_size=2 * mpi_world_size,
 )
 test_tensor = torch.tensor(rank)
 
 x = dist.all_reduce(test_tensor, op=dist.ReduceOp.SUM)
 print(x)
-print(f"Test value: {test_tensor.item()}, expected: {sum(range(4))}")
+print(f"Test value: {test_tensor.item()}, expected: {sum(range(world_size))}")
