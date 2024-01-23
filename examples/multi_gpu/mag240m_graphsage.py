1c8f525ef4f21c38413818f8708bc14e6239a684
puririshi98 <puririshi98@berkeley.edu>
2024-01-23 11:47:52 -0800

    rewrite/cleanup

--- examples/multi_gpu/mag240m_graphsage.py
+++ examples/multi_gpu/mag240m_graphsage.py
@@ -17,12 +17,8 @@ from tqdm import tqdm
 from torch_geometric import seed_everything
 from torch_geometric.data import Batch
 from torch_geometric.loader.neighbor_loader import NeighborLoader
-<<<<<<< HEAD
 from torch_geometric.nn import SAGEConv, HeteroConv
 from torch_geometric.nn.norm import BatchNorm
-=======
-from torch_geometric.nn import HeteroConv, SAGEConv
->>>>>>> 22686124d059110419f404edd3a0840878bb7897
 from torch_geometric.typing import Adj, EdgeType, NodeType
 
 
