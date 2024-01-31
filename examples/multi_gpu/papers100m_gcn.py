e0fc93369ae47357aa713696c98d0d81ea42412e
Rishi Puri <puririshi98@berkeley.edu>
2024-01-30 16:32:48 -0800

    cuda sync for timing

--- examples/multi_gpu/papers100m_gcn.py
+++ examples/multi_gpu/papers100m_gcn.py
@@ -199,6 +199,7 @@ def run_train(rank, data, world_size, model, epochs, batch_size, fan_out,
         with Join([model], divide_by_initial_world_size=False):
             for i, batch in enumerate(train_loader):
                 if i == warmup_steps:
+                    torch.cuda.synchronize()
                     start = time.time()
                 batch = batch.to(rank)
 
