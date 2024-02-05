197234de98345920ebf4a27a198126bd3f741fe2
rusty1s <matthias.fey@tu-dortmund.de>
2024-02-05 12:34:11 +0000

    update

--- examples/distributed/pyg/launch.py
+++ examples/distributed/pyg/launch.py
@@ -1,5 +1,4 @@
 import argparse
-import json
 import logging
 import multiprocessing
 import os
@@ -15,48 +14,47 @@ from typing import Optional
 
 
 def clean_runs(get_all_remote_pids, conn):
-    """This process clean up the remaining remote training tasks."""
-    print("cleanup runs")
+    """This process cleans up the remaining remote training tasks."""
+    print("Cleanup runs")
     signal.signal(signal.SIGINT, signal.SIG_IGN)
-
     data = conn.recv()
-    # If the launch process exits normally, this process doesn't need to do anything.
+
+    # If the launch process exits normally, don't do anything:
     if data == "exit":
         sys.exit(0)
     else:
         remote_pids = get_all_remote_pids()
         for (ip, port), pids in remote_pids.items():
             kill_proc(ip, port, pids)
-    print("cleanup exits")
+    print("Cleanup exits")
 
 
 def kill_proc(ip, port, pids):
-    """Ssh to remote node and kill the specified proc."""
+    """SSH to remote nodes and kill the specified processes."""
     curr_pid = os.getpid()
     killed_pids = []
     pids.sort()
     for pid in pids:
         assert curr_pid != pid
-        print("kill process {} on {}:{}".format(pid, ip, port), flush=True)
+        print(f"Kill process {pid} on {ip}:{port}", flush=True)
         kill_cmd = ("ssh -o StrictHostKeyChecking=no -p " + str(port) + " " +
                     ip + " 'kill {}'".format(pid))
         subprocess.run(kill_cmd, shell=True)
         killed_pids.append(pid)
     for i in range(3):
-        killed_pids = get_pids_to_killed(ip, port, killed_pids)
+        killed_pids = get_pids_to_kill(ip, port, killed_pids)
         if len(killed_pids) == 0:
             break
         else:
             killed_pids.sort()
             for pid in killed_pids:
-                print("kill process {} on {}:{}".format(pid, ip, port),
-                      flush=True)
+                print(f"Kill process {pid} on {ip}:{port}", flush=True)
                 kill_cmd = ("ssh -o StrictHostKeyChecking=no -p " + str(port) +
                             " " + ip + " 'kill -9 {}'".format(pid))
                 subprocess.run(kill_cmd, shell=True)
 
 
-def get_pids_to_killed(ip, port, killed_pids):
+def get_pids_to_kill(ip, port, killed_pids):
     """Get the process IDs that we want to kill but are still alive."""
     killed_pids = [str(pid) for pid in killed_pids]
     killed_pids = ",".join(killed_pids)
@@ -65,9 +63,9 @@ def get_pids_to_killed(ip, port, killed_pids):
     res = subprocess.run(ps_cmd, shell=True, stdout=subprocess.PIPE)
     pids = []
     for p in res.stdout.decode("utf-8").split("\n"):
-        l = p.split()
-        if len(l) > 0:
-            pids.append(int(l[0]))
+        ps = p.split()
+        if len(ps) > 0:
+            pids.append(int(ps[0]))
     return pids
 
 
@@ -76,7 +74,7 @@ def remote_execute(
     state_q: queue.Queue,
     ip: str,
     port: int,
-    username: Optional[str] = "",
+    username: Optional[str] = None,
 ) -> Thread:
     """Execute command line on remote machine via ssh.
 
@@ -85,25 +83,21 @@ def remote_execute(
         state_q: A queue collecting Thread exit states.
         ip: The ip-address of the host to run the command on.
         port: Port number that the host is listening on.
-        thread_list:
-        username: Optional. If given, this will specify a username to use when issuing commands over SSH.
-            Useful when your infra requires you to explicitly specify a username to avoid permission issues.
+        username: If given, this will specify a username to use when issuing
+            commands over SSH. Useful when your infra requires you to
+            explicitly specify a username to avoid permission issues.
 
     Returns:
-        thread: The Thread whose run() is to run the `cmd` on the remote host. Returns when the cmd completes
-            on the remote host.
+        thread: The thread who runs the command on the remote host.
+            Returns when the command completes on the remote host.
     """
     ip_prefix = ""
-    if username:
-        ip_prefix += "{username}@".format(username=username)
+    if username is not None:
+        ip_prefix += f"{username}@"
 
     # Construct ssh command that executes `cmd` on the remote host
-    ssh_cmd = "ssh -o StrictHostKeyChecking=no -p {port} {ip_prefix}{ip} '{cmd}'".format(
-        port=str(port),
-        ip_prefix=ip_prefix,
-        ip=ip,
-        cmd=cmd,
-    )
+    ssh_cmd = (f"ssh -o StrictHostKeyChecking=no -p {port} {ip_prefix}{ip} "
+               f"'{cmd}'")
 
     print(f"----- ssh_cmd={ssh_cmd} ")
 
@@ -127,7 +121,7 @@ def remote_execute(
     )
     thread.setDaemon(True)
     thread.start()
-    # sleep for a while in case of ssh is rejected by peer due to busy connection
+    # Sleep for a while in case SSH is rejected by peer due to busy connection:
     time.sleep(0.2)
     return thread
 
@@ -136,22 +130,23 @@ def get_remote_pids(ip, port, cmd_regex):
     """Get the process IDs that run the command in the remote machine."""
     pids = []
     curr_pid = os.getpid()
-    # Here we want to get the python processes. We may get some ssh processes, so we should filter them out.
-    ps_cmd = ("ssh -o StrictHostKeyChecking=no -p " + str(port) + " " + ip +
-              " 'ps -aux | grep python | grep -v StrictHostKeyChecking'")
+    # We want to get the Python processes. However, we may get some SSH
+    # processes, so we should filter them out:
+    ps_cmd = (f"ssh -o StrictHostKeyChecking=no -p {port} {ip} "
+              f"'ps -aux | grep python | grep -v StrictHostKeyChecking'")
     res = subprocess.run(ps_cmd, shell=True, stdout=subprocess.PIPE)
     for p in res.stdout.decode("utf-8").split("\n"):
-        l = p.split()
-        if len(l) < 2:
+        ps = p.split()
+        if len(ps) < 2:
             continue
-        # We only get the processes that run the specified command.
+        # We only get the processes that run the specified command:
         res = re.search(cmd_regex, p)
-        if res is not None and int(l[1]) != curr_pid:
-            pids.append(l[1])
+        if res is not None and int(ps[1]) != curr_pid:
+            pids.append(ps[1])
 
     pid_str = ",".join([str(pid) for pid in pids])
-    ps_cmd = ("ssh -o StrictHostKeyChecking=no -p " + str(port) + " " + ip +
-              " 'pgrep -P {}'".format(pid_str))
+    ps_cmd = (f"ssh -o StrictHostKeyChecking=no -p {port} {ip} "
+              f" 'pgrep -P {pid_str}'")
     res = subprocess.run(ps_cmd, shell=True, stdout=subprocess.PIPE)
     pids1 = res.stdout.decode("utf-8").split("\n")
     all_pids = []
@@ -168,8 +163,9 @@ def get_all_remote_pids(hosts, ssh_port, udf_command):
     remote_pids = {}
     for node_id, host in enumerate(hosts):
         ip, _ = host
-        # When creating training processes in remote machines, we may insert some arguments
-        # in the commands. We need to use regular expressions to match the modified command.
+        # When creating training processes in remote machines, we may insert
+        # some arguments in the commands. We need to use regular expressions to
+        # match the modified command.
         cmds = udf_command.split()
         new_udf_command = " .*".join(cmds)
         pids = get_remote_pids(ip, ssh_port, new_udf_command)
@@ -178,7 +174,9 @@ def get_all_remote_pids(hosts, ssh_port, udf_command):
 
 
 def wrap_cmd_w_envvars(cmd: str, env_vars: str) -> str:
-    """Wraps a CLI command with desired env vars with the following properties:
+    """Wraps a CLI command with desired environment variables with the
+    following properties:
+
     Example:
         >>> cmd = "ls && pwd"
         >>> env_vars = "VAR1=value1 VAR2=value2"
@@ -192,7 +190,7 @@ def wrap_cmd_w_envvars(cmd: str, env_vars: str) -> str:
 
 
 def wrap_cmd_w_extra_envvars(cmd: str, env_vars: list) -> str:
-    """Wraps a CLI command with extra env vars
+    """Wraps a CLI command with extra environment variables.
 
     Example:
         >>> cmd = "ls && pwd"
@@ -212,28 +210,24 @@ def get_available_port(ip):
     for port in range(1234, 65535):
         try:
             sock.connect((ip, port))
-        except:
+        except Exception:
             return port
-    raise RuntimeError("Failed to get available port for ip~{}".format(ip))
+    raise RuntimeError(f"Failed to get available port for ip~{ip}")
 
 
 def submit_all_jobs(args, udf_command, dry_run=False):
     if dry_run:
-        print(
-            "Currently it's in dry run mode which means no jobs will be launched."
-        )
+        print("Dry run mode, no jobs will be launched")
+
     servers_cmd = []
     hosts = []
     thread_list = []
-    server_count_per_machine = 0
 
-    # Get the IP addresses of the cluster.
+    # Get the IP addresses of the cluster:
     ip_config = os.path.join(args.workspace, args.ip_config)
-    print(ip_config)
     with open(ip_config) as f:
         for line in f:
             result = line.strip().split()
-            print(f"----- result={result}")
             if len(result) == 2:
                 ip = result[0]
                 port = int(result[1])
@@ -243,7 +237,7 @@ def submit_all_jobs(args, udf_command, dry_run=False):
                 port = get_available_port(ip)
                 hosts.append((ip, port))
             else:
-                raise RuntimeError("Format error of ip_config.")
+                raise RuntimeError("Format error of 'ip_config'")
 
     state_q = queue.Queue()
 
@@ -255,20 +249,26 @@ def submit_all_jobs(args, udf_command, dry_run=False):
         cmd = (wrap_cmd_w_extra_envvars(cmd, args.extra_envs)
                if len(args.extra_envs) > 0 else cmd)
 
-        cmd = cmd[:
-                  -1] + f" --dataset_root_dir={args.dataset_root_dir} --dataset={args.dataset} --num_nodes={args.num_nodes} --num_neighbors={args.num_neighbors} --node_rank={i} --master_addr={master_ip} --num_epochs={args.num_epochs} --batch_size={args.batch_size} --num_workers={args.num_workers} --concurrency={args.concurrency} --logging --progress_bar --ddp_port={args.ddp_port}" + ")"
+        cmd = cmd[:-1]
+        cmd += f" --dataset_root_dir={args.dataset_root_dir}"
+        cmd += f" --dataset={args.dataset}"
+        cmd += f" --num_nodes={args.num_nodes}"
+        cmd += f" --num_neighbors={args.num_neighbors}"
+        cmd += f" --node_rank={i}"
+        cmd += f" --master_addr={master_ip}"
+        cmd += f" --num_epochs={args.num_epochs}"
+        cmd += f" --batch_size={args.batch_size}"
+        cmd += f" --num_workers={args.num_workers}"
+        cmd += f" --concurrency={args.concurrency}"
+        cmd += f" --logging --progress_bar --ddp_port={args.ddp_port})"
         servers_cmd.append(cmd)
+
         if not dry_run:
             thread_list.append(
-                remote_execute(
-                    cmd,
-                    state_q,
-                    ip,
-                    args.ssh_port,
-                    username=args.ssh_username,
-                ))
+                remote_execute(cmd, state_q, ip, args.ssh_port,
+                               username=args.ssh_username))
 
-    # Start a cleanup process dedicated for cleaning up remote training jobs.
+    # Start a cleanup process dedicated for cleaning up remote training jobs:
     conn1, conn2 = multiprocessing.Pipe()
     func = partial(get_all_remote_pids, hosts, args.ssh_port, udf_command)
     process = multiprocessing.Process(target=clean_runs, args=(func, conn1))
@@ -276,7 +276,7 @@ def submit_all_jobs(args, udf_command, dry_run=False):
 
     def signal_handler(signal, frame):
         logging.info("Stop launcher")
-        # We need to tell the cleanup process to kill remote training jobs.
+        # We need to tell the cleanup process to kill remote training jobs:
         conn2.send("cleanup")
         sys.exit(0)
 
@@ -287,11 +287,10 @@ def submit_all_jobs(args, udf_command, dry_run=False):
         thread.join()
         err_code = state_q.get()
         if err_code != 0:
-            # Record err_code
-            # We record one of the error if there are multiple
-            err = err_code
+            err = err_code  # Record error code:
 
-    # The training processes complete. We should tell the cleanup process to exit.
+    # The training processes completed.
+    # We tell the cleanup process to exit.
     conn2.send("exit")
     process.join()
     if err != 0:
@@ -302,134 +301,132 @@ def submit_all_jobs(args, udf_command, dry_run=False):
 
 def main():
     parser = argparse.ArgumentParser(description="Launch a distributed job")
-    parser.add_argument("--ssh_port", type=int, default=22, help="SSH Port.")
     parser.add_argument(
-        "--ssh_username", default="", help=
-        "Optional. When issuing commands (via ssh) to cluster, use the provided username in the ssh cmd. "
-        "Example: If you provide --ssh_username=bob, then the ssh command will be like: 'ssh bob@1.2.3.4 CMD' "
+        "--ssh_port",
+        type=int,
+        default=22,
+        help="SSH port",
     )
-    parser.add_argument("--workspace", type=str,
-                        help="Path of user directory of distributed tasks. ")
+    parser.add_argument(
+        "--ssh_username",
+        type=str,
+        default="",
+        help=("When issuing commands (via ssh) to the cluster, use the "
+              "provided username in the ssh cmd. For example, if you provide "
+              "--ssh_username=bob, then the ssh command will be like "
+              "'ssh bob@1.2.3.4 CMD'"),
+    )
+    parser.add_argument(
+        "--workspace",
+        type=str,
+        required=True,
+        help="Path of user directory of distributed tasks",
+    )
     parser.add_argument(
         "--dataset",
         type=str,
-        default='ogbn-products',
-        help="The name of ogbn dataset.",
+        default="ogbn-products",
+        help="The name of the dataset",
     )
     parser.add_argument(
         "--dataset_root_dir",
         type=str,
         default='../../data/products',
-        help="The root directory (relative path) of partitioned ogbn dataset.",
+        help="The root directory (relative path) of partitioned dataset",
     )
-
     parser.add_argument(
         "--num_nodes",
         type=int,
         default=2,
-        help="Number of distributed nodes.",
+        help="Number of distributed nodes",
     )
     parser.add_argument(
         "--num_neighbors",
         type=str,
         default="15,10,5",
-        help="number of the layers",
+        help="Number of node neighbors sampled at each layer",
     )
-
     parser.add_argument(
         "--node_rank",
         type=int,
         default=0,
-        help="The current node rank.",
+        help="The current node rank",
     )
     parser.add_argument(
         "--num_training_procs",
         type=int,
         default=2,
-        help="The number of traning processes per node.",
+        help="The number of training processes per node",
     )
-
     parser.add_argument(
         "--master_addr",
         type=str,
         default='localhost',
-        help="The master address for RPC initialization.",
+        help="The master address for RPC initialization",
     )
-
     parser.add_argument(
         "--num_epochs",
         type=int,
         default=100,
-        help="The number of training epochs.",
+        help="The number of training epochs",
     )
     parser.add_argument(
         "--batch_size",
         type=int,
         default=1024,
-        help="Batch size for the training and testing dataloader.",
+        help="Batch size for training and testing",
     )
     parser.add_argument(
         "--num_workers",
         type=int,
         default=2,
-        help="number of sampler workers.",
+        help="Number of sampler sub-processes",
     )
     parser.add_argument(
         "--concurrency",
         type=int,
         default=2,
-        help="concurrency number for mp.queue to send the sampler output.",
+        help="Number of maximum concurrent RPC for each sampler",
     )
     parser.add_argument(
         "--learning_rate",
         type=float,
         default=0.0004,
-        help="learning rate",
+        help="Learning rate",
     )
     parser.add_argument(
         '--ddp_port',
         type=int,
         default=11111,
-        help='The port used for PyTorch\'s DDP communication.',
+        help="Port used for PyTorch's DDP communication",
     )
-
     parser.add_argument(
         "--part_config",
         type=str,
-        help="The file (in workspace) of the partition config",
+        required=True,
+        help="File (in workspace) of the partition configuration",
     )
     parser.add_argument(
         "--ip_config",
+        required=True,
         type=str,
-        help="The file (in workspace) of IP configuration for server processes",
+        help="File (in workspace) of IP configuration for server processes",
     )
     parser.add_argument(
         "--extra_envs",
         nargs="+",
         type=str,
         default=[],
-        help="Extra environment parameters need to be set. For example, \
-                        you can set the LD_LIBRARY_PATH by adding: \
-                        --extra_envs LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH ",
+        help=("Extra environment parameters be set. For example, you can set "
+              "the 'LD_LIBRARY_PATH' by adding: --extra_envs "
+              "LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH"),
     )
     args, udf_command = parser.parse_known_args()
 
-    print(f"------ udf_command={udf_command}")
-
-    assert len(udf_command) == 1, "Please provide user command line."
-    assert (args.workspace
-            is not None), "A user has to specify a workspace with --workspace."
-    assert (
-        args.part_config is not None
-    ), "A user has to specify a partition configuration file with --part_config."
-    assert (
-        args.ip_config is not None
-    ), "A user has to specify an IP configuration file with --ip_config."
-
     udf_command = str(udf_command[0])
     if "python" not in udf_command:
-        raise RuntimeError(
-            "PyG launching script can only support Python executable file.")
+        raise RuntimeError("Launching script does only support a Python "
+                           "executable file")
     submit_all_jobs(args, udf_command)
 
 
--- examples/distributed/pyg/run_dist.sh
+++ examples/distributed/pyg/run_dist.sh
@@ -1,50 +1,48 @@
 #!/bin/bash
 
-CONDA_ENV=/home/XXX/anaconda3/envs/pyg24
+CONDA_ENV=/home/XXX/anaconda3/envs/pyg
 PYG_WORKSPACE=$PWD
 PY_EXEC=${CONDA_ENV}/bin/python
-EXEC_SCRIPT=${PYG_WORKSPACE}/distributed_cpu.py
+EXEC_SCRIPT=${PYG_WORKSPACE}/node_ogb_cpu.py
 
-# node number
+# Node number:
 NUM_NODES=2
 
-# dataset folder
+# Dataset folder:
 DATASET_ROOT_DIR="/home/XXX/mag/2-parts"
 
-# process number for training
-NUM_TRAINING_PROCS=1
-
-# dataset name
+# Dataset name:
 DATASET=ogbn-mag
 
-# num epochs to run for
+# Number of epochs:
 NUM_EPOCHS=3
 
+# The batch size:
 BATCH_SIZE=1024
 
-# number of workers for sampling
+# Number of workers for sampling:
 NUM_WORKERS=2
 CONCURRENCY=2
 
-#partition data directory
+# Partition data directory:
 PART_CONFIG="/home/XXX/mag/2-parts/ogbn-products-partitions/META.json"
-NUMPART=2
+NUM_PARTS=2
 
 DDP_PORT=12351
-# fanout per layer
+
+# Fanout per layer:
 NUM_NEIGHBORS="15,10,5"
 
-#ip_config path
+# IP configuration path:
 IP_CONFIG=${PYG_WORKSPACE}/ip_config.yaml
 
-
-# Folder and filename where you want your logs.
+# Folder and filename to place logs:
 logdir="logs"
 mkdir -p "logs"
-logname=log_${DATASET}_${NUMPART}_$RANDOM
+logname=log_${DATASET}_${NUM_PARTS}_$RANDOM
 echo $logname
 set -x
 
-# stdout stored in /logdir/logname.out
-python launch.py --workspace ${PYG_WORKSPACE} --num_nodes ${NUM_NODES} --num_neighbors ${NUM_NEIGHBORS} --dataset_root_dir ${DATASET_ROOT_DIR} --dataset ${DATASET}  --num_epochs ${NUM_EPOCHS} --batch_size ${BATCH_SIZE} --num_workers ${NUM_WORKERS} --concurrency ${CONCURRENCY} --ddp_port ${DDP_PORT} --part_config ${PART_CONFIG} --ip_config ${IP_CONFIG} "cd /home/XXX; source ${CONDA_ENV}/bin/activate; cd ${PYG_WORKSPACE}; ${PY_EXEC} ${EXEC_SCRIPT}" |& tee ${logdir}/${logname}.txt
+# stdout stored in `/logdir/logname.out`.
+python launch.py --workspace "${PYG_WORKSPACE}" --num_nodes ${NUM_NODES} --num_neighbors ${NUM_NEIGHBORS} --dataset_root_dir ${DATASET_ROOT_DIR} --dataset ${DATASET}  --num_epochs ${NUM_EPOCHS} --batch_size ${BATCH_SIZE} --num_workers ${NUM_WORKERS} --concurrency ${CONCURRENCY} --ddp_port ${DDP_PORT} --part_config ${PART_CONFIG} --ip_config "${IP_CONFIG}" "cd /home/XXX; source ${CONDA_ENV}/bin/activate; cd ${PYG_WORKSPACE}; ${PY_EXEC} ${EXEC_SCRIPT}" |& tee ${logdir}/${logname}.txt
 set +x
