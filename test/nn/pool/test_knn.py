1b08c5206f3b765ff0731f8ade1e956599e90f38
Rishi Puri <puririshi98@berkeley.edu>
2024-02-22 13:15:11 -0800

    adding L2 and MIPS knn approx ot unit test

--- test/nn/pool/test_knn.py
+++ test/nn/pool/test_knn.py
@@ -1,7 +1,7 @@
 import pytest
 import torch
 
-from torch_geometric.nn import ApproxMIPSKNNIndex, L2KNNIndex, MIPSKNNIndex
+from torch_geometric.nn import ApproxMIPSKNNIndex, ApproxL2KNNIndex, L2KNNIndex, MIPSKNNIndex
 from torch_geometric.testing import withCUDA, withPackage
 
 
@@ -48,6 +48,29 @@ def test_MIPS_knn(device, k):
 
     mat = lhs @ rhs.t()
     score, index = mat.sort(dim=-1, descending=True)
+
+    assert torch.allclose(out.score, score[:, :k])
+    assert torch.equal(out.index, index[:, :k])
+
+@withCUDA
+@withPackage('faiss')
+@pytest.mark.parametrize('k', [2])
+def test_L2_knn(device, k):
+    lhs = torch.randn(10, 16, device=device)
+    rhs = torch.randn(100, 16, device=device)
+
+    index = ApproxL2KNNIndex(rhs)
+    assert index.get_emb().device == device
+    assert torch.equal(index.get_emb(), rhs)
+
+    out = index.search(lhs, k)
+    assert out.score.device == device
+    assert out.index.device == device
+    assert out.score.size() == (10, k)
+    assert out.index.size() == (10, k)
+
+    mat = torch.linalg.norm(lhs.unsqueeze(1) - rhs.unsqueeze(0), dim=-1).pow(2)
+    score, index = mat.sort(dim=-1)
 
     assert torch.allclose(out.score, score[:, :k])
     assert torch.equal(out.index, index[:, :k])
