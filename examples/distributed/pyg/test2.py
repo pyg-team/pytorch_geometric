4bfaf4b546c7e80d75525fa06a889c164d245b6c
JakubPietrakIntel <jakub.pietrak@intel.com>
2024-02-29 16:00:05 +0000

    test mods

---
+++ examples/distributed/pyg/test2.py
@@ -0,0 +1,40 @@
+
+import os
+import socket
+from torch.multiprocessing import Process
+import intel_extension_for_pytorch as ipex
+import oneccl_bindings_for_pytorch  # noqa
+import torch
+import torch.distributed as dist
+
+def run(rank, size, hostname):
+
+    print(f"I am {rank} of {size} in {hostname}")
+    mpi_rank = int(os.environ.get("PMI_RANK", -1))
+    device = torch.device(f'xpu:{mpi_rank}')
+    tensor = torch.zeros(1).to(device)
+    dist.barrier()
+    if rank == 0:
+        tensor += 1
+        # Send the tensor to process 1
+        dist.send(tensor=tensor, dst=1)
+    else:
+        # Receive tensor from process 0
+        dist.recv(tensor=tensor, src=0)
+    print('Rank ', rank, ' has data ', tensor[0])
+
+if __name__ == "__main__":
+    size = int(os.environ.get("PMI_SIZE", -1))
+    rank = int(os.environ.get("PMI_RANK", -1))
+    master_addr = "10.211.176.210" #DUT1005
+    master_port = "29500" # '11111'
+    os.environ["MASTER_ADDR"] = master_addr
+    os.environ["MASTER_PORT"] = master_port
+    hostname = socket.gethostname()
+    dist.init_process_group(
+        backend="ccl", 
+        rank=rank, 
+        world_size=size,
+        init_method=f"tcp://{master_addr}:{master_port}",
+    )
+    run(rank, size, hostname)
--- examples/distributed/pyg/test_torch_dist.py
+++ examples/distributed/pyg/test_torch_dist.py
@@ -9,7 +9,7 @@ logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
 num_nodes = 2
 master_addr = "10.211.176.210" #DUT1005
-master_port = "11111" # '11111'
+master_port = "29500" # '11111'
 os.environ["MASTER_ADDR"] = master_addr
 os.environ["MASTER_PORT"] = master_port
 
@@ -28,31 +28,31 @@ logging.info(f"num_nodes: {num_nodes}, mpi_world_size:{mpi_world_size} -> world_
 
 dist.init_process_group(
     backend="ccl",
-    # rank=rank,
-    # world_size=world_size,
-    # init_method=f"tcp://{master_addr}:{master_port}",
+    rank=rank,
+    world_size=world_size,
+    init_method=f"tcp://{master_addr}:{master_port}",
 )
 
 device = torch.device(f'xpu:{mpi_rank}')
 my_rank = dist.get_rank()
 my_size = dist.get_world_size()
 logging.info(f"{device}: ddp connected my rank = {my_rank}  my size = {my_size})")
-dist.barrier()
-
-
-# test_tensor = torch.tensor(rank).to(device)
-# logging.info(test_tensor)
-# x = dist.all_reduce(test_tensor)
 # dist.barrier()
-# logging.info(f"node_rank: {node_rank}, mpi_rank: {mpi_rank} -> RESULT received: {x}, (expected: {sum(range(world_size))})")
 
-x = torch.ones([2, 2])
-y = torch.ones([4, 4])
-with torch.autograd.profiler.profile(record_shapes=True) as prof:
-    for _ in range(10):
-        dist.all_reduce(x)
-        dist.all_reduce(y)
-dist.barrier()
-print(prof.key_averages(group_by_input_shape=True).table(sort_by="self_cpu_time_total"))
 
-dist.destroy_process_group()
+test_tensor = torch.tensor(rank).to(device)
+logging.info(test_tensor)
+x = dist.all_reduce(test_tensor)
+# dist.barrier()
+logging.info(f"node_rank: {node_rank}, mpi_rank: {mpi_rank} -> RESULT received: {x}, (expected: {sum(range(world_size))})")
+
+# x = torch.ones([2, 2])
+# y = torch.ones([4, 4])
+# with torch.autograd.profiler.profile(record_shapes=True) as prof:
+#     for _ in range(10):
+#         dist.all_reduce(x)
+#         dist.all_reduce(y)
+# dist.barrier()
+# logging.info(prof.key_averages(group_by_input_shape=True).table(sort_by="self_cpu_time_total"))
+
+# dist.destroy_process_group()
