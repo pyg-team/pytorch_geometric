4925841ac942ec7d37df305325da9d830af235f2
rusty1s <matthias.fey@tu-dortmund.de>
2024-02-29 13:48:02 +0000

    update

--- examples/multi_gpu/mag240m_graphsage.py
+++ examples/multi_gpu/mag240m_graphsage.py
@@ -1,351 +1,255 @@
 import argparse
 import os
-import time
-from typing import Tuple
 
 import torch
 import torch.distributed as dist
 import torch.multiprocessing as mp
 import torch.nn.functional as F
 from ogb.lsc import MAG240MDataset
-from torch import Tensor
 from torch.nn.parallel import DistributedDataParallel
 from torchmetrics import Accuracy
+from tqdm import tqdm
 
-from torch_geometric import seed_everything
-from torch_geometric.data import Batch
-from torch_geometric.loader.neighbor_loader import NeighborLoader
-from torch_geometric.nn import HeteroConv, SAGEConv
-from torch_geometric.nn.norm import BatchNorm
+from torch_geometric.loader import NeighborLoader
+from torch_geometric.nn import BatchNorm, HeteroConv, SAGEConv
 
 
-def common_step(batch: Batch, model) -> Tuple[Tensor, Tensor]:
-    batch_size = batch["paper"].batch_size
-    y_hat = model(batch)["paper"][:batch_size]
-    y = batch["paper"].y[:batch_size].to(torch.long)
+def common_step(batch, model):
+    batch_size = batch['paper'].batch_size
+    x_dict = model(batch.x_dict, batch.edge_index_dict)
+    y_hat = x_dict['paper'][:batch_size]
+    y = batch['paper'].y[:batch_size].to(torch.long)
     return y_hat, y
 
 
-def training_step(batch: Batch, acc, model) -> Tuple[Tensor, Tensor]:
+def training_step(batch, acc, model):
     y_hat, y = common_step(batch, model)
     train_loss = F.cross_entropy(y_hat, y)
-    train_acc = acc(y_hat.softmax(dim=-1), y)
-    return train_loss, train_acc
+    acc(y_hat, y)
+    return train_loss
 
 
-def validation_step(batch: Batch, acc, model):
+def validation_step(batch, acc, model):
     y_hat, y = common_step(batch, model)
-    return acc(y_hat.softmax(dim=-1), y)
+    acc(y_hat, y)
 
 
-class SAGEConvLayer(torch.nn.Module):
-    def __init__(
-        self,
-        in_feat,
-        out_feat,
-        dropout,
-        edges,
-        nodes,
-        output_layer=False,
-    ):
+class HeteroSAGEConv(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, dropout, node_types,
+                 edge_types, is_output_layer=False):
         super().__init__()
-        self.in_feat = in_feat
-        self.out_feat = out_feat
-        self.conv = HeteroConv(
-            {e_type: SAGEConv(in_feat, out_feat)
-             for e_type in edges})
-        self.output_layer = output_layer
-        if not self.output_layer:
-            self.dropout_conv = torch.nn.Dropout(dropout)
-            self.activation = torch.nn.ReLU()
-            self.normalizations = torch.nn.ModuleDict()
-            for node in nodes:
-                self.normalizations[node] = BatchNorm(out_feat)
+        self.conv = HeteroConv({
+            edge_type: SAGEConv(in_channels, out_channels)
+            for edge_type in edge_types
+        })
+        if not is_output_layer:
+            self.norm_dict = torch.nn.ModuleDict({
+                node_type:
+                BatchNorm(out_channels)
+                for node_type in node_types
+            })
+
+        self.dropout = torch.nn.Dropout(dropout)
+        self.is_output_layer = is_output_layer
 
     def forward(self, x_dict, edge_index_dict):
-        h = self.conv(x_dict, edge_index_dict)
-        if not self.output_layer:
-            for node_type in h.keys():
-                h[node_type] = self.normalizations[node_type](self.activation(
-                    self.dropout_conv(h[node_type])))
-        return h
+        x_dict = self.conv(x_dict, edge_index_dict)
+        if not self.is_output_layer:
+            for node_type, norm in self.norm_dict.items():
+                x = norm(self.dropout(x_dict[node_type]).relu())
+                x_dict[node_type] = x
+        return x_dict
 
 
-class GraphSAGE(torch.nn.Module):
+class HeteroGraphSAGE(torch.nn.Module):
     def __init__(self, in_channels, hidden_channels, num_layers, out_channels,
-                 dropout, metadata):
+                 dropout, node_types, edge_types):
         super().__init__()
-        self.num_layers = num_layers
-        self.metadata = metadata
-        # only use edges where 'paper' is the source
-        # this will propogate info from paper to other node types
-        self.in_conv = SAGEConvLayer(
-            in_channels, in_channels, dropout,
-            [e for e in self.metadata[1] if e[0] == 'paper'], self.metadata[0])
-        # only use edges where 'institution' is not the source
-        # `institution` still has no features, learn them from `author`/`paper`
-        self.in_conv2 = SAGEConvLayer(
-            in_channels, hidden_channels, dropout,
-            [e for e in self.metadata[1] if e[0] != 'institution'],
-            self.metadata[0])
-        self.hidden_convs = []
-        if self.num_layers > 2:
-            for i in range(self.num_layers - 2):
-                self.hidden_convs.append(
-                    SAGEConvLayer(hidden_channels, hidden_channels, dropout,
-                                  self.metadata[1], self.metadata[0]))
-        self.output_conv = SAGEConvLayer(hidden_channels, out_channels,
-                                         dropout, self.metadata[1],
-                                         self.metadata[0], output_layer=True)
 
-    def forward(self, batch):
-        x_dict = batch.collect('x')
-        edge_index_dict = batch.collect('edge_index')
-        x_dict = self.in_conv(x_dict, edge_index_dict)
-        x_dict = self.in_conv2(x_dict, edge_index_dict)
-        if self.num_layers > 2:
-            for i in range(self.num_layers - 2):
-                x_dict = self.hidden_convs[i](x_dict, edge_index_dict)
-        x_dict = self.output_conv(x_dict, edge_index_dict)
+        self.convs = torch.nn.ModuleList()
+        for i in range(num_layers):
+            conv = HeteroSAGEConv(
+                in_channels if i == 0 else hidden_channels,
+                out_channels if i == num_layers - 1 else hidden_channels,
+                dropout=dropout,
+                node_types=node_types,
+                edge_types=edge_types,
+                is_output_layer=i == num_layers - 1,
+            )
+            self.convs.append(conv)
+
+    def forward(self, x_dict, edge_index_dict):
+        for conv in self.convs:
+            x_dict = conv(x_dict, edge_index_dict)
         return x_dict
 
 
 def run(
     rank,
     data,
-    n_devices=1,
+    num_devices=1,
     num_epochs=1,
     num_steps_per_epoch=-1,
     log_every_n_steps=1,
     batch_size=1024,
-    sizes=[128],
+    num_neighbors=[25, 15],
     hidden_channels=1024,
     dropout=0.5,
-    eval_steps=100,
-    num_warmup_iters_for_timing=10,
+    num_val_steps=100,
     lr=.001,
 ):
-    seed_everything(12345)
-    if n_devices > 1:
+    if num_devices > 1:
         if rank == 0:
             print("Setting up distributed...")
-        os.environ["MASTER_ADDR"] = "localhost"
-        os.environ["MASTER_PORT"] = "12355"
-        dist.init_process_group("nccl", rank=rank, world_size=n_devices)
-    if rank == 0:
-        print("Setting up GNN...")
-    acc = Accuracy(task="multiclass", num_classes=data.num_classes)
-    in_channels = data["paper"].x.size(-1)
-    model = GraphSAGE(
-        in_channels=in_channels,
+        os.environ['MASTER_ADDR'] = 'localhost'
+        os.environ['MASTER_PORT'] = '12355'
+        dist.init_process_group('nccl', rank=rank, world_size=num_devices)
+
+    acc = Accuracy(task='multiclass', num_classes=data.num_classes)
+    model = HeteroGraphSAGE(
+        in_channels=-1,
         hidden_channels=hidden_channels,
-        num_layers=len(sizes),
+        num_layers=len(num_neighbors),
         out_channels=data.num_classes,
         dropout=dropout,
-        metadata=data.metadata(),
+        node_types=data.node_types,
+        edge_types=data.edge_types,
     )
 
-    if rank == 0:
-        print(f"# GNN Params: \
-            {sum([p.numel() for p in model.parameters()])/10**6:.1f}M")
-        print('Setting up NeighborLoaders...')
-    train_idx = data["paper"].train_mask.nonzero(as_tuple=False).view(-1)
-    eval_idx = data["paper"].val_mask.nonzero(as_tuple=False).view(-1)
-    if n_devices > 1:
-        # Split indices into `n_devices` many chunks:
-        train_idx = train_idx.split(train_idx.size(0) // n_devices)[rank]
-        eval_idx = eval_idx.split(eval_idx.size(0) // n_devices)[rank]
+    train_idx = data['paper'].train_mask.nonzero(as_tuple=False).view(-1)
+    val_idx = data['paper'].val_mask.nonzero(as_tuple=False).view(-1)
+    if num_devices > 1:  # Split indices into `num_devices` many chunks:
+        train_idx = train_idx.split(train_idx.size(0) // num_devices)[rank]
+        val_idx = val_idx.split(val_idx.size(0) // num_devices)[rank]
 
-    # delete unused tensors to not sample
-    del data["paper"].train_mask
-    del data["paper"].val_mask
-    del data["paper"].test_mask
-    del data["paper"].year
+    # Delete unused tensors to not sample:
+    del data['paper'].train_mask
+    del data['paper'].val_mask
+    del data['paper'].test_mask
+    del data['paper'].year
 
-    kwargs = dict(batch_size=batch_size, num_workers=16,
-                  persistent_workers=True, num_neighbors=sizes, drop_last=True)
+    kwargs = dict(
+        batch_size=batch_size,
+        num_workers=16,
+        persistent_workers=True,
+        num_neighbors=num_neighbors,
+        drop_last=True,
+    )
+
     train_loader = NeighborLoader(
         data,
-        input_nodes=("paper", train_idx),
+        input_nodes=('paper', train_idx),
         shuffle=True,
         **kwargs,
     )
+    val_loader = NeighborLoader(data, input_nodes=('paper', val_idx), **kwargs)
 
-    eval_loader = NeighborLoader(
-        data,
-        input_nodes=("paper", eval_idx),
-        shuffle=True,
-        **kwargs,
-    )
-
-    # original OGB example also tests on eval_idx
-    # it saves test_idx for the final hidden test
-    # for the OGB competition.
-    test_loader = NeighborLoader(
-        data,
-        input_nodes=("paper", eval_idx),
-        **kwargs,
-    )
-
-    if rank == 0:
-        print("Final setup...")
-    if n_devices > 0:
+    if num_devices > 0:
         model = model.to(rank)
         acc = acc.to(rank)
-    if rank == 0:
-        print("about to make optimizer")
-    if n_devices > 1:
+    if num_devices > 1:
         model = DistributedDataParallel(model, device_ids=[rank])
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-    if rank == 0:
-        print("Beginning loop...")
+
     for epoch in range(1, num_epochs + 1):
         model.train()
-        time_sum = 0
-        for i, batch in enumerate(train_loader):
+        for i, batch in enumerate(tqdm(train_loader)):
             if num_steps_per_epoch >= 0 and i >= num_steps_per_epoch:
                 break
-            if i >= num_warmup_iters_for_timing:
-                torch.cuda.synchronize()
-                since = time.time()
-            optimizer.zero_grad()
 
-            if n_devices > 0:
-                batch = batch.to(rank, "x", "y", "edge_index")
-                # Features loaded in as fp16, train in 32bits
+            if num_devices > 0:
+                batch = batch.to(rank, 'x', 'y', 'edge_index')
+                # Features loaded in as 16 bits, train in 32 bits:
                 batch['paper'].x = batch['paper'].x.to(torch.float32)
-            loss, train_acc = training_step(batch, acc, model)
+
+            optimizer.zero_grad()
+            loss = training_step(batch, acc, model)
             loss.backward()
             optimizer.step()
-            if i >= num_warmup_iters_for_timing:
-                torch.cuda.synchronize()
-                iter_time = time.time() - since
-                time_sum += iter_time
-                if i % log_every_n_steps == 0:
-                    acc_sum = acc.compute()
-                    if rank == 0:
-                        print(f"Epoch: {epoch:02d}, Step: {i:d}, \
-                            Loss: {loss:.4f}, \
-                            Train Acc: {acc_sum * 100.0:.2f}%, \
-                            Most Recent Step Time: {iter_time:.4f}s")
-        if n_devices > 1:
-            dist.barrier()
-        acc_sum = acc.compute()
-        if rank == 0:
-            num_batches = i + 1
-            print(f"Epoch: {epoch:02d}, Loss: {loss:.4f}, \
-                Train Acc:{acc_sum * 100.0:.2f}%, \
-                Average Step Time: \
-                {time_sum/(num_batches - num_warmup_iters_for_timing):.4f}s")
 
-        # Eval
+            if i % log_every_n_steps == 0:
+                if rank == 0:
+                    print(f"Epoch: {epoch:02d}, Step: {i:d}, "
+                          f"Loss: {loss:.4f}, "
+                          f"Train Acc: {acc.compute():.4f}")
+
+        if num_devices > 1:
+            dist.barrier()
+
+        if rank == 0:
+            print(f"Epoch: {epoch:02d}, Loss: {loss:.4f}, "
+                  f"Train Acc :{acc.compute():.4f}")
+        acc.reset()
+
         model.eval()
         with torch.no_grad():
-            for i, batch in enumerate(eval_loader):
-                if eval_steps >= 0 and i >= eval_steps:
+            for i, batch in enumerate(tqdm(val_loader)):
+                if num_val_steps >= 0 and i >= num_val_steps:
                     break
-                if n_devices > 0:
-                    batch = batch.to(rank, "x", "y", "edge_index")
-                    # Features loaded in as fp16, train in 32bits
+
+                if num_devices > 0:
+                    batch = batch.to(rank, 'x', 'y', 'edge_index')
                     batch['paper'].x = batch['paper'].x.to(torch.float32)
-                val_acc = validation_step(batch, acc, model)  # noqa
-            acc_sum = acc.compute()
+
+                validation_step(batch, acc, model)
+
+            if num_devices > 1:
+                dist.barrier()
+
             if rank == 0:
-                print(f"Validation Accuracy: {acc_sum * 100.0:.4f}%", )
-    if n_devices > 1:
-        dist.barrier()
+                print(f"Val Acc: {acc.compute():.4f}")
+            acc.reset()
+
     model.eval()
 
-    # Test
-    with torch.no_grad():
-        for i, batch in enumerate(test_loader):
-            if n_devices > 0:
-                batch = batch.to(rank, "x", "y", "edge_index")
-                # Features loaded in as fp16, train in 32bits
-                batch['paper'].x = batch['paper'].x.to(torch.float32)
-            test_acc = validation_step(batch, acc, model)  # noqa
-        final_test_acc = acc.compute()
-        print(f"Test Accuracy: {final_test_acc:.4f}%", )
-    if n_devices > 1:
+    if num_devices > 1:
         dist.destroy_process_group()
-    torch.save(model, 'trained_graphsage_for_mag240m.pt')
-    assert final_test_acc >= 68.0
 
 
-if __name__ == "__main__":
-    help_str = "-1 by default means run the full \
-                    dataset each epoch, \
-                    otherwise select how many steps to take."
-
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--hidden_channels", type=int, default=1024)
     parser.add_argument("--batch_size", type=int, default=1024)
     parser.add_argument("--dropout", type=float, default=0.5)
     parser.add_argument("--lr", type=float, default=0.001)
-    parser.add_argument("--epochs", type=int, default=20)
-    parser.add_argument("--num_steps_per_epoch", type=int, default=-1,
-                        help=help_str)
+    parser.add_argument("--num_epochs", type=int, default=20)
+    parser.add_argument("--num_steps_per_epoch", type=int, default=-1)
     parser.add_argument("--log_every_n_steps", type=int, default=100)
-    parser.add_argument("--eval_steps", type=int, default=-1, help=50)
-    parser.add_argument("--num_warmup_iters_for_timing", type=int, default=100)
-    parser.add_argument(
-        "--subgraph", type=float, default=1,
-        help='decimal from (0,1] representing the \
-        portion of nodes to use in subgraph')
-    parser.add_argument("--sizes", type=str, default="25-15")
-    parser.add_argument("--n_devices", type=int, default=1,
-                        help="0 devices for CPU, or 1-8 to use GPUs")
+    parser.add_argument("--num_val_steps", type=int, default=-1, help=50)
+    parser.add_argument("--num_neighbors", type=str, default="25-15")
+    parser.add_argument("--num_devices", type=int, default=1)
     args = parser.parse_args()
-    args.sizes = [int(i) for i in args.sizes.split("-")]
-    print(args)
+
+    args.num_neighbors = [int(i) for i in args.num_neighbors.split('-')]
 
     import warnings
     warnings.simplefilter("ignore")
 
     if not torch.cuda.is_available():
-        print("No GPUs available, running with CPU")
-        args.n_devices = 0
-    if args.n_devices > torch.cuda.device_count():
-        print(
-            args.n_devices,
-            "GPUs requested but only",
-            torch.cuda.device_count(),
-            "GPUs available",
-        )
-        args.n_devices = torch.cuda.device_count()
-    print("Loading Data...")
+        args.num_devices = 0
+    elif args.num_devices > torch.cuda.device_count():
+        args.num_devices = torch.cuda.device_count()
+
     dataset = MAG240MDataset()
     data = dataset.to_pyg_hetero_data()
-    print("Data =", data)
 
-    if args.subgraph < 1.0:
-        print("Making a subgraph of the data to \
-            save and reduce hardware requirements...")
-        data = data.subgraph({
-            n_type:
-            torch.randperm(
-                data[n_type].num_nodes)[:int(data[n_type].num_nodes *
-                                             args.subgraph)]
-            for n_type in data.node_types
-        })
-    if args.n_devices > 1:
-        print("Let's use", args.n_devices, "GPUs!")
+    if args.num_devices > 1:
+        print("Let's use", args.num_devices, "GPUs!")
         from torch.multiprocessing.spawn import ProcessExitedException
         try:
             mp.spawn(
                 run,
                 args=(
                     data,
-                    args.n_devices,
-                    args.epochs,
+                    args.num_devices,
+                    args.num_epochs,
                     args.num_steps_per_epoch,
                     args.log_every_n_steps,
                     args.batch_size,
-                    args.sizes,
+                    args.num_neighbors,
                     args.hidden_channels,
                     args.dropout,
-                    args.eval_steps,
-                    args.num_warmup_iters_for_timing,
+                    args.num_val_steps,
                     args.lr,
                 ),
                 nprocs=args.n_devices,
@@ -356,22 +260,17 @@ if __name__ == "__main__":
             print("Exceptions/SIGBUS/Errors may be caused by a lack of RAM")
 
     else:
-        if args.n_devices == 1:
-            print("Using a single GPU")
-        else:
-            print("Using CPU")
         run(
             0,
             data,
-            args.n_devices,
-            args.epochs,
+            args.num_devices,
+            args.num_epochs,
             args.num_steps_per_epoch,
             args.log_every_n_steps,
             args.batch_size,
-            args.sizes,
+            args.num_neighbors,
             args.hidden_channels,
             args.dropout,
-            args.eval_steps,
-            args.num_warmup_iters_for_timing,
+            args.num_val_steps,
             args.lr,
         )
