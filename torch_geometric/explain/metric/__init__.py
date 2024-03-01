f3e7bff576fbe3439e2a0914daa687535ba0e8c2
Xu Zheng <1242474153@qq.com>
2024-03-01 17:20:22 -0500

    add robust fidelity

--- torch_geometric/explain/metric/__init__.py
+++ torch_geometric/explain/metric/__init__.py
@@ -1,10 +1,12 @@
 from .basic import groundtruth_metrics
 from .fidelity import fidelity, characterization_score, fidelity_curve_auc
 from .faithfulness import unfaithfulness
+from .robust_fidelity import robust_fidelity
 
 __all__ = classes = [
     'groundtruth_metrics',
     'fidelity',
+    'robust_fidelity',
     'characterization_score',
     'fidelity_curve_auc',
     'unfaithfulness',
