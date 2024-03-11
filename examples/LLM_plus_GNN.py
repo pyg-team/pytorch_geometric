0031f8489bd52691a60cdfb4cb97a72f48b4b526
puririshi98 <puririshi98@berkeley.edu>
2024-03-11 11:23:59 -0700

    WIP

--- examples/LLM_plus_GNN.py
+++ examples/LLM_plus_GNN.py
@@ -154,7 +154,7 @@ class GAT_LLAMA(nn.Module):
             num_layers=4,
             heads=4,
         ).to(self.model.device)
-
+        print("self.model.device =", self.model.device)
         self.projector = nn.Sequential(
             nn.Linear(1024, 2048),
             nn.Sigmoid(),
