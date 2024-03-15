6abd9f98b1803997230b08cd2ae31d329b759eb7
puririshi98 <puririshi98@berkeley.edu>
2024-03-15 14:35:06 -0700

    in linting, induced a bug, solving...

--- torch_geometric/datasets/web_qsp_dataset.py
+++ torch_geometric/datasets/web_qsp_dataset.py
@@ -34,6 +34,113 @@ except ImportError as e:  # noqa
 from torch_geometric.data import Data, InMemoryDataset
 
 
+def retrieval_via_pcst(graph: Data, q_emb: torch.Tensor,
+                       textual_nodes: df,
+                       textual_edges: df, topk: int = 3,
+                       topk_e: int = 3,
+                       cost_e: float = 0.5) -> Tuple[Data, str]:
+    # from original G-Retriever work
+    w# https://arxiv.org/abs/2402.07630
+    c = 0.01
+    if len(textual_nodes) == 0 or len(textual_edges) == 0:
+        desc = textual_nodes.to_csv(index=False) + "\n" + textual_edges.to_csv(
+            index=False, columns=["src", "edge_attr", "dst"])
+        graph = Data(x=graph.x, edge_index=graph.edge_index,
+                     edge_attr=graph.edge_attr, num_nodes=graph.num_nodes)
+        return graph, desc
+
+    root = -1  # unrooted
+    num_clusters = 1
+    pruning = "gw"
+    verbosity_level = 0
+    if topk > 0:
+        n_prizes = torch.nn.CosineSimilarity(dim=-1)(q_emb, graph.x)
+        topk = min(topk, graph.num_nodes)
+        _, topk_n_indices = torch.topk(n_prizes, topk, largest=True)
+
+        n_prizes = torch.zeros_like(n_prizes)
+        n_prizes[topk_n_indices] = torch.arange(topk, 0, -1).float()
+    else:
+        n_prizes = torch.zeros(graph.num_nodes)
+
+    if topk_e > 0:
+        e_prizes = torch.nn.CosineSimilarity(dim=-1)(q_emb, graph.edge_attr)
+        topk_e = min(topk_e, e_prizes.unique().size(0))
+
+        topk_e_values, _ = torch.topk(e_prizes.unique(), topk_e, largest=True)
+        e_prizes[e_prizes < topk_e_values[-1]] = 0.0
+        last_topk_e_value = topk_e
+        for k in range(topk_e):
+            indices = e_prizes == topk_e_values[k]
+            value = min((topk_e - k) / sum(indices), last_topk_e_value - c)
+            e_prizes[indices] = value
+            last_topk_e_value = value
+        # cost_e = max(min(cost_e, e_prizes.max().item()-c), 0)
+    else:
+        e_prizes = torch.zeros(graph.num_edges)
+
+    costs = []
+    edges = []
+    vritual_n_prizes = []
+    virtual_edges = []
+    virtual_costs = []
+    mapping_n = {}
+    mapping_e = {}
+    for i, (src, dst) in enumerate(graph.edge_index.T.numpy()):
+        prize_e = e_prizes[i]
+        if prize_e <= cost_e:
+            mapping_e[len(edges)] = i
+            edges.append((src, dst))
+            costs.append(cost_e - prize_e)
+        else:
+            virtual_node_id = graph.num_nodes + len(vritual_n_prizes)
+            mapping_n[virtual_node_id] = i
+            virtual_edges.append((src, virtual_node_id))
+            virtual_edges.append((virtual_node_id, dst))
+            virtual_costs.append(0)
+            virtual_costs.append(0)
+            vritual_n_prizes.append(prize_e - cost_e)
+
+    prizes = np.concatenate([n_prizes, np.array(vritual_n_prizes)])
+    num_edges = len(edges)
+    if len(virtual_costs) > 0:
+        costs = np.array(costs + virtual_costs)
+        edges = np.array(edges + virtual_edges)
+
+    vertices, edges = pcst_fast(edges, prizes, costs, root, num_clusters,
+                                pruning, verbosity_level)
+
+    selected_nodes = vertices[vertices < graph.num_nodes]
+    selected_edges = [mapping_e[e] for e in edges if e < num_edges]
+    virtual_vertices = vertices[vertices >= graph.num_nodes]
+    if len(virtual_vertices) > 0:
+        virtual_vertices = vertices[vertices >= graph.num_nodes]
+        virtual_edges = [mapping_n[i] for i in virtual_vertices]
+        selected_edges = np.array(selected_edges + virtual_edges)
+
+    edge_index = graph.edge_index[:, selected_edges]
+    selected_nodes = np.unique(
+        np.concatenate(
+            [selected_nodes, edge_index[0].numpy(), edge_index[1].numpy()]))
+
+    n = textual_nodes.iloc[selected_nodes]
+    e = textual_edges.iloc[selected_edges]
+    desc = n.to_csv(index=False) + "\n" + e.to_csv(
+        index=False, columns=["src", "edge_attr", "dst"])
+
+    mapping = {n: i for i, n in enumerate(selected_nodes.tolist())}
+
+    x = graph.x[selected_nodes]
+    edge_attr = graph.edge_attr[selected_edges]
+    src = [mapping[i] for i in edge_index[0].tolist()]
+    dst = [mapping[i] for i in edge_index[1].tolist()]
+    edge_index = torch.LongTensor([src, dst])
+    data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr,
+                num_nodes=len(selected_nodes))
+
+    return data, desc
+
+
 class Dataset(torch.utils.data.Dataset):
     def __init__(self, input_ids: torch.Tensor,
                  attention_mask: torch.Tensor) -> None:
@@ -169,124 +276,6 @@ class WebQSPDataset(InMemoryDataset):
         super().__init__(root, None, None, force_reload=force_reload)
         self.load(self.processed_paths[0])
 
-    def retrieval_via_pcst(self, graph: Data, q_emb: torch.Tensor,
-                           textual_nodes: df, textual_edges: df, topk: int = 3,
-                           topk_e: int = 3,
-                           cost_e: float = 0.5) -> Tuple[Data, str]:
-        # from original G-Retriever work
-        # https://arxiv.org/abs/2402.07630
-        c = 0.01
-        # explicit casting for linting
-        num_nodes: int = graph.num_nodes  # type: ignore
-        num_edges: int = graph.num_edges
-        e_idx: torch.Tensor = graph.edge_index  # type: ignore
-        e_attr: torch.Tensor = graph.edge_attr  # type: ignore
-        node_feat = torch.Tensor(graph.x)
-        if len(textual_nodes) == 0 or len(textual_edges) == 0:
-            desc = textual_nodes.to_csv(
-                index=False) + "\n" + textual_edges.to_csv(
-                    index=False, columns=["src", "edge_attr", "dst"])
-            new_graph = Data(x=node_feat, edge_index=e_idx, edge_attr=e_attr,
-                             num_nodes=num_nodes)
-            return new_graph, desc
-
-        root = -1  # unrooted
-        num_clusters = 1
-        pruning = "gw"
-        verbosity_level = 0
-        if topk > 0:
-            n_prizes = torch.nn.CosineSimilarity(dim=-1)(q_emb, node_feat)
-            topk = min(topk, num_nodes)
-            _, topk_n_indices = torch.topk(n_prizes, topk, largest=True)
-
-            n_prizes = torch.zeros_like(n_prizes)
-            n_prizes[topk_n_indices] = torch.arange(topk, 0, -1).float()
-        else:
-            n_prizes = torch.zeros(num_nodes)
-
-        if topk_e > 0:
-            e_prizes = torch.nn.CosineSimilarity(dim=-1)(q_emb, e_attr)
-            topk_e = min(topk_e, e_prizes.unique().size(0))
-
-            topk_e_values, _ = torch.topk(e_prizes.unique(), topk_e,
-                                          largest=True)
-            e_prizes[e_prizes < topk_e_values[-1]] = 0.0
-            last_topk_e_value = topk_e
-            for k in range(topk_e):
-                indices = e_prizes == topk_e_values[k]
-                value = min((topk_e - k) / sum(indices), last_topk_e_value - c)
-                e_prizes[indices] = value
-                last_topk_e_value = value
-        else:
-            e_prizes = torch.zeros(num_edges)
-
-        cost_list = []
-        edge_list = []  # type: ignore
-        virtual_n_prizes = []  # type: ignore
-        virtual_edges = []
-        virtual_costs = []
-        mapping_n = {}
-        mapping_e = {}
-        for i, (src, dst) in enumerate(e_idx.T.numpy()):
-            prize_e = e_prizes[i]
-            if prize_e <= cost_e:
-                mapping_e[len(edge_list)] = i
-                edge_list.append((src, dst))
-                cost_list.append(cost_e - prize_e)
-            else:
-                virtual_node_id = num_nodes + len(virtual_n_prizes)
-                mapping_n[virtual_node_id] = i
-                virtual_edges.append((src, virtual_node_id))
-                virtual_edges.append((virtual_node_id, dst))
-                virtual_costs.append(0)
-                virtual_costs.append(0)
-                virtual_n_prizes.append(prize_e - cost_e)
-
-        prizes = np.concatenate([n_prizes, np.array(virtual_n_prizes)])
-        num_edges = len(edge_list)
-        if len(virtual_costs) > 0:
-            costs = np.array(cost_list + virtual_costs)
-            edges = np.array(edge_list + virtual_edges)
-        else:
-            costs = cost_list # type: ignore
-            edges = edge_list # type: ignore
-
-        vertices, edges = pcst_fast(edges, prizes, costs, root, num_clusters,
-                                    pruning, verbosity_level)
-
-        selected_nodes = vertices[vertices < num_nodes]
-        selected_edges = [mapping_e[e] for e in edges if e < num_edges]
-        virtual_vertices = vertices[vertices >= num_nodes]
-        if len(virtual_vertices) > 0:
-            virtual_vertices = vertices[vertices >= num_nodes]
-            new_virtual_edges = [mapping_n[i] for i in virtual_vertices]
-            new_selected_edges = np.array(selected_edges + new_virtual_edges)
-        else:
-            new_selected_edges = selected_edges # type: ignore
-
-        edge_index = e_idx[:, new_selected_edges]
-        new_selected_nodes = np.unique(
-            np.concatenate(
-                [selected_nodes, edge_index[0].numpy(),
-                 edge_index[1].numpy()]))
-
-        n = textual_nodes.iloc[new_selected_nodes]
-        e = textual_edges.iloc[new_selected_edges]
-        desc = n.to_csv(index=False) + "\n" + e.to_csv(
-            index=False, columns=["src", "edge_attr", "dst"])
-
-        mapping = {n: i for i, n in enumerate(new_selected_nodes.tolist())}
-
-        x = node_feat[new_selected_nodes]
-        edge_attr = e_attr[new_selected_edges]
-        src = [mapping[i] for i in edge_index[0].tolist()]
-        dst = [mapping[i] for i in edge_index[1].tolist()]
-        edge_index = torch.LongTensor([src, dst])
-        data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr,
-                    num_nodes=len(selected_nodes))
-
-        return data, desc
-
     @property
     def raw_file_names(self) -> List[str]:
         return []
@@ -360,7 +349,7 @@ class WebQSPDataset(InMemoryDataset):
             label = ("|").join(data_i["answer"]).lower()
             raw_graph = Data(x=x, edge_index=edge_index, edge_attr=edge_attr,
                              num_nodes=len(nodes)).to("cpu")
-            psct_subgraph, desc = self.retrieval_via_pcst(
+            psct_subgraph, desc = retrieval_via_pcst(
                 raw_graph, q_embs[index], nodes, edges, topk=3, topk_e=5,
                 cost_e=0.5)
             psct_subgraph["question"] = question
