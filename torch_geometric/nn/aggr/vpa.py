c560d28a098205bc32c0c6ba1fe8e22231ea4b13
Richard Freinschlag <freinschlag@ml.jku.at>
2024-03-20 15:37:35 +0100

    formatting

--- torch_geometric/nn/aggr/vpa.py
+++ torch_geometric/nn/aggr/vpa.py
@@ -8,8 +8,8 @@ from torch_geometric.nn.aggr import Aggregation
 class VariancePreservingAggregation(Aggregation):
 
     r"""
-    Performs the Variance Preserving Aggregation (VPA) for graph neural networks,
-    as described in https://arxiv.org/pdf/2403.04747.pdf
+    Performs the Variance Preserving Aggregation (VPA) for graph
+    neural networks, as described in https://arxiv.org/pdf/2403.04747.pdf
 
     .. math::
         \mathrm{vpa}(\mathcal{X}) = \frac{1}{\sqrt{|\mathcal{X}|}}
@@ -26,7 +26,7 @@ class VariancePreservingAggregation(Aggregation):
     ) -> Tensor:
 
         # apply sum aggregation on x
-        sum_aggregation = self.reduce(x, index, ptr, dim_size, dim, reduce="sum")
+        sum_aggr = self.reduce(x, index, ptr, dim_size, dim, reduce="sum")
 
         # count the number of neighbours
         shape = [1 for _ in x.shape]
@@ -34,6 +34,7 @@ class VariancePreservingAggregation(Aggregation):
         ones = torch.ones(size=shape, dtype=x.dtype, device=x.device)
         counts = self.reduce(ones, index, ptr, dim_size, dim, reduce="sum")
         # simpler variant that consumes more memory:
-        # counts = self.reduce(torch.ones_like(x), index, ptr, dim_size, dim, reduce="sum")
+        # counts = self.reduce(torch.ones_like(x), index, ptr, dim_size, dim,
+        # reduce="sum")
 
-        return torch.nan_to_num(sum_aggregation / torch.sqrt(counts))
+        return torch.nan_to_num(sum_aggr / torch.sqrt(counts))
