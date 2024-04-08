9b21cb0db91d74db2af880b1b1baa6fd0039c8dc
rusty1s <matthias.fey@tu-dortmund.de>
2024-04-08 09:37:53 +0000

    update

--- torch_geometric/nn/conv/message_passing.py
+++ torch_geometric/nn/conv/message_passing.py
@@ -518,9 +518,9 @@ class MessagePassing(torch.nn.Module):
             fuse = False
             if is_sparse(edge_index):
                 fuse = True
-            elif (self.SUPPORTS_FUSED_EDGE_INDEX
-                  and not torch.jit.is_scripting()
-                  and isinstance(edge_index, EdgeIndex)):
+            elif (not torch.jit.is_scripting()
+                  and isinstance(edge_index, EdgeIndex)
+                  and self.SUPPORTS_FUSED_EDGE_INDEX):
                 if edge_index.is_sorted_by_col:
                     fuse = True
         else:
