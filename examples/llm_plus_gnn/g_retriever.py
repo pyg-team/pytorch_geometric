c3cf07609b54a9b075f425de9256b0a3bb35826c
puririshi98 <puririshi98@berkeley.edu>
2024-04-09 16:43:20 -0700

    cleaning up

--- examples/llm_plus_gnn/g_retriever.py
+++ examples/llm_plus_gnn/g_retriever.py
@@ -235,7 +235,7 @@ def minimal_demo(model, dataset):
         pure_llm_hallucin_sum += bool(pure_llm_hallucinates)
         # showcase LLM hallucinations solved by GNN
         if pure_llm_hallucinates and not gnn_llm_hallucinates:
-            final_prnt_str += "Question: " + question + "\n"
+            final_prnt_str += "Prompt: " + question + "\n"
             final_prnt_str += "Correct Answer: " + correct_answer + "\n"
             final_prnt_str += "Pure LLM Prediction: " + pure_llm_pred + "\n"
             final_prnt_str += "GNN+LLM Prediction:" + gnn_llm_pred + "\n"
