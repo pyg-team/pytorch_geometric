a52a3f85fbe60cdfcc74610a649fcd43c26f20c1
rusty1s <matthias.fey@tu-dortmund.de>
2024-04-11 14:08:07 +0000

    update

--- torch_geometric/transforms/laplacian_lambda_max.py
+++ torch_geometric/transforms/laplacian_lambda_max.py
@@ -1,6 +1,5 @@
 from typing import Optional
 
-import numpy
 from scipy.sparse.linalg import eigs, eigsh
 
 from torch_geometric.data import Data
@@ -63,7 +62,7 @@ class LaplacianLambdaMax(BaseTransform):
             eig_fn = eigsh
 
         lambda_max = eig_fn(L, k=1, which='LM', return_eigenvectors=False)
-        data.lambda_max = float(numpy.ndarray.item(lambda_max.real))
+        data.lambda_max = lambda_max.real.item()
 
         return data
 
