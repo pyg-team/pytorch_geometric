e90bd34c897c2a0ccbdd245365b49fec529474d4
Han Xiao <xiaohan2012@gmail.com>
2024-04-26 09:38:54 +0300

    update docstring

--- torch_geometric/nn/pool/mincut.py
+++ torch_geometric/nn/pool/mincut.py
@@ -11,13 +11,14 @@ def mincut_pool(
     mask: Optional[Tensor] = None,
     temp: float = 1.0,
 ) -> Tuple[Tensor, Tensor, Tensor, Tensor]:
-    r"""
+    r"""MinCut pooling for sparse adjacency matrices, which mirros its dense counter-part `dense_mincut_pool`
+
     Args:
         x (torch.Tensor): Node feature tensor
             :math:`\mathbf{X} \in \mathbb{R}^{B \times N \times F}`, with
             batch-size :math:`B`, (maximum) number of nodes :math:`N` for
             each graph, and feature dimension :math:`F`.
-        adj (torch.Tensor): Adjacency tensor
+        adj (torch.Tensor): Sparse adjacency tensor
             :math:`\mathbf{A} \in \mathbb{R}^{B \times N \times N}`.
         s (torch.Tensor): Assignment tensor
             :math:`\mathbf{S} \in \mathbb{R}^{B \times N \times C}`
@@ -50,7 +51,10 @@ def mincut_pool(
     n_batches = s.shape[0]
     out_adj = torch.matmul(
         torch.stack(
-            [torch.matmul(s.transpose(1, 2)[b], adj[b]) for b in range(n_batches)],
+            [
+                torch.matmul(s.transpose(1, 2)[b], adj[b])
+                for b in range(n_batches)
+            ],
             dim=0,
         ),
         s,
@@ -63,12 +67,14 @@ def mincut_pool(
     mincut_den = _rank3_trace(
         torch.matmul(
             torch.stack(
-                [torch.matmul(s.transpose(1, 2)[b], d[b]) for b in range(n_batches)],
+                [
+                    torch.matmul(s.transpose(1, 2)[b], d[b])
+                    for b in range(n_batches)
+                ],
                 dim=0,
             ),
             s,
-        )
-    )
+        ))
 
     mincut_loss = -(mincut_num / mincut_den)
     mincut_loss = torch.mean(mincut_loss)
@@ -77,7 +83,8 @@ def mincut_pool(
     ss = torch.matmul(s.transpose(1, 2), s)
     i_s = torch.eye(k).type_as(ss)
     ortho_loss = torch.norm(
-        ss / torch.norm(ss, dim=(-1, -2), keepdim=True) - i_s / torch.norm(i_s),
+        ss / torch.norm(ss, dim=(-1, -2), keepdim=True) -
+        i_s / torch.norm(i_s),
         dim=(-1, -2),
     )
     ortho_loss = torch.mean(ortho_loss)
@@ -104,4 +111,5 @@ def _sparse_diag_2d(diag):
 
 
 def _sparse_rank3_diag(d_flat):
-    return torch.stack([_sparse_diag_2d(diag.to_dense()) for diag in d_flat], dim=0)
+    return torch.stack([_sparse_diag_2d(diag.to_dense()) for diag in d_flat],
+                       dim=0)
