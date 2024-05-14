bc86bd62acff8b245af275dc502dca55ae9895bd
puririshi98 <puririshi98@berkeley.edu>
2024-05-14 10:56:55 -0700

    initial draft, needs work

---
+++ examples/llm_plus_gnn/wiki_retriever.py
@@ -0,0 +1,84 @@
+"""This example implements G-retriever using PyG.
+Original Paper: https://arxiv.org/abs/2402.07630
+“G-Retriever significantly reduces hallucinations
+by 54% compared to the [LLM] baseline“.
+requirements on top of basic PyG:
+pip install datasets transformers pcst_fast sentencepiece tqdm pandas
+"""
+import argparse
+import gc
+import math
+import re
+import time
+from os import path
+
+import pandas as pd
+import torch
+from torch.nn.utils import clip_grad_norm_
+from tqdm import tqdm
+
+from torch_geometric import seed_everything
+from torch_geometric.datasets import Squad_WikiGraph
+from torch_geometric.loader import DataLoader
+from torch_geometric.nn.models import GRetriever
+from torch_geometric.nn.text import LLM
+from .g_retriever import minimal_demo, train
+
+def get_loss(model, batch, model_save_name) -> torch.Tensor:
+    if model_save_name == "llm":
+        return model(batch.question, batch.label, batch.desc)
+    else:
+        return model(batch.question, batch.x, batch.edge_index, batch.batch,
+                     batch.ptr, batch.label, batch.edge_attr, batch.desc)
+
+
+def inference_step(model, batch, model_save_name):
+    if model_save_name == "llm":
+        return model.inference(batch.question, batch.desc)
+    else:
+        return model.inference(batch.question, batch.x, batch.edge_index,
+                               batch.batch, batch.ptr, batch.edge_attr,
+                               batch.desc)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gnn_hidden_channels', type=int, default=1024)
+    parser.add_argument('--num_gnn_layers', type=int, default=4)
+    parser.add_argument('--lr', type=float, default=1e-5)
+    parser.add_argument('--epochs', type=int, default=2)
+    parser.add_argument('--batch_size', type=int, default=4)
+    parser.add_argument('--eval_batch_size', type=int, default=8)
+    parser.add_argument(
+        "--checkpointing", action="store_true",
+        help="Use this flag to checkpoint each time a \
+        new best val loss is achieved")
+
+    args = parser.parse_args()
+    # check if saved model
+    retrain = True
+    if path.exists("gnn_llm.pt") and path.exists("gnn_llm_eval_outs.pt"):
+        print("Existing trained model found.")
+        print("Would you like to retrain?")
+        user_input = str(input("(y/n):")).lower()
+        retrain = user_input == "y"
+    else:
+        retrain = True
+    if retrain:
+        since = time.time()
+        prep_time, dataset, gnn_llm_eval_outs = train(
+            since, args.epochs, args.gnn_hidden_channels, args.num_gnn_layers,
+            args.batch_size, args.eval_batch_size, args.lr,
+            checkpointing=args.checkpointing)
+        torch.cuda.empty_cache()
+        torch.cuda.reset_max_memory_allocated()
+        gc.collect()
+        e2e_time = round(time.time() - since, 2)
+        print("E2E time (e2e_time) =", e2e_time, "seconds")
+        print("E2E tme minus Prep Time =", e2e_time - prep_time, "seconds")
+    else:
+        gnn_llm_eval_outs = torch.load("gnn_llm_eval_outs.pt")
+        dataset = Squad_WikiGraph()
+    print("Here's a demo showcasing how GNN reduces LLM hallucinations:")
+    minimal_demo(gnn_llm_eval_outs, dataset, args.lr, args.epochs,
+                 args.batch_size, args.eval_batch_size)
---
+++ torch_geometric/datasets/squad_wikigraph.py
@@ -0,0 +1,117 @@
+from torch_geometric.data import Data
+from typing import Optional
+import random
+try:
+    import datasets
+    WITH_DATASETS = True
+except ImportError as e:  # noqa
+    WITH_DATASETS = False
+from torch_geometric.nn.text import SentenceTransformer, text2embedding
+from torch_geometric.nn.text.llm import llama2_str_name
+
+
+def get_wiki_data(question: str, model:SentenceTransformer, device: torch.device, seed_nodes: int = 3, fan_out: int = 3, num_hops: int = 2, label: Optional[str] = None) -> Data:
+	"""
+	Performs neighborsampling on wikipedia
+	"""
+	import wikipedia
+	search_list = wikipedia.search(question)
+	seed_doc_names = search_list[:seed_nodes]
+	seed_docs = [wikipedia.page(doc_name) for doc_name in seed_doc_names]
+	# initialize our doc graph with seed docs
+	doc_contents = [doc.content for doc in seed_docs]
+	title_2_node_id_map = {doc.title:i for doc in enumerate(seed_docs)}
+	# do neighborsampling and create graph
+	src_n_ids = []
+	dst_n_ids = []
+	for hop in num_hops:
+		next_hops_seed_docs = []
+		for src_doc in seed_docs:
+			full_fan_links = src_doc.links
+			randomly_chosen_neighbor_links = list(random.sample(full_fan_links, k=fan_out))
+			new_seed_docs = [wikipedia.page(link) for link in randomly_chosen_neighbor_links]
+			for dst_doc in new_seed_docs
+				dst_doc_title = dst_doc.title
+				if dst_doc_title not in title_2_node_id_map:
+					# add new node to graph
+					title_2_node_id_map[dst_doc_name] = len(title_2_node_id_map)
+					doc_contents.append(dst_doc.content)
+				next_hops_seed_docs.append(doc)
+				src_n_ids.append(title_2_node_id_map[src_doc.title])
+				dst_n_ids.append(title_2_node_id_map[dst_doc.title])
+
+		# root nodes for the next hop
+		seed_docs = next_hops_seed_docs
+
+	# put docs into model
+	embedded_docs = text2embedding(model, doc_contents)
+	del doc_contents
+
+	# create node features, x
+	x = torch.cat(embedded_docs)
+
+
+	# construct and return Data object
+	return Data(x=x, edge_index=torch.tensor([src_n_ids, dst_n_ids]), n_id=torch.tensor(title_2_node_id_map.values()), question=question, label=label).to("cpu")
+
+
+# create SQUAD_WikiGraph dataset by calling wikiloader for each SQUAD question
+class SQUAD_WikiGraph(InMemoryDataset):
+    r"""This dataset uses the SQUAD dataset for Questions and Answers.
+    It uses Wikipedia for the corresponding knowledge graphs.
+    SQUAD source: https://huggingface.co/datasets/rajpurkar/squad
+
+    Args:
+        root (str): Root directory where the dataset should be saved.
+        force_reload (bool, optional): Whether to re-process the dataset.
+            (default: :obj:`False`)
+    """
+    def __init__(
+        self,
+        root: str = "",
+        force_reload: bool = False,
+    ) -> None:
+        if not WITH_DATASETS:
+            raise ImportError("Please pip install datasets")
+        self.device = torch.device(
+            "cuda" if torch.cuda.is_available() else "cpu")
+        super().__init__(root, None, None, force_reload=force_reload)
+        self.load(self.processed_paths[0])
+
+    @property
+    def raw_file_names(self) -> List[str]:
+        return []
+
+    @property
+    def processed_file_names(self) -> List[str]:
+        return ["list_of_graphs.pt", "pre_filter.pt", "pre_transform.pt"]
+
+    def download(self) -> None:
+        dataset = datasets.load_dataset("rajpurkar/squad")
+        self.raw_dataset = datasets.concatenate_datasets(
+            [dataset["train"], dataset["validation"], dataset["test"]])
+        self.split_idxs = {
+            "train":
+            torch.arange(len(dataset["train"])),
+            "val":
+            torch.arange(len(dataset["validation"])) + len(dataset["train"]),
+            "test":
+            torch.arange(len(dataset["test"])) + len(dataset["train"]) +
+            len(dataset["validation"])
+        }
+
+    def process(self) -> None:
+        self.model = SentenceTransformer(llama2_str_name)
+        self.model.eval()
+        self.questions = [i["question"] for i in self.raw_dataset]
+        list_of_data_objs = []
+        for index in tqdm(range(len(self.raw_dataset))):
+            data_i = self.raw_dataset[index]
+            question = f"Question: {data_i['question']}\nAnswer: "
+            label = data_i["answer"].lower()
+            pyg_data_obj = get_wiki_data(question, self.model, self.device, label=label)
+            list_of_data_objs.append(pyg_data_obj)
+
+        self.save(list_of_data_objs, self.processed_paths[0])
+
+
