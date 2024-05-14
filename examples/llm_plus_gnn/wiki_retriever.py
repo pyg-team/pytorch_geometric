1dd7009f7be9be48e3a1327ede015298663eae12
puririshi98 <puririshi98@berkeley.edu>
2024-05-14 13:46:00 -0700

    initial draft, needs work

--- examples/llm_plus_gnn/wiki_retriever.py
+++ examples/llm_plus_gnn/wiki_retriever.py
@@ -24,21 +24,33 @@ from torch_geometric.nn.models import GRetriever
 from torch_geometric.nn.text import LLM
 from .g_retriever import minimal_demo, train
 
+def get_list_of_embeddings_from_batch(batch):
+    rag_embeddings = []
+    ptr = batch.ptr
+    for i in range(len(ptr) - 1):
+        if ptr[i] - ptr[i+1] > 0:
+            rag_embeddings.append(batch.x[ptr[i]:ptr[i+1]])
+        else:
+            rag_embeddings.append(None)
+    return rag_embeddings
+
+
 def get_loss(model, batch, model_save_name) -> torch.Tensor:
     if model_save_name == "llm":
-        return model(batch.question, batch.label, batch.desc)
+        rag_embeddings = get_list_of_embeddings_from_batch(batch)
+        return model(batch.question, batch.label, rag_embeddings)
     else:
         return model(batch.question, batch.x, batch.edge_index, batch.batch,
-                     batch.ptr, batch.label, batch.edge_attr, batch.desc)
+                     batch.ptr, batch.label, batch.edge_attr)
 
 
 def inference_step(model, batch, model_save_name):
     if model_save_name == "llm":
-        return model.inference(batch.question, batch.desc)
+        rag_embeddings = get_list_of_embeddings_from_batch(batch)
+        return model.inference(batch.question, rag_embeddings)
     else:
         return model.inference(batch.question, batch.x, batch.edge_index,
-                               batch.batch, batch.ptr, batch.edge_attr,
-                               batch.desc)
+                               batch.batch, batch.ptr, batch.edge_attr)
 
 
 if __name__ == "__main__":
@@ -68,7 +80,7 @@ if __name__ == "__main__":
         since = time.time()
         prep_time, dataset, gnn_llm_eval_outs = train(
             since, args.epochs, args.gnn_hidden_channels, args.num_gnn_layers,
-            args.batch_size, args.eval_batch_size, args.lr,
+            args.batch_size, args.eval_batch_size, args.lr, get_loss, inference_step,
             checkpointing=args.checkpointing)
         torch.cuda.empty_cache()
         torch.cuda.reset_max_memory_allocated()
@@ -81,4 +93,4 @@ if __name__ == "__main__":
         dataset = Squad_WikiGraph()
     print("Here's a demo showcasing how GNN reduces LLM hallucinations:")
     minimal_demo(gnn_llm_eval_outs, dataset, args.lr, args.epochs,
-                 args.batch_size, args.eval_batch_size)
+                 args.batch_size, args.eval_batch_size, get_loss, inference_step)
