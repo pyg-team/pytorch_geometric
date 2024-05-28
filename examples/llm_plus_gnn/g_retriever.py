157c910ed7dc0dfbd04dc24ceab21d930f6b18de
puririshi98 <puririshi98@berkeley.edu>
2024-05-28 13:38:31 -0700

    adding tiny-llama

--- examples/llm_plus_gnn/g_retriever.py
+++ examples/llm_plus_gnn/g_retriever.py
@@ -153,9 +153,9 @@ def train(since, num_epochs, hidden_channels, num_gnn_layers, batch_size,
     if model is None:
         if tiny_llama:
             model = GRetriever(llm_to_use="TinyLlama/TinyLlama-1.1B-Chat-v0.1",
+                               num_llm_params=1, # 1 Billion
                                gnn_hidden_channels=hidden_channels,
                                num_gnn_layers=num_gnn_layers,)
-                               #mlp_out_dim=2048)
         else:
             model = GRetriever(gnn_hidden_channels=hidden_channels,
                                num_gnn_layers=num_gnn_layers)
