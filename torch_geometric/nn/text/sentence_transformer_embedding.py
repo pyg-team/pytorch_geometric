8ca12800337e4cb27ee154be51690c9d426d955f
puririshi98 <puririshi98@berkeley.edu>
2024-05-28 15:00:41 -0700

    wip

--- torch_geometric/nn/text/sentence_transformer_embedding.py
+++ torch_geometric/nn/text/sentence_transformer_embedding.py
@@ -20,17 +20,17 @@ class SentenceTransformer(torch.nn.Module):
         if device is not None:
             self.llm = AutoModel.from_pretrained(pretrained_repo).to(device)
         else:
-            if autocast_dtype is None:
-                autocast_dtype = torch.bfloat16
+            if self.autocast_dtype is None:
+                self.autocast_dtype = torch.bfloat16
             self.mem_needed = get_mem_needed_for_llm(num_params)
             kwargs, _ = get_llm_kwargs(self.mem_needed)
             self.llm = AutoModel.from_pretrained(pretrained_repo,
                                                  **kwargs)
             self.llm_device = self.llm.device
-        if autocast_dtype is None:
+        if self.autocast_dtype is None:
             self.autocast_context = nullcontext()
         else:
-            self.autocast_context = torch.cuda.amp.autocast(dtype=self.llm_dtype)
+            self.autocast_context = torch.cuda.amp.autocast(dtype=self.autocast_dtype)
     def mean_pooling(self, token_embeddings: torch.Tensor,
                      attention_mask: torch.Tensor) -> torch.Tensor:
         data_type = token_embeddings.dtype
