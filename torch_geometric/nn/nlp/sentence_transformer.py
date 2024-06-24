1ad05562f50b184fa135e893c372b1ed86d91fe3
puririshi98 <puririshi98@berkeley.edu>
2024-06-24 16:43:35 -0700

    recent upstream sentencetransformer has issues, looking how to fix. reverting first

--- torch_geometric/nn/nlp/sentence_transformer.py
+++ torch_geometric/nn/nlp/sentence_transformer.py
@@ -1,38 +1,46 @@
-from typing import List, Optional
+from enum import Enum
+from typing import List, Optional, Union
 
 import torch
 import torch.nn.functional as F
 from torch import Tensor
 
 
-class SentenceTransformer(torch.nn.Module):
-    r"""Embeds text as a vector using Huggingface transformers.
+class PoolingStrategy(Enum):
+    MEAN = 'mean'
+    LAST = 'last'
+    CLS = 'cls'
 
-    model_name (str): A string representing the huggingface model to use.
-            (default: :obj:`"sentence-transformers/all-roberta-large-v1"`)
-    """
+
+class SentenceTransformer(torch.nn.Module):
     def __init__(
         self,
         model_name: Optional[
             str] = "sentence-transformers/all-roberta-large-v1",
+        pooling_strategy: Union[PoolingStrategy, str] = 'mean',
     ) -> None:
         super().__init__()
+
         self.model_name = model_name
+        self.pooling_strategy = PoolingStrategy(pooling_strategy)
 
         from transformers import AutoModel, AutoTokenizer
 
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.model = AutoModel.from_pretrained(model_name)
 
-    def mean_pooling(self, emb: Tensor, attention_mask: Tensor) -> Tensor:
-        mask = attention_mask.unsqueeze(-1).expand(emb.size()).to(emb.dtype)
-        return (emb * mask).sum(dim=1) / mask.sum(dim=1).clamp(min=1e-9)
-
     def forward(self, input_ids: Tensor, attention_mask: Tensor) -> Tensor:
         out = self.model(input_ids=input_ids, attention_mask=attention_mask)
 
         emb = out[0]  # First element contains all token embeddings.
-        emb = self.mean_pooling(emb, attention_mask)
+        if self.pooling_strategy == PoolingStrategy.MEAN:
+            emb = mean_pooling(emb, attention_mask)
+        elif self.pooling_strategy == PoolingStrategy.LAST:
+            emb = last_pooling(emb, attention_mask)
+        else:
+            assert self.pooling_strategy == PoolingStrategy.CLS
+            emb = emb[:, 0, :]
+
         emb = F.normalize(emb, p=2, dim=1)
         return emb
 
@@ -47,9 +55,8 @@ class SentenceTransformer(torch.nn.Module):
         batch_size: Optional[int] = None,
         output_device: Optional[torch.device] = None,
     ) -> Tensor:
-        if len(text) == 0:
-            return torch.zeros((0, 1024))
         batch_size = len(text) if batch_size is None else batch_size
+
         embs: List[Tensor] = []
         for start in range(0, len(text), batch_size):
             token = self.tokenizer(
@@ -70,3 +77,19 @@ class SentenceTransformer(torch.nn.Module):
 
     def __repr__(self) -> str:
         return f'{self.__class__.__name__}(model_name={self.model_name})'
+
+
+def mean_pooling(emb: Tensor, attention_mask: Tensor) -> Tensor:
+    mask = attention_mask.unsqueeze(-1).expand(emb.size()).to(emb.dtype)
+    return (emb * mask).sum(dim=1) / mask.sum(dim=1).clamp(min=1e-9)
+
+
+def last_pooling(emb: Tensor, attention_mask: Tensor) -> Tensor:
+    # Check whether language model uses left padding,
+    # which is always used for decoder LLMs
+    left_padding = attention_mask[:, -1].sum() == attention_mask.size(0)
+    if left_padding:
+        return emb[:, -1]
+
+    seq_indices = attention_mask.sum(dim=1) - 1
+    return emb[torch.arange(emb.size(0), device=emb.device), seq_indices]
