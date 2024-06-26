3c60f03845b6416181fd5ffe38a7862b93f73009
Zack Aristei <zaristei@zaristei-nvidia.client.nvidia.com>
2024-06-25 17:35:47 -0700

    begin nvtx profiler

---
+++ torch_geometric/profile/nvtx.py
@@ -0,0 +1,27 @@
+import torch
+from typing import Optional
+from functools import wraps
+
+
+def nvtxit(name: str, n_warmups: int = 0, n_iters: Optional[int] = None):
+    def nvtx(func):
+        iters_so_far = 0
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            nonlocal iters_so_far
+            if not torch.cuda.is_available():
+                return func(*args, **kwargs)
+            elif iters_so_far < n_warmups:
+                iters_so_far += 1
+                return func(*args, **kwargs)
+            elif n_iters is None or iters_so_far < n_iters + n_warmups:
+                torch.cuda.nvtx.range_push(f"{name}_{iters_so_far}")
+                result = func(*args, **kwargs)
+                torch.cuda.nvtx.range_pop()
+                iters_so_far += 1
+                return result
+            else:
+                return func(*args, **kwargs)
+        return wrapper
+    return nvtx
