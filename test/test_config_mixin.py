507e772592bad6058f88af811b62954161be7928
rusty1s <matthias.fey@tu-dortmund.de>
2024-06-26 08:58:01 +0200

    update

--- test/test_config_mixin.py
+++ test/test_config_mixin.py
@@ -43,14 +43,28 @@ def test_config_mixin() -> None:
 
     model = Module.from_config(cfg)
     assert isinstance(model, Module)
-    assert cfg.x == 0
-    assert isinstance(cfg.data, Dataclass)
-    assert cfg.data.x == 1
-    assert cfg.data.y == 2
+    assert model.x == 0
+    assert isinstance(model.data, Dataclass)
+    assert model.data.x == 1
+    assert model.data.y == 2
 
     model = Base.from_config(cfg)
     assert isinstance(model, Module)
-    assert cfg.x == 0
-    assert isinstance(cfg.data, Dataclass)
-    assert cfg.data.x == 1
-    assert cfg.data.y == 2
+    assert model.x == 0
+    assert isinstance(model.data, Dataclass)
+    assert model.data.x == 1
+    assert model.data.y == 2
+
+    model = Base.from_config(cfg, 3)
+    assert isinstance(model, Module)
+    assert model.x == 3
+    assert isinstance(model.data, Dataclass)
+    assert model.data.x == 1
+    assert model.data.y == 2
+
+    model = Base.from_config(cfg, data=Dataclass(x=2, y=3))
+    assert isinstance(model, Module)
+    assert model.x == 0
+    assert isinstance(model.data, Dataclass)
+    assert model.data.x == 2
+    assert model.data.y == 3
