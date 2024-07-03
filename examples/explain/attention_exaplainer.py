78add8443878833f8bafef8859a79d370c0ad3c6
Zhang, Chaojun <chaojun.zhang@intel.com>
2024-07-03 10:29:52 +0000

    Add attention explainer example

--- examples/explain/attention_exaplainer.py
+++ examples/explain/attention_exaplainer.py
@@ -5,8 +5,8 @@ import torch.nn.functional as F
 
 import torch_geometric
 from torch_geometric.datasets import Planetoid
-from torch_geometric.explain import Explainer, GraphMaskExplainer
-from torch_geometric.nn import GCNConv
+from torch_geometric.explain import Explainer, AttentionExplainer
+from torch_geometric.nn import GATConv
 
 if torch.cuda.is_available():
     device = torch.device('cuda')
@@ -20,23 +20,23 @@ dataset = Planetoid(path, name='Cora')
 data = dataset[0].to(device)
 
 
-# GCN Node Classification =====================================================
+# GAT Node Classification =====================================================
 
-
-class GCN(torch.nn.Module):
+class GAT(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = GCNConv(dataset.num_features, 16)
-        self.conv2 = GCNConv(16, dataset.num_classes)
+        self.conv1 = GATConv(dataset.num_features, 8, heads=8, dropout=0.6)
+        self.conv2 = GATConv(64, dataset.num_classes, heads=1, concat=False, dropout=0.6)
 
     def forward(self, x, edge_index):
-        x = self.conv1(x, edge_index).relu()
-        x = F.dropout(x, training=self.training)
+        x = F.dropout(x, p=0.6, training=self.training)
+        x = F.elu(self.conv1(x, edge_index))
+        x = F.dropout(x, p=0.6, training=self.training)
         x = self.conv2(x, edge_index)
-        return F.log_softmax(x, dim=1)
+        return x
 
 
-model = GCN().to(device)
+model = GAT().to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
 
 for epoch in range(1, 201):
@@ -49,7 +49,7 @@ for epoch in range(1, 201):
 
 explainer = Explainer(
     model=model,
-    algorithm=GraphMaskExplainer(2, epochs=5),
+    algorithm=AttentionExplainer(),
     explanation_type='model',
     node_mask_type='attributes',
     edge_mask_type='object',
@@ -60,6 +60,6 @@ explainer = Explainer(
     ),
 )
 
-node_index = 10
+node_index = torch.tensor([10, 20])
 explanation = explainer(data.x, data.edge_index, index=node_index)
 print(f'Generated explanations in {explanation.available_explanations}')
