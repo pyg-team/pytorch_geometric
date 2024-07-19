ecd4cab57f356bdd16b0f00b7caf3764544412e8
rusty1s <matthias.fey@tu-dortmund.de>
2024-07-19 19:09:36 +0200

    update

--- torch_geometric/nn/aggr/patch_transformer.py
+++ torch_geometric/nn/aggr/patch_transformer.py
@@ -1,98 +1,88 @@
-from typing import Optional
+import math
+from typing import List, Optional, Union
 
 import torch
 from torch import Tensor
 
 from torch_geometric.experimental import disable_dynamic_shapes
 from torch_geometric.nn.aggr import Aggregation
-from torch_geometric.nn.aggr.pt_utils import (
-    FeatEncoder,
-    PositionalEncoding1D,
-    TransformerBlock,
-)
+from torch_geometric.nn.aggr.utils import MultiheadAttentionBlock
 from torch_geometric.nn.encoding import PositionalEncoding
+from torch_geometric.utils import scatter
 
 
 class PatchTransformerAggregation(Aggregation):
-    r"""Performs  Heterogeneous Link Encoder embedding
-    and Semantic Patches Fusion
-    from `"Simplifying Temporal Heterogeneous Network for
-    Continuous-Time Link Prediction
-    "<https://dl.acm.org/doi/pdf/10.1145/3583780.3615059>`_paper.
+    r"""Performs patch transformer aggregation in which the elements to
+    aggregate are processed by multi-head attention blocks across patches, as
+    described in the `"Simplifying Temporal Heterogeneous Network for
+    Continuous-Time Link Prediction"
+    <https://dl.acm.org/doi/pdf/10.1145/3583780.3615059>`_paper.
 
     Args:
-        channels (int): Size of each edge feature
-            use to specify the time embedding dimension of edge timestamps.
-        hidden_channels (int, optional): Hidden channels.
-        out_dim (int, optional): output dimension.
-        max_edge (int, optional): max number of edges per node.
-        num_encoder_blocks (int, optional): Number of transformer blocks
+        in_channels (int): Size of each input sample.
+        out_channels (int): Size of each output sample.
+        patch_size (int): Number of elements in a patch.
+        hidden_channels (int): Intermediate size of each sample.
+        num_transformer_blocks (int, optional): Number of transformer blocks
             (default: :obj:`1`).
         heads (int, optional): Number of multi-head-attentions.
             (default: :obj:`1`)
-        patch_size (int, optional): Number of links in a patch.
-            (default: :obj:`5`)
-        norm (str, optional): If set to :obj:`True`, will apply layer
-            normalization. (default: :obj:`False`)
         dropout (float, optional): Dropout probability of attention weights.
-            (default: :obj:`0`)
-        aggr (list[str], optional): Aggregation module, ['sum','max','mean'].
-        time_dim (int, optional): time embedding dimension,
-
+            (default: :obj:`0.0`)
+        aggr (str or list[str], optional): The aggregation module, *e.g.*,
+            :obj:`"sum"`, :obj:`"mean"`, :obj:`"min"`, :obj:`"max"`,
+            :obj:`"var"`, :obj:`"std"`. (default: :obj:`"mean"`)
     """
     def __init__(
         self,
-        channels: int,
-        hidden_channels: int = 64,
-        out_dim: int = 128,
-        max_edge: int = 16,
-        num_encoder_blocks: int = 1,
+        in_channels: int,
+        out_channels: int,
+        patch_size: int,
+        hidden_channels: int,
+        num_transformer_blocks: int = 1,
         heads: int = 1,
-        patch_size: int = 4,
         dropout: float = 0.0,
-        aggr: list[str] = None,
-        time_dim: int = 0,
-    ):
+        aggr: Union[str, List[str]] = 'mean',
+    ) -> None:
         super().__init__()
-        self.channels = channels
-        self.time_dim = time_dim
-        self.hidden_channels = hidden_channels
-        self.out_dim = out_dim
-        self.heads = heads
-        self.patch_size = patch_size
-        self.dropout = dropout
-        self.max_edge = max_edge
-        self.aggr = aggr
-        if (self.aggr is None):
-            self.aggr = ["mean"]
 
-        # encoders and linear layers
-        self.feat_encoder = FeatEncoder(self.channels, self.hidden_channels,
-                                        time_dim=self.time_dim)
-        self.layernorm = torch.nn.LayerNorm(self.hidden_channels)
-        self.atten_blocks = torch.nn.ModuleList([
-            TransformerBlock(hidden_channels=self.hidden_channels,
-                             heads=self.heads, dropout=self.dropout)
-            for _ in range(num_encoder_blocks)
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.patch_size = patch_size
+        self.aggrs = [aggr] if isinstance(aggr, str) else aggr
+
+        assert len(self.aggrs) > 0
+        for aggr in self.aggrs:
+            assert aggr in ['sum', 'mean', 'min', 'max', 'var', 'std']
+
+        self.lin = torch.nn.Linear(in_channels, hidden_channels)
+        self.pad_projector = torch.nn.Linear(
+            patch_size * hidden_channels,
+            hidden_channels,
+        )
+        self.pe = PositionalEncoding(hidden_channels)
+
+        self.blocks = torch.nn.ModuleList([
+            MultiheadAttentionBlock(
+                channels=hidden_channels,
+                heads=heads,
+                layer_norm=True,
+                dropout=dropout,
+            ) for _ in range(num_transformer_blocks)
         ])
 
-        # input is node feature and transformer node embed
-        self.fc = torch.nn.Linear(self.hidden_channels * (len(self.aggr)),
-                                  self.out_dim)
+        self.fc = torch.nn.Linear(
+            hidden_channels * len(self.aggrs),
+            out_channels,
+        )
 
-        # padding
-        self.stride = self.patch_size
-        self.pad_projector = torch.nn.Linear(
-            self.patch_size * self.hidden_channels, self.hidden_channels)
-        self.pe = PositionalEncoding1D(self.hidden_channels)
-        self.pe = PositionalEncoding(self.hidden_channels)
-
-    def reset_parameters(self):
-        self.feat_encoder.reset_parameters()
-        for atten_block in self.atten_blocks:
-            atten_block.reset_parameters()
-        self.fc.reset_parameters()
+    def reset_parameters(self) -> None:
+        self.lin.reset_parameters()
         self.pad_projector.reset_parameters()
+        self.pe.reset_parameters()
+        for block in self.blocks:
+            block.reset_parameters()
+        self.fc.reset_parameters()
 
     @disable_dynamic_shapes(required_args=['dim_size', 'max_num_elements'])
     def forward(
@@ -105,52 +95,49 @@ class PatchTransformerAggregation(Aggregation):
         max_num_elements: Optional[int] = None,
     ) -> Tensor:
 
-        # x = [num_edges, input_channels]
-        x = self.feat_encoder(x)
-
-        # x = [num_edges, hidden_channels]
-        edge_feat_size = x.shape[1]
-
-        # x = [batch_size, max_num_elements, -1]
-        x, mask = self.to_dense_batch(x, index, ptr, dim_size, dim,
-                                      max_num_elements=max_num_elements)
-
-        x_feat = x.view(mask.shape[0] * max_num_elements, edge_feat_size)
-        # x_feat = [batch_size, # patch, patch_size * edge_feat_size]
-        x_feat = x_feat.view(-1, self.max_edge // self.patch_size,
-                             self.patch_size * x_feat.shape[-1])
-        # x_feat = [batch_size, max_edge, edge_feat_size]
-        x_feat = self.pad_projector(x_feat)
-
-        # x_feat = [batch_size, max_edge, edge_feat_size]
-        x_feat = x_feat + self.pe(x_feat)
-
-        # x_feat = [batch_size, max_edge, edge_feat_size]
-        for atten_block in self.atten_blocks:
-            x_feat = atten_block(x_feat)
-
-        x_feat = self.layernorm(x_feat)
-
-        # aggregate with a list of operations
-        out_list = []
-        for aggr in self.aggr:
-            if aggr == "sum":
-                out_list.append(torch.sum(x_feat, dim=1))
-            elif aggr == "mean":
-                out_list.append(torch.mean(x_feat, dim=1))
-            elif aggr == "max":
-                out, _ = torch.max(x_feat, dim=1)
-                out_list.append(out)
+        if max_num_elements is None:
+            if ptr is not None:
+                count = ptr.diff()
             else:
-                raise ValueError(f"the following aggregation is not supported"
-                                 f"'{aggr}'")
-        x_final = torch.cat(out_list, dim=1)
-        return self.fc(x_final)
+                count = scatter(torch.ones_like(index), index, dim=0,
+                                dim_size=dim_size, reduce='sum')
+            max_num_elements = int(count.max()) + 1
+
+        # Set `max_num_elements` to a multiple of `patch_size`:
+        max_num_elements = (math.floor(max_num_elements / self.patch_size) *
+                            self.patch_size)
+
+        x = self.lin(x)
+
+        # TODO If groups are heavily unbalanced, this will create a lot of
+        # "empty" patches. Try to figure out a way to fix this.
+        # [batch_size, num_patches * patch_size, hidden_channels]
+        x, _ = self.to_dense_batch(x, index, ptr, dim_size, dim,
+                                   max_num_elements=max_num_elements)
+
+        # [batch_size, num_patches, patch_size * hidden_channels]
+        x = x.view(x.size(0), max_num_elements // self.patch_size,
+                   self.patch_size * x.size(-1))
+
+        # [batch_size, num_patches, hidden_channels]
+        x = self.pad_projector(x)
+
+        x = x + self.pe(torch.arange(x.size(1), device=x.device))
+
+        # [batch_size, num_patches, hidden_channels]
+        for block in self.blocks:
+            x = block(x, x)
+
+        # [batch_size, hidden_channels]
+        outs: List[Tensor] = []
+        for aggr in self.aggrs:
+            out = getattr(torch, aggr)(x, dim=1)
+            outs.append(out[0] if isinstance(out, tuple) else out)
+        out = torch.cat(outs, dim=1) if len(outs) > 1 else outs[0]
+
+        # [batch_size, out_channels]
+        return self.fc(out)
 
     def __repr__(self) -> str:
-        return (f'{self.__class__.__name__}({self.channels},'
-                f'hidden_channels={self.hidden_channels},'
-                f'heads={self.heads},'
-                f'patch_size={self.patch_size},'
-                f'aggr={self.aggr},'
-                f'dropout={self.dropout})')
+        return (f'{self.__class__.__name__}({self.in_channels}, '
+                f'{self.out_channels}, patch_size={self.patch_size})')
