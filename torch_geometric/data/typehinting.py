0d84b832e2fd20efcf488046747737eae8aa69cf
Arian R. Jamasb <arian@jamasb.io>
2024-07-23 23:32:52 +0200

    dependency: add import-level support for older jaxtyping versions

--- torch_geometric/data/typehinting.py
+++ torch_geometric/data/typehinting.py
@@ -18,7 +18,11 @@ from typing import (
 
 import numpy as np
 from beartype.door import is_bearable
-from jaxtyping._array_types import _MetaAbstractArray, _MetaAbstractDtype
+
+try:
+    from jaxtyping._array_types import _MetaAbstractArray, _MetaAbstractDtype
+except ImportError:
+    from jaxtyping.array_types import _MetaAbstractArray, _MetaAbstractDtype
 
 from torch_geometric.data import Batch, Data
 from torch_geometric.data.batch import DynamicInheritance
@@ -39,21 +43,22 @@ def _check(hint: Any, argument_name: str, value: Any) -> None:
     Raises:
         TypeError: If the value does not match the type hint.
     """
+
     def _check_instance(value: Any) -> None:
         if not isinstance(value, Data) and not isinstance(value, Batch):
             raise TypeError(
-                f"{value} ({type(value)}) is not a pyg Data or Batch object.")
+                f"{value} ({type(value)}) is not a pyg Data or Batch object."
+            )
 
     # Check if all required attributes are present
     def _check_attributes(hint: Any, attributes: Dict[str, Any]) -> None:
-        if hint.check_only_specified and set(
-                attributes.keys()) != hint.attributes:
+        if hint.check_only_specified and set(attributes.keys()) != hint.attributes:
             raise TypeError(
                 f"{argument_name} Data attributes  {set(attributes.keys())} do not match \
-                    required set {hint.attributes}")
+                    required set {hint.attributes}"
+            )
 
-        if not hint.check_only_specified and not hint.attributes.issubset(
-                attributes):
+        if not hint.check_only_specified and not hint.attributes.issubset(attributes):
             raise TypeError(
                 f"{argument_name} is missing some attributes from {hint.attributes}"
             )
@@ -63,7 +68,7 @@ def _check(hint: Any, argument_name: str, value: Any) -> None:
         dtypes = {k: type(v) for k, v in value._store.items()}
         for colname, dt in hint.dtypes.items():
             if isinstance(dt, _MetaAbstractDtype) or isinstance(
-                    dt, _MetaAbstractArray
+                dt, _MetaAbstractArray
             ):  # Check for a jaxtyping annotation and use its typechecker
                 if not is_bearable(getattr(value, colname), dt):
                     raise TypeError(
@@ -72,9 +77,11 @@ def _check(hint: Any, argument_name: str, value: Any) -> None:
             # Otherwise just check type
             else:
                 if not np.issubdtype(dtypes[colname], np.dtype(dt)):
-                    raise TypeError(f"{dtypes[colname]} is not a \
+                    raise TypeError(
+                        f"{dtypes[colname]} is not a \
                         subtype of {dt} for data/batch \
-                        attribute {colname}")
+                        attribute {colname}"
+                    )
 
     _check_instance(value)
     attributes = value._store
@@ -107,12 +114,13 @@ def typecheck(_f: Optional[Callable] = None, strict: bool = False) -> Callable:
             bound = signature.bind(*args, **kwargs)
             for argument_name, value in bound.arguments.items():
                 hint = hints[argument_name]
-                if argument_name in hints and (isinstance(
-                        hint, (DataMeta, BatchMeta))):
+                if argument_name in hints and (isinstance(hint, (DataMeta, BatchMeta))):
                     _check(hint, argument_name, value)
-                elif (argument_name in hints
-                      and not (isinstance(hint,
-                                          (DataMeta, BatchMeta))) and strict):
+                elif (
+                    argument_name in hints
+                    and not (isinstance(hint, (DataMeta, BatchMeta)))
+                    and strict
+                ):
                     if isinstance(hint, NewType):
                         if not isinstance(value, hint.__supertype__):
                             raise TypeError(
@@ -164,7 +172,8 @@ def _resolve_type(t: Any) -> Any:
         <class 'int'>
     """
     if isinstance(t, _MetaAbstractDtype) or isinstance(
-            t, _MetaAbstractDtype):  # support for NewType in type hinting
+        t, _MetaAbstractDtype
+    ):  # support for NewType in type hinting
         return t
     if hasattr(t, "__supertype__"):
         return _resolve_type(t.__supertype__)
@@ -190,18 +199,20 @@ class DataMeta(GenericMeta, ABCMeta):
     Methods:
         None specific to DataMeta.
     """
+
     def __new__(metacls, name, bases, namespace, **kargs):
         return super().__new__(metacls, name, bases, namespace)
 
     @_tp_cache
     def __getitem__(self, parameters):
-        if hasattr(self, "__origin__") and (self.__origin__ is not None
-                                            or self._gorg is not DataT):
+        if hasattr(self, "__origin__") and (
+            self.__origin__ is not None or self._gorg is not DataT
+        ):
             return super().__getitem__(parameters)
         if parameters == ():
-            return super().__getitem__((_TypingEmpty, ))
+            return super().__getitem__((_TypingEmpty,))
         if not isinstance(parameters, tuple):
-            parameters = (parameters, )
+            parameters = (parameters,)
         parameters = list(parameters)
 
         check_only_specified = True
@@ -235,18 +246,20 @@ class BatchMeta(GenericMeta, DynamicInheritance):
     Methods:
         None specific to BatchMeta.
     """
+
     def __new__(metacls, name, bases, namespace, **kargs):
         return super().__new__(metacls, name, bases, namespace)
 
     @_tp_cache
     def __getitem__(self, parameters):
-        if hasattr(self, "__origin__") and (self.__origin__ is not None
-                                            or self._gorg is not BatchT):
+        if hasattr(self, "__origin__") and (
+            self.__origin__ is not None or self._gorg is not BatchT
+        ):
             return super().__getitem__(parameters)
         if parameters == ():
-            return super().__getitem__((_TypingEmpty, ))
+            return super().__getitem__((_TypingEmpty,))
         if not isinstance(parameters, tuple):
-            parameters = (parameters, )
+            parameters = (parameters,)
         parameters = list(parameters)
 
         check_only_specified = True
@@ -297,8 +310,10 @@ def _get_attribute_dtypes(
         attributes |= subattributes
         dtypes.update(subdtypes)
     else:
-        raise TypeError("DataT[attr1, attr2, ...]: each attribute must be \
-            a string, list or set.")
+        raise TypeError(
+            "DataT[attr1, attr2, ...]: each attribute must be \
+            a string, list or set."
+        )
     return attributes, dtypes
 
 
@@ -354,7 +369,8 @@ class DataT(Data, extra=Generic, metaclass=DataMeta):
         if not hasattr(cls, "_gorg") or cls._gorg is DataT:
             raise TypeError(
                 "Type 'GraphT' cannot be instantiated directly. "
-                "It is intended to be used as a type annotation only.")
+                "It is intended to be used as a type annotation only."
+            )
 
 
 class BatchT(Batch, extra=Generic, metaclass=BatchMeta):
@@ -407,4 +423,5 @@ class BatchT(Batch, extra=Generic, metaclass=BatchMeta):
         if not hasattr(cls, "_gorg") or cls._gorg is BatchT:
             raise TypeError(
                 "Type 'BatchT' cannot be instantiated directly. "
-                "It is intended to be used as a type annotation only.")
+                "It is intended to be used as a type annotation only."
+            )
