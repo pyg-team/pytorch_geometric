dde79cf78b973bd234790a37bd3a603dbda8f178
Rishi Puri <puririshi98@berkeley.edu>
2024-08-16 14:51:16 -0700

    Update conftest.py

--- test/conftest.py
+++ test/conftest.py
@@ -42,8 +42,6 @@ def load_dataset(root: str, name: str, *args, **kwargs) -> Dataset:
     if name.lower() in ['hetero']:
         from torch_geometric.testing import FakeHeteroDataset
         return FakeHeteroDataset(*args, **kwargs)
-    if name.lower() == 'webqspdataset':
-        return WebQSPDataset()
 
     raise ValueError(f"Cannot load dataset with name '{name}'")
 
