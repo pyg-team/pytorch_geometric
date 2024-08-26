117ec9e8b2eb5b63862aea2b5713c6061a0f9b63
Zack Aristei <zaristei@zaristei-nvidia.client.nvidia.com>
2024-08-26 14:23:22 -0700

    begin incorporating approx knn so that dataset can be fully generated

--- examples/llm_plus_gnn/rag_feature_store.py
+++ examples/llm_plus_gnn/rag_feature_store.py
@@ -11,6 +11,9 @@ from torch_geometric.distributed import LocalFeatureStore
 from torch_geometric.nn.nlp import SentenceTransformer
 from torch_geometric.sampler import HeteroSamplerOutput, SamplerOutput
 from torch_geometric.typing import InputEdges, InputNodes
+import gc
+from torch_geometric.nn.pool import ApproxMIPSKNNIndex
+
 
 
 # NOTE: Only compatible with Homogeneous graphs for now
@@ -35,18 +38,10 @@ class KNNRAGFeatureStore(LocalFeatureStore):
         return self.get_tensor(group_name=(None, None), attr_name='edge_attr')
 
     def retrieve_seed_nodes(self, query: Any, k_nodes: int = 5) -> InputNodes:
-        """Retrieve the seed nodes for a given query using KNN search after
-            embedding the query.
-
-        Args:
-            query (Any): Input to be embedded and used for KNN search.
-            k_nodes (int, optional): Number of nodes to search for in KNN
-                search. Defaults to 5.
-
-        Returns:
-            InputNodes: Input object to be used in a PyG Sampler.
-        """
-        return next(self._retrieve_seed_nodes_batch([query], k_nodes))
+        result = next(self._retrieve_seed_nodes_batch([query], k_nodes))
+        gc.collect()
+        torch.cuda.empty_cache()
+        return result
 
     def _retrieve_seed_nodes_batch(self, query: Iterable[Any],
                                    k_nodes: int) -> Iterator[InputNodes]:
@@ -62,18 +57,10 @@ class KNNRAGFeatureStore(LocalFeatureStore):
             yield indices
 
     def retrieve_seed_edges(self, query: Any, k_edges: int = 3) -> InputEdges:
-        """Retrieve the seed edges for a given query using KNN search after
-        embedding the query.
-
-        Args:
-            query (Any): Input to be embedded and used for KNN search.
-            k_edges (int, optional): Number of edges to search for in KNN
-                search. Defaults to 3.
-
-        Returns:
-            InputNodes: Input object to be used in a PyG Sampler.
-        """
-        return next(self._retrieve_seed_edges_batch([query], k_edges))
+        result = next(self._retrieve_seed_edges_batch([query], k_edges)) 
+        gc.collect()
+        torch.cuda.empty_cache()
+        return result
 
     def _retrieve_seed_edges_batch(self, query: Iterable[Any],
                                    k_edges: int) -> Iterator[InputEdges]:
@@ -93,17 +80,7 @@ class KNNRAGFeatureStore(LocalFeatureStore):
     def load_subgraph(
         self, sample: Union[SamplerOutput, HeteroSamplerOutput]
     ) -> Union[Data, HeteroData]:
-        """Retrieve subgraph features corresponding to the given sampler
-        output.
 
-        Args:
-            sample (Union[SamplerOutput, HeteroSamplerOutput]): Output from a
-                PyG Sampler to retrieve subgraph features for.
-
-        Returns:
-            Union[Data, HeteroData]: Data object containing subgraph features
-                and edge indices.
-        """
         if isinstance(sample, HeteroSamplerOutput):
             raise NotImplementedError
 
@@ -119,9 +96,82 @@ class KNNRAGFeatureStore(LocalFeatureStore):
                     node_idx=node_id, edge_idx=edge_id)
 
 
+# TODO: Refactor because composition >> inheritance
+
+def _add_features_to_knn_index(knn_index: ApproxMIPSKNNIndex, emb: Tensor, device: torch.device, batch_size: int = 2**20):
+    """Add new features to the existing KNN index in batches.
+
+    Args:
+        knn_index (ApproxMIPSKNNIndex): Index to add features to.
+        emb (Tensor): Embeddings to add.
+        device (torch.device): Device to store in
+        batch_size (int, optional): Batch size to iterate by. Defaults to 2**20, which equates to 4GB if working with 1024 dim floats.
+    """
+    for i in range(0, emb.size(0), batch_size):
+        if emb.size(0) - i >= batch_size:
+            emb_batch = emb[i:i+batch_size].to(device)
+        else:
+            emb_batch = emb[i:].to(device) 
+        knn_index.add(emb_batch)
+
+class ApproxKNNRAGFeatureStore(KNNRAGFeatureStore):
+    def __init__(self, enc_model: Type[Module],
+                 model_kwargs: Optional[Dict[str,
+                                             Any]] = None, *args, **kwargs):
+        # TODO: Add kwargs for approx KNN to parameters here.
+        super().__init__(enc_model, model_kwargs, *args, **kwargs)
+        self.node_knn_index = None
+        self.edge_knn_index = None
+
+    def _retrieve_seed_nodes_batch(self, query: Iterable[Any],
+                                   k_nodes: int) -> Iterator[InputNodes]:
+        if isinstance(self.meta, dict) and self.meta.get("is_hetero", False):
+            raise NotImplementedError
+
+        enc_model = self.enc_model.to(self.device)
+        query_enc = enc_model.encode(query,
+                                          **self.model_kwargs).to(self.device)
+        del enc_model
+        gc.collect()
+        torch.cuda.empty_cache()
+
+        if self.node_knn_index is None:
+            self.node_knn_index = ApproxMIPSKNNIndex(num_cells=100, num_cells_to_visit=100, bits_per_vector=4)
+            # Need to add in batches to avoid OOM
+            _add_features_to_knn_index(self.node_knn_index, self.x, self.device)
+
+        output = self.node_knn_index.search(query_enc, k=k_nodes)
+        yield from output.index
+
+    def _retrieve_seed_edges_batch(self, query: Iterable[Any],
+                                   k_edges: int) -> Iterator[InputEdges]:
+        if isinstance(self.meta, dict) and self.meta.get("is_hetero", False):
+            raise NotImplementedError
+
+        enc_model = self.enc_model.to(self.device)
+        query_enc = enc_model.encode(query,
+                                          **self.model_kwargs).to(self.device)
+        del enc_model
+        gc.collect()
+        torch.cuda.empty_cache()
+
+        if self.edge_knn_index is None:
+            self.edge_knn_index = ApproxMIPSKNNIndex(num_cells=100, num_cells_to_visit=100, bits_per_vector=4)
+            # Need to add in batches to avoid OOM
+            _add_features_to_knn_index(self.edge_knn_index, self.edge_attr, self.device)
+
+        output = self.edge_knn_index.search(query_enc, k=k_edges)
+        yield from output.index
+
+
+
+# TODO: These two classes should be refactored
 class SentenceTransformerFeatureStore(KNNRAGFeatureStore):
     def __init__(self, *args, **kwargs):
-        kwargs['model_name'] = \
-            kwargs.get('model_name',
-                       'sentence-transformers/all-roberta-large-v1')
+        kwargs['model_name'] = kwargs.get('model_name', 'sentence-transformers/all-roberta-large-v1')
+        super().__init__(SentenceTransformer, *args, **kwargs)
+
+class SentenceTransformerApproxFeatureStore(ApproxKNNRAGFeatureStore):
+    def __init__(self, *args, **kwargs):
+        kwargs['model_name'] = kwargs.get('model_name','sentence-transformers/all-roberta-large-v1')
         super().__init__(SentenceTransformer, *args, **kwargs)
