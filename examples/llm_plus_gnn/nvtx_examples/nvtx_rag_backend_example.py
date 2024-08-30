ab53471c0e96093dd3ac96b3ff8fd57493208f80
Zack Aristei <zaristei@zaristei-nvidia.client.nvidia.com>
2024-08-30 13:50:14 -0700

    typo

--- examples/llm_plus_gnn/nvtx_examples/nvtx_rag_backend_example.py
+++ examples/llm_plus_gnn/nvtx_examples/nvtx_rag_backend_example.py
@@ -17,7 +17,7 @@ from torch_geometric.nn.nlp import SentenceTransformer
 from torch_geometric.profile.nvtx import nvtxit
 
 sys.path.append('..')
-from profiling_utils import create_remote_backend_from_triplets  # noqa: E402
+from rag_backend_utils import create_remote_backend_from_triplets  # noqa: E402
 from rag_feature_store import SentenceTransformerFeatureStore  # noqa: E402
 from rag_graph_store import NeighborSamplingRAGGraphStore  # noqa: E402
 
