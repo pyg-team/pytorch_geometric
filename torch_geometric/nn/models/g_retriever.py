99374884fad42a967aa2faf1e5e30c3c946a60d1
puririshi98 <puririshi98@berkeley.edu>
2024-09-10 15:21:06 -0700

    updating to accomodate matthias's changes

--- torch_geometric/nn/models/g_retriever.py
+++ torch_geometric/nn/models/g_retriever.py
@@ -126,7 +126,10 @@ class GRetriever(torch.nn.Module):
         x = self.projector(x)
         xs = x.split(1, dim=0)
         batch_unique = torch.unique(batch)
-        xs = [x if i in batch_unique else None for i, x in enumerate(xs)]
+        batch_size = len(question)
+        # handle questions with no node features
+        if len(batch_unique) < batch_size
+            xs = [xs[i] if i in batch_unique else None for i in range(batch_size)]
 
         (
             inputs_embeds,
@@ -176,7 +179,10 @@ class GRetriever(torch.nn.Module):
         x = self.projector(x)
         xs = x.split(1, dim=0)
         batch_unique = torch.unique(batch)
-        xs = [x if i in batch_unique else None for i, x in enumerate(xs)]
+        batch_size = len(question)
+        # handle questions with no node features
+        if len(batch_unique) < batch_size
+            xs = [xs[i] if i in batch_unique else None for i in range(batch_size)]
         inputs_embeds, attention_mask, _ = self.llm._get_embeds(
             question, additional_text_context, xs)
 
