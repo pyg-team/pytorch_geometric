92411cb7ca6277b6611146873e2e2954687556a6
puririshi98 <puririshi98@berkeley.edu>
2024-09-30 11:36:36 -0700

    wip

--- torch_geometric/datasets/ogbg_code2.py
+++ torch_geometric/datasets/ogbg_code2.py
@@ -169,7 +169,7 @@ class OGBG_Code2(InMemoryDataset):
         raw_datasets = datasets.load_dataset("claudios/code_search_net",
                                                  "python")
         self.combined_rawset = datasets.concatenate_datasets(
-            [dataset["train"], dataset["validation"], dataset["test"]])
+            [raw_datasets["train"], raw_datasets["validation"], raw_datasets["test"]])
         self.df = make_df_from_raw_data(self.raw_dataset)
         for dataset, path in zip(
             [
