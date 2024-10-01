6f4a8ed2f7144f288819f368e8b1152e536c6a12
puririshi98 <puririshi98@berkeley.edu>
2024-10-01 10:34:37 -0700

    training time

--- examples/llm/ogbg_code2.py
+++ examples/llm/ogbg_code2.py
@@ -13,6 +13,7 @@ master_prompt = "Please provide the name of the python function."
 def get_loss_ogbg(model, batch, **kwargs) -> torch.Tensor:
     questions = [master_prompt for i in range(len(batch.y))]
     labels = ['|'.join(label) for label in batch.y]
+    print("batch.x=",batch.x)
     return model(questions, batch.x, batch.edge_index, batch.batch,
                  labels, batch.edge_attr, batch.desc)
 
