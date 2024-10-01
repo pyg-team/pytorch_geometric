636e0bc5464c8b7066d433ff0aa5474f53410d27
puririshi98 <puririshi98@berkeley.edu>
2024-10-01 10:23:55 -0700

    training time

--- examples/llm/ogbg_code2.py
+++ examples/llm/ogbg_code2.py
@@ -12,19 +12,21 @@ master_prompt = "Please provide the name of the python function."
 
 def get_loss_ogbg(model, batch, **kwargs) -> torch.Tensor:
     questions = [master_prompt for i in range(len(batch.y))]
+    labels = ['|'.join(label) for label in batch.y]
     return model(questions, batch.x, batch.edge_index, batch.batch, batch.ptr,
-                 '|'.join(batch.y), batch.edge_attr, batch.desc)
+                 labels, batch.edge_attr, batch.desc)
 
 
 def inference_step_ogbg(model, batch, **kwargs):
     questions = [master_prompt for i in range(len(batch.y))]
     pred = model.inference(questions, batch.x, batch.edge_index, batch.batch,
                            batch.ptr, batch.edge_attr, batch.desc)
+    labels = ['|'.join(label) for label in batch.y]
     eval_data = {
         "pred": pred,
         "question": batch.question,
         "desc": batch.desc,
-        "label": '|'.join(batch.y)
+        "label": labels
     }
     return eval_data
 
