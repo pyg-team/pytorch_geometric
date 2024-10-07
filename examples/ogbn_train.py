81424a0a82b7ea0a6a9dc6b109927da58d108c50
Guanxin Li <guanxinl@nvidia.com>
2024-10-07 17:03:58 +0000

    remove evaluation on test set

--- examples/ogbn_train.py
+++ examples/ogbn_train.py
@@ -39,11 +39,6 @@ parser.add_argument(
     action='store_true',
     help='Whether or not to use GAT model',
 )
-parser.add_argument(
-    '--verbose',
-    action='store_true',
-    help='Whether or not to generate statistical report',
-)
 parser.add_argument('--device', type=str, default='cuda')
 parser.add_argument('-e', '--epochs', type=int, default=10,
                     help='number of training epochs.')
@@ -74,13 +69,11 @@ if "papers" in str(args.dataset) and (psutil.virtual_memory().total /
     print("Warning: may not have enough RAM to use this many GPUs.")
     print("Consider upgrading RAM if an error occurs.")
     print("Estimated RAM Needed: ~390GB.")
-verbose = args.verbose
-if verbose:
-    wall_clock_start = time.perf_counter()
-    if args.use_gat:
-        print(f"Training {args.dataset} with GAT model.")
-    else:
-        print(f"Training {args.dataset} with GraphSage model.")
+wall_clock_start = time.perf_counter()
+if args.use_gat:
+    print(f"Training {args.dataset} with GAT model.")
+else:
+    print(f"Training {args.dataset} with GraphSage model.")
 
 if not torch.cuda.is_available():
     args.device = "cpu"
@@ -202,18 +195,16 @@ model = model.to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=args.lr,
                              weight_decay=args.wd)
 
-if verbose:
-    prep_time = round(time.perf_counter() - wall_clock_start, 2)
-    print("Total time before training begins (prep_time)=", prep_time,
-          "seconds")
-    print("Training...")
+prep_time = round(time.perf_counter() - wall_clock_start, 2)
+print("Total time before training begins (prep_time)=", prep_time,
+      "seconds")
+print("Training...")
 
-test_accs = []
 val_accs = []
 times = []
 train_times = []
 inference_times = []
-best_val = best_test = 0.
+best_val = 0.
 start = time.perf_counter()
 
 model.reset_parameters()
@@ -227,40 +218,30 @@ for epoch in range(1, num_epochs + 1):
 
     inference_start = time.time()
     val_acc = test(val_loader)
-    test_acc = test(test_loader)
 
     inference_times.append(time.time() - inference_start)
-    test_accs.append(test_acc)
     val_accs.append(val_acc)
     print(f'Epoch {epoch:02d}, Loss: {loss:.4f}, ',
           f'Train Time: {train_end - train_start:.4f}s')
-    print(f'Val: {val_acc * 100.0:.2f}%,', f'Test: {test_acc * 100.0:.2f}%')
+    print(f'Val: {val_acc * 100.0:.2f}%,')
 
     if val_acc > best_val:
         best_val = val_acc
-    if test_acc > best_test:
-        best_test = test_acc
     times.append(time.time() - train_start)
 
-if verbose:
-    test_acc = torch.tensor(test_accs)
-    val_acc = torch.tensor(val_accs)
-    print('============================')
-    print("Average Epoch Time on training: {:.4f}".format(
-        torch.tensor(train_times).mean()))
-    print("Average Epoch Time on inference: {:.4f}".format(
-        torch.tensor(inference_times).mean()))
-    print(f"Average Epoch Time: {torch.tensor(times).mean():.4f}")
-    print(f"Median time per epoch: {torch.tensor(times).median():.4f}s")
-    print(f'Final Test: {test_acc.mean():.4f} ± {test_acc.std():.4f}')
-    print(f'Final Validation: {val_acc.mean():.4f} ± {val_acc.std():.4f}')
-    print(f"Best validation accuracy: {best_val:.4f}")
-    print(f"Best testing accuracy: {best_test:.4f}")
+val_acc = torch.tensor(val_accs)
+print('============================')
+print("Average Epoch Time on training: {:.4f}".format(
+    torch.tensor(train_times).mean()))
+print("Average Epoch Time on inference: {:.4f}".format(
+    torch.tensor(inference_times).mean()))
+print(f"Average Epoch Time: {torch.tensor(times).mean():.4f}")
+print(f"Median time per epoch: {torch.tensor(times).median():.4f}s")
+print(f'Final Validation: {val_acc.mean():.4f} ± {val_acc.std():.4f}')
+print(f"Best validation accuracy: {best_val:.4f}")
 
-if verbose:
-    print("Testing...")
+print("Testing...")
 test_final_acc = test(test_loader)
 print(f'Test Accuracy: {100.0 * test_final_acc:.2f}%')
-if verbose:
-    total_time = round(time.perf_counter() - wall_clock_start, 2)
-    print("Total Program Runtime (total_time) =", total_time, "seconds")
+total_time = round(time.perf_counter() - wall_clock_start, 2)
+print("Total Program Runtime (total_time) =", total_time, "seconds")
