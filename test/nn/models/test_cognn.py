22a82d57c8e6b0068ed481784c4f55e309fba3bb
Ben Finkelshtein <ben.finkelshtein95@gmail.com>
2024-10-08 14:41:43 +0100

    Update test_cognn.py

--- test/nn/models/test_cognn.py
+++ test/nn/models/test_cognn.py
@@ -54,13 +54,13 @@ def test_cognn() -> None:
   (dropout): Dropout(p=0.1, inplace=False)
 )'''
 
-    # Test train:
+    # Train:
     model.train()
     out = model(x, edge_index)
     assert out.size() == (num_nodes, 1)
     assert out.min().item() >= -0.1 and out.max().item() < 1.3
 
-    # Test train:
+    # Test:
     model.eval()
     out = model(x, edge_index)
     assert out.size() == (num_nodes, 1)
