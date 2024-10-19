028dc3a1526cc58574a0bb13518ad087a4e7a313
xnuohz <xnuohz@126.com>
2024-10-20 00:58:48 +0800

    update

--- test/datasets/test_molecule_gpt_dataset.py
+++ test/datasets/test_molecule_gpt_dataset.py
@@ -3,7 +3,7 @@ from torch_geometric.datasets import MoleculeGPTDataset
 
 # @onlyOnline
 def test_molecule_gpt_dataset():
-    dataset = MoleculeGPTDataset(root='./data/MoleculeGPT', force_reload=True)
+    dataset = MoleculeGPTDataset(root='./data/MoleculeGPT')
     assert str(dataset) == f'MoleculeGPTDataset({len(dataset)})'
     assert dataset.num_edge_features == 4
-    assert dataset.num_node_features == 5
+    assert dataset.num_node_features == 6
