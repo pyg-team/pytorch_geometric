09a31257bdd4a048abc580872d4ed66debe2ff5b
Rishi Puri <puririshi98@berkeley.edu>
2024-10-25 14:22:10 -0700

    Update hotpot_qa.py

--- examples/llm/hotpot_qa.py
+++ examples/llm/hotpot_qa.py
@@ -36,9 +36,35 @@ for idx in tqdm(data_idxs, desc="Building KG"):
         txt=context_doc,
         QA_pair=QA_pair,
     )
-# (TODO) make RAGQueryLoader from kg_maker, need rebase onto Zack's PR
-# based on example:
-# https://github.com/pyg-team/pytorch_geometric/blob/f607374fc8250e5f08b10e82e8ada2adf2ed18cc/examples/llm/g_retriever_utils/rag_generate.py
+# (TODO) need rebase onto Zack's PR to be able to use the RAGQueryLoader
+from itertools import chain
+from torch_geometric.datasets.web_qsp_dataset import (
+    preprocess_triplet,
+    retrieval_via_pcst,
+)
+from torch_geometric.loader import RAGQueryLoader
+from torch_geometric.nn.nlp import SentenceTransformer
+from rag_backend_utils import create_remote_backend_from_triplets
+from rag_feature_store import SentenceTransformerFeatureStore
+from rag_graph_store import NeighborSamplingRAGGraphStore
+from g_retriever_utils import apply_retrieval_via_pcst
+
+triples = chain.from_iterable(triple_set for triple_set in kg_maker.relevant_triples.values())
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = SentenceTransformer(
+    model_name='sentence-transformers/all-roberta-large-v1').to(device)
+fs, gs = create_remote_backend_from_triplets(
+    triplets=triplets, node_embedding_model=model,
+    node_method_to_call="encode", path="backend",
+    pre_transform=preprocess_triplet, node_method_kwargs={
+        "batch_size": 256
+    }, graph_db=NeighborSamplingRAGGraphStore,
+    feature_db=SentenceTransformerFeatureStore).load()
+
+query_loader = RAGQueryLoader(data=(fs, gs), seed_nodes_kwargs={"k_nodes": 5},
+                              seed_edges_kwargs={"k_edges": 5},
+                              sampler_kwargs={"num_neighbors": [50] * 2},
+                              local_filter=transform)
 """
 approx precision = num_relevant_out_of_retrieved/num_retrieved_triples
 We will use precision as a proxy for recall. This is because for recall,
@@ -48,7 +74,10 @@ but this is not known.
 precisions = []
 for QA_pair in kg_maker.relevant_triples.keys()
     relevant_triples = kg_maker.relevant_triples[QA_pair]
-    retrieved_triples = #(TODO) call RAGQueryLoader
+    q = QA_pair[0]
+    retrieved_subgraph = query_loader.query(q)
+    print("retrieved_subgraph=", retrieved_subgraph)
+    retrieved_triples = # extract triples from subgraph
     num_relevant_out_of_retrieved = float(sum([int(bool(retrieved_triple in relevant_triples)) for retrieved_triple in retrieved_triples]))
     precisions.append(num_relevant_out_of_retrieved/len(retrieved_triples))
 approx_precision = mean(precisions)
