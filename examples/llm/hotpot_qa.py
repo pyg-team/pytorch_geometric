9c7eb7cf1037af57266d0a6326247ac3314c28cb
puririshi98 <puririshi98@berkeley.edu>
2024-11-06 21:21:21 -0800

    cleaning

--- examples/llm/hotpot_qa.py
+++ examples/llm/hotpot_qa.py
@@ -51,48 +51,48 @@ if __name__ == '__main__':
     # (TODO) need rebase onto Zack's PR to be able to use the RAGQueryLoader
     # Zacks PR: https://github.com/pyg-team/pytorch_geometric/pull/9666
     # Note: code below here will not work until the rebase is done
-    # from itertools import chain
+    from itertools import chain
 
-    # from g_retriever_utils.rag_generater import apply_retrieval_via_pcst
-    # from g_retriever_utils.rag_backend_utils import create_remote_backend_from_triplets
-    # from g_retriever_utils.rag_feature_store import SentenceTransformerFeatureStore
-    # from g_retriever_utils.rag_graph_store import NeighborSamplingRAGGraphStore
+    from g_retriever_utils.rag_generater import apply_retrieval_via_pcst
+    from g_retriever_utils.rag_backend_utils import create_remote_backend_from_triplets
+    from g_retriever_utils.rag_feature_store import SentenceTransformerFeatureStore
+    from g_retriever_utils.rag_graph_store import NeighborSamplingRAGGraphStore
 
-    # from torch_geometric.datasets.web_qsp_dataset import (
-    #     preprocess_triplet,
-    #     retrieval_via_pcst,
-    # )
-    # from itertools import chain
-    # from torch_geometric.loader import RAGQueryLoader
-    # from torch_geometric.nn.nlp import SentenceTransformer
+    from torch_geometric.datasets.web_qsp_dataset import (
+        preprocess_triplet,
+        retrieval_via_pcst,
+    )
+    from itertools import chain
+    from torch_geometric.loader import RAGQueryLoader
+    from torch_geometric.nn.nlp import SentenceTransformer
 
-    # triples = chain.from_iterable(triple_set for triple_set in kg_maker.relevant_triples.values())
-    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # model = SentenceTransformer(
-    #     model_name='sentence-transformers/all-roberta-large-v1').to(device)
-    # fs, gs = create_remote_backend_from_triplets(
-    #     triplets=triples, node_embedding_model=model,
-    #     node_method_to_call="encode", path="backend",
-    #     pre_transform=preprocess_triplet, node_method_kwargs={
-    #         "batch_size": 256
-    #     }, graph_db=NeighborSamplingRAGGraphStore,
-    #     feature_db=SentenceTransformerFeatureStore).load()
+    triples = chain.from_iterable(triple_set for triple_set in kg_maker.relevant_triples.values())
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = SentenceTransformer(
+        model_name='sentence-transformers/all-roberta-large-v1').to(device)
+    fs, gs = create_remote_backend_from_triplets(
+        triplets=triples, node_embedding_model=model,
+        node_method_to_call="encode", path="backend",
+        pre_transform=preprocess_triplet, node_method_kwargs={
+            "batch_size": min(len(data_idxs), 256)
+        }, graph_db=NeighborSamplingRAGGraphStore,
+        feature_db=SentenceTransformerFeatureStore).load()
 
-    # query_loader = RAGQueryLoader(data=(fs, gs), seed_nodes_kwargs={"k_nodes": 5},
-    #                               seed_edges_kwargs={"k_edges": 5},
-    #                               sampler_kwargs={"num_neighbors": [50] * 2},
-    #                               local_filter=transform)
-    # """
-    # approx precision = num_relevant_out_of_retrieved/num_retrieved_triples
-    # We will use precision as a proxy for recall. This is because for recall,
-    # we must know how many relevant triples exist for each question,
-    # but this is not known.
-    # """
-    # precisions = []
-    # for QA_pair in kg_maker.relevant_triples.keys():
-    #     relevant_triples = kg_maker.relevant_triples[QA_pair]
-    #     q = QA_pair[0]
-    #     retrieved_subgraph = query_loader.query(q)
+    query_loader = RAGQueryLoader(data=(fs, gs), seed_nodes_kwargs={"k_nodes": 5},
+                                  seed_edges_kwargs={"k_edges": 5},
+                                  sampler_kwargs={"num_neighbors": [50] * 2},
+                                  local_filter=transform)
+    """
+    approx precision = num_relevant_out_of_retrieved/num_retrieved_triples
+    We will use precision as a proxy for recall. This is because for recall,
+    we must know how many relevant triples exist for each question,
+    but this is not known.
+    """
+    precisions = []
+    for QA_pair in kg_maker.relevant_triples.keys():
+        relevant_triples = kg_maker.relevant_triples[QA_pair]
+        q = QA_pair[0]
+        retrieved_subgraph = query_loader.query(q)
     #     print("retrieved_subgraph=", retrieved_subgraph)
     #     retrieved_triples = # extract triples from subgraph
     #     num_relevant_out_of_retrieved = float(sum([int(bool(retrieved_triple in relevant_triples)) for retrieved_triple in retrieved_triples]))
