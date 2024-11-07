2821344f6f728a75b940921950eb1ee6b3f4efad
Rishi Puri <puririshi98@berkeley.edu>
2024-11-06 20:08:40 -0800

    fixing import issue

--- examples/llm/g_retriever_utils/rag_generate.py
+++ examples/llm/g_retriever_utils/rag_generate.py
@@ -6,9 +6,9 @@ from typing import Tuple
 import pandas as pd
 import torch
 import tqdm
-from rag_backend_utils import create_remote_backend_from_triplets
-from rag_feature_store import SentenceTransformerFeatureStore
-from rag_graph_store import NeighborSamplingRAGGraphStore
+from g_retriever_utils.rag_backend_utils import create_remote_backend_from_triplets
+from g_retriever_utils.rag_feature_store import SentenceTransformerFeatureStore
+from g_retriever_utils.rag_graph_store import NeighborSamplingRAGGraphStore
 
 from torch_geometric.data import Data
 from torch_geometric.datasets import WebQSPDataset
