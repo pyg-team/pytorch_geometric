d3d920306c60a59651eeca8f232d6bc7be124ac9
puririshi98 <puririshi98@berkeley.edu>
2024-11-08 10:18:54 -0800

    reorg of zacks rag utils to put it under torch_geometric.utils.rag

--- examples/llm/hotpot_qa.py
+++ examples/llm/hotpot_qa.py
@@ -95,12 +95,12 @@ if __name__ == '__main__':
     we must know how many relevant triples exist for each question,
     but this is not known.
     """
-    precisions = []
-    for QA_pair in kg_maker.relevant_triples.keys():
-        relevant_triples = kg_maker.relevant_triples[QA_pair]
-        q = QA_pair[0]
-        retrieved_subgraph = query_loader.query(q)
-        print("retrieved_subgraph=", retrieved_subgraph)
+    # precisions = []
+    # for QA_pair in kg_maker.relevant_triples.keys():
+    #     relevant_triples = kg_maker.relevant_triples[QA_pair]
+    #     q = QA_pair[0]
+    #     retrieved_subgraph = query_loader.query(q)
+    #     print("retrieved_subgraph=", retrieved_subgraph)
     #     retrieved_triples = # extract triples from subgraph
     #     num_relevant_out_of_retrieved = float(sum([int(bool(retrieved_triple in relevant_triples)) for retrieved_triple in retrieved_triples]))
     #     precisions.append(num_relevant_out_of_retrieved/len(retrieved_triples))
