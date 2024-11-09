87a8f30efd89bf040ae8150816f1034fc5123127
puririshi98 <puririshi98@berkeley.edu>
2024-11-08 17:42:50 -0800

    reorg

--- torch_geometric/utils/rag/backend_utils.py
+++ torch_geometric/utils/rag/backend_utils.py
@@ -16,12 +16,12 @@ from typing import (
 import torch
 from torch import Tensor
 from torch.nn import Module
-
 from torch_geometric.data import (
     FeatureStore,
     GraphStore,
     LargeGraphIndexer,
     TripletLike,
+    Data
 )
 from torch_geometric.data.large_graph_indexer import EDGE_RELATION
 from torch_geometric.datasets.web_qsp_dataset import retrieval_via_pcst
