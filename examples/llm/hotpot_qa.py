2a251e667e5588c8bc5eea87bda8b80b5ccc4258
puririshi98 <puririshi98@berkeley.edu>
2024-11-12 14:45:59 -0800

    cleaning

--- examples/llm/hotpot_qa.py
+++ examples/llm/hotpot_qa.py
@@ -64,9 +64,9 @@ if __name__ == '__main__':
             )
         kg_maker.save_kg("hotpot_kg.pt")
         relevant_triples = kg_maker.relevant_triples
-        print("Total number of context characters parsed",
+        print("Total number of context characters parsed by LLM",
               kg_maker.total_chars_parsed)
-        print("Average number of context characters parsed per second=",
+        print("Average number of context characters parsed by LLM per second=",
               kg_maker.avg_chars_parsed_per_sec)
     print("Size of KG (number of triples) =",
           sum([len(rel_trips) for rel_trips in relevant_triples.values()]))
