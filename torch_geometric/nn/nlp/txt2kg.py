aaaedb66fc761df2c08abbed1ad04d920715dbd2
puririshi98 <puririshi98@berkeley.edu>
2024-11-12 16:02:27 -0800

    cleaning

--- torch_geometric/nn/nlp/txt2kg.py
+++ torch_geometric/nn/nlp/txt2kg.py
@@ -96,7 +96,10 @@ class TXT2KG():
                     triple_str = triple_str.replace("(", "").replace(
                         ")", "").replace("'", "")
                 split_trip = triple_str.split(',')
+                # remove blank space at beginning or end
                 split_trip = [(i[1:] if i[0] == " " else i)
+                              for i in split_trip]
+                split_trip = [(i[:-1] if i[-1] == " " else i)
                               for i in split_trip]
                 potential_trip = tuple(split_trip)
             except:  # noqa
