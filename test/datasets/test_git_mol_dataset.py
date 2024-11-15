8bf086d7afe8b854a742b47302858cfee50f8d68
xnuohz <xnuohz@126.com>
2024-11-16 01:27:22 +0800

    update

--- test/datasets/test_git_mol_dataset.py
+++ test/datasets/test_git_mol_dataset.py
@@ -7,6 +7,6 @@ def test_git_mol_dataset():
     dataset = GitMolDataset(root='./data/GITMol')
 
     assert len(dataset) == 3610
-    assert dataset[0].image.size() == (3, 224, 224)
+    assert dataset[0].image.size() == (1, 3, 224, 224)
     assert dataset[0].num_node_features == 9
     assert dataset[0].num_edge_features == 3
