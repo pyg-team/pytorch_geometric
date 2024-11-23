14ae2fbd49d66044d0bf278c21d7085d11c69233
zaristei <zaristei@berkeley.edu>
2024-11-23 09:46:39 +0000

    col limits on prints 2

--- examples/llm/g_retriever_utils/benchmark_model_archs_rag.py
+++ examples/llm/g_retriever_utils/benchmark_model_archs_rag.py
@@ -19,9 +19,7 @@ from minimal_demo import (  # noqa: E402
 
 # %%
 parser = argparse.ArgumentParser(description="""Benchmarker for GRetriever
-NOTE: Evaluating with smaller samples may result in poorer performance for the trained models compared to untrained models."""
-                                 # noqa: E501
-                                 )
+NOTE: Evaluating with smaller samples may result in poorer performance for the trained models compared to untrained models.""") # noqa: E501
 parser.add_argument("--hidden_channels", type=int, default=1024)
 parser.add_argument("--learning_rate", type=float, default=1e-5)
 parser.add_argument("--epochs", type=int, default=2)
