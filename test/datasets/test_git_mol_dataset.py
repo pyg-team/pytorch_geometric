f8760ec39951715def19a95ee62ae4fc3ed056fd
Matthias Fey <matthias.fey@tu-dortmund.de>
2024-11-25 11:24:00 +0100

    Run `GitMolDataset` tests only in full test mode (#9804)

--- test/datasets/test_git_mol_dataset.py
+++ test/datasets/test_git_mol_dataset.py
@@ -1,16 +1,19 @@
+from typing import Tuple
+
 import pytest
 
 from torch_geometric.datasets import GitMolDataset
-from torch_geometric.testing import withPackage
+from torch_geometric.testing import onlyFullTest, withPackage
 
 
+@onlyFullTest
 @withPackage('torchvision', 'rdkit', 'PIL')
 @pytest.mark.parametrize('split', [
     (0, 3610),
     (1, 451),
     (2, 451),
 ])
-def test_git_mol_dataset(split):
+def test_git_mol_dataset(split: Tuple[int, int]) -> None:
     dataset = GitMolDataset(root='./data/GITMol', split=split[0])
 
     assert len(dataset) == split[1]
