712645b62d65e8425597c8389a1fe3c9e1f783b8
Rishi Puri <puririshi98@berkeley.edu>
2024-11-26 14:40:23 -0800

    Draft: Multiprocessing for txt2kg w NIMs (#9799)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- torch_geometric/nn/nlp/txt2kg.py
+++ torch_geometric/nn/nlp/txt2kg.py
@@ -1,8 +1,17 @@
 import math
+import os
 import time
 from typing import List, Optional, Tuple
 
 import torch
+import torch.multiprocessing as mp
+
+CLIENT_INITD = False
+
+CLIENT = None
+NIM_MODEL = None
+GLOBAL_NIM_KEY = ""
+SYSTEM_PROMPT = "Please convert the above text into a list of knowledge triples with the form ('entity', 'relation', 'entity'). Seperate each with a new line. Do not output anything else.”"
 
 
 class TXT2KG():
@@ -25,16 +34,10 @@ class TXT2KG():
         if self.local_LM:
             self.initd_LM = False
         else:
-            # We use NIMs since most PyG users may not be able to run a 70B+ model
             assert NVIDIA_API_KEY != '', "Please pass NVIDIA_API_KEY or set local_small_lm flag to True"
-            from openai import OpenAI
-            self.client = OpenAI(
-                base_url="https://integrate.api.nvidia.com/v1",
-                api_key=NVIDIA_API_KEY)
-            self.model = "nvidia/llama-3.1-nemotron-70b-instruct"
+            self.NVIDIA_API_KEY = NVIDIA_API_KEY
 
         self.chunk_size = 512
-        self.system_prompt = "Please convert the above text into a list of knowledge triples with the form ('entity', 'relation', 'entity'). Seperate each with a new line. Do not output anything else.”"
         # useful for approximating recall of subgraph retrieval algos
         self.doc_id_counter = 0
         self.relevant_triples = {}
@@ -44,67 +47,21 @@ class TXT2KG():
     def save_kg(self, path: str) -> None:
         torch.save(self.relevant_triples, path)
 
-    def chunk_to_triples_str(self, txt: str) -> str:
+    def chunk_to_triples_str_local(self, txt: str) -> str:
         # call LLM on text
         chunk_start_time = time.time()
-        if self.local_LM:
-            if not self.initd_LM:
-                from torch_geometric.nn.nlp import LLM
-                LM_name = "VAGOsolutions/SauerkrautLM-v2-14b-DPO"
-                self.model = LLM(LM_name, num_params=14).eval()
-                self.initd_LM = True
-            out_str = self.model.inference(
-                question=[txt + '\n' + self.system_prompt],
-                max_tokens=self.chunk_size)[0]
-        else:
-            completion = self.client.chat.completions.create(
-                model=self.model, messages=[{
-                    "role":
-                    "user",
-                    "content":
-                    txt + '\n' + self.system_prompt
-                }], temperature=0, top_p=1, max_tokens=1024, stream=True)
-            out_str = ""
-            for chunk in completion:
-                if chunk.choices[0].delta.content is not None:
-                    out_str += chunk.choices[0].delta.content
+        if not self.initd_LM:
+            from torch_geometric.nn.nlp import LLM
+            LM_name = "VAGOsolutions/SauerkrautLM-v2-14b-DPO"
+            self.model = LLM(LM_name, num_params=14).eval()
+            self.initd_LM = True
+        out_str = self.model.inference(question=[txt + '\n' + SYSTEM_PROMPT],
+                                       max_tokens=self.chunk_size)[0]
+        # for debug
         self.total_chars_parsed += len(txt)
         self.time_to_parse += round(time.time() - chunk_start_time, 2)
         self.avg_chars_parsed_per_sec = self.total_chars_parsed / self.time_to_parse
         return out_str
-
-    def parse_n_check_triples(self,
-                              triples_str: str) -> List[Tuple[str, str, str]]:
-        # use pythonic checks for triples
-        processed = []
-        split_by_newline = triples_str.split("\n")
-        # sometimes LLM fails to obey the prompt
-        if len(split_by_newline) > 1:
-            split_triples = split_by_newline
-            llm_obeyed = True
-        else:
-            # handles form "(e, r, e) (e, r, e) ... (e, r, e)""
-            split_triples = triples_str[1:-1].split(") (")
-            llm_obeyed = False
-        for triple_str in split_triples:
-            try:
-                if llm_obeyed:
-                    # remove parenthesis and single quotes for parsing
-                    triple_str = triple_str.replace("(", "").replace(
-                        ")", "").replace("'", "")
-                split_trip = triple_str.split(',')
-                # remove blank space at beginning or end
-                split_trip = [(i[1:] if i[0] == " " else i)
-                              for i in split_trip]
-                split_trip = [(i[:-1] if i[-1] == " " else i)
-                              for i in split_trip]
-                potential_trip = tuple(split_trip)
-            except:  # noqa
-                continue
-            if 'tuple' in str(
-                    type(potential_trip)) and len(potential_trip) == 3:
-                processed.append(potential_trip)
-        return processed
 
     def add_doc_2_KG(
         self,
@@ -121,8 +78,105 @@ class TXT2KG():
             key = QA_pair
         else:
             key = self.doc_id_counter
-        self.relevant_triples[key] = []
-        for chunk in chunks:
-            self.relevant_triples[key] += self.parse_n_check_triples(
-                self.chunk_to_triples_str(chunk))
+        if self.local_LM:
+            # just for debug, no need to scale
+            self.relevant_triples[key] = llm_then_python_parse(
+                chunks, parse_n_check_triples, self.chunk_to_triples_str_local)
+        else:
+            num_procs = min(len(chunks), get_num_procs())
+            meta_chunk_size = int(len(chunks) / num_procs)
+            in_chunks_per_proc = {
+                j:
+                chunks[j * meta_chunk_size:min((j + 1) *
+                                               meta_chunk_size, len(chunks))]
+                for j in range(num_procs)
+            }
+            mp.spawn(
+                multiproc_helper,
+                args=(in_chunks_per_proc, parse_n_check_triples,
+                      chunk_to_triples_str_cloud, self.NVIDIA_API_KEY),
+                nprocs=num_procs)
+            self.relevant_triples[key] = []
+            for rank in range(num_procs):
+                self.relevant_triples[key] += torch.load(
+                    "/tmp/outs_for_proc_" + str(rank))
+                os.remove("/tmp/outs_for_proc_" + str(rank))
         self.doc_id_counter += 1
+
+
+def chunk_to_triples_str_cloud(txt: str, GLOBAL_NIM_KEY='') -> str:
+    global CLIENT_INITD
+    if not CLIENT_INITD:
+        # We use NIMs since most PyG users may not be able to run a 70B+ model
+        from openai import OpenAI
+        global CLIENT
+        CLIENT = OpenAI(base_url="https://integrate.api.nvidia.com/v1",
+                        api_key=GLOBAL_NIM_KEY)
+        global NIM_MODEL
+        NIM_MODEL = "nvidia/llama-3.1-nemotron-70b-instruct"
+        CLIENT_INITD = True
+    completion = CLIENT.chat.completions.create(
+        model=NIM_MODEL, messages=[{
+            "role": "user",
+            "content": txt + '\n' + SYSTEM_PROMPT
+        }], temperature=0, top_p=1, max_tokens=1024, stream=True)
+    out_str = ""
+    for chunk in completion:
+        if chunk.choices[0].delta.content is not None:
+            out_str += chunk.choices[0].delta.content
+    return out_str
+
+
+def parse_n_check_triples(triples_str: str) -> List[Tuple[str, str, str]]:
+    # use pythonic checks for triples
+    processed = []
+    split_by_newline = triples_str.split("\n")
+    # sometimes LLM fails to obey the prompt
+    if len(split_by_newline) > 1:
+        split_triples = split_by_newline
+        llm_obeyed = True
+    else:
+        # handles form "(e, r, e) (e, r, e) ... (e, r, e)""
+        split_triples = triples_str[1:-1].split(") (")
+        llm_obeyed = False
+    for triple_str in split_triples:
+        try:
+            if llm_obeyed:
+                # remove parenthesis and single quotes for parsing
+                triple_str = triple_str.replace("(", "").replace(")",
+                                                                 "").replace(
+                                                                     "'", "")
+            split_trip = triple_str.split(',')
+            # remove blank space at beginning or end
+            split_trip = [(i[1:] if i[0] == " " else i) for i in split_trip]
+            split_trip = [(i[:-1] if i[-1] == " " else i) for i in split_trip]
+            potential_trip = tuple(split_trip)
+        except:  # noqa
+            continue
+        if 'tuple' in str(type(potential_trip)) and len(potential_trip) == 3:
+            processed.append(potential_trip)
+    return processed
+
+
+def llm_then_python_parse(chunks, py_fn, llm_fn, **kwargs):
+    relevant_triples = []
+    for chunk in chunks:
+        relevant_triples += py_fn(llm_fn(chunk, **kwargs))
+    return relevant_triples
+
+
+def multiproc_helper(rank, in_chunks_per_proc, py_fn, llm_fn, NIM_KEY):
+    out = llm_then_python_parse(in_chunks_per_proc[rank], py_fn, llm_fn,
+                                GLOBAL_NIM_KEY=NIM_KEY)
+    torch.save(out, "/tmp/outs_for_proc_" + str(rank))
+
+
+def get_num_procs():
+    if hasattr(os, "sched_getaffinity"):
+        try:
+            num_proc = len(os.sched_getaffinity(0)) / (2)
+        except Exception:
+            pass
+    if num_proc is None:
+        num_proc = os.cpu_count() / (2)
+    return int(num_proc)
