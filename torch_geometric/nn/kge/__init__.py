f610d00009088d349db800f06e0d4b6de2460213
yukew1998 <yukew1998@gmail.com>
2024-12-19 17:05:29 -0500

    add TransF implementation

--- torch_geometric/nn/kge/__init__.py
+++ torch_geometric/nn/kge/__init__.py
@@ -5,6 +5,7 @@ from .transe import TransE
 from .complex import ComplEx
 from .distmult import DistMult
 from .rotate import RotatE
+from .transf import TransF
 
 __all__ = classes = [
     'KGEModel',
@@ -12,4 +13,5 @@ __all__ = classes = [
     'ComplEx',
     'DistMult',
     'RotatE',
+    'TransF'
 ]
