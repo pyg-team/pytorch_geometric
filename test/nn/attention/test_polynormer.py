ab3b2d9271ec7f39df9e0cf766ad3acec4c94d27
xnuohz <xnuohz@126.com>
2025-01-01 16:50:16 +0800

    update

--- test/nn/attention/test_polynormer.py
+++ test/nn/attention/test_polynormer.py
@@ -4,11 +4,9 @@ from torch_geometric.nn.attention import PolynormerAttention
 
 
 def test_polynormer_attention():
-    x = torch.randn(1, 4, 16)
+    x = torch.randn(4, 16)
     attn = PolynormerAttention(channels=16, heads=4)
     out = attn(x)
-    import pdb
-    pdb.set_trace()
-    assert out.shape == (1, 4, 16)
+    assert out.shape == (4, 256)
     assert str(attn) == ('PolynormerAttention(heads=4, '
-                         'head_channels=64 kernel=ReLU())')
+                         'head_channels=64)')
