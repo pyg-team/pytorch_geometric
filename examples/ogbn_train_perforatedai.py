0496528147fa7678ea9cfb38a4cccc83d06b63b6
Rorry <rabrenne19@gmail.com>
2025-01-08 17:02:36 -0500

    updated changelog and intro comment

--- examples/ogbn_train_perforatedai.py
+++ examples/ogbn_train_perforatedai.py
@@ -20,51 +20,6 @@ from perforatedai import pb_utils as PBU
 
 
 
-'''
-This PR is to include an example of how Perforated Backpropagation can be used to improve pytorch_geometric models.  As well as adding a scheduler to the original example.  Both of which improve upon the original results.
-
-Run docker from torch_geometric directory
-
-   docker run --gpus all -i --shm-size=8g -v .:/pai -w /pai -t nvcr.io/nvidia/pyg:24.11-py3 /bin/bash
-
-Within Docker
-
-    pip install -e .
-    cd examples
-    pip install PAI wheel file
-    
-Run original with:
-
-    CUDA_VISIBLE_DEVICES=0 python ogbn_train_original.py --dataset ogbn-products --batch_size 128
-
-Results:
-
-    Test Accuracy: 75.52%
-    
-Run original scheduler with:
-
-    CUDA_VISIBLE_DEVICES=0 python ogbn_train_scheduler.py --dataset ogbn-products --batch_size 128
-
-Results:
-
-    Test Accuracy: 77.51%
-    
-    
-Run PAI with:
-
-    CUDA_VISIBLE_DEVICES=0 python ogbn_trainPAI.py --dataset ogbn-products --batch_size 128 --saveName ogbnPAI
-    
-Results:
-
-    Test Accuracy: 78.10%
-'''
-
-
-
-
-
-
-
 
 '''
 PAI README:
