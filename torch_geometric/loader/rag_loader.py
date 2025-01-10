c207009f091fce3c2f12030b9482b11fb518b01b
riship <riship@nvidia.com>
2025-01-10 11:13:43 -0800

    debug

--- torch_geometric/loader/rag_loader.py
+++ torch_geometric/loader/rag_loader.py
@@ -104,7 +104,7 @@ class RAGQueryLoader:
         data = self.feature_store.load_subgraph(sample=subgraph_sample,
                                                 **self.loader_kwargs)
         # Feature store does not have access to edge_index, must fill here
-        data.edge_index = self.graph_store.edge_index[data.edge_idx]
+        data.edge_index = self.graph_store.edge_index[:, data.edge_idx]
         if self.local_filter:
             data = self.local_filter(data, query)
         return data
