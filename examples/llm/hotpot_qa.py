cbd645fbc7c22359fed0d4fe0074a94a2ec08dfc
riship <riship@nvidia.com>
2025-01-09 20:29:00 -0800

    typo fix

--- examples/llm/hotpot_qa.py
+++ examples/llm/hotpot_qa.py
@@ -127,7 +127,7 @@ if __name__ == '__main__':
     # number of hops for neighborsampling
     num_hops = 2
     query_loader = RAGQueryLoader(
-        data=(fs, gs), seed_nodes_kwargs={"k_nodes": knn_neighbsample_bs},
+        data=(fs, gs), seed_nodes_kwargs={"k_nodes": knn_neighsample_bs},
         seed_edges_kwargs={"k_edges": knn_neighsample_bs},
         sampler_kwargs={"num_neighbors": [fanout] * num_hops},
         local_filter=make_pcst_filter(triples, model))
