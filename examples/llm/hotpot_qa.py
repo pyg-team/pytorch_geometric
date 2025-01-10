845f18692c0cdb25a139b0c0bb2cf4c672f44566
riship <riship@nvidia.com>
2025-01-09 23:10:16 -0800

    debug

--- examples/llm/hotpot_qa.py
+++ examples/llm/hotpot_qa.py
@@ -123,7 +123,7 @@ if __name__ == '__main__':
     # k for KNN
     knn_neighsample_bs = 4096
     # number of neighbors for each seed node selected by KNN
-    fanout = 200
+    fanout = 400
     # number of hops for neighborsampling
     num_hops = 2
     query_loader = RAGQueryLoader(
