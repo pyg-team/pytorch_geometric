17933a0dbd167cf39554b94ecee256394f134c13
riship <riship@nvidia.com>
2025-01-10 15:09:44 -0800

    debug

--- torch_geometric/data/large_graph_indexer.py
+++ torch_geometric/data/large_graph_indexer.py
@@ -537,14 +537,7 @@ class LargeGraphIndexer:
         """
         x = torch.Tensor(self.get_node_features(node_feature_name))
         node_id = torch.LongTensor(range(len(x)))
-<<<<<<< HEAD
         print("self.get_edge_features(EDGE_INDEX)[:10]=",self.get_edge_features(EDGE_INDEX)[:10])
-=======
-        print("self.get_edge_features(EDGE_INDEX)[:10]=",
-              self.get_edge_features(EDGE_INDEX)[:10])
-        print("self.get_edge_features(EDGE_INDEX)[:10]=",
-              self.get_edge_features(EDGE_INDEX)[10])
->>>>>>> 3632cec84da62f274e35f3907b1a3b26fc625215
         edge_index = torch.t(
             torch.LongTensor(self.get_edge_features(EDGE_INDEX)))
         print("edge_index tensor of full graph=", edge_index[:, :10])
