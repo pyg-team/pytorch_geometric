f8f908e4f76dcc19a6d2e55d4c4e0e107678580b
riship <riship@nvidia.com>
2025-01-13 10:50:38 -0800

    debug

--- torch_geometric/utils/rag/feature_store.py
+++ torch_geometric/utils/rag/feature_store.py
@@ -94,12 +94,12 @@ class KNNRAGFeatureStore(LocalFeatureStore):
         print("num dst ids not in node ids inside `load_subgraph`=",
               sum([dst_id not in node_id for dst_id in sample.col]))
         edge_id = sample.edge
+        edge_index = torch.stack((sample.row, sample.col), dim=0)
         x = self.x[node_id]
         edge_attr = self.edge_attr[edge_id]
-        # do not have access to original edge_index
-        # must fill outside this function
-        return Data(x=x, edge_attr=edge_attr, node_idx=node_id,
-                    edge_idx=edge_id)
+
+        return Data(x=x, edge_index=edge_index, edge_attr=edge_attr,
+                    node_idx=node_id, edge_idx=edge_id)
 
 
 # TODO: Refactor because composition >> inheritance
