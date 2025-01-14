6350622a808c767672a364a2eab336baf0c875f4
riship <riship@nvidia.com>
2025-01-13 23:39:04 -0800

    debug

--- torch_geometric/loader/rag_loader.py
+++ torch_geometric/loader/rag_loader.py
@@ -5,7 +5,7 @@ from torch_geometric.data import Data, FeatureStore, HeteroData
 from torch_geometric.sampler import HeteroSamplerOutput, SamplerOutput
 from torch_geometric.typing import InputEdges, InputNodes
 
-
+import torch
 class RAGFeatureStore(Protocol):
     """Feature store template for remote GNN RAG backend."""
     @abstractmethod
