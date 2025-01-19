60e3444b54b5c986a9e615a94b1b28106f7cf412
Rorry <rabrenne19@gmail.com>
2025-01-19 14:04:37 -0500

    formatting updates

--- examples/ogbn_train_perforatedai.py
+++ examples/ogbn_train_perforatedai.py
@@ -17,10 +17,6 @@ from torch_geometric.utils import to_undirected
 from perforatedai import pb_globals as PBG
 from perforatedai import pb_models as PBM
 from perforatedai import pb_utils as PBU
-
-
-
-
 '''
 PAI README:
 
@@ -51,28 +47,27 @@ Run PAI with:
     
 Results:
 
-    Test Accuracy: 78.29%
+    Test Accuracy: 78.37%
 
 '''
-
 
 # Set Perforated Backpropagation settings for this training run
 
 # When to switch between Dendrite learning and neuron learning is determined by when the history of validation score or correlation scores shows those scores are no longer improving
-PBG.switchMode = PBG.doingHistory 
+PBG.switchMode = PBG.doingHistory
 # When calculating that just look at the current score, not a recent average
 PBG.historyLookback = 1
-# How many normal epochs to wait for before switching modes.  
-PBG.nEpochsToSwitch = 25  
+# How many normal epochs to wait for before switching modes.
+PBG.nEpochsToSwitch = 25
 PBG.pEpochsToSwitch = 15  # Same as above for Dendrite epochs
 # The default shape of input tensors will be [batch size, number of neurons in the layer]
 PBG.inputDimensions = [-1, 0]
 # This allows Dendrites to train as long as they keep improving rather than capping
 # Dendrite training cycles to only be as long as the first neuron training cycle.
-PBG.capAtN = False  
+PBG.capAtN = False
 # Stop the run after 4 dendrites are created
 PBG.maxDendrites = 4
-# If a set of Dendrites does not improve the system try it 3 times before giving up
+# If a set of Dendrites does not improve the system try it 2 times before giving up
 PBG.maxDendriteTries = 2
 # Make sure correlation scores improve from epoch to epoch by at least 25% and a raw value of 1e-4 to conclude that the correlation scores have gone up.
 PBG.pbImprovementThreshold = 0.25
@@ -108,8 +103,7 @@ parser.add_argument('--fan_out', type=int, default=10,
                     help='number of neighbors in each layer')
 parser.add_argument('--hidden_channels', type=int, default=256)
 # This can be set to 0 to run this code without Perforated Backpropagation happening.
-parser.add_argument('--doingPB', type=int, default=1,                    
-                    help='doing PB')
+parser.add_argument('--doingPB', type=int, default=1, help='doing PB')
 parser.add_argument('--lr', type=float, default=0.003)
 parser.add_argument('--wd', type=float, default=0.0)
 parser.add_argument('--dropout', type=float, default=0.5)
@@ -255,24 +249,32 @@ model = PBU.convertNetwork(model)
 # This initializes the Perforated Backpropagation Tracker object which organizes
 # communication between each individual Dendrite convereted module within a full network
 PBG.pbTracker.initialize(
-    doingPB = args.doingPB, #This can be set to false if you want to do just normal training 
-    saveName=args.saveName,  # Change the save name for different parameter runs
-maximizingScore=True, # True for maximizing validation score, false for minimizing validation loss
-makingGraphs=True)  # True if you want graphs to be saved
-
+    doingPB=args.
+    doingPB,  #This can be set to false if you want to do just normal training 
+    saveName=args.
+    saveName,  # Change the save name for different parameter runs
+    maximizingScore=
+    True,  # True for maximizing validation score, false for minimizing validation loss
+    makingGraphs=True)  # True if you want graphs to be saved
+'''
+# This can be added to pick up where it left off if something crashes.
 print('pre loading')
 model = PBU.loadSystem(model, args.saveName, 'latest')
 print('loaded')
-
+'''
 
 # This change is required since the PBTracker object handles the scheduler
 # All values are the same, just bassed in as kwargs
 PBG.pbTracker.setOptimizer(torch.optim.Adam)
 PBG.pbTracker.setScheduler(torch.optim.lr_scheduler.ReduceLROnPlateau)
-schedArgs = {'mode':'max', 'patience': 5}
-optimArgs = {'params':model.parameters(),'lr':args.lr, 'weight_decay':args.wd}
-optimizer, scheduler = PBG.pbTracker.setupOptimizer(model, optimArgs, schedArgs)
-
+schedArgs = {'mode': 'max', 'patience': 5}
+optimArgs = {
+    'params': model.parameters(),
+    'lr': args.lr,
+    'weight_decay': args.wd
+}
+optimizer, scheduler = PBG.pbTracker.setupOptimizer(model, optimArgs,
+                                                    schedArgs)
 
 print(f'Total time before training begins took '
       f'{time.perf_counter() - wall_clock_start:.4f}s')
@@ -282,7 +284,6 @@ times = []
 train_times = []
 inference_times = []
 best_val = 0.
-
 '''
 # These lines can be used to run full test with a final model file
 num_epochs = 0
@@ -314,7 +315,6 @@ for epoch in range(1, num_epochs + 1):
     times.append(time.perf_counter() - train_start)
     # Add the extra scores to be graphed
     PBG.pbTracker.addExtraScore(train_acc, 'Train Accuracy')
-    
     '''
     Add the validation score
     This is the function that determines when to add new Dendrites for Dendrite training
@@ -325,19 +325,26 @@ for epoch in range(1, num_epochs + 1):
     restructured - If a restructing did happen
     trainingComplete - if the tracker has determined that this is the final model to use
     '''
-    model, improved, restructured, trainingComplete = PBG.pbTracker.addValidationScore(val_acc, 
-    model,
-    args.saveName)
+    model, improved, restructured, trainingComplete = PBG.pbTracker.addValidationScore(
+        val_acc, model, args.saveName)
     # Need to setup GPU settings of the new model
     model = model.to(device)
     # When training is complete break the loop of epochs
-    if(trainingComplete):
+    if (trainingComplete):
         break
     # If the network was restructured reinitialize the optimizer and scheduler for the new paramters
-    elif(restructured):
-        schedArgs = {'mode':'max', 'patience': 5} #Make sure this is lower than epochs to switch
-        optimArgs = {'params':model.parameters(),'lr':args.lr, 'weight_decay':args.wd}
-        optimizer, scheduler = PBG.pbTracker.setupOptimizer(model, optimArgs, schedArgs)
+    elif (restructured):
+        schedArgs = {
+            'mode': 'max',
+            'patience': 5
+        }  #Make sure this is lower than epochs to switch
+        optimArgs = {
+            'params': model.parameters(),
+            'lr': args.lr,
+            'weight_decay': args.wd
+        }
+        optimizer, scheduler = PBG.pbTracker.setupOptimizer(
+            model, optimArgs, schedArgs)
 
 print(f'Average Epoch Time on training: '
       f'{torch.tensor(train_times).mean():.4f}s')
