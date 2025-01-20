f024e81cc544fa16e67b9874bd0afca628d3dbf6
Thomas Bauer <thomas.bauer@insa-rouen.fr>
2025-01-20 16:09:24 +0100

    Premiere doc (copie TUDataset)

--- torch_geometric/datasets/greyc.py
+++ torch_geometric/datasets/greyc.py
@@ -6,12 +6,70 @@ import torch
 from torch_geometric.data import InMemoryDataset, download_url, extract_zip
 
 
-class DatasetNotFoundError(Exception):
-    pass
-
-
 class GreycDataset(InMemoryDataset):
-    """Class to load three GREYC Datasets as pytorch geometric dataset."""
+    r"""A variety of graph kernel benchmark datasets, *.e.g.*,
+    :obj:`"IMDB-BINARY"`, :obj:`"REDDIT-BINARY"` or :obj:`"PROTEINS"`,
+    collected from the `TU Dortmund University
+    <https://chrsmrrs.github.io/datasets>`_.
+    In addition, this dataset wrapper provides `cleaned dataset versions
+    <https://github.com/nd7141/graph_datasets>`_ as motivated by the
+    `"Understanding Isomorphism Bias in Graph Data Sets"
+    <https://arxiv.org/abs/1910.12091>`_ paper, containing only non-isomorphic
+    graphs.
+
+    .. note::
+        Some datasets may not come with any node labels.
+        You can then either make use of the argument :obj:`use_node_attr`
+        to load additional continuous node attributes (if present) or provide
+        synthetic node features using transforms such as
+        :class:`torch_geometric.transforms.Constant` or
+        :class:`torch_geometric.transforms.OneHotDegree`.
+
+    Args:
+        root (str): Root directory where the dataset should be saved.
+        name (str): The `name
+            <https://chrsmrrs.github.io/datasets/docs/datasets/>`_ of the
+            dataset.
+        transform (callable, optional): A function/transform that takes in an
+            :obj:`torch_geometric.data.Data` object and returns a transformed
+            version. The data object will be transformed before every access.
+            (default: :obj:`None`)
+        pre_transform (callable, optional): A function/transform that takes in
+            an :obj:`torch_geometric.data.Data` object and returns a
+            transformed version. The data object will be transformed before
+            being saved to disk. (default: :obj:`None`)
+        pre_filter (callable, optional): A function that takes in an
+            :obj:`torch_geometric.data.Data` object and returns a boolean
+            value, indicating whether the data object should be included in the
+            final dataset. (default: :obj:`None`)
+        force_reload (bool, optional): Whether to re-process the dataset.
+            (default: :obj:`False`)
+
+    **STATS:**
+
+    .. list-table::
+        :widths: 20 10 10 10 10 10
+        :header-rows: 1
+
+        * - Name
+          - #graphs
+          - #nodes
+          - #edges
+          - #features
+          - #classes
+        * - MUTAG
+          - 188
+          - ~17.9
+          - ~39.6
+          - 7
+          - 2
+        * - ENZYMES
+          - 600
+          - ~32.6
+          - ~124.3
+          - 3
+          - 6
+    """
 
     URL = ('http://localhost:3000/')
 
@@ -25,6 +83,8 @@ class GreycDataset(InMemoryDataset):
         force_reload: bool = False,
     ) -> None:
         self.name = name.lower()
+        if self.name not in {"acyclic", "alkane", "mao"}:
+            raise ValueError(f"Dataset {self.name} not found.")
         super().__init__(root, transform, pre_transform, pre_filter,
                          force_reload=force_reload)
         self.data, self.slices = torch.load(self.processed_paths[0])
@@ -38,14 +98,12 @@ class GreycDataset(InMemoryDataset):
         return f"{self.name.lower()}.pth"
 
     def download(self) -> None:
-        """Load the right data according to initializer."""
         path = download_url(GreycDataset.URL + self.name + ".zip",
                             self.raw_dir)
         extract_zip(path, self.raw_dir)
         os.unlink(path)
 
     def process(self):
-        """Read data into huge `Data` list."""
         data_list = torch.load(os.path.join(self.raw_dir, self.name + ".pth"))
 
         if self.pre_filter is not None:
