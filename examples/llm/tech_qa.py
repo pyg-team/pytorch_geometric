0d721e186eede6378e0e5481ac576c4677daaac1
riship <riship@nvidia.com>
2025-01-24 09:56:50 -0800

    drafting

--- examples/llm/tech_qa.py
+++ examples/llm/tech_qa.py
@@ -229,7 +229,6 @@ def test(model, test_loader, args):
         return llm_judge.score(question, pred, correct_answer)
 
     scores = []
-<<<<<<< HEAD
     eval_tuples = []
     for test_batch in tqdm(test_loader, desc="Testing"):
         pred = (inference_step(model, test_batch))
@@ -237,19 +236,6 @@ def test(model, test_loader, args):
             eval_tuples.append((question, pred, label))
     for question, pred, label in tqdm(eval_tuples, desc="Eval"):
         scores.append(eval(question, pred, label))
-=======
-    for test_batch in test_loader:
-        preds = inference_step(model, test_batch)
-        """
-        TQDM here since only 1 mini-batch for testing
-        since theres only 11 test questions and eval-batch-size
-        is 16 by default
-        """
-        for question, pred, label in tqdm(
-                list(zip(test_batch.question, preds, test_batch.label)),
-                desc="Test:"):
-            scores.append(eval(question, pred, label))
->>>>>>> 2689f15431c6da5a6c7535884f10c50104bef1a0
     avg_scores = sum(scores) / len(scores)
     print("Avg marlin accuracy=", avg_scores)
 
