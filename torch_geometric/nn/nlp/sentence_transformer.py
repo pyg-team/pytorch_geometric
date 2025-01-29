26c363df137519f5492e29c2ee32e2a31d29ac9b
riship <riship@nvidia.com>
2025-01-29 11:19:56 -0800

    drafting a NIM sentence transformer so can use nvidia embed

--- torch_geometric/nn/nlp/sentence_transformer.py
+++ torch_geometric/nn/nlp/sentence_transformer.py
@@ -61,15 +61,12 @@ class NIMSentenceTransformer():
         embs: List[Tensor] = []
         for start in range(0, len(text), batch_size):
             batch = text[start:start + batch_size]
-            print("batch[:3]=", batch[:3])
             response = self.client.embeddings.create(
                 input=batch, model=self.embedding_model_name,
                 encoding_format="float", extra_body={
                     "input_type": "passage",
                     "truncate": "END"
                 })
-            print("type(response)=", type(response))
-            print("reposnse[:10]", response[:10])
             embs.append(
                 torch.tensor([[d.embedding for d in response.data]
                               ]).to(torchfloat32).to(output_device))
