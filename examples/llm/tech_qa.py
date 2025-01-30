d5325c3eb782b74d28468ea99b98da9d9501180d
riship <riship@nvidia.com>
2025-01-30 11:05:44 -0800

    improving

--- examples/llm/tech_qa.py
+++ examples/llm/tech_qa.py
@@ -170,6 +170,8 @@ def make_dataset(args):
             total_data_list.append(subgraph)
             extracted_triple_sizes.append(len(subgraph.triples))
         random.shuffle(total_data_list)
+        print("Min # of Extracted Triples =", min(extracted_triple_sizes))
+        print("Max # of Extracted Triples =", max(extracted_triple_sizes))
         print("Average # of Extracted Triples =", sum(extracted_triple_sizes) / len(extracted_triple_sizes))
 
         # 60:20:20 split
