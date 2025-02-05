7b2ffaf43ef447730e57eabf2638c7449bb94c22
riship <riship@nvidia.com>
2025-02-05 13:18:04 -0800

    vanilla rag w frozen  70b LM. from 32 out tokens->256

--- examples/llm/g_retriever.py
+++ examples/llm/g_retriever.py
@@ -172,7 +172,7 @@ def inference_step(model, batch, model_save_name="llm"):
     # Check the type of model being used to determine the input arguments
     if model_save_name == 'llm':
         # Perform inference on the question and textual graph description
-        return model.inference(batch.question, max_out_tokens=256)# batch.desc)
+        return model.inference(batch.question, max_tokens=256)# batch.desc)
     else:  # (GNN+LLM)
         return model.inference(
             batch.question,
@@ -181,7 +181,7 @@ def inference_step(model, batch, model_save_name="llm"):
             batch.batch,  # batch indices
             batch.edge_attr,  # edge attributes
             batch.desc,  # description
-            max_out_tokens=256,
+            max_out_tokens=512,
         )
 
 
