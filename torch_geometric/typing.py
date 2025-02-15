0f911452e28a89b5ae529a92752809db45925ad7
rusty1s <matthias.fey@tu-dortmund.de>
2025-02-15 20:05:46 +0000

    update

--- torch_geometric/typing.py
+++ torch_geometric/typing.py
@@ -69,10 +69,13 @@ try:
         pyg_lib.sampler.neighbor_sample).parameters)
     WITH_WEIGHTED_NEIGHBOR_SAMPLE = ('edge_weight' in inspect.signature(
         pyg_lib.sampler.neighbor_sample).parameters)
-    # WITH_CPU_HASH_MAP = hasattr(torch.classes.pyg, 'CPUHashMap')
-    # WITH_CUDA_HASH_MAP = hasattr(torch.classes.pyg, 'CUDAHashMap')
-    WITH_CPU_HASH_MAP = False
-    WITH_CUDA_HASH_MAP = False
+    print(torch.classes.pyg.CPUHashMap)
+    WITH_CPU_HASH_MAP = hasattr(torch.classes.pyg, 'CPUHashMap')
+    print(WITH_CPU_HASH_MAP)
+    WITH_CUDA_HASH_MAP = hasattr(torch.classes.pyg, 'CUDAHashMap')
+    print(WITH_CUDA_HASH_MAP)
+    # WITH_CPU_HASH_MAP = False
+    # WITH_CUDA_HASH_MAP = False
 except Exception as e:
     if not isinstance(e, ImportError):  # pragma: no cover
         warnings.warn(f"An issue occurred while importing 'pyg-lib'. "
