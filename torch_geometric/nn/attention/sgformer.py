cea9b3ff338fd7c6422461de935a3e00a51c6a56
Rishi Puri <puririshi98@berkeley.edu>
2025-02-18 14:19:52 -0800

    Fix SGFormer to not attend across batch

--- torch_geometric/nn/attention/sgformer.py
+++ torch_geometric/nn/attention/sgformer.py
@@ -1,6 +1,6 @@
 import torch
 from torch import Tensor
-
+from torch_geometric.utils import to_dense_batch
 
 class SGFormerAttention(torch.nn.Module):
     r"""The simple global attention mechanism from the
@@ -38,34 +38,45 @@ class SGFormerAttention(torch.nn.Module):
         self.k = torch.nn.Linear(channels, inner_channels, bias=qkv_bias)
         self.v = torch.nn.Linear(channels, inner_channels, bias=qkv_bias)
 
-    def forward(self, x: Tensor) -> Tensor:
+    def forward(self, x: Tensor, batch: Tensor) -> Tensor:
+        """
+        x (torch.Tensor): The input node features.
+        batch (torch.Tensor, optional): The batch vector
+            :math:`\mathbf{b} \in {\{ 0, \ldots, B-1\}}^N`, which assigns
+            each element to a specific example.
+        """
         # feature transformation
-        qs = self.q(x).reshape(-1, self.heads, self.head_channels)
-        ks = self.k(x).reshape(-1, self.heads, self.head_channels)
-        vs = self.v(x).reshape(-1, self.heads, self.head_channels)
+        batch_size = len(torch.unique(batch))
+        x, mask = to_dense_batch(x, batch) # [num_nodes, num_feats] -> [num_batches, nodes_in_batch, num_feats]
+        qs = self.q(x).reshape(batch_size, -1, self.heads, self.head_channels)
+        ks = self.k(x).reshape(batch_size, -1, self.heads, self.head_channels)
+        vs = self.v(x).reshape(batch_size, -1, self.heads, self.head_channels)
 
         # normalize input
-        qs = qs / torch.norm(qs, p=2)  # [N, H, M]
-        ks = ks / torch.norm(ks, p=2)  # [L, H, M]
+        qs = qs / torch.norm(qs, p=2)
+        ks = ks / torch.norm(ks, p=2)
         N = qs.shape[0]
 
         # numerator
         kvs = torch.einsum("lhm,lhd->hmd", ks, vs)
-        attention_num = torch.einsum("nhm,hmd->nhd", qs, kvs)  # [N, H, D]
+        attention_num = torch.einsum("nhm,hmd->nhd", qs, kvs)
         attention_num += N * vs
 
         # denominator
         all_ones = torch.ones([ks.shape[0]]).to(ks.device)
         ks_sum = torch.einsum("lhm,l->hm", ks, all_ones)
-        attention_normalizer = torch.einsum("nhm,hm->nh", qs, ks_sum)  # [N, H]
-
+        attention_normalizer = torch.einsum("nhm,hm->nh", qs, ks_sum)
         # attentive aggregated results
         attention_normalizer = torch.unsqueeze(
-            attention_normalizer, len(attention_normalizer.shape))  # [N, H, 1]
+            attention_normalizer, len(attention_normalizer.shape))
         attention_normalizer += torch.ones_like(attention_normalizer) * N
-        attn_output = attention_num / attention_normalizer  # [N, H, D]
+        attn_output = attention_num / attention_normalizer
+        list_to_cat = []
+        for i, submask in enumerate(mask):
+            list_to_cat.append(attn_output[i][submask].mean(dim=1))
+        torch.cat(list_to_cat)
 
-        return attn_output.mean(dim=1)
+        return attn_output
 
     def reset_parameters(self):
         self.q.reset_parameters()
