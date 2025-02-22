a8ceb982e0d61fc43df094769c31b59087b2d7bf
rusty1s <matthias.fey@tu-dortmund.de>
2025-02-22 06:41:18 +0000

    update

--- torch_geometric/hash_tensor.py
+++ torch_geometric/hash_tensor.py
@@ -427,7 +427,7 @@ def _slice(
 
     if dim == 0 or dim == -tensor.dim():
         copy = start is None or (start == 0 or start <= -tensor.size(0))
-        copy &= end is None or (end > tensor.size(0))
+        copy &= end is None or end > tensor.size(0)
         copy &= step == 1
         if copy:
             return tensor._shallow_copy()
