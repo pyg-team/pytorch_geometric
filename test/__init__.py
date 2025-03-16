9893990e3e86c3cd462a5aefd7cd33ca119b1623
xnuohz <xnuohz@126.com>
2025-03-16 14:58:44 +0800

    add coredumpy

---
+++ test/__init__.py
@@ -0,0 +1,11 @@
+# Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
+
+import os
+
+if os.getenv("GITHUB_ACTIONS") or os.getenv("ENABLE_COREDUMPY"):
+    try:
+        import coredumpy
+        coredumpy.patch_unittest(
+            directory=os.getenv("COREDUMPY_DUMP_DIR", "./"))
+    except ImportError:
+        pass
