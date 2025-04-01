21269a7a98a3109f468c1cdbb240ed6ae6cb3d99
Zack Aristei <zaristei@zaristei-nvidia.client.nvidia.com>
2025-04-01 14:41:27 -0700

    precommit

--- torch_geometric/datasets/web_qsp_dataset.py
+++ torch_geometric/datasets/web_qsp_dataset.py
@@ -425,8 +425,9 @@ class WebQSPDataset(KGQABaseDataset):
     ) -> None:
         # Modify these paramters if running into memory/compute issues
         default_retrieval_kwargs = {
-            'max_batch_size': 250, # Lower batch size to reduce memory usage
-            'num_workers': None, # Use all available workers, or set to number of threads
+            'max_batch_size': 250,  # Lower batch size to reduce memory usage
+            'num_workers':
+            None,  # Use all available workers, or set to number of threads
         }
         retrieval_kwargs = {**default_retrieval_kwargs, **retrieval_kwargs}
         dataset_name = 'rmanluo/RoG-webqsp'
