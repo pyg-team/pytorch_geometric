dce2fe6648f611314cf93399ea1e6e2e29a2e892
Zack Aristei <zaristei@zaristei-nvidia.client.nvidia.com>
2025-04-15 18:45:33 -0700

    precommit

--- test/sampler/test_sampler_base.py
+++ test/sampler/test_sampler_base.py
@@ -6,7 +6,7 @@ from torch_geometric.sampler.base import (
     NumNeighbors,
     SamplerOutput,
 )
-from torch_geometric.sampler.utils import local_to_global_node_idx, global_to_local_node_idx
+from torch_geometric.sampler.utils import global_to_local_node_idx
 from torch_geometric.testing import get_random_edge_index
 from torch_geometric.utils import is_undirected
 
@@ -121,6 +121,7 @@ def test_heterogeneous_to_bidirectional():
     assert is_undirected(
         torch.stack([obj.row['v1', 'to', 'v1'], obj.col['v1', 'to', 'v1']], 0))
 
+
 def test_homogeneous_sampler_output_global_fields():
     output = SamplerOutput(
         node=torch.tensor([0, 2, 3]),
@@ -152,7 +153,8 @@ def test_homogeneous_sampler_output_global_fields():
     global_values.append(seed_node)
 
     output_bidirectional = output.to_bidirectional(keep_orig_edges=True)
-    global_bidir_row, global_bidir_col = output_bidirectional.global_row, output_bidirectional.global_col
+    global_bidir_row, global_bidir_col = \
+        output_bidirectional.global_row, output_bidirectional.global_col
     assert torch.equal(global_bidir_row, torch.tensor([2, 0, 3, 2]))
     assert torch.equal(global_bidir_col, torch.tensor([0, 2, 2, 3]))
     local_values.append(output_bidirectional.row)
@@ -162,10 +164,12 @@ def test_homogeneous_sampler_output_global_fields():
 
     assert torch.equal(output.global_row, output_bidirectional.global_orig_row)
     assert torch.equal(output.global_col, output_bidirectional.global_orig_col)
-    
+
     # Make sure reverse mapping is correct
     for local_value, global_value in zip(local_values, global_values):
-        assert torch.equal(global_to_local_node_idx(output.node, global_value), local_value)
+        assert torch.equal(global_to_local_node_idx(output.node, global_value),
+                           local_value)
+
 
 def test_heterogeneous_sampler_output_global_fields():
     def _tensor_dict_equal(dict1, dict2):
@@ -177,46 +181,89 @@ def test_heterogeneous_sampler_output_global_fields():
 
     output = HeteroSamplerOutput(
         node={"person": torch.tensor([0, 2, 3])},
-        row={("person", "works_with", "person"): torch.tensor([1]), ("person", "leads", "person"): torch.tensor([0])},
-        col={("person", "works_with", "person"): torch.tensor([2]), ("person", "leads", "person"): torch.tensor([1])},
-        edge={("person", "works_with", "person"): torch.tensor([1]), ("person", "leads", "person"): torch.tensor([0])},
+        row={
+            ("person", "works_with", "person"): torch.tensor([1]),
+            ("person", "leads", "person"): torch.tensor([0])
+        },
+        col={
+            ("person", "works_with", "person"): torch.tensor([2]),
+            ("person", "leads", "person"): torch.tensor([1])
+        },
+        edge={
+            ("person", "works_with", "person"): torch.tensor([1]),
+            ("person", "leads", "person"): torch.tensor([0])
+        },
         batch={"person": torch.tensor([0, 0, 0])},
         num_sampled_nodes={"person": torch.tensor([1, 1, 1])},
-        num_sampled_edges={("person", "works_with", "person"): torch.tensor([1]), ("person", "leads", "person"): torch.tensor([1])},
+        num_sampled_edges={
+            ("person", "works_with", "person"): torch.tensor([1]),
+            ("person", "leads", "person"): torch.tensor([1])
+        },
         orig_row=None,
         orig_col=None,
         metadata=(None, None),
     )
 
-    local_values = []
-    global_values = []
-
     global_row, global_col = output.global_row, output.global_col
-    assert _tensor_dict_equal(global_row, {("person", "works_with", "person"): torch.tensor([2]), ("person", "leads", "person"): torch.tensor([0])})
-    assert _tensor_dict_equal(global_col, {("person", "works_with", "person"): torch.tensor([3]), ("person", "leads", "person"): torch.tensor([2])})
+    assert _tensor_dict_equal(
+        global_row, {
+            ("person", "works_with", "person"): torch.tensor([2]),
+            ("person", "leads", "person"): torch.tensor([0])
+        })
+    assert _tensor_dict_equal(
+        global_col, {
+            ("person", "works_with", "person"): torch.tensor([3]),
+            ("person", "leads", "person"): torch.tensor([2])
+        })
 
-    local_row_dict = {k: global_to_local_node_idx(output.node[k[0]], v) for k, v in global_row.items()}
+    local_row_dict = {
+        k: global_to_local_node_idx(output.node[k[0]], v)
+        for k, v in global_row.items()
+    }
     assert _tensor_dict_equal(local_row_dict, output.row)
 
-    local_col_dict = {k: global_to_local_node_idx(output.node[k[2]], v) for k, v in global_col.items()}
+    local_col_dict = {
+        k: global_to_local_node_idx(output.node[k[2]], v)
+        for k, v in global_col.items()
+    }
     assert _tensor_dict_equal(local_col_dict, output.col)
 
     seed_node = output.seed_node
     assert _tensor_dict_equal(seed_node, {"person": torch.tensor([0, 0, 0])})
 
-    local_batch_dict = {k: global_to_local_node_idx(output.node[k], v) for k, v in seed_node.items()}
+    local_batch_dict = {
+        k: global_to_local_node_idx(output.node[k], v)
+        for k, v in seed_node.items()
+    }
     assert _tensor_dict_equal(local_batch_dict, output.batch)
 
     output_bidirectional = output.to_bidirectional(keep_orig_edges=True)
-    global_bidir_row, global_bidir_col = output_bidirectional.global_row, output_bidirectional.global_col
-    assert _tensor_dict_equal(global_bidir_row, {("person", "works_with", "person"): torch.tensor([3, 2]), ("person", "leads", "person"): torch.tensor([2, 0])})
-    assert _tensor_dict_equal(global_bidir_col, {("person", "works_with", "person"): torch.tensor([2, 3]), ("person", "leads", "person"): torch.tensor([0, 2])})
+    global_bidir_row, global_bidir_col = \
+        output_bidirectional.global_row, output_bidirectional.global_col
+    assert _tensor_dict_equal(
+        global_bidir_row, {
+            ("person", "works_with", "person"): torch.tensor([3, 2]),
+            ("person", "leads", "person"): torch.tensor([2, 0])
+        })
+    assert _tensor_dict_equal(
+        global_bidir_col, {
+            ("person", "works_with", "person"): torch.tensor([2, 3]),
+            ("person", "leads", "person"): torch.tensor([0, 2])
+        })
 
-    local_bidir_row_dict = {k: global_to_local_node_idx(output_bidirectional.node[k[0]], v) for k, v in global_bidir_row.items()}
+    local_bidir_row_dict = {
+        k: global_to_local_node_idx(output_bidirectional.node[k[0]], v)
+        for k, v in global_bidir_row.items()
+    }
     assert _tensor_dict_equal(local_bidir_row_dict, output_bidirectional.row)
 
-    local_bidir_col_dict = {k: global_to_local_node_idx(output_bidirectional.node[k[2]], v) for k, v in global_bidir_col.items()}
+    local_bidir_col_dict = {
+        k: global_to_local_node_idx(output_bidirectional.node[k[2]], v)
+        for k, v in global_bidir_col.items()
+    }
     assert _tensor_dict_equal(local_bidir_col_dict, output_bidirectional.col)
 
-    assert _tensor_dict_equal(output.global_row, output_bidirectional.global_orig_row)
-    assert _tensor_dict_equal(output.global_col, output_bidirectional.global_orig_col)
+    assert _tensor_dict_equal(output.global_row,
+                              output_bidirectional.global_orig_row)
+    assert _tensor_dict_equal(output.global_col,
+                              output_bidirectional.global_orig_col)
