52a7a9f271db936d8ff630a0a269f8b132c0b422
Leon <huidongl@omi-rapid-17.omirapid.oxford-man.ox.ac.uk>
2025-05-11 07:29:14 +0100

    add smoke test

--- test/utils/test_total_influence.py
+++ test/utils/test_total_influence.py
@@ -20,17 +20,28 @@ class GNN(torch.nn.Module):
 def test_total_influence_smoke():
     x = torch.randn(6, 5)
     edge_index = torch.tensor([[0, 1, 2, 3, 4], [1, 2, 3, 4, 5]])
-
+    max_hops = 2
+    num_samples = 4
     data = Data(
         x=x,
         edge_index=edge_index,
     )
     model = GNN()
-    I, R = total_influence(model, data, max_hops=2, num_samples=4)
+    I, R = total_influence(
+        model, 
+        data, 
+        max_hops=max_hops, 
+        num_samples=num_samples,
+    )
 
-    assert I.shape == torch.Size([3])
-    assert 0.0 <= R <= 1.0
+    assert I.shape == torch.Size([max_hops+1])
+    assert 0.0 <= R <= max_hops
 
-    I, R = total_influence(model, data, max_hops=1, num_samples=4,
-                           average=False)
-    assert I.shape == torch.Size([4, 3])
+    I, R = total_influence(
+        model, 
+        data, 
+        max_hops=max_hops, 
+        num_samples=num_samples,
+        average=False,
+    )
+    assert I.shape == torch.Size([num_samples, max_hops+1])
