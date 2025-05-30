704ce5c9746a73b6a7a7a8bfc9aa7c1362064d38
xnuohz <xnuohz@126.com>
2025-05-30 05:44:06 +0000

    update

--- test/nn/models/test_polynormer.py
+++ test/nn/models/test_polynormer.py
@@ -4,10 +4,11 @@ import torch
 from torch_geometric.nn.models import Polynormer
 
 
+@pytest.mark.parametrize('local_attn', [True, False])
 @pytest.mark.parametrize('qk_shared', [True, False])
 @pytest.mark.parametrize('pre_ln', [True, False])
 @pytest.mark.parametrize('post_bn', [True, False])
-def test_polynormer(qk_shared, pre_ln, post_bn):
+def test_polynormer(local_attn, qk_shared, pre_ln, post_bn):
     x = torch.randn(10, 16)
     edge_index = torch.tensor([
         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
@@ -22,6 +23,7 @@ def test_polynormer(qk_shared, pre_ln, post_bn):
         qk_shared=qk_shared,
         pre_ln=pre_ln,
         post_bn=post_bn,
+        local_attn=local_attn,
     )
     out = model(x, edge_index, batch)
     assert out.size() == (10, 40)
