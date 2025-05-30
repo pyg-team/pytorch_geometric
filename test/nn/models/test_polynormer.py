29940cc74f5325748e2b3db2fc342c76545cfaa9
xnuohz <xnuohz@126.com>
2025-05-30 04:56:35 +0000

    update

--- test/nn/models/test_polynormer.py
+++ test/nn/models/test_polynormer.py
@@ -4,9 +4,10 @@ import torch
 from torch_geometric.nn.models import Polynormer
 
 
+@pytest.mark.parametrize('qkv_bias', [True, False])
 @pytest.mark.parametrize('pre_ln', [True, False])
 @pytest.mark.parametrize('post_bn', [True, False])
-def test_polynormer(pre_ln, post_bn):
+def test_polynormer(qkv_bias, pre_ln, post_bn):
     x = torch.randn(10, 16)
     edge_index = torch.tensor([
         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
@@ -18,6 +19,7 @@ def test_polynormer(pre_ln, post_bn):
         in_channels=16,
         hidden_channels=128,
         out_channels=40,
+        qkv_bias=qkv_bias,
         pre_ln=pre_ln,
         post_bn=post_bn,
     )
