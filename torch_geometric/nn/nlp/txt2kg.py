d8a16d716b4aaccd6e9f647f4bf38f09ab5a5ad3
Rishi Puri <riship@nvidia.com>
2025-07-01 12:19:03 -0700

    Update txt2kg.py

--- torch_geometric/nn/nlp/txt2kg.py
+++ torch_geometric/nn/nlp/txt2kg.py
@@ -152,10 +152,8 @@ class TXT2KG():
                 try:
                     pass
                 except ImportError:
-                    print(
-                        "Failed to import `openai` package, please install it and rerun the script"
-                    )
-                    sys.exit(1)
+                    quit("Failed to import `openai` package, please install it and rerun the script") # noqa
+                    
                 # Process chunks in parallel using multiple processes
                 num_procs = min(len(chunks), _get_num_procs())
                 meta_chunk_size = int(len(chunks) / num_procs)
