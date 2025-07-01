5e7eaf7ca5975039b387b483901a2e236d8ebb14
Federico Lopez <fedelopez77@gmail.com>
2025-07-01 10:27:16 +0000

    Adding device arg to norm layers

--- test/nn/norm/test_msg_norm.py
+++ test/nn/norm/test_msg_norm.py
@@ -1,14 +1,15 @@
 import torch
 
 from torch_geometric.nn import MessageNorm
-from torch_geometric.testing import is_full_test
+from torch_geometric.testing import is_full_test, withDevice
 
 
-def test_message_norm():
-    norm = MessageNorm(learn_scale=True)
+@withDevice
+def test_message_norm(device):
+    norm = MessageNorm(learn_scale=True, device=device)
     assert str(norm) == 'MessageNorm(learn_scale=True)'
-    x = torch.randn(100, 16)
-    msg = torch.randn(100, 16)
+    x = torch.randn(100, 16, device=device)
+    msg = torch.randn(100, 16, device=device)
     out = norm(x, msg)
     assert out.size() == (100, 16)
 
@@ -16,7 +17,7 @@ def test_message_norm():
         jit = torch.jit.script(norm)
         assert torch.allclose(jit(x, msg), out)
 
-    norm = MessageNorm(learn_scale=False)
+    norm = MessageNorm(learn_scale=False, device=device)
     assert str(norm) == 'MessageNorm(learn_scale=False)'
     out = norm(x, msg)
     assert out.size() == (100, 16)
