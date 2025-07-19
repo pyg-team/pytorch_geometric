ca3eeb90fca8c1b15dc2a37f440645a13ab8e690
xnuohz <xnuohz@126.com>
2025-07-19 20:42:12 +0800

    update

--- test/test_inspector.py
+++ test/test_inspector.py
@@ -27,7 +27,7 @@ def test_type_repr() -> None:
         'Tuple[Tensor, Optional[Tensor]]')
     assert inspector.type_repr(
         Final[Optional[Tensor]]) == ('typing.Final[Optional[typing.Any]]')
-    assert inspector.type_repr(Union[None, Tensor]) == 'Optional[typing.Any]'
+    assert inspector.type_repr(Union[None, Tensor]) == 'Optional[Tensor]'
     assert inspector.type_repr(Optional[Tensor]) == 'Optional[Tensor]'
     assert inspector.type_repr(Set[Tensor]) == 'typing.Set[Tensor]'
     assert inspector.type_repr(List) == 'List'
