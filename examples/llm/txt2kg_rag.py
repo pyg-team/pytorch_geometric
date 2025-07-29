7f4c873daeb9a9101817b43c23c9d4871a3f5d1b
Ralph Liu <ralphl@nvidia.com>
2025-07-28 19:51:24 -0700

    Support max tokens and tuple storing

--- examples/llm/txt2kg_rag.py
+++ examples/llm/txt2kg_rag.py
@@ -2,6 +2,7 @@ import argparse
 import gc
 import json
 import os
+import pickle
 import random
 import re
 import sys
@@ -151,6 +152,13 @@ def parse_args():
         '--use_x_percent_corpus', default=100.0, type=float,
         help="Debug flag that allows user to only use a random percentage "
         "of available knowledge base corpus for RAG")
+    parser.add_argument(
+        '--store_eval_tuples', action="store_true",
+        help="Store tuple answers from test step to .pkl file for evaluation")
+    parser.add_argument(
+        '--use_stored_eval_tuples', action="store_true",
+        help="Retrieve previously saved tuples for eval instead of generating"
+        "new ones")
     args = parser.parse_args()
 
     assert args.NV_NIM_KEY, "NVIDIA API key is required for TXT2KG and eval"
@@ -227,6 +235,24 @@ def _process_and_chunk_text(text, chunk_size, doc_parsing_mode):
             chunks = _chunk_text(paragraph)
         full_chunks.extend(chunks)
     return full_chunks
+
+
+def _store_tuples(eval_tuples):
+    with open('tuples.pkl', 'wb') as f:
+        pickle.dump(eval_tuples, f)
+
+
+def _get_stored_tuples():
+    tuples = None
+    with open('tuples.pkl', 'rb') as f:
+        tuples = pickle.load(f)
+
+    if tuples is None:
+        raise FileNotFoundError(
+            "Error: Could not open stored tuples. Have you checked that "
+            "tuples.pkl exists?")
+
+    return tuples
 
 
 def get_data(args):
@@ -684,6 +710,7 @@ def train(args, train_loader, val_loader):
 
 
 def test(model, test_loader, args):
+    print(f"LLMJudge using {args.NV_NIM_MODEL}")
     llm_judge = LLMJudge(args.NV_NIM_MODEL, args.NV_NIM_KEY, args.ENDPOINT_URL)
 
     def eval(question: str, pred: str, correct_answer: str):
@@ -694,8 +721,8 @@ def test(model, test_loader, args):
     if not args.use_stored_eval_tuples:
         for ii, test_batch in enumerate(tqdm(test_loader, desc="Testing")):
             # early termination to evaluate scoring
-            if ii > 20:
-                break
+            # if ii > 20:
+                # break
             new_qs = []
             raw_qs = test_batch["question"]
             for i, q in enumerate(test_batch["question"]):
