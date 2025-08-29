5087a29e6430e3c24e6cc772d7e27763cbfaef52
Rishi Puri <riship@nvidia.com>
2025-08-29 13:49:11 -0700

    Update sage_conv.py

--- torch_geometric/nn/conv/cugraph/sage_conv.py
+++ torch_geometric/nn/conv/cugraph/sage_conv.py
@@ -27,8 +27,8 @@ class CuGraphSAGEConv(CuGraphModule):  # pragma: no cover
     :class:`~torch_geometric.nn.conv.SAGEConv` based on the :obj:`cugraph-ops`
     package that fuses message passing computation for accelerated execution
     and lower memory footprint.
-    The easiest way to enable :obj:`cugraph-ops` is to use
-    https://catalog.ngc.nvidia.com/orgs/nvidia/containers/pyg
+    The easiest way to enable :obj:`cugraph-ops` is to use `The NVIDIA PyG Container
+    <https://catalog.ngc.nvidia.com/orgs/nvidia/containers/pyg>`_.
     """
     def __init__(
         self,
