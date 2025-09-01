2db31fc0d9e4ca07fab0e8058ce4653805cca982
Jinu Sunil <jinu@kumo.ai>
2025-09-01 10:14:04 -0700

    Update torch_geometric/datasets/modelnet.py

--- torch_geometric/datasets/modelnet.py
+++ torch_geometric/datasets/modelnet.py
@@ -78,8 +78,8 @@ class ModelNet(InMemoryDataset):
     """
 
     urls = {
-        '10': 'http://3dvision.princeton.edu/'
-        'projects/2014/3DShapeNets/ModelNet10.zip',
+        '10':
+        'http://3dvision.princeton.edu/projects/2014/3DShapeNets/ModelNet10.zip',
         '40': 'http://modelnet.cs.princeton.edu/ModelNet40.zip'
     }
 
