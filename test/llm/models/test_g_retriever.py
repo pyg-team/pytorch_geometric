2d21aa8cb7a34b7ac2c994e1ee7b3691f7b987d8
Rishi Puri <riship@nvidia.com>
2025-10-02 14:14:26 -0700

    Update test_g_retriever.py

--- test/llm/models/test_g_retriever.py
+++ test/llm/models/test_g_retriever.py
@@ -10,7 +10,7 @@ from torch_geometric.testing import onlyRAG, withPackage
 @onlyRAG
 @withPackage('transformers', 'sentencepiece', 'accelerate')
 def test_g_retriever() -> None:
-    llm = LLM(model_name='Qwen/Qwen3-0.6B', num_params=1, dtype=torch.float16,
+    llm = LLM(model_name='Qwen/Qwen3-0.6B', dtype=torch.float16,
               sys_prompt="You're an agent, answer my questions.")
 
     gnn = GAT(
@@ -54,49 +54,49 @@ def test_g_retriever() -> None:
     torch.cuda.empty_cache()
 
 
-# @onlyRAG
-# @withPackage('transformers', 'sentencepiece', 'accelerate')
-# def test_g_retriever_many_tokens() -> None:
-#     llm = LLM(model_name='Qwen/Qwen3-0.6B', num_params=1, dtype=torch.float16,
-#               sys_prompt="You're an agent, answer my questions.")
+@onlyRAG
+@withPackage('transformers', 'sentencepiece', 'accelerate')
+def test_g_retriever_many_tokens() -> None:
+    llm = LLM(model_name='Qwen/Qwen3-0.6B', dtype=torch.float16,
+              sys_prompt="You're an agent, answer my questions.")
 
-#     gnn = GAT(
-#         in_channels=1024,
-#         out_channels=1024,
-#         hidden_channels=1024,
-#         num_layers=2,
-#         heads=4,
-#         norm='batch_norm',
-#     )
+    gnn = GAT(
+        in_channels=1024,
+        out_channels=1024,
+        hidden_channels=1024,
+        num_layers=2,
+        heads=4,
+        norm='batch_norm',
+    )
 
-#     model = GRetriever(
-#         llm=llm,
-#         gnn=gnn,
-#         mlp_out_tokens=2,
-#     )
-#     assert str(model) == ('GRetriever(\n'
-#                           '  llm=LLM(Qwen/Qwen3-0.6B),\n'
-#                           '  gnn=GAT(1024, 1024, num_layers=2),\n'
-#                           ')')
+    model = GRetriever(
+        llm=llm,
+        gnn=gnn,
+        mlp_out_tokens=2,
+    )
+    assert str(model) == ('GRetriever(\n'
+                          '  llm=LLM(Qwen/Qwen3-0.6B),\n'
+                          '  gnn=GAT(1024, 1024, num_layers=2),\n'
+                          ')')
 
-#     x = torch.randn(10, 1024)
-#     edge_index = torch.tensor([
-#         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-#         [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
-#     ])
-#     edge_attr = torch.randn(edge_index.size(1), 1024)
-#     batch = torch.zeros(x.size(0), dtype=torch.long)
+    x = torch.randn(10, 1024)
+    edge_index = torch.tensor([
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+    ])
+    edge_attr = torch.randn(edge_index.size(1), 1024)
+    batch = torch.zeros(x.size(0), dtype=torch.long)
 
-#     question = ["Is PyG the best open-source GNN library?"]
-#     label = ["yes!"]
+    question = ["Is PyG the best open-source GNN library?"]
+    label = ["yes!"]
 
-#     # Test train:
-#     loss = model(question, x, edge_index, batch, label, edge_attr)
-#     assert loss >= 0
+    # Test train:
+    loss = model(question, x, edge_index, batch, label, edge_attr)
+    assert loss >= 0
 
-#     # Test inference:
-#     pred = model.inference(question, x, edge_index, batch, edge_attr)
-#     assert len(pred) == 1
-#     del model, llm, gnn
-#     gc.collect()
-#     torch.cuda.empty_cache()
+    # Test inference:
+    pred = model.inference(question, x, edge_index, batch, edge_attr)
+    assert len(pred) == 1
+    del model, llm, gnn
+    gc.collect()
+    torch.cuda.empty_cache()
