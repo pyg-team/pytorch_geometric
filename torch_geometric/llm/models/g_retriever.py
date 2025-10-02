a0a1ebe635691e24ee17c602ab2a886c9a97f62e
Rishi Puri <riship@nvidia.com>
2025-10-02 09:48:29 -0700

    Put LLM CI back
    all tests pass but somehow gretriever and webqsp tests throw exit code 139 w/ no info. Will put the LLM CI back first, disabling the 2 problematic tests. 
    Then ill submit a follow up pr to fix them somehow.

--- torch_geometric/llm/models/g_retriever.py
+++ torch_geometric/llm/models/g_retriever.py
@@ -24,7 +24,7 @@ class GRetriever(torch.nn.Module):
 
     .. warning::
         This module has been tested with the following HuggingFace models
-
+        * :obj:`llm_to_use="meta-llama/Meta-Llama-3.1-8B-Instruct"
         * :obj:`llm_to_use="meta-llama/Llama-2-7b-chat-hf"`
         * :obj:`llm_to_use="google/gemma-7b"`
 
