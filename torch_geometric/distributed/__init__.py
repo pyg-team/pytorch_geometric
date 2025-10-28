f90e5639d8842c1c50ea46c13fe0746eee930343
Rishi Puri <riship@nvidia.com>
2025-10-28 14:47:39 -0700

    Update __init__.py

--- torch_geometric/distributed/__init__.py
+++ torch_geometric/distributed/__init__.py
@@ -9,17 +9,6 @@ from .dist_loader import DistLoader
 from .dist_neighbor_loader import DistNeighborLoader
 from .dist_link_neighbor_loader import DistLinkNeighborLoader
 
-warn(
-    "`torch_geometric.distributed` has been deprecated since 2.7.0 and will "
-    "no longer be maintained. For distributed training, refer to our "
-    "tutorials on distributed training at "
-    "https://pytorch-geometric.readthedocs.io/en/latest/tutorial/distributed.html "  # noqa: E501
-    "or cuGraph examples at "
-    "https://github.com/rapidsai/cugraph-gnn/tree/main/python/cugraph-pyg/cugraph_pyg/examples",  # noqa: E501
-    stacklevel=2,
-    category=DeprecationWarning,
-)
-
 __all__ = classes = [
     'DistContext',
     'LocalFeatureStore',
