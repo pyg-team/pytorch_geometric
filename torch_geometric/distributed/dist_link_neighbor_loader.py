c3214f908b05776d1644e96529cb7d860e5a4d55
Rishi Puri <riship@nvidia.com>
2025-10-28 14:52:50 -0700

    Update dist_link_neighbor_loader.py

--- torch_geometric/distributed/dist_link_neighbor_loader.py
+++ torch_geometric/distributed/dist_link_neighbor_loader.py
@@ -93,6 +93,15 @@ class DistLinkNeighborLoader(LinkLoader, DistLoader):
                 channel=channel,
                 concurrency=concurrency,
             )
+        else:
+            warn(
+                "`torch_geometric.distributed` has been deprecated since 2.7.0 and will "
+                "no longer be maintained. For distributed training, refer to our "
+                "tutorials on distributed training at "
+                "https://pytorch-geometric.readthedocs.io/en/latest/tutorial/distributed.html "  # noqa: E501
+                "or cuGraph examples at "
+                "https://github.com/rapidsai/cugraph-gnn/tree/main/python/cugraph-pyg/cugraph_pyg/examples",  # noqa: E501
+            )
 
         DistLoader.__init__(
             self,
