eca7f101359326c9e15950069a99a837d5afdb16
xnuohz <xnuohz@126.com>
2025-11-01 20:56:15 +0800

    update

--- test/llm/models/test_llm.py
+++ test/llm/models/test_llm.py
@@ -1,21 +1,24 @@
 import gc
 
+import pytest
 import torch
 from torch import Tensor
 
 from torch_geometric.llm.models import LLM
-from torch_geometric.testing import onlyRAG, withPackage
+from torch_geometric.testing import withPackage
 
 
-@onlyRAG
+# @onlyRAG
 @withPackage('transformers', 'accelerate')
-def test_llm() -> None:
+@pytest.mark.parametrize('n_gpus', [None, 1])
+def test_llm(n_gpus) -> None:
     question = ["Is PyG the best open-source GNN library?"]
     answer = ["yes!"]
 
     model = LLM(model_name='Qwen/Qwen3-0.6B', num_params=1,
                 dtype=torch.float16,
-                sys_prompt="You're an agent, answer my questions.")
+                sys_prompt="You're an agent, answer my questions.",
+                n_gpus=n_gpus)
     assert str(model) == 'LLM(Qwen/Qwen3-0.6B)'
 
     loss = model(question, answer)
