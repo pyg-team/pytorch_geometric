1ec3ccc4f55b3f11f3b6518049567e799f0cffa9
Reed Johns <115313701+vertumnal@users.noreply.github.com>
2025-12-08 18:23:53 -0700

    Implement error check for negative edge weights
    Add error handling for negative edge weights in GCN.

--- torch_geometric/nn/conv/gcn_conv.py
+++ torch_geometric/nn/conv/gcn_conv.py
@@ -233,6 +233,11 @@ class GCNConv(MessagePassing):
                              f"does not support bipartite message passing. "
                              f"Please try other layers such as 'SAGEConv' or "
                              f"'GraphConv' instead")
+            
+        if edge_weight is not None and (edge_weight < 0).any().item():
+            raise ValueError(f"'{self.__class__.__name__}' does not support "
+                             f"negative edge weights as input. Please try "
+                             f"'SignedConv' instead if they are necessary.")
 
         if self.normalize:
             if isinstance(edge_index, Tensor):
