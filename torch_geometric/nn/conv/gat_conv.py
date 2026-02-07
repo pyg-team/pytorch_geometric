111f7ad76f6934717d40ad5a44682cff273b814c
Akihiro Nitta <akihiro@kumo.ai>
2026-02-07 00:36:37 -0800

    Apply suggestions from code review

--- torch_geometric/nn/conv/gat_conv.py
+++ torch_geometric/nn/conv/gat_conv.py
@@ -379,7 +379,7 @@ class GATConv(MessagePassing):
         if self.bias is not None:
             out = out + self.bias
 
-        if return_attention_weights is True:
+        if return_attention_weights:
             if isinstance(edge_index, Tensor):
                 if is_torch_sparse_tensor(edge_index):
                     # TODO TorchScript requires to return a tuple
