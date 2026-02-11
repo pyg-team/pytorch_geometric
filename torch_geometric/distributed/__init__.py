dc74e3c630e424be32344f1bdcc043bfd6abb8a0
Akihiro Nitta <nitta@akihironitta.com>
2026-02-11 15:06:03 -0800

    Update torch_geometric/distributed/__init__.py

--- torch_geometric/distributed/__init__.py
+++ torch_geometric/distributed/__init__.py
@@ -1,7 +1,6 @@
 from .dist_context import DistContext
 from .local_feature_store import LocalFeatureStore
 from .local_graph_store import LocalGraphStore
-
 from .partition import Partitioner
 from .dist_neighbor_sampler import DistNeighborSampler
 from .dist_loader import DistLoader
