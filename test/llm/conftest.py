7e0d8f5cddf22fd29bae19e217c57963b726f5ba
Akihiro Nitta <nitta@akihironitta.com>
2026-03-17 18:26:47 +0000

    Fix marks

--- test/llm/conftest.py
+++ test/llm/conftest.py
@@ -1,6 +1,11 @@
+import pathlib
+
 import pytest
+
+LLM_DIR = pathlib.Path(__file__).parent
 
 
 def pytest_collection_modifyitems(items):
     for item in items:
-        item.add_marker(pytest.mark.rag)
+        if pathlib.Path(item.fspath).is_relative_to(LLM_DIR):
+            item.add_marker(pytest.mark.rag)
