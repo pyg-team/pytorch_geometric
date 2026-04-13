38f3fad68f03508eb16acadd2b0e808144382fdd
Rodrigue Govan <rodrigue.govan@gmail.com>
2026-04-13 13:05:32 +1100

    Fix: E501 line too long in spa_pool.py

--- torch_geometric/nn/pool/spa_pool.py
+++ torch_geometric/nn/pool/spa_pool.py
@@ -71,7 +71,8 @@ class SelectSageModule(torch.nn.Module):
 
 
 class SelectTopKModule(torch.nn.Module):
-    r"""Node selection module based on a learned projection vector (TopK-style).
+    r"""Node selection module based on a learned projection vector
+    (TopK-style).
 
     Args:
         in_channels (int): Size of each input node feature vector.
