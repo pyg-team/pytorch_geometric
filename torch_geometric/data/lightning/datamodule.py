04b535dfbaee85003ce5cb273fc8b7e33c4e9888
rusty1s <matthias.fey@tu-dortmund.de>
2026-04-16 06:32:26 +0000

    update

--- torch_geometric/data/lightning/datamodule.py
+++ torch_geometric/data/lightning/datamodule.py
@@ -11,11 +11,9 @@ from torch_geometric.sampler import BaseSampler, NeighborSampler
 from torch_geometric.typing import InputEdges, InputNodes, OptTensor
 
 try:
-    # from lightning.pytorch import LightningDataModule as _LightningDataModule
-    from pytorch_lightning import LightningDataModule as _LightningDataModule
+    from lightning.pytorch import LightningDataModule as _LightningDataModule
     _pl_is_available = True
 except ImportError:
-    print("IMPORT ERROR")
     try:
         from pytorch_lightning import \
             LightningDataModule as _LightningDataModule
