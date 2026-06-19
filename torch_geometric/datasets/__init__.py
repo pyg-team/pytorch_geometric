f2c6ceada926576ed5306c5dd782b184099b1d21
Domenico Tortorella <dom.tortorella@gmail.com>
2026-06-19 20:44:07 +0200

    Add OGX datasets
    OpenGraphXAI benchmark datasets from the paper "A method for the systematic generation of graph XAI benchmarks via Weisfeiler–Leman coloring", Data Mining and Knowledge Discovery, vol. 40(4), article no. 42 (2026)

--- torch_geometric/datasets/__init__.py
+++ torch_geometric/datasets/__init__.py
@@ -86,6 +86,7 @@ from .tag_dataset import TAGDataset
 from .city import CityNetwork
 from .teeth3ds import Teeth3DS
 from .graphland import GraphLandDataset
+from .ogx_datasets import OGXBenchmark
 
 from .dbp15k import DBP15K
 from .aminer import AMiner
@@ -209,6 +210,7 @@ homo_datasets = [
     'CityNetwork',
     'Teeth3DS',
     'GraphLandDataset',
+    'OGXBenchmark'
 ]
 
 hetero_datasets = [
